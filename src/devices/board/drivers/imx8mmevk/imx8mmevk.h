e4b2d4ba14e659a956b783817e38f40ba54ff0d5
Jagdish Gediya <jagdish.gediya@linaro.org>
2022-11-23 22:54:44 +0000

    [board][imx8mmevk] Add board initial driver and binding rules
    Tests: builds successfully, driver gets loaded, print in the thread
    function is visible on the console
    
    Change-Id: I8bd0a39e6aad5e4444f44b69160e4e1e50992006
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/763444
    Reviewed-by: Christopher Anderson <cja@google.com>
    Commit-Queue: Sarah Chan <spqchan@google.com>
    Reviewed-by: Sarah Chan <spqchan@google.com>
    Reviewed-by: Braden Kell <bradenkell@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>

---
+++ src/devices/board/drivers/imx8mmevk/imx8mmevk.h
@@ -0,0 +1,38 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_DEVICES_BOARD_DRIVERS_IMX8MMEVK_IMX8MMEVK_H_
+#define SRC_DEVICES_BOARD_DRIVERS_IMX8MMEVK_IMX8MMEVK_H_
+
+#include <fidl/fuchsia.hardware.platform.bus/cpp/driver/fidl.h>
+#include <threads.h>
+
+#include <ddktl/device.h>
+
+namespace imx8mm_evk {
+
+class Imx8mmEvk : public ddk::Device<Imx8mmEvk> {
+ public:
+  Imx8mmEvk(zx_device_t* parent, fdf::ClientEnd<fuchsia_hardware_platform_bus::PlatformBus> pbus,
+            fuchsia_hardware_platform_bus::TemporaryBoardInfo board_info)
+      : ddk::Device<Imx8mmEvk>(parent),
+        pbus_(std::move(pbus)),
+        board_info_(std::move(board_info)) {}
+
+  static zx_status_t Create(void* ctx, zx_device_t* parent);
+
+  void DdkRelease() { delete this; }
+
+ private:
+  zx_status_t Start();
+  int Thread();
+
+  const fdf::WireSyncClient<fuchsia_hardware_platform_bus::PlatformBus> pbus_;
+  const fuchsia_hardware_platform_bus::TemporaryBoardInfo board_info_;
+  thrd_t thread_;
+};
+
+}  // namespace imx8mm_evk
+
+#endif  // SRC_DEVICES_BOARD_DRIVERS_IMX8MMEVK_IMX8MMEVK_H_
