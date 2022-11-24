80924e8fe9eae64cd23e5b655980aa2027121ce1
Jay Zhuang <jayzhuang@google.com>
2022-11-24 07:02:05 +0000

    [internal_sdk] Include third_party/re2
    ... in @internal_sdk, this make it available as a dependency in Bazel
    build. This target is a transitive dependency of debug_agent, which is
    the first component we want to migrate.
    
    NOTE: `src/re2/stringpiece.h` is added to public headers because re2.h
    includes it, and if we don't make it available in the SDK, Bazel build
    would fail.
    
    fx set core.x64
    fx build bazel_workspace
    fx bazel build --platforms=//build/bazel/platforms:fuchsia_x64 //build/bazel/examples/hello_sdk:pkg
    
    Change-Id: I2d46ab9b269ba8d8f4ea42aa5233b98d8c2d3f5f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/761902
    Commit-Queue: Jay Zhuang <jayzhuang@google.com>
    Reviewed-by: Shai Barack <shayba@google.com>

--- build/bazel/examples/hello_sdk/hello_world.cc
+++ build/bazel/examples/hello_sdk/hello_world.cc
@@ -4,11 +4,15 @@
 
 #include <iostream>
 
+#include <re2/re2.h>
+
 #include "src/lib/files/file.h"
 #include "src/lib/files/glob.h"
 #include "src/lib/fxl/command_line.h"
 
 int main() {
+  // Unused, only tests if re2 include is working.
+  const re2::RE2 kUnused(".*_unused_re");
   std::cout << "Hello, my dear in-tree Bazel world!\n";
   fxl::CommandLine();
   files::IsFile("/tmp/test");
