00d4bfcb1b97a4589a7408063f87bd9d8ae1eec9
Megan Batty <mgnb@google.com>
2022-11-24 02:27:46 +0000

    [ffx][fho] Don't load tool metadata until needed
    To avoid issues around hermetic builds and just to in general reduce the
    amount of file IO we perform at startup, only identify subtools
    probabilistically and then validate them when needed for either command
    listing or running the command.
    
    Fixed: 114879
    Change-Id: I39f6b9613ab075c9f34ac5c5d08c37b2c97e45c3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/757781
    Reviewed-by: Andrew Davies <awdavies@google.com>
    Commit-Queue: Megan Batty <mgnb@google.com>

--- src/developer/ffx/lib/fho/src/search.rs
+++ src/developer/ffx/lib/fho/src/search.rs
@@ -17,6 +17,15 @@ use std::{
 
 use crate::{FhoToolMetadata, FhoVersion};
 
+/// Path information about a subtool
+#[derive(Clone)]
+struct SubToolLocation {
+    source: FfxToolSource,
+    name: String,
+    tool_path: PathBuf,
+    metadata_path: PathBuf,
+}
+
 /// A subtool discovered in a user's workspace or sdk
 #[derive(Clone)]
 pub struct ExternalSubTool {
@@ -28,7 +37,7 @@ pub struct ExternalSubTool {
 #[derive(Clone)]
 pub struct ExternalSubToolSuite {
     context: EnvironmentContext,
-    available_commands: HashMap<String, FfxToolInfo>,
+    available_commands: HashMap<String, SubToolLocation>,
 }
 
 #[async_trait::async_trait(?Send)]
@@ -68,7 +77,7 @@ impl ExternalSubToolSuite {
         args: &[&str],
     ) -> Option<ExternalSubTool> {
         let name = args.first().copied()?;
-        let cmd = match self.available_commands.get(name) {
+        let cmd = match self.available_commands.get(name).and_then(SubToolLocation::validate_tool) {
             Some(FfxToolInfo { path: Some(path), .. }) => {
                 let context = self.context.clone();
                 let cmd_line = ffx_cmd.clone();
@@ -92,7 +101,7 @@ impl ToolSuite for ExternalSubToolSuite {
     }
 
     fn command_list(&self) -> Vec<FfxToolInfo> {
-        self.available_commands.values().cloned().collect()
+        self.available_commands.values().filter_map(SubToolLocation::validate_tool).collect()
     }
 
     fn try_from_args(
@@ -136,7 +145,7 @@ impl FhoToolMetadata {
 
 /// Searches a set of directories for tools matching the path `ffx-<name>`
 /// and returns information about them based on known abis
-fn find_tools<P>(subtool_paths: &[P]) -> impl Iterator<Item = FfxToolInfo> + '_
+fn find_tools<P>(subtool_paths: &[P]) -> impl Iterator<Item = SubToolLocation> + '_
 where
     P: AsRef<Path>,
 {
@@ -144,35 +153,52 @@ where
         .iter()
         .filter_map(|path| {
             Some(std::fs::read_dir(path.as_ref()).ok()?.filter_map(move |entry| {
-                check_ffx_tool(FfxToolSource::Workspace, &entry.ok()?.path())
+                SubToolLocation::from_path(FfxToolSource::Workspace, &entry.ok()?.path())
             }))
         })
         .flatten()
 }
 
-/// Evaluate the given path for if it looks like a subtool based on the current
-/// requirements of FHO's protocol.
-fn check_ffx_tool(source: FfxToolSource, tool_path: &Path) -> Option<FfxToolInfo> {
-    let file_name = tool_path.file_name()?.to_str()?.to_owned();
-    if let Some(suffix) = file_name.strip_prefix("ffx-") {
-        let metadata_file = tool_path.with_extension("json");
-        let name = suffix.to_lowercase();
-        // require the presence of a metadata file
-        if metadata_file.exists() {
-            // bail early if for whatever reason we can't read the metadata.
-            let metadata: FhoToolMetadata =
-                File::open(metadata_file).ok().and_then(|f| serde_json::from_reader(f).ok())?;
-            // also if it requires an fho version we don't support
-            metadata.is_supported()?;
-            // ignore the tool if the metadata's name is incorrect
-            if metadata.name == name {
-                let description = metadata.description;
-                let path = Some(tool_path.to_owned());
-                return Some(FfxToolInfo { source, name, description, path });
+impl SubToolLocation {
+    /// Evaluate the given path for if it looks like a subtool based on filename and the
+    /// presence of a metadata file.
+    fn from_path(source: FfxToolSource, tool_path: &Path) -> Option<SubToolLocation> {
+        let file_name = tool_path.file_name()?.to_str()?;
+        if let Some(suffix) = file_name.strip_prefix("ffx-") {
+            let metadata_path = tool_path.with_extension("json");
+            let name = suffix.to_lowercase();
+            // require the presence of a metadata file
+            if metadata_path.exists() {
+                let tool_path = tool_path.to_owned();
+                return Some(SubToolLocation { source, name, tool_path, metadata_path });
             }
         }
+        None
     }
-    None
+
+    /// Loads the details of the metadata from the file to validate that it is a runnable
+    /// command with the current fho version and obtaining extra metadata from the metadata
+    /// file.
+    ///
+    /// Doing this in two steps avoids reading files unnecessarily until we want to either
+    /// run one or list it.
+    fn validate_tool(&self) -> Option<FfxToolInfo> {
+        // bail early if for whatever reason we can't read the metadata.
+        let metadata: FhoToolMetadata =
+            File::open(&self.metadata_path).ok().and_then(|f| serde_json::from_reader(f).ok())?;
+        // also if it requires an fho version we don't support
+        metadata.is_supported()?;
+        // ignore the tool if the metadata's name is incorrect
+        if metadata.name == self.name {
+            let source = self.source;
+            let name = metadata.name;
+            let description = metadata.description;
+            let path = Some(self.tool_path.to_owned());
+            Some(FfxToolInfo { source, name, description, path })
+        } else {
+            None
+        }
+    }
 }
 
 #[cfg(test)]
@@ -186,6 +212,10 @@ mod tests {
         NotThere,
     }
     use MockMetadata::*;
+
+    fn check_ffx_tool(source: FfxToolSource, path: &Path) -> Option<FfxToolInfo> {
+        SubToolLocation::from_path(source, path).as_ref().and_then(SubToolLocation::validate_tool)
+    }
 
     // Sets up a mock subtool in `dir` with the name `subtool_name` and, adjacent metadata based on the
     // `metadata` argument.
