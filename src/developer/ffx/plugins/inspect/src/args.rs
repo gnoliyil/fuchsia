05d1cbdea77eb0bff9b05544439787d558e8b132
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2022-11-28 19:48:16 +0000

    [ffx][inspect] Improve help message to explain how to read JSON
    ```
    λ ffx inspect --help
    Usage: ffx inspect <command> [<args>]
    
    Query component nodes exposed via the Inspect API.
    
    If you wish to see the JSON format of Inspect, you must pass `--machine json` to the `ffx` command.
    For example to see the Inspect JSON of all components running in the system, run:
    
    ```
    ffx --machine json inspect show
    ```
    
    Options:
      --help            display usage information
    
    Commands:
      apply-selectors   Apply selectors from a file interactively.
      list              List components that expose inspect
      list-accessors    List ArchiveAccessor files.
      list-files        List all inspect files
      selectors         List available selectors
      show              Print inspect hierarchies
    ```
    
    Bug: 116029
    
    Change-Id: Ib405fabde8d326a836be79254577b0d80702634b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/766002
    Reviewed-by: Clayton McCray <claytonmccray@google.com>
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>
    Commit-Queue: Miguel Flores <miguelfrde@google.com>

--- src/developer/ffx/plugins/inspect/src/args.rs
+++ src/developer/ffx/plugins/inspect/src/args.rs
@@ -9,7 +9,13 @@ use {argh::FromArgs, ffx_core::ffx_command, ffx_inspect_sub_command::SubCommand}
 #[argh(
     subcommand,
     name = "inspect",
-    description = "Query component nodes exposed via the Inspect API"
+    description = "Query component nodes exposed via the Inspect API. \n\n\
+    If you wish to see the JSON format of Inspect, you must pass `--machine json` to the `ffx` \
+    command. \n\
+    For example to see the Inspect JSON of all components running in the system, run: \n\n\
+    ```\n\
+    ffx --machine json inspect show\n\
+    ```"
 )]
 pub struct InspectCommand {
     #[argh(subcommand)]
