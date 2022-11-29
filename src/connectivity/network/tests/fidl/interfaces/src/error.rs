ded63071f2a72c28501214e630ec76512e7ad0f0
Peter Johnston <peterjohnston@google.com>
2022-11-29 18:19:28 +0000

    [network/tests] Move data race test to errors package
    This test exercises a race between a client sending data and the test
    closing the interface through which data is being sent. When run with
    Fast UDP enabled, it emits error logs, because some amount of messages
    have been sent by the client (and are enqueued in the zircon socket
    shared with the netstack) but have not yet been read by the netstack and
    written to the gVisor endpoint. This write emits an error log once the
    device has been closed.
    
    Move the test to the netstack-error-fidl-tests package, which allows
    high-severity logs which normally cause tests to fail.
    
    Fixed: 114866
    Multiply: netstack-interfaces-error-fidl-test
    Change-Id: I0f46dab9dbe75a83d583e9aaf43427d8054af54d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/768063
    Reviewed-by: Nick Brown <nickbrow@google.com>
    Commit-Queue: Peter Johnston <peterjohnston@google.com>
    Fuchsia-Auto-Submit: Peter Johnston <peterjohnston@google.com>

--- src/connectivity/network/tests/fidl/interfaces/src/error.rs
+++ src/connectivity/network/tests/fidl/interfaces/src/error.rs
@@ -8,10 +8,17 @@
 
 use std::collections::HashMap;
 
+use anyhow::Context as _;
 use fidl::endpoints::Proxy as _;
+use fidl_fuchsia_netemul_network as fnetemul_network;
 use fuchsia_zircon as zx;
-use net_declare::fidl_subnet;
-use netstack_testing_common::realms::{Netstack, NetstackVersion, TestSandboxExt as _};
+use futures::StreamExt as _;
+use net_declare::{fidl_subnet, std_ip};
+use netemul::RealmUdpSocket as _;
+use netstack_testing_common::{
+    interfaces,
+    realms::{Netstack, NetstackVersion, TestSandboxExt as _},
+};
 use netstack_testing_macros::variants_test;
 
 #[variants_test]
@@ -68,4 +75,116 @@ async fn interfaces_watcher_after_invalid_state_request<N: Netstack>(name: &str)
         NetstackVersion::Netstack2WithFastUdp => panic!("unexpected netstack version"),
     };
     assert_eq!(interfaces, expected);
+}
+
+/// Tests races between data traffic and closing a device.
+#[variants_test]
+async fn test_close_data_race<N: Netstack, E: netemul::Endpoint>(name: &str) {
+    let sandbox = netemul::TestSandbox::new().expect("create sandbox");
+    let net = sandbox.create_network("net").await.expect("create network");
+    let fake_ep = net.create_fake_endpoint().expect("create fake endpoint");
+    let realm = sandbox.create_netstack_realm::<N, _>(name).expect("create netstack realm");
+
+    // NOTE: We only run this test with IPv4 sockets since we only care about
+    // exciting the tx path, the domain is irrelevant.
+    const DEVICE_ADDRESS_IN_SUBNET: fidl_fuchsia_net::Subnet = fidl_subnet!("192.168.0.2/24");
+    // We're going to send data over a UDP socket to a multicast address so we
+    // skip ARP resolution.
+    const MCAST_ADDR: std::net::IpAddr = std_ip!("224.0.0.1");
+
+    let interface_state = realm
+        .connect_to_protocol::<fidl_fuchsia_net_interfaces::StateMarker>()
+        .expect("connect to protocol");
+    let event_stream = fidl_fuchsia_net_interfaces_ext::event_stream_from_state(&interface_state)
+        .expect("get interface event stream");
+    futures::pin_mut!(event_stream);
+    let mut if_map = HashMap::new();
+    for _ in 0..10u64 {
+        let dev = net
+            .create_endpoint::<E, _>("ep")
+            .await
+            .expect("create endpoint")
+            .into_interface_in_realm(&realm)
+            .await
+            .expect("add endpoint to Netstack");
+
+        let expect_enable = !(E::NETEMUL_BACKING == fnetemul_network::EndpointBacking::Ethertap
+            && N::VERSION == NetstackVersion::Netstack3);
+        assert_eq!(
+            expect_enable,
+            dev.control().enable().await.expect("send enable").expect("enable")
+        );
+        let () = dev.set_link_up(true).await.expect("bring device up");
+
+        let address_state_provider = interfaces::add_subnet_address_and_route_wait_assigned(
+            &dev,
+            DEVICE_ADDRESS_IN_SUBNET,
+            fidl_fuchsia_net_interfaces_admin::AddressParameters::EMPTY,
+        )
+        .await
+        .expect("add subnet address and route");
+        let () = address_state_provider.detach().expect("detach address lifetime");
+
+        // Create a socket and start sending data on it nonstop.
+        let fidl_fuchsia_net_ext::IpAddress(bind_addr) = DEVICE_ADDRESS_IN_SUBNET.addr.into();
+        let sock = fuchsia_async::net::UdpSocket::bind_in_realm(
+            &realm,
+            std::net::SocketAddr::new(bind_addr, 0),
+        )
+        .await
+        .expect("create socket");
+
+        // Keep sending data until writing to the socket fails.
+        let io_fut = async {
+            loop {
+                match sock
+                    .send_to(&[1u8, 2, 3, 4], std::net::SocketAddr::new(MCAST_ADDR, 1234))
+                    .await
+                {
+                    Ok(_sent) => {}
+                    // We expect only "os errors" to happen, ideally we'd look
+                    // only at specific errors (EPIPE, ENETUNREACH), but that
+                    // made this test very flaky due to the branching error
+                    // paths in gVisor when removing an interface.
+                    Err(e) if e.raw_os_error().is_some() => break Result::Ok(()),
+                    Err(e) => break Err(e).context("send_to error"),
+                }
+
+                // Enqueue some data on the rx path.
+                let () = fake_ep
+                    // We don't care that it's a valid frame, only that it excites
+                    // the rx path.
+                    .write(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
+                    .await
+                    .expect("send frame on fake_ep");
+
+                // Wait on a short timer to avoid too much log noise when
+                // running the test.
+                let () = fuchsia_async::Timer::new(fuchsia_async::Time::after(
+                    fuchsia_zircon::Duration::from_micros(10),
+                ))
+                .await;
+            }
+        };
+
+        let id = dev.id();
+        let drop_fut = async move {
+            let () = fuchsia_async::Timer::new(fuchsia_async::Time::after(
+                fuchsia_zircon::Duration::from_millis(3),
+            ))
+            .await;
+            std::mem::drop(dev);
+        };
+
+        let iface_dropped = fidl_fuchsia_net_interfaces_ext::wait_interface(
+            event_stream.by_ref(),
+            &mut if_map,
+            |if_map| (!if_map.contains_key(&id)).then_some(()),
+        );
+
+        let (io_result, iface_dropped, ()) =
+            futures::future::join3(io_fut, iface_dropped, drop_fut).await;
+        let () = io_result.expect("unexpected error on io future");
+        let () = iface_dropped.expect("observe interface removal");
+    }
 }
