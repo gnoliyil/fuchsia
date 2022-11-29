672f855dc535429b915ca425b00733271e623e80
Corey Tabaka <eieio@google.com>
2022-11-29 03:48:57 +0000

    [zircon][ktrace] Emit process record for kernel.
    Emit a process record for PID 0 named "kernel" to restore parity with
    the original ktrace importer.
    
    Test: Observe a process named "kernel" for PID 0 with expected kernel
          threads and per-CPU event tracks.
    Change-Id: I7e1f473fba2c9b20508a6432317ca428801041dd
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/765731
    Reviewed-by: Gwen Mittertreiner <gmtr@google.com>
    Commit-Queue: Corey Tabaka <eieio@google.com>

--- zircon/kernel/object/diagnostics.cc
+++ zircon/kernel/object/diagnostics.cc
@@ -363,6 +363,10 @@ void DumpHandlesForKoid(zx_koid_t id) {
 }  // namespace
 
 void ktrace_report_live_processes() {
+  // PID 0 refers to the kernel.
+  fxt_kernel_object(TAG_PROC_NAME, /*always*/ true, /* koid */ 0, ZX_OBJ_TYPE_PROCESS,
+                    fxt::StringRef("kernel"));
+
   auto walker = MakeProcessWalker([](ProcessDispatcher* process) {
     char name[ZX_MAX_NAME_LEN];
     process->get_name(name);
