f7108b2c3d91941e472ba6f6981a6843ee27249a
Sam Hansen <hansens@google.com>
2022-12-01 18:51:41 +0000

    [usb] tests/ style compliance for implementation headers
    This is the result of:
    
    for p in $(find src/devices/usb/tests -type d); do
      sed -i 's|#include "\([^/]\+\.h\)"|#include "'${p}'/\1"|g' $p/*.{cc,c,h}
    done && fx format-code
    
    Change-Id: I13dd86e2b89d34a5131e816e36b59777fb91ac2e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/769055
    Commit-Queue: Sam Hansen <hansens@google.com>
    Reviewed-by: Alex Legg <alexlegg@google.com>

--- src/devices/usb/tests/usb-hci-test/usb-hci-test-driver.cc
+++ src/devices/usb/tests/usb-hci-test/usb-hci-test-driver.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "usb-hci-test-driver.h"
+#include "src/devices/usb/tests/usb-hci-test/usb-hci-test-driver.h"
 
 #include <fuchsia/hardware/usb/c/banjo.h>
 #include <lib/ddk/debug.h>
