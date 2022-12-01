0f6c1028ea9574134c983b05881c0ac926091009
Ben Bergkamp <benbergkamp@google.com>
2022-12-01 19:00:44 +0000

    [sdk] Update copyright
    Change-Id: Ice4072522ee718195d6f818d750b964a1984d34a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/768307
    API-Review: Adam Barth <abarth@google.com>
    Reviewed-by: Adam Barth <abarth@google.com>

--- sdk/lib/fidl/cpp/wire/include/lib/fidl/cpp/wire/internal/client_continuation.h
+++ sdk/lib/fidl/cpp/wire/include/lib/fidl/cpp/wire/internal/client_continuation.h
@@ -1,6 +1,6 @@
-// Copyright 2022 The Fuchsia Authors. All rights reserved. Use of this source
-// code is governed by a BSD-style license that can be found in the LICENSE
-// file.
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #ifndef LIB_FIDL_CPP_WIRE_INCLUDE_LIB_FIDL_CPP_WIRE_INTERNAL_CLIENT_CONTINUATION_H_
 #define LIB_FIDL_CPP_WIRE_INCLUDE_LIB_FIDL_CPP_WIRE_INTERNAL_CLIENT_CONTINUATION_H_
