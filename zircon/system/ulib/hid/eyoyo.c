8467c318421270f7ae7fc9906ea39ec6c82a8a0d
James Robinson <jamesr@google.com>
2022-12-06 21:13:13 +0000

    [hid] Remove dependencies on deprecated FIDL C bindings
    The HID library declared but did not use deprecated FIDL C bindings.
    This removes the dependencies and updates the allowlist.
    
    Bug: 95833
    Change-Id: Ia13cf1c08f7ded2b29736a85feb314d9dda4b080
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/763902
    Reviewed-by: John Grossman <johngro@google.com>
    Fuchsia-Auto-Submit: James Robinson <jamesr@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- zircon/system/ulib/hid/eyoyo.c
+++ zircon/system/ulib/hid/eyoyo.c
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <fuchsia/hardware/input/c/fidl.h>
 #include <string.h>
 #include <zircon/errors.h>
 
