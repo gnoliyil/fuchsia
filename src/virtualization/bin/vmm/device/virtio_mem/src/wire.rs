7f02a20ae37441a38b7af980d9be466892342553
Alexander Radchenko <alerad@google.com>
2022-12-06 02:50:13 +0000

    [virtualization] Implement virtio_mem
    Implemented plug, unplug, unplug_all and get state for the virtio_mem device
    Add the integration test to plug memory, allocate and unplug the half of
    plugged memory.
    
    Bug: 100514
    Multiply: virtualization_mem_test
    Change-Id: I0b7ab3ca33da6593e81d0fcec0a2b6a20115f007
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/761682
    Reviewed-by: Tim Detwiler <tjdetwiler@google.com>
    Commit-Queue: Alexander Radchenko <alerad@google.com>

--- src/virtualization/bin/vmm/device/virtio_mem/src/wire.rs
+++ src/virtualization/bin/vmm/device/virtio_mem/src/wire.rs
@@ -4,8 +4,45 @@
 // Keep all consts and type defs for completeness.
 #![allow(dead_code)]
 
+use zerocopy::{AsBytes, FromBytes};
+
 pub use zerocopy::byteorder::little_endian::{U16 as LE16, U32 as LE32, U64 as LE64};
 
 // 5.15.1 Virtqueues
 //
 pub const GUESTREQUESTQ: u16 = 0;
+
+// 5.15.6 Device Operation
+pub const VIRTIO_MEM_REQ_PLUG: u16 = 0;
+pub const VIRTIO_MEM_REQ_UNPLUG: u16 = 1;
+pub const VIRTIO_MEM_REQ_UNPLUG_ALL: u16 = 2;
+pub const VIRTIO_MEM_REQ_STATE: u16 = 3;
+
+#[derive(Debug, Default, Copy, Clone, AsBytes, FromBytes)]
+#[repr(C, packed)]
+pub struct VirtioMemRequest {
+    pub ty: LE16,
+    pub _padding: [LE16; 3],
+    // payload is identical for PLUG, UNPLUG, UNPLUG_ALL and STATE
+    pub addr: LE64,
+    pub nb_blocks: LE16,
+    pub _payload_padding: [LE16; 3],
+}
+
+pub const VIRTIO_MEM_RESP_ACK: u16 = 0;
+pub const VIRTIO_MEM_RESP_NACK: u16 = 1;
+pub const VIRTIO_MEM_RESP_BUSY: u16 = 2;
+pub const VIRTIO_MEM_RESP_ERROR: u16 = 3;
+
+pub const VIRTIO_MEM_STATE_PLUGGED: u16 = 0;
+pub const VIRTIO_MEM_STATE_UNPLUGGED: u16 = 1;
+pub const VIRTIO_MEM_STATE_MIXED: u16 = 2;
+
+#[derive(Debug, Default, Copy, Clone, AsBytes, FromBytes)]
+#[repr(C, packed)]
+pub struct VirtioMemResponse {
+    pub ty: LE16,
+    pub _padding: [LE16; 3],
+    // payload is the same for all responses
+    pub state: LE16,
+}
