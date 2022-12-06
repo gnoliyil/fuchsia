f87ec66a7b0c4f89d1e1d86d854459476fd79c37
Megan Batty <mgnb@google.com>
2022-12-06 03:21:28 +0000

    [ffx][assembly] use ffx-assembly exe in builds
    This adds building the ffx-assembly "separate subtool" in parallel to
    its inclusion in ffx as a built-in plugin, and makes the build actions
    that previously ran it through ffx use it directly through ffx_action.
    
    Fixed: 116713
    Bug: 116712
    Change-Id: Ic55b7ea6504335575a5d2b0371f253a81706915d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/771162
    Commit-Queue: Megan Batty <mgnb@google.com>
    Reviewed-by: Shai Barack <shayba@google.com>
    Reviewed-by: Aaron Wood <aaronwood@google.com>

---
+++ src/developer/ffx/plugins/assembly/src/main.rs
@@ -0,0 +1,10 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+use ffx_assembly::AssemblyTool;
+use fho::FfxMain;
+
+#[fuchsia_async::run_singlethreaded]
+async fn main() {
+    AssemblyTool::execute_tool().await
+}
