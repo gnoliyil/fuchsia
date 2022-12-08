8b2690d005657194960c1e65e02a47f2bbff2d21
Richard Chen <richardrchen@google.com>
2022-12-08 00:41:34 +0000

    [ffx][component] Update ffx component copy help message
    This CL updates `ffx component copy`'s help message to include target
    to target and wildcards. It also fixes some of the formatting from the
    previous message.
    
    Change-Id: I4c4be5bede954640fefb8b8eea09030c9f9a988a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/773263
    Reviewed-by: Xyan Bhatnagar <xbhatnag@google.com>
    Reviewed-by: Yaneury Fermin <yaneury@google.com>
    Commit-Queue: Richard Chen <richardrchen@google.com>

--- src/developer/ffx/plugins/component/copy/src/args.rs
+++ src/developer/ffx/plugins/component/copy/src/args.rs
@@ -13,11 +13,12 @@ use {argh::FromArgs, ffx_core::ffx_command};
                    Paths may be a host or remote path and at least two paths must be supplied.",
     example = "To copy from target to host: \n\
                ffx component copy /some/moniker::/path/file./txt /path/on/host/file.txt \n\n\
-               To copy to target from host:
+               To copy to target from host: \n\
                ffx component copy /path/on/host/file.txt /some/moniker::/path/file.txt\n\n\
-               Target to target is also supported.\n\
-               To use wildcards in the above commands: \n\n
-               ffx component copy /some/moniker::/*  /some/directory",
+               To copy from target to target: \n\
+               ffx component copy /some/moniker::/path/file.txt /some/moniker::/path/file.txt\n\n\
+               To copy using wildcards: \n\
+               ffx component copy /some/moniker::/* /some/directory",
     note = "To learn more about the command see https://fuchsia.dev/fuchsia-src/development/sdk/ffx/copy-files-to-and-from-a-component"
 )]
 
@@ -25,7 +26,7 @@ pub struct CopyComponentCommand {
     #[argh(positional)]
     /// paths containing a host filepath or a path in a component's namespace.
     pub paths: Vec<String>,
-    /// flag used to print output to the console.
+    /// flag used to print messages such as successfully copied paths and paths ignored to the console.
     #[argh(switch, short = 'v')]
     pub verbose: bool,
 }
