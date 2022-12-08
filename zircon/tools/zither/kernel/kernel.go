e4d0d3da5c6fd862ca2a9c83c30cc7987e9b5b1b
Joshua Seaton <joshuaseaton@google.com>
2022-12-08 06:33:37 +0000

    [zither][kernel] Add kernel-wrappers.inc support
    This change completes the current iteration of the kernel backend,
    adding support for the generation of kazoo's kernel-wrappers.inc.
    
    Bug: 110294
    Change-Id: Idced9d97d437d8cc3020fe9f039e04ddabb47810
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/771368
    Fuchsia-Auto-Submit: Joshua Seaton <joshuaseaton@google.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- zircon/tools/zither/kernel/kernel.go
+++ zircon/tools/zither/kernel/kernel.go
@@ -21,28 +21,36 @@ var templates embed.FS
 // Kernel sources, given by its include path. Each file has a corresponding
 // template named "Generate-${file basename}".
 var includePaths = []string{
-	filepath.Join("lib", "syscalls", "zx-syscall-numbers.h"),
 	filepath.Join("lib", "syscalls", "category.inc"),
 	filepath.Join("lib", "syscalls", "kernel.inc"),
+	filepath.Join("lib", "syscalls", "kernel-wrappers.inc"),
 	filepath.Join("lib", "syscalls", "syscalls.inc"),
-	// TODO(fxbug.dev/110295):
-	// filepath.Join("lib", "syscalls", "kernel-wrappers.inc"),
+	filepath.Join("lib", "syscalls", "zx-syscall-numbers.h"),
 }
 
 type Generator struct {
 	fidlgen.Generator
 }
 
-func NewInternalGenerator(formatter fidlgen.Formatter) *Generator {
+func NewGenerator(formatter fidlgen.Formatter) *Generator {
 	gen := fidlgen.NewGenerator("ZirconKernelTemplates", templates, formatter, template.FuncMap{
 		"LowerCaseWithUnderscores": zither.LowerCaseWithUnderscores,
-		"Increment":                Increment,
+		"Increment":                func(n int) int { return n + 1 },
+		"IsUserOutHandle":          isUserOutHandle,
 		"KernelIncDecl": func(syscall zither.Syscall) string {
 			return SyscallCDecl(syscall, PointerViewKernel)
 		},
+		"LastParameterIndex": func(syscall zither.Syscall) int {
+			return len(syscall.Parameters) - 1
+		},
+		"ParameterType": func(param zither.SyscallParameter) string {
+			return cDeclParameterType(param, PointerViewUserspace, false)
+		},
+		"PassedAsPointer": passedAsPointer,
 		"SyscallIncDecl": func(syscall zither.Syscall) string {
 			return SyscallCDecl(syscall, PointerViewUserspace)
 		},
+		"UserOutHandles": userOutHandles,
 	})
 	return &Generator{*gen}
 }
@@ -81,4 +89,19 @@ func (gen *Generator) Generate(summaries []zither.FileSummary, outputDir string)
 // Template functions.
 //
 
-func Increment(n int) int { return n + 1 }
+// Whether the parameter represents a singular, handle out parameter (that is,
+// not part of an array of handle out parameters).
+func isUserOutHandle(param zither.SyscallParameter) bool {
+	return param.IsStrictOutput() && param.Type.Kind == zither.TypeKindHandle && !param.HasTag(zither.ParameterTagDecayedFromVector)
+}
+
+// Returns the list of user out handles.
+func userOutHandles(syscall zither.Syscall) []zither.SyscallParameter {
+	var outHandles []zither.SyscallParameter
+	for _, param := range syscall.Parameters {
+		if isUserOutHandle(param) {
+			outHandles = append(outHandles, param)
+		}
+	}
+	return outHandles
+}
