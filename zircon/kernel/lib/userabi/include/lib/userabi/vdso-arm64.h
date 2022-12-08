b34279aa19a25383cf2458a1f5d50a728e1d1227
Joshua Seaton <joshuaseaton@google.com>
2022-12-08 05:02:20 +0000

    [zircon][build] Limit use of kazoo internal build details
    This will ease an upcoming replacement of this logic with
    zither-specific build abstractions.
    
    Bug: 110294
    Change-Id: Icba0716c0babf72b21906a6c3a732e629946f515
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/772244
    Fuchsia-Auto-Submit: Joshua Seaton <joshuaseaton@google.com>
    Commit-Queue: Joshua Seaton <joshuaseaton@google.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>

---
+++ zircon/kernel/lib/userabi/include/lib/userabi/vdso-arm64.h
@@ -0,0 +1,12 @@
+// Copyright 2022 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#ifndef ZIRCON_KERNEL_LIB_USERABI_INCLUDE_LIB_USERABI_VDSO_ARM64_H_
+#define ZIRCON_KERNEL_LIB_USERABI_INCLUDE_LIB_USERABI_VDSO_ARM64_H_
+
+#define ARM64_SYSCALL_SPECULATION_BARRIER_SIZE (12)
+
+#endif  // ZIRCON_KERNEL_LIB_USERABI_INCLUDE_LIB_USERABI_VDSO_ARM64_H_
