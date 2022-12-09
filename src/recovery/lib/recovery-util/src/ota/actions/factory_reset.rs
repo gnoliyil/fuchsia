4ce72707bf282b37c016a88e923c41274bcb7dfa
Mike Comfoltey <comfoltey@google.com>
2022-12-09 04:40:21 +0000

    [recovery-ota] Add FactoryReset screen and testing
    Fixes a bug where the FactoryResetAction does not detect errors from the
    factory reset server. FactoryReset uses zx_status error codes, so only
    ZX_OK is a valid non-failure result.
    
    Add confirm screen for FactoryReset.
    Add negative test case to check error codes.
    
    Test: fx test -s 5 recovery-util-test ota_lib_unittests ota_unittests
    system_recovery_tests recovery_integration_test ota_integration_test
    with ui-v2 enabled
    Bug: b/260797450
    
    Change-Id: Idc557b3fab6d97036b86d3960b4d7c4ec3cebb1c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/773542
    Reviewed-by: Michael Brunson <mbrunson@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Eric Stone <ecstone@google.com>
    Fuchsia-Auto-Submit: Mike Comfoltey <comfoltey@google.com>

--- src/recovery/lib/recovery-util/src/ota/actions/factory_reset.rs
+++ src/recovery/lib/recovery-util/src/ota/actions/factory_reset.rs
@@ -7,6 +7,8 @@ use crate::ota::state_machine::Event;
 use fidl_fuchsia_recovery::{FactoryResetMarker, FactoryResetProxy};
 use fuchsia_async::{self as fasync};
 use fuchsia_component::client::connect_to_protocol;
+use fuchsia_zircon::sys::ZX_OK;
+use fuchsia_zircon_status::{self as zx_status};
 
 /// Asynchronously performs a factory reset.
 /// It reboots almost instantly
@@ -15,41 +17,51 @@ pub struct FactoryResetAction {}
 impl FactoryResetAction {
     pub fn run(event_sender: Box<dyn SendEvent>) {
         let proxy = connect_to_protocol::<FactoryResetMarker>().unwrap();
-        Self::run_with_proxy(event_sender, proxy)
+        let task = Self::run_with_proxy(event_sender, proxy);
+        // TODO(b/261822338): Actions should yield more control over their tasks
+        fasync::Task::local(task).detach();
     }
 
-    fn run_with_proxy(mut event_sender: Box<dyn SendEvent>, proxy: FactoryResetProxy) {
-        let task = async move {
-            println!("recovery: Executing factory reset command");
-            let result = proxy.reset().await;
-            if let Err(error) = result {
+    async fn run_with_proxy(mut event_sender: Box<dyn SendEvent>, proxy: FactoryResetProxy) {
+        // Wait 2 seconds so the UI transition is less jarring
+        fasync::Timer::new(fasync::Duration::from_seconds(2)).await;
+        println!("recovery: Executing factory reset command");
+        let result = proxy.reset().await;
+        match result {
+            Ok(status) if status != ZX_OK => {
+                event_sender.send(Event::Error(format!(
+                    "Factory Reset failed: {:?}",
+                    zx_status::Status::from_raw(status)
+                )));
+            }
+            Ok(_) => { /* ignore success */ }
+            Err(error) => {
                 event_sender.send(Event::Error(format!("Factory Reset failed: {:?}", error)));
             }
-        };
-        fasync::Task::local(task).detach();
+        }
     }
 }
 
 #[cfg(test)]
 mod test {
-    use super::FactoryResetAction;
+    use super::*;
     use crate::ota::controller::{MockSendEvent, SendEvent};
     use crate::ota::state_machine::Event;
     use anyhow::Error;
     use fidl_fuchsia_recovery::{FactoryResetMarker, FactoryResetProxy, FactoryResetRequest};
     use fuchsia_async::TimeoutExt;
     use fuchsia_async::{self as fasync};
-    use fuchsia_zircon::sys::ZX_OK;
     use fuchsia_zircon::Duration;
     use futures::{channel::mpsc, StreamExt, TryStreamExt};
-    use mockall::predicate::eq;
+    use mockall::predicate::{self, eq};
 
     const RESET_CALLED: i32 = 123456;
 
     // For future reference this test structure comes from
     // fxr/753732/4/src/recovery/lib/recovery-util/src/reboot.rs#49
-    fn create_mock_factory_reset_server() -> Result<(FactoryResetProxy, mpsc::Receiver<i32>), Error>
-    {
+    fn create_mock_factory_reset_server(
+        return_status: i32,
+    ) -> Result<(FactoryResetProxy, mpsc::Receiver<i32>), Error> {
         let (mut sender, receiver) = mpsc::channel(1);
         let (proxy, mut request_stream) =
             fidl::endpoints::create_proxy_and_stream::<FactoryResetMarker>()?;
@@ -59,10 +71,9 @@ mod test {
                 request_stream.try_next().await.expect("failed to read mock request")
             {
                 match request {
-                    // This is the only possible value
                     FactoryResetRequest::Reset { responder } => {
                         sender.start_send(RESET_CALLED).unwrap();
-                        responder.send(ZX_OK).ok();
+                        responder.send(return_status).ok();
                     }
                 }
             }
@@ -77,8 +88,23 @@ mod test {
         event_sender.expect_send().with(eq(Event::Cancel)).times(0).return_const(());
         let event_sender: Box<dyn SendEvent> = Box::new(event_sender);
 
-        let (proxy, mut receiver) = create_mock_factory_reset_server().unwrap();
-        FactoryResetAction::run_with_proxy(event_sender, proxy);
+        let (proxy, mut receiver) = create_mock_factory_reset_server(ZX_OK).unwrap();
+        FactoryResetAction::run_with_proxy(event_sender, proxy).await;
+        let status = receiver.next().on_timeout(Duration::from_seconds(5), || None).await.unwrap();
+        assert_eq!(status, RESET_CALLED);
+    }
+
+    #[fuchsia::test]
+    async fn test_reset_error() {
+        let mut event_sender = MockSendEvent::new();
+        event_sender.expect_send().with(eq(Event::Cancel)).times(0).return_const(());
+        let error_check = predicate::function(|e: &Event| matches!(e, Event::Error(_)));
+        event_sender.expect_send().with(error_check).once().return_const(());
+        let event_sender: Box<dyn SendEvent> = Box::new(event_sender);
+
+        let (proxy, mut receiver) =
+            create_mock_factory_reset_server(zx_status::Status::ACCESS_DENIED.into_raw()).unwrap();
+        FactoryResetAction::run_with_proxy(event_sender, proxy).await;
         let status = receiver.next().on_timeout(Duration::from_seconds(5), || None).await.unwrap();
         assert_eq!(status, RESET_CALLED);
     }
