7913f80bb6d7b207721afa0c0fbf1c42a5348038
Bruno Dal Bo <brunodalbo@google.com>
2022-12-12 17:41:45 +0000

    [asix-8879] Use netdevice migration
    Move asix-8879 driver to use netdevice migration shim.
    
    Update integration tests to reflect new identity on devfs.
    
    Multiply: asix-88179-integration-test :30
    
    Change-Id: I69c080b475a7b93a2da74f0474e5b214a1067216
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/773951
    Reviewed-by: Joon-Hyuk Han <hanjh@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Peter Johnston <peterjohnston@google.com>
    Fuchsia-Auto-Submit: Bruno Dal Bo <brunodalbo@google.com>

--- src/connectivity/ethernet/drivers/asix-88179/asix-88179-test.cc
+++ src/connectivity/ethernet/drivers/asix-88179/asix-88179-test.cc
@@ -5,7 +5,7 @@
 #include <endian.h>
 #include <fcntl.h>
 #include <fidl/fuchsia.hardware.ax88179/cpp/wire.h>
-#include <fidl/fuchsia.hardware.ethernet/cpp/wire.h>
+#include <lib/async-loop/testing/cpp/real_loop.h>
 #include <lib/component/incoming/cpp/service_client.h>
 #include <lib/fdio/cpp/caller.h>
 #include <lib/fdio/fdio.h>
@@ -13,23 +13,22 @@
 #include <lib/fzl/vmo-mapper.h>
 #include <lib/usb-virtual-bus-launcher/usb-virtual-bus-launcher.h>
 #include <lib/zx/clock.h>
-#include <zircon/device/ethernet.h>
 
 #include <usb/cdc.h>
 #include <usb/usb.h>
 #include <zxtest/zxtest.h>
 
 #include "asix-88179-regs.h"
+#include "src/connectivity/lib/network-device/cpp/network_device_client.h"
 
 namespace usb_ax88179 {
 namespace {
 
 using usb_virtual::BusLauncher;
 
-namespace ethernet = fuchsia_hardware_ethernet;
 namespace ax88179 = fuchsia_hardware_ax88179;
 
-class UsbAx88179Test : public zxtest::Test {
+class UsbAx88179Test : public zxtest::Test, loop_fixture::RealLoop {
  public:
   void SetUp() override {
     auto bus = BusLauncher::Create();
@@ -72,11 +71,11 @@ class UsbAx88179Test : public zxtest::Test {
     config_descs.push_back(std::move(config_desc));
     ASSERT_OK(bus_->SetupPeripheralDevice(std::move(device_desc), std::move(config_descs)));
 
-    fbl::unique_fd fd(openat(bus_->GetRootFd(), "class/ethernet", O_RDONLY));
+    fbl::unique_fd fd(openat(bus_->GetRootFd(), "class/network", O_RDONLY));
     while (fdio_watch_directory(fd.get(), usb_virtual_bus::WaitForAnyFile, ZX_TIME_INFINITE,
                                 dev_path) != ZX_ERR_STOP) {
     }
-    *dev_path = fbl::String::Concat({fbl::String("class/ethernet/"), *dev_path});
+    *dev_path = fbl::String::Concat({fbl::String("class/network/"), *dev_path});
 
     fd.reset(openat(bus_->GetRootFd(), "class/test-asix-function", O_RDONLY));
     while (fdio_watch_directory(fd.get(), usb_virtual_bus::WaitForAnyFile, ZX_TIME_INFINITE,
@@ -86,42 +85,44 @@ class UsbAx88179Test : public zxtest::Test {
         fbl::String::Concat({fbl::String("class/test-asix-function/"), *test_function_path});
   }
 
-  void ConnectEthernetClient() {
+  void ConnectNetdeviceClient() {
     fdio_cpp::UnownedFdioCaller caller(bus_->GetRootFd());
-    zx::result controller =
-        component::ConnectAt<ethernet::Controller>(caller.directory(), dev_path_);
-    ASSERT_OK(controller);
-    zx::result endpoints = fidl::CreateEndpoints<ethernet::Device>();
-    ASSERT_OK(endpoints);
-    auto& [client, server] = endpoints.value();
-    ASSERT_OK(fidl::WireCall(controller.value())->OpenSession(std::move(server)).status());
-    ethernet_client_.Bind(std::move(client));
+    zx::result instance = component::ConnectAt<fuchsia_hardware_network::DeviceInstance>(
+        caller.directory(), dev_path_);
+    ASSERT_OK(instance);
 
-    // Get device information
-    const fidl::WireResult info_result = ethernet_client_->GetInfo();
-    ASSERT_OK(info_result);
-    const fuchsia_hardware_ethernet::wire::Info& info = info_result.value().info;
-    const fidl::WireResult fifos_result = ethernet_client_->GetFifos();
-    ASSERT_OK(fifos_result);
-    const fit::result fifos_response = fifos_result.value();
-    ASSERT_TRUE(fifos_response.is_ok(), "%s", zx_status_get_string(fifos_response.error_value()));
-    fuchsia_hardware_ethernet::wire::Fifos& fifos = fifos_response.value()->fifos;
-    // Calculate optimal size of VMO, and set up RX and TX buffers.
-    size_t optimal_vmo_size = (fifos.rx_depth * info.mtu) + (fifos.tx_depth * info.mtu);
-    zx::vmo vmo;
-    fzl::VmoMapper mapper;
-    ASSERT_OK(mapper.CreateAndMap(optimal_vmo_size, ZX_VM_FLAG_PERM_READ | ZX_VM_FLAG_PERM_WRITE,
-                                  nullptr, &vmo));
-    auto set_io_buffer_result = ethernet_client_->SetIoBuffer(std::move(vmo));
-    ASSERT_OK(set_io_buffer_result.status());
+    zx::result device_endpoints = fidl::CreateEndpoints<fuchsia_hardware_network::Device>();
+    ASSERT_OK(device_endpoints);
+    auto& [device_client, device_server] = device_endpoints.value();
+    ASSERT_OK(fidl::WireCall(instance.value())->GetDevice(std::move(device_server)));
 
-    rx_fifo_ = std::move(fifos.rx);
-    tx_fifo_ = std::move(fifos.tx);
+    client_ = std::make_unique<network::client::NetworkDeviceClient>(std::move(device_client),
+                                                                     dispatcher());
+    client_->SetErrorCallback([this](zx_status_t error) {
+      loop().Quit();
+      ADD_FATAL_FAILURE("client failed with %s", zx_status_get_string(error));
+    });
+
+    std::optional<zx::result<std::vector<fuchsia_hardware_network::wire::PortId>>> ports;
+    client_->GetPorts([&ports](auto found_ports) { ports = std::move(found_ports); });
+    RunLoopUntil([&ports]() { return ports.has_value(); });
+    ASSERT_OK(ports.value());
+    ASSERT_EQ(ports.value().value().size(), 1ul);
+    port_id_ = ports.value().value()[0];
+    {
+      std::optional<zx_status_t> status;
+      client_->OpenSession("test_session", [&status](zx_status_t result) { status = result; });
+      RunLoopUntil([&status]() { return status.has_value(); });
+      ASSERT_OK(status.value());
+    }
   }
 
   void StartDevice() {
-    auto start_result = ethernet_client_->Start();
-    ASSERT_OK(start_result.status());
+    std::optional<zx_status_t> status;
+    client_->AttachPort(port_id_, {fuchsia_hardware_network::wire::FrameType::kEthernet},
+                        [&status](zx_status_t result) { status = result; });
+    RunLoopUntil([&status]() { return status.has_value(); });
+    ASSERT_OK(status.value());
   }
 
   void SetDeviceOnline() {
@@ -130,74 +131,74 @@ class UsbAx88179Test : public zxtest::Test {
     ASSERT_OK(test_client_end.status_value());
     fidl::WireSyncClient test_client{std::move(*test_client_end)};
 
-    // Ensure SIGNAL_STATUS is de-asserted before we set it.
-    ASSERT_OK(ethernet_client_->GetStatus().status());
-
     auto online_result = test_client->SetOnline(true);
     ASSERT_OK(online_result.status());
     auto result = online_result->status;
     ASSERT_OK(result);
   }
 
-  ethernet::wire::DeviceStatus GetDeviceStatus() {
-    auto status_result = ethernet_client_->GetStatus();
-    ZX_ASSERT(status_result.status() == ZX_OK);
-    return status_result->device_status;
+  zx::result<bool> GetDeviceOnline() {
+    std::optional<fuchsia_hardware_network::wire::PortStatus> status;
+    zx::result handle =
+        client_->WatchStatus(port_id_, [&status](auto new_status) { status = new_status; });
+    if (handle.is_error()) {
+      return handle.take_error();
+    }
+    RunLoopUntil([&status]() { return status.has_value(); });
+    return zx::ok(status.value().flags() & fuchsia_hardware_network::wire::StatusFlags::kOnline);
   }
 
-  void ExpectStatusOnline() {
-    // Two attempts, as SIGNAL_STATUS is triggered by both ethernet.cc starting
-    // up and also by our fake function driver. As we can't control the delivery
-    // order, confirm that we get to online eventually.
-    for (int tries = 0; tries < 2; ++tries) {
-      zx_signals_t pending;
-      ASSERT_OK(rx_fifo_.wait_one(ethernet::wire::kSignalStatus, zx::time::infinite(), &pending));
-      ASSERT_EQ((pending & ethernet::wire::kSignalStatus), ethernet::wire::kSignalStatus);
-      if (GetDeviceStatus() & ethernet::wire::DeviceStatus::kOnline) {
-        return;
-      }
-    }
-    ADD_FATAL_FAILURE();
+  void WaitDeviceOnline() {
+    bool online = false;
+    zx::result handle = client_->WatchStatus(
+        port_id_, [&online](fuchsia_hardware_network::wire::PortStatus new_status) {
+          EXPECT_TRUE(new_status.has_flags());
+          online = static_cast<bool>(new_status.flags() &
+                                     fuchsia_hardware_network::wire::StatusFlags::kOnline);
+        });
+    ASSERT_OK(handle);
+    RunLoopUntil([&online]() { return online; });
   }
 
  protected:
   std::optional<BusLauncher> bus_;
   fbl::String dev_path_;
   fbl::String test_function_path_;
-  fidl::WireSyncClient<ethernet::Device> ethernet_client_;
-  zx::fifo rx_fifo_;
-  zx::fifo tx_fifo_;
+  std::unique_ptr<network::client::NetworkDeviceClient> client_;
+  fuchsia_hardware_network::wire::PortId port_id_;
 };
 
 TEST_F(UsbAx88179Test, SetupShutdownTest) { ASSERT_NO_FATAL_FAILURE(); }
 
 TEST_F(UsbAx88179Test, OfflineByDefault) {
-  ASSERT_NO_FATAL_FAILURE(ConnectEthernetClient());
+  ASSERT_NO_FATAL_FAILURE(ConnectNetdeviceClient());
 
   ASSERT_NO_FATAL_FAILURE(StartDevice());
 
-  ASSERT_FALSE(GetDeviceStatus() & ethernet::wire::DeviceStatus::kOnline);
+  zx::result online = GetDeviceOnline();
+  ASSERT_OK(online);
+  ASSERT_FALSE(online.value());
 }
 
 TEST_F(UsbAx88179Test, SetOnlineAfterStart) {
-  ASSERT_NO_FATAL_FAILURE(ConnectEthernetClient());
+  ASSERT_NO_FATAL_FAILURE(ConnectNetdeviceClient());
 
   ASSERT_NO_FATAL_FAILURE(StartDevice());
 
   ASSERT_NO_FATAL_FAILURE(SetDeviceOnline());
 
-  ASSERT_NO_FATAL_FAILURE(ExpectStatusOnline());
+  ASSERT_NO_FATAL_FAILURE(WaitDeviceOnline());
 }
 
 // This is for https://fxbug.dev/40786#c41.
 TEST_F(UsbAx88179Test, SetOnlineBeforeStart) {
-  ASSERT_NO_FATAL_FAILURE(ConnectEthernetClient());
+  ASSERT_NO_FATAL_FAILURE(ConnectNetdeviceClient());
 
   ASSERT_NO_FATAL_FAILURE(SetDeviceOnline());
 
   ASSERT_NO_FATAL_FAILURE(StartDevice());
 
-  ASSERT_NO_FATAL_FAILURE(ExpectStatusOnline());
+  ASSERT_NO_FATAL_FAILURE(WaitDeviceOnline());
 }
 
 }  // namespace
