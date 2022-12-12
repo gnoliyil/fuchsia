986d959eec6c2bdc5ba451d0a696cd1a31bf17c6
Josh Conner <joshconner@google.com>
2022-12-12 21:17:53 +0000

    [bt-hci-marvell] Device initialization
    Add support for performing device initialization, started by
    ddkInit and run on a separate thread.
    
    Create a DeviceOracle class that provides static information about
    the target device.
    
    Split the test classes into multiple files covering:
    * BtHciMockSdio a class derived from ddk::MockSdio that
      simulates some register operations and limits the sdio calls
      that we track.
    * BtHciMockSdioTest: Provides the basic test framework for
      tests that use BtHciMockSdio, including device instantiation,
      initialization, and teardown.
    
    Test: fx test bt-hci-marvell-tests
    Change-Id: I394efd0209fa311a13e9696cf5fb5ba33abf812e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/771530
    Commit-Queue: Joshua Conner <joshconner@google.com>
    Reviewed-by: Ben Lawson <benlawson@google.com>

--- src/connectivity/bluetooth/hci/vendor/marvell/bt-hci-marvell.cc
+++ src/connectivity/bluetooth/hci/vendor/marvell/bt-hci-marvell.cc
@@ -4,6 +4,7 @@
 
 #include "src/connectivity/bluetooth/hci/vendor/marvell/bt-hci-marvell.h"
 
+#include <lib/async/cpp/task.h>
 #include <lib/ddk/driver.h>
 
 #include <fbl/alloc_checker.h>
@@ -43,7 +44,172 @@ zx_status_t BtHciMarvell::Bind(void* ctx, zx_device_t* parent) {
   return ZX_OK;
 }
 
-void BtHciMarvell::DdkInit(ddk::InitTxn txn) { txn.Reply(ZX_OK); }
+void BtHciMarvell::DdkInit(ddk::InitTxn txn) {
+  // Start up an independent driver thread
+  loop_.emplace(&kAsyncLoopConfigNoAttachToCurrentThread);
+  zx_status_t status = loop_->StartThread("bt-hci-marvell");
+  if (status != ZX_OK) {
+    zxlogf(ERROR, "Failed to start thread: %s", zx_status_get_string(status));
+    OnInitComplete(status, std::move(txn));
+    return;
+  }
+  dispatcher_ = loop_->dispatcher();
+
+  // Continue initialization in the new thread.
+  async::PostTask(dispatcher_, [this, txn = std::move(txn)]() mutable {
+    zx_status_t status = Init();
+    OnInitComplete(status, std::move(txn));
+  });
+}
+
+zx_status_t BtHciMarvell::Init() {
+  zx_status_t status;
+  sdio_hw_info_t hw_info;
+
+  // Retrieve our product ID
+  if ((status = sdio_.GetDevHwInfo(&hw_info)) != ZX_OK) {
+    zxlogf(ERROR, "Failed to find SDIO hardware info: %s", zx_status_get_string(status));
+    return status;
+  }
+  uint32_t product_id = hw_info.func_hw_info.product_id;
+
+  // Determine if this device is supported. If so, instantiate our oracle.
+  zx::result result = DeviceOracle::Create(product_id);
+  if (result.is_error()) {
+    zxlogf(ERROR, "Unable to find supported device matching product id %0x" PRIu32, product_id);
+    return result.error_value();
+  }
+  device_oracle_ = std::move(result.value());
+
+  if ((status = sdio_.EnableFn()) != ZX_OK) {
+    zxlogf(ERROR, "Failed to enable SDIO function: %s", zx_status_get_string(status));
+    return status;
+  }
+
+  // Set the SDIO block size
+  uint16_t block_size = device_oracle_->GetSdioBlockSize();
+  if ((status = sdio_.UpdateBlockSize(block_size, false)) != ZX_OK) {
+    zxlogf(ERROR, "Failed to set SDIO block size to %u: %s", block_size,
+           zx_status_get_string(status));
+    return status;
+  }
+
+  if ((status = LoadFirmware()) != ZX_OK) {
+    zxlogf(ERROR, "Failed to load firmware: %s", zx_status_get_string(status));
+    return status;
+  }
+
+  // Retrieve the address of the IO Port, which is where we will perform read/writes of HCI packets
+  uint32_t ioport_addr_reg_addr = device_oracle_->GetRegAddrIoportAddr();
+  if ((status = Read24(ioport_addr_reg_addr, &ioport_addr_)) != ZX_OK) {
+    return status;
+  }
+  zxlogf(INFO, "IO port address: %#x", ioport_addr_);
+
+  // Set interrupt behavior to "clear-on-read"
+  uint8_t rsr = 0xff;
+  uint32_t rsr_reg_addr = device_oracle_->GetRegAddrInterruptRsr();
+  if ((status = Read8(rsr_reg_addr, &rsr)) != ZX_OK) {
+    return status;
+  }
+  rsr &= ~kRsrClearOnReadMask;
+  rsr |= kRsrClearOnReadValue;
+  if ((status = Write8(rsr_reg_addr, rsr)) != ZX_OK) {
+    return status;
+  }
+
+  // Configure interrupts to automatically re-enable
+  uint8_t misc_cfg;
+  uint32_t misc_cfg_reg_addr = device_oracle_->GetRegAddrMiscCfg();
+  if ((status = Read8(misc_cfg_reg_addr, &misc_cfg)) != ZX_OK) {
+    return status;
+  }
+  misc_cfg &= ~kMiscCfgAutoReenableMask;
+  misc_cfg |= kMiscCfgAutoReenableValue;
+  if ((status = Write8(misc_cfg_reg_addr, misc_cfg)) != ZX_OK) {
+    return status;
+  }
+
+  return status;
+}
+
+void BtHciMarvell::OnInitComplete(zx_status_t status, ddk::InitTxn txn) {
+  if (status != ZX_OK) {
+    zxlogf(ERROR, "Device initialization failed: %s", zx_status_get_string(status));
+  }
+  txn.Reply(status);
+}
+
+// For now, we rely on wlan to load the firmware image for us, we just have to wait for it.
+zx_status_t BtHciMarvell::LoadFirmware() {
+  uint32_t fw_status_reg_addr = device_oracle_->GetRegAddrFirmwareStatus();
+
+  for (size_t secs_left = 0; secs_left < kFirmwareWaitSeconds; secs_left++) {
+    zx_status_t status;
+    uint16_t fw_status = 0xffff;
+
+    status = Read16(fw_status_reg_addr, &fw_status);
+    if (status == ZX_OK && fw_status == kFirmwareStatusReady) {
+      return ZX_OK;
+    }
+    zx::nanosleep(zx::deadline_after(zx::sec(1)));
+  }
+  return ZX_ERR_TIMED_OUT;
+}
+
+zx_status_t BtHciMarvell::Read8(uint32_t addr, uint8_t* out_value) {
+  zx_status_t status = sdio_.DoRwByte(/* write */ false, addr, /* write_byte */ 0, out_value);
+  if (status != ZX_OK) {
+    zxlogf(ERROR, "Failure reading from address %#x: %s", addr, zx_status_get_string(status));
+    *out_value = 0xff;
+  }
+  return status;
+}
+
+// Read two adjacent (but not necessarily aligned) registers, and return the value adjusted for
+// host endianness.
+zx_status_t BtHciMarvell::Read16(uint32_t addr, uint16_t* out_value) {
+  zx_status_t status;
+  *out_value = 0xffff;
+  uint8_t lobits, hibits;
+
+  // Block reads have to be word-aligned, so it's a bit more straightforward to just read the
+  // bytes independently.
+  if (((status = Read8(addr, &lobits)) == ZX_OK) &&
+      ((status = Read8(addr + 1, &hibits)) == ZX_OK)) {
+    *out_value = hibits;
+    (*out_value) <<= CHAR_BIT;
+    *out_value |= lobits;
+  }
+  return status;
+}
+
+// Read three adjacent (but not necessarily aligned) registers, and return the value adjusted for
+// host endianness.
+zx_status_t BtHciMarvell::Read24(uint32_t addr, uint32_t* out_value) {
+  zx_status_t status;
+  *out_value = 0xffff;
+  uint8_t lobits, midbits, hibits;
+
+  if (((status = Read8(addr, &lobits)) == ZX_OK) &&
+      ((status = Read8(addr + 1, &midbits)) == ZX_OK) &&
+      ((status = Read8(addr + 2, &hibits)) == ZX_OK)) {
+    *out_value = hibits;
+    (*out_value) <<= CHAR_BIT;
+    *out_value |= midbits;
+    (*out_value) <<= CHAR_BIT;
+    *out_value |= lobits;
+  }
+  return status;
+}
+
+zx_status_t BtHciMarvell::Write8(uint32_t addr, uint8_t value) {
+  zx_status_t status = sdio_.DoRwByte(/* write */ true, addr, value, /* out_read_byte */ nullptr);
+  if (status != ZX_OK) {
+    zxlogf(ERROR, "Failure writing to address %#x: %s", addr, zx_status_get_string(status));
+  }
+  return status;
+}
 
 void BtHciMarvell::DdkUnbind(ddk::UnbindTxn txn) { txn.Reply(); }
 
---
+++ src/connectivity/bluetooth/hci/vendor/marvell/tests/bt-hci-mock-sdio-test.h
@@ -0,0 +1,150 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_CONNECTIVITY_BLUETOOTH_HCI_VENDOR_MARVELL_TESTS_BT_HCI_MOCK_SDIO_TEST_H_
+#define SRC_CONNECTIVITY_BLUETOOTH_HCI_VENDOR_MARVELL_TESTS_BT_HCI_MOCK_SDIO_TEST_H_
+
+#include <fuchsia/hardware/sdio/cpp/banjo-mock.h>
+
+#include <zxtest/zxtest.h>
+
+#include "src/connectivity/bluetooth/hci/vendor/marvell/bt-hci-marvell.h"
+#include "src/connectivity/bluetooth/hci/vendor/marvell/tests/bt-hci-mock-sdio.h"
+#include "src/devices/testing/mock-ddk/mock-device.h"
+
+namespace bt_hci_marvell {
+
+class BtHciMockSdioTest : public zxtest::Test {
+ public:
+  // 88W8987
+  static constexpr uint32_t kSimProductId = 0x914a;
+
+  // The value we'll return for reads from the ioport address registers
+  static constexpr uint32_t kSimIoportAddr = 0x4a4d43;
+  static constexpr uint8_t kSimIoportAddrLow = kSimIoportAddr & 0xff;
+  static constexpr uint8_t kSimIoportAddrMid = (kSimIoportAddr & 0xff00) >> CHAR_BIT;
+  static constexpr uint8_t kSimIoportAddrHigh = (kSimIoportAddr & 0xff0000) >> (CHAR_BIT * 2);
+
+  // Go through all of the steps to set up our test environment: create our two devices (SDIO fake
+  // and our dut) and driver interfaces (SDIO fake and our own driver). Performs the simulated bind
+  // and calls DdkInit().
+  void SetUp() override {
+    zx::result<std::unique_ptr<DeviceOracle>> result;
+    result = DeviceOracle::Create(kSimProductId);
+    ASSERT_TRUE(result.is_ok());
+    device_oracle_ = std::move(result.value());
+
+    zxtest::Test::SetUp();
+
+    // Construct the root of our mock device tree
+    const sdio_protocol_t* sdio_proto = mock_sdio_device_.GetProto();
+    fake_parent_->AddProtocol(ZX_PROTOCOL_SDIO, sdio_proto->ops, sdio_proto->ctx);
+
+    // Verify basic device initialization
+    sdio_hw_info_t hw_info;
+    hw_info.func_hw_info.product_id = kSimProductId;
+    uint16_t expected_block_size = device_oracle_->GetSdioBlockSize();
+    mock_sdio_device_.ExpectGetDevHwInfo(ZX_OK, hw_info)
+        .ExpectEnableFn(ZX_OK)
+        .ExpectUpdateBlockSize(ZX_OK, expected_block_size, /* deflt */ false);
+
+    // Expect a read from the firmware status registers
+    uint32_t fw_status_addr = device_oracle_->GetRegAddrFirmwareStatus();
+    mock_sdio_device_
+        .ExpectDoRwByte(ZX_OK, /* write */ false, fw_status_addr, /* write_byte */ 0,
+                        kFirmwareStatusReady & 0xff)
+        .ExpectDoRwByte(ZX_OK, /* write */ false, fw_status_addr + 1,
+                        /* write_byte */ 0, (kFirmwareStatusReady & 0xff00) >> CHAR_BIT);
+
+    // Read the IOPort address
+    uint32_t ioport_addr = device_oracle_->GetRegAddrIoportAddr();
+    mock_sdio_device_
+        .ExpectDoRwByte(ZX_OK, /* write */ false, ioport_addr, /* write_byte */ 0,
+                        kSimIoportAddrLow)
+        .ExpectDoRwByte(ZX_OK, /* write */ false, ioport_addr + 1, /* write_byte */ 0,
+                        kSimIoportAddrMid)
+        .ExpectDoRwByte(ZX_OK, /* write */ false, ioport_addr + 2, /* write_byte */ 0,
+                        kSimIoportAddrHigh);
+
+    // Set "clear-on-read" bits
+    uint32_t rsr_addr = device_oracle_->GetRegAddrInterruptRsr();
+    uint8_t rsr_value = 0;
+    mock_sdio_device_.ExpectDoRwByte(ZX_OK, /* write */ false, rsr_addr, /* write_byte */ 0,
+                                     /* out_read_byte */ rsr_value);
+    rsr_value &= ~kRsrClearOnReadMask;
+    rsr_value |= kRsrClearOnReadValue;
+    mock_sdio_device_.ExpectDoRwByte(ZX_OK, /* write */ true, rsr_addr, /* write_byte */ rsr_value,
+                                     /* out_read_byte */ 0);
+
+    // Set the "automatically re-enable interrupts" bits
+    uint32_t misc_cfg_addr = device_oracle_->GetRegAddrMiscCfg();
+    uint8_t misc_cfg_value = 0;
+    mock_sdio_device_.ExpectDoRwByte(ZX_OK, /* write */ false, misc_cfg_addr, /* write_byte */ 0,
+                                     /* out_read_byte */ misc_cfg_value);
+    misc_cfg_value &= ~kMiscCfgAutoReenableMask;
+    misc_cfg_value |= kMiscCfgAutoReenableValue;
+    mock_sdio_device_.ExpectDoRwByte(ZX_OK, /* write */ true, misc_cfg_addr,
+                                     /* write_byte */ misc_cfg_value, /* out_read_byte */ 0);
+
+    // Create our driver instance, which should bind to the SDIO mock
+    ASSERT_EQ(ZX_OK, BtHciMarvell::Bind(/* ctx */ nullptr, fake_parent_.get()));
+
+    // Find the device that represents our Marvell device in the mock device tree
+    dut_ = fake_parent_->GetLatestChild();
+    ASSERT_NOT_NULL(dut_);
+
+    // Find the Marvell driver
+    driver_instance_ = dut_->GetDeviceContext<BtHciMarvell>();
+    ASSERT_NOT_NULL(driver_instance_);
+
+    // Call DdkInit
+    Init();
+
+    // Run all of the checks to verify that the expected calls into the mock SDIO API were made
+    // during initialization
+    ASSERT_NO_FATAL_FAILURE(mock_sdio_device_.VerifyAndClear());
+  }
+
+  void TearDown() override {
+    // Deconstruct the mock device tree
+    UnbindAndRelease();
+
+    zxtest::Test::TearDown();
+  }
+
+  void Init() {
+    // Invoke ddkInit()
+    dut_->InitOp();
+
+    // Verify that the Init operation returned a completion status
+    ASSERT_EQ(ZX_OK, dut_->WaitUntilInitReplyCalled());
+
+    // Verify that the status was success
+    ASSERT_EQ(ZX_OK, dut_->InitReplyCallStatus());
+  }
+
+  void UnbindAndRelease() {
+    // Flag our mock device for removal
+    device_async_remove(dut_);
+
+    // Remove from the mock device tree
+    mock_ddk::ReleaseFlaggedDevices(fake_parent_.get());
+  }
+
+ protected:
+  // The root of our MockDevice tree
+  std::shared_ptr<MockDevice> fake_parent_ = MockDevice::FakeRootParent();
+
+  // This is the object that will handle all of our SDIO calls to the fake parent
+  BtHciMockSdio mock_sdio_device_;
+
+ private:
+  std::unique_ptr<DeviceOracle> device_oracle_;
+  MockDevice* dut_;
+  BtHciMarvell* driver_instance_;
+};
+
+}  // namespace bt_hci_marvell
+
+#endif  // SRC_CONNECTIVITY_BLUETOOTH_HCI_VENDOR_MARVELL_TESTS_BT_HCI_MOCK_SDIO_TEST_H_
