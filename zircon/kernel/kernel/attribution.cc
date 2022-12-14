0bd2492e392151e3b0202c79ec31d02c996817ec
Luke Nicholson <lukenicholson@google.com>
2022-12-14 20:35:32 +0000

    [kbma] Introduce the kernel attribution object.
    Bug: 117196
    Change-Id: I1629dd7caedfea54bf5970fb726fb0843f6d54ff
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/773354
    Reviewed-by: Adrian Danis <adanis@google.com>
    Reviewed-by: Corey Tabaka <eieio@google.com>
    Reviewed-by: Rasha Eqbal <rashaeqbal@google.com>
    Commit-Queue: Luke Nicholson <lukenicholson@google.com>

---
+++ zircon/kernel/kernel/attribution.cc
@@ -0,0 +1,31 @@
+// Copyright 2022 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+#include "kernel/attribution.h"
+
+fbl::DoublyLinkedList<AttributionObject*> AttributionObject::all_attribution_objects_;
+
+AttributionObject AttributionObject::kernel_attribution_{0};
+
+void AttributionObject::KernelAttributionInit() TA_NO_THREAD_SAFETY_ANALYSIS {
+  AttributionObject::kernel_attribution_.Adopt();
+  all_attribution_objects_.push_back(&AttributionObject::kernel_attribution_);
+}
+
+AttributionObject::~AttributionObject() {
+  AttributionObject::RemoveAttributionFromGlobalList(this);
+}
+
+void AttributionObject::AddAttributionToGlobalList(AttributionObject* obj) {
+  ZX_DEBUG_ASSERT(obj != nullptr);
+  Guard<Mutex> guard{AllAttributionObjectsLock::Get()};
+  all_attribution_objects_.push_back(obj);
+}
+
+void AttributionObject::RemoveAttributionFromGlobalList(AttributionObject* obj) {
+  ZX_DEBUG_ASSERT(obj != nullptr);
+  Guard<Mutex> guard{AllAttributionObjectsLock::Get()};
+  all_attribution_objects_.erase(*obj);
+}
