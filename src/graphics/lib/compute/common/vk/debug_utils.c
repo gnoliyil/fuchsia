45b3ab5d727c617556ceb1532cec16196f73097e
Allan MacKinnon <allanmac@google.com>
2022-12-14 19:04:24 +0000

    [graphics][compute][common] Simplify VK_EXT_debug_utils initialization
    Harmonize the existing macros with similar PFN macros being used
    elsewhere.
    
    No change in behavior.
    
    Verified with debugger that the vkInstance PFNs are still being
    acquired.
    
    Change-Id: I3dd8ff183b4c9954438de9f19b205eb64f073063
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/775203
    Reviewed-by: Craig Stout <cstout@google.com>
    Commit-Queue: Allan MacKinnon <allanmac@google.com>

--- src/graphics/lib/compute/common/vk/debug_utils.c
+++ src/graphics/lib/compute/common/vk/debug_utils.c
@@ -16,25 +16,24 @@
 
 #define DU_PFN_TYPE(name_) PFN_##name_
 
-#define DU_PFN_INIT(instance_, name_)                                                              \
+#define DU_PFN_DECL(name_) DU_PFN_TYPE(name_) DU_PFN_NAME(name_) = NULL
+
+#define DU_PFN_INSTANCE_INIT(instance_, name_)                                                     \
   DU_PFN_NAME(name_) = (DU_PFN_TYPE(name_))vkGetInstanceProcAddr(instance_, #name_)
 
 //
 // The pfns default to NULL
 //
-
-// clang-format off
-DU_PFN_TYPE(vkSetDebugUtilsObjectNameEXT)    DU_PFN_NAME(vkSetDebugUtilsObjectNameEXT)    = NULL;
-DU_PFN_TYPE(vkSetDebugUtilsObjectTagEXT)     DU_PFN_NAME(vkSetDebugUtilsObjectTagEXT)     = NULL;
-DU_PFN_TYPE(vkQueueBeginDebugUtilsLabelEXT)  DU_PFN_NAME(vkQueueBeginDebugUtilsLabelEXT)  = NULL;
-DU_PFN_TYPE(vkQueueEndDebugUtilsLabelEXT)    DU_PFN_NAME(vkQueueEndDebugUtilsLabelEXT)    = NULL;
-DU_PFN_TYPE(vkQueueInsertDebugUtilsLabelEXT) DU_PFN_NAME(vkQueueInsertDebugUtilsLabelEXT) = NULL;
-DU_PFN_TYPE(vkCmdBeginDebugUtilsLabelEXT)    DU_PFN_NAME(vkCmdBeginDebugUtilsLabelEXT)    = NULL;
-DU_PFN_TYPE(vkCmdEndDebugUtilsLabelEXT)      DU_PFN_NAME(vkCmdEndDebugUtilsLabelEXT)      = NULL;
-DU_PFN_TYPE(vkCmdInsertDebugUtilsLabelEXT)   DU_PFN_NAME(vkCmdInsertDebugUtilsLabelEXT)   = NULL;
-DU_PFN_TYPE(vkCreateDebugUtilsMessengerEXT)  DU_PFN_NAME(vkCreateDebugUtilsMessengerEXT)  = NULL;
-DU_PFN_TYPE(vkSubmitDebugUtilsMessageEXT)    DU_PFN_NAME(vkSubmitDebugUtilsMessageEXT)    = NULL;
-// clang-format on
+DU_PFN_DECL(vkSetDebugUtilsObjectNameEXT);
+DU_PFN_DECL(vkSetDebugUtilsObjectTagEXT);
+DU_PFN_DECL(vkQueueBeginDebugUtilsLabelEXT);
+DU_PFN_DECL(vkQueueEndDebugUtilsLabelEXT);
+DU_PFN_DECL(vkQueueInsertDebugUtilsLabelEXT);
+DU_PFN_DECL(vkCmdBeginDebugUtilsLabelEXT);
+DU_PFN_DECL(vkCmdEndDebugUtilsLabelEXT);
+DU_PFN_DECL(vkCmdInsertDebugUtilsLabelEXT);
+DU_PFN_DECL(vkCreateDebugUtilsMessengerEXT);
+DU_PFN_DECL(vkSubmitDebugUtilsMessageEXT);
 
 //
 // Initialize debug util instance extension pfns.
@@ -43,16 +42,16 @@ DU_PFN_TYPE(vkSubmitDebugUtilsMessageEXT)    DU_PFN_NAME(vkSubmitDebugUtilsMessa
 void
 vk_debug_utils_init(VkInstance instance)
 {
-  DU_PFN_INIT(instance, vkSetDebugUtilsObjectNameEXT);
-  DU_PFN_INIT(instance, vkSetDebugUtilsObjectTagEXT);
-  DU_PFN_INIT(instance, vkQueueBeginDebugUtilsLabelEXT);
-  DU_PFN_INIT(instance, vkQueueEndDebugUtilsLabelEXT);
-  DU_PFN_INIT(instance, vkQueueInsertDebugUtilsLabelEXT);
-  DU_PFN_INIT(instance, vkCmdBeginDebugUtilsLabelEXT);
-  DU_PFN_INIT(instance, vkCmdEndDebugUtilsLabelEXT);
-  DU_PFN_INIT(instance, vkCmdInsertDebugUtilsLabelEXT);
-  DU_PFN_INIT(instance, vkCreateDebugUtilsMessengerEXT);
-  DU_PFN_INIT(instance, vkSubmitDebugUtilsMessageEXT);
+  DU_PFN_INSTANCE_INIT(instance, vkSetDebugUtilsObjectNameEXT);
+  DU_PFN_INSTANCE_INIT(instance, vkSetDebugUtilsObjectTagEXT);
+  DU_PFN_INSTANCE_INIT(instance, vkQueueBeginDebugUtilsLabelEXT);
+  DU_PFN_INSTANCE_INIT(instance, vkQueueEndDebugUtilsLabelEXT);
+  DU_PFN_INSTANCE_INIT(instance, vkQueueInsertDebugUtilsLabelEXT);
+  DU_PFN_INSTANCE_INIT(instance, vkCmdBeginDebugUtilsLabelEXT);
+  DU_PFN_INSTANCE_INIT(instance, vkCmdEndDebugUtilsLabelEXT);
+  DU_PFN_INSTANCE_INIT(instance, vkCmdInsertDebugUtilsLabelEXT);
+  DU_PFN_INSTANCE_INIT(instance, vkCreateDebugUtilsMessengerEXT);
+  DU_PFN_INSTANCE_INIT(instance, vkSubmitDebugUtilsMessageEXT);
 }
 
 //
