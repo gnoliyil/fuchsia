eebdeea72ddf61433aad939d4442b9ba584a3537
Prashanth Swaminathan <prashanthsw@google.com>
2022-12-15 00:23:38 +0000

    [lowpan] Add RA/RS fields to telemetry
    Bug: 117401
    Test: ffx inspect show core/lowpanservice
    Change-Id: Ifa0bafb793798435eb86a5c1dd3ce5e1e583afd6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/776587
    API-Review: Bruno Dal Bo <brunodalbo@google.com>
    Commit-Queue: Prashanth Swaminathan <prashanthsw@google.com>
    Reviewed-by: Robert Quattlebaum <rquattle@google.com>

--- src/connectivity/lowpan/lib/openthread_rust/src/ot/types/border_routing_counters.rs
+++ src/connectivity/lowpan/lib/openthread_rust/src/ot/types/border_routing_counters.rs
@@ -33,4 +33,34 @@ impl BorderRoutingCounters {
     pub fn outbound_multicast(&self) -> &PacketsAndBytes {
         PacketsAndBytes::ref_from_ot_ref(&self.0.mOutboundMulticast)
     }
+
+    /// The number of received RA packets.
+    pub fn ra_rx(&self) -> u32 {
+        self.0.mRaRx
+    }
+
+    /// The number of RA packets successfully transmitted.
+    pub fn ra_tx_success(&self) -> u32 {
+        self.0.mRaTxSuccess
+    }
+
+    /// The number of RA packets failed to transmit.
+    pub fn ra_tx_failure(&self) -> u32 {
+        self.0.mRaTxFailure
+    }
+
+    /// The number of received RS packets.
+    pub fn rs_rx(&self) -> u32 {
+        self.0.mRsRx
+    }
+
+    /// The number of RS packets successfully transmitted.
+    pub fn rs_tx_success(&self) -> u32 {
+        self.0.mRsTxSuccess
+    }
+
+    /// The number of RS packets failed to transmit.
+    pub fn rs_tx_failure(&self) -> u32 {
+        self.0.mRsTxFailure
+    }
 }
