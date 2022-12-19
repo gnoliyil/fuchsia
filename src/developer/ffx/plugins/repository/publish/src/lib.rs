8b8b6b2c2f19ed7c23ce2ebdcc8c76adf3c672cb
Erick Tryzelaar <etryzelaar@google.com>
2022-12-19 20:59:24 +0000

    [ffx][repo] Graduate ffx repository publish
    This graduates `ffx repository publish` from being an experimental
    command.
    
    Change-Id: Ib2d7d13a60e34677c22017f56caab4015ee652f9
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/779502
    Commit-Queue: Erick Tryzelaar <etryzelaar@google.com>
    Reviewed-by: MJ Ramos <mjramos@google.com>
    Reviewed-by: Amit Uttamchandani <amituttam@google.com>

--- src/developer/ffx/plugins/repository/publish/src/lib.rs
+++ src/developer/ffx/plugins/repository/publish/src/lib.rs
@@ -6,7 +6,7 @@ use {anyhow::Result, errors::ffx_error, ffx_core::ffx_plugin};
 
 pub use ffx_repository_publish_args::RepoPublishCommand;
 
-#[ffx_plugin("ffx_repository")]
+#[ffx_plugin()]
 pub async fn cmd_repo_publish(cmd: RepoPublishCommand) -> Result<()> {
     package_tool::cmd_repo_publish(cmd).await.map_err(|err| ffx_error!(err))?;
     Ok(())
