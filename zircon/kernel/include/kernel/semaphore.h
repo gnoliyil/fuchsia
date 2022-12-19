7a6ceaa63b8ba76f0c8f824d43c4c2f3b7f79359
Nick Maniscalco <maniscalco@google.com>
2022-12-19 18:53:11 +0000

    [kernel][port] Improve scalability of ports and Semaphore
    The purpose of this change is to reduce thread lock contention caused
    by ports.
    
    PortDispatcher uses a Semaphore to wake threads that are blocked in
    zx_port_wait.  Every packet generated results in a call to
    Semaphore::Post and every call to zx_port_wait results in a call to
    Semaphore::Wait.  Prior to this change, every Wait and Post operation
    on Semaphore acquired the thread lock regardless of whether there was
    any need to block (in the case of Wait) or any waiters to wake (in the
    cast Post).
    
    This change improves port scalability by eliminating the need to need
    to acquire the thread lock in the following scenarios:
    
    * zx_port_wait when the packet queue contains one or more packets.
    * Queuing a packet when the port has no waiters.
    
    Bug: 111882
    Run-All-Tests: true
    Change-Id: I8939854ad22d40b529f79727c8768cb774b9b53e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/730292
    Reviewed-by: Adrian Danis <adanis@google.com>
    Commit-Queue: Nick Maniscalco <maniscalco@google.com>
    Reviewed-by: Corey Tabaka <eieio@google.com>
    Reviewed-by: John Grossman <johngro@google.com>

--- zircon/kernel/include/kernel/semaphore.h
+++ zircon/kernel/include/kernel/semaphore.h
@@ -14,44 +14,90 @@
 #include <kernel/thread.h>
 #include <kernel/thread_lock.h>
 #include <kernel/wait.h>
+#include <ktl/atomic.h>
 
-// A basic counting semaphore. It directly uses the low-level wait queue API.
+// A basic counting semaphore used to control access to a shared resource.
+//
+// Think of Semaphore as a gatekeeper that allows a certain number of threads to
+// pass through a gate so they can access some resource.
+//
+// Threads queue up at the gate by calling Wait and then block until the
+// gatekeeper lets them through or they give up and leave the queue because of
+// timeout or thread signal.  In the case of timeout or thread signal, the
+// waiter may not proceed to the resource.
+//
+// Calling Post tells the gatekeeper that they may immediately admit one through
+// the gate (if there's a waiter in the queue) or admit one in the future once a
+// waiter has queued up.
 class Semaphore {
  public:
-  explicit Semaphore(uint64_t initial_count = 0) : count_(initial_count) {}
+  explicit Semaphore(int64_t initial_count = 0) : count_(initial_count) {}
 
   Semaphore(const Semaphore&) = delete;
   Semaphore(Semaphore&&) = delete;
   Semaphore& operator=(const Semaphore&) = delete;
   ~Semaphore() = default;
 
-  // Release a single thread if there are any waiting, otherwise increment the
-  // internal count by one.
+  // Unblocks a single thread if any are waiting.  If none are waiting, this
+  // operation logically increments the count such that a future call to |Wait|
+  // may return without blocking.
+  //
+  // |Post| has release memory order semantics and synchronizes with |Wait|.
   void Post();
 
-  // If the count is positive, decrement the count by exactly one.  Otherwise,
-  // wait until some other thread wakes us, or our wait is interrupted by
-  // timeout, suspend, or thread death.
+  // If the count is positive, decrement the count and return ZX_OK.  Otherwise,
+  // decrement the count and wait until some other thread wakes us via |Post|,
+  // or our wait is interrupted by timeout, thread suspend, or thread kill.
   //
   // The return value can be ZX_ERR_TIMED_OUT if the deadline had passed or one
   // of ZX_ERR_INTERNAL_INTR errors if the thread had a signal delivered.
+  //
+  // |Wait| has acquire memory order semantics and synchronizes with |Post|.
   zx_status_t Wait(const Deadline& deadline);
 
   // Observe the current internal count of the semaphore.
-  uint64_t count() {
-    Guard<MonitoredSpinLock, IrqSave> guard{ThreadLock::Get(), SOURCE_TAG};
-    return count_;
-  }
+  //
+  // This should only be used for testing/diagnostic purposes.
+  int64_t count() const { return count_.load(ktl::memory_order_relaxed); }
 
-  // Observe the current internal count of waiters
-  uint64_t num_waiters() {
+  // Observe the current internal count of waiters.
+  //
+  // This should only be used for testing/diagnostic purposes.
+  uint64_t num_waiters() const TA_EXCL(thread_lock) {
     Guard<MonitoredSpinLock, IrqSave> guard{ThreadLock::Get(), SOURCE_TAG};
     return waitq_.Count();
   }
 
  private:
-  uint64_t count_ TA_GUARDED(thread_lock);
-  WaitQueue waitq_;
+  // This class has two fields that must be kept in sync, a count and a
+  // WaitQueue.  There are some rules for keeping them in sync.  See the
+  // implementation comments of |Post| and |Wait| for details.
+  //
+  // When |count_| is greater than zero, a call to |Wait| will decrement the
+  // count and "fall through" without blocking.
+  //
+  // When |count_| is zero or negative, a call to |Wait| will decrement |count_|
+  // and block.  Note, a negative value does not necessarily imply there are
+  // currently waiters in the |waitq_|.  It's possible to have a negative count
+  // and no waiters because a |Wait| can timeout or be interrupted (e.g. by
+  // thread kill or thread suspend).  The next call to |Post| after an
+  // interrupted |Wait| will update the |count_|.
+  //
+  // Most methods on |waitq_| require the caller to be holding the ThreadLock.
+  // In order to reduce lock contention and improve scalability, the |Post| and
+  // |Wait| operations are designed to only acquire the lock when necessary.  In
+  // particular:
+  //
+  // 1. |Post| does not need to access |waitq_| if it knows that no threads are
+  // blocked (i.e. when |count_| is non-negative).
+  //
+  // 2. |Wait| does not need to access the |waitq_| if the caller does not need
+  // to block (i.e. when |count_| is greater than zero).
+  //
+  // It's critical that the lock protecting |waitq_| be held when transitioning
+  // |count_| from negative to non-negative or from non-negative to negative.
+  ktl::atomic<int64_t> count_;
+  WaitQueue waitq_ TA_GUARDED(thread_lock);
 };
 
 #endif  // ZIRCON_KERNEL_INCLUDE_KERNEL_SEMAPHORE_H_
--- zircon/kernel/kernel/semaphore.cc
+++ zircon/kernel/kernel/semaphore.cc
@@ -11,35 +11,119 @@
 
 #include <kernel/thread_lock.h>
 
+// Every call to this method must wake one waiter unless the queue is empty.
+//
+// When leaving this method, we must have either
+//   - incremented a non-negative count or
+//   - unblocked one thread and
+//     - left an empty queue with a count of zero or
+//     - left a non-empty queue with negative count
 void Semaphore::Post() {
+  // Is the count greater than or equal to zero?  If so, increment and return.
+  // Take care to not increment a negative count.
+  int64_t old_count = count_.load(ktl::memory_order_relaxed);
+  while (old_count >= 0) {
+    if (count_.compare_exchange_weak(old_count, old_count + 1, ktl::memory_order_release,
+                                     ktl::memory_order_relaxed)) {
+      return;
+    }
+  }
+
+  // We observed a negative count and have not yet incremented it.  There might
+  // be waiters waiting.  We'll need to acquire the lock and check.
   Guard<MonitoredSpinLock, IrqSave> guard{ThreadLock::Get(), SOURCE_TAG};
 
-  // Either the number of waiters in the wait queue, or the semaphore count,
-  // must be 0.  It should never be possible for there to be waiters, and a
-  // positive count.
-  DEBUG_ASSERT((count_ == 0) || waitq_.IsEmpty());
-
-  // If we have no waiters, increment the count.  Otherwise, release a waiter.
-  if (waitq_.IsEmpty()) {
-    ++count_;
-  } else {
-    waitq_.WakeOne(ZX_OK);
+  // Because we hold the lock we know that no other thread can transition the
+  // count from non-negative to negative or vice versa.  If the count is
+  // non-negative, then there must be no watiers so just increment and return.
+  old_count = count_.load(ktl::memory_order_relaxed);
+  if (old_count >= 0) {
+    [[maybe_unused]] uint32_t q;
+    DEBUG_ASSERT_MSG((q = waitq_.Count()) == 0, "q=%u old_count=%ld\n", q, old_count);
+    count_.fetch_add(1, ktl::memory_order_release);
+    return;
   }
+
+  // Because we hold the lock we know the number of waiters cannot change out
+  // from under us.  At this point we know the count is negative.  Check for
+  // waiters to wake.
+  const uint32_t num_in_queue = waitq_.Count();
+  if (num_in_queue == 0) {
+    // There are no waiters to wake.  They must have timed out or been
+    // interrupted.  Reset the count and perform our increment.
+    count_.store(1, ktl::memory_order_release);
+    return;
+  }
+
+  // At this point we know there's at least one waiter waiting.  We're committed
+  // to waking.  However, we need to determine what the count should be when
+  // drop the lock and return.  After we wake, if there are no waiters left, the
+  // count should be reset to zero, otherwise it should remain negative.
+  //
+  // Because waking via |WakeOne| may reschedule the local CPU and allow another
+  // thread to "interrupt" our critical section, it's crucial that we adjust the
+  // count *before* calling |WakeOne|.
+  if (num_in_queue == 1) {
+    count_.store(0, ktl::memory_order_release);
+  }
+  [[maybe_unused]] const bool woke_one = waitq_.WakeOne(ZX_OK);
+  DEBUG_ASSERT(woke_one);
 }
 
+// Handling failed waits -- Waits can fail due to timeout or thread signal.
+// When a Wait fails, the caller cannot proceed to the resource guarded by the
+// semaphore.  It's as if the waiter gave up and left.  We want to ensure failed
+// waits do not impact other waiters.  While it seems like a failed wait should
+// simply "undo" its decrement, it is not safe to do so in Wait.  Instead, we
+// "fix" the count in subsequent call to Post.
+//
+// To understand why we can't simply fix the count in Wait after returning from
+// Block, let's look at an alternative (buggy) implementation of Post and Wait.
+// In this hypothetical implementation, Post increments and Wait decrements
+// before Block, but also increments if Block returns an error.  With this
+// hypothetical implementation in mind, consider the following sequence of
+// operations:
+//
+//    Q  C
+//    0  0
+// 1W 1 -1  B
+// 1T 0 -1
+// 2P 0  0
+// 3W 1 -1  B
+// 1R 1  0
+//
+// The way to read the sequence above is that the wait queue (Q) starts empty
+// and the count (C) starts at zero.  Thread1 calls Wait (1W) and blocks (B).
+// Thread1's times out (1T) and is removed from the queue, but has not yet
+// resumed execution.  Thread2 calls Post (2P), but does not unblock any threads
+// because it finds an empty queue. Thread3 calls Wait (3W) and blocks (B).
+// Thread1 returns from Block, resumes execution (1R), and increments the count.
+// At this point Thread3 is blocked as it should be (two Posts and one failed
+// Wait), however, a subsequent call to Post will not unblock it.  We have a
+// "lost wakeup".
 zx_status_t Semaphore::Wait(const Deadline& deadline) {
+  // Is the count greater than zero?  If so, decrement and return.  Take care to
+  // not decrement zero or a negative value.
+  int64_t old_count = count_.load(ktl::memory_order_relaxed);
+  while (old_count > 0) {
+    if (count_.compare_exchange_weak(old_count, old_count - 1, ktl::memory_order_acquire,
+                                     ktl::memory_order_relaxed)) {
+      return ZX_OK;
+    }
+  }
+
+  // We either observed that count is zero or negative.  We have not decremented
+  // yet.  We may need to block.
   Guard<MonitoredSpinLock, IrqSave> guard{ThreadLock::Get(), SOURCE_TAG};
 
-  DEBUG_ASSERT((count_ == 0) || waitq_.IsEmpty());
-
-  // If the count is positive, simply decrement it and get out.
-  if (count_ > 0) {
-    --count_;
+  // Because we hold the lock we know that no other thread can transition the
+  // count from non-negative to negative or vice versa.  If we can decrement a
+  // positive count, then we're done and don't need to block.
+  old_count = count_.fetch_sub(1, ktl::memory_order_acquire);
+  if (old_count > 0) {
     return ZX_OK;
   }
 
-  // Wait in an interruptible state.  We will either be woken by a Post
-  // operation, or by a timeout or signal.  Whatever happens, return the reason
-  // the wait operation ended.
+  // We either decremented zero or a negative value.  We must block.
   return waitq_.Block(deadline, Interruptible::Yes);
 }
