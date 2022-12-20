b82e2f48fc9b05be4c19dcdd9d02fdddcc1f7dee
Tamir Duberstein <tamird@google.com>
2022-12-20 21:28:33 +0000

    [sysconfig-client] Disallow conversion warnings
    Bug: 58162
    Change-Id: I1bffb69299f13a884b76dcb52debc591c62c0a02
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/779667
    Fuchsia-Auto-Submit: Tamir Duberstein <tamird@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Bruno Dal Bo <brunodalbo@google.com>

--- zircon/system/ulib/sysconfig-client/shared/abr-wear-leveling.c
+++ zircon/system/ulib/sysconfig-client/shared/abr-wear-leveling.c
@@ -39,37 +39,41 @@ void find_latest_abr_metadata_page(const struct sysconfig_header *header, const 
   // Abr metadatas are appended from the first to the last page in the
   // sub-partition. Thus, we scan backward and find the first valid
   // page.
-  int i, num_pages = header->abr_metadata.size / page_size;
+  const uint64_t num_pages = header->abr_metadata.size / page_size;
   const uint8_t *start = (const uint8_t *)abr_subpart;
   abrwlP("Finding page with latest abr metadata\n");
-  for (i = num_pages - 1; i >= 0; i--) {
+  for (uint64_t i = num_pages - 1;; i--) {
     if (abr_metadata_ext_valid(start + i * page_size)) {
       abrwlP("page %d has valid abr metadata\n", i);
       memcpy(out, start + i * page_size, sizeof(struct abr_metadata_ext));
-      return;
+      break;
+    }
+    if (i == 0) {
+      // Default to the first page if there is no page with valid magic,
+      abrwlP("no page with valid magic found. use page 0 as default\n");
+      memcpy(out, start, sizeof(struct abr_metadata_ext));
+      break;
     }
   }
-  // Default to the first page if there is no page with valid magic,
-  abrwlP("no page with valid magic found. use page 0 as default\n");
-  memcpy(out, start, sizeof(struct abr_metadata_ext));
 }
 
 bool find_empty_page_for_wear_leveling(const struct sysconfig_header *header,
                                        const uint8_t *abr_subpart, uint64_t page_size,
-                                       int64_t *out) {
+                                       uint64_t *out) {
   // NAND page programming has to be consecutive from the first to last
   // within a block. Thus we find the first empty page such that all pages
   // behind it are also empty, or in other work, the immediate empty page
   // after the last non-empty page in the sub-partition;
-  int64_t i, j, num_pages = header->abr_metadata.size / page_size;
+  const uint64_t num_pages = header->abr_metadata.size / page_size;
 
   const uint8_t *page;
   abrwlP("Finding empty page, total pages: %ld\n", num_pages);
-  for (i = 1; i <= num_pages; i++) {
+  uint64_t i = 1;
+  for (; i <= num_pages; i++) {
     page = abr_subpart + (num_pages - i) * page_size;
     bool page_empty = true;
     // Check whether the page is empty.
-    for (j = 0; j < (int64_t)page_size; j++) {
+    for (uint64_t j = 0; j < page_size; j++) {
       if (page[j] != 0xff) {
         abrwlP("page %ld, offset %ld = 0x%x != 0xff\n", i, j, page[j]);
         page_empty = false;
