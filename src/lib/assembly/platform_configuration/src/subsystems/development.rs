fd47536f5712d31379b464ce4c6087f1f05d0864
Aidan Wolter <awolter@google.com>
2022-12-21 20:45:39 +0000

    [assembly] Move bringup kernel args to platform development subsystem
    This subsystem reads the development_support platform flag, and uses
    that to choose the correct platform kernel args. If one is not supplied,
    we use a default based on the build type.
    
    Bug: 117758
    Change-Id: Icd2d674aa08f95bf0bca17f5cef3c42c306b299c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/778967
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Aaron Wood <aaronwood@google.com>
    Fuchsia-Auto-Submit: Aidan Wolter <awolter@google.com>

---
+++ src/lib/assembly/platform_configuration/src/subsystems/development.rs
@@ -0,0 +1,42 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use crate::subsystems::prelude::*;
+use assembly_config_schema::{
+    platform_config::development_support_config::DevelopmentSupportConfig, BuildType,
+    FeatureSupportLevel,
+};
+
+pub(crate) struct DevelopmentConfig;
+impl DefineSubsystemConfiguration<Option<DevelopmentSupportConfig>> for DevelopmentConfig {
+    fn define_configuration<'a>(
+        context: &ConfigurationContext<'a>,
+        config: &Option<DevelopmentSupportConfig>,
+        builder: &mut dyn ConfigurationBuilder,
+    ) -> anyhow::Result<()> {
+        if matches!(context.feature_set_level, FeatureSupportLevel::Empty) {
+            return Ok(());
+        }
+
+        // Select the correct AIB based on the user-provided setting if present
+        // and fall-back to the default by build-type.
+        let aib_name = match (config, context.build_type) {
+            (Some(DevelopmentSupportConfig { enabled: true }), BuildType::User) => {
+                anyhow::bail!("Development support is not allowed on user builds");
+            }
+
+            // User-provided development setting for non-user builds.
+            (Some(DevelopmentSupportConfig { enabled: true }), _) => "kernel_args_eng",
+            (Some(DevelopmentSupportConfig { enabled: false }), _) => "kernel_args_user",
+
+            // Default development setting by build-type.
+            (None, BuildType::Eng) => "kernel_args_eng",
+            (None, BuildType::UserDebug) => "kernel_args_userdebug",
+            (None, BuildType::User) => "kernel_args_user",
+        };
+
+        builder.platform_bundle(aib_name);
+        Ok(())
+    }
+}
