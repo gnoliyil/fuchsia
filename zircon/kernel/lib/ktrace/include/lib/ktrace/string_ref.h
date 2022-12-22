1f9aeaa734b7c3afa68f31c3eba651ea9815eaa8
Corey Tabaka <eieio@google.com>
2022-12-22 01:33:37 +0000

    [kernel][ktrace] Rebase StringRef on InternedString.
    This is the second of several steps to migrate the kernel's StringRef to
    the more general fxt library.
    
    - Add simple adapter from _stringref to _intern literal operator.
    - Add alias from StringRef to InternedString.
    - Remove duplicate functionality from lib/ktrace.
    - Fixup fix up pointer/reference differences.
    - Set the string registration callback during ktrace init.
    
    Test: Verified traces with various settings.
    Change-Id: If5b3a5468b4c4a525e18805f81a79d85325b893b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/780184
    Reviewed-by: Gwen Mittertreiner <gmtr@google.com>
    Commit-Queue: Corey Tabaka <eieio@google.com>

--- zircon/kernel/lib/ktrace/include/lib/ktrace/string_ref.h
+++ zircon/kernel/lib/ktrace/include/lib/ktrace/string_ref.h
@@ -7,73 +7,13 @@
 #ifndef ZIRCON_KERNEL_LIB_KTRACE_INCLUDE_LIB_KTRACE_STRING_REF_H_
 #define ZIRCON_KERNEL_LIB_KTRACE_INCLUDE_LIB_KTRACE_STRING_REF_H_
 
-#include <lib/special-sections/special-sections.h>
-#include <zircon/types.h>
+#include <lib/fxt/interned_string.h>
 
-#include <ktl/atomic.h>
-
-// Represents an internalized string that may be referenced in traces by id to
-// improve the efficiency of labels and other strings. This type does not define
-// constructors or a destructor and contains trivially constructible members so
-// that it may be aggregate-initialized to avoid static initializers and guards.
-struct StringRef {
-  static constexpr uint16_t kInvalidId = 0;
-
-  const char* string{nullptr};
-  ktl::atomic<uint16_t> id{kInvalidId};
-  StringRef* next{nullptr};
-
-  // Returns the numeric id for this string ref. If this is the first runtime
-  // encounter with this string ref a new id is generated and the string ref
-  // is added to the global linked list.
-  uint16_t GetId() {
-    const uint16_t ref_id = id.load(ktl::memory_order_relaxed);
-    return ref_id == kInvalidId ? Register(this) : ref_id;
-  }
-
-  // Returns the head of the global string ref linked list.
-  static StringRef* head() { return head_.load(ktl::memory_order_acquire); }
-
-  // Pre-registers all StringRef instances on supported compilers.
-  //
-  // Clang correctly implements section attributes on static template members in ELF targets,
-  // resulting in every StringRef instance from instantiations of the _stringref literal operator
-  // being placed in the "string_refs_table" section. However, GCC ignores section attributes on
-  // COMDAT symbols as of this writing, resulting in an empty section when compiled with GCC.
-  // TODO(fxbug.dev/27083): Cleanup this comment when GCC supports section attributes on COMDAT.
-  static void PreRegister();
-
- private:
-  // TODO(fxbug.dev/33293): Replace runtime lock-free linked list with comdat linker
-  // sections once the toolchain supports it.
-  static uint16_t Register(StringRef* string_ref);
-
-  static ktl::atomic<uint16_t> id_counter_;
-  static ktl::atomic<StringRef*> head_;
-};
-
-// String literal template operator that generates a unique StringRef instance
-// for the given string literal. Every invocation for a given string literal
-// value returns the same StringRef instance, such that the set of StringRef
-// instances behaves as an internalized string table.
-//
-// This implementation uses the N3599 extension supported by Clang and GCC.
-// C++20 ratified a slightly different syntax that is simple to switch to, once
-// available, without affecting call sites.
-// TODO(fxbug.dev/33284): Update to C++20 syntax when available.
-//
-// References:
-//     http://open-std.org/JTC1/SC22/WG21/docs/papers/2013/n3599.html
-//     http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2017/p0424r2.pdf
-//
-// Example:
-//     ktrace_probe("probe_name"_stringref, ...);
-//
 template <typename T, T... chars>
-inline StringRef* operator""_stringref() {
-  static const char storage[] = {chars..., '\0'};
-  static StringRef string_ref SPECIAL_SECTION("__trace_string_refs_table", StringRef){storage};
-  return &string_ref;
+inline fxt::InternedString* operator""_stringref() {
+  return const_cast<fxt::InternedString*>(&fxt::operator""_intern<T, chars... >());
 }
+
+using StringRef = fxt::InternedString;
 
 #endif  // ZIRCON_KERNEL_LIB_KTRACE_INCLUDE_LIB_KTRACE_STRING_REF_H_
