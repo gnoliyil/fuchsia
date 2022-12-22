4629895bb6764f58d8aebc3ce10724f0ccedc781
Adam Perry <adamperry@google.com>
2022-12-22 00:38:18 +0000

    [rust][3p] Stop fetching licenses from GitHub.
    Leaves the script in place but just as a check to ensure that every crate
    we vendor has a license file rather than
    downloading them if missing.
    
    It turns out our currently vendored crates don't require any remotely
    fetched licenses which is a big improvement. This functionality has been
    error-prone and has needed a lot of tweaking over time.
    
    Change-Id: I3d8af93f769d6836cbc0861d66afb368b5e327f9
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/780903
    Reviewed-by: Tyler Mandry <tmandry@google.com>
    Commit-Queue: Adam Perry <adamperry@google.com>

--- scripts/rust/check_rust_licenses.py
+++ scripts/rust/check_rust_licenses.py
@@ -4,121 +4,41 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# A script to fetch missing LICENSE files when building a vendored repo
+# A script to check for missing LICENSE files when vendoring Rust crates
 
 # Should be run from top-level of third_party/rust_crates repository.
 
 import argparse
-import json
 import os
-import re
-import shutil
 import sys
-import urllib.request
-from multiprocessing import Pool
-
-repo_re = re.compile('\s*repository\s*=\s*"(.*)"\s*$')
-
 
 def die(reason):
     raise Exception(reason)
 
 
-def get_repo_path(subdir):
-    for line in open(os.path.join(subdir, 'Cargo.toml')):
-        m = repo_re.match(line)
-        if m:
-            return m.group(1)
-
-
-def find_github_blob_path(path):
-    s = path.split('/')
-    if s[2] == 'github.com':
-        s[2] = 'raw.githubusercontent.com'
-        # github redirects "github.com/$USER/$PROJECT.git" to
-        # "github.com/$USER/$PROJECT".
-        if s[4].endswith('.git'):
-            s[4] = s[4][:-len('.git')]
-    else:
-        die('don\'t know raw content path for ' + path)
-    if s[-1] == '':
-        del s[-1]
-    if len(s) >= 6 and s[5] == 'tree':
-        del s[5]
-    else:
-        s.append('HEAD')
-    return '/'.join(s)
-
-
-def fetch_license(subdir):
-    repo_path = get_repo_path(subdir)
-    if repo_path is None:
-        die('can\'t find repo path for ' + subdir)
-    base_path = find_github_blob_path(repo_path)
-    first = True
-    with open(os.path.join(subdir, 'LICENSE'), 'wb') as license_out:
-        # 'LICENCE' is a British English spelling variant used in https://github.com/ebarnard/rust-plist
-        for filename in ('LICENSE', 'LICENSE-APACHE', 'LICENSE-MIT', 'COPYING',
-                         'LICENCE', 'LICENSE.md'):
-            url = '/'.join((base_path, filename))
-            try:
-                with urllib.request.urlopen(url) as resp:
-                    if first:
-                        first = False
-                    else:
-                        license_out.write(bytearray('=' * 40 + '\n', 'utf-8'))
-                    license_out.write(bytearray(url + ':\n\n', 'utf-8'))
-                    license_out.write(resp.read())
-            except urllib.error.HTTPError as err:
-                if err.code != 404:
-                    raise
-    if first:
-        die('no licenses found under ' + repo_path)
-
-
-def check_subdir_license(arg):
-    # it seems multiprocessing only passes one argument to each function
-    (subdir, verify) = arg
-
+def check_subdir_license(subdir):
     if subdir.startswith('.') or not os.path.isdir(subdir):
-        sys.stdout.write("-")
-        sys.stdout.flush()
-        return
-
-    # delete crates that are empty except for an OWNERS file
-    if os.listdir(subdir) == ["OWNERS"]:
-        shutil.rmtree(subdir)
         return
 
     license_files = [
         file for file in os.listdir(subdir) if file.startswith('LICENSE') or
         file.startswith('LICENCE') or file.startswith('license')
     ]
-    if license_files:
-        sys.stdout.write(".")
-        sys.stdout.flush()
-        return
-
-    if verify:
-        die('MISSING  %s' % subdir)
-
-    fetch_license(subdir)
-    sys.stdout.write("*")
-    sys.stdout.flush()
+    if not license_files: # FIXME better way to track whether one exists
+        die('Missing license for %s' % subdir)
 
 
-def check_licenses(directory, verify=False):
+def check_licenses(directory):
     success = True
     os.chdir(directory)
-    print("Checking licenses.")
     subdirs = sorted(os.listdir(os.getcwd()))
-    try:
-        with Pool(16) as p:
-            p.map(check_subdir_license, [(s, verify) for s in subdirs])
-    except Exception as err:
-        print('ERROR    %s' % err)
-        success = False
-    print("\nDone checking licenses.")
+    for subdir in subdirs:
+        try:
+            check_subdir_license(subdir)
+        except Exception as err:
+            print('ERROR    %s' % err)
+            success = False
+    print("Done checking licenses.")
     return success
 
 
@@ -129,12 +49,8 @@ def main():
         '--directory',
         help='Directory containing the crates',
         default=os.getcwd())
-    parser.add_argument(
-        '--verify',
-        help='Simply check whether licenses are up-to-date',
-        action='store_true')
     args = parser.parse_args()
-    if not check_licenses(args.directory, verify=args.verify):
+    if not check_licenses(args.directory):
         sys.exit(1)
 
 
