e4ba3a46b5e324ff293f566726f8ab0aba5c7f72
Tim Detwiler <tjdetwiler@google.com>
2022-12-28 20:27:34 +0000

    [wayland] Add C++ ffi wrappers for wayland_server
    Add a c++ shim library that allows the rust wayland server to link with
    c++ components. This shim still exposes a channel-based API for
    simplicity.
    
    This will allow us to link the wayland server with the c++ virtio_wl
    device.
    
    Change-Id: If514542b544047f9f1b326719d6d7196674ab24c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/782263
    Reviewed-by: Alexander Radchenko <alerad@google.com>
    Commit-Queue: Tim Detwiler <tjdetwiler@google.com>

---
+++ src/lib/ui/wayland/server/cpp/ffi/src/lib.rs
@@ -0,0 +1,107 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {
+    fuchsia_async as fasync,
+    fuchsia_zircon::{self as zx, HandleBased},
+    futures::{channel::mpsc as async_mpsc, StreamExt},
+    std::sync::mpsc as sync_mpsc,
+    wayland_bridge::dispatcher::WaylandDispatcher,
+};
+
+pub enum WaylandCommand {
+    PushClient(zx::Channel),
+}
+
+#[repr(C)]
+#[allow(non_camel_case_types)]
+pub struct wayland_server_handle_t {
+    pub sender: async_mpsc::UnboundedSender<WaylandCommand>,
+}
+
+fn start_wayland_server() -> Result<(fasync::LocalExecutor, WaylandDispatcher), zx::Status> {
+    let mut executor = fasync::LocalExecutor::new()?;
+    let mut dispatcher = WaylandDispatcher::new().map_err(|e| {
+        tracing::error!("Failed to create wayland dispatcher: {}", e);
+        zx::Status::INTERNAL
+    })?;
+    // Try to get display properties before serving.
+    let scenic = dispatcher.display.scenic().clone();
+    match executor.run_singlethreaded(async { scenic.get_display_info().await }) {
+        Ok(display_info) => dispatcher.display.set_display_info(&display_info),
+        Err(e) => eprintln!("get_display_info error: {:?}", e),
+    }
+    Ok((executor, dispatcher))
+}
+
+#[no_mangle]
+#[allow(clippy::not_unsafe_ptr_arg_deref)]
+pub extern "C" fn wayland_server_create(
+    out: *mut *mut wayland_server_handle_t,
+) -> zx::sys::zx_status_t {
+    let (sender, mut receiver) = async_mpsc::unbounded::<WaylandCommand>();
+    let (result_sender, result_receiver) = sync_mpsc::sync_channel::<zx::sys::zx_status_t>(0);
+    std::thread::spawn(move || {
+        let (mut executor, dispatcher) = match start_wayland_server() {
+            Ok(result) => {
+                result_sender.send(zx::sys::ZX_OK).unwrap();
+                result
+            }
+            Err(status) => {
+                result_sender.send(status.into_raw()).unwrap();
+                return;
+            }
+        };
+
+        executor.run_singlethreaded(async move {
+            while let Some(WaylandCommand::PushClient(channel)) = receiver.next().await {
+                dispatcher
+                    .display
+                    .clone()
+                    .spawn_new_client(fasync::Channel::from_channel(channel).unwrap(), false);
+            }
+        });
+    });
+
+    let result = match result_receiver.recv() {
+        Ok(status) => status,
+        Err(_) => zx::sys::ZX_ERR_INTERNAL,
+    };
+
+    if result == zx::sys::ZX_OK {
+        // Allocate the type we provide to the caller on the heap. This will be owned by the client
+        // so we leak the pointer here.
+        let ptr = Box::leak(Box::new(wayland_server_handle_t { sender }));
+
+        unsafe { *out = ptr };
+    }
+    result
+}
+
+#[no_mangle]
+#[allow(clippy::not_unsafe_ptr_arg_deref)]
+pub extern "C" fn wayland_server_push_client(
+    server_handle: *mut wayland_server_handle_t,
+    channel: zx::sys::zx_handle_t,
+) {
+    let handle = unsafe { zx::Handle::from_raw(channel) };
+    // Unwrap here since this will only fail if we're provided with a null pointer, which is not
+    // something we support.
+    let server = unsafe { server_handle.as_mut().unwrap() };
+    // Unwrap here since the only failure mode is if this is called after `wayland_server_destroy`,
+    // which is not supported.
+    server
+        .sender
+        .unbounded_send(WaylandCommand::PushClient(zx::Channel::from_handle(handle)))
+        .unwrap();
+}
+
+#[no_mangle]
+#[allow(clippy::not_unsafe_ptr_arg_deref)]
+pub extern "C" fn wayland_server_destroy(server_handle: *mut wayland_server_handle_t) {
+    unsafe {
+        // Drop the boxed pointer.
+        let _ = Box::from_raw(server_handle);
+    }
+}
