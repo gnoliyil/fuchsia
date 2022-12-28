ea59ac25d5eb32810a9ab6d7775d250a662fda75
Shai Barack <shayba@google.com>
2022-12-28 23:32:59 +0000

    [bazel] Add tests demonstrating sanitizer failures
    Fixed: 115145
    Fixed: 116361
    
    Change-Id: Ie31d8309bdbf7a9e4445ac6b6228549ce6c99a41
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/777163
    Reviewed-by: Jay Zhuang <jayzhuang@google.com>
    Fuchsia-Auto-Submit: Shai Barack <shayba@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- build/bazel/examples/hello_cpp/hello_gtest.cc
+++ build/bazel/examples/hello_cpp/hello_gtest.cc
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <lib/fdio/spawn.h>
+#include <lib/zx/process.h>
+
 #include <iostream>
 
 #include <gtest/gtest.h>
@@ -15,3 +18,63 @@ TEST(HelloTest, BasicAssertions) {
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
 }
+
+void subprocess(const char* bin_path, int64_t* retval) {
+  zx::process process;
+  const char* argv[] = {bin_path, nullptr};
+  zx_status_t status = fdio_spawn(ZX_HANDLE_INVALID, FDIO_SPAWN_CLONE_ALL, bin_path, argv,
+                                  process.reset_and_get_address());
+  ASSERT_EQ(status, ZX_OK);
+  status = process.wait_one(ZX_TASK_TERMINATED, zx::time::infinite(), nullptr);
+  ASSERT_EQ(status, ZX_OK);
+  zx_info_process_t proc_info{};
+  status = process.get_info(ZX_INFO_PROCESS, &proc_info, sizeof(proc_info), nullptr, nullptr);
+  ASSERT_EQ(status, ZX_OK);
+  *retval = proc_info.return_code;
+}
+
+#if defined(__has_feature)
+#if __has_feature(address_sanitizer) || __has_feature(undefined_behavior_sanitizer)
+#include <zircon/sanitizer.h>
+void SanLog(std::string_view s) { __sanitizer_log_write(s.data(), s.size()); }
+#endif
+#endif
+
+#if defined(__has_feature)
+#if __has_feature(address_sanitizer)
+TEST(HelloTest, AddressSanitizerWorks) {
+  SanLog("[===ASAN EXCEPT BLOCK START===]");
+  ASSERT_DEATH(
+      {
+        // Volatile to suppress optimizations
+        volatile int* p = new int;
+        delete p;
+        // Use after free!
+        *p = 42;
+      },
+      "");
+  SanLog("[===ASAN EXCEPT BLOCK END===]");
+}
+#endif
+#endif
+
+#if defined(__has_feature)
+#if __has_feature(undefined_behavior_sanitizer)
+#include <limits.h>
+#include <stdio.h>
+
+TEST(HelloTest, UndefinedBehaviorSanitizerWorks) {
+  SanLog("[===UBSAN EXCEPT BLOCK START===]");
+  ASSERT_DEATH(
+      {
+        int overflow = INT_MAX;
+        // Integer overflow!
+        overflow += 1;
+        // Side effect to suppress optimizations.
+        printf("overflow = %i\n", overflow);
+      },
+      "");
+  SanLog("[===UBSAN EXCEPT BLOCK END===]");
+}
+#endif
+#endif
