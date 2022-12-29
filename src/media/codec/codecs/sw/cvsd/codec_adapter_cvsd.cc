3499b1f22bc5dbdd1906ba959388f1a00c760d01
Dayeong Lee <dayeonglee@google.com>
2022-12-29 07:12:40 +0000

    [media][codec] Refactor and implement CVSD decoder
    Generalize the CVSD encoder to a CVSD adapter class that both encoder
    and decoder can inherit. Encoder and decoder can override the methods
    that need specific implementation and the remaining common code that is
    mostly related to queueing/dequeueing packet and memory-managing output
    packet/buffer can live in the base class.
    
    CVSD decoder differs from the encoder in the way that codec input
    item is handled: instead of queueing the input packet's data into a
    chunk input stream, we process the input data byte by byte to
    decode into uncompressed 16 bytes of audio. The decoded audio is queued
    into current output buffer until it can no longer used to decode more
    input data.
    
    Bug: 108717, 108718
    Change-Id: If4d60dff45ef76d83fcfc217ce9f5ab0cc7b2e97
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/720822
    Reviewed-by: Dustin Green <dustingreen@google.com>
    Reviewed-by: Marie Janssen <jamuraa@google.com>
    Commit-Queue: Dayeong Lee <dayeonglee@google.com>

---
+++ src/media/codec/codecs/sw/cvsd/codec_adapter_cvsd.cc
@@ -0,0 +1,102 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "codec_adapter_cvsd.h"
+
+#include <lib/async/cpp/task.h>
+
+bool AreJbitsEqual(const CodecParams& params) {
+  uint32_t lastKBits = params.historic_bits;
+  for (uint32_t i = 0; i < params.k - params.j + 1; i++) {
+    auto tmp = lastKBits & params.equal_bit_mask;
+    if (tmp == 0 || tmp == params.equal_bit_mask) {
+      return true;
+    }
+    lastKBits = lastKBits >> 1;
+  }
+  return false;
+}
+
+// Calculates the new step size based on Bluetooth Core v5.3 section
+// 9.2 CVSD CODEC, equations 16 and 17.
+double GetNewStepSize(const CodecParams& params) {
+  // EQ 16 and EQ 17.
+  // If J bits in the last K output bits are equal, step size is incremented
+  // otherwise, it's decremented.
+  double new_delta;
+  if (AreJbitsEqual(params)) {
+    new_delta =
+        std::min(params.step_size + kStepDecaySizeMin, static_cast<double>(kStepDecaySizeMax));
+  } else {
+    new_delta = static_cast<int16_t>(
+        std::max(params.step_size * kStepDecayNumerator / kStepDecayDenominator,
+                 static_cast<double>(kStepDecaySizeMin)));
+  }
+
+  ZX_DEBUG_ASSERT(new_delta <= std::numeric_limits<int16_t>::max());
+  ZX_DEBUG_ASSERT(new_delta >= std::numeric_limits<int16_t>::min());
+
+  return new_delta;
+}
+
+// Calculates the new accumulator value based on Bluetooth Core v5.3 section
+// 9.2 CVSD CODEC equations.
+double GetNewAccumulator(const CodecParams& params, const uint8_t& bit) {
+  double new_accum = params.accumulator;
+  new_accum += (bit) ? -params.step_size : params.step_size;
+
+  // EQ 18. Limit the internal reference.
+  if (new_accum >= 0) {
+    new_accum = std::min(new_accum, static_cast<double>(kAccumulatorPosSaturation));
+  } else {
+    new_accum = std::max(new_accum, static_cast<double>(kAccumulatorNegSaturation));
+  }
+  // EQ 14. Multiply by the decay factor.
+  new_accum = new_accum * kAccumDecayNumerator / kAccumDecayDenominator;
+
+  ZX_DEBUG_ASSERT(new_accum <= std::numeric_limits<int16_t>::max());
+  ZX_DEBUG_ASSERT(new_accum >= std::numeric_limits<int16_t>::min());
+
+  return new_accum;
+}
+
+void UpdateCVSDParams(CodecParams* params, uint8_t bit) {
+  // Shift last value into historic bits buffer.
+  params->historic_bits = ((params->historic_bits << 1) | bit) & params->historic_bit_mask;
+
+  // Update the step size.
+  params->step_size = GetNewStepSize(*params);
+
+  // Update the internal accumulator.
+  params->accumulator = GetNewAccumulator(*params, bit);
+}
+
+void PostSerial(async_dispatcher_t* dispatcher, fit::closure to_run) {
+  zx_status_t post_result = async::PostTask(dispatcher, std::move(to_run));
+  ZX_ASSERT_MSG(post_result == ZX_OK, "async::PostTask() failed - result: %d", post_result);
+}
+
+void InitCodecParams(std::optional<CodecParams>& codec_params) {
+  // Number of equal bits (run of 1's or 0's), J, should be less than or equal to
+  // the number of historic output bits we keep track of.
+  codec_params.emplace(CodecParams{
+      .k = kK,
+      .j = kJ,
+      .equal_bit_mask = static_cast<uint32_t>(pow(2, kJ) - 1),
+      .historic_bit_mask = static_cast<uint32_t>(pow(2, kK) - 1),
+      .historic_bits = 0,
+      .accumulator = 0,
+      .step_size = 0,
+  });
+}
+
+void SetOutputItem(std::optional<OutputItem>& output_item, CodecPacket* packet,
+                   const CodecBuffer* buffer) {
+  output_item.emplace(OutputItem{
+      packet,
+      buffer,
+      0,
+      std::nullopt,
+  });
+}
