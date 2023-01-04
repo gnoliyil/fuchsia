8d422a70a40f9d3998dd547414baba5b2df4cd41
Yaneury Fermin <yaneury@google.com>
2023-01-04 22:03:02 +0000

    [component] Remove outgoing lib dep on incoming
    This CL removes the dependency that `component_outgoing_cpp`
    has on `component_incoming_cpp`. The former depended on the latter
    solely to import some constant variables. In order to split these off,
    the constants were duplicated across both libs using different names.
    
    Change-Id: Ifc768de650e6e8229382a39bd0c263d427ed3ff0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/785764
    Reviewed-by: Gary Bressler <geb@google.com>
    Reviewed-by: David Gilhooley <dgilhooley@google.com>
    Commit-Queue: Yaneury Fermin <yaneury@google.com>

--- sdk/lib/driver/component/cpp/outgoing_directory.h
+++ sdk/lib/driver/component/cpp/outgoing_directory.h
@@ -5,20 +5,20 @@
 #ifndef LIB_DRIVER_COMPONENT_CPP_OUTGOING_DIRECTORY_H_
 #define LIB_DRIVER_COMPONENT_CPP_OUTGOING_DIRECTORY_H_
 
+#include <lib/component/outgoing/cpp/handlers.h>
 #include <lib/component/outgoing/cpp/outgoing_directory.h>
 #include <lib/driver/component/cpp/handlers.h>
 #include <lib/fdf/cpp/channel.h>
 #include <lib/fdf/cpp/protocol.h>
 #include <lib/fdf/dispatcher.h>
+#include <lib/fidl/cpp/wire/service_handler.h>
 #include <lib/fidl_driver/cpp/transport.h>
-
-#include "lib/component/outgoing/cpp/handlers.h"
-#include "lib/fidl/cpp/wire/service_handler.h"
 
 namespace fdf {
 
 // The name of the default FIDL Service instance.
-constexpr const std::string_view kDefaultInstance = component::kDefaultInstance;
+constexpr const std::string_view kDefaultInstance =
+    component::OutgoingDirectory::kDefaultServiceInstance;
 
 // Driver specific implementation for an outgoing directory that wraps around
 // |component::OutgoingDirectory|.
