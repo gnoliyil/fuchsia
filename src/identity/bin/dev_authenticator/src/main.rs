afdf309b6772978fb3847155ee8243ec875d2c31
Angad Pal Singh <apsbhatia@google.com>
2023-01-04 08:04:49 +0000

    [dev_authenticator] Cleanup operation Mode from DevAuthenticator
    - Remove Mode argument from DevAuthenticator component.
    - Remove AlwaysSucceed and AlwaysFail variants and their references from
      the code.
    - DevAuthenticator always operates in Interaction mode now i.e., it runs
      with Interaction enabled and uses TestInteraction to determine if the
      the Authenticate operation is successful or not.
    
    Bug: 116213
    Change-Id: Ibc6a9f7ecdba1782b737816f0eacbe50af99c586
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/784484
    Reviewed-by: Jody Sankey <jsankey@google.com>
    Commit-Queue: Angad Pal Singh <apsbhatia@google.com>

--- src/identity/bin/dev_authenticator/src/main.rs
+++ src/identity/bin/dev_authenticator/src/main.rs
@@ -11,37 +11,22 @@ mod storage_unlock;
 mod test_interaction;
 
 use {
-    crate::storage_unlock::{Mode, StorageUnlockMechanism},
-    anyhow::{anyhow, Error},
+    crate::storage_unlock::StorageUnlockMechanism,
+    anyhow::Error,
     fuchsia_async as fasync,
     fuchsia_component::server::ServiceFs,
     futures::StreamExt,
-    std::convert::TryFrom,
     tracing::{error, info},
 };
 
-/// This required command line option (prefixed with `--`) determines the mode of operation.
-const MODE_OPTION: &str = "mode";
-/// These are the valid arguments for the mode command line option.
-const MODE_ARG_ALWAYS_SUCCEED: &str = "ALWAYS_SUCCEED";
-const MODE_ARG_ALWAYS_FAIL_AUTHENTICATION: &str = "ALWAYS_FAIL_AUTHENTICATION";
-const MODE_ARG_INTERACTION: &str = "INTERACTION";
-
 #[fuchsia::main(logging_tags = ["identity", "dev_authenticator"])]
 async fn main() -> Result<(), Error> {
-    let mut opts = getopts::Options::new();
-    opts.reqopt("", MODE_OPTION, "set the mode of operation", "MODE");
-    let args: Vec<String> = std::env::args().collect();
-    let options = opts.parse(args)?;
-    let mode_arg_str = options.opt_str(MODE_OPTION).expect("Internal getopts error");
-    let mode = Mode::try_from(mode_arg_str.as_ref())?;
-
     info!("Starting dev authenticator");
 
     let mut fs = ServiceFs::new();
     fs.dir("svc").add_fidl_service(move |stream| {
         fasync::Task::spawn(async move {
-            let mechanism = StorageUnlockMechanism::new(mode);
+            let mechanism = StorageUnlockMechanism::new();
             mechanism
                 .handle_requests_from_stream(stream)
                 .await
@@ -54,16 +39,4 @@ async fn main() -> Result<(), Error> {
 
     fs.collect::<()>().await;
     Ok(())
-}
-
-impl TryFrom<&str> for Mode {
-    type Error = Error;
-    fn try_from(s: &str) -> Result<Mode, Self::Error> {
-        match s {
-            MODE_ARG_ALWAYS_SUCCEED => Ok(Mode::AlwaysSucceed),
-            MODE_ARG_ALWAYS_FAIL_AUTHENTICATION => Ok(Mode::AlwaysFailAuthentication),
-            MODE_ARG_INTERACTION => Ok(Mode::Interaction),
-            s => Err(anyhow!("Unrecognized mode: {}", s)),
-        }
-    }
 }
