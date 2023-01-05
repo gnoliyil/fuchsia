ce4cb235091d0e43d4d69a3000b615c923cb9077
Roland McGrath <mcgrathr@google.com>
2023-01-05 00:49:12 +0000

    [elfldltl] Clean up VMO name handling in VmarLoader
    The SetVmoName helper doesn't need to be a method and so doesn't
    need to be declared in the public header.  Regardless, the
    constants used as template parameters don't need to be public
    since they're not themsleves used from code in the header.
    
    Bug: 91206
    Change-Id: Icb13c9bc704f2eb2da39bd052b994c3e377959a2
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/785746
    Reviewed-by: Alex Brachet <abrachet@google.com>
    Reviewed-by: Luke Nicholson <lukenicholson@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>

--- src/lib/elfldltl/vmar-loader.cc
+++ src/lib/elfldltl/vmar-loader.cc
@@ -4,13 +4,51 @@
 
 #include "lib/elfldltl/vmar-loader.h"
 
+#include <lib/stdcompat/bit.h>
+#include <lib/stdcompat/span.h>
 #include <lib/zx/vmar.h>
 #include <zircon/assert.h>
 #include <zircon/status.h>
 
 namespace {
+
 constexpr std::string_view kVmoNameUnknown = "<unknown ELF file>";
+
+constexpr std::string_view kVmoNamePrefixData = "data";
+constexpr std::string_view kVmoNamePrefixBss = "bss";
+
+constexpr char kHexDigits[] = "0132456789ABCDEF";
+
+template <const std::string_view& Prefix>
+void SetVmoName(zx::unowned_vmo vmo, std::string_view base_name, size_t n) {
+  std::array<char, ZX_MAX_NAME_LEN> buffer{};
+  cpp20::span vmo_name(buffer);
+
+  // First, "data" or "bss".
+  size_t name_idx = Prefix.copy(vmo_name.data(), vmo_name.size());
+
+  // Then the ordinal in hex (almost surely just one digit, but who knows).
+  // Count the bits and divide with round-up to count the nybbles.
+  const size_t hex_chars = (cpp20::bit_width(n | 1) + 3) / 4;
+  cpp20::span hex = cpp20::span(vmo_name).subspan(name_idx, hex_chars);
+  for (auto it = hex.rbegin(); it != hex.rend(); ++it) {
+    *it = kHexDigits[n & 0xf];
+    n >>= 4;
+  }
+  name_idx += hex.size();
+
+  // Then `:`, it's guaranteed that the worst case "dataffffffffffffffff:" (21)
+  // definitely fits in ZX_MAX_NAME_LEN (32).
+  vmo_name[name_idx++] = ':';
+
+  // Finally append the original VMO name, however much fits.
+  name_idx += base_name.copy(vmo_name.data(), vmo_name.size(), name_idx);
+  ZX_DEBUG_ASSERT(name_idx <= vmo_name.size());
+
+  vmo->set_property(ZX_PROP_NAME, vmo_name.data(), vmo_name.size());
 }
+
+}  // namespace
 
 namespace elfldltl {
 
@@ -18,43 +56,12 @@ VmarLoader::VmoName VmarLoader::GetVmoName(zx::unowned_vmo vmo) {
   VmarLoader::VmoName base_vmo_name{};
   zx_status_t status = vmo->get_property(ZX_PROP_NAME, base_vmo_name.data(), base_vmo_name.size());
 
-  if (status != ZX_OK || base_vmo_name[0] == '\0') {
-    memcpy(base_vmo_name.data(), kVmoNameUnknown.data(), kVmoNameUnknown.size());
+  if (status != ZX_OK || base_vmo_name.front() == '\0') {
+    kVmoNameUnknown.copy(base_vmo_name.data(), base_vmo_name.size());
   }
 
   return base_vmo_name;
 }
-
-template <const std::string_view& Prefix>
-void VmarLoader::SetVmoName(zx::unowned_vmo vmo, std::string_view base_name, size_t n) {
-  VmoName vmo_name{};
-  memcpy(vmo_name.data(), Prefix.data(), Prefix.size());
-
-  auto hex_chars = (cpp20::bit_width(n | 1) + 3) / 4;
-  constexpr char alphabet[] = "0132456789ABCDEF";
-
-  size_t hex_mask = n;
-  size_t index = hex_chars + Prefix.size() - 1;
-  while (hex_mask != 0) {
-    vmo_name[index] = alphabet[hex_mask & 0x0F];
-    hex_mask >>= 4;
-    index--;
-  }
-  vmo_name[Prefix.size() + hex_chars] = ':';
-
-  memcpy(vmo_name.data() + Prefix.size() + hex_chars + 1, base_name.data(),
-         ZX_MAX_NAME_LEN - vmo_name.size());
-
-  vmo->set_property(ZX_PROP_NAME, vmo_name.data(), vmo_name.size());
-}
-
-template void VmarLoader::SetVmoName<VmarLoader::kVmoNamePrefixData>(zx::unowned_vmo vmo,
-                                                                     std::string_view base_name,
-                                                                     size_t n);
-template void VmarLoader::SetVmoName<VmarLoader::kVmoNamePrefixBss>(zx::unowned_vmo vmo,
-                                                                    std::string_view base_name,
-                                                                    size_t n);
-
 zx_status_t VmarLoader::AllocateVmar(size_t vaddr_size, size_t vaddr_start) {
   zx_vaddr_t child_addr;
   zx_status_t status = vmar_->allocate(
