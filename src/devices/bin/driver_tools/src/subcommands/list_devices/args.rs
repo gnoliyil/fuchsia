819b15a0acf1b670e0cbdbfebe15a46b057d46f7
Ian Fisher <iafisher@google.com>
2023-01-05 01:32:17 +0000

    [ffx][driver] Add `--fail-on-missing` flag to `ffx driver list-devices`
    This makes it easier to use `ffx driver list-devices` on shell scripts
    to detect the presence of a certain device and fail otherwise.
    
    Preferably we would always silently return a non-zero exit code when no
    matching drivers are found, but FFX will print an error message and a
    backtrace if a subcommand returns an error, which is not desirable as
    the default for this case.
    
    Bug: 113465
    Change-Id: Ia390769b7d4138250fe768f4f928b282038c1bcf
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/781346
    Reviewed-by: David Gilhooley <dgilhooley@google.com>
    Fuchsia-Auto-Submit: Ian Fisher <iafisher@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/devices/bin/driver_tools/src/subcommands/list_devices/args.rs
+++ src/devices/bin/driver_tools/src/subcommands/list_devices/args.rs
@@ -15,7 +15,7 @@ use argh::FromArgs;
     error_code(1, "Failed to connect to the driver development service")
 )]
 pub struct ListDevicesCommand {
-    /// device filter - either an exact topo path, or a fuzzy match on the device's name
+    /// device filter - either an exact topo path, or a fuzzy match on the device's name.
     #[argh(positional)]
     pub device: Option<String>,
 
@@ -27,7 +27,11 @@ pub struct ListDevicesCommand {
     #[argh(switch, short = 'v', long = "verbose")]
     pub verbose: bool,
 
-    /// require an exact match on the device filter, instead of a substring match
+    /// require an exact match on the device filter, instead of a substring match.
     #[argh(switch, long = "exact")]
     pub exact: bool,
+
+    /// return a non-zero exit code if no matching devices are found.
+    #[argh(switch, long = "fail-on-missing")]
+    pub fail_on_missing: bool,
 }
