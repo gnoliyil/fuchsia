a4d327dd5236346b970e22ce88121b44c9892f4b
Roland McGrath <mcgrathr@google.com>
2023-01-05 17:13:06 +0000

    [elfldltl] Refactor segment-loading in VmarLoader
    This separates much-simpler read-only case from the others, using
    just a trivial Map wrapper for that trivial case.
    
    MapFileData is replaced with MapWritable, which handles only the
    writable (COW clone creation) case.  Both it and MapZeroFill use
    constant flags for R/W page permissions, which are always what
    writable segments use.
    
    Error messages for mapping failures include the address or
    file offset, which can be matched up with PT_LOAD details to
    see exactly which segment failed to load.
    
    Bug: 91206
    Change-Id: Ie77eb950286a43951c53474062880683846bc494
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/785766
    Reviewed-by: Alex Brachet <abrachet@google.com>
    Reviewed-by: Luke Nicholson <lukenicholson@google.com>
    Commit-Queue: Roland McGrath <mcgrathr@google.com>

--- src/lib/elfldltl/vmar-loader.cc
+++ src/lib/elfldltl/vmar-loader.cc
@@ -62,15 +62,16 @@ VmarLoader::VmoName VmarLoader::GetVmoName(zx::unowned_vmo vmo) {
 
   return base_vmo_name;
 }
+
 zx_status_t VmarLoader::AllocateVmar(size_t vaddr_size, size_t vaddr_start) {
   zx_vaddr_t child_addr;
   zx_status_t status = vmar_->allocate(
       ZX_VM_CAN_MAP_READ | ZX_VM_CAN_MAP_WRITE | ZX_VM_CAN_MAP_EXECUTE | ZX_VM_CAN_MAP_SPECIFIC, 0,
-      vaddr_size, &child_vmar_, &child_addr);
+      vaddr_size, &load_image_vmar_, &child_addr);
 
   if (status == ZX_OK) {
-    // Convert the absolute address of the child VMAR to a platform-generic address type
-    // needed for the generic DirectMemory abstraction.
+    // Convert the absolute address of the child VMAR to a platform-generic
+    // address type needed for the generic DirectMemory abstraction.
     memory_.set_image({reinterpret_cast<std::byte*>(child_addr), vaddr_size});
     memory_.set_base(vaddr_start);
   }
@@ -78,55 +79,32 @@ zx_status_t VmarLoader::AllocateVmar(size_t vaddr_size, size_t vaddr_start) {
   return status;
 }
 
-zx_status_t VmarLoader::MapAnonymousData(zx_vm_option_t options, size_t zero_size,
-                                         std::string_view base_name, size_t vmar_offset,
-                                         size_t& num_mapped_anonymous_regions) {
-  // Create a zero-fill VMO for the entire remainder of the segment, including intersecting
-  // page.
-  zx::vmo zero_fill_vmo;
-  zx_status_t status = zx::vmo::create(zero_size, 0, &zero_fill_vmo);
-
+zx_status_t VmarLoader::MapWritable(uintptr_t vmar_offset, zx::unowned_vmo vmo,
+                                    std::string_view base_name, uint64_t vmo_offset, size_t size,
+                                    size_t& num_data_segments) {
+  zx::vmo writable_vmo;
+  zx_status_t status =
+      vmo->create_child(ZX_VMO_CHILD_SNAPSHOT_AT_LEAST_ON_WRITE, vmo_offset, size, &writable_vmo);
   if (status != ZX_OK) [[unlikely]] {
     return status;
   }
 
-  SetVmoName<kVmoNamePrefixBss>(zero_fill_vmo.borrow(), base_name, num_mapped_anonymous_regions);
-  num_mapped_anonymous_regions++;
+  SetVmoName<kVmoNamePrefixData>(writable_vmo.borrow(), base_name, num_data_segments++);
 
-  zx_vaddr_t mapping_addr;
-  return child_vmar_.map(options, vmar_offset, zero_fill_vmo, 0, zero_size, &mapping_addr);
+  return Map(vmar_offset, kMapWritable, writable_vmo.borrow(), 0, size);
 }
 
-zx_status_t VmarLoader::MapFileData(size_t segment_offset, bool is_writable, zx_vm_option_t options,
-                                    size_t map_size, zx::unowned_vmo vmo,
-                                    std::string_view base_name, size_t vmar_offset,
-                                    size_t& num_mapped_file_regions) {
-  zx::vmo cloned_vmo;
-
-  // If we don't create a new child, we map in from the segment offset. If we do create
-  // a new child, we map in from the start of the child. mapping_vmo_offset captures
-  // the conditional offset into the mapping VMO.
-  size_t mapping_vmo_offset = segment_offset;
-
-  if (is_writable) {
-    zx_status_t status = vmo->create_child(ZX_VMO_CHILD_SNAPSHOT_AT_LEAST_ON_WRITE, segment_offset,
-                                           map_size, &cloned_vmo);
-
-    if (status != ZX_OK) [[unlikely]] {
-      return status;
-    }
-
-    // The mapping_vmo contains exclusively the data we want to map, so our mapping
-    // offset is 0.
-    mapping_vmo_offset = 0;
-
-    SetVmoName<kVmoNamePrefixData>(cloned_vmo.borrow(), base_name, num_mapped_file_regions);
-    num_mapped_file_regions++;
+zx_status_t VmarLoader::MapZeroFill(uintptr_t vmar_offset, std::string_view base_name, size_t size,
+                                    size_t& num_zero_segments) {
+  zx::vmo vmo;
+  zx_status_t status = zx::vmo::create(size, 0, &vmo);
+  if (status != ZX_OK) [[unlikely]] {
+    return status;
   }
 
-  zx_vaddr_t mapping_addr;
-  return child_vmar_.map(options, vmar_offset, cloned_vmo ? *(cloned_vmo.borrow()) : *vmo,
-                         mapping_vmo_offset, map_size, &mapping_addr);
+  SetVmoName<kVmoNamePrefixBss>(vmo.borrow(), base_name, num_zero_segments++);
+
+  return Map(vmar_offset, kMapWritable, vmo.borrow(), 0, size);
 }
 
 }  // namespace elfldltl
