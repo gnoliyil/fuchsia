a12163cdd647e27156c73be45b62188014afa0a2
Clayton Wilkinson <wilkinsonclay@google.com>
2023-01-06 00:17:38 +0000

    [emu] Removing references to `fx emu`
    This updates the comments in the upscripts provided as part of Fuchsia.
    
    Change-Id: I48ce61b8951e7da52d033598c6e9a10c11d6abe8
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/786690
    Reviewed-by: Renato Mangini Dias <mangini@google.com>
    Commit-Queue: Clayton Wilkinson <wilkinsonclay@google.com>

--- tools/devshell/lib/emu-ifup-macos.sh
+++ tools/devshell/lib/emu-ifup-macos.sh
@@ -3,8 +3,12 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 #
-# This script is required by fx emu to bring up the network
+# This script is required by the emulator to bring up the TAP network
 # interface on Mac OSX. Do not remove or edit this script
-# without testing it against fx emu.
+# without testing it against ffx emu.
+#
+# If you are using `--net tap` option with ffx emu, configure this file as
+# the upscript to use, by running:
+# ffx config set emu.upscript $FUCHSIA_ROOT/tools/devshell/lib/emu-ifup-macos.sh
 
 sudo ifconfig tap0 inet6 fc00::/7 up
