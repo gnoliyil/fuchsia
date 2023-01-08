89353a9532aaa3c17b684c85701e41f181f344f5
Sergii Parubochyi <sergiip@google.com>
2023-01-08 23:47:14 +0000

    [kernel][lib][efi] Add Runtime Services mock class
    Gigaboot UEFI feature reguires mocking `RuntimeServices`.
    
    Bug: b/260723701
    Change-Id: Ie7a64dd2b3d23c1a088099e41f64b56b7386ed5f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/778343
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: David Pursell <dpursell@google.com>
    Fuchsia-Auto-Submit: Sergii Parubochyi <sergiip@google.com>

---
+++ zircon/kernel/lib/efi/testing/include/lib/efi/testing/stub_runtime_services.h
@@ -0,0 +1,177 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ZIRCON_KERNEL_LIB_EFI_TESTING_INCLUDE_LIB_EFI_TESTING_STUB_RUNTIME_SERVICES_H_
+#define ZIRCON_KERNEL_LIB_EFI_TESTING_INCLUDE_LIB_EFI_TESTING_STUB_RUNTIME_SERVICES_H_
+
+#include <list>
+#include <map>
+#include <optional>
+
+#include <efi/runtime-services.h>
+#include <gmock/gmock.h>
+
+#include "mock_protocol_base.h"
+
+namespace efi {
+
+// Runtime services EFI stubs.
+//
+// The runtime services EFI table is complicated enough that it would be
+// difficult to fake out all the APIs properly. Instead, we provide these stubs
+// to allow tests to easily mock out the functionality they need, either with
+// gmock or by subclassing and implementing the functions they need.
+//
+// Some of the more trivial functionality will be implemented, but can still
+// be overridden by subclasses.
+//
+// Tests that are willing to use gmock should generally prefer to use
+// MockRuntimeServices instead, which hooks up the proper mock wrappers and adds
+// some additional utility functions.
+class StubRuntimeServices {
+ private:
+  // Not copyable or movable.
+  StubRuntimeServices(const StubRuntimeServices&) = delete;
+  StubRuntimeServices& operator=(const StubRuntimeServices&) = delete;
+
+ public:
+  // IMPORTANT: only ONE StubRuntimeServices can exist at a time. Since this is
+  // intended to be a global singleton in EFI this shouldn't be a problem,
+  // but if a test does attempt to create a second it will cause an exit().
+  StubRuntimeServices();
+  virtual ~StubRuntimeServices();
+
+  // Returns the underlying efi_runtime_services struct.
+  efi_runtime_services* services() { return &services_; }
+
+  struct VariableName {
+    std::u16string var_name;
+    efi_guid guid;
+  };
+  using VariableValue = std::vector<uint8_t>;
+
+  void SetVariables(const std::list<std::pair<VariableName, VariableValue>>& vars);
+  void UnsetVariables();
+
+  // EFI function implementations.
+
+  virtual efi_status GetTime(efi_time* time, efi_time_capabilities* capabilities) {
+    return EFI_UNSUPPORTED;
+  }
+  virtual efi_status SetTime(efi_time* time) { return EFI_UNSUPPORTED; }
+  virtual efi_status GetWakeupTime(bool* enabled, bool* pending, efi_time* time) {
+    return EFI_UNSUPPORTED;
+  }
+  virtual efi_status SetWakeupTime(bool enable, efi_time* time) { return EFI_UNSUPPORTED; }
+  virtual efi_status SetVirtualAddressMap(size_t memory_map_size, size_t desc_size,
+                                          uint32_t desc_version,
+                                          efi_memory_descriptor* virtual_map) {
+    return EFI_UNSUPPORTED;
+  }
+  virtual efi_status ConvertPointer(size_t debug_disposition, void** addr) {
+    return EFI_UNSUPPORTED;
+  }
+  virtual efi_status GetVariable(char16_t* var_name, efi_guid* vendor_guid, uint32_t* attributes,
+                                 size_t* data_size, void* data);
+  virtual efi_status GetNextVariableName(size_t* var_name_size, char16_t* var_name,
+                                         efi_guid* vendor_guid);
+  virtual efi_status SetVariable(char16_t* var_name, efi_guid* vendor_guid, uint32_t attributes,
+                                 size_t data_size, const void* data) {
+    return EFI_UNSUPPORTED;
+  }
+  virtual efi_status GetNextHighMonotonicCount(uint32_t* high_count) { return EFI_UNSUPPORTED; }
+  virtual efi_status ResetSystem(efi_reset_type reset_type, efi_status reset_status,
+                                 size_t data_size, void* reset_data) {
+    return EFI_UNSUPPORTED;
+  }
+  virtual efi_status UpdateCapsule(efi_capsule_header** capsule_header_array, size_t capsule_count,
+                                   efi_physical_addr scatter_gather_list) {
+    return EFI_UNSUPPORTED;
+  }
+  virtual efi_status QueryCapsuleCapabilities(efi_capsule_header** capsule_header_array,
+                                              size_t capsule_count, uint64_t* max_capsule_size,
+                                              efi_reset_type* reset_type) {
+    return EFI_UNSUPPORTED;
+  }
+  virtual efi_status QueryVariableInfo(uint32_t attributes, uint64_t* max_var_storage_size,
+                                       uint64_t* remaining_var_storage_size,
+                                       uint64_t* max_var_size);
+
+ private:
+  efi_runtime_services services_;
+
+  std::optional<std::list<std::pair<VariableName, VariableValue>>> vars_;
+  std::list<std::pair<VariableName, VariableValue>>::const_iterator var_it_;
+};
+
+// Subclasses StubRuntimeServices to mock out methods using gmock.
+//
+// This will likely be the most common way to test runtime services, but gmock
+// is significantly more complicated than gtest and some projects may prefer
+// to avoid it, so the base class is still available for direct use.
+class MockRuntimeServices : public StubRuntimeServices {
+ public:
+  MOCK_METHOD(efi_status, GetTime, (efi_time * time, efi_time_capabilities* capabilities),
+              (override));
+
+  MOCK_METHOD(efi_status, SetTime, (efi_time * time), (override));
+
+  MOCK_METHOD(efi_status, GetWakeupTime, (bool* enabled, bool* pending, efi_time* time),
+              (override));
+
+  MOCK_METHOD(efi_status, SetWakeupTime, (bool enable, efi_time* time), (override));
+
+  MOCK_METHOD(efi_status, SetVirtualAddressMap,
+              (size_t memory_map_size, size_t desc_size, uint32_t desc_version,
+               efi_memory_descriptor* virtual_map),
+              (override));
+
+  MOCK_METHOD(efi_status, ConvertPointer, (size_t debug_disposition, void** addr), (override));
+
+  MOCK_METHOD(efi_status, GetVariable,
+              (char16_t* var_name, efi_guid* vendor_guid, uint32_t* attributes, size_t* data_size,
+               void* data),
+              (override));
+
+  MOCK_METHOD(efi_status, GetNextVariableName,
+              (size_t * var_name_size, char16_t* var_name, efi_guid* vendor_guid), (override));
+
+  MOCK_METHOD(efi_status, SetVariable,
+              (char16_t* var_name, efi_guid* vendor_guid, uint32_t attributes, size_t data_size,
+               const void* data),
+              (override));
+
+  MOCK_METHOD(efi_status, GetNextHighMonotonicCount, (uint32_t * high_count), (override));
+
+  MOCK_METHOD(efi_status, ResetSystem,
+              (efi_reset_type reset_type, efi_status reset_status, size_t data_size,
+               void* reset_data),
+              (override));
+
+  MOCK_METHOD(efi_status, UpdateCapsule,
+              (efi_capsule_header * *capsule_header_array, size_t capsule_count,
+               efi_physical_addr scatter_gather_list),
+              (override));
+
+  MOCK_METHOD(efi_status, QueryCapsuleCapabilities,
+              (efi_capsule_header * *capsule_header_array, size_t capsule_count,
+               uint64_t* max_capsule_size, efi_reset_type* reset_type),
+              (override));
+
+  MOCK_METHOD(efi_status, QueryVariableInfo,
+              (uint32_t attributes, uint64_t* max_var_storage_size,
+               uint64_t* remaining_var_storage_size, uint64_t* max_var_size),
+              (override));
+};
+
+bool operator!=(const StubRuntimeServices::VariableName& l,
+                const StubRuntimeServices::VariableName& r);
+bool operator==(const StubRuntimeServices::VariableName& l,
+                const StubRuntimeServices::VariableName& r);
+bool operator<(const StubRuntimeServices::VariableName& l,
+               const StubRuntimeServices::VariableName& r);
+
+}  // namespace efi
+
+#endif  // ZIRCON_KERNEL_LIB_EFI_TESTING_INCLUDE_LIB_EFI_TESTING_STUB_RUNTIME_SERVICES_H_
---
+++ zircon/kernel/lib/efi/testing/stub_runtime_services.cc
@@ -0,0 +1,179 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/efi/testing/stub_runtime_services.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <algorithm>
+#include <iterator>
+
+namespace efi {
+
+namespace {
+
+using ::testing::_;
+using ::testing::DoAll;
+using ::testing::Return;
+using ::testing::SetArgPointee;
+
+// We need to stash the global StubRuntimeServices object here since there's
+// no "self" parameter to any of these functions.
+StubRuntimeServices* active_stub = nullptr;
+
+// Wrapper to bounce the EFI C function pointer into our global StubRuntimeServices
+// object.
+template <auto func, typename... Args>
+EFIAPI efi_status Wrap(Args... args) {
+  if (!active_stub) {
+    // Someone held onto the underlying function table after deleting
+    // the parent StubRuntimeServices.
+    return EFI_NOT_READY;
+  }
+  return (active_stub->*func)(args...);
+}
+
+}  // namespace
+
+StubRuntimeServices::StubRuntimeServices()
+    : services_{
+          .GetTime = Wrap<&StubRuntimeServices::GetTime>,
+          .SetTime = Wrap<&StubRuntimeServices::SetTime>,
+          .GetWakeupTime = Wrap<&StubRuntimeServices::GetWakeupTime>,
+          .SetWakeupTime = Wrap<&StubRuntimeServices::SetWakeupTime>,
+          .SetVirtualAddressMap = Wrap<&StubRuntimeServices::SetVirtualAddressMap>,
+          .ConvertPointer = Wrap<&StubRuntimeServices::ConvertPointer>,
+          .GetVariable = Wrap<&StubRuntimeServices::GetVariable>,
+          .GetNextVariableName = Wrap<&StubRuntimeServices::GetNextVariableName>,
+          .SetVariable = Wrap<&StubRuntimeServices::SetVariable>,
+          .GetNextHighMonotonicCount = Wrap<&StubRuntimeServices::GetNextHighMonotonicCount>,
+          .ResetSystem = Wrap<&StubRuntimeServices::ResetSystem>,
+          .UpdateCapsule = Wrap<&StubRuntimeServices::UpdateCapsule>,
+          .QueryCapsuleCapabilities = Wrap<&StubRuntimeServices::QueryCapsuleCapabilities>,
+          .QueryVariableInfo = Wrap<&StubRuntimeServices::QueryVariableInfo>,
+      } {
+  if (active_stub) {
+    // We cannot support multiple StubRuntimeServices due to the global singleton
+    // nature. Rather than causing hard-to-debug test behavior here, just fail
+    // loudly and immediately.
+    fprintf(stderr, "ERROR: cannot create multiple StubRuntimeService objects - exiting\n");
+    exit(1);
+  }
+  active_stub = this;
+}
+
+StubRuntimeServices::~StubRuntimeServices() { active_stub = nullptr; }
+
+void StubRuntimeServices::SetVariables(
+    const std::list<std::pair<VariableName, VariableValue>>& vars) {
+  vars_ = vars;
+  var_it_ = vars_->end();
+}
+
+void StubRuntimeServices::UnsetVariables() { vars_.reset(); }
+
+// Safe length() implementation. Returns number of elements before `u'\0'` or `len`
+inline size_t StrNLength(const char16_t* str, size_t len) {
+  const char16_t* eof = std::char_traits<char16_t>::find(str, len, u'\0');
+  return eof ? std::distance(str, eof) : len;
+}
+
+efi_status StubRuntimeServices::GetVariable(char16_t* var_name, efi_guid* vendor_guid,
+                                            uint32_t* attributes, size_t* data_size, void* data) {
+  if (!vars_)
+    return EFI_UNSUPPORTED;
+
+  auto it = std::find_if(vars_->begin(), vars_->end(),
+                         [&](const std::pair<VariableName, VariableValue>& it) {
+                           return it.first == VariableName{var_name, *vendor_guid};
+                         });
+  if (it == vars_->end())
+    return EFI_NOT_FOUND;
+
+  if (*data_size < it->second.size()) {
+    *data_size = it->second.size();
+    return EFI_BUFFER_TOO_SMALL;
+  }
+
+  std::copy_n(it->second.begin(), it->second.size(), (uint8_t*)data);
+  *data_size = it->second.size();
+
+  return EFI_SUCCESS;
+}
+
+efi_status StubRuntimeServices::GetNextVariableName(size_t* var_name_size, char16_t* var_name,
+                                                    efi_guid* vendor_guid) {
+  if (!vars_)
+    return EFI_UNSUPPORTED;
+
+  size_t str_len_safe = StrNLength(var_name, *var_name_size);
+  std::u16string var_name_str(var_name, str_len_safe);
+  auto it = var_it_;
+  if (var_name_str.compare(u"") == 0) {
+    // startover from the beginning
+    var_it_ = vars_->begin();
+    it = var_it_;
+  } else {
+    // If last value is passed increment tmp iterator
+    if (it == vars_->end())
+      return EFI_NOT_FOUND;
+
+    if (it->first == VariableName{var_name_str, *vendor_guid})
+      ++it;
+  }
+
+  if (it == vars_->end())
+    return EFI_NOT_FOUND;
+
+  const auto& it_var_name = it->first.var_name;
+  const size_t it_var_name_size = (it_var_name.size() + 1) * sizeof(it_var_name[0]);
+  if (it_var_name_size > *var_name_size) {
+    *var_name_size = it_var_name_size;
+    return EFI_BUFFER_TOO_SMALL;
+  }
+
+  it_var_name.copy(var_name, it_var_name.size());
+  var_name[it_var_name.size()] = u'\0';
+  *vendor_guid = it->first.guid;
+  var_it_ = it;
+
+  return EFI_SUCCESS;
+}
+
+efi_status StubRuntimeServices::QueryVariableInfo(uint32_t attributes,
+                                                  uint64_t* max_var_storage_size,
+                                                  uint64_t* remaining_var_storage_size,
+                                                  uint64_t* max_var_size) {
+  *max_var_storage_size = 1;
+  *remaining_var_storage_size = 2;
+  *max_var_size = 3;
+  return EFI_SUCCESS;
+}
+
+bool operator<(const StubRuntimeServices::VariableName& l,
+               const StubRuntimeServices::VariableName& r) {
+  auto cmp = l.var_name.compare(r.var_name);
+  if (cmp == 0) {
+    return l.guid < r.guid;
+  } else if (cmp < 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool operator==(const StubRuntimeServices::VariableName& l,
+                const StubRuntimeServices::VariableName& r) {
+  if (l.var_name.compare(r.var_name) == 0) {
+    return l.guid == r.guid;
+  }
+  return false;
+}
+
+bool operator!=(const StubRuntimeServices::VariableName& l,
+                const StubRuntimeServices::VariableName& r) {
+  return !(l == r);
+}
+
+}  // namespace efi
