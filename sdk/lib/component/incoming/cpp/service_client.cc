07ac27d4078d1ce69d7c3e726d74a18a70424eca
Yaneury Fermin <yaneury@google.com>
2023-01-09 22:03:41 +0000

    [incoming_cpp] Move internal functions to separate file
    This CL moves all functions including the component::internal
    namespace to the files internal.h and internal.cc. This is to make
    it easier for grok the API as an user.
    
    Bug: 119096
    Change-Id: I6a13cfe215150df6dd9b689236efd04584317cb3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/787438
    Reviewed-by: Gary Bressler <geb@google.com>
    Commit-Queue: Yaneury Fermin <yaneury@google.com>

--- sdk/lib/component/incoming/cpp/service_client.cc
+++ sdk/lib/component/incoming/cpp/service_client.cc
@@ -14,72 +14,6 @@ zx::result<fidl::ClientEnd<fuchsia_io::Directory>> OpenServiceRoot(std::string_v
   return component::Connect<fuchsia_io::Directory>(path);
 }
 
-namespace internal {
-
-zx::result<zx::channel> ConnectRaw(std::string_view path) {
-  zx::channel client_end, server_end;
-  if (zx_status_t status = zx::channel::create(0, &client_end, &server_end); status != ZX_OK) {
-    return zx::error(status);
-  }
-  if (zx::result<> status = ConnectRaw(std::move(server_end), path); status.is_error()) {
-    return status.take_error();
-  }
-  return zx::ok(std::move(client_end));
-}
-
-zx::result<> ConnectRaw(zx::channel server_end, std::string_view path) {
-  if (zx_status_t status = fdio_service_connect(std::string(path).c_str(), server_end.release());
-      status != ZX_OK) {
-    return zx::error(status);
-  }
-  return zx::ok();
-}
-
-zx::result<zx::channel> ConnectAtRaw(fidl::UnownedClientEnd<fuchsia_io::Directory> svc_dir,
-                                     std::string_view protocol_name) {
-  zx::channel client_end, server_end;
-  if (zx_status_t status = zx::channel::create(0, &client_end, &server_end); status != ZX_OK) {
-    return zx::error(status);
-  }
-  if (zx::result<> status = ConnectAtRaw(svc_dir, std::move(server_end), protocol_name);
-      status.is_error()) {
-    return status.take_error();
-  }
-  return zx::ok(std::move(client_end));
-}
-
-zx::result<> ConnectAtRaw(fidl::UnownedClientEnd<fuchsia_io::Directory> svc_dir,
-                          zx::channel server_end, std::string_view protocol_name) {
-  if (zx_status_t status = fdio_service_connect_at(
-          svc_dir.handle()->get(), std::string(protocol_name).c_str(), server_end.release());
-      status != ZX_OK) {
-    return zx::error(status);
-  }
-  return zx::ok();
-}
-
-zx::result<> CloneRaw(fidl::UnownedClientEnd<fuchsia_io::Node>&& node, zx::channel server_end) {
-  const fidl::Status result =
-      fidl::WireCall(node)->Clone(fuchsia_io::wire::OpenFlags::kCloneSameRights,
-                                  fidl::ServerEnd<fuchsia_io::Node>(std::move(server_end)));
-  if (!result.ok()) {
-    return zx::error(result.status());
-  }
-  return zx::ok();
-}
-
-zx::result<> CloneRaw(fidl::UnownedClientEnd<fuchsia_unknown::Cloneable>&& cloneable,
-                      zx::channel server_end) {
-  const fidl::Status result = fidl::WireCall(cloneable)->Clone2(
-      fidl::ServerEnd<fuchsia_unknown::Cloneable>(std::move(server_end)));
-  if (!result.ok()) {
-    return zx::error(result.status());
-  }
-  return zx::ok();
-}
-
-}  // namespace internal
-
 }  // namespace component
 
 namespace component {
@@ -116,21 +50,6 @@ zx::result<fidl::StringView> ValidateAndJoinPath(fidl::Array<char, kMaxPath>* bu
 }
 
 }  // namespace
-
-namespace internal {
-
-zx::result<> DirectoryOpenFunc(zx::unowned_channel dir, fidl::StringView path,
-                               fidl::internal::AnyTransport remote) {
-  constexpr fuchsia_io::wire::OpenFlags flags =
-      fuchsia_io::wire::OpenFlags::kRightReadable | fuchsia_io::wire::OpenFlags::kRightWritable;
-  fidl::UnownedClientEnd<fuchsia_io::Directory> dir_end(dir);
-  fidl::ServerEnd<fuchsia_io::Node> node_end(remote.release<fidl::internal::ChannelTransport>());
-  fidl::Status result =
-      fidl::WireCall<fuchsia_io::Directory>(dir_end)->Open(flags, 0755u, path, std::move(node_end));
-  return zx::make_result(result.status());
-}
-
-}  // namespace internal
 
 zx::result<> OpenNamedServiceAt(fidl::UnownedClientEnd<fuchsia_io::Directory> dir,
                                 std::string_view service, std::string_view instance,
