03aec6d62359ed450a8494c6225d3d9f9ae24bb0
Alex Brachet <abrachet@google.com>
2023-01-12 01:23:15 +0000

    [inspector][libc] Add build workaround for gwp-asan roll
    Bug: 119315
    Change-Id: Ic6fba899af8fed4d8eed9f9e4c810ceab5d018a5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/788782
    Fuchsia-Auto-Submit: Alex Brachet <abrachet@google.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- zircon/system/ulib/inspector/gwp-asan.cc
+++ zircon/system/ulib/inspector/gwp-asan.cc
@@ -121,7 +121,13 @@ bool inspector_get_gwp_asan_info(const zx::process& process,
   }
 
   if (!faulting_addr) {
+// TODO(fxbug.dev/119315): Remove this workaround after gwp-asan rolls
+#ifndef NEW_CRASH_ADDR_API
     faulting_addr = __gwp_asan_get_internal_crash_address(&state);
+#else
+    faulting_addr =
+        __gwp_asan_get_internal_crash_address(&state, state.internallyDetectedErrorFaultAddress());
+#endif
   }
 
   info->faulting_addr = faulting_addr;
