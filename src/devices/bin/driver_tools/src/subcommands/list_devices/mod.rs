a0e39a585495aedf5caa9b7da1594a39b7c55f11
Sarah Chan <spqchan@google.com>
2023-01-12 19:31:22 +0000

    [fdf] Add warning to ffx driver list-devices
    Add a warning to ffx driver list-devices that
    unbound composites are no longer added to the topology
    in DFv1. This warning only appears in DFv1.
    
    Bug: 118660
    Change-Id: I1a327512841adafd1364cc962f04c2f8da024e77
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/788708
    Reviewed-by: David Gilhooley <dgilhooley@google.com>
    Commit-Queue: Sarah Chan <spqchan@google.com>

--- src/devices/bin/driver_tools/src/subcommands/list_devices/mod.rs
+++ src/devices/bin/driver_tools/src/subcommands/list_devices/mod.rs
@@ -202,5 +202,15 @@ pub async fn list_devices(
             println!("No devices found.");
         }
     }
+
+    // TODO(fxb/118660): Remove this warning once ffx driver supports unbound composites.
+    if !driver_development_proxy.is_dfv2().await? {
+        println!(
+            "\n\n\
+            WARNING: Unbound composites are no longer added to the topology in DFv1. \
+            See fxbug.dev/118660 for more information."
+        );
+    }
+
     Ok(())
 }
