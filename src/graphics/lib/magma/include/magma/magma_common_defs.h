8a1ef2c991d8707f30a95796f4cac49a995b9470
John Bauman <jbauman@google.com>
2023-01-13 14:58:47 +0000

    [magma] Use #defines for constants
    This is consistent with the C library rubric. The rubric also requires
    that all defines include a cast to the proper type.
    
    Bug: 108279
    Change-Id: I145c59f2a1d3fdad8cdff86ca3f0b5960ca9e9a4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/788837
    Commit-Queue: John Bauman <jbauman@google.com>
    Reviewed-by: Craig Stout <cstout@google.com>

--- src/graphics/lib/magma/include/magma/magma_common_defs.h
+++ src/graphics/lib/magma/include/magma/magma_common_defs.h
@@ -19,181 +19,182 @@ extern "C" {
 #define MAGMA_EXPORT
 #endif
 
+typedef uint64_t magma_query_t;
+
 // This is a list of vendor-neutral queries that can be passed to magma_query.
 // Returns the hardware vendor ID (simple result) - should be the PCI ID of the GPU vendor
 // if possible, or the Khronos vendor ID otherwise.
-#define MAGMA_QUERY_VENDOR_ID 0
+#define MAGMA_QUERY_VENDOR_ID ((magma_query_t)0)
 // Returns the hardware device ID (simple result)
-#define MAGMA_QUERY_DEVICE_ID 1
+#define MAGMA_QUERY_DEVICE_ID ((magma_query_t)1)
 // 2 was MAGMA_QUERY_IS_TEST_RESTART_SUPPORTED
 // Returns true if MAGMA_QUERY_TOTAL_TIME is supported (simple result)
-#define MAGMA_QUERY_IS_TOTAL_TIME_SUPPORTED 3
+#define MAGMA_QUERY_IS_TOTAL_TIME_SUPPORTED ((magma_query_t)3)
 // 4 was MAGMA_QUERY_MINIMUM_MAPPABLE_ADDRESS
 // Upper 32bits: max inflight messages, lower 32bits: max inflight memory (MB) (simple result)
-#define MAGMA_QUERY_MAXIMUM_INFLIGHT_PARAMS 5
+#define MAGMA_QUERY_MAXIMUM_INFLIGHT_PARAMS ((magma_query_t)5)
 
 // All vendor-specific queries IDs that can be passed to magma_query must be >=
 // MAGMA_QUERY_VENDOR_PARAM_0.
-#define MAGMA_QUERY_VENDOR_PARAM_0 10000
+#define MAGMA_QUERY_VENDOR_PARAM_0 ((magma_query_t)10000)
 
 // This is a list of vendor-neutral queries that can be passed to magma_query.
 // Returns a struct magma_total_time_query_result (buffer result)
-#define MAGMA_QUERY_TOTAL_TIME 500
+#define MAGMA_QUERY_TOTAL_TIME ((magma_query_t)500)
 
 // reserved ID to represent an invalid object
-#define MAGMA_INVALID_OBJECT_ID 0ull
+#define MAGMA_INVALID_OBJECT_ID ((uint64_t)0ull)
 
 // All vendor-specific command buffer flags must be >=
 // MAGMA_COMMAND_BUFFER_VENDOR_FLAGS_0.
-#define MAGMA_COMMAND_BUFFER_VENDOR_FLAGS_0 (1ull << 16)
-
-// possible values for magma_status_t
-#define MAGMA_STATUS_OK (0)
-#define MAGMA_STATUS_INTERNAL_ERROR (-1)
-#define MAGMA_STATUS_INVALID_ARGS (-2)
-#define MAGMA_STATUS_ACCESS_DENIED (-3)
-#define MAGMA_STATUS_MEMORY_ERROR (-4)
-#define MAGMA_STATUS_CONTEXT_KILLED (-5)
-#define MAGMA_STATUS_CONNECTION_LOST (-6)
-#define MAGMA_STATUS_TIMED_OUT (-7)
-#define MAGMA_STATUS_UNIMPLEMENTED (-8)
-// This error means that an object was not in the right state for an operation on it.
-#define MAGMA_STATUS_BAD_STATE (-9)
-#define MAGMA_STATUS_ALIAS_FOR_LAST MAGMA_STATUS_BAD_STATE
-
-// possible values for magma_cache_operation_t
-#define MAGMA_CACHE_OPERATION_CLEAN 0
-#define MAGMA_CACHE_OPERATION_CLEAN_INVALIDATE 1
-
-// possible values for magma_cache_policy_t
-#define MAGMA_CACHE_POLICY_CACHED 0
-#define MAGMA_CACHE_POLICY_WRITE_COMBINING 1
-#define MAGMA_CACHE_POLICY_UNCACHED 2
-
-#define MAGMA_DUMP_TYPE_NORMAL (1 << 0)
-
-#define MAGMA_PERF_COUNTER_RESULT_DISCONTINUITY (1 << 0)
-
-enum {
-  // Values must match fuchsia.sysmem.PixelFormatType
-  MAGMA_FORMAT_INVALID = 0,
-  MAGMA_FORMAT_R8G8B8A8 = 1,
-  MAGMA_FORMAT_BGRA32 = 101,
-  MAGMA_FORMAT_I420 = 102,
-  MAGMA_FORMAT_M420 = 103,
-  MAGMA_FORMAT_NV12 = 104,
-  MAGMA_FORMAT_YUY2 = 105,
-  MAGMA_FORMAT_MJPEG = 106,
-  MAGMA_FORMAT_YV12 = 107,
-  MAGMA_FORMAT_BGR24 = 108,
-  MAGMA_FORMAT_RGB565 = 109,
-  MAGMA_FORMAT_RGB332 = 110,
-  MAGMA_FORMAT_RGB2220 = 111,
-  MAGMA_FORMAT_L8 = 112,
-  MAGMA_FORMAT_R8 = 113,
-  MAGMA_FORMAT_R8G8 = 114,
-};
-
-// These must match the fuchsia.sysmem format modifier values.
-enum {
-  MAGMA_FORMAT_MODIFIER_LINEAR = 0x0000000000000000,
-
-  MAGMA_FORMAT_MODIFIER_INTEL_X_TILED = 0x0100000000000001,
-  MAGMA_FORMAT_MODIFIER_INTEL_Y_TILED = 0x0100000000000002,
-  MAGMA_FORMAT_MODIFIER_INTEL_YF_TILED = 0x0100000000000003,
-
-  MAGMA_FORMAT_MODIFIER_INTEL_Y_TILED_CCS = 0x0100000001000002,
-  MAGMA_FORMAT_MODIFIER_INTEL_YF_TILED_CCS = 0x0100000001000003,
-
-  MAGMA_FORMAT_MODIFIER_ARM_YUV_BIT = 0x10,
-  MAGMA_FORMAT_MODIFIER_ARM_SPLIT_BLOCK_BIT = 0x20,
-  MAGMA_FORMAT_MODIFIER_ARM_SPARSE_BIT = 0x40,
-  MAGMA_FORMAT_MODIFIER_ARM_BCH_BIT = 0x800,
-  MAGMA_FORMAT_MODIFIER_ARM_TE_BIT = 0x1000,
-  MAGMA_FORMAT_MODIFIER_ARM_TILED_HEADER_BIT = 0x2000,
-
-  MAGMA_FORMAT_MODIFIER_ARM = 0x0800000000000000,
-  MAGMA_FORMAT_MODIFIER_ARM_AFBC_16X16 = 0x0800000000000001,
-  MAGMA_FORMAT_MODIFIER_ARM_AFBC_32X8 = 0x0800000000000002,
-  MAGMA_FORMAT_MODIFIER_ARM_LINEAR_TE = 0x0800000000001000,
-  MAGMA_FORMAT_MODIFIER_ARM_AFBC_16X16_TE = 0x0800000000001001,
-  MAGMA_FORMAT_MODIFIER_ARM_AFBC_32X8_TE = 0x0800000000001002,
-
-  MAGMA_FORMAT_MODIFIER_ARM_AFBC_16X16_YUV_TILED_HEADER = 0x0800000000002011,
-
-  MAGMA_FORMAT_MODIFIER_ARM_AFBC_16X16_SPLIT_BLOCK_SPARSE_YUV = 0x0800000000000071,
-  MAGMA_FORMAT_MODIFIER_ARM_AFBC_16X16_SPLIT_BLOCK_SPARSE_YUV_TE = 0x0800000000001071,
-
-  MAGMA_FORMAT_MODIFIER_ARM_AFBC_16X16_SPLIT_BLOCK_SPARSE_YUV_TILED_HEADER = 0x0800000000002071,
-  MAGMA_FORMAT_MODIFIER_ARM_AFBC_16X16_SPLIT_BLOCK_SPARSE_YUV_TE_TILED_HEADER = 0x0800000000003071,
-};
-
-// Must match fuchsia.sysmem.ColorSpaceType values.
-enum {
-  MAGMA_COLORSPACE_INVALID = 0,
-  MAGMA_COLORSPACE_SRGB = 1,
-  MAGMA_COLORSPACE_REC601_NTSC = 2,
-  MAGMA_COLORSPACE_REC601_NTSC_FULL_RANGE = 3,
-  MAGMA_COLORSPACE_REC601_PAL = 4,
-  MAGMA_COLORSPACE_REC601_PAL_FULL_RANGE = 5,
-  MAGMA_COLORSPACE_REC709 = 6,
-  MAGMA_COLORSPACE_REC2020 = 7,
-  MAGMA_COLORSPACE_REC2100 = 8,
-};
-
-enum {
-  MAGMA_COHERENCY_DOMAIN_CPU = 0,
-  MAGMA_COHERENCY_DOMAIN_RAM = 1,
-  MAGMA_COHERENCY_DOMAIN_INACCESSIBLE = 2,
-};
-
-enum { MAGMA_POLL_TYPE_SEMAPHORE = 1, MAGMA_POLL_TYPE_HANDLE = 2 };
-
-enum {
-  MAGMA_POLL_CONDITION_READABLE = 1,
-  MAGMA_POLL_CONDITION_SIGNALED = 3,
-};
-
-enum {
-  // Eagerly populate hardware page tables with the pages mapping in this range, committing pages as
-  // needed. This is not needed for MAGMA_MAP_FLAG_GROWABLE allocations, since the page tables
-  // will be populated on demand.
-  MAGMA_BUFFER_RANGE_OP_POPULATE_TABLES = 1,
-  // Commit memory on the client thread. Hardware page tables may not be populated. This should be
-  // used
-  // before POPULATE_TABLES to ensure the expensive work of committing pages happens with the
-  // correct priority and without blocking the processing in the MSD of commands from other threads
-  // from the same connection.
-  MAGMA_BUFFER_RANGE_OP_COMMIT = 2,
-  // Depopulate hardware page table mappings for this range. This prevents the hardware from
-  // accessing
-  // pages in that range, but the pages retain their contents.
-  MAGMA_BUFFER_RANGE_OP_DEPOPULATE_TABLES = 3,
-  // Decommit memory wholy on the client thread. This may fail if the MSD currently has the page
-  // tables populated.
-  MAGMA_BUFFER_RANGE_OP_DECOMMIT = 4,
-};
-
-enum {
-  // Set `is_secure` flag in the `BufferMemorySettings` so protected memory is allocated.
-  MAGMA_SYSMEM_FLAG_PROTECTED = 1 << 0,
-  // This flag is only used to modify the name of the buffer to signal that the client requested it
-  // using vkAllocateMemory or similar.
-  MAGMA_SYSMEM_FLAG_FOR_CLIENT = 1 << 2,
-};
-
-#define MAGMA_MAX_IMAGE_PLANES 4
-
-#define MAGMA_MAX_DRM_FORMAT_MODIFIERS 16
+#define MAGMA_COMMAND_BUFFER_VENDOR_FLAGS_0 ((uint64_t)(1ull << 16))
 
 typedef int32_t magma_status_t;
 
-// Normal bool doesn't have to be a particular size.
-typedef uint8_t magma_bool_t;
+// possible values for magma_status_t
+#define MAGMA_STATUS_OK ((magma_status_t)(0))
+#define MAGMA_STATUS_INTERNAL_ERROR ((magma_status_t)(-1))
+#define MAGMA_STATUS_INVALID_ARGS ((magma_status_t)(-2))
+#define MAGMA_STATUS_ACCESS_DENIED ((magma_status_t)(-3))
+#define MAGMA_STATUS_MEMORY_ERROR ((magma_status_t)(-4))
+#define MAGMA_STATUS_CONTEXT_KILLED ((magma_status_t)(-5))
+#define MAGMA_STATUS_CONNECTION_LOST ((magma_status_t)(-6))
+#define MAGMA_STATUS_TIMED_OUT ((magma_status_t)(-7))
+#define MAGMA_STATUS_UNIMPLEMENTED ((magma_status_t)(-8))
+// This error means that an object was not in the right state for an operation on it.
+#define MAGMA_STATUS_BAD_STATE ((magma_status_t)(-9))
+#define MAGMA_STATUS_ALIAS_FOR_LAST MAGMA_STATUS_BAD_STATE
 
 typedef uint32_t magma_cache_operation_t;
+// possible values for magma_cache_operation_t
+#define MAGMA_CACHE_OPERATION_CLEAN ((magma_cache_operation_t)0)
+#define MAGMA_CACHE_OPERATION_CLEAN_INVALIDATE ((magma_cache_operation_t)1)
 
 typedef uint32_t magma_cache_policy_t;
+// possible values for magma_cache_policy_t
+#define MAGMA_CACHE_POLICY_CACHED ((magma_cache_policy_t)0)
+#define MAGMA_CACHE_POLICY_WRITE_COMBINING ((magma_cache_policy_t)1)
+#define MAGMA_CACHE_POLICY_UNCACHED ((magma_cache_policy_t)2)
+
+#define MAGMA_DUMP_TYPE_NORMAL ((uint32_t)(1 << 0))
+
+#define MAGMA_PERF_COUNTER_RESULT_DISCONTINUITY ((uint32_t)(1 << 0))
+
+typedef uint32_t magma_format_t;
+
+// Values must match fuchsia.sysmem.PixelFormatType
+#define MAGMA_FORMAT_INVALID ((magma_format_t)0)
+#define MAGMA_FORMAT_R8G8B8A8 ((magma_format_t)1)
+#define MAGMA_FORMAT_BGRA32 ((magma_format_t)101)
+#define MAGMA_FORMAT_I420 ((magma_format_t)102)
+#define MAGMA_FORMAT_M420 ((magma_format_t)103)
+#define MAGMA_FORMAT_NV12 ((magma_format_t)104)
+#define MAGMA_FORMAT_YUY2 ((magma_format_t)105)
+#define MAGMA_FORMAT_MJPEG ((magma_format_t)106)
+#define MAGMA_FORMAT_YV12 ((magma_format_t)107)
+#define MAGMA_FORMAT_BGR24 ((magma_format_t)108)
+#define MAGMA_FORMAT_RGB565 ((magma_format_t)109)
+#define MAGMA_FORMAT_RGB332 ((magma_format_t)110)
+#define MAGMA_FORMAT_RGB2220 ((magma_format_t)111)
+#define MAGMA_FORMAT_L8 ((magma_format_t)112)
+#define MAGMA_FORMAT_R8 ((magma_format_t)113)
+#define MAGMA_FORMAT_R8G8 ((magma_format_t)114)
+
+typedef uint64_t magma_format_modifier_t;
+// These must match the fuchsia.sysmem format modifier values.
+#define MAGMA_FORMAT_MODIFIER_LINEAR ((magma_format_modifier_t)0x0000000000000000)
+
+#define MAGMA_FORMAT_MODIFIER_INTEL_X_TILED ((magma_format_modifier_t)0x0100000000000001)
+#define MAGMA_FORMAT_MODIFIER_INTEL_Y_TILED ((magma_format_modifier_t)0x0100000000000002)
+#define MAGMA_FORMAT_MODIFIER_INTEL_YF_TILED ((magma_format_modifier_t)0x0100000000000003)
+
+#define MAGMA_FORMAT_MODIFIER_INTEL_Y_TILED_CCS ((magma_format_modifier_t)0x0100000001000002)
+#define MAGMA_FORMAT_MODIFIER_INTEL_YF_TILED_CCS ((magma_format_modifier_t)0x0100000001000003)
+
+#define MAGMA_FORMAT_MODIFIER_ARM_YUV_BIT ((magma_format_modifier_t)0x10)
+#define MAGMA_FORMAT_MODIFIER_ARM_SPLIT_BLOCK_BIT ((magma_format_modifier_t)0x20)
+#define MAGMA_FORMAT_MODIFIER_ARM_SPARSE_BIT ((magma_format_modifier_t)0x40)
+#define MAGMA_FORMAT_MODIFIER_ARM_BCH_BIT ((magma_format_modifier_t)0x800)
+#define MAGMA_FORMAT_MODIFIER_ARM_TE_BIT ((magma_format_modifier_t)0x1000)
+#define MAGMA_FORMAT_MODIFIER_ARM_TILED_HEADER_BIT ((magma_format_modifier_t)0x2000)
+
+#define MAGMA_FORMAT_MODIFIER_ARM ((magma_format_modifier_t)0x0800000000000000)
+#define MAGMA_FORMAT_MODIFIER_ARM_AFBC_16X16 ((magma_format_modifier_t)0x0800000000000001)
+#define MAGMA_FORMAT_MODIFIER_ARM_AFBC_32X8 ((magma_format_modifier_t)0x0800000000000002)
+#define MAGMA_FORMAT_MODIFIER_ARM_LINEAR_TE ((magma_format_modifier_t)0x0800000000001000)
+#define MAGMA_FORMAT_MODIFIER_ARM_AFBC_16X16_TE ((magma_format_modifier_t)0x0800000000001001)
+#define MAGMA_FORMAT_MODIFIER_ARM_AFBC_32X8_TE ((magma_format_modifier_t)0x0800000000001002)
+
+#define MAGMA_FORMAT_MODIFIER_ARM_AFBC_16X16_YUV_TILED_HEADER \
+  ((magma_format_modifier_t)0x0800000000002011)
+
+#define MAGMA_FORMAT_MODIFIER_ARM_AFBC_16X16_SPLIT_BLOCK_SPARSE_YUV \
+  ((magma_format_modifier_t)0x0800000000000071)
+#define MAGMA_FORMAT_MODIFIER_ARM_AFBC_16X16_SPLIT_BLOCK_SPARSE_YUV_TE \
+  ((magma_format_modifier_t)0x0800000000001071)
+
+#define MAGMA_FORMAT_MODIFIER_ARM_AFBC_16X16_SPLIT_BLOCK_SPARSE_YUV_TILED_HEADER \
+  ((magma_format_modifier_t)0x0800000000002071)
+#define MAGMA_FORMAT_MODIFIER_ARM_AFBC_16X16_SPLIT_BLOCK_SPARSE_YUV_TE_TILED_HEADER \
+  ((magma_format_modifier_t)0x0800000000003071)
+
+typedef uint32_t magma_colorspace_t;
+
+// Must match fuchsia.sysmem.ColorSpaceType values.
+#define MAGMA_COLORSPACE_INVALID ((magma_colorspace_t)0)
+#define MAGMA_COLORSPACE_SRGB ((magma_colorspace_t)1)
+#define MAGMA_COLORSPACE_REC601_NTSC ((magma_colorspace_t)2)
+#define MAGMA_COLORSPACE_REC601_NTSC_FULL_RANGE ((magma_colorspace_t)3)
+#define MAGMA_COLORSPACE_REC601_PAL ((magma_colorspace_t)4)
+#define MAGMA_COLORSPACE_REC601_PAL_FULL_RANGE ((magma_colorspace_t)5)
+#define MAGMA_COLORSPACE_REC709 ((magma_colorspace_t)6)
+#define MAGMA_COLORSPACE_REC2020 ((magma_colorspace_t)7)
+#define MAGMA_COLORSPACE_REC2100 ((magma_colorspace_t)8)
+
+typedef uint32_t magma_coherency_domain_t;
+
+#define MAGMA_COHERENCY_DOMAIN_CPU ((magma_coherency_domain_t)0)
+#define MAGMA_COHERENCY_DOMAIN_RAM ((magma_coherency_domain_t)1)
+#define MAGMA_COHERENCY_DOMAIN_INACCESSIBLE ((magma_coherency_domain_t)2)
+
+#define MAGMA_POLL_TYPE_SEMAPHORE ((uint32_t)1)
+#define MAGMA_POLL_TYPE_HANDLE ((uint32_t)2)
+
+#define MAGMA_POLL_CONDITION_READABLE ((uint32_t)1)
+#define MAGMA_POLL_CONDITION_SIGNALED ((uint32_t)3)
+
+typedef uint32_t magma_buffer_range_op_t;
+
+// Eagerly populate hardware page tables with the pages mapping in this range, committing pages as
+// needed. This is not needed for MAGMA_MAP_FLAG_GROWABLE allocations, since the page tables
+// will be populated on demand.
+#define MAGMA_BUFFER_RANGE_OP_POPULATE_TABLES ((magma_buffer_range_op_t)1)
+// Commit memory on the client thread. Hardware page tables may not be populated. This should be
+// used
+// before POPULATE_TABLES to ensure the expensive work of committing pages happens with the
+// correct priority and without blocking the processing in the MSD of commands from other threads
+// from the same connection.
+#define MAGMA_BUFFER_RANGE_OP_COMMIT ((magma_buffer_range_op_t)2)
+// Depopulate hardware page table mappings for this range. This prevents the hardware from
+// accessing
+// pages in that range, but the pages retain their contents.
+#define MAGMA_BUFFER_RANGE_OP_DEPOPULATE_TABLES ((magma_buffer_range_op_t)3)
+// Decommit memory wholy on the client thread. This may fail if the MSD currently has the page
+// tables populated.
+#define MAGMA_BUFFER_RANGE_OP_DECOMMIT ((magma_buffer_range_op_t)4)
+
+// Set `is_secure` flag in the `BufferMemorySettings` so protected memory is allocated.
+#define MAGMA_SYSMEM_FLAG_PROTECTED ((uint32_t)(1 << 0))
+// This flag is only used to modify the name of the buffer to signal that the client requested it
+// using vkAllocateMemory or similar.
+#define MAGMA_SYSMEM_FLAG_FOR_CLIENT ((uint32_t)(1 << 2))
+
+#define MAGMA_MAX_IMAGE_PLANES ((uint32_t)4)
+
+#define MAGMA_MAX_DRM_FORMAT_MODIFIERS ((uint32_t)16)
+
+// Normal bool doesn't have to be a particular size.
+typedef uint8_t magma_bool_t;
 
 typedef uint64_t magma_device_t;
 
@@ -276,25 +277,21 @@ struct magma_buffer_offset {
 };
 
 // The top 16 bits are reserved for vendor-specific flags.
-#define MAGMA_MAP_FLAG_VENDOR_SHIFT 16
+#define MAGMA_MAP_FLAG_VENDOR_SHIFT ((uint64_t)16)
 
-enum MAGMA_MAP_FLAGS {
-  MAGMA_MAP_FLAG_READ = (1 << 0),
-  MAGMA_MAP_FLAG_WRITE = (1 << 1),
-  MAGMA_MAP_FLAG_EXECUTE = (1 << 2),
-  MAGMA_MAP_FLAG_GROWABLE = (1 << 3),
-  MAGMA_MAP_FLAG_VENDOR_0 = (1 << MAGMA_MAP_FLAG_VENDOR_SHIFT),
-};
+#define MAGMA_MAP_FLAG_READ ((uint64_t)(1 << 0))
+#define MAGMA_MAP_FLAG_WRITE ((uint64_t)(1 << 1))
+#define MAGMA_MAP_FLAG_EXECUTE ((uint64_t)(1 << 2))
+#define MAGMA_MAP_FLAG_GROWABLE ((uint64_t)(1 << 3))
+#define MAGMA_MAP_FLAG_VENDOR_0 ((uint64_t)(1 << MAGMA_MAP_FLAG_VENDOR_SHIFT))
 
 typedef struct {
   uint64_t committed_byte_count;
   uint64_t size;
 } magma_buffer_info_t;
 
-enum {
-  MAGMA_IMAGE_CREATE_FLAGS_PRESENTABLE = 1,
-  MAGMA_IMAGE_CREATE_FLAGS_VULKAN_USAGE = 1 << 1,
-};
+#define MAGMA_IMAGE_CREATE_FLAGS_PRESENTABLE ((uint32_t)1)
+#define MAGMA_IMAGE_CREATE_FLAGS_VULKAN_USAGE ((uint32_t)(1 << 1))
 
 typedef struct {
   // A format specified by DRM (Linux Direct Rendering Manager)
