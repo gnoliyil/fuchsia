d8ed959befa858dae36034c5a234e10c464b77f9
Ian Fisher <iafisher@google.com>
2023-01-17 17:54:21 +0000

    [ffx][driver] Fix failure to connect in 'ffx driver dump -s'
    The selector parsing logic changed in fxr/744243, breaking our tool.
    
    Test: driver_dump_select_test; verified that this test fails without the
          fix
    MULTIPLY: driver_dump_select_test
    Bug: 85516
    Fixed: 113457
    Change-Id: I3f2eeb6d05473ac5ee8dcfd00ce38bb4665fe46e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/749422
    Reviewed-by: David Gilhooley <dgilhooley@google.com>
    Commit-Queue: Ian Fisher <iafisher@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>

---
+++ src/devices/bin/driver_tools/tests/src/main.rs
@@ -0,0 +1,24 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {
+    anyhow::Result,
+    fidl_fuchsia_driver_test as fdt,
+    fuchsia_component_test::RealmBuilder,
+    fuchsia_driver_test::{DriverTestRealmBuilder, DriverTestRealmInstance},
+    tracing::info,
+};
+
+#[fuchsia::main(logging = true)]
+async fn main() -> Result<(), anyhow::Error> {
+    // This is a test component that exists solely to spin up an instance of Driver Test Realm so
+    // that driver_dump_select_test.sh can test `ffx driver dump --select`.
+    let builder = RealmBuilder::new().await?;
+    builder.driver_test_realm_setup().await?;
+    let instance = builder.build().await?;
+    instance.driver_test_realm_start(fdt::RealmArgs::EMPTY).await?;
+
+    info!("Started Driver Test Realm. Now looping forever.");
+    loop {}
+}
