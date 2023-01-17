80e8e27c946c47b8ca70ee573a9a1fc0eb7fc5db
Kiet Tran <kiettran@google.com>
2023-01-17 22:33:39 +0000

    Reland "[wlan] Rust FullMAC USME"
    This is a reland of commit 97a8287a918c9bc43528c56755fd4330548e60a1
    
    Original change's description:
    > [wlan] Rust FullMAC USME
    >
    > Migrate wlanif conversion code into Rust FullMAC library with a
    > C-binding interface. At the same time, run SME in this FullMAC library.
    > Update the package configs so that the FullMAC code path no longer go
    > through wlanstack but the new library.
    >
    > Bug: 66772
    > Test:
    > 1. Verified that connecting manually to a protected network succeeded.
    > 2. WlanPhyComplianceABGTest and WlanSecurityComplianceABGTest ACTS tests
    >    passed.
    >
    > Change-Id: Ifbdab89b1d26e1062eaed5dd71a09c4da76f99fd
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/700883
    > Reviewed-by: Miguel Flores <miguelfrde@google.com>
    > Reviewed-by: Mike Comfoltey <comfoltey@google.com>
    > Commit-Queue: Kiet Tran <kiettran@google.com>
    > Size-Review: Gopi Krishna Chitluri <gopichitluri@google.com>
    > Reviewed-by: Dylan Swiggett <swiggett@google.com>
    > Reviewed-by: Aaron Wood <aaronwood@google.com>
    
    Bug: 66772
    Change-Id: Ifeb4e62b399c27bae52f2267939a4d6e6bef93b1
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/790542
    Reviewed-by: Dylan Swiggett <swiggett@google.com>
    Reviewed-by: Miguel Flores <miguelfrde@google.com>
    Reviewed-by: Aaron Wood <aaronwood@google.com>
    Commit-Queue: Kiet Tran <kiettran@google.com>

---
+++ src/connectivity/wlan/drivers/wlanif/fullmac_mlme.cc
@@ -0,0 +1,122 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "fullmac_mlme.h"
+
+#include <wlan/common/logging.h>
+
+#include "device.h"
+#include "fuchsia/wlan/common/c/banjo.h"
+#include "src/connectivity/wlan/lib/mlme/fullmac/c-binding/bindings.h"
+
+namespace wlanif {
+
+#define DEVICE(c) static_cast<Device *>(c)
+
+FullmacMlme::FullmacMlme(Device *device)
+    : device_(device), rust_mlme_(nullptr, delete_fullmac_mlme) {
+  debugfn();
+}
+
+void FullmacMlme::Init() {
+  debugfn();
+
+  auto rust_device = rust_fullmac_device_interface_t{
+      .device = static_cast<void *>(this->device_),
+      .start = [](void *device, const rust_wlan_fullmac_ifc_protocol_copy_t *ifc,
+                  zx_handle_t *out_sme_channel) -> zx_status_t {
+        zx::channel channel;
+        zx_status_t result = DEVICE(device)->Start(ifc, &channel);
+        *out_sme_channel = channel.release();
+        return result;
+      },
+      .query_device_info = [](void *device) -> wlan_fullmac_query_info_t {
+        wlan_fullmac_query_info_t out_resp;
+        DEVICE(device)->QueryDeviceInfo(&out_resp);
+        return out_resp;
+      },
+      .query_mac_sublayer_support = [](void *device) -> mac_sublayer_support_t {
+        mac_sublayer_support_t out_resp;
+        DEVICE(device)->QueryMacSublayerSupport(&out_resp);
+        return out_resp;
+      },
+      .query_security_support = [](void *device) -> security_support_t {
+        security_support_t out_resp;
+        DEVICE(device)->QuerySecuritySupport(&out_resp);
+        return out_resp;
+      },
+      .query_spectrum_management_support = [](void *device) -> spectrum_management_support_t {
+        spectrum_management_support_t out_resp;
+        DEVICE(device)->QuerySpectrumManagementSupport(&out_resp);
+        return out_resp;
+      },
+      .start_scan = [](void *device,
+                       wlan_fullmac_scan_req_t *req) { DEVICE(device)->StartScan(req); },
+      .connect_req = [](void *device,
+                        wlan_fullmac_connect_req_t *req) { DEVICE(device)->ConnectReq(req); },
+      .reconnect_req = [](void *device,
+                          wlan_fullmac_reconnect_req_t *req) { DEVICE(device)->ReconnectReq(req); },
+      .auth_resp = [](void *device,
+                      wlan_fullmac_auth_resp_t *resp) { DEVICE(device)->AuthenticateResp(resp); },
+      .deauth_req = [](void *device,
+                       wlan_fullmac_deauth_req_t *req) { DEVICE(device)->DeauthenticateReq(req); },
+      .assoc_resp = [](void *device,
+                       wlan_fullmac_assoc_resp_t *resp) { DEVICE(device)->AssociateResp(resp); },
+      .disassoc_req =
+          [](void *device, wlan_fullmac_disassoc_req_t *req) {
+            DEVICE(device)->DisassociateReq(req);
+          },
+      .reset_req = [](void *device,
+                      wlan_fullmac_reset_req_t *req) { DEVICE(device)->ResetReq(req); },
+      .start_req = [](void *device,
+                      wlan_fullmac_start_req_t *req) { DEVICE(device)->StartReq(req); },
+      .stop_req = [](void *device, wlan_fullmac_stop_req_t *req) { DEVICE(device)->StopReq(req); },
+      .set_keys_req = [](void *device,
+                         wlan_fullmac_set_keys_req_t *req) -> wlan_fullmac_set_keys_resp_t {
+        wlan_fullmac_set_keys_resp_t out_resp;
+        DEVICE(device)->SetKeysReq(req, &out_resp);
+        return out_resp;
+      },
+      .del_keys_req = [](void *device,
+                         wlan_fullmac_del_keys_req_t *req) { DEVICE(device)->DeleteKeysReq(req); },
+      .eapol_req = [](void *device,
+                      wlan_fullmac_eapol_req_t *req) { DEVICE(device)->EapolReq(req); },
+      .get_iface_counter_stats = [](void *device,
+                                    int32_t *out_status) -> wlan_fullmac_iface_counter_stats_t {
+        wlan_fullmac_iface_counter_stats_t out_stats;
+        *out_status = DEVICE(device)->GetIfaceCounterStats(&out_stats);
+        return out_stats;
+      },
+      .get_iface_histogram_stats = [](void *device,
+                                      int32_t *out_status) -> wlan_fullmac_iface_histogram_stats_t {
+        wlan_fullmac_iface_histogram_stats_t out_stats;
+        *out_status = DEVICE(device)->GetIfaceHistogramStats(&out_stats);
+        return out_stats;
+      },
+      .sae_handshake_resp =
+          [](void *device, wlan_fullmac_sae_handshake_resp_t *resp) {
+            DEVICE(device)->SaeHandshakeResp(resp);
+          },
+      .sae_frame_tx = [](void *device,
+                         wlan_fullmac_sae_frame_t *frame) { DEVICE(device)->SaeFrameTx(frame); },
+      .wmm_status_req = [](void *device) { DEVICE(device)->WmmStatusReq(); },
+      .on_link_state_changed = [](void *device,
+                                  bool online) { DEVICE(device)->OnLinkStateChanged(online); },
+
+  };
+  rust_mlme_ = RustFullmacMlme(start_fullmac_mlme(rust_device), delete_fullmac_mlme);
+}
+
+void FullmacMlme::StopMainLoop() { stop_fullmac_mlme(rust_mlme_.get()); }
+
+std::optional<zx_handle_t> FullmacMlme::DuplicateInspectVmo() {
+  zx_handle_t vmo;
+  auto status = duplicate_inspect_vmo(rust_mlme_.get(), &vmo);
+  if (status != ZX_OK) {
+    return {};
+  }
+  return vmo;
+}
+
+}  // namespace wlanif
---
+++ src/connectivity/wlan/lib/mlme/fullmac/c-binding/src/lib.rs
@@ -0,0 +1,58 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {
+    fuchsia_zircon as zx,
+    log::error,
+    wlan_fullmac_mlme::{device::FullmacDeviceInterface, FullmacMlme, FullmacMlmeHandle},
+};
+
+#[no_mangle]
+pub extern "C" fn start_fullmac_mlme(device: FullmacDeviceInterface) -> *mut FullmacMlmeHandle {
+    match FullmacMlme::start(device) {
+        Ok(mlme) => Box::into_raw(Box::new(mlme)),
+        Err(e) => {
+            error!("Failed to start FullMAC MLME: {}", e);
+            std::ptr::null_mut()
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn stop_fullmac_mlme(mlme: &mut FullmacMlmeHandle) {
+    mlme.stop();
+}
+
+/// FFI interface: Stop and delete a FullMAC MLME via the FullmacMlmeHandle. Takes ownership
+/// and invalidates the passed FullmacMlmeHandle.
+///
+/// # Safety
+///
+/// This fn accepts a raw pointer that is held by the FFI caller as a handle to
+/// the MLME. This API is fundamentally unsafe, and relies on the caller to
+/// pass the correct pointer and make no further calls on it later.
+#[no_mangle]
+pub unsafe extern "C" fn delete_fullmac_mlme(mlme: *mut FullmacMlmeHandle) {
+    if !mlme.is_null() {
+        let mlme = Box::from_raw(mlme);
+        mlme.delete();
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn duplicate_inspect_vmo(
+    mlme: &mut FullmacMlmeHandle,
+    inspect_vmo: &mut zx::Vmo,
+) -> zx::sys::zx_status_t {
+    match mlme.duplicate_inspect_vmo() {
+        Some(vmo) => {
+            *inspect_vmo = vmo;
+            zx::sys::ZX_OK
+        }
+        None => {
+            error!("Unable to retrieve Inspect VMO");
+            zx::sys::ZX_ERR_NOT_SUPPORTED
+        }
+    }
+}
