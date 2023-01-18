1696939ce89f2faaec3739a51f1efe28760b1f7e
Nick Van der Auwermeulen <nickvander@google.com>
2023-01-18 01:34:02 +0000

    [doc-checker] Support glossary.yaml (without _) to publish the file.
    Change-Id: I3aa1fe804bfb3df110584d39c35cc42555df20ad
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/790175
    Reviewed-by: Clayton Wilkinson <wilkinsonclay@google.com>
    Commit-Queue: Nick Van der Auwermeulen <nickvander@google.com>

--- tools/doc_checker/src/yaml.rs
+++ tools/doc_checker/src/yaml.rs
@@ -217,6 +217,7 @@ impl DocYamlCheck for YamlChecker {
                 Some("_drivers_epitaphs.yaml") => check_drivers_epitaphs(filename, yaml_value),
                 Some("_eng_council.yaml") => check_eng_council(filename, yaml_value),
                 Some("_glossary.yaml") => check_glossary(filename, yaml_value),
+                Some("glossary.yaml") => check_glossary(filename, yaml_value),
                 Some("_metadata.yaml") => check_metadata(filename, yaml_value),
                 Some("_problems.yaml") => check_problems(filename, yaml_value),
                 Some("_redirects.yaml") => check_redirects(filename, yaml_value),
