4b6a3a941fec11adeff6392ff751ddb34980a807
Jocelyn Dang <jocelyndang@google.com>
2023-01-19 02:22:53 +0000

    [driver_runtime] Add benchmark for Channel read C API.
    It seems to be inefficient for Channel reads to return a
    zx::status<ReadReturn>. Changing this API to be more C
    style like zx::channel led to improvements averaging
    at 100ns.
    
    Since it seems like callers like fidl_transport use the
    C API anyway, I don't think it's worth adding a new
    C style API to fdf::Channel for reading. I have added
    the extra benchmark so we can more accurately benchmark
    for what is being used.
    
    Mean    Std dev        Min        Max     Median Unit         Test case
    799        577        662      12961        756 nanoseconds   Channel/WriteRead/64bytes/0handles.read
    589        181        546       6225        579 nanoseconds   Channel/WriteReadC/64bytes/0handles.read
    
    Bug: 88531, 120119
    Change-Id: I2072377256435b468cc4f73d119940901784cdaa
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/791111
    Commit-Queue: Jocelyn Dang <jocelyndang@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>

--- src/devices/bin/driver_runtime/microbenchmarks/channels.cc
+++ src/devices/bin/driver_runtime/microbenchmarks/channels.cc
@@ -53,6 +53,52 @@ bool ChannelWriteReadTest(perftest::RepeatState* state, uint32_t message_size,
   return true;
 }
 
+// Same as |ChannelWriteReadTest|, but reading is done using the C API.
+bool ChannelWriteReadCTest(perftest::RepeatState* state, uint32_t message_size,
+                           uint32_t handle_count) {
+  state->DeclareStep("write");
+  state->DeclareStep("read");
+  state->SetBytesProcessedPerRun(message_size);
+
+  auto channel_pair = fdf::ChannelPair::Create(0);
+  ASSERT_OK(channel_pair.status_value());
+
+  constexpr uint32_t kTag = 'BNCH';
+  fdf::Arena arena(kTag);
+
+  void* data = arena.Allocate(message_size);
+  auto handles_buf = static_cast<zx_handle_t*>(arena.Allocate(handle_count * sizeof(zx_handle_t)));
+
+  cpp20::span<zx_handle_t> handles(handles_buf, handle_count);
+  for (auto& handle : handles) {
+    fdf_handle_t peer;
+    ASSERT_OK(fdf_channel_create(0, &handle, &peer));
+    // We only need one end of the channel to transfer.
+    fdf_handle_close(peer);
+  }
+
+  while (state->KeepRunning()) {
+    auto status = channel_pair->end0.Write(0, arena, data, message_size, std::move(handles));
+    ASSERT_OK(status.status_value());
+    state->NextStep();
+
+    fdf_arena_t* read_arena;
+    uint32_t num_bytes;
+    zx_handle_t* read_handles;
+    uint32_t num_handles;
+    auto read_status = fdf_channel_read(channel_pair->end1.get(), 0, &read_arena, &data, &num_bytes,
+                                        &read_handles, &num_handles);
+    ASSERT_OK(read_status);
+    fdf::Arena arena(read_arena);
+    handles = cpp20::span{read_handles, num_handles};
+  }
+
+  for (auto& handle : handles) {
+    fdf_handle_close(handle);
+  }
+  return true;
+}
+
 void RegisterTests() {
   static const unsigned kMessageSizesInBytes[] = {
       64,
@@ -69,6 +115,14 @@ void RegisterTests() {
       auto write_read_name =
           fbl::StringPrintf("Channel/WriteRead/%ubytes/%uhandles", message_size, handle_count);
       perftest::RegisterTest(write_read_name.c_str(), ChannelWriteReadTest, message_size,
+                             handle_count);
+    }
+  }
+  for (auto message_size : kMessageSizesInBytes) {
+    for (auto handle_count : kHandleCounts) {
+      auto write_read_name =
+          fbl::StringPrintf("Channel/WriteReadC/%ubytes/%uhandles", message_size, handle_count);
+      perftest::RegisterTest(write_read_name.c_str(), ChannelWriteReadCTest, message_size,
                              handle_count);
     }
   }
