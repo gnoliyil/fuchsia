ebfb36043be08c8bce9671982642fcfa42892b36
Adrian Danis <adanis@google.com>
2023-01-19 00:58:20 +0000

    [zircon] Guard likely/unlikely
    Wraps an #ifdef guard around the definitions of likely and unlikely for
    scenarios where the user also declares them.
    
    Bug: 30337
    Bug: 60238
    Change-Id: I9bc41359ef199be86b8a28d8b10841992cbc9fb9
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/790864
    Fuchsia-Auto-Submit: Adrian Danis <adanis@google.com>
    API-Review: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>

--- zircon/system/public/zircon/compiler.h
+++ zircon/system/public/zircon/compiler.h
@@ -255,8 +255,13 @@
 //     error(...);
 //   }
 //
+#ifndef likely
 #define likely(x) __builtin_expect(!!(x), 1)
+#endif
+
+#ifndef unlikely
 #define unlikely(x) __builtin_expect(!!(x), 0)
+#endif
 
 // Return the program counter of the calling function.
 #define __GET_CALLER(x) __builtin_return_address(0)
