99967085fa78d68e7b60c6797a3e257da52cc619
Kevan Hollbach <khollbach@google.com>
2023-01-19 03:25:56 +0000

    [a11y] Adapt OneFingerDragRecognizer to new API
    Adapt existing recognizer code to use the Event data type from the new
    TouchSource API. This way we can use it with the new GestureManagerV2 /
    GestureArenaV2 code.
    
    Fixed: 119566
    Test: a11y-lib-tests
    Change-Id: I80309c3ec8e59fc32b89b959bb032de982913d2c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/789515
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Kevan Hollbach <khollbach@google.com>
    Reviewed-by: Lucas Radaelli <lucasradaelli@google.com>

--- src/ui/a11y/lib/gesture_manager/recognizers_v2/one_finger_drag_recognizer.h
+++ src/ui/a11y/lib/gesture_manager/recognizers_v2/one_finger_drag_recognizer.h
@@ -5,12 +5,13 @@
 #ifndef SRC_UI_A11Y_LIB_GESTURE_MANAGER_RECOGNIZERS_V2_ONE_FINGER_DRAG_RECOGNIZER_H_
 #define SRC_UI_A11Y_LIB_GESTURE_MANAGER_RECOGNIZERS_V2_ONE_FINGER_DRAG_RECOGNIZER_H_
 
+#include <fuchsia/ui/pointer/augment/cpp/fidl.h>
 #include <lib/fit/function.h>
 #include <lib/zx/time.h>
 
-#include "src/ui/a11y/lib/gesture_manager/arena/contest_member.h"
-#include "src/ui/a11y/lib/gesture_manager/arena/recognizer.h"
-#include "src/ui/a11y/lib/gesture_manager/gesture_util/util.h"
+#include "src/ui/a11y/lib/gesture_manager/arena_v2/participation_token_interface.h"
+#include "src/ui/a11y/lib/gesture_manager/arena_v2/recognizer_v2.h"
+#include "src/ui/a11y/lib/gesture_manager/gesture_util_v2/util.h"
 #include "src/ui/a11y/lib/gesture_manager/recognizers_v2/timing_constants.h"
 
 namespace a11y::recognizers_v2 {
@@ -21,19 +22,19 @@ namespace a11y::recognizers_v2 {
 // Minimal effort is taken towards ignoring 2-finger gestures. For feature parity, while a second
 // finger is down, events will be suppressed. When it is released, the remaining pointer must be the
 // original. This requirement should probably be dropped in the future.
-class OneFingerDragRecognizer : public GestureRecognizer {
+class OneFingerDragRecognizer : public GestureRecognizerV2 {
  public:
   // Minimum distance (in NDC) that a drag gesture must cover in order to invoke another update.
   // Value 1.f / 16 is chosen based on one finger tap recognizer maximum displacement.
   static constexpr float kMinDragDistanceForUpdate = 1.f / 16.f;
 
   // Signature for various drag recognizer callback functions.
-  using DragGestureCallback = fit::function<void(GestureContext)>;
+  using DragGestureCallback = fit::function<void(gesture_util_v2::GestureContext)>;
 
   // on_drag_started: Callback invoked at most once when the recognizer has won the arena. Callback
   // only occurs if at least one pointer is on the screen.
   //
-  // on_drag_update: Callback invoked as new MOVE events are handled AFTER the drag gesture is
+  // on_drag_update: Callback invoked as new CHANGE events are handled AFTER the drag gesture is
   // recognized and has won the arena. Callbacks only occur while exactly one pointer is on the
   // screen.
   //
@@ -49,37 +50,38 @@ class OneFingerDragRecognizer : public GestureRecognizer {
                           zx::duration drag_gesture_delay = kMinDragDuration);
   ~OneFingerDragRecognizer() override;
 
-  void HandleEvent(const fuchsia::ui::input::accessibility::PointerEvent& pointer_event) override;
+  void HandleEvent(const fuchsia::ui::pointer::augment::TouchEventWithLocalHit& event) override;
   void OnWin() override;
   void OnDefeat() override;
-  void OnContestStarted(std::unique_ptr<ContestMember> contest_member) override;
+  void OnContestStarted(std::unique_ptr<ParticipationTokenInterface> token) override;
   std::string DebugName() const override;
 
  private:
-  // Represents state internal to a contest, i.e. contest member, accept delay, and pointer state.
+  // Represents state internal to a contest, i.e. participation token, accept delay, and pointer
+  // state.
   struct Contest;
 
   // Resets gesture_context_ and contest_.
   void ResetRecognizer();
 
-  // Helper function to handle move events (for readability to avoid nested switch statements).
-  void HandleMoveEvent(const fuchsia::ui::input::accessibility::PointerEvent& pointer_event);
+  // Helper function to handle CHANGE events (for readability to avoid nested switch statements).
+  void HandleChangeEvent(const fuchsia::ui::pointer::augment::TouchEventWithLocalHit& event);
 
-  // Returns true if distance between current pointer event and event that prompted previous call to
+  // Returns true if distance between current touch event and event that prompted previous call to
   // update callback exceeds kMinDragDistanceForUpdate.
   bool DragDistanceExceedsUpdateThreshold(
-      const fuchsia::ui::input::accessibility::PointerEvent& pointer_event) const;
+      const fuchsia::ui::pointer::augment::TouchEventWithLocalHit& event) const;
 
   // Callback invoked once the drag gesture has been recognized.
   DragGestureCallback on_drag_started_;
 
-  // Callback invoked as new MOVE events are handled AFTER the drag gesture is recognized.
+  // Callback invoked as new CHANGE events are handled AFTER the drag gesture is recognized.
   DragGestureCallback on_drag_update_;
 
   // Callback invoked when the drag gesture is completed (as finger is lifted from screen).
   DragGestureCallback on_drag_complete_;
 
-  GestureContext gesture_context_;
+  gesture_util_v2::GestureContext gesture_context_;
 
   // Once a drag is recognized and the recognizer claims the win, it should call update callback
   // whenever the pointer location changes by a distance exceeding kMinDragDistanceForUpdate. In
@@ -87,7 +89,7 @@ class OneFingerDragRecognizer : public GestureRecognizer {
   // update. This field stores the location of the previous update (if the recognizer is the winner)
   // OR the location of the previous pointer event ingested (if the recognizer is not yet the
   // winner).
-  PointerLocation previous_update_location_;
+  gesture_util_v2::PointerLocation previous_update_location_;
 
   // Minimum time a finger can be in contact with the screen to be considered a drag.
   zx::duration drag_gesture_delay_;
--- src/ui/a11y/lib/gesture_manager/recognizers_v2/tests/one_finger_drag_recognizer_test.cc
+++ src/ui/a11y/lib/gesture_manager/recognizers_v2/tests/one_finger_drag_recognizer_test.cc
@@ -4,72 +4,80 @@
 
 #include "src/ui/a11y/lib/gesture_manager/recognizers_v2/one_finger_drag_recognizer.h"
 
-#include <fuchsia/ui/input/accessibility/cpp/fidl.h>
+#include <fuchsia/ui/pointer/augment/cpp/fidl.h>
 
 #include <memory>
 
 #include <gtest/gtest.h>
 
 #include "src/lib/testing/loop_fixture/test_loop_fixture.h"
-#include "src/ui/a11y/lib/gesture_manager/arena/tests/mocks/mock_contest_member.h"
-#include "src/ui/a11y/lib/gesture_manager/gesture_util/util.h"
+#include "src/ui/a11y/lib/gesture_manager/gesture_util_v2/util.h"
+#include "src/ui/a11y/lib/gesture_manager/recognizers_v2/tests/mocks/mock_participation_token.h"
 #include "src/ui/a11y/lib/gesture_manager/recognizers_v2/timing_constants.h"
-#include "src/ui/a11y/lib/testing/input.h"
+#include "src/ui/a11y/lib/testing/input_v2.h"
 
 #include <glm/glm.hpp>
 
 namespace accessibility_test {
 namespace {
 
-using AccessibilityPointerEvent = fuchsia::ui::input::accessibility::PointerEvent;
-using Phase = fuchsia::ui::input::PointerEventPhase;
+using input_v2::AddEvent;
+using input_v2::ChangeEvents;
+using input_v2::DragEvents;
+using input_v2::RemoveEvent;
 
 class OneFingerDragRecognizerTest : public gtest::TestLoopFixture {
  public:
   OneFingerDragRecognizerTest()
       : recognizer_(
-            [this](a11y::GestureContext context) { gesture_start_callback_called_ = true; },
-            [this](a11y::GestureContext context) { gesture_updates_.push_back(context); },
-            [this](a11y::GestureContext context) { gesture_complete_callback_called_ = true; },
+            [this](a11y::gesture_util_v2::GestureContext context) {
+              gesture_start_callback_called_ = true;
+            },
+            [this](a11y::gesture_util_v2::GestureContext context) {
+              gesture_updates_.push_back(std::move(context));
+            },
+            [this](a11y::gesture_util_v2::GestureContext context) {
+              gesture_complete_callback_called_ = true;
+            },
             a11y::recognizers_v2::kMinDragDuration) {}
 
-  void SendPointerEvents(const std::vector<PointerParams>& events) {
+  void SendPointerEvents(const std::vector<input_v2::PointerParams>& events) {
     for (const auto& event : events) {
       SendPointerEvent(event);
     }
   }
 
-  void SendPointerEvent(const PointerParams& event) {
-    if (member_.is_held()) {
-      recognizer_.HandleEvent(ToPointerEvent(event, 0));
+  void SendPointerEvent(const input_v2::PointerParams& event) {
+    if (token_.is_held()) {
+      recognizer_.HandleEvent(ToTouchEvent(event, 0));
     }
   }
 
  protected:
-  MockContestMember member_;
+  MockParticipationTokenHandle token_;
   a11y::recognizers_v2::OneFingerDragRecognizer recognizer_;
-  std::vector<a11y::GestureContext> gesture_updates_;
+  std::vector<a11y::gesture_util_v2::GestureContext> gesture_updates_;
   bool gesture_start_callback_called_ = false;
   bool gesture_complete_callback_called_ = false;
 };
 
 // Tests successful drag detection case.
 TEST_F(OneFingerDragRecognizerTest, WonAfterGestureDetected) {
-  recognizer_.OnContestStarted(member_.TakeInterface());
+  recognizer_.OnContestStarted(token_.TakeToken());
 
   glm::vec2 first_update_ndc_position = {0, .7f};
-  auto first_update_local_coordinates = ToLocalCoordinates(first_update_ndc_position);
+  auto first_update_local_coordinates = input_v2::ToLocalCoordinates(first_update_ndc_position);
 
-  SendPointerEvents(DownEvents(1, {}) + MoveEvents(1, {}, first_update_ndc_position));
+  SendPointerEvents(AddEvent(1, {}) + ChangeEvents(1, {}, first_update_ndc_position));
 
-  EXPECT_EQ(member_.status(), a11y::ContestMember::Status::kUndecided);
+  EXPECT_EQ(token_.status(), MockParticipationTokenHandle::Status::kUndecided);
   EXPECT_TRUE(gesture_updates_.empty());
 
   // Wait for the drag delay to elapse, at which point the recognizer should claim the win and
   // invoke the update callback.
   RunLoopFor(a11y::recognizers_v2::kMinDragDuration);
 
-  ASSERT_EQ(member_.status(), a11y::ContestMember::Status::kAccepted);
+  EXPECT_EQ(token_.status(), MockParticipationTokenHandle::Status::kAccept);
   recognizer_.OnWin();
 
   EXPECT_TRUE(gesture_start_callback_called_);
@@ -83,19 +91,19 @@ TEST_F(OneFingerDragRecognizerTest, WonAfterGestureDetected) {
     EXPECT_EQ(location.y, first_update_local_coordinates.y);
   }
 
-  SendPointerEvents(MoveEvents(1, {0, .7f}, {0, .85f}) + UpEvents(1, {0, .85f}));
+  SendPointerEvents(ChangeEvents(1, {0, .7f}, {0, .85f}) + RemoveEvent(1, {0, .85f}));
 
-  EXPECT_FALSE(member_.is_held());
+  EXPECT_FALSE(token_.is_held());
   EXPECT_TRUE(gesture_complete_callback_called_);
 
-  // Since MoveEvents() generates 10 evenly-spaced pointer events between the starting point (0, .7)
-  // and ending point (0, .85), the recognizer will receive a series of MOVE events at (0, .715),
-  // (0, .73), ..., (0, .85). The first for which the distance covered since the initial update,
-  // which occurred at (0, .7), will be the event at (0, .775). We therefore expect an update to
-  // occur at this point. We would expect an additional update when the distance between the pointer
-  // and (0, .775) exceeds .0625, which will occur at (0, .85).
-  auto second_update_local_coordinates = ToLocalCoordinates({0, .775f});
-  auto third_update_local_coordinates = ToLocalCoordinates({0, .85f});
+  // Since ChangeEvents() generates 10 evenly-spaced pointer events between the starting point (0,
+  // .7) and ending point (0, .85), the recognizer will receive a series of CHANGE events at (0,
+  // .715), (0, .73), ..., (0, .85). The first for which the distance covered since the initial
+  // update, which occurred at (0, .7), will be the event at (0, .775). We therefore expect an
+  // update to occur at this point. We would expect an additional update when the distance between
+  // the pointer and (0, .775) exceeds .0625, which will occur at (0, .85).
+  auto second_update_local_coordinates = input_v2::ToLocalCoordinates({0, .775f});
+  auto third_update_local_coordinates = input_v2::ToLocalCoordinates({0, .85f});
 
   EXPECT_EQ(gesture_updates_.size(), 3u);
   {
@@ -114,50 +122,50 @@ TEST_F(OneFingerDragRecognizerTest, WonAfterGestureDetected) {
 // Verifies that recognizer rejects if multiple pointers are onscreen prior to
 // accept.
 TEST_F(OneFingerDragRecognizerTest, RejectMultifinger) {
-  recognizer_.OnContestStarted(member_.TakeInterface());
+  recognizer_.OnContestStarted(token_.TakeToken());
 
-  SendPointerEvents(DownEvents(1, {}) + MoveEvents(1, {}, {0, .7f}) + DownEvents(2, {}));
+  SendPointerEvents(AddEvent(1, {}) + ChangeEvents(1, {}, {0, .7f}) + AddEvent(2, {}));
 
-  ASSERT_EQ(member_.status(), a11y::ContestMember::Status::kRejected);
+  EXPECT_EQ(token_.status(), MockParticipationTokenHandle::Status::kReject);
 }
 
 // Verifies that recognizer ignores additional pointers after accepting.
 TEST_F(OneFingerDragRecognizerTest, SuppressMultitouchAfterAccept) {
-  recognizer_.OnContestStarted(member_.TakeInterface());
+  recognizer_.OnContestStarted(token_.TakeToken());
 
-  SendPointerEvents(DownEvents(1, {}) + MoveEvents(1, {}, {0, .7f}));
+  SendPointerEvents(AddEvent(1, {}) + ChangeEvents(1, {}, {0, .7f}));
 
   // Wait for the drag delay to elapse, at which point the recognizer should claim the win and
   // invoke the update callback.
   RunLoopFor(a11y::recognizers_v2::kMinDragDuration);
 
-  ASSERT_EQ(member_.status(), a11y::ContestMember::Status::kAccepted);
+  EXPECT_EQ(token_.status(), MockParticipationTokenHandle::Status::kAccept);
   recognizer_.OnWin();
 
   EXPECT_TRUE(gesture_start_callback_called_);
 
-  SendPointerEvents(DownEvents(2, {}) + MoveEvents(1, {0, .7f}, {0, .85f}));
+  SendPointerEvents(AddEvent(2, {}) + ChangeEvents(1, {0, .7f}, {0, .85f}));
 
   EXPECT_EQ(gesture_updates_.size(), 1u);
 }
 
 // Tests that distance threshold between updates is enforced after first update.
 TEST_F(OneFingerDragRecognizerTest, MinimumDistanceRequirementForUpdatesEnforced) {
-  recognizer_.OnContestStarted(member_.TakeInterface());
+  recognizer_.OnContestStarted(token_.TakeToken());
 
-  SendPointerEvents(DownEvents(1, {}) + MoveEvents(1, {}, {0, .7f}));
+  SendPointerEvents(AddEvent(1, {}) + ChangeEvents(1, {}, {0, .7f}));
 
   // Wait for the drag delay to elapse, at which point the recognizer should claim the win and
   // invoke the update callback.
   RunLoopFor(a11y::recognizers_v2::kMinDragDuration);
 
-  ASSERT_EQ(member_.status(), a11y::ContestMember::Status::kAccepted);
+  EXPECT_EQ(token_.status(), MockParticipationTokenHandle::Status::kAccept);
   recognizer_.OnWin();
 
   // Move pointer to location that does NOT meet the minimum threshold update.
-  SendPointerEvents(MoveEvents(1, {0, .7f}, {0, .75f}) + UpEvents(1, {0, .75f}));
+  SendPointerEvents(ChangeEvents(1, {0, .7f}, {0, .75f}) + RemoveEvent(1, {0, .75f}));
 
-  EXPECT_FALSE(member_.is_held());
+  EXPECT_FALSE(token_.is_held());
   EXPECT_TRUE(gesture_complete_callback_called_);
 
   // The update callback should only be invoked again if the pointer moves a sufficient distance
@@ -168,15 +176,15 @@ TEST_F(OneFingerDragRecognizerTest, MinimumDistanceRequirementForUpdatesEnforced
 
 // Verifies that recognizer does not accept gesture before delay period elapses.
 TEST_F(OneFingerDragRecognizerTest, DoNotAcceptPriorToDelayElapsing) {
-  recognizer_.OnContestStarted(member_.TakeInterface());
+  recognizer_.OnContestStarted(token_.TakeToken());
 
   SendPointerEvents(DragEvents(1, {}, {0, .7f}));
 
-  EXPECT_EQ(member_.status(), a11y::ContestMember::Status::kRejected);
+  EXPECT_EQ(token_.status(), MockParticipationTokenHandle::Status::kReject);
 
   // Wait for the drag delay to elapse to ensure that task scheduled to claim win was cancelled.
   // The task calls Accept(), and then invokes the drag update callback. Therefore, if it was
-  // cancelled successfully, we would not expect either method to have been called. The mock member_
+  // cancelled successfully, we would not expect either method to have been called. The mock token_
   // has an assertion that if it was rejected, it may not have Accept() called on it.
   RunLoopFor(a11y::recognizers_v2::kMinDragDuration);
 
@@ -186,15 +194,15 @@ TEST_F(OneFingerDragRecognizerTest, DoNotAcceptPriorToDelayElapsing) {
 
 // Tests that recognizer abandons gesture if it is defeated.
 TEST_F(OneFingerDragRecognizerTest, Defeat) {
-  recognizer_.OnContestStarted(member_.TakeInterface());
+  recognizer_.OnContestStarted(token_.TakeToken());
 
-  SendPointerEvents(DownEvents(1, {}) + MoveEvents(1, {}, {0, .7f}));
+  SendPointerEvents(AddEvent(1, {}) + ChangeEvents(1, {}, {0, .7f}));
 
   // Wait for the drag delay to elapse, at which point the recognizer should attempt to claim the
   // win.
   RunLoopFor(a11y::recognizers_v2::kMinDragDuration);
 
-  ASSERT_EQ(member_.status(), a11y::ContestMember::Status::kAccepted);
+  EXPECT_EQ(token_.status(), MockParticipationTokenHandle::Status::kAccept);
   // When it loses, the recognizer should NOT call the update task, and should instead abandon the
   // gesture.
   recognizer_.OnDefeat();
