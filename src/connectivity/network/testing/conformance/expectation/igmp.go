0d1ece417d8a1f2fdaa4c32ba8b80ca443d4ca35
Nick Brown <nickbrow@google.com>
2023-01-19 23:22:57 +0000

    [netconform] Update IGMP expectations
    Some tests are newly failing after enabling IGMPv3 because the test
    runner expects IGMPv2.
    
    Bug: 120246, 120281
    Change-Id: Icb5fdc76fbe77c1e0ab14ad83cb10b8197163553
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/791642
    Reviewed-by: Amanda Tait <atait@google.com>
    Commit-Queue: Nick Brown <nickbrow@google.com>

--- src/connectivity/network/testing/conformance/expectation/igmp.go
+++ src/connectivity/network/testing/conformance/expectation/igmp.go
@@ -9,22 +9,29 @@ import (
 )
 
 var igmpExpectations map[AnvlCaseNumber]outcome.Outcome = map[AnvlCaseNumber]outcome.Outcome{
-	{1, 1}:  Pass,
-	{2, 9}:  Pass,
+	// TODO(https://fxbug.dev/120281): Run the IGMPv3 suite instead.
+	{1, 1}: Fail,
+	// TODO(https://fxbug.dev/120281): Run the IGMPv3 suite instead.
+	{2, 9}:  Fail,
 	{2, 10}: Pass,
 	{2, 11}: Pass,
 	{3, 1}:  Pass,
 	{3, 4}:  Pass,
 	{3, 5}:  Pass,
-	{3, 6}:  Flaky, // TODO(https://fxbug.dev/104720): Investigate flake.
-	{3, 7}:  Pass,
-	{5, 1}:  Pass,
-	{5, 2}:  Pass,
-	{5, 3}:  Pass,
-	{5, 5}:  Pass,
-	{5, 7}:  Pass,
-	{5, 8}:  Fail,
-	{5, 9}:  Fail,
+	// TODO(https://fxbug.dev/104720): Investigate flake.
+	// TODO(https://fxbug.dev/120281): Run the IGMPv3 suite instead.
+	// Note that if https://fxbug.dev/104720 recurs in the v3 suite, it
+	// should be marked Flaky, otherwise marked Pass.
+	{3, 6}: Fail,
+	{3, 7}: Pass,
+	{5, 1}: Pass,
+	{5, 2}: Pass,
+	{5, 3}: Pass,
+	// TODO(https://fxbug.dev/120281): Run the IGMPv3 suite instead.
+	{5, 5}: Fail,
+	{5, 7}: Pass,
+	{5, 8}: Fail,
+	{5, 9}: Fail,
 	// TODO(https://fxbug.dev/118202): Fix.
 	{5, 11}: Fail,
 	{6, 2}:  Pass,
