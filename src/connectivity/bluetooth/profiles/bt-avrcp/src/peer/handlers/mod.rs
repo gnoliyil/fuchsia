2a7d771b3be6badfbdf723b58884a0537d75763e
Ani Ramakrishnan <aniramakri@google.com>
2023-01-19 20:26:43 +0000

    [bt][avrcp] Update stale bug reference
    Test: Docs only, fx build
    Change-Id: Id8e3b1c1a6eda346964abd5ef1e3378cf6a9ecef
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/791513
    Fuchsia-Auto-Submit: Ani Ramakrishnan <aniramakri@google.com>
    Reviewed-by: Zac Bowling <zbowling@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Marie Janssen <jamuraa@google.com>

--- src/connectivity/bluetooth/profiles/bt-avrcp/src/peer/handlers/mod.rs
+++ src/connectivity/bluetooth/profiles/bt-avrcp/src/peer/handlers/mod.rs
@@ -290,7 +290,7 @@ fn notification_response(
     // Unsupported events:
     // Probably never: TrackReachedEnd, TrackReachedStart,
     // TODO(fxbug.dev/47597): Battery/power notifications: BattStatusChanged, SystemStatusChanged,
-    // TODO(fxbug.dev/2744): Browse channel notifications: NowPlayingContentChanged,
+    // TODO(fxbug.dev/45904): Browse channel notifications: NowPlayingContentChanged,
     //                       AvailablePlayersChanged, and UidsChanged
 
     Ok(match notify_event_id {
