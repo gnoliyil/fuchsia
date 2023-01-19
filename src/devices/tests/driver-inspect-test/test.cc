197776753be58d0ef94c98fd751c28efa4f7379e
Tamir Duberstein <tamird@google.com>
2023-01-19 02:06:12 +0000

    [driver-inspect-test] Use typed channels
    Bug: 69585
    Change-Id: I3f49bf7e5cea3e6a77f744cf7a9c16f9ff1f1d6b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/791311
    Fuchsia-Auto-Submit: Tamir Duberstein <tamird@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Yifei Teng <yifeit@google.com>

--- src/devices/tests/driver-inspect-test/test.cc
+++ src/devices/tests/driver-inspect-test/test.cc
@@ -28,77 +28,71 @@ class InspectTestCase : public InspectTestHelper, public zxtest::Test {
   void SetUp() override {
     IsolatedDevmgr::Args args;
 
-    board_test::DeviceEntry dev = {};
-    dev.vid = PDEV_VID_TEST;
-    dev.pid = PDEV_PID_INSPECT_TEST;
-    dev.did = 0;
-    args.device_list.push_back(dev);
+    args.device_list.push_back({
+        .vid = PDEV_VID_TEST,
+        .pid = PDEV_PID_INSPECT_TEST,
+        .did = 0,
+    });
 
-    zx_status_t status = IsolatedDevmgr::Create(&args, &devmgr_);
-    ASSERT_OK(status);
+    ASSERT_OK(IsolatedDevmgr::Create(&args, &devmgr_));
 
     zx::result channel = device_watcher::RecursiveWaitForFile(devmgr_.devfs_root().get(),
                                                               "sys/platform/11:13:0/inspect-test");
     ASSERT_OK(channel.status_value());
-
-    chan_ = std::move(channel.value());
-    ASSERT_NE(chan_.get(), ZX_HANDLE_INVALID);
+    client_ = fidl::ClientEnd<TestInspect>{std::move(channel.value())};
   }
 
   const IsolatedDevmgr& devmgr() { return devmgr_; }
-  zx::unowned_channel channel() { return zx::unowned(chan_); }
+  const fidl::ClientEnd<TestInspect>& client() { return client_; }
 
  private:
   IsolatedDevmgr devmgr_;
-  zx::channel chan_;
+  fidl::ClientEnd<TestInspect> client_;
 };
 
 TEST_F(InspectTestCase, InspectDevfs) {
   // Check if inspect-test device is hosted in diagnostics folder
-  ASSERT_OK(device_watcher::RecursiveWaitForFile(devmgr().devfs_root().get(), "diagnostics/class")
-                .status_value());
+  ASSERT_OK(device_watcher::RecursiveWaitForFile(devmgr().devfs_root().get(), "diagnostics/class"));
   ASSERT_OK(device_watcher::RecursiveWaitForFile(devmgr().devfs_root().get(),
-                                                 "diagnostics/class/test/000.inspect")
-                .status_value());
+                                                 "diagnostics/class/test/000.inspect"));
 }
 
 TEST_F(InspectTestCase, ReadInspectData) {
   // Wait for inspect data to appear
   zx::result inspect_channel = device_watcher::RecursiveWaitForFile(
       devmgr().devfs_root().get(), "diagnostics/class/test/000.inspect");
-  ASSERT_OK(inspect_channel.status_value());
+  ASSERT_OK(inspect_channel);
   fbl::unique_fd fd;
-  zx_status_t status =
-      fdio_fd_create(inspect_channel.value().release(), fd.reset_and_get_address());
-  ASSERT_OK(status);
-  zx_handle_t out_vmo = ZX_HANDLE_INVALID;
-  ASSERT_OK(fdio_get_vmo_clone(fd.get(), &out_vmo));
-  ASSERT_NE(out_vmo, ZX_HANDLE_INVALID);
+  ASSERT_OK(fdio_fd_create(inspect_channel.value().release(), fd.reset_and_get_address()));
+  {
+    zx::vmo vmo;
+    ASSERT_OK(fdio_get_vmo_clone(fd.get(), vmo.reset_and_get_address()));
 
-  // Check initial inspect data
-  zx::vmo inspect_vmo(out_vmo);
-  ASSERT_NO_FATAL_FAILURE(ReadInspect(inspect_vmo));
-  // testBeforeDdkAdd: "OK"
-  ASSERT_NO_FATAL_FAILURE(
-      CheckProperty(hierarchy().node(), "testBeforeDdkAdd", inspect::StringPropertyValue("OK")));
+    // Check initial inspect data
+    ASSERT_NO_FATAL_FAILURE(ReadInspect(vmo));
+    // testBeforeDdkAdd: "OK"
+    ASSERT_NO_FATAL_FAILURE(
+        CheckProperty(hierarchy().node(), "testBeforeDdkAdd", inspect::StringPropertyValue("OK")));
+  }
 
   // Call test-driver to modify inspect data
-  auto result = fidl::WireCall<TestInspect>(channel())->ModifyInspect();
-  ASSERT_OK(result.status());
-  ASSERT_FALSE(result->is_error());
+  const fidl::WireResult result = fidl::WireCall(client())->ModifyInspect();
+  ASSERT_OK(result);
+  const fit::result response = result.value();
+  ASSERT_TRUE(response.is_ok(), "%s", zx_status_get_string(response.error_value()));
 
   // Verify new inspect data is reflected
-  out_vmo = ZX_HANDLE_INVALID;
-  ASSERT_OK(fdio_get_vmo_clone(fd.get(), &out_vmo));
-  ASSERT_NE(out_vmo, ZX_HANDLE_INVALID);
-  inspect_vmo = zx::vmo(out_vmo);
-  ASSERT_NO_FATAL_FAILURE(ReadInspect(inspect_vmo));
-  // Previous values
-  ASSERT_NO_FATAL_FAILURE(
-      CheckProperty(hierarchy().node(), "testBeforeDdkAdd", inspect::StringPropertyValue("OK")));
-  // New addition - testModify: "OK"
-  ASSERT_NO_FATAL_FAILURE(
-      CheckProperty(hierarchy().node(), "testModify", inspect::StringPropertyValue("OK")));
+  {
+    zx::vmo vmo;
+    ASSERT_OK(fdio_get_vmo_clone(fd.get(), vmo.reset_and_get_address()));
+    ASSERT_NO_FATAL_FAILURE(ReadInspect(vmo));
+    // Previous values
+    ASSERT_NO_FATAL_FAILURE(
+        CheckProperty(hierarchy().node(), "testBeforeDdkAdd", inspect::StringPropertyValue("OK")));
+    // New addition - testModify: "OK"
+    ASSERT_NO_FATAL_FAILURE(
+        CheckProperty(hierarchy().node(), "testModify", inspect::StringPropertyValue("OK")));
+  }
 }
 
 }  // namespace
