427f60102de74b379133b52dbce6fc1c9c390986
Jay Zhuang <jayzhuang@google.com>
2023-01-19 17:54:00 +0000

    [bazel][assembly] Product configuration verifier
    Replace the existing product config verifier with a more custom one.
    Also start to do this verification in GN so it's more flexible. For
    example, GN can easily access files from the Bazel workspace, including
    its output, and it's not the case in the opposite direction. This is a
    temporary tool to facilitate GN->Bazel assembly migration anyways, and
    can go away when GN assembly is replaced.
    
    Legacy product configuration verification rules in Bazel will soon be
    removed when usage in other repos are removed.
    
    fx set core.x64 \
      --with //build/bazel/assembly/product_configurations:verifications
    
    fx build \
      obj/build/bazel/assembly/product_configurations/verify_minimal_product_configuration/diff \
      obj/build/bazel/assembly/product_configurations/verify_core_product_configuration/diff \
      obj/build/bazel/assembly/product_configurations/verify_workstation_eng_product_configuration/diff
    
    Change-Id: I83233ed0a3d5d5bab3d0af93e227dde0d795090b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/788265
    Reviewed-by: Yaar Schnitman <yaar@google.com>
    Commit-Queue: Yaar Schnitman <yaar@google.com>

---
+++ build/bazel/assembly/product_configurations/verify_product_configuration.py
@@ -0,0 +1,134 @@
+# Copyright 2023 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import difflib
+import hashlib
+import json
+import os
+import sys
+
+
+def file_sha1(path):
+    sha1 = hashlib.sha1()
+    with open(path, "rb") as f:
+        sha1.update(f.read())
+    return sha1.hexdigest()
+
+
+def normalize_platform(config, root_dir):
+    if "platform" not in config:
+        return
+
+    platform = config["platform"]
+
+    # When unset, set config_data to empty list for consistency, to avoid noisy
+    # diff.
+    if "additional_serial_log_tags" not in platform:
+        platform["additional_serial_log_tags"] = []
+
+
+def normalize_product(config, root_dir):
+    if "product" not in config:
+        return
+
+    product = config["product"]
+
+    if "packages" in product:
+        packages = product["packages"]
+        for pkg_set in ["base", "cache"]:
+            if pkg_set not in packages:
+                continue
+
+            for pkg in packages[pkg_set]:
+                with open(os.path.join(root_dir, pkg["manifest"]), "r") as f:
+                    manifest = json.load(f)
+                    pkg["name"] = manifest["package"]["name"]
+                    pkg["version"] = manifest["package"]["version"]
+
+                # Skip comparison of manifest paths, because:
+                #
+                # 1. These paths are different
+                # 2. Contents of these manifests can be different, because
+                #    manifests contain paths to outputs from GN or Bazel
+                # 3. Detailed comparison of blobs and other information in
+                #    package manifests are possible, but non-trivial, so
+                #    deferred to final assembly output comparison for now.
+                pkg.pop("manifest", None)
+
+                if "config_data" not in pkg:
+                    # When unset, set config_data to empty list for consistency,
+                    # to avoid noisy diff.
+                    pkg["config_data"] = []
+                    continue
+
+                for config_data in pkg["config_data"]:
+                    # Config data source can have different paths, but they
+                    # should have consistent content, so replace them with a
+                    # file hash for comparison.
+                    config_data["source_sha1"] = file_sha1(
+                        os.path.join(root_dir, config_data["source"]))
+                    config_data["package_name"] = pkg["name"]
+                    config_data.pop("source", None)
+
+            packages[pkg_set].sort(key=lambda x: x["name"])
+
+    if "drivers" in product:
+        # TODO(jayzhuang): Normalize `drivers` field when we have product
+        # configs with this field set for comparison.
+        pass
+
+    return
+
+
+def normalize(config, root_dir):
+    normalize_platform(config, root_dir)
+    normalize_product(config, root_dir)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Compares assembly product configurations")
+    parser.add_argument(
+        "--product_config1", type=argparse.FileType("r"), required=True)
+    parser.add_argument(
+        "--root_dir1",
+        help="Directory where paths in --product_config1 are relative to",
+        required=True)
+    parser.add_argument(
+        "--product_config2", type=argparse.FileType("r"), required=True)
+    parser.add_argument(
+        "--root_dir2",
+        help="Directory where paths in --product_config2 are relative to",
+        required=True)
+    parser.add_argument("--output", type=argparse.FileType("w"), required=True)
+    args = parser.parse_args()
+
+    product_config_json1 = json.load(args.product_config1)
+    product_config_json2 = json.load(args.product_config2)
+
+    normalize(product_config_json1, args.root_dir1)
+    normalize(product_config_json2, args.root_dir2)
+
+    canon1 = json.dumps(
+        product_config_json1, sort_keys=True, indent=2).splitlines()
+    canon2 = json.dumps(
+        product_config_json2, sort_keys=True, indent=2).splitlines()
+
+    diff = difflib.unified_diff(
+        canon1,
+        canon2,
+        args.product_config1.name,
+        args.product_config2.name,
+        lineterm="")
+    diffstr = "\n".join(diff)
+    args.output.write(diffstr)
+
+    if (len(diffstr) != 0):
+        print(f"Error: non-empty diff product configs:\n{diffstr}")
+        return 1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
