4c3a08aac37861caffd547122e21f3a47819de4c
Josh Gargus <jjosh@google.com>
2023-01-20 00:24:49 +0000

    [tiles-session] Support Gfx.
    tiles-session implements fuchsia.element.GraphicalPresenter, which
    supports methods for presenting both Flatland and Gfx content.  On
    startup, tiles-session calls Scenic.UsesFlatland() to decide whether
    to accept Gfx or Flatland content (requests to present the other type
    of content are ignored).
    
    The Flatland implementation is almost unchanged, other than being
    factored into a trait impl so that Gfx can also be supported.  There
    is one exception:
    
    Both the Flatland and Gfx implementations use
    fuchsia.session.scene.Manager.PresentRootView() instead of
    Manager.SetRootView().  This simplifies things because tiles-session
    no longer needs to implement ViewProvider and wait for a request
    after calling SetRootView().
    
    Bug: 89425
    Fixed: 116933
    Change-Id: I74e794e677120b4826d71db1b5f31e1fdc9bd672
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/773337
    Reviewed-by: Adam Perry <adamperry@google.com>
    Reviewed-by: Joshua Seaton <joshuaseaton@google.com>
    Commit-Queue: Josh Gargus <jjosh@google.com>

--- src/session/examples/tiles-session/src/main.rs
+++ src/session/examples/tiles-session/src/main.rs
@@ -2,265 +2,28 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+mod base;
+mod flatland;
+mod gfx;
+
 use {
-    anyhow::{anyhow, Context as _, Error},
-    fidl::endpoints::RequestStream,
-    fidl::endpoints::{create_proxy, create_request_stream, ControlHandle, Proxy},
-    fidl_fuchsia_element as element,
-    fidl_fuchsia_session_scene::{
-        ManagerMarker as SceneManagerMarker, ManagerProxy as SceneManagerProxy,
+    crate::{
+        base::{MessageInternal, TilesSession},
+        flatland::FlatlandTilesSession,
+        gfx::GfxTilesSession,
     },
-    fidl_fuchsia_ui_app as ui_app, fidl_fuchsia_ui_composition as ui_comp,
-    fidl_fuchsia_ui_views::{self as ui_views},
-    fuchsia_async as fasync,
+    anyhow::Error,
+    fidl_fuchsia_element as element, fidl_fuchsia_ui_scenic as ui_scenic, fuchsia_async as fasync,
     fuchsia_component::{client::connect_to_protocol, server::ServiceFs, server::ServiceObj},
-    fuchsia_scenic::{self as scenic, flatland},
-    fuchsia_zircon as zx,
     futures::{channel::mpsc::UnboundedSender, StreamExt, TryStreamExt},
     tracing::{error, warn},
 };
 
-enum ExposedServices {
-    GraphicalPresenter(element::GraphicalPresenterRequestStream),
-}
-
-enum MessageInternal {
-    GraphicalPresenterPresentView {
-        view_spec: element::ViewSpec,
-        annotation_controller: Option<element::AnnotationControllerProxy>,
-        view_controller_request_stream: Option<element::ViewControllerRequestStream>,
-        responder: element::GraphicalPresenterPresentViewResponder,
-    },
-    DismissClient {
-        control_handle: fidl_fuchsia_element::ViewControllerControlHandle,
-    },
-    ClientDied {},
-    ReceivedClientViewRef {
-        view_ref: ui_views::ViewRef,
-    },
-}
-
-struct RootView {
-    parent_viewport_watcher: ui_comp::ParentViewportWatcherProxy,
-    view_focuser: fidl_fuchsia_ui_views::FocuserProxy,
-}
-
 // The maximum number of concurrent services to serve.
 const NUM_CONCURRENT_REQUESTS: usize = 5;
 
-#[fuchsia::main(logging = true)]
-async fn main() -> Result<(), Error> {
-    let result = inner_main().await;
-    if let Err(e) = result {
-        error!("Uncaught error in main(): {}", e);
-        return Err(e);
-    }
-    Ok(())
-}
-
-// TODO(fxbug.dev/89425): Ideally we wouldn't need to have separate inner_main() and main()
-// functions in order to catch and log top-level errors.  Instead, the #[fuchsia::main] macro
-// could catch and log the error.
-async fn inner_main() -> Result<(), Error> {
-    let (internal_sender, mut internal_receiver) =
-        futures::channel::mpsc::unbounded::<MessageInternal>();
-
-    // We start listening for service requests, but don't yet start serving those requests until we
-    // we receive confirmation that we are hooked up to the Scene Manager.
-    let fs = expose_services()?;
-
-    let scene_manager = connect_to_protocol::<SceneManagerMarker>()
-        .expect("failed to connect to fuchsia.scene.Manager");
-    let flatland = connect_to_protocol::<flatland::FlatlandMarker>()
-        .expect("failed to connect to fuchsia.ui.composition.Flatland");
-    let mut id_generator = flatland::IdGenerator::new();
-
-    let root_transform_id = id_generator.next_transform_id();
-    let root_view =
-        set_scene_manager_root_view(&scene_manager, &flatland, root_transform_id.clone()).await?;
-
-    let layout_info = root_view.parent_viewport_watcher.get_layout().await?;
-
-    // TODO(fxbug.dev/88656): do something like this to instantiate the library component that knows
-    // how to generate a Flatland scene to lay views out on a tiled grid.  It will be used in the
-    // event loop below.
-    // let tiles_helper = tile_helper::TilesHelper::new();
-
-    run_services(fs, internal_sender.clone());
-
-    // TODO(fxbug.dev/88656): if we encapsulate all of the state in e.g. "struct TilesSession {...}"
-    // then we could make this more testable by having e.g.
-    //   let session = TilesSession::new(...);
-    //   ...
-    //   while let Some(message) = internal_receiver.next().await {
-    //       session.handle_message(message);
-    //   }
-    //
-    // (or, if we think it's cleaner, still do the matching in the loop, and in each match arm,
-    // unwrap the args and use them to call a method on the session, e.g.
-    //    responder.send(session.present_view(view_spec, ...));
-    //
-    // This is more testable because we can set up the session and either create MessageInternals
-    // for it to handle, or call methods on it, depending on which option above is chosen.
-    while let Some(message) = internal_receiver.next().await {
-        match message {
-            // The ElementManager has asked us (via GraphicalPresenter::PresentView()) to display
-            // the view provided by a newly-launched element.
-            MessageInternal::GraphicalPresenterPresentView {
-                view_spec,
-                annotation_controller,
-                view_controller_request_stream,
-                responder,
-            } => {
-                // We have either a view holder token OR a viewport_creation_token, but for
-                // Flatland we can expect a viewport creation token.
-                let mut viewport_creation_token = match view_spec.viewport_creation_token {
-                    Some(token) => token,
-                    None => {
-                        warn!("Client attempted to present Gfx component but only Flatland is supported.");
-                        continue;
-                    }
-                };
-
-                // Create a Viewport that houses the view we are creating.
-                let viewport_content_id = id_generator.next_content_id();
-                let viewport_properties = fuchsia_scenic::flatland::ViewportProperties {
-                    logical_size: Some(layout_info.logical_size.unwrap()),
-                    ..ui_comp::ViewportProperties::EMPTY
-                };
-
-                // Attach the client to the scene graph.
-                let (child_view_watcher, child_view_watcher_request) =
-                    create_proxy::<fidl_fuchsia_ui_composition::ChildViewWatcherMarker>()?;
-                flatland.create_viewport(
-                    &mut viewport_content_id.clone(),
-                    &mut viewport_creation_token,
-                    viewport_properties,
-                    child_view_watcher_request,
-                )?;
-
-                let new_view_transform_id = id_generator.next_transform_id();
-                flatland.create_transform(&mut new_view_transform_id.clone())?;
-                flatland.set_content(
-                    &mut new_view_transform_id.clone(),
-                    &mut viewport_content_id.clone(),
-                )?;
-                flatland.add_child(
-                    &mut root_transform_id.clone(),
-                    &mut new_view_transform_id.clone(),
-                )?;
-
-                // Flush the changes.
-                flatland.present(flatland::PresentArgs {
-                    requested_presentation_time: Some(0),
-                    ..flatland::PresentArgs::EMPTY
-                })?;
-
-                // Ignore for now.
-                let _ = annotation_controller;
-
-                // Alert the client that the view has been presented.
-                let view_controller_request_stream = view_controller_request_stream.unwrap();
-                view_controller_request_stream.control_handle().send_on_presented()?;
-
-                run_client_view_controller_request_stream(
-                    view_controller_request_stream,
-                    internal_sender.clone(),
-                );
-                watch_child_view(child_view_watcher, internal_sender.clone());
-
-                if let Err(e) = responder.send(&mut Ok(())) {
-                    warn!("Failed to send response for GraphicalPresenter.PresentView(): {}", e);
-                }
-            }
-            MessageInternal::DismissClient { control_handle } => {
-                warn!("Gotta destroy client resources!");
-                control_handle.shutdown_with_epitaph(zx::Status::OK);
-            }
-            MessageInternal::ClientDied {} => {
-                warn!("Gotta destroy client resources!");
-            }
-            MessageInternal::ReceivedClientViewRef { mut view_ref } => {
-                match root_view.view_focuser.request_focus(&mut view_ref).await {
-                    Ok(_) => {}
-                    Err(e) => {
-                        error!("RequestFocus FIDL error: {}", e);
-                    }
-                }
-            }
-        }
-    }
-
-    Ok(())
-}
-
-async fn set_scene_manager_root_view(
-    scene_manager: &SceneManagerProxy,
-    flatland: &flatland::FlatlandProxy,
-    root_transform_id: flatland::TransformId,
-) -> Result<RootView, Error> {
-    let (view_provider, mut view_provider_request_stream) =
-        create_request_stream::<ui_app::ViewProviderMarker>()?;
-
-    // Don't await the result yet, because the future will not resolve until we handle the
-    // ViewProvider request below.
-    let view_ref = scene_manager.set_root_view(view_provider);
-    let (view_focuser, view_focuser_request) =
-        fidl::endpoints::create_proxy::<ui_views::FocuserMarker>()
-            .expect("Failed to create Focuser channel");
-
-    if let Some(request) = view_provider_request_stream
-        .try_next()
-        .await
-        .context("Failed to obtain next ViewProvider request from stream")?
-    {
-        match request {
-            ui_app::ViewProviderRequest::CreateView { .. } => {
-                Err(anyhow!("ViewProvider impl only handles CreateView2()"))
-            }
-            ui_app::ViewProviderRequest::CreateViewWithViewRef { .. } => {
-                Err(anyhow!("ViewProvider impl only handles CreateView2()"))
-            }
-            ui_app::ViewProviderRequest::CreateView2 { args, .. } => {
-                let (parent_viewport_watcher, parent_viewport_watcher_request) =
-                    create_proxy::<flatland::ParentViewportWatcherMarker>()?;
-
-                if let Some(mut view_creation_token) = args.view_creation_token {
-                    flatland.create_transform(&mut root_transform_id.clone())?;
-                    flatland.set_root_transform(&mut root_transform_id.clone())?;
-
-                    let mut view_identity =
-                        ui_views::ViewIdentityOnCreation::from(scenic::ViewRefPair::new()?);
-
-                    let view_bound_protocols = flatland::ViewBoundProtocols {
-                        view_focuser: Some(view_focuser_request),
-                        ..flatland::ViewBoundProtocols::EMPTY
-                    };
-
-                    flatland.create_view2(
-                        &mut view_creation_token,
-                        &mut view_identity,
-                        view_bound_protocols,
-                        parent_viewport_watcher_request,
-                    )?;
-
-                    flatland.present(flatland::PresentArgs {
-                        requested_presentation_time: Some(0),
-                        ..flatland::PresentArgs::EMPTY
-                    })?;
-                } else {
-                    return Err(anyhow!("CreateView2() missing view_creation_token field"));
-                }
-
-                // Now that we've handled the ViewProvider request, we can await the ViewRef.
-                let _view_ref = view_ref.await?;
-
-                Ok(RootView { parent_viewport_watcher, view_focuser })
-            }
-        }
-    } else {
-        Err(anyhow!("ViewProvider request stream closed before CreateView2() was called"))
-    }
+enum ExposedServices {
+    GraphicalPresenter(element::GraphicalPresenterRequestStream),
 }
 
 fn expose_services() -> Result<ServiceFs<ServiceObj<'static, ExposedServices>>, Error> {
@@ -351,50 +114,37 @@ fn run_graphical_presenter_service(
     .detach();
 }
 
-fn run_client_view_controller_request_stream(
-    mut request_stream: fidl_fuchsia_element::ViewControllerRequestStream,
-    internal_sender: UnboundedSender<MessageInternal>,
-) {
-    fasync::Task::local(async move {
-        if let Some(Ok(fidl_fuchsia_element::ViewControllerRequest::Dismiss { control_handle })) =
-            request_stream.next().await
-        {
-            {
-                warn!("We should dismiss ourselves!");
-                internal_sender
-                    .unbounded_send(MessageInternal::DismissClient { control_handle })
-                    .expect("Failed to send MessageInternal::DismissClient");
-            }
+#[fuchsia::main(logging = true)]
+async fn main() -> Result<(), Error> {
+    let (internal_sender, mut internal_receiver) =
+        futures::channel::mpsc::unbounded::<MessageInternal>();
+
+    // We start listening for service requests, but don't yet start serving those requests until we
+    // we receive confirmation that we are hooked up to the Scene Manager.
+    let fs = expose_services()?;
+
+    // Determine whether to use GFX or Flatland and instantiate the appropriate session, which will
+    // connect to the scene owner and attach our tiles view to it.
+    let scenic = connect_to_protocol::<ui_scenic::ScenicMarker>()
+        .expect("failed to connect to fuchsia.ui.scenic.Scenic");
+    let use_flatland =
+        scenic.uses_flatland().await.expect("Failed to get flatland info from Scenic.");
+    let mut tiles_session: Box<dyn TilesSession> = if use_flatland {
+        Box::new(FlatlandTilesSession::new(internal_sender.clone()).await?)
+    } else {
+        Box::new(GfxTilesSession::new(&scenic, internal_sender.clone()).await?)
+    };
+
+    // Serve the FIDL services on the message loop, proxying them into internal messages.
+    run_services(fs, internal_sender.clone());
+
+    // Process internal messages using the tiles session, then cleanup when done.
+    while let Some(message) = internal_receiver.next().await {
+        if let Err(e) = tiles_session.handle_message(message).await {
+            error!("Error handling message: {e}");
+            break;
         }
-    })
-    .detach();
-}
+    }
 
-fn watch_child_view(
-    proxy: ui_comp::ChildViewWatcherProxy,
-    internal_sender: UnboundedSender<MessageInternal>,
-) {
-    // Get view ref, then listen for channel closure.
-    fasync::Task::local(async move {
-        match proxy.get_view_ref().await {
-            Ok(view_ref) => {
-                internal_sender
-                    .unbounded_send(MessageInternal::ReceivedClientViewRef { view_ref })
-                    .expect("Failed to send MessageInternal::ReceivedClientViewRef");
-            }
-            Err(_) => {
-                internal_sender
-                    .unbounded_send(MessageInternal::ClientDied {})
-                    .expect("Failed to send MessageInternal::ClientDied");
-                return;
-            }
-        }
-
-        let _ = proxy.on_closed().await;
-
-        internal_sender
-            .unbounded_send(MessageInternal::ClientDied {})
-            .expect("Failed to send MessageInternal::ClientDied");
-    })
-    .detach();
+    Ok(())
 }
