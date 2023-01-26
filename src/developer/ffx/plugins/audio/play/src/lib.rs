b372e2e25f5686657820e11b0523fbf214503de3
camlloyd <camlloyd@google.com>
2023-01-26 21:51:27 +0000

    [ffx][audio] Add device play
    Change-Id: Idb00a61c02863ce5b092d98cb587f9ec9c587af0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/783346
    Reviewed-by: Dale Sather <dalesat@google.com>
    Commit-Queue: Cam Lloyd <camlloyd@google.com>
    Reviewed-by: Tom Bergan <tombergan@google.com>
    API-Review: Dale Sather <dalesat@google.com>

--- src/developer/ffx/plugins/audio/play/src/lib.rs
+++ src/developer/ffx/plugins/audio/play/src/lib.rs
@@ -6,8 +6,9 @@ use {
     anyhow::Result,
     blocking::Unblock,
     errors::ffx_bail,
-    ffx_audio_play_args::{PlayCommand, RenderCommand, SubCommand},
+    ffx_audio_play_args::{PlayCommand, SubCommand},
     ffx_core::ffx_plugin,
+    fidl::Socket,
     fidl_fuchsia_audio_ffxdaemon::{AudioDaemonPlayRequest, AudioDaemonProxy},
 };
 
@@ -16,36 +17,58 @@ use {
     AudioDaemonProxy = "core/audio_ffx_daemon:expose:fuchsia.audio.ffxdaemon.AudioDaemon"
 )]
 pub async fn play_cmd(audio_proxy: AudioDaemonProxy, cmd: PlayCommand) -> Result<()> {
+    let (play_remote, play_local) = fidl::Socket::create(fidl::SocketOpts::DATAGRAM)?;
     match cmd.subcommand {
         SubCommand::Render(renderer_command) => {
-            renderer_play(audio_proxy, renderer_command).await?
-        }
-    }
-    Ok(())
-}
-
-pub async fn renderer_play(audio_proxy: AudioDaemonProxy, cmd: RenderCommand) -> Result<()> {
-    let (play_remote, play_local) = fidl::Socket::create(fidl::SocketOpts::DATAGRAM)?;
-
-    let futs = futures::future::try_join(
-        async {
             let request = AudioDaemonPlayRequest {
                 socket: Some(play_remote),
                 location: Some(fidl_fuchsia_audio_ffxdaemon::PlayLocation::Renderer(
                     fidl_fuchsia_audio_ffxdaemon::RendererInfo {
-                        usage: Some(cmd.usage),
+                        usage: Some(renderer_command.usage),
                         ..fidl_fuchsia_audio_ffxdaemon::RendererInfo::EMPTY
                     },
                 )),
 
                 gain_settings: Some(fidl_fuchsia_audio_ffxdaemon::GainSettings {
-                    mute: Some(cmd.mute),
-                    gain: Some(cmd.gain),
+                    mute: Some(renderer_command.mute),
+                    gain: Some(renderer_command.gain),
                     ..fidl_fuchsia_audio_ffxdaemon::GainSettings::EMPTY
                 }),
                 ..AudioDaemonPlayRequest::EMPTY
             };
+            play(request, audio_proxy, play_local).await?
+        }
+        SubCommand::Device(device_command) => {
+            let request = AudioDaemonPlayRequest {
+                socket: Some(play_remote),
+                location: Some(fidl_fuchsia_audio_ffxdaemon::PlayLocation::RingBuffer(
+                    fidl_fuchsia_audio_ffxdaemon::DeviceSelector {
+                        is_input: Some(true),
+                        id: Some(device_command.id),
+                        ..fidl_fuchsia_audio_ffxdaemon::DeviceSelector::EMPTY
+                    },
+                )),
 
+                gain_settings: Some(fidl_fuchsia_audio_ffxdaemon::GainSettings {
+                    mute: None, // TODO
+                    gain: None, // TODO
+                    ..fidl_fuchsia_audio_ffxdaemon::GainSettings::EMPTY
+                }),
+                ..AudioDaemonPlayRequest::EMPTY
+            };
+            play(request, audio_proxy, play_local).await?;
+        }
+    }
+    Ok(())
+}
+
+pub async fn play(
+    request: AudioDaemonPlayRequest,
+    audio_proxy: AudioDaemonProxy,
+    play_local: Socket,
+) -> Result<()> {
+    let futs = futures::future::try_join(
+        async {
             let (stdout_sock, stderr_sock) = match audio_proxy.play(request).await? {
                 Ok(value) => (
                     value.stdout.ok_or(anyhow::anyhow!("No stdout socket"))?,
