2aca1567f91ca176ed69becb57f23e46d68165f2
Xyan Bhatnagar <xbhatnag@google.com>
2023-01-27 23:05:31 +0000

    [component_debug] Move graph functionality to library
    Graph functionality is now handled by `component_debug`
    
    Bug: 108157
    Change-Id: If7c3beedfab16ebe1ddb014b127ef6492ac4b76f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/794182
    Fuchsia-Auto-Submit: Xyan Bhatnagar <xbhatnag@google.com>
    Reviewed-by: Caslyn Tonelli <caslyn@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/developer/ffx/plugins/component/graph/src/args.rs
+++ src/developer/ffx/plugins/component/graph/src/args.rs
@@ -2,30 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use {argh::FromArgs, component_debug::list::ListFilter, ffx_core::ffx_command, std::str::FromStr};
-
-/// Determines the visual orientation of the graph's nodes.
-#[derive(Debug, PartialEq)]
-pub enum GraphOrientation {
-    /// The graph's nodes should be ordered from top to bottom.
-    TopToBottom,
-    /// The graph's nodes should be ordered from left to right.
-    LeftToRight,
-    /// The graph's nodes should be ordered in GraphViz's default orientation.
-    Default,
-}
-
-impl FromStr for GraphOrientation {
-    type Err = &'static str;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.to_lowercase().replace("_", "").replace("-", "").as_str() {
-            "tb" | "toptobottom" => Ok(GraphOrientation::TopToBottom),
-            "lr" | "lefttoright" => Ok(GraphOrientation::LeftToRight),
-            _ => Err("graph orientation should be 'top_to_bottom' or 'left_to_right'."),
-        }
-    }
-}
+use {
+    argh::FromArgs,
+    component_debug::{graph::GraphOrientation, list::ListFilter},
+    ffx_core::ffx_command,
+};
 
 #[ffx_command()]
 #[derive(FromArgs, Debug, PartialEq)]
@@ -64,19 +45,19 @@ impl FromStr for GraphOrientation {
     To graph both the ancestors and descendants of a component named `foo`:
 
     $ ffx component graph --only relatives:foo
-    
+
     To order the graph's nodes from left-to-right (instead of top-to-bottom):
-    
+
     $ ffx component graph --orientation left_to_right"
 )]
 
 pub struct ComponentGraphCommand {
     #[argh(option, long = "only", short = 'o')]
-    /// outputs only cmx/cml/running/stopped components depending on the flag.
-    pub only: Option<ListFilter>,
+    /// filter the instance list by a criteria: cmx, cml, running, stopped
+    pub filter: Option<ListFilter>,
 
-    #[argh(option, long = "orientation", short = 'r')]
+    #[argh(option, long = "orientation", short = 'r', default = "GraphOrientation::TopToBottom")]
     /// changes the visual orientation of the graph's nodes.
-    /// Allowed values are "left_to_right"/"lr" and "top_to_bottom"/"tb".
-    pub orientation: Option<GraphOrientation>,
+    /// Allowed values are "lefttoright"/"lr" and "toptobottom"/"tb".
+    pub orientation: GraphOrientation,
 }
