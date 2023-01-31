020bd6c6a2cf98eead525c7becd3cb260b9cc017
Jocelyn Dang <jocelyndang@google.com>
2023-01-31 02:19:48 +0000

    [driver_runtime] Add benchmark for inline vs non-inlined channel 1-way xfrs.
    Mean     Min      Max       Median   Test case
    18128268 12587324 48088650  17033356 AllowSyncCalls_1024_64bytes
    18555594 11156768 201642715 16834208 AllowSyncCalls_1024_65536bytes
    56848    9755     5986103   12653    AllowSyncCalls_1_64bytes
    49414    10076    1480616   12804    AllowSyncCalls_1_65536bytes
    
    3863540  3389247  16824574  3839681  Inline_Synchronized_1024_64bytes
    3860270  3462979  4629085   3849138  Inline_Synchronized_1024_65536bytes
    4483     3357     318521    3736     Inline_Synchronized_1_64bytes
    5004     3515     687344    3697     Inline_Synchronized_1_65536bytes
    
    3801086  3361507  6078075   3789955  Inline_Unsynchronized_1024_64bytes
    3887595  3488693  4464439   3888651  Inline_Unsynchronized_1024_65536bytes
    3839     3346     152349    3514     Inline_Unsynchronized_1_64bytes
    4175     3518     125963    3720     Inline_Unsynchronized_1_65536bytes
    
    2408360  2067638  8597251   2371808  Zircon_1024_64bytes
    12163053 10848509 38908584  11793803 Zircon_1024_65536bytes
    43592    4875     7577897   8663     Zircon_1_64bytes
    68942    11613    1941925   27658    Zircon_1_65536bytes
    
    Bug: 88531
    
    Change-Id: I6281611ef12415ae321f2282ef2c65c1674d0637
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/794242
    Commit-Queue: Jocelyn Dang <jocelyndang@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>

---
+++ src/devices/bin/driver_runtime/microbenchmarks/one_way.cc
@@ -0,0 +1,265 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/async-loop/cpp/loop.h>
+#include <lib/async-loop/default.h>
+#include <lib/async/cpp/task.h>
+#include <lib/async/cpp/wait.h>
+#include <lib/fdf/cpp/channel.h>
+#include <lib/fdf/cpp/channel_read.h>
+#include <lib/fdf/cpp/dispatcher.h>
+#include <lib/fdf/cpp/env.h>
+#include <lib/fdf/env.h>
+#include <lib/fdf/testing.h>
+#include <lib/fit/function.h>
+#include <lib/sync/completion.h>
+#include <lib/sync/cpp/completion.h>
+#include <lib/syslog/cpp/macros.h>
+#include <lib/zx/channel.h>
+
+#include <map>
+#include <vector>
+
+#include <fbl/string_printf.h>
+
+#include "src/devices/bin/driver_runtime/microbenchmarks/assert.h"
+#include "src/devices/bin/driver_runtime/microbenchmarks/test_runner.h"
+
+namespace {
+
+// Test one way transfers using fdf channels. The client writes |msg_count| messages,
+// and the benchmark waits until the server has received all of them.
+class OneWayTest {
+ public:
+  OneWayTest(uint32_t dispatcher_options, uint32_t msg_count, uint32_t msg_size)
+      : msg_count_(msg_count), msg_size_(msg_size) {
+    auto channel_pair = fdf::ChannelPair::Create(0);
+    ASSERT_OK(channel_pair.status_value());
+
+    client_ = std::move(channel_pair->end0);
+    server_ = std::move(channel_pair->end1);
+
+    {
+      auto dispatcher = CreateDispatcher(&client_fake_driver_, dispatcher_options, "client");
+      ASSERT_OK(dispatcher.status_value());
+      client_dispatcher_ = *std::move(dispatcher);
+    }
+
+    {
+      auto dispatcher = CreateDispatcher(&server_fake_driver_, dispatcher_options, "server");
+      ASSERT_OK(dispatcher.status_value());
+      server_dispatcher_ = *std::move(dispatcher);
+    }
+
+    constexpr uint32_t kTag = 'BNCH';
+    arena_ = fdf::Arena(kTag);
+
+    // Create the messages to transfer.
+    for (uint32_t i = 0; i < msg_count_; i++) {
+      msgs_.push_back(arena_.Allocate(msg_size_));
+    }
+    // TODO(https://fxbug.dev/119188) migrate off this once setting a default dispatcher
+    // with managed threads is possible.
+    fdf_testing_push_driver(reinterpret_cast<const void*>(client_fake_driver_));
+  }
+
+  void Run() {
+    uint32_t num_msgs_read = 0;
+    libsync::Completion completion;
+    auto channel_read = std::make_unique<fdf::ChannelRead>(
+        server_.get(), 0 /* options */,
+        [&](fdf_dispatcher_t* dispatcher, fdf::ChannelRead* channel_read,
+            zx_status_t status) mutable {
+          ASSERT_OK(status);
+          FX_CHECK(channel_read->channel() == server_.get());
+
+          fdf_arena_t* read_arena;
+          void* data;
+          uint32_t num_bytes;
+          zx_handle_t* read_handles;
+          uint32_t num_handles;
+          auto read_status = fdf_channel_read(channel_read->channel(), 0, &read_arena, &data,
+                                              &num_bytes, &read_handles, &num_handles);
+          ASSERT_OK(read_status);
+          fdf::Arena arena(read_arena);
+
+          num_msgs_read++;
+          // If we haven't read enough messages yet, register the read again.
+          if (num_msgs_read < msg_count_) {
+            ASSERT_OK(channel_read->Begin(dispatcher));
+          } else {
+            // Test run is complete.
+            completion.Signal();
+          }
+        });
+
+    zx_status_t status = channel_read->Begin(server_dispatcher_.get());
+    ASSERT_OK(status);
+
+    // Send the messages from client to server.
+    async_dispatcher_t* async_dispatcher = client_dispatcher_.async_dispatcher();
+    FX_CHECK(async_dispatcher != nullptr);
+
+    for (const auto& msg : msgs_) {
+      ASSERT_OK(
+          client_.Write(0, arena_, msg, msg_size_, cpp20::span<zx_handle_t>()).status_value());
+    }
+    ASSERT_OK(completion.Wait());
+    FX_CHECK(num_msgs_read == msg_count_);
+  }
+
+  void TearDown() {
+    client_dispatcher_.ShutdownAsync();
+    server_dispatcher_.ShutdownAsync();
+    ASSERT_OK(client_dispatcher_shutdown_.Wait());
+    ASSERT_OK(server_dispatcher_shutdown_.Wait());
+
+    fdf_testing_pop_driver();
+  }
+
+  void ShutdownHandler(fdf_dispatcher_t* dispatcher) {
+    FX_CHECK((dispatcher == client_dispatcher_.get()) || (dispatcher == server_dispatcher_.get()));
+    if (dispatcher == client_dispatcher_.get()) {
+      client_dispatcher_shutdown_.Signal();
+    } else {
+      server_dispatcher_shutdown_.Signal();
+    }
+  }
+
+ private:
+  zx::result<fdf::Dispatcher> CreateDispatcher(void* owner, uint32_t options,
+                                               cpp17::string_view name) {
+    if ((options & FDF_DISPATCHER_OPTION_SYNCHRONIZATION_MASK) ==
+        FDF_DISPATCHER_OPTION_SYNCHRONIZED) {
+      return fdf_env::DispatcherBuilder::CreateSynchronizedWithOwner(
+          owner, fdf::SynchronizedDispatcher::Options{.value = options}, name,
+          fit::bind_member(this, &OneWayTest::ShutdownHandler));
+    } else {
+      return fdf_env::DispatcherBuilder::CreateUnsynchronizedWithOwner(
+          owner, fdf::UnsynchronizedDispatcher::Options{.value = options}, name,
+          fit::bind_member(this, &OneWayTest::ShutdownHandler));
+    }
+  }
+
+  uint32_t msg_count_;
+  uint32_t msg_size_;
+
+  // Arena-allocated messages to transfer.
+  std::vector<void*> msgs_;
+
+  fdf::Channel client_;
+  fdf::Dispatcher client_dispatcher_;
+  libsync::Completion client_dispatcher_shutdown_;
+
+  fdf::Channel server_;
+  fdf::Dispatcher server_dispatcher_;
+  libsync::Completion server_dispatcher_shutdown_;
+
+  fdf::Arena arena_{nullptr};
+
+  uint32_t client_fake_driver_;
+  uint32_t server_fake_driver_;
+};
+
+// Test one way transfers using Zircon channels. The client writes |msg_count| messages,
+// and the benchmark waits until the server has received all of them.
+class OneWayZirconTest {
+ public:
+  OneWayZirconTest(uint32_t msg_count, uint32_t msg_size)
+      : loop_(&kAsyncLoopConfigNoAttachToCurrentThread),
+        msg_count_(msg_count),
+        msg_size_(msg_size),
+        msg_buffer_(msg_size),
+        read_buffer_(msg_size) {
+    zx_status_t status = zx::channel::create(0, &client_, &server_);
+    ASSERT_OK(status);
+
+    loop_.StartThread();
+  }
+
+  void Run() {
+    uint32_t num_msgs_read = 0;
+    libsync::Completion completion;
+
+    wait_ = std::make_unique<async::Wait>(
+        server_.get(), ZX_CHANNEL_READABLE, 0,
+        async::Wait::Handler([&, this](async_dispatcher_t* dispatcher, async::Wait* wait,
+                                       zx_status_t status, const zx_packet_signal_t* signal) {
+          uint32_t actual_bytes, actual_handles;
+          ASSERT_OK(server_.read(0, read_buffer_.data(), nullptr, msg_size_, 0, &actual_bytes,
+                                 &actual_handles));
+          FX_CHECK(actual_bytes == msg_size_);
+
+          num_msgs_read++;
+          // If we haven't read enough messages yet, register the read again.
+          if (num_msgs_read < msg_count_) {
+            wait_->Begin(loop_.dispatcher());
+          } else {
+            // Test run is complete.
+            completion.Signal();
+          }
+        }));
+
+    ASSERT_OK(wait_->Begin(loop_.dispatcher()));
+
+    for (uint32_t i = 0; i < msg_count_; i++) {
+      ASSERT_OK(client_.write(0, msg_buffer_.data(), msg_size_, nullptr, 0));
+    }
+
+    ASSERT_OK(completion.Wait());
+  }
+
+  void TearDown() {
+    loop_.Quit();
+    loop_.JoinThreads();
+  }
+
+ private:
+  async::Loop loop_;
+
+  uint32_t msg_count_;
+  uint32_t msg_size_;
+
+  std::vector<char> msg_buffer_;
+  std::vector<char> read_buffer_;
+
+  zx::channel client_;
+  zx::channel server_;
+
+  std::unique_ptr<async::Wait> wait_;
+};
+
+void RegisterTests() {
+  std::map<uint32_t, std::string> kDispatcherTypes = {
+      {FDF_DISPATCHER_OPTION_SYNCHRONIZED, "Inline_Synchronized"},
+      {FDF_DISPATCHER_OPTION_ALLOW_SYNC_CALLS, "AllowSyncCalls"},
+      {FDF_DISPATCHER_OPTION_UNSYNCHRONIZED, "Inline_Unsynchronized"},
+  };
+
+  static const unsigned kMessageSizesInBytes[] = {
+      64,
+      64 * 1024,
+  };
+  static const unsigned kNumMessages[] = {
+      1,
+      1024,
+  };
+  for (auto message_size : kMessageSizesInBytes) {
+    for (auto num_messages : kNumMessages) {
+      for (auto const& [option, name] : kDispatcherTypes) {
+        auto benchmark_name =
+            fbl::StringPrintf("OneWay_%s_%u_%ubytes", name.c_str(), num_messages, message_size);
+        driver_runtime_benchmark::RegisterTest<OneWayTest>(benchmark_name.c_str(), option,
+                                                           num_messages, message_size);
+      }
+      auto benchmark_name =
+          fbl::StringPrintf("OneWay_Zircon_%u_%ubytes", num_messages, message_size);
+      driver_runtime_benchmark::RegisterTest<OneWayZirconTest>(benchmark_name.c_str(), num_messages,
+                                                               message_size);
+    }
+  }
+}
+PERFTEST_CTOR(RegisterTests)
+
+}  // namespace
