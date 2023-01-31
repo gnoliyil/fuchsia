2c43c216db7ed2f72b837de2c655ff408165da5c
Novin Changizi <novinc@google.com>
2023-01-31 19:14:01 +0000

    [driver][sdk] Pull TestNode into its own library
    Pulls out the TestNode class into a separate library inside the sdk.
    This will be used in followup cls by the testing library.
    
    Tests: No behavior changes
    Change-Id: If658f927cb4e9162cbd1cbeca1c84f7a5a4e22a1
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/794931
    Reviewed-by: David Gilhooley <dgilhooley@google.com>
    Commit-Queue: Novin Changizi <novinc@google.com>

---
+++ sdk/lib/driver/testing/cpp/test_node.cc
@@ -0,0 +1,54 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/driver/testing/cpp/test_node.h>
+
+namespace fdf_testing {
+
+zx::result<fidl::ClientEnd<fuchsia_driver_framework::Node>> TestNode::CreateNodeChannel() {
+  if (node_binding_.has_value()) {
+    return zx::error(ZX_ERR_ALREADY_EXISTS);
+  }
+  zx::result endpoints = fidl::CreateEndpoints<fuchsia_driver_framework::Node>();
+  if (endpoints.is_error()) {
+    return endpoints.take_error();
+  }
+  node_binding_.emplace(dispatcher_, std::move(endpoints->server), this,
+                        [this](fidl::UnbindInfo) { Remove(); });
+  return zx::ok(std::move(endpoints->client));
+}
+
+void TestNode::AddChild(AddChildRequestView request, AddChildCompleter::Sync& completer) {
+  std::string name{request->args.name().get()};
+  auto [it, inserted] = children_.try_emplace(name, dispatcher_, name);
+  if (!inserted) {
+    completer.ReplyError(fuchsia_driver_framework::NodeError::kNameAlreadyExists);
+    return;
+  }
+  TestNode& node = it->second;
+  node.parent_ = *this;
+  node.controller_binding_.emplace(dispatcher_, std::move(request->controller), &node,
+                                   fidl::kIgnoreBindingClosure);
+  if (request->node) {
+    node.node_binding_.emplace(dispatcher_, std::move(request->node), &node,
+                               [this](fidl::UnbindInfo) { Remove(); });
+  }
+
+  completer.ReplySuccess();
+}
+
+void TestNode::Remove() {
+  children_.clear();
+  node_binding_.reset();
+  controller_binding_.reset();
+
+  if (!parent_.has_value()) {
+    return;
+  }
+  // After this call we are destructed, so don't access anything else.
+  size_t count = parent_.value().get().children_.erase(name_);
+  ZX_ASSERT_MSG(count == 1, "Should've removed 1 child, removed %ld", count);
+}
+
+}  // namespace fdf_testing
