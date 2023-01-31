2a7040d49cbe4f0c3f64fa6ce4abda2a105f8b66
Ruby Zhuang <rdzhuang@google.com>
2023-01-31 19:13:50 +0000

    [usb-mass-storage] Migrate to mock-ddk
    Bug: 115052
    
    Test: ums-test
          ums-block-test
    
    Change-Id: Ia50ca6796c37d4b58e1b80c78d5564587f5787e2
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/794073
    Reviewed-by: Sam Hansen <hansens@google.com>
    Commit-Queue: Ruby Zhuang <rdzhuang@google.com>

--- src/devices/block/drivers/usb-mass-storage/tests/block.cc
+++ src/devices/block/drivers/usb-mass-storage/tests/block.cc
@@ -4,147 +4,90 @@
 
 #include "../block.h"
 
-#include <lib/fake_ddk/fake_ddk.h>
-
 #include <fbl/string.h>
 #include <zxtest/zxtest.h>
 
 #include "../usb-mass-storage.h"
+#include "src/devices/testing/mock-ddk/mock-device.h"
 
 namespace {
 
-struct Context {
-  ums::UmsBlockDevice* dev;
-  fbl::String name;
-  block_info_t info;
-  block_op_t* op;
-  zx_status_t status;
-  ums::Transaction* txn;
-};
-
-class Binder : public fake_ddk::Bind {
+class UmsBlockTest : public zxtest::Test {
  public:
-  zx_status_t DeviceRemove(zx_device_t* dev) {
-    Context* context = reinterpret_cast<Context*>(dev);
-    context->dev->DdkRelease();
-    fake_ddk::Bind::DeviceRemove(dev);
-    return ZX_OK;
+  UmsBlockTest() : parent_(MockDevice::FakeRootParent()) {}
+
+  void SetUp() override {
+    dev_ = std::make_unique<ums::UmsBlockDevice>(
+        parent_.get(), 5, [&](ums::Transaction* txn) { context_.txn = txn; });
+    ums::BlockDeviceParameters params = {};
+    params.lun = 5;
+    EXPECT_TRUE(params == dev_->GetBlockDeviceParameters(),
+                "Parameters must be set to user-provided values.");
+    dev_->Adopt();
   }
-  zx_status_t DeviceAdd(zx_driver_t* drv, zx_device_t* parent, device_add_args_t* args,
-                        zx_device_t** out) {
-    *out = parent;
-    Context* context = reinterpret_cast<Context*>(parent);
-    context->name = fbl::String(args->name);
-    return ZX_OK;
+
+  void TearDown() override {
+    device_async_remove(dev_->zxdev());
+    mock_ddk::ReleaseFlaggedDevices(parent_.get());
+  }
+
+ protected:
+  struct Context {
+    block_op_t* op;
+    zx_status_t status;
+    ums::Transaction* txn;
+  };
+
+  std::shared_ptr<zx_device> parent_;
+  std::unique_ptr<ums::UmsBlockDevice> dev_;
+  Context context_;
+
+  static void BlockCallback(void* ctx, zx_status_t status, block_op_t* op) {
+    Context* context = reinterpret_cast<Context*>(ctx);
+    context->status = status;
+    context->op = op;
   }
 };
 
-static void BlockCallback(void* ctx, zx_status_t status, block_op_t* op) {
-  Context* context = reinterpret_cast<Context*>(ctx);
-  context->status = status;
-  context->op = op;
-}
+TEST_F(UmsBlockTest, AddTest) { EXPECT_EQ(ZX_OK, dev_->Add(), "Expected Add to succeed"); }
 
-TEST(UmsBlock, ConstructorTest) {
-  Binder ddk;
-  Context context;
-  ums::UmsBlockDevice dev(reinterpret_cast<zx_device_t*>(&context), 5,
-                          [&](ums::Transaction* txn) { context.txn = txn; });
-  context.dev = &dev;
-  ums::BlockDeviceParameters params = {};
-  params.lun = 5;
-  EXPECT_TRUE(params == dev.GetBlockDeviceParameters(),
-              "Parameters must be set to user-provided values.");
-  dev.Adopt();
-  EXPECT_TRUE(dev.Release(), "Expected to free the device");
-}
-
-TEST(UmsBlock, AddTest) {
-  Binder ddk;
-  Context context;
-  auto fake_zxdev = reinterpret_cast<zx_device_t*>(&context);
-  ums::UmsBlockDevice dev(fake_zxdev, 5, [&](ums::Transaction* txn) { context.txn = txn; });
-  context.dev = &dev;
-  ums::BlockDeviceParameters params = {};
-  params.lun = 5;
-  EXPECT_TRUE(params == dev.GetBlockDeviceParameters(),
-              "Parameters must be set to user-provided values.");
-  dev.Adopt();
-  EXPECT_EQ(ZX_OK, dev.Add(), "Expected Add to succeed");
-  dev.DdkAsyncRemove();
-  ddk.WaitUntilRemove();
-  EXPECT_TRUE(dev.Release(), "Expected to free the device");
-}
-
-TEST(UmsBlock, NotSupportedTest) {
-  Binder ddk;
-  Context context;
-  auto fake_zxdev = reinterpret_cast<zx_device_t*>(&context);
-  ums::UmsBlockDevice dev(fake_zxdev, 5, [&](ums::Transaction* txn) { context.txn = txn; });
-  context.dev = &dev;
-  dev.Adopt();
-  EXPECT_EQ(ZX_OK, dev.Add(), "Expected Add to succeed");
-  EXPECT_TRUE(fbl::String("lun-005") == context.name);
+TEST_F(UmsBlockTest, NotSupportedTest) {
+  EXPECT_EQ(ZX_OK, dev_->Add(), "Expected Add to succeed");
+  MockDevice* child = parent_->GetLatestChild();
+  EXPECT_TRUE(fbl::String("lun-005") == child->name());
   ums::Transaction txn;
   txn.op.command = BLOCK_OP_MASK;
-  dev.BlockImplQueue(&txn.op, BlockCallback, &context);
-  EXPECT_EQ(ZX_ERR_NOT_SUPPORTED, context.status);
-  dev.DdkAsyncRemove();
-  ddk.WaitUntilRemove();
-  EXPECT_TRUE(dev.Release(), "Expected to free the device");
+  dev_->BlockImplQueue(&txn.op, BlockCallback, &context_);
+  EXPECT_EQ(ZX_ERR_NOT_SUPPORTED, context_.status);
 }
 
-TEST(UmsBlock, ReadTest) {
-  Binder ddk;
-  Context context;
-  auto fake_zxdev = reinterpret_cast<zx_device_t*>(&context);
-  ums::UmsBlockDevice dev(fake_zxdev, 5, [&](ums::Transaction* txn) { context.txn = txn; });
-  context.dev = &dev;
-  dev.Adopt();
-  EXPECT_EQ(ZX_OK, dev.Add(), "Expected Add to succeed");
-  EXPECT_TRUE(fbl::String("lun-005") == context.name);
+TEST_F(UmsBlockTest, ReadTest) {
+  EXPECT_EQ(ZX_OK, dev_->Add(), "Expected Add to succeed");
+  MockDevice* child = parent_->GetLatestChild();
+  EXPECT_TRUE(fbl::String("lun-005") == child->name());
   ums::Transaction txn;
   txn.op.command = BLOCK_OP_READ;
-  dev.BlockImplQueue(&txn.op, BlockCallback, &context);
-  dev.DdkAsyncRemove();
-  ddk.WaitUntilRemove();
-  EXPECT_TRUE(dev.Release(), "Expected to free the device");
+  dev_->BlockImplQueue(&txn.op, BlockCallback, &context_);
 }
 
-TEST(UmsBlock, WriteTest) {
-  Binder ddk;
-  Context context;
-  auto fake_zxdev = reinterpret_cast<zx_device_t*>(&context);
-  ums::UmsBlockDevice dev(fake_zxdev, 5, [&](ums::Transaction* txn) { context.txn = txn; });
-  context.dev = &dev;
-  dev.Adopt();
-  EXPECT_EQ(ZX_OK, dev.Add(), "Expected Add to succeed");
-  EXPECT_TRUE(fbl::String("lun-005") == context.name);
+TEST_F(UmsBlockTest, WriteTest) {
+  EXPECT_EQ(ZX_OK, dev_->Add(), "Expected Add to succeed");
+  MockDevice* child = parent_->GetLatestChild();
+  EXPECT_TRUE(fbl::String("lun-005") == child->name());
   ums::Transaction txn;
   txn.op.command = BLOCK_OP_WRITE;
-  dev.BlockImplQueue(&txn.op, BlockCallback, &context);
-  EXPECT_EQ(&txn, context.txn);
-  dev.DdkAsyncRemove();
-  ddk.WaitUntilRemove();
-  EXPECT_TRUE(dev.Release(), "Expected to free the device");
+  dev_->BlockImplQueue(&txn.op, BlockCallback, &context_);
+  EXPECT_EQ(&txn, context_.txn);
 }
 
-TEST(UmsBlock, FlushTest) {
-  Binder ddk;
-  Context context;
-  auto fake_zxdev = reinterpret_cast<zx_device_t*>(&context);
-  ums::UmsBlockDevice dev(fake_zxdev, 5, [&](ums::Transaction* txn) { context.txn = txn; });
-  context.dev = &dev;
-  dev.Adopt();
-  EXPECT_EQ(ZX_OK, dev.Add(), "Expected Add to succeed");
-  EXPECT_TRUE(fbl::String("lun-005") == context.name);
+TEST_F(UmsBlockTest, FlushTest) {
+  EXPECT_EQ(ZX_OK, dev_->Add(), "Expected Add to succeed");
+  MockDevice* child = parent_->GetLatestChild();
+  EXPECT_TRUE(fbl::String("lun-005") == child->name());
   ums::Transaction txn;
   txn.op.command = BLOCK_OP_FLUSH;
-  dev.BlockImplQueue(&txn.op, BlockCallback, &context);
-  EXPECT_EQ(&txn, context.txn);
-  dev.DdkAsyncRemove();
-  ddk.WaitUntilRemove();
-  EXPECT_TRUE(dev.Release(), "Expected to free the device");
+  dev_->BlockImplQueue(&txn.op, BlockCallback, &context_);
+  EXPECT_EQ(&txn, context_.txn);
 }
 
 }  // namespace
