c74b41417b3933595d1104b4f354c9520c13b4aa
Sen Jiang <senj@google.com>
2023-02-06 22:16:50 +0000

    [fuchsia-pkg-testing] Add helper to generate delivery blob.
    Also added option to RepositoryBuilder to support delivery blob.
    
    Bug: 120322
    Change-Id: Id7ef447b3200bc119307310d8eab65014bf9dd4a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/797189
    Commit-Queue: Sen Jiang <senj@google.com>
    Reviewed-by: Ben Keller <galbanum@google.com>

---
+++ src/sys/pkg/lib/fuchsia-pkg-testing/src/delivery_blob.rs
@@ -0,0 +1,65 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {
+    crate::process::wait_for_process_termination,
+    anyhow::Context as _,
+    std::{
+        fmt,
+        fs::{self, File},
+        path::Path,
+    },
+};
+
+/// Given an uncompressed blob, generate a delivery blob.
+/// Requires /tmp directory in the namespace.
+pub async fn generate_delivery_blob(
+    uncompressed_blob: impl AsRef<[u8]>,
+) -> Result<Vec<u8>, anyhow::Error> {
+    let tmp = tempfile::tempdir().context("create tmp")?;
+    fs::write(tmp.path().join("uncompressed"), uncompressed_blob)
+        .context("write uncompressed blob")?;
+    generate_delivery_blob_in_path(&tmp, "uncompressed", &tmp, "delivery").await?;
+    fs::read(tmp.path().join("delivery")).context("read delivery blob")
+}
+
+pub(crate) async fn generate_delivery_blob_in_path(
+    uncompressed_blob_dir: impl AsRef<Path>,
+    uncompressed_blob_path: impl fmt::Display,
+    delivery_blob_dir: impl AsRef<Path>,
+    delivery_blob_path: impl fmt::Display,
+) -> Result<(), anyhow::Error> {
+    let process = fdio::SpawnBuilder::new()
+        .options(fdio::SpawnOptions::CLONE_ALL - fdio::SpawnOptions::CLONE_NAMESPACE)
+        .arg("blobfs-compression")?
+        // TODO(fxbug.dev/120322): Specify the type of delivery blob once blobfs-compression supports it.
+        .arg(format!("--source_file=/in/{uncompressed_blob_path}"))?
+        .arg(format!("--compressed_file=/out/{delivery_blob_path}"))?
+        .add_dir_to_namespace(
+            "/in",
+            File::open(uncompressed_blob_dir).context("open uncompressed_blob_dir")?,
+        )?
+        .add_dir_to_namespace(
+            "/out",
+            File::open(delivery_blob_dir).context("open delivery_blob_dir")?,
+        )?
+        .spawn_from_path("/pkg/bin/blobfs-compression", &fuchsia_runtime::job_default())
+        .context("spawning blobfs-compression")?;
+    wait_for_process_termination(process).await.context("waiting for blobfs-compression")
+}
+
+#[cfg(test)]
+mod tests {
+    use {super::*, fuchsia_async as fasync};
+
+    #[fasync::run_singlethreaded(test)]
+    async fn generate_delivery_blob_empty() {
+        generate_delivery_blob([]).await.unwrap();
+    }
+
+    #[fasync::run_singlethreaded(test)]
+    async fn generate_delivery_blob_small() {
+        generate_delivery_blob([1; 234]).await.unwrap();
+    }
+}
