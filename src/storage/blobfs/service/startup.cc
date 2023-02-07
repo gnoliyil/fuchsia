979389bcad48f6ef94a58fe56b1e2e03097d8a74
Gianfranco Valentino <gevalentino@google.com>
2023-02-07 22:31:43 +0000

    [block-device] Silence RemoteBlockDevice::Create
    This method has been deprecated for relying on Block fidl interface,
    pre existing callers have been marked with reference to the
    tracking bug.
    
    Bug: 121465
    Test: fx build
    Change-Id: I319e693eeddacfb1435919c6aabc704e94162a1b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/798787
    Reviewed-by: Tamir Duberstein <tamird@google.com>
    Commit-Queue: Gianfranco Valentino <gevalentino@google.com>

--- src/storage/blobfs/service/startup.cc
+++ src/storage/blobfs/service/startup.cc
@@ -94,8 +94,22 @@ void StartupService::Start(StartRequestView request, StartCompleter::Sync& compl
   // TODO(https://fxbug.dev/97783): Consider removing this when multiple sessions are permitted.
   zx::result<> result = [&]() -> zx::result<> {
     std::unique_ptr<block_client::RemoteBlockDevice> device;
+
+// TODO(fxbug.dev/121465): Use  Volume instead of Block.
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#elif __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
     zx_status_t status =
         block_client::RemoteBlockDevice::Create(std::move(request->device), &device);
+#ifdef __clang__
+#pragma clang diagnostic pop
+#elif __GNUC__
+#pragma GCC diagnostic pop
+#endif
     if (status != ZX_OK) {
       FX_PLOGS(ERROR, status) << "Could not initialize block device";
       return zx::error(status);
@@ -114,8 +128,22 @@ void StartupService::Format(FormatRequestView request, FormatCompleter::Sync& co
   // TODO(https://fxbug.dev/97783): Consider removing this when multiple sessions are permitted.
   zx::result<> result = [&]() -> zx::result<> {
     std::unique_ptr<block_client::RemoteBlockDevice> device;
+
+// TODO(fxbug.dev/121465): Use  Volume instead of Block.
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#elif __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
     zx_status_t status =
         block_client::RemoteBlockDevice::Create(std::move(request->device), &device);
+#ifdef __clang__
+#pragma clang diagnostic pop
+#elif __GNUC__
+#pragma GCC diagnostic pop
+#endif
     if (status != ZX_OK) {
       FX_LOGS(ERROR) << "Could not initialize block device: " << zx_status_get_string(status);
       return zx::error(status);
@@ -137,8 +165,22 @@ void StartupService::Check(CheckRequestView request, CheckCompleter::Sync& compl
   // TODO(https://fxbug.dev/97783): Consider removing this when multiple sessions are permitted.
   zx::result<> result = [&]() -> zx::result<> {
     std::unique_ptr<block_client::RemoteBlockDevice> device;
+
+// TODO(fxbug.dev/121465): Use  Volume instead of Block.
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#elif __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
     zx_status_t status =
         block_client::RemoteBlockDevice::Create(std::move(request->device), &device);
+#ifdef __clang__
+#pragma clang diagnostic pop
+#elif __GNUC__
+#pragma GCC diagnostic pop
+#endif
     if (status != ZX_OK) {
       FX_PLOGS(ERROR, status) << "Could not initialize block device";
       return zx::error(status);
