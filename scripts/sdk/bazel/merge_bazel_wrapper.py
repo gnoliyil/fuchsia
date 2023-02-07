3658b3574808a3c3050b640c25bd9ff0469503e5
Jiaming Li <lijiaming@google.com>
2023-02-07 21:48:18 +0000

    [bazel][sdk] Move merge_bazel_wrapper.py to fuchsia.git
    Change-Id: I540588e0ab2e27a78adc72c27d5804c01ea42c12
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/794070
    Reviewed-by: Dave Schuyler <dschuyler@google.com>
    Commit-Queue: Jiaming Li <lijiaming@google.com>
    Reviewed-by: Darren Chan <chandarren@google.com>
    Reviewed-by: Chase Latta <chaselatta@google.com>

---
+++ scripts/sdk/bazel/merge_bazel_wrapper.py
@@ -0,0 +1,65 @@
+# Copyright 2023 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Recursively copy all files from --source-dir to the dest-dir therein.
+If a meta/manifest.json is found, the arch target will be merged into that file
+(rather than replacing it).
+"""
+
+
+import argparse
+import filecmp
+import os
+import shutil
+import json
+
+
+def merge_new_manifest(manifest_json, new_manifest):
+    if manifest_json is None:
+        manifest_json = new_manifest
+    else:
+        manifest_json["arch"]["target"].extend(new_manifest["arch"]["target"])
+
+    return manifest_json
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--source-dir")
+    parser.add_argument("--dest-dir")
+    args = parser.parse_args()
+    manifest_json = None
+
+    for directory in os.listdir(args.source_dir):
+        directory = os.path.join(args.source_dir, directory)
+        for root, _, files in os.walk(directory):
+            for f in files:
+                source_file = os.path.join(root, f)
+                relpath = os.path.relpath(source_file, directory)
+                dest_file = os.path.join(args.dest_dir, relpath)
+                if relpath == "meta/manifest.json":
+                    with open(source_file, "r") as manifest_file:
+                        new_manifest = json.load(manifest_file)
+                    manifest_json = merge_new_manifest(
+                        manifest_json, new_manifest)
+                    continue
+                if os.path.exists(dest_file):
+                    # Coherence check: Ensure that files with the same path have equal content.
+                    assert filecmp.cmp(
+                        source_file, dest_file), f"Cannot merge {source_file} and {dest_file}, which both have different content."
+                    continue
+                if not os.path.exists(os.path.dirname(dest_file)):
+                    os.makedirs(os.path.dirname(dest_file))
+                shutil.copy(source_file, dest_file)
+
+    # Write meta/manifest.json
+    dest_file = os.path.join(dest_dir, "meta/manifest.json")
+    if not os.path.exists(os.path.dirname(dest_file)):
+        os.makedirs(os.path.dirname(dest_file))
+    with open(dest_file, "w") as f:
+        json.dump(manifest_json, f, indent=2)
+
+
+if __name__ == "__main__":
+    main()
