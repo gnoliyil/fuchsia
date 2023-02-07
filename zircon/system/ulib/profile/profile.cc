18d909815c7f2208c772c87babf28624bc3f6d5c
Yifei Teng <yifeit@google.com>
2023-02-07 06:32:10 +0000

    [fixit][profile] Migrate profile to C++ bindings
    This is a pure refactor.
    
    Bug: 95833
    Change-Id: Iba7377f9a98d2c7137c50404739da0fc5b049015
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/798348
    Reviewed-by: Tamir Duberstein <tamird@google.com>
    Reviewed-by: Corey Tabaka <eieio@google.com>
    Fuchsia-Auto-Submit: Yifei Teng <yifeit@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- zircon/system/ulib/profile/profile.cc
+++ zircon/system/ulib/profile/profile.cc
@@ -4,9 +4,8 @@
 
 #include "zircon/syscalls/profile.h"
 
-#include <fuchsia/scheduler/c/fidl.h>
+#include <fidl/fuchsia.scheduler/cpp/wire.h>
 #include <inttypes.h>
-#include <lib/fidl-async/bind.h>
 #include <lib/fit/result.h>
 #include <lib/profile/profile.h>
 #include <lib/syslog/cpp/macros.h>
@@ -34,84 +33,99 @@ using zircon_profile::MaybeMediaRole;
 using zircon_profile::ParseRoleSelector;
 using zircon_profile::ProfileMap;
 
-struct Context {
-  const zx_handle_t root_job;
-  ProfileMap profiles;
+class ProfileProvider : public fidl::WireServer<fuchsia_scheduler::ProfileProvider> {
+ public:
+  static zx::result<ProfileProvider*> Create(const zx::job& root_job);
 
-  static Context* Get(void* ctx) { return static_cast<Context*>(ctx); }
+  void OnConnect(async_dispatcher_t* dispatcher,
+                 fidl::ServerEnd<fuchsia_scheduler::ProfileProvider> server_end) {
+    bindings_.AddBinding(dispatcher, std::move(server_end), this, fidl::kIgnoreBindingClosure);
+  }
+
+ private:
+  ProfileProvider(const zx::job& root_job, ProfileMap profiles)
+      : root_job_(root_job), profiles_(std::move(profiles)) {}
+
+  void GetProfile(GetProfileRequestView request, GetProfileCompleter::Sync& completer) override;
+
+  void GetDeadlineProfile(GetDeadlineProfileRequestView request,
+                          GetDeadlineProfileCompleter::Sync& completer) override;
+
+  void GetCpuAffinityProfile(GetCpuAffinityProfileRequestView request,
+                             GetCpuAffinityProfileCompleter::Sync& completer) override;
+
+  void SetProfileByRole(SetProfileByRoleRequestView request,
+                        SetProfileByRoleCompleter::Sync& completer) override;
+
+  fidl::ServerBindingGroup<fuchsia_scheduler::ProfileProvider> bindings_;
+  zx::unowned_job root_job_;
+  ProfileMap profiles_;
 };
 
-zx_status_t GetProfileSimple(void* ctx, uint32_t priority, const char* name_data, size_t name_size,
-                             fidl_txn_t* txn) {
-  Context* const context = Context::Get(ctx);
-
-  const std::string name{name_data, name_size};
-  FX_SLOG(INFO, "Priority requested", KV("name", name.c_str()), KV("priority", priority),
+void ProfileProvider::GetProfile(GetProfileRequestView request,
+                                 GetProfileCompleter::Sync& completer) {
+  const std::string name{request->name.get()};
+  FX_SLOG(INFO, "Priority requested", KV("name", name.c_str()), KV("priority", request->priority),
           KV("tag", "ProfileProvider"));
 
-  zx_profile_info_t info = {};
-  info.flags = ZX_PROFILE_INFO_FLAG_PRIORITY;
-  info.priority =
-      std::min<uint32_t>(std::max<uint32_t>(priority, ZX_PRIORITY_LOWEST), ZX_PRIORITY_HIGHEST);
+  zx_profile_info_t info = {
+      .flags = ZX_PROFILE_INFO_FLAG_PRIORITY,
+      .priority = static_cast<int32_t>(std::min<uint32_t>(
+          std::max<uint32_t>(request->priority, ZX_PRIORITY_LOWEST), ZX_PRIORITY_HIGHEST)),
+  };
 
   zx::profile profile;
-  zx_status_t status =
-      zx_profile_create(context->root_job, 0u, &info, profile.reset_and_get_address());
-  return fuchsia_scheduler_ProfileProviderGetProfile_reply(
-      txn, status, status == ZX_OK ? profile.release() : ZX_HANDLE_INVALID);
+  zx_status_t status = zx::profile::create(*root_job_, 0u, &info, &profile);
+  completer.Reply(status, std::move(profile));
 }
 
-zx_status_t GetDeadlineProfileSimple(void* ctx, uint64_t capacity, uint64_t relative_deadline,
-                                     uint64_t period, const char* name_data, size_t name_size,
-                                     fidl_txn_t* txn) {
-  Context* const context = Context::Get(ctx);
+void ProfileProvider::GetDeadlineProfile(GetDeadlineProfileRequestView request,
+                                         GetDeadlineProfileCompleter::Sync& completer) {
+  const std::string name{request->name.get()};
+  const double utilization =
+      static_cast<double>(request->capacity) / static_cast<double>(request->deadline);
+  FX_SLOG(INFO, "Deadline requested", KV("name", name.c_str()), KV("capacity", request->capacity),
+          KV("deadline", request->deadline), KV("period", request->period),
+          KV("utilization", utilization), KV("tag", "ProfileProvider"));
 
-  const std::string name{name_data, name_size};
-  const double utilization = static_cast<double>(capacity) / static_cast<double>(relative_deadline);
-  FX_SLOG(INFO, "Deadline requested", KV("name", name.c_str()), KV("capacity", capacity),
-          KV("deadline", relative_deadline), KV("period", period), KV("utilization", utilization),
-          KV("tag", "ProfileProvider"));
-
-  zx_profile_info_t info = {};
-  info.flags = ZX_PROFILE_INFO_FLAG_DEADLINE;
-  info.deadline_params.capacity = capacity;
-  info.deadline_params.relative_deadline = relative_deadline;
-  info.deadline_params.period = period;
+  zx_profile_info_t info = {
+      .flags = ZX_PROFILE_INFO_FLAG_DEADLINE,
+      .deadline_params =
+          zx_sched_deadline_params_t{
+              .capacity = static_cast<zx_duration_t>(request->capacity),
+              .relative_deadline = static_cast<zx_duration_t>(request->deadline),
+              .period = static_cast<zx_duration_t>(request->period),
+          },
+  };
 
   zx::profile profile;
-  zx_status_t status =
-      zx_profile_create(context->root_job, 0u, &info, profile.reset_and_get_address());
-  return fuchsia_scheduler_ProfileProviderGetDeadlineProfile_reply(
-      txn, status, status == ZX_OK ? profile.release() : ZX_HANDLE_INVALID);
+  zx_status_t status = zx::profile::create(*root_job_, 0u, &info, &profile);
+  completer.Reply(status, std::move(profile));
 }
 
-zx_status_t GetCpuAffinityProfileSimple(void* ctx, const fuchsia_scheduler_CpuSet* cpu_mask,
-                                        fidl_txn_t* txn) {
-  Context* const context = Context::Get(ctx);
+void ProfileProvider::GetCpuAffinityProfile(GetCpuAffinityProfileRequestView request,
+                                            GetCpuAffinityProfileCompleter::Sync& completer) {
+  zx_profile_info_t info = {
+      .flags = ZX_PROFILE_INFO_FLAG_CPU_MASK,
+  };
 
-  zx_profile_info_t info = {};
-  info.flags = ZX_PROFILE_INFO_FLAG_CPU_MASK;
-
-  static_assert(sizeof(info.cpu_affinity_mask.mask) == sizeof(cpu_mask->mask));
-  static_assert(std::size(info.cpu_affinity_mask.mask) == std::size(decltype(cpu_mask->mask){}));
-  memcpy(info.cpu_affinity_mask.mask, cpu_mask->mask, sizeof(cpu_mask->mask));
+  static_assert(sizeof(info.cpu_affinity_mask.mask) == sizeof(request->cpu_mask.mask));
+  static_assert(std::size(info.cpu_affinity_mask.mask) ==
+                std::size(decltype(request->cpu_mask.mask){}));
+  memcpy(info.cpu_affinity_mask.mask, request->cpu_mask.mask.begin(),
+         sizeof(request->cpu_mask.mask));
 
   zx::profile profile;
-  zx_status_t status =
-      zx_profile_create(context->root_job, 0u, &info, profile.reset_and_get_address());
-  return fuchsia_scheduler_ProfileProviderGetCpuAffinityProfile_reply(
-      txn, status, status == ZX_OK ? profile.release() : ZX_HANDLE_INVALID);
+  zx_status_t status = zx::profile::create(*root_job_, 0u, &info, &profile);
+  completer.Reply(status, std::move(profile));
 }
 
-zx_status_t SetProfileByRoleSimple(void* ctx, zx_handle_t thread_handle, const char* role_data,
-                                   size_t role_size, fidl_txn_t* txn) {
-  zx::thread thread{thread_handle};
-  Context* const context = Context::Get(ctx);
-
+void ProfileProvider::SetProfileByRole(SetProfileByRoleRequestView request,
+                                       SetProfileByRoleCompleter::Sync& completer) {
   // Log the requested role and PID:TID of the thread being assigned.
   zx_info_handle_basic_t handle_info{};
-  zx_status_t status =
-      thread.get_info(ZX_INFO_HANDLE_BASIC, &handle_info, sizeof(handle_info), nullptr, nullptr);
+  zx_status_t status = request->thread.get_info(ZX_INFO_HANDLE_BASIC, &handle_info,
+                                                sizeof(handle_info), nullptr, nullptr);
   if (status != ZX_OK) {
     FX_SLOG(WARNING, "Failed to get info for thread handle",
             KV("status", zx_status_get_string(status)));
@@ -119,14 +133,14 @@ zx_status_t SetProfileByRoleSimple(void* ctx, zx_handle_t thread_handle, const c
     handle_info.related_koid = ZX_KOID_INVALID;
   }
 
-  const std::string role_selector{role_data, role_size};
+  const std::string role_selector{request->role.get()};
   FX_SLOG(INFO, "Role requested", KV("ProfileProvider", role_selector.c_str()),
           KV("pid", handle_info.related_koid), KV("tid", handle_info.koid),
           KV("tag", "ProfileProvider"));
 
   const fit::result role_result = ParseRoleSelector(role_selector);
   if (role_result.is_error()) {
-    return fuchsia_scheduler_ProfileProviderSetProfileByRole_reply(txn, ZX_ERR_INVALID_ARGS);
+    return completer.Reply(ZX_ERR_INVALID_ARGS);
   }
 
   // Select the profile parameters based on the role selector. The builtin roles cannot be
@@ -136,11 +150,10 @@ zx_status_t SetProfileByRoleSimple(void* ctx, zx_handle_t thread_handle, const c
     info.flags = ZX_PROFILE_INFO_FLAG_PRIORITY;
     info.priority = ZX_PRIORITY_DEFAULT;
   } else if (role_result->name == "fuchsia.test-role" && role_result->has("not-found")) {
-    return fuchsia_scheduler_ProfileProviderSetProfileByRole_reply(txn, ZX_ERR_NOT_FOUND);
+    return completer.Reply(ZX_ERR_NOT_FOUND);
   } else if (role_result->name == "fuchsia.test-role" && role_result->has("ok")) {
-    return fuchsia_scheduler_ProfileProviderSetProfileByRole_reply(txn, ZX_OK);
-  } else if (auto search = context->profiles.find(role_result->name);
-             search != context->profiles.cend()) {
+    return completer.Reply(ZX_OK);
+  } else if (auto search = profiles_.find(role_result->name); search != profiles_.cend()) {
     info = search->second.info;
   } else if (const auto media_role = MaybeMediaRole(*role_result); media_role.is_ok()) {
     // TODO(fxbug.dev/40858): If a media profile is not found in the system config, use the
@@ -151,7 +164,7 @@ zx_status_t SetProfileByRoleSimple(void* ctx, zx_handle_t thread_handle, const c
       FX_SLOG(WARNING, "Skipping media profile with no override and invalid selectors",
               KV("capacity", media_role->capacity), KV("deadline", media_role->deadline),
               KV("role", role_result->name.c_str()), KV("tag", "ProfileProvider"));
-      return fuchsia_scheduler_ProfileProviderSetProfileByRole_reply(txn, ZX_OK);
+      return completer.Reply(ZX_OK);
     }
 
     FX_SLOG(INFO, "Using selector parameters for media profile with no override",
@@ -165,40 +178,31 @@ zx_status_t SetProfileByRoleSimple(void* ctx, zx_handle_t thread_handle, const c
   } else {
     FX_SLOG(WARNING, "Requested role not found", KV("role", role_result->name.c_str()),
             KV("tag", "ProfileProvider"));
-    return fuchsia_scheduler_ProfileProviderSetProfileByRole_reply(txn, ZX_ERR_NOT_FOUND);
+    return completer.Reply(ZX_ERR_NOT_FOUND);
   }
 
   zx::profile profile;
-  status = zx_profile_create(context->root_job, 0u, &info, profile.reset_and_get_address());
+  status = zx::profile::create(*root_job_, 0u, &info, &profile);
   if (status != ZX_OK) {
     // Failing to create a profile is likely due to invalid profile parameters.
-    return fuchsia_scheduler_ProfileProviderSetProfileByRole_reply(txn, ZX_ERR_INTERNAL);
+    return completer.Reply(ZX_ERR_INTERNAL);
   }
 
-  status = thread.set_profile(profile, 0);
-  return fuchsia_scheduler_ProfileProviderSetProfileByRole_reply(txn, status);
+  status = request->thread.set_profile(profile, 0);
+  completer.Reply(status);
 }
 
-fuchsia_scheduler_ProfileProvider_ops ops = {
-    .GetProfile = GetProfileSimple,
-    .GetDeadlineProfile = GetDeadlineProfileSimple,
-    .GetCpuAffinityProfile = GetCpuAffinityProfileSimple,
-    .SetProfileByRole = SetProfileByRoleSimple,
-};
-
 constexpr const char* profile_svc_names[] = {
-    fuchsia_scheduler_ProfileProvider_Name,
+    fidl::DiscoverableProtocolName<fuchsia_scheduler::ProfileProvider>,
     nullptr,
 };
 
-zx_status_t init(void** out_ctx) {
-  const auto root_job = static_cast<zx_handle_t>(reinterpret_cast<uintptr_t>(*out_ctx));
-
+zx::result<ProfileProvider*> ProfileProvider::Create(const zx::job& root_job) {
   auto result = zircon_profile::LoadConfigs(kConfigPath);
   if (result.is_error()) {
     FX_SLOG(ERROR, "Failed to load configs", KV("error", result.error_value().c_str()),
             KV("tag", "ProfileProvider"));
-    return ZX_ERR_INTERNAL;
+    return zx::error(ZX_ERR_INTERNAL);
   }
 
   // Apply the dispatch role if defined.
@@ -206,8 +210,7 @@ zx_status_t init(void** out_ctx) {
   const auto search = result->find(dispatch_role);
   if (search != result->end()) {
     zx::profile profile;
-    zx_status_t status =
-        zx_profile_create(root_job, 0u, &search->second.info, profile.reset_and_get_address());
+    zx_status_t status = zx::profile::create(root_job, 0u, &search->second.info, &profile);
     if (status != ZX_OK) {
       FX_SLOG(ERROR, "Failed to create profile for role", KV("role", dispatch_role.c_str()),
               KV("error", zx_status_get_string(status)), KV("tag", "ProfileProvider"));
@@ -220,19 +223,27 @@ zx_status_t init(void** out_ctx) {
     }
   }
 
-  *out_ctx = new Context{root_job, std::move(result.value())};
+  return zx::ok(new ProfileProvider{root_job, std::move(result.value())});
+}
+
+zx_status_t init(void** out_ctx) {
+  const auto root_job =
+      zx::unowned_job{static_cast<zx_handle_t>(reinterpret_cast<uintptr_t>(*out_ctx))};
+  zx::result provider = ProfileProvider::Create(*root_job);
+  if (!provider.is_ok()) {
+    return provider.status_value();
+  }
+  *out_ctx = provider.value();
   return ZX_OK;
 }
 
 zx_status_t connect(void* ctx, async_dispatcher_t* dispatcher, const char* service_name,
                     zx_handle_t request) {
-  if (strcmp(service_name, fuchsia_scheduler_ProfileProvider_Name) == 0) {
-    auto callback_adapter = [](void* ctx, fidl_txn* txn, fidl_incoming_msg* msg,
-                               const void* ops) -> int {
-      const auto* provider_ops = static_cast<const fuchsia_scheduler_ProfileProvider_ops_t*>(ops);
-      return fuchsia_scheduler_ProfileProvider_dispatch(ctx, txn, msg, provider_ops);
-    };
-    return fidl_bind(dispatcher, request, callback_adapter, ctx, &ops);
+  if (std::string_view{service_name} ==
+      fidl::DiscoverableProtocolName<fuchsia_scheduler::ProfileProvider>) {
+    static_cast<ProfileProvider*>(ctx)->OnConnect(
+        dispatcher, fidl::ServerEnd<fuchsia_scheduler::ProfileProvider>{zx::channel{request}});
+    return ZX_OK;
   }
 
   zx_handle_close(request);
