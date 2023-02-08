c796528a4d3c73c876ca8b22e2e54962b216544b
Tamir Duberstein <tamird@google.com>
2023-02-08 19:27:07 +0000

    [utest][miscsvc] Use typed channels
    Bug: 69585
    Change-Id: I51fb205089bc16be3c097e148dd690da7fca11eb
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/799585
    Fuchsia-Auto-Submit: Tamir Duberstein <tamird@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: David Gilhooley <dgilhooley@google.com>

--- zircon/system/utest/miscsvc/main.cc
+++ zircon/system/utest/miscsvc/main.cc
@@ -2,33 +2,23 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <fcntl.h>
 #include <fidl/fuchsia.paver/cpp/wire.h>
-#include <lib/fdio/directory.h>
-#include <lib/fdio/fd.h>
-#include <lib/fdio/fdio.h>
-#include <lib/fidl/cpp/wire/connect_service.h>
-#include <lib/zx/channel.h>
-#include <lib/zx/vmo.h>
-#include <zircon/status.h>
-#include <zircon/syscalls.h>
+#include <lib/component/incoming/cpp/protocol.h>
 
 #include <zxtest/zxtest.h>
 
 namespace {
 
-using fuchsia_paver::Paver;
-
 TEST(MiscSvcTest, PaverSvccEnumeratesSuccessfully) {
-  zx::channel local, remote;
-  ASSERT_OK(zx::channel::create(0, &local, &remote));
-  ASSERT_OK(fdio_service_connect(fidl::DiscoverableProtocolDefaultPath<Paver>, remote.release()));
+  zx::result client_end = component::Connect<fuchsia_paver::Paver>();
+  ASSERT_OK(client_end);
+  fidl::WireSyncClient paver(std::move(client_end.value()));
 
-  zx::channel local2, remote2;
-  ASSERT_OK(zx::channel::create(0, &local2, &remote2));
+  zx::result endpoints = fidl::CreateEndpoints<fuchsia_paver::DataSink>();
+  ASSERT_OK(endpoints);
+  auto& [client, server] = endpoints.value();
 
-  fidl::WireSyncClient<Paver> paver(std::move(local));
-  auto result = paver->FindDataSink(std::move(local2));
+  const fidl::OneWayStatus result = paver->FindDataSink(std::move(server));
   ASSERT_OK(result.status());
 }
 
