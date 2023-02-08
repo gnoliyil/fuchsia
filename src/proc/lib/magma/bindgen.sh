26bfb87f7d71483ef4d1e1471c9fcf4fde46c57a
C Stout <cstout@google.com>
2023-02-08 18:45:22 +0000

    [proc][magma] Update bindings
    Some #defines must now be hardcoded because type casts
    were added and bindgen doesn't handle that yet.
    
    Change-Id: I93befbd35d396773c85ddd03b7d0b239d6d6d6be
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/798867
    Commit-Queue: Craig Stout <cstout@google.com>
    Reviewed-by: Kevin Lindkvist <lindkvist@google.com>

--- src/proc/lib/magma/bindgen.sh
+++ src/proc/lib/magma/bindgen.sh
@@ -14,7 +14,21 @@ readonly RAW_LINES="// Copyright 2021 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use zerocopy::{AsBytes, FromBytes};"
+use zerocopy::{AsBytes, FromBytes};
+
+// TODO: BEGIN remove these hardcoded values when bindgen is fixed
+pub const MAGMA_STATUS_OK: u32 = 0;
+pub const MAGMA_STATUS_INVALID_ARGS: i32 = -2;
+pub const MAGMA_IMAGE_CREATE_FLAGS_PRESENTABLE: u32 = 1;
+pub const MAGMA_IMAGE_CREATE_FLAGS_VULKAN_USAGE: u32 = 2;
+pub const MAGMA_MAX_IMAGE_PLANES: u32 = 4;
+pub const MAGMA_COHERENCY_DOMAIN_CPU: u32 = 0;
+pub const MAGMA_COHERENCY_DOMAIN_RAM: u32 = 1;
+pub const MAGMA_COHERENCY_DOMAIN_INACCESSIBLE: u32 = 2;
+pub const MAGMA_POLL_TYPE_SEMAPHORE: u32 = 1;
+pub const MAGMA_POLL_TYPE_HANDLE: u32 = 2;
+// TODO: END remove these hardcoded values when bindgen is fixed
+"
 
 PATH="$PWD/prebuilt/third_party/rust/linux-x64/bin:$PATH" \
 ./prebuilt/third_party/rust_bindgen/linux-x64/bindgen \
