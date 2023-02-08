32044c2f70e62a128f58225b663ed6513c52674a
Tamir Duberstein <tamird@google.com>
2023-02-08 13:38:22 +0000

    [kernel-debug] Avoid C FIDL bindings
    Bug: 95833
    Change-Id: I881656f04879ff24867e16b6560ff18483b4ed3a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/798351
    Reviewed-by: David Gilhooley <dgilhooley@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Tamir Duberstein <tamird@google.com>
    Reviewed-by: Ian McKellar <ianloic@google.com>

--- zircon/system/ulib/kernel-debug/include/lib/kernel-debug/kernel-debug.h
+++ zircon/system/ulib/kernel-debug/include/lib/kernel-debug/kernel-debug.h
@@ -2,8 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#pragma once
+#ifndef LIB_KERNEL_DEBUG_KERNEL_DEBUG_H_
+#define LIB_KERNEL_DEBUG_KERNEL_DEBUG_H_
 
 #include <lib/svc/service.h>
 
-const zx_service_provider_t* kernel_debug_get_service_provider(void);
+const zx_service_provider_t* kernel_debug_get_service_provider();
+
+#endif  // LIB_KERNEL_DEBUG_KERNEL_DEBUG_H_
--- zircon/system/ulib/kernel-debug/kernel-debug.cc
+++ zircon/system/ulib/kernel-debug/kernel-debug.cc
@@ -2,56 +2,58 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <fidl/fuchsia.kernel/cpp/wire.h>
 #include <lib/kernel-debug/kernel-debug.h>
-
-#include <fuchsia/kernel/c/fidl.h>
-#include <lib/fidl-async/bind.h>
 #include <lib/zircon-internal/ktrace.h>
-#include <string.h>
-#include <zircon/assert.h>
-#include <zircon/syscalls.h>
+#include <lib/zx/resource.h>
 
 namespace {
 
-zx_status_t HandleSendDebugCommand(void* ctx, const char* command, size_t command_size,
-                                   fidl_txn_t* txn) {
-  const zx_handle_t root_resource = static_cast<zx_handle_t>(reinterpret_cast<uintptr_t>(ctx));
-  const auto status = zx_debug_send_command(root_resource, command, command_size);
-  return fuchsia_kernel_DebugBrokerSendDebugCommand_reply(txn, status);
-}
+class DebugBroker : public fidl::WireServer<fuchsia_kernel::DebugBroker> {
+ public:
+  explicit DebugBroker(zx::unowned_resource root_resource)
+      : root_resource_(std::move(root_resource)) {}
 
-zx_status_t HandleSetTracingEnabled(void* ctx, bool enabled, fidl_txn_t* txn) {
-  const zx_handle_t root_resource = static_cast<zx_handle_t>(reinterpret_cast<uintptr_t>(ctx));
-  zx_status_t status;
-  if (enabled) {
-    status = zx_ktrace_control(root_resource, KTRACE_ACTION_START, KTRACE_GRP_ALL, nullptr);
-  } else {
-    status = zx_ktrace_control(root_resource, KTRACE_ACTION_STOP, 0, nullptr);
-    if (status == ZX_OK) {
-      status = zx_ktrace_control(root_resource, KTRACE_ACTION_REWIND, 0, nullptr);
-    }
+ private:
+  void SendDebugCommand(SendDebugCommandRequestView request,
+                        SendDebugCommandCompleter::Sync& completer) override {
+    completer.Reply(zx_debug_send_command(root_resource_->get(), request->command.data(),
+                                          request->command.size()));
   }
-  return fuchsia_kernel_DebugBrokerSendDebugCommand_reply(txn, status);
-}
 
-constexpr const fuchsia_kernel_DebugBroker_ops_t kInterfaceOps = {
-    .SendDebugCommand = HandleSendDebugCommand,
-    .SetTracingEnabled = HandleSetTracingEnabled,
+  void SetTracingEnabled(SetTracingEnabledRequestView request,
+                         SetTracingEnabledCompleter::Sync& completer) override {
+    zx_status_t status;
+    if (request->enabled) {
+      status =
+          zx_ktrace_control(root_resource_->get(), KTRACE_ACTION_START, KTRACE_GRP_ALL, nullptr);
+    } else {
+      status = zx_ktrace_control(root_resource_->get(), KTRACE_ACTION_STOP, 0, nullptr);
+      if (status == ZX_OK) {
+        status = zx_ktrace_control(root_resource_->get(), KTRACE_ACTION_REWIND, 0, nullptr);
+      }
+    }
+    completer.Reply(status);
+  }
+
+  const zx::unowned_resource root_resource_;
 };
 
 zx_status_t Connect(void* ctx, async_dispatcher_t* dispatcher, const char* service_name,
                     zx_handle_t request) {
-  if (!strcmp(service_name, fuchsia_kernel_DebugBroker_Name)) {
-    return fidl_bind(dispatcher, request, (fidl_dispatch_t*)fuchsia_kernel_DebugBroker_dispatch,
-                     ctx, &kInterfaceOps);
+  zx::channel channel{request};
+  if (fidl::DiscoverableProtocolName<fuchsia_kernel::DebugBroker> == service_name) {
+    const zx_handle_t resource = static_cast<zx_handle_t>(reinterpret_cast<uintptr_t>(ctx));
+    fidl::BindServer(dispatcher, fidl::ServerEnd<fuchsia_kernel::DebugBroker>{std::move(channel)},
+                     std::make_unique<DebugBroker>(zx::unowned_resource{resource}));
+    return ZX_OK;
   }
 
-  zx_handle_close(request);
   return ZX_ERR_NOT_SUPPORTED;
 }
 
 constexpr const char* kServices[] = {
-    fuchsia_kernel_DebugBroker_Name,
+    fidl::DiscoverableProtocolName<fuchsia_kernel::DebugBroker>,
     nullptr,
 };
 
