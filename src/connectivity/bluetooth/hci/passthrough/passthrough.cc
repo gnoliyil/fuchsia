dc5ccd6a1fe8e27b269c5f52a78ecd5441d6ee9c
David Gilhooley <dgilhooley@google.com>
2023-02-08 19:51:17 +0000

    [bluetooth][passthrough] Full C++ conversion
    Fully convert this driver to C++. There should be no functional
    changes here hopefully.
    
    DdkUnbind was removed because it wasn't doing anything.
    
    Bug: 95833
    Bug: 121382
    Change-Id: I50f66b61b735202844b93ccdcea5ae748f24d763
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/799484
    Reviewed-by: Marie Janssen <jamuraa@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Tamir Duberstein <tamird@google.com>
    Fuchsia-Auto-Submit: David Gilhooley <dgilhooley@google.com>

--- src/connectivity/bluetooth/hci/passthrough/passthrough.cc
+++ src/connectivity/bluetooth/hci/passthrough/passthrough.cc
@@ -3,8 +3,9 @@
 // found in the LICENSE file.
 
 #include <assert.h>
-#include <fuchsia/hardware/bluetooth/c/fidl.h>
+#include <fidl/fuchsia.hardware.bluetooth/cpp/wire.h>
 #include <fuchsia/hardware/bt/hci/c/banjo.h>
+#include <lib/ddk/debug.h>
 #include <lib/ddk/device.h>
 #include <lib/ddk/driver.h>
 #include <stdio.h>
@@ -13,110 +14,104 @@
 #include <unistd.h>
 #include <zircon/status.h>
 
+#include <ddktl/device.h>
+#include <ddktl/fidl.h>
+
 #include "src/connectivity/bluetooth/hci/passthrough/bt_hci_passthrough_bind.h"
 
-typedef struct {
-  zx_device_t* dev;
-  zx_device_t* transport_dev;
+namespace {
 
+class PassthroughDevice;
+
+using PassthroughDeviceType = ddk::Device<PassthroughDevice, ddk::GetProtocolable,
+                                          ddk::Messageable<fuchsia_hardware_bluetooth::Hci>::Mixin>;
+
+class PassthroughDevice : public PassthroughDeviceType {
+ public:
+  explicit PassthroughDevice(zx_device_t* parent) : PassthroughDeviceType(parent) {}
+
+  // Ddk Apis.
+  static zx_status_t Bind(void* ctx, zx_device_t* device);
+  zx_status_t DdkGetProtocol(uint32_t proto_id, void* out);
+  void DdkRelease();
+
+ private:
+  // fuchsia.hardware.bluetooth.Hci APIs.
+  void OpenCommandChannel(OpenCommandChannelRequestView request,
+                          OpenCommandChannelCompleter::Sync& completer) override;
+  void OpenAclDataChannel(OpenAclDataChannelRequestView request,
+                          OpenAclDataChannelCompleter::Sync& completer) override;
+  void OpenSnoopChannel(OpenSnoopChannelRequestView request,
+                        OpenSnoopChannelCompleter::Sync& completer) override;
   bt_hci_protocol_t hci;
-} passthrough_t;
+};
 
-static zx_status_t bt_hci_passthrough_get_protocol(void* ctx, uint32_t proto_id, void* out_proto) {
+zx_status_t PassthroughDevice::DdkGetProtocol(uint32_t proto_id, void* out_proto) {
   if (proto_id != ZX_PROTOCOL_BT_HCI) {
     return ZX_ERR_NOT_SUPPORTED;
   }
 
-  passthrough_t* passthrough = static_cast<passthrough_t*>(ctx);
-  bt_hci_protocol_t* hci_proto = static_cast<bt_hci_protocol_t*>(out_proto);
-
   // Forward the underlying bt-transport ops.
-  hci_proto->ops = passthrough->hci.ops;
-  hci_proto->ctx = passthrough->hci.ctx;
-
+  bt_hci_protocol_t* hci_proto = static_cast<bt_hci_protocol_t*>(out_proto);
+  hci_proto->ops = this->hci.ops;
+  hci_proto->ctx = this->hci.ctx;
   return ZX_OK;
 }
 
-static void bt_hci_passthrough_unbind(void* ctx) {
-  passthrough_t* passthrough = static_cast<passthrough_t*>(ctx);
+void PassthroughDevice::DdkRelease() { delete this; }
 
-  device_unbind_reply(passthrough->dev);
-}
-
-static void bt_hci_passthrough_release(void* ctx) { free(ctx); }
-
-zx_status_t fidl_bt_hci_open_command_channel(void* ctx, zx_handle_t channel) {
-  passthrough_t* hci = static_cast<passthrough_t*>(ctx);
-  return bt_hci_open_command_channel(&hci->hci, channel);
-}
-
-zx_status_t fidl_bt_hci_open_acl_data_channel(void* ctx, zx_handle_t channel) {
-  passthrough_t* hci = static_cast<passthrough_t*>(ctx);
-  return bt_hci_open_acl_data_channel(&hci->hci, channel);
-}
-
-zx_status_t fidl_bt_hci_open_snoop_channel(void* ctx, zx_handle_t channel) {
-  passthrough_t* hci = static_cast<passthrough_t*>(ctx);
-  return bt_hci_open_snoop_channel(&hci->hci, channel);
-}
-
-const fuchsia_hardware_bluetooth_Hci_ops_t fidl_ops = {
-    .OpenCommandChannel = fidl_bt_hci_open_command_channel,
-    .OpenAclDataChannel = fidl_bt_hci_open_acl_data_channel,
-    .OpenSnoopChannel = fidl_bt_hci_open_snoop_channel,
-};
-
-static zx_status_t fuchsia_bt_hci_message_instance(void* ctx, fidl_incoming_msg_t* msg,
-                                                   fidl_txn_t* txn) {
-  return fuchsia_hardware_bluetooth_Hci_dispatch(ctx, txn, msg, &fidl_ops);
-}
-
-static zx_protocol_device_t passthrough_device_proto = {
-    .version = DEVICE_OPS_VERSION,
-    .get_protocol = bt_hci_passthrough_get_protocol,
-    .unbind = bt_hci_passthrough_unbind,
-    .release = bt_hci_passthrough_release,
-    .message = fuchsia_bt_hci_message_instance,
-};
-
-static zx_status_t bt_hci_passthrough_bind(void* ctx, zx_device_t* device) {
-  printf("bt_hci_passthrough_bind: starting\n");
-  passthrough_t* passthrough = static_cast<passthrough_t*>(calloc(1, sizeof(passthrough_t)));
-  if (!passthrough) {
-    printf("bt_hci_passthrough_bind: not enough memory\n");
-    return ZX_ERR_NO_MEMORY;
+void PassthroughDevice::OpenCommandChannel(OpenCommandChannelRequestView request,
+                                           OpenCommandChannelCompleter::Sync& completer) {
+  if (zx_status_t status = bt_hci_open_command_channel(&this->hci, request->channel.release());
+      status != ZX_OK) {
+    completer.Close(status);
   }
+}
+
+void PassthroughDevice::OpenAclDataChannel(OpenAclDataChannelRequestView request,
+                                           OpenAclDataChannelCompleter::Sync& completer) {
+  if (zx_status_t status = bt_hci_open_acl_data_channel(&this->hci, request->channel.release());
+      status != ZX_OK) {
+    completer.Close(status);
+  }
+}
+
+void PassthroughDevice::OpenSnoopChannel(OpenSnoopChannelRequestView request,
+                                         OpenSnoopChannelCompleter::Sync& completer) {
+  if (zx_status_t status = bt_hci_open_snoop_channel(&this->hci, request->channel.release());
+      status != ZX_OK) {
+    completer.Close(status);
+  }
+}
+
+zx_status_t PassthroughDevice::Bind(void* ctx, zx_device_t* device) {
+  zxlogf(INFO, "bt_hci_passthrough_bind: starting");
+  std::unique_ptr passthrough = std::make_unique<PassthroughDevice>(device);
 
   zx_status_t status = device_get_protocol(device, ZX_PROTOCOL_BT_HCI, &passthrough->hci);
   if (status != ZX_OK) {
-    printf("bt_hci_passthrough_bind: failed protocol: %s\n", zx_status_get_string(status));
+    zxlogf(ERROR, "bt_hci_passthrough_bind: failed protocol: %s", zx_status_get_string(status));
     return status;
   }
 
-  device_add_args_t args = {
-      .version = DEVICE_ADD_ARGS_VERSION,
-      .name = "bt_hci_passthrough",
-      .ctx = passthrough,
-      .ops = &passthrough_device_proto,
-      .proto_id = ZX_PROTOCOL_BT_HCI,
-  };
-
-  passthrough->transport_dev = device;
-
-  status = device_add(device, &args, &passthrough->dev);
-  if (status == ZX_OK) {
+  if (zx_status_t status = passthrough->DdkAdd(
+          ddk::DeviceAddArgs("bt_hci_passthrough").set_proto_id(ZX_PROTOCOL_BT_HCI));
+      status != ZX_OK) {
+    zxlogf(ERROR, "bt_hci_passthrough_bind failed: %s", zx_status_get_string(status));
     return status;
   }
 
-  printf("bt_hci_passthrough_bind failed: %s\n", zx_status_get_string(status));
-  bt_hci_passthrough_release(passthrough);
-  return status;
+  // The driver framework now owns the memory for passthrough.
+  std::ignore = passthrough.release();
+  return ZX_OK;
 }
 
-static zx_driver_ops_t bt_hci_passthrough_driver_ops = {
+zx_driver_ops_t bt_hci_passthrough_driver_ops = {
     .version = DRIVER_OPS_VERSION,
-    .bind = bt_hci_passthrough_bind,
+    .bind = PassthroughDevice::Bind,
 };
+
+}  // namespace
 
 // This should be the last driver queried, so we match any transport.
 ZIRCON_DRIVER(bt_hci_passthrough, bt_hci_passthrough_driver_ops, "fuchsia", "0.1");
