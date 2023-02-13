7d0d760ca8a32a88fae65b0d603beb00f4541175
Adrian Danis <adanis@google.com>
2023-02-13 01:34:26 +0000

    [kernel][vm] LZ4 compressor
    Adds an LZ4 compressor guarded by a build flag.
    
    Bug: 60238
    Change-Id: Ie00496227339160cae8f9350435d72e8a639d9ef
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/792562
    Reviewed-by: Nick Maniscalco <maniscalco@google.com>
    Fuchsia-Auto-Submit: Adrian Danis <adanis@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Rasha Eqbal <rashaeqbal@google.com>

---
+++ zircon/kernel/vm/include/vm/lz4_compressor.h
@@ -0,0 +1,55 @@
+// Copyright 2023 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#ifndef ZIRCON_KERNEL_VM_INCLUDE_VM_LZ4_COMPRESSOR_H_
+#define ZIRCON_KERNEL_VM_INCLUDE_VM_LZ4_COMPRESSOR_H_
+
+// The lz4 library is not included as a dependency if PAGE_COMPRESSION is not enabled and so the
+// includes and the entire class definition need to be skipped.
+#if PAGE_COMPRESSION
+
+#include <fbl/array.h>
+#include <kernel/mutex.h>
+#include <lz4/lz4.h>
+#include <vm/compression.h>
+
+class VmLz4Compressor final : public VmCompressionStrategy {
+ public:
+  // Returns nullptr on allocation or other failure.
+  static fbl::RefPtr<VmLz4Compressor> Create();
+  ~VmLz4Compressor() override = default;
+  DISALLOW_COPY_ASSIGN_AND_MOVE(VmLz4Compressor);
+
+  CompressResult Compress(const void* src, void* dst, size_t dst_limit) override;
+  void Decompress(const void* src, size_t src_len, void* dst) override;
+  void Dump() const override;
+
+ private:
+  // Constructor is private to ensure that Init() gets called to initialize state.
+  VmLz4Compressor(int lz4_acceleration) : acceleration_(lz4_acceleration) {}
+
+  DECLARE_MUTEX(VmLz4Compressor) compress_lock_;
+
+  // Internal helper that initializes the stream_ and compressed_zero_. If this returns false the
+  // object should be destroyed and not used.
+  bool Init();
+
+  // The acceleration factor that is directly passed into the lz4 compress methods. Is set by the
+  // constructor and has no default value. Making this non-const would require a different form of
+  // zero detection.
+  const int acceleration_;
+
+  // The compressed representation, using the |acceleration_| value, of a page of zeroes. Used to
+  // determine if any |Compress| request is actually just a zero page by memcmp'ing with the result.
+  fbl::Array<char> compressed_zero_;
+
+  // The LZ4_stream_t used to hold compression state.
+  LZ4_stream_t stream_ TA_GUARDED(compress_lock_);
+};
+
+#endif  // PAGE_COMPRESSION
+
+#endif  // ZIRCON_KERNEL_VM_INCLUDE_VM_LZ4_COMPRESSOR_H_
---
+++ zircon/kernel/vm/lz4_compressor.cc
@@ -0,0 +1,98 @@
+// Copyright 2023 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#include <lib/boot-options/boot-options.h>
+#include <lib/fit/defer.h>
+
+#include <vm/lz4_compressor.h>
+#include <vm/physmap.h>
+
+#if PAGE_COMPRESSION
+
+VmLz4Compressor::CompressResult VmLz4Compressor::Compress(const void *src, void *dst,
+                                                          size_t dst_limit) {
+  int compressed_result;
+  int threshold = static_cast<int>(dst_limit);
+  {
+    Guard<Mutex> guard{&compress_lock_};
+    compressed_result = LZ4_compress_fast_extState_fastReset(
+        &stream_, static_cast<const char *>(src), static_cast<char *>(dst), PAGE_SIZE, threshold,
+        acceleration_);
+  }
+  if (compressed_result == 0) {
+    return FailTag{};
+  }
+  DEBUG_ASSERT(compressed_result > 0 && compressed_result <= threshold);
+  size_t compressed_size = static_cast<size_t>(compressed_result);
+  if (compressed_size == compressed_zero_.size()) {
+    if (memcmp(dst, compressed_zero_.get(), compressed_size) == 0) {
+      return ZeroTag{};
+    }
+  }
+  return compressed_size;
+}
+
+void VmLz4Compressor::Decompress(const void *src, size_t src_len, void *dst) {
+  int result = LZ4_decompress_safe(static_cast<const char *>(src), static_cast<char *>(dst),
+                                   static_cast<int>(src_len), PAGE_SIZE);
+  ASSERT(result == PAGE_SIZE);
+}
+
+void VmLz4Compressor::Dump() const {}
+
+bool VmLz4Compressor::Init() {
+  Guard<Mutex> guard{&compress_lock_};
+  // Initialize the stream. As our stream should be exactly sized and aligned there is no reason for
+  // this to return anything other than the original pointer.
+  LZ4_stream_t *stream = LZ4_initStream(&stream_, sizeof(stream_));
+  if (stream != &stream_) {
+    return false;
+  }
+
+  // Zero page should compress quite well, so just use a small stack allocation.
+  constexpr size_t kMaxZeroPageStorage = 128;
+  char temp_zero_compress[kMaxZeroPageStorage];
+  int compress_result = LZ4_compress_fast_extState_fastReset(
+      &stream_, static_cast<const char *>(paddr_to_physmap(vm_get_zero_page_paddr())),
+      temp_zero_compress, PAGE_SIZE, kMaxZeroPageStorage, acceleration_);
+  if (compress_result == 0) {
+    printf("ERROR: LZ4 failed to compress zero page with acceleration %d into %zu bytes\n",
+           acceleration_, kMaxZeroPageStorage);
+    return false;
+  }
+  DEBUG_ASSERT(compress_result > 0);
+  size_t compressed_size = static_cast<size_t>(compress_result);
+  DEBUG_ASSERT(compressed_size <= kMaxZeroPageStorage);
+  // Now allocate the exact storage.
+  fbl::AllocChecker ac;
+  compressed_zero_ = fbl::MakeArray<char>(&ac, compressed_size);
+  if (!ac.check()) {
+    return false;
+  }
+  memcpy(compressed_zero_.get(), temp_zero_compress, compressed_size);
+
+  return true;
+}
+
+fbl::RefPtr<VmLz4Compressor> VmLz4Compressor::Create() {
+  // TODO(fxbug.dev/60238): Configure this using a kernel cmdline parameter.
+  const int acceleration = 11;
+
+  fbl::AllocChecker ac;
+  fbl::RefPtr<VmLz4Compressor> lz4 =
+      fbl::AdoptRef<VmLz4Compressor>(new (&ac) VmLz4Compressor(acceleration));
+  if (!ac.check()) {
+    return nullptr;
+  }
+
+  if (!lz4->Init()) {
+    return nullptr;
+  }
+
+  return lz4;
+}
+
+#endif
