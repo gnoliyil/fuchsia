bb6719835f5c2202566f7597cba21acc334bdee3
Roland McGrath <mcgrathr@google.com>
2023-02-14 05:20:17 +0000

    [zxdump] Handle alignment issues in Process::read_memory
    Change-Id: I1ef1b9b487cc12a0fd8509c025e7364158aa36bc
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/793908
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Reviewed-by: Joshua Seaton <joshuaseaton@google.com>
    Reviewed-by: Gianfranco Valentino <gevalentino@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/lib/elfldltl/include/lib/elfldltl/note.h
+++ src/lib/elfldltl/include/lib/elfldltl/note.h
@@ -94,7 +94,7 @@ struct ElfNote {
 };
 
 // This is a forward-iterable container view of notes in a note segment,
-// constructible from the raw bytes known to be properly aligned.
+// constructible from the raw bytes.
 template <ElfData Data = ElfData::kNative>
 class ElfNoteSegment {
  public:
@@ -171,7 +171,13 @@ class ElfNoteSegment {
 
  private:
   // This is safe only if the size has been checked.
-  static auto& Header(Bytes data) { return *reinterpret_cast<const Nhdr*>(data.data()); }
+  static Nhdr Header(Bytes data) {
+    assert(data.size_bytes() >= sizeof(Nhdr));
+    // Use memcpy just in case the data is not properly aligned in memory.
+    Nhdr nhdr;
+    memcpy(&nhdr, data.data(), sizeof(nhdr));
+    return nhdr;
+  }
 
   // Returns true if the data starts with a valid note.
   static bool Check(Bytes data) {
