07b279f88c95203ee7da9a96cf568ce1a4dfcbd9
Roland McGrath <mcgrathr@google.com>
2023-02-14 05:20:17 +0000

    [zxdump] Handle alignment issues in Process::read_memory
    Change-Id: I1ef1b9b487cc12a0fd8509c025e7364158aa36bc
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/793908
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Reviewed-by: Joshua Seaton <joshuaseaton@google.com>
    Reviewed-by: Gianfranco Valentino <gevalentino@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/lib/zxdump/include/lib/zxdump/task.h
+++ src/lib/zxdump/include/lib/zxdump/task.h
@@ -492,11 +492,16 @@ class Process : public Task {
           .status_ = ZX_ERR_INVALID_ARGS,
       }};
     }
-    auto result = ReadMemoryImpl(vaddr, count * sizeof(T), size_mode);
-    if (result.is_error()) {
+
+    Buffer<> buffer;
+    if (auto result = ReadMemoryImpl(vaddr, count * sizeof(T), size_mode, alignof(T));
+        result.is_ok()) {
+      buffer = *std::move(result);
+    } else {
       return result.take_error();
     }
-    return fit::ok(static_cast<Buffer<T, View>>(*std::move(result)));
+
+    return fit::ok(static_cast<Buffer<T, View>>(std::move(buffer)));
   }
 
   // This just reads a single datum of type T from dumped process memory at
@@ -570,8 +575,8 @@ class Process : public Task {
     uint64_t offset, filesz, memsz;
   };
 
-  fit::result<Error, Buffer<>> ReadMemoryImpl(uint64_t vaddr, size_t size,
-                                              ReadMemorySize size_mode);
+  fit::result<Error, Buffer<>> ReadMemoryImpl(uint64_t vaddr, size_t size, ReadMemorySize size_mode,
+                                              size_t alignment);
   fit::result<Error, Buffer<>> ReadLiveMemory(uint64_t vaddr, size_t size,
                                               ReadMemorySize size_mode);
 
