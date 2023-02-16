6c48e0901622460761c557d123e14686d3139dca
Adam Perry <adamperry@google.com>
2023-02-16 20:33:42 +0000

    [rust][3p] Remove gpu-allocator.
    Change-Id: Ia1fa436fa3ade332d2e65263810448a21678ebb4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/804274
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Erick Tryzelaar <etryzelaar@google.com>
    Fuchsia-Auto-Submit: Adam Perry <adamperry@google.com>

--- third_party/rust_crates/vendor/gpu-allocator-0.19.1/src/allocator/dedicated_block_allocator/visualizer.rs
+++
@@ -1,8 +0,0 @@
-use super::DedicatedBlockAllocator;
-use crate::visualizer::SubAllocatorVisualizer;
-
-impl SubAllocatorVisualizer for DedicatedBlockAllocator {
-    fn draw_base_info(&self, ui: &imgui::Ui<'_>) {
-        ui.text("Dedicated Block");
-    }
-}
