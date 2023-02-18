2ab2a1c2fd3412e31225a42110a23a67739327c1
Megan Batty <mgnb@google.com>
2023-02-18 03:04:19 +0000

    [ffx][guest] Migrate to dual-mode subtool
    Change-Id: I80d8d208e8be6b425589b94c851a176eb56c5f5f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/803826
    Reviewed-by: Casey Dahlin <sadmac@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Megan Batty <mgnb@google.com>

---
+++ src/virtualization/ffx/src/main.rs
@@ -0,0 +1,8 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#[fuchsia_async::run_singlethreaded]
+async fn main() {
+    ffx_guest_suite::fho_suite_main().await
+}
