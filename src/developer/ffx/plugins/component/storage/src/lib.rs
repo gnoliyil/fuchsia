28bd2ff3231dc3e922232e71cbec011e7fbafc00
Xyan Bhatnagar <xbhatnag@google.com>
2023-02-22 23:13:07 +0000

    [component_debug] Storage plugin uses new RealmQuery methods
    Following changes in https://fxrev.dev/797883, component_debug
    library now uses new RealmQuery methods for the `storage` plugin.
    
    Change-Id: I0c74a362589ee1db6e0fa14c88a51a43b4d4d6bc
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/802987
    Reviewed-by: Yegor Pomortsev <ypomortsev@google.com>
    Commit-Queue: Xyan Bhatnagar <xbhatnag@google.com>

--- src/developer/ffx/plugins/component/storage/src/lib.rs
+++ src/developer/ffx/plugins/component/storage/src/lib.rs
@@ -7,14 +7,14 @@ use component_debug::cli::{
     storage_copy_cmd, storage_delete_cmd, storage_list_cmd, storage_make_directory_cmd,
 };
 use errors::FfxError;
-use ffx_component::rcs::connect_to_lifecycle_controller;
+use ffx_component::rcs::connect_to_realm_query;
 use ffx_component_storage_args::{StorageCommand, SubCommandEnum};
 use ffx_core::ffx_plugin;
 use fidl_fuchsia_developer_remotecontrol::RemoteControlProxy;
 
 #[ffx_plugin()]
 pub async fn storage(remote_proxy: RemoteControlProxy, args: StorageCommand) -> Result<()> {
-    let lifecycle_controller = connect_to_lifecycle_controller(&remote_proxy).await?;
+    let realm_query = connect_to_realm_query(&remote_proxy).await?;
 
     // All errors from component_debug library are user-visible.
     match args.subcommand {
@@ -24,25 +24,19 @@ pub async fn storage(remote_proxy: RemoteControlProxy, args: StorageCommand) -> 
                 args.capability,
                 copy_args.source_path,
                 copy_args.destination_path,
-                lifecycle_controller,
+                realm_query,
             )
             .await
         }
         SubCommandEnum::Delete(delete_args) => {
-            storage_delete_cmd(
-                args.provider,
-                args.capability,
-                delete_args.path,
-                lifecycle_controller,
-            )
-            .await
+            storage_delete_cmd(args.provider, args.capability, delete_args.path, realm_query).await
         }
         SubCommandEnum::List(list_args) => {
             storage_list_cmd(
                 args.provider,
                 args.capability,
                 list_args.path,
-                lifecycle_controller,
+                realm_query,
                 std::io::stdout(),
             )
             .await
@@ -52,7 +46,7 @@ pub async fn storage(remote_proxy: RemoteControlProxy, args: StorageCommand) -> 
                 args.provider,
                 args.capability,
                 make_dir_args.path,
-                lifecycle_controller,
+                realm_query,
             )
             .await
         }
