3a9c80b88bce39ccb5b2eb99b263e313f35a0aaf
Pavan Kumar Juturu <jpavankumar@google.com>
2023-02-22 00:47:04 +0000

    [HoneyDew] - Adding honeydew/utils/http_utils.py
    Change-Id: Ib307f7b5f3d1ff643a345f27ab991b4a7bc35c34
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/807068
    Reviewed-by: Jeremiah Griffin <jzgriffin@google.com>
    Reviewed-by: Kevin Cho <chok@google.com>
    Commit-Queue: Pavan Kumar Juturu <jpavankumar@google.com>
    Reviewed-by: Christopher Johnson <crjohns@google.com>

---
+++ src/testing/end_to_end/honeydew/tests/unit_tests/utility_tests/http_utils_test.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env fuchsia-vendored-python
+# Copyright 2023 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Unit tests for honeydew.utils.http_utils.py."""
+
+import json
+import unittest
+from http.client import RemoteDisconnected
+from unittest import mock
+
+from honeydew import errors
+from honeydew.utils import http_utils
+from parameterized import parameterized
+
+# pylint: disable=protected-access
+_PARAMS = {
+    "url": "http://12.34.56.78",
+    "data": {
+        "key1": "value1",
+        "key2": "",
+        "key3": {
+            "k.3.1": "v.3.1"
+        },
+    },
+    "headers":
+        {
+            "Content-Type": "application/json; charset=utf-8",
+            "Content-Length": 58  # len(json.dumps(_PARAMS["data"]))
+        },
+    "timeout": http_utils._TIMEOUTS["HTTP_RESPONSE"] + 15,
+    "attempts": http_utils._DEFAULTS["ATTEMPTS"] + 5,
+    "interval": http_utils._DEFAULTS["INTERVAL"] + 5,
+    "exceptions_to_skip": []
+}
+_MOCK_ARGS = {
+    "urlopen_resp": b'{"id": "", "result": "fuchsia-emulator", "error": null}'
+}
+
+
+def _custom_test_name_func(testcase_func, _, param):
+    """Custom name function method."""
+    test_func_name = testcase_func.__name__
+
+    params_dict = param.args[0]
+    test_label = parameterized.to_safe_name(params_dict["label"])
+
+    return f"{test_func_name}_with_{test_label}"
+
+
+class HttpUtilsTests(unittest.TestCase):
+    """Unit tests for honeydew.utils.http_utils.py."""
+
+    @parameterized.expand(
+        [
+            (
+                {
+                    "label": "no_optional_params",
+                    "url": _PARAMS["url"],
+                    "optional_params": {},
+                    "urlopen_resp": _MOCK_ARGS["urlopen_resp"],
+                },),
+            (
+                {
+                    "label": "data_and_headers",
+                    "url": _PARAMS["url"],
+                    "optional_params":
+                        {
+                            "data": _PARAMS["data"],
+                            "headers": _PARAMS["headers"],
+                        },
+                    "urlopen_resp": _MOCK_ARGS["urlopen_resp"],
+                },),
+            (
+                {
+                    "label": "data_but_no_headers",
+                    "url": _PARAMS["url"],
+                    "optional_params": {
+                        "data": _PARAMS["data"],
+                    },
+                    "urlopen_resp": _MOCK_ARGS["urlopen_resp"],
+                },),
+            (
+                {
+                    "label": "all_optional_params",
+                    "url": _PARAMS["url"],
+                    "optional_params":
+                        {
+                            "data": _PARAMS["data"],
+                            "headers": _PARAMS["headers"],
+                            "timeout": _PARAMS["timeout"],
+                            "attempts": _PARAMS["attempts"],
+                            "interval": _PARAMS["interval"],
+                            "exceptions_to_skip": _PARAMS["exceptions_to_skip"]
+                        },
+                    "urlopen_resp": _MOCK_ARGS["urlopen_resp"],
+                },),
+        ],
+        name_func=_custom_test_name_func)
+    @mock.patch.object(http_utils.urllib.request, 'urlopen', autospec=True)
+    def test_send_http_request_success(self, parameterized_dict, mock_urlopen):
+        """Test case for http_utils.send_http_request() success case."""
+
+        urlopen_return_value = mock.MagicMock()
+        urlopen_return_value.read.return_value = parameterized_dict[
+            "urlopen_resp"]
+        urlopen_return_value.__enter__.return_value = urlopen_return_value
+        mock_urlopen.return_value = urlopen_return_value
+
+        result = http_utils.send_http_request(
+            url=parameterized_dict["url"],
+            **parameterized_dict["optional_params"])
+
+        expected_output = json.loads(
+            parameterized_dict["urlopen_resp"].decode("utf-8"))
+
+        self.assertEqual(result, expected_output)
+
+        mock_urlopen.assert_called_once()
+
+    @mock.patch.object(
+        http_utils.urllib.request,
+        'urlopen',
+        side_effect=RemoteDisconnected,
+        autospec=True)
+    def test_send_http_request_with_exceptions_to_skip(self, mock_urlopen):
+        """Testcase to make sure http_utils.send_http_request() do not
+        fail when it receives an exception that is part of exceptions_to_skip
+        input arg"""
+        response = http_utils.send_http_request(
+            url=_PARAMS["url"], exceptions_to_skip=[RemoteDisconnected])
+        self.assertEqual(response, {})
+        mock_urlopen.assert_called_once()
+
+    @mock.patch.object(http_utils.time, 'sleep', autospec=True)
+    @mock.patch.object(
+        http_utils.urllib.request,
+        'urlopen',
+        side_effect=RuntimeError("some run time error"),
+        autospec=True)
+    def test_send_http_request_fail_because_of_exception(
+            self, mock_urlopen, mock_sleep):
+        """Testcase for http_utils.send_http_request() failure case because of
+        an exception."""
+        with self.assertRaisesRegex(errors.HttpRequestError,
+                                    "Failed to send the HTTP request"):
+            http_utils.send_http_request(
+                url=_PARAMS["url"],
+                interval=_PARAMS["interval"],
+                attempts=_PARAMS["attempts"])
+
+        self.assertEqual(mock_urlopen.call_count, _PARAMS["attempts"])
+        self.assertEqual(mock_sleep.call_count, _PARAMS["attempts"] - 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
