002a5243c84349de3e3dad406863e2db09731cbf
Jay Zhuang <jayzhuang@google.com>
2023-02-22 15:21:39 +0000

    [assembly] Bazel x64 qemu-x64 partitions configs
    Also made all verification scripts follow symlinks for depfile entries.
    
    Bug: 119893
    Change-Id: I73ed451421b72e118a8e36c60842509c0296e82c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/806808
    Commit-Queue: Jay Zhuang <jayzhuang@google.com>
    Reviewed-by: David Turner <digit@google.com>

--- build/bazel/assembly/images_configurations/verify_images_configuration.py
+++ build/bazel/assembly/images_configurations/verify_images_configuration.py
@@ -22,6 +22,8 @@ def file_sha1(path):
 def replace_with_file_hash(dict, key, root_dir, extra_files_read):
     p = os.path.join(root_dir, dict[key])
     dict[key] = file_sha1(p)
+    # Follow links for depfile entry.
+    p = os.path.relpath(os.path.realpath(p))
     extra_files_read.append(p)
 
 
