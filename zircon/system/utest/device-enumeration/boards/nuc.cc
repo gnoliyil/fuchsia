8a0a30ab55be952eb7deefa8706efe44cdbc1b4c
Novin Changizi <novinc@google.com>
2023-02-23 22:50:10 +0000

    [device-enumeration][NUC] Board specific NUC tests
    The device enumeration test for NUCs are board specific. The nuc_env
    that the test_spec calls for includes multiple types of nucs. This adds
    board type querying to make sure the right test runs on the right board.
    
    Change-Id: Ia0a01c2010962791d986e13b630b578e8efa64a1
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/809109
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Commit-Queue: Novin Changizi <novinc@google.com>
    Reviewed-by: Vinicius Felizardo <felizardo@google.com>

--- zircon/system/utest/device-enumeration/boards/nuc.cc
+++ zircon/system/utest/device-enumeration/boards/nuc.cc
@@ -2,11 +2,65 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <fidl/fuchsia.sysinfo/cpp/wire.h>
+#include <lib/component/incoming/cpp/protocol.h>
+
 #include "zircon/system/utest/device-enumeration/common.h"
 
 namespace {
 
-TEST_F(DeviceEnumerationTest, NucTest) {
+enum NucType {
+  NUC7i5DNB,
+};
+
+std::string NucTypeToString(NucType nuc_type) {
+  if (nuc_type == NucType::NUC7i5DNB) {
+    return "NUC7i5DNB";
+  }
+
+  return "N/A";
+}
+
+std::variant<NucType, std::string> GetNucType() {
+  zx::result sys_info = component::Connect<fuchsia_sysinfo::SysInfo>();
+  EXPECT_EQ(ZX_OK, sys_info.status_value(), "Couldn't connect to SysInfo.");
+
+  const fidl::WireResult result = fidl::WireCall(sys_info.value())->GetBoardName();
+  EXPECT_EQ(ZX_OK, result.status(), "Couldn't call GetBoardName.");
+
+  const fidl::WireResponse response = result.value();
+  EXPECT_EQ(ZX_OK, response.status, "GetBoardName failed.");
+
+  const std::string_view board_name = response.name.get();
+  if (board_name == "NUC7i5DNB") {
+    return NucType::NUC7i5DNB;
+  }
+
+  return std::string(board_name);
+}
+
+bool CheckTestMatch(NucType desired_nuc_type) {
+  std::variant nuc_type = GetNucType();
+  const std::string* unknown_nuc_type = std::get_if<std::string>(&nuc_type);
+  if (unknown_nuc_type) {
+    printf("Skipping unknown NUC type: %s", unknown_nuc_type->c_str());
+    return false;
+  }
+
+  NucType nuc = std::get<NucType>(nuc_type);
+  if (nuc != desired_nuc_type) {
+    printf("Skipping NUC type: %s", NucTypeToString(nuc).c_str());
+    return false;
+  }
+
+  return true;
+}
+
+TEST_F(DeviceEnumerationTest, Nuc7i5DNBTest) {
+  if (!CheckTestMatch(NucType::NUC7i5DNB)) {
+    return;
+  }
+
   static const char* kDevicePaths[] = {
       "sys/platform/pt/PCI0/bus/00:02.0_/pci-00:02.0-fidl/intel_i915/intel-gpu-core",
       "sys/platform/pt/PCI0/bus/00:02.0_/pci-00:02.0-fidl/intel_i915/intel-display-controller/display-controller",
