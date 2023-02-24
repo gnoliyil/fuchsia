f94134665b15a98c58d17e8e56b8994cd447072d
David Fang <fangism@google.com>
2023-02-24 01:53:35 +0000

    [rbe][rust] Use re-client built-in write-if-change
    For Rust.
    
    Rewrapper 0.92 now comes with a --preserve_unchanged_output_mtime
    option to suppress downloading over unchanged outputs, which lets
    ninja with restat=true prune downstream actions in incremental builds.
    
    The major benefit is that the remote rust command is no longer
    differentiated by `restat_rust=true/false`, thus combining the once
    silo'd remote cache for the same action.
    
    To make restat_rust behavior consistent between remote and local execution
    (which can come from --exec_strategy={local,remote_local_fallback,racing}),
    we must ensure that local execution does not unconditionally overwrite
    the output file as well, and thus still need the output_cacher wrapper
    script to be used as a --local_wrapper (new option in rewrapper 0.93).
    
    The --local_wrapper argument is limited to a single shell token,
    therefore the output_cacher invocation prefix must be written
    to a temporary script first.  This is done in
    output_cacher_local_wrapper.sh.  This maintains wrapper composability
    with action_tracer.py by hiding shell syntax and logic inside this
    wrapper.
    
    Change-Id: I600440599204f04153332255896f5b1db4e756a4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/773326
    Reviewed-by: Dan Johnson <computerdruid@google.com>
    Reviewed-by: David Turner <digit@google.com>
    Commit-Queue: David Fang <fangism@google.com>

---
+++ build/tracer/output_cacher_local_wrapper.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+# Copyright 2022 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This script expects args in the form of three command slices, separated by --:
+#   outer_wrapper ... -- RBE_wrapper ... -- command ...
+#
+# This writes the outer_wrapper command prefix into a temporary file
+# and passes that through --local_wrapper to the RBE_wrapper (which accepts
+# rewrapper options).  --local_wrapper is limited to a single shell token.
+# This script could have been implemented pure using shell syntax however,
+# to make it composable with other out wrapper (like the action tracer),
+# we hide all logic and syntax inside this script.
+
+# The first command slice is the outer wrapper.
+# An example of such a wrapper is build/tracer/output_cacher.py.
+outer_wrapper_command=()
+for arg
+do
+  case "$arg" in
+    --) shift; break ;;
+    *) outer_wrapper_command+=( "$arg" ) ;;
+  esac
+  shift
+done
+
+# The second command slice is an rewrapper-like command.
+# This could be `rewrapper` itself, or another wrapper script like
+# `fuchsia-rbe-action.sh`, `rustc-remote-wrapper.sh`,
+# `cxx-remote-wrapper.sh`.
+rewrapper_command_prefix=()
+
+for arg
+do
+  case "$arg" in
+    --) shift; break ;;
+    *) rewrapper_command_prefix+=( "$arg" ) ;;
+  esac
+  shift
+done
+
+primary_command=("$@")
+
+# Find a suitable temporary file name, based on declared outputs.
+# output_cacher.py --outputs takes repeated arguments until the next flag.
+looking_at_output=0
+output_candidates=()
+for arg in "${outer_wrapper_command[@]}"
+do
+  case "$arg" in
+    --outputs) looking_at_output=1 ;;
+    -- | --*) looking_at_output=0 ;;
+    *) if [[ "$looking_at_output" == 1 ]] ; then
+      output_candidates+=( "$arg" )
+      fi
+      ;;
+  esac
+done
+
+output="${output_candidates[0]}"
+# Extract "FILENAME" from "substitute_after:flag:FILENAME".
+case "$output" in
+  substitute_after:*) output="${output#substitute_after:*:}" ;;
+esac
+
+
+# Write the outer_wrapper_command into a file so that it can be
+# invoked by rewrapper --local_wrapper as a single shell token.
+# This is equivalent to forwarding all tokens to the outer wrapper.
+# @Q protects tokens with spaces by single-quoting them.
+local_wrapper="$output".wrap.sh
+cat > "$local_wrapper" <<EOF
+#!/bin/sh
+${outer_wrapper_command[@]@Q} -- "\$@"
+EOF
+chmod +x "$local_wrapper"
+
+function cleanup() {
+  rm -f "$local_wrapper"
+}
+
+trap cleanup EXIT
+
+"${rewrapper_command_prefix[@]}" --local_wrapper="$local_wrapper" -- "${primary_command[@]}"
