46e5d466ea15571bc78af04d5fef6e9a7984bd0e
Pavan Kumar Juturu <jpavankumar@google.com>
2023-02-27 20:58:26 +0000

    [HoneyDew] - Adding FuchsiaDeviceBase default implementation - Part 3
    Change-Id: Ia028e1b63c0c5b4b833a245249c3b2d6d13e4b91
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/807807
    Reviewed-by: Jeremiah Griffin <jzgriffin@google.com>
    Commit-Queue: Pavan Kumar Juturu <jpavankumar@google.com>
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Reviewed-by: Kevin Cho <chok@google.com>

--- src/testing/end_to_end/honeydew/device_classes/fuchsia_device_base.py
+++ src/testing/end_to_end/honeydew/device_classes/fuchsia_device_base.py
@@ -4,10 +4,14 @@
 # found in the LICENSE file.
 """Default implementation of FuchsiaDevice abstract base class."""
 
+import base64
 import logging
+import os
 import subprocess
 import time
+from datetime import datetime
 from functools import lru_cache
+from http.client import RemoteDisconnected
 from typing import Any, Dict, Iterable, Optional, Type
 
 from honeydew import custom_types, errors
@@ -29,9 +33,16 @@ _SL4F_METHODS = {
     "GetDeviceInfo": "hwinfo_facade.HwinfoGetDeviceInfo",
     "GetProductInfo": "hwinfo_facade.HwinfoGetProductInfo",
     "GetVersion": "device_facade.GetVersion",
+    "LogError": "logging_facade.LogErr",
+    "LogInfo": "logging_facade.LogInfo",
+    "LogWarning": "logging_facade.LogWarn",
+    "Reboot": "hardware_power_statecontrol_facade.SuspendReboot",
+    "Snapshot": "feedback_data_provider_facade.GetSnapshot",
 }
 
 _TIMEOUTS = {
+    "BOOT_UP_COMPLETE": 60,
+    "OFFLINE": 60,
     "SL4F_RESPONSE": 30,
     "SSH_COMMAND_ARG": 3,
     "SSH_COMMAND_RESPONSE": 60,
@@ -175,7 +186,7 @@ class FuchsiaDeviceBase(fuchsia_device.FuchsiaDevice,
     # List all the public methods in alphabetical order
     def close(self) -> None:
         """Clean up method."""
-        raise NotImplementedError
+        return
 
     def log_message_to_device(self, message: str, level: custom_types.LEVEL):
         """Log message to fuchsia device at specified level.
@@ -183,29 +194,68 @@ class FuchsiaDeviceBase(fuchsia_device.FuchsiaDevice,
         Args:
             message: Message that need to logged.
             level: Log message level.
+
+        Raises:
+            errors.FuchsiaDeviceError: On failure.
         """
-        raise NotImplementedError
+        self._send_sl4f_command(
+            method=_SL4F_METHODS[f"Log{level.name.capitalize()}"],
+            params={"message": message})
 
     def reboot(self) -> None:
-        """Soft reboot the device."""
-        raise NotImplementedError
+        """Soft reboot the device.
+
+        Raises:
+            errors.FuchsiaDeviceError: On failure.
+        """
+        _LOGGER.info("Rebooting %s...", self.name)
+        self._send_sl4f_command(
+            method=_SL4F_METHODS["Reboot"],
+            exceptions_to_skip=[RemoteDisconnected])
+        self._wait_for_offline()
+        self._wait_for_bootup_complete()
 
     def snapshot(
             self, directory: str, snapshot_file: Optional[str] = None) -> str:
         """Captures the snapshot of the device.
 
         Args:
-            directory: Absolute path on the host where snapshot file need
-                to be saved.
+            directory: Absolute path on the host where snapshot file will be
+                saved. If this directory does not exist, this method will create
+                it.
 
-            snapshot_file: Name of the file to be used to save snapshot file.
+            snapshot_file: Name of the output snapshot file.
                 If not provided, API will create a name using
                 "Snapshot_{device_name}_{'%Y-%m-%d-%I-%M-%S-%p'}" format.
 
         Returns:
             Absolute path of the snapshot file.
+
+        Raises:
+            errors.FuchsiaDeviceError: On failure.
         """
-        raise NotImplementedError
+        directory = os.path.abspath(directory)
+        try:
+            os.makedirs(directory)
+        except FileExistsError:
+            pass
+
+        if not snapshot_file:
+            timestamp = datetime.now().strftime("%Y-%m-%d-%I-%M-%S-%p")
+            snapshot_file = f"Snapshot_{self.name}_{timestamp}.zip"
+        snapshot_file_path = os.path.join(directory, snapshot_file)
+
+        snapshot_resp = self._send_sl4f_command(
+            method=_SL4F_METHODS["Snapshot"])
+        snapshot_base64_encoded_str = snapshot_resp["result"]["zip"]
+        snapshot_base64_decoded_bytes = base64.b64decode(
+            snapshot_base64_encoded_str)
+
+        with open(snapshot_file_path, "wb") as snapshot_binary_zip:
+            snapshot_binary_zip.write(snapshot_base64_decoded_bytes)
+
+        _LOGGER.info("Snapshot file has been saved @ '%s'", snapshot_file_path)
+        return snapshot_file_path
 
     # List all private methods in alphabetical order
     def _check_sl4f_connection(self):
@@ -221,6 +271,73 @@ class FuchsiaDeviceBase(fuchsia_device.FuchsiaDevice,
         if device_name != self.name:
             raise errors.FuchsiaDeviceError(
                 f"Failed to start SL4F server on '{self.name}'.")
+
+    def _check_ssh_connection_to_device(self, timeout: float):
+        """Checks the SSH connection from host to Fuchsia device.
+
+        Args:
+            timeout: How long in sec to wait for SSH connection.
+
+        Raises:
+            errors.FuchsiaDeviceError: If fails to establish SSH connection.
+        """
+        start_time = time.time()
+        end_time = start_time + timeout
+
+        _LOGGER.info("Waiting for %s to allow ssh connection...", self.name)
+        while time.time() < end_time:
+            try:
+                self._run_ssh_command_on_host(command=_CMDS['ECHO'])
+                break
+            except Exception:  # pylint: disable=broad-except
+                time.sleep(1)
+        else:
+            raise errors.FuchsiaDeviceError(
+                f"Failed to connect to '{self.name}' via SSH.")
+        _LOGGER.info("%s is available via ssh.", self.name)
+
+    def _get_device_ip_address(self, timeout: float):
+        """Returns the device IP(V4|V6) address used for SSHing from host.
+
+        Args:
+            timeout: How long in sec to retry for ip address.
+
+        Raises:
+            errors.FuchsiaDeviceError: If fails to gets the ip address.
+        """
+        start_time = time.time()
+        end_time = start_time + timeout
+
+        while time.time() < end_time:
+            try:
+                return ffx_cli.get_target_address(self.name, timeout=timeout)
+            except Exception:  # pylint: disable=broad-except
+                time.sleep(1)
+        raise errors.FuchsiaDeviceError(
+            f"Failed to get the ip address of '{self.name}'.")
+
+    def _ping_device(self, timeout: float):
+        """Pings Fuchsia device from the host.
+
+        Args:
+            timeout: How long in sec to wait for ping to succeed.
+
+        Raises:
+            errors.FuchsiaDeviceError: If ping is not successful.
+        """
+        start_time = time.time()
+        end_time = start_time + timeout
+
+        # wait until device is responsive to pings
+        _LOGGER.info("Waiting for %s to become pingable...", self.name)
+        while time.time() < end_time:
+            if host_utils.is_pingable(self._ip_address):
+                break
+            time.sleep(1)
+        else:
+            raise errors.FuchsiaDeviceError(
+                f"'{self.name}' failed to become pingable in {timeout}sec.")
+        _LOGGER.info("%s now pingable.", self.name)
 
     @property
     @lru_cache
@@ -251,7 +368,7 @@ class FuchsiaDeviceBase(fuchsia_device.FuchsiaDevice,
             SSH command output.
 
         Raises:
-            CalledProcessError: On failure.
+            subprocess.CalledProcessError: On failure.
         """
         ssh_command = _SSH_COMMAND.format(
             ssh_options=_SSH_OPTIONS,
@@ -293,10 +410,10 @@ class FuchsiaDeviceBase(fuchsia_device.FuchsiaDevice,
         Raises:
             errors.FuchsiaDeviceError: On failure.
         """
-        if params is None:
+        if not params:
             params = {}
 
-        if exceptions_to_skip is None:
+        if not exceptions_to_skip:
             exceptions_to_skip = []
 
         # pylint: disable=protected-access
@@ -332,7 +449,7 @@ class FuchsiaDeviceBase(fuchsia_device.FuchsiaDevice,
                         "iteration %s/%s", method, error, attempt, attempts)
                     continue
                 else:
-                    exception_msg += f" Error: '{error}'."
+                    exception_msg = f"{exception_msg} Error: '{error}'."
                     break
 
             except Exception as err:
@@ -350,3 +467,56 @@ class FuchsiaDeviceBase(fuchsia_device.FuchsiaDevice,
 
         # verify the device is responsive to SL4F requests
         self._check_sl4f_connection()
+
+    def _wait_for_bootup_complete(
+            self, timeout: float = _TIMEOUTS["BOOT_UP_COMPLETE"]) -> None:
+        """Wait for Fuchsia device to complete the boot.
+
+        Args:
+            timeout: How long in sec to wait for bootup.
+
+        Raises:
+            errors.FuchsiaDeviceError: If bootup operation(s) fail.
+        """
+        end_time = time.time() + timeout
+
+        # IP address may change on reboot. So get the ip address once again
+        self._ip_address = self._get_device_ip_address(timeout)
+
+        # wait until device is responsive to pings
+        remaining_timeout = max(0, end_time - time.time())
+        self._ping_device(remaining_timeout)
+
+        # wait until device to allow ssh connection
+        remaining_timeout = max(0, end_time - time.time())
+        self._check_ssh_connection_to_device(remaining_timeout)
+
+        # Restart SL4F server on the device
+        self._start_sl4f_server()
+
+    def _wait_for_offline(self, timeout: float = _TIMEOUTS["OFFLINE"]) -> None:
+        """Wait for Fuchsia device to go offline.
+
+        Args:
+            timeout: How long in sec to wait for device to go offline.
+
+        Raises:
+            errors.FuchsiaDeviceError: If device is not offline.
+        """
+        # wait until device is not responsive to pings
+        _LOGGER.info("Waiting for %s to go offline...", self.name)
+        start_time = time.time()
+        end_time = start_time + timeout
+        count = 0
+        while time.time() < end_time:
+            if not host_utils.is_pingable(self._ip_address):
+                count += 1  # Ensure device is really offline not just a blip
+            else:
+                count = 0
+            if count == 3:
+                _LOGGER.info("%s is offline.", self.name)
+                break
+            time.sleep(1)
+        else:
+            raise errors.FuchsiaDeviceError(
+                f"'{self.name}' failed to go offline in {timeout}sec.")
--- src/testing/end_to_end/honeydew/tests/unit_tests/device_class_tests/fuchsia_device_base_test.py
+++ src/testing/end_to_end/honeydew/tests/unit_tests/device_class_tests/fuchsia_device_base_test.py
@@ -4,7 +4,10 @@
 # found in the LICENSE file.
 """Unit tests for honeydew.device_classes.fuchsia_device_base.py."""
 
+import base64
+import subprocess
 import unittest
+from http.client import RemoteDisconnected
 from unittest import mock
 
 from honeydew import errors
@@ -34,6 +37,8 @@ _MOCK_ARGS = {
         'error': 'some error',
     },
 }
+
+_BASE64_ENCODED_STR = "some base64 encoded string=="
 
 
 def _custom_test_name_func(testcase_func, _, param):
@@ -263,6 +268,134 @@ class FuchsiaDeviceBaseTests(unittest.TestCase):
         mock_send_sl4f_command.assert_called_once_with(
             self.fd_obj, method=fuchsia_device_base._SL4F_METHODS["GetVersion"])
 
+    # List all the tests related to public methods in alphabetical order
+    def test_close(self):
+        """Testcase for FuchsiaDeviceBase.close()"""
+        self.fd_obj.close()
+
+    @parameterized.expand(
+        [
+            (
+                {
+                    "label": "info_level",
+                    "log_level": fuchsia_device_base.custom_types.LEVEL.INFO,
+                    "sl4f_method": "LogInfo",
+                    "log_message": "info message",
+                },),
+            (
+                {
+                    "label": "warning_level",
+                    "log_level": fuchsia_device_base.custom_types.LEVEL.WARNING,
+                    "sl4f_method": "LogWarning",
+                    "log_message": "warning message",
+                },),
+            (
+                {
+                    "label": "error_level",
+                    "log_level": fuchsia_device_base.custom_types.LEVEL.ERROR,
+                    "sl4f_method": "LogError",
+                    "log_message": "error message",
+                },),
+        ],
+        name_func=_custom_test_name_func)
+    @mock.patch.object(
+        fuchsia_device_base.FuchsiaDeviceBase,
+        "_send_sl4f_command",
+        autospec=True)
+    def test_log_message_to_device(
+            self, parameterized_dict, mock_send_sl4f_command):
+        """Testcase for FuchsiaDeviceBase.log_message_to_device()"""
+        self.fd_obj.log_message_to_device(
+            level=parameterized_dict["log_level"],
+            message=parameterized_dict["log_message"])
+
+        mock_send_sl4f_command.assert_called_once_with(
+            self.fd_obj,
+            method=fuchsia_device_base._SL4F_METHODS[
+                parameterized_dict["sl4f_method"]],
+            params={"message": parameterized_dict["log_message"]})
+
+    @mock.patch.object(
+        fuchsia_device_base.FuchsiaDeviceBase,
+        "_wait_for_bootup_complete",
+        autospec=True)
+    @mock.patch.object(
+        fuchsia_device_base.FuchsiaDeviceBase,
+        "_wait_for_offline",
+        autospec=True)
+    @mock.patch.object(
+        fuchsia_device_base.FuchsiaDeviceBase,
+        "_send_sl4f_command",
+        autospec=True)
+    def test_reboot(
+            self, mock_send_sl4f_command, mock_wait_for_offline,
+            mock_wait_for_bootup_complete):
+        """Testcase for FuchsiaDeviceBase.reboot()"""
+        self.fd_obj.reboot()
+
+        mock_send_sl4f_command.assert_called_once_with(
+            self.fd_obj,
+            method=fuchsia_device_base._SL4F_METHODS["Reboot"],
+            exceptions_to_skip=[RemoteDisconnected])
+
+        mock_wait_for_offline.assert_called_once()
+
+        mock_wait_for_bootup_complete.assert_called_once()
+
+    @parameterized.expand(
+        [
+            (
+                {
+                    "label": "no_snapshot_file_arg",
+                    "directory": "/tmp",
+                    "optional_params": {},
+                },),
+            (
+                {
+                    "label": "snapshot_file_arg",
+                    "directory": "/tmp",
+                    "optional_params": {
+                        "snapshot_file": "snapshot.zip",
+                    },
+                },),
+        ],
+        name_func=_custom_test_name_func)
+    @mock.patch.object(
+        fuchsia_device_base.FuchsiaDeviceBase,
+        "_send_sl4f_command",
+        return_value={"result": {
+            "zip": _BASE64_ENCODED_STR
+        }},
+        autospec=True)
+    @mock.patch.object(fuchsia_device_base.os, "makedirs", autospec=True)
+    def test_snapshot(
+            self, parameterized_dict, mock_makedirs, mock_send_sl4f_command):
+        """Testcase for FuchsiaDeviceBase.snapshot()"""
+        directory = parameterized_dict["directory"]
+        optional_params = parameterized_dict["optional_params"]
+
+        with mock.patch("builtins.open", mock.mock_open()) as mocked_file:
+            snapshot_file_path = self.fd_obj.snapshot(
+                directory=directory, **optional_params)
+
+        if "snapshot_file" in optional_params:
+            self.assertEqual(
+                snapshot_file_path,
+                f"{directory}/{optional_params['snapshot_file']}")
+        else:
+            self.assertRegex(
+                snapshot_file_path,
+                f"{directory}/Snapshot_{self.fd_obj.name}_.*.zip")
+
+        mocked_file.assert_called_once_with(snapshot_file_path, 'wb')
+        mocked_file().write.assert_called_once_with(
+            base64.b64decode(_BASE64_ENCODED_STR))
+
+        mock_makedirs.assert_called_once_with(directory)
+
+        mock_send_sl4f_command.assert_called_once_with(
+            self.fd_obj, method=fuchsia_device_base._SL4F_METHODS["Snapshot"])
+
     # List all the tests related to private methods in alphabetical order
 
     # Note - Test case for FuchsiaDeviceBase._start_sl4f_server() is covered in
@@ -280,6 +413,101 @@ class FuchsiaDeviceBaseTests(unittest.TestCase):
         mock_send_sl4f_command.assert_called_once_with(
             self.fd_obj,
             method=fuchsia_device_base._SL4F_METHODS["GetDeviceName"])
+
+    @mock.patch("time.sleep", autospec=True)
+    @mock.patch.object(
+        fuchsia_device_base.FuchsiaDeviceBase,
+        "_run_ssh_command_on_host",
+        side_effect=[subprocess.CalledProcessError, b'some output'],
+        autospec=True)
+    def test_check_ssh_connection_to_device_success(
+            self, mock_run_ssh_command_on_host, mock_sleep):
+        """Testcase for FuchsiaDeviceBase._check_ssh_connection_to_device()
+        success case"""
+        self.fd_obj._check_ssh_connection_to_device(timeout=5)
+
+        mock_run_ssh_command_on_host.assert_called_with(
+            self.fd_obj, command=fuchsia_device_base._CMDS["ECHO"])
+        mock_sleep.assert_called_with(1)
+
+    @mock.patch.object(
+        fuchsia_device_base.FuchsiaDeviceBase,
+        "_run_ssh_command_on_host",
+        side_effect=subprocess.CalledProcessError,
+        autospec=True)
+    def test_check_ssh_connection_to_device_fail(
+            self, mock_run_ssh_command_on_host):
+        """Testcase for FuchsiaDeviceBase._check_ssh_connection_to_device()
+        failure case"""
+        with self.assertRaisesRegex(
+                fuchsia_device_base.errors.FuchsiaDeviceError,
+                f"Failed to connect to '{self.fd_obj.name}' via SSH."):
+            self.fd_obj._check_ssh_connection_to_device(timeout=2)
+
+        mock_run_ssh_command_on_host.assert_called_with(
+            self.fd_obj, command=fuchsia_device_base._CMDS["ECHO"])
+
+    @mock.patch("time.sleep", autospec=True)
+    @mock.patch.object(
+        fuchsia_device_base.ffx_cli,
+        "get_target_address",
+        side_effect=[
+            errors.FuchsiaDeviceError, _MOCK_ARGS["device_ip_address"]
+        ],
+        autospec=True)
+    def test_get_device_ip_address_success(
+            self, mock_get_target_address, mock_sleep):
+        """Testcase for FuchsiaDeviceBase._get_device_ip_address() success
+        case"""
+        self.fd_obj._get_device_ip_address(timeout=5)
+
+        mock_get_target_address.assert_called_with(self.fd_obj.name, timeout=5)
+        mock_sleep.assert_called_with(1)
+
+    @mock.patch.object(
+        fuchsia_device_base.ffx_cli,
+        "get_target_address",
+        side_effect=errors.FuchsiaDeviceError,
+        autospec=True)
+    def test_get_device_ip_address_fail(self, mock_get_target_address):
+        """Testcase for FuchsiaDeviceBase._get_device_ip_address() failure
+        case"""
+
+        with self.assertRaisesRegex(
+                fuchsia_device_base.errors.FuchsiaDeviceError,
+                f"Failed to get the ip address of '{self.fd_obj.name}'."):
+            self.fd_obj._get_device_ip_address(timeout=2)
+
+        mock_get_target_address.assert_called_with(self.fd_obj.name, timeout=2)
+
+    @mock.patch("time.sleep", autospec=True)
+    @mock.patch.object(
+        fuchsia_device_base.host_utils,
+        "is_pingable",
+        side_effect=[False, False, True],
+        autospec=True)
+    def test_ping_device_success(self, mock_is_pingable, mock_sleep):
+        """Testcase for FuchsiaDeviceBase._ping_device() success
+        case"""
+        self.fd_obj._ping_device(timeout=5)
+
+        mock_is_pingable.assert_called_with(self.fd_obj._ip_address)
+        mock_sleep.assert_called_with(1)
+
+    @mock.patch.object(
+        fuchsia_device_base.host_utils,
+        "is_pingable",
+        return_value=False,
+        autospec=True)
+    def test_ping_device_fail(self, mock_is_pingable):
+        """Testcase for FuchsiaDeviceBase._ping_device() failure
+        case"""
+        with self.assertRaisesRegex(
+                fuchsia_device_base.errors.FuchsiaDeviceError,
+                f"'{self.fd_obj.name}' failed to become pingable in 2sec."):
+            self.fd_obj._ping_device(timeout=2)
+
+        mock_is_pingable.assert_called_with(self.fd_obj._ip_address)
 
     @mock.patch.object(
         fuchsia_device_base.subprocess,
@@ -369,6 +597,60 @@ class FuchsiaDeviceBaseTests(unittest.TestCase):
                 method=method, attempts=5, interval=0)
 
         mock_send_http_request.assert_called_once()
+
+    @mock.patch.object(
+        fuchsia_device_base.FuchsiaDeviceBase,
+        "_start_sl4f_server",
+        autospec=True)
+    @mock.patch.object(
+        fuchsia_device_base.FuchsiaDeviceBase,
+        "_check_ssh_connection_to_device",
+        autospec=True)
+    @mock.patch.object(
+        fuchsia_device_base.FuchsiaDeviceBase, "_ping_device", autospec=True)
+    @mock.patch.object(
+        fuchsia_device_base.FuchsiaDeviceBase,
+        "_get_device_ip_address",
+        autospec=True)
+    def test_wait_for_bootup_complete(
+            self, mock_get_device_ip_address, mock_ping_device,
+            mock_check_ssh_connection_to_device, mock_start_sl4f_server):
+        """Testcase for FuchsiaDeviceBase._wait_for_bootup_complete()"""
+        self.fd_obj._wait_for_bootup_complete(timeout=10)
+
+        mock_get_device_ip_address.assert_called_once()
+        mock_ping_device.assert_called_once()
+        mock_check_ssh_connection_to_device.assert_called_once()
+        mock_start_sl4f_server.assert_called_once()
+
+    @mock.patch("time.sleep", autospec=True)
+    @mock.patch.object(
+        fuchsia_device_base.host_utils,
+        "is_pingable",
+        side_effect=[
+            True, False, True, False, False, True, False, False, False
+        ],
+        autospec=True)
+    def test_wait_for_offline_success(self, mock_is_pingable, mock_sleep):
+        """Testcase for FuchsiaDeviceBase._wait_for_offline() success case"""
+        self.fd_obj._wait_for_offline()
+
+        mock_is_pingable.assert_called_with(self.fd_obj._ip_address)
+        mock_sleep.assert_called_with(1)
+
+    @mock.patch.object(
+        fuchsia_device_base.host_utils,
+        "is_pingable",
+        return_value=True,
+        autospec=True)
+    def test_wait_for_offline_fail(self, mock_is_pingable):
+        """Testcase for FuchsiaDeviceBase._wait_for_offline() failure case"""
+        with self.assertRaisesRegex(
+                fuchsia_device_base.errors.FuchsiaDeviceError,
+                "failed to go offline"):
+            self.fd_obj._wait_for_offline(timeout=2)
+
+        mock_is_pingable.assert_called_with(self.fd_obj._ip_address)
 
 
 if __name__ == '__main__':
