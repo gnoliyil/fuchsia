852f1fd64c18c4920fa9b479fc9fadabbd478903
Tony Gong <gongt@google.com>
2023-02-27 16:52:24 +0000

    [network/tests] Add DHCPv[46] netemul-guest tests
    Reland what was removed in https://fxrev.dev/667707 now that netemul
    guests are supported in CFv2 (see https://fxrev.dev/761264).
    
    The two tests reintroduced are:
    1. a test ensuring Netstack2's in-stack DHCPv4 client acquires an
      address; and
    2. a test ensuring DHCPv6 client can obtain DNS servers in stateless
      mode.
    
    Add a GN template for creating a `fuchsia_test_package` containing
    netemul-guest tests and use it for the added tests and the
    netemul-guest self test.
    
    Test: fx test netstack-guest-integration-test
    Multiply: netstack-guest-integration-test
    Fixed: 119208
    Change-Id: I1c09aff0630d6a94546215ea781cdac33dcc63ed
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/796205
    Reviewed-by: Bruno Dal Bo <brunodalbo@google.com>
    Reviewed-by: Nick Brown <nickbrow@google.com>
    Reviewed-by: Tamir Duberstein <tamird@google.com>
    Commit-Queue: Tony Gong <gongt@google.com>

---
+++ src/connectivity/network/tests/integration/guest/data/dhcp_setup.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+#
+# Copyright 2023 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -Eeuxo pipefail
+
+# The IPv4 address must be in the subnet declared in
+# `//src/connectivity/network/tests/integration/guest/dhcpv4/data/dhcpd.conf`.
+# Likewise the IPv6 address must be in the subnet declared in
+# `//src/connectivity/network/tests/integration/guest/dhcpv6/data/dhcpd.conf`.
+declare -Ar ADDRS=( [4]="192.0.2.1/24" [6]="2001:db8::1/64" )
+readonly DHCP_CONFIG_FILE="/etc/default/isc-dhcp-server"
+
+# Mask and stop NetworkManager to prevent interference from the DHCPv4 client
+# that it runs.
+systemctl mask NetworkManager.service
+systemctl stop NetworkManager.service
+# Without the following stop, it's possible for this script's attempt to start
+# the service to race with the one started when the guest is booted.
+systemctl stop isc-dhcp-server.service
+
+declare -a ip
+
+# Parse arguments:
+#   -4 enables DHCPv4 server.
+#   -6 enables DHCPv6 server.
+while getopts "46" arg; do
+  case "${arg}" in
+    4)
+      ip+=(4)
+      ;;
+    6)
+      ip+=(6)
+      ;;
+    *)
+      echo "usage: ${0} [-46]" >&2
+      exit 1
+      ;;
+  esac
+done
+
+# If run without an IP version argument, start server with both DHCPv4 and
+# DHCPv6 enabled.
+if [[ "${#ip[@]}" == 0 ]]; then
+  ip=(4 6)
+fi
+readonly ip
+
+# The ethernet interface's name varies depending on the number of block devices
+# that are added when starting the guest.  Discover the interface name prior to
+# configuration.
+iface_name=""
+
+for iface_path in /sys/class/net/*; do
+  candidate=$(basename "${iface_path}")
+  if [[ "${candidate}" != "lo" ]]; then
+    iface_name="${candidate}"
+    readonly iface_name
+    break
+  fi
+done
+
+if [[ -z "${iface_name}" ]]; then
+  echo "Unable to find an interface other than loopback" >&2
+  exit 1
+fi
+
+# Overwrite the default configuration file to enable DHCPv4 or DHCPv6 on the
+# selected interface as appropriate.
+#
+# The interface also needs an IP address or else isc-dhcp-server will not
+# start.
+rm "${DHCP_CONFIG_FILE}"
+for ip_version in "${ip[@]}"; do
+  echo "INTERFACESv${ip_version}=\"${iface_name}\"" >> "${DHCP_CONFIG_FILE}"
+  ip addr add dev "${iface_name}" "${ADDRS[${ip_version}]}"
+done
+
+ip link set "${iface_name}" up
+
+# Restart isc-dhcp-server because it was started at boot.
+if ! systemctl restart isc-dhcp-server; then
+  # If restarting fails, print out the logs to aid in troubleshooting.
+  journalctl -b --no-pager -u isc-dhcp-server >&2
+  exit 1
+fi
+systemctl is-active --quiet isc-dhcp-server
