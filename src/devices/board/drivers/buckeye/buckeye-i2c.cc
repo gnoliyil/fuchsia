aa971608d35d53d661e72962458590726968ab72
Braden Kell <bradenkell@google.com>
2023-02-28 22:56:59 +0000

    [board][av400][buckeye][clover] Add one I2C bus device per controller
    Bug: 120971
    Test: CQ
    Change-Id: I5e0d777568cecd6147c0484199ac03b54520f681
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/811530
    Reviewed-by: Gurjant Kalsi <gkalsi@google.com>
    Commit-Queue: Braden Kell <bradenkell@google.com>

--- src/devices/board/drivers/buckeye/buckeye-i2c.cc
+++ src/devices/board/drivers/buckeye/buckeye-i2c.cc
@@ -10,6 +10,9 @@
 #include <lib/ddk/platform-defs.h>
 #include <limits.h>
 
+#include <span>
+#include <vector>
+
 #include <soc/aml-a5/a5-hw.h>
 
 #include "buckeye-gpios.h"
@@ -21,72 +24,46 @@ namespace buckeye {
 namespace fpbus = fuchsia_hardware_platform_bus;
 using i2c_channel_t = fidl_metadata::i2c::Channel;
 
-static const std::vector<fpbus::Mmio> i2c_mmios{
-    {{
-        .base = A5_I2C_A_BASE,
-        .length = A5_I2C_LENGTH,
-    }},
-    {{
-        .base = A5_I2C_B_BASE,
-        .length = A5_I2C_LENGTH,
-    }},
-    {{
-        .base = A5_I2C_C_BASE,
-        .length = A5_I2C_LENGTH,
-    }},
-    {{
-        .base = A5_I2C_D_BASE,
-        .length = A5_I2C_LENGTH,
-    }},
+struct I2cBus {
+  zx_paddr_t mmio;
+  uint32_t irq;
+  cpp20::span<const i2c_channel_t> channels;
 };
 
-static const std::vector<fpbus::Irq> i2c_irqs{
-    {{
-        .irq = A5_I2C_A_IRQ,
-        .mode = ZX_INTERRUPT_MODE_EDGE_HIGH,
-    }},
-    {{
-        .irq = A5_I2C_B_IRQ,
-        .mode = ZX_INTERRUPT_MODE_EDGE_HIGH,
-    }},
-    {{
-        .irq = A5_I2C_C_IRQ,
-        .mode = ZX_INTERRUPT_MODE_EDGE_HIGH,
-    }},
-    {{
-        .irq = A5_I2C_D_IRQ,
-        .mode = ZX_INTERRUPT_MODE_EDGE_HIGH,
-    }},
-};
-
-static const i2c_channel_t i2c_channels[] = {
+constexpr i2c_channel_t i2c_a_channels[]{
     // 0 - I2C_A: PMIC
     {
-        .bus_id = 0,
         .address = 0x40,
         .vid = 0,
         .pid = 0,
         .did = 0,
         .name = "pmic",
     },
+};
+
+constexpr i2c_channel_t i2c_b_channels[]{
     // 1 - I2C_B: Type-C CC controller
     {
-        .bus_id = 1,
         .address = 0x61,
         .vid = 0,
         .pid = 0,
         .did = 0,
         .name = "usb",
     },
+};
+
+constexpr i2c_channel_t i2c_c_channels[]{
     // 2 - I2C_C: Temperature sensor
-    {.bus_id = 2, .address = 0x48, .vid = 0, .pid = 0, .did = 0, .name = "temp"},
+    {.address = 0x48, .vid = 0, .pid = 0, .did = 0, .name = "temp"},
     // 3 - I2C_C: Woofer codec
-    {.bus_id = 2, .address = 0x3C, .vid = 0, .pid = 0, .did = 0, .name = "woofer"},
+    {.address = 0x3C, .vid = 0, .pid = 0, .did = 0, .name = "woofer"},
     // 4 - I2C_C: SHTV3 temperature sensor
-    {.bus_id = 2, .address = 0x70, .vid = 0, .pid = 0, .did = 0, .name = "shtv3"},
+    {.address = 0x70, .vid = 0, .pid = 0, .did = 0, .name = "shtv3"},
+};
+
+constexpr i2c_channel_t i2c_d_channels[]{
     // 5 - I2C_D: Ambient light sensor
     {
-        .bus_id = 3,
         .address = 0x29,
         .vid = 0,
         .pid = 0,
@@ -95,26 +72,67 @@ static const i2c_channel_t i2c_channels[] = {
     },
     // 6 - I2C_D: Ambient light sensor
     {
-        .bus_id = 3,
         .address = 0x39,
         .vid = 0,
         .pid = 0,
         .did = 0,
         .name = "als2",
     },
-
 };
 
-zx_status_t Buckeye::I2cInit() {
+constexpr I2cBus buses[]{
+    {
+        .mmio = A5_I2C_A_BASE,
+        .irq = A5_I2C_A_IRQ,
+        .channels{i2c_a_channels, std::size(i2c_a_channels)},
+    },
+    {
+        .mmio = A5_I2C_B_BASE,
+        .irq = A5_I2C_B_IRQ,
+        .channels{i2c_b_channels, std::size(i2c_b_channels)},
+    },
+    {
+        .mmio = A5_I2C_C_BASE,
+        .irq = A5_I2C_C_IRQ,
+        .channels{i2c_c_channels, std::size(i2c_c_channels)},
+    },
+    {
+        .mmio = A5_I2C_D_BASE,
+        .irq = A5_I2C_D_IRQ,
+        .channels{i2c_d_channels, std::size(i2c_d_channels)},
+    },
+};
+
+zx_status_t AddI2cBus(const uint32_t bus_id, const I2cBus& bus,
+                      const fdf::WireSyncClient<fuchsia_hardware_platform_bus::PlatformBus>& pbus) {
+  const std::vector<fpbus::Mmio> i2c_mmios{
+      {{
+          .base = bus.mmio,
+          .length = A5_I2C_LENGTH,
+      }},
+  };
+
+  const std::vector<fpbus::Irq> i2c_irqs{
+      {{
+          .irq = bus.irq,
+          .mode = ZX_INTERRUPT_MODE_EDGE_HIGH,
+      }},
+
+  };
+
+  char name[32];
+  snprintf(name, sizeof(name), "i2c-%u", bus_id);
+
   fpbus::Node i2c_dev;
-  i2c_dev.name() = "i2c";
+  i2c_dev.name() = name;
   i2c_dev.vid() = PDEV_VID_AMLOGIC;
   i2c_dev.pid() = PDEV_PID_GENERIC;
   i2c_dev.did() = PDEV_DID_AMLOGIC_I2C;
+  i2c_dev.instance_id() = bus_id;
   i2c_dev.mmio() = i2c_mmios;
   i2c_dev.irq() = i2c_irqs;
 
-  auto i2c_status = fidl_metadata::i2c::I2CChannelsToFidl(i2c_channels);
+  auto i2c_status = fidl_metadata::i2c::I2CChannelsToFidl(bus.channels);
   if (i2c_status.is_error()) {
     zxlogf(ERROR, "I2cInit: Failed to fidl encode i2c channels: %d", i2c_status.error_value());
     return i2c_status.error_value();
@@ -130,6 +148,24 @@ zx_status_t Buckeye::I2cInit() {
   };
   i2c_dev.metadata() = std::move(i2c_metadata);
 
+  fidl::Arena<> fidl_arena;
+  fdf::Arena arena('I2C_');
+  auto result = pbus.buffer(arena)->NodeAdd(fidl::ToWire(fidl_arena, i2c_dev));
+  if (!result.ok()) {
+    zxlogf(ERROR, "%s: NodeAdd I2c(i2c_dev) request failed: %s", __func__,
+           result.FormatDescription().data());
+    return result.status();
+  }
+  if (result->is_error()) {
+    zxlogf(ERROR, "%s: NodeAdd I2c(i2c_dev) failed: %s", __func__,
+           zx_status_get_string(result->error_value()));
+    return result->error_value();
+  }
+
+  return ZX_OK;
+}
+
+zx_status_t Buckeye::I2cInit() {
   // I2C_A
   gpio_impl_.SetAltFunction(A5_GPIOZ(15), A5_GPIOZ_15_I2C0_SCL_FN);
   gpio_impl_.SetAltFunction(A5_GPIOZ(14), A5_GPIOZ_14_I2C0_SDA_FN);
@@ -146,18 +182,8 @@ zx_status_t Buckeye::I2cInit() {
   gpio_impl_.SetAltFunction(A5_GPIOC(8), A5_GPIOC_8_I2C3_SCL_FN);
   gpio_impl_.SetAltFunction(A5_GPIOC(7), A5_GPIOC_7_I2C3_SDA_FN);
 
-  fidl::Arena<> fidl_arena;
-  fdf::Arena arena('I2C_');
-  auto result = pbus_.buffer(arena)->NodeAdd(fidl::ToWire(fidl_arena, i2c_dev));
-  if (!result.ok()) {
-    zxlogf(ERROR, "%s: NodeAdd I2c(i2c_dev) request failed: %s", __func__,
-           result.FormatDescription().data());
-    return result.status();
-  }
-  if (result->is_error()) {
-    zxlogf(ERROR, "%s: NodeAdd I2c(i2c_dev) failed: %s", __func__,
-           zx_status_get_string(result->error_value()));
-    return result->error_value();
+  for (uint32_t i = 0; i < std::size(buses); i++) {
+    AddI2cBus(i, buses[i], pbus_);
   }
 
   return ZX_OK;
