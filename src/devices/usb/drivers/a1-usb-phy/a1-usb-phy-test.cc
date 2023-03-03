bebf301c0c83697b44ab77fdf60b9b67b5d90e33
David Gilhooley <dgilhooley@google.com>
2023-03-03 20:42:56 +0000

    [a1-usb-phy] Update to platform device FIDL
    Bug: 110796
    Change-Id: Ia18b5ffbccd3b439ed65db7d75a857b615ac28a8
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/813170
    Reviewed-by: Sarah Chan <spqchan@google.com>
    Fuchsia-Auto-Submit: David Gilhooley <dgilhooley@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/devices/usb/drivers/a1-usb-phy/a1-usb-phy-test.cc
+++ src/devices/usb/drivers/a1-usb-phy/a1-usb-phy-test.cc
@@ -6,9 +6,13 @@
 
 #include <fuchsia/hardware/platform/device/c/banjo.h>
 #include <lib/async-loop/cpp/loop.h>
+#include <lib/async-loop/default.h>
+#include <lib/async_patterns/testing/cpp/dispatcher_bound.h>
+#include <lib/component/outgoing/cpp/outgoing_directory.h>
 #include <lib/ddk/device.h>
 #include <lib/ddk/driver.h>
 #include <lib/ddk/metadata.h>
+#include <lib/fake-resource/resource.h>
 #include <lib/zx/clock.h>
 #include <lib/zx/interrupt.h>
 #include <zircon/errors.h>
@@ -70,6 +74,11 @@ class FakeMmio {
   uint64_t reg_values_[kRegisterCount] = {};
 };
 
+struct IncomingNamespace {
+  fake_pdev::FakePDevFidl pdev_server;
+  component::OutgoingDirectory outgoing{async_get_default_dispatcher()};
+};
+
 // Fixture that supports tests of A1UsbPhy::Create.
 class A1UsbPhyTest : public zxtest::Test {
  public:
@@ -77,13 +86,29 @@ class A1UsbPhyTest : public zxtest::Test {
     static constexpr uint32_t kMagicNumbers[8] = {};
     root_->SetMetadata(DEVICE_METADATA_PRIVATE, &kMagicNumbers, sizeof(kMagicNumbers));
 
-    pdev_.set_mmio(0, mmio_[0].mmio_info());
-    pdev_.set_mmio(1, mmio_[1].mmio_info());
-    pdev_.set_mmio(2, mmio_[2].mmio_info());
-    pdev_.set_mmio(3, mmio_[3].mmio_info());
-    ASSERT_OK(pdev_.PDevGetSmc(0, &smc_monitor_));
+    fake_pdev::FakePDevFidl::Config config;
+    config.mmios[0] = mmio_[0].mmio_info();
+    config.mmios[1] = mmio_[1].mmio_info();
+    config.mmios[2] = mmio_[2].mmio_info();
+    config.mmios[3] = mmio_[3].mmio_info();
 
-    root_->AddProtocol(ZX_PROTOCOL_PDEV, pdev_.proto()->ops, pdev_.proto()->ctx, "pdev");
+    ASSERT_OK(fake_root_resource_create(smc_monitor_.reset_and_get_address()));
+    config.smcs[0] = {};
+    smc_monitor_.duplicate(ZX_RIGHT_SAME_RIGHTS, &config.smcs[0]);
+
+    zx::result outgoing_endpoints = fidl::CreateEndpoints<fuchsia_io::Directory>();
+    ASSERT_OK(outgoing_endpoints);
+    ASSERT_OK(incoming_loop_.StartThread("incoming-ns-thread"));
+    incoming_.SyncCall([config = std::move(config), server = std::move(outgoing_endpoints->server)](
+                           IncomingNamespace* infra) mutable {
+      infra->pdev_server.SetConfig(std::move(config));
+      ASSERT_OK(infra->outgoing.AddService<fuchsia_hardware_platform_device::Service>(
+          infra->pdev_server.GetInstanceHandler()));
+      ASSERT_OK(infra->outgoing.Serve(std::move(server)));
+    });
+    ASSERT_NO_FATAL_FAILURE();
+    root_->AddFidlService(fuchsia_hardware_platform_device::Service::Name,
+                          std::move(outgoing_endpoints->client), "pdev");
 
     ASSERT_OK(A1UsbPhy::Create(nullptr, root_.get()));
   }
@@ -95,7 +120,9 @@ class A1UsbPhyTest : public zxtest::Test {
 
  private:
   FakeMmio mmio_[kRegisterBanks];
-  fake_pdev::FakePDev pdev_;
+  async::Loop incoming_loop_{&kAsyncLoopConfigNoAttachToCurrentThread};
+  async_patterns::TestDispatcherBound<IncomingNamespace> incoming_{incoming_loop_.dispatcher(),
+                                                                   std::in_place};
   zx::resource smc_monitor_;
 };
 
