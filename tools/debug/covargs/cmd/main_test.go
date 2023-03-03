c54d63dc47e1850e6c2451ce5a6668913dc127cd
Gulfem Savrun Yeniceri <gulfem@google.com>
2023-03-03 20:31:03 +0000

    [covargs] Use llvm-cov debuginfod support
    https://reviews.llvm.org/D136702 added debuginfod support into llvm-cov.
    This patch modifies covargs to use that support. It also restructures and cleans up covargs.
    
    Bug: 121303
    Change-Id: I4b9aea455449488bc63f94487a77be9a57b4b567
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/801768
    Reviewed-by: Ina Huh <ihuh@google.com>
    Commit-Queue: Gulfem Savrun Yeniceri <gulfem@google.com>

--- tools/debug/covargs/cmd/main_test.go
+++ tools/debug/covargs/cmd/main_test.go
@@ -5,16 +5,13 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
-	"strconv"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
 	"go.fuchsia.dev/fuchsia/tools/testing/runtests"
 )
 
@@ -75,16 +72,10 @@ func TestReadSummary(t *testing.T) {
 		summaryFiles = append(summaryFiles, summaryFile)
 	}
 
-	expected := map[string]runtests.DataSinkMap{
-		"": {"llvm-profile": []runtests.DataSink{
-			{Name: "profile0", File: filepath.Join(tempDir, "llvm-profile/profile0")},
-		},
-		},
-		"version": {"llvm-profile": []runtests.DataSink{
-			{Name: "profile1", File: filepath.Join(tempDir, "llvm-profile/profile1")},
-			{Name: "profile2", File: filepath.Join(tempDir, "llvm-profile/profile2")},
-		},
-		},
+	expected := map[string]string{
+		filepath.Join(tempDir, "llvm-profile/profile0"): "",
+		filepath.Join(tempDir, "llvm-profile/profile1"): "version",
+		filepath.Join(tempDir, "llvm-profile/profile2"): "version",
 	}
 
 	actual, err := readSummary(summaryFiles)
@@ -94,119 +85,5 @@ func TestReadSummary(t *testing.T) {
 
 	if diff := cmp.Diff(actual, expected); diff != "" {
 		t.Errorf("Unexpected sinks (-got +want):\n%s", diff)
-	}
-}
-
-const validModule = "1696251c"
-
-// getLLVMProfdata returns the path to a mock tool which prints the provided output.
-func getLLVMProfdata(t *testing.T, filepath string, output string, wantErr bool) string {
-	if wantErr {
-		if err := os.WriteFile(filepath, []byte("#!/bin/bash\nexit 1"), os.ModePerm); err != nil {
-			t.Fatalf("failed to write mock llvm-profdata tool: %s", err)
-		}
-	} else {
-		if err := os.WriteFile(filepath, []byte(fmt.Sprintf("#!/bin/bash\necho \"%s\"", output)), os.ModePerm); err != nil {
-			t.Fatalf("failed to write mock llvm-profdata tool: %s", err)
-		}
-	}
-	return filepath
-}
-
-type mockVersionFetcher struct {
-}
-
-// getVersion returns the number at the end of the file name as the version.
-func (m *mockVersionFetcher) getVersion(file string) (uint64, error) {
-	return strconv.ParseUint(string(file[len(file)-1]), 10, 64)
-}
-
-func TestMergeEntries(t *testing.T) {
-	ctx := context.Background()
-	tempDir := t.TempDir()
-	malformedProfdata := getLLVMProfdata(t, filepath.Join(tempDir, "malformed"), "", true)
-	validProfdata := getLLVMProfdata(t, filepath.Join(tempDir, "valid"), fmt.Sprintf("Binary IDs:\n%s\n", validModule), false)
-	missingProfdata := getLLVMProfdata(t, filepath.Join(tempDir, "missing"), "Binary IDs:\n", false)
-	cases := []struct {
-		name            string
-		partitions      map[string]*partition
-		sinks           map[string][]runtests.DataSink
-		expectedEntries []profileEntry
-		expectedSinks   map[string][]string
-		wantErr         bool
-	}{
-		{
-			name:       "valid",
-			partitions: map[string]*partition{"": {tool: validProfdata}, "version1": {tool: malformedProfdata}, "version2": {tool: validProfdata}},
-			sinks:      map[string][]runtests.DataSink{"version2": {{File: "sink1"}, {File: "sink2"}}, "": {{File: "sink0"}}},
-			expectedEntries: []profileEntry{
-				{Profile: "sink1", Module: validModule},
-				{Profile: "sink2", Module: validModule},
-				{Profile: "sink0", Module: validModule},
-			},
-			expectedSinks: map[string][]string{"": {"sink0"}, "version2": {"sink1", "sink2"}},
-		},
-		{
-			name:       "fail if missing llvm-profdata version",
-			partitions: map[string]*partition{"": {tool: validProfdata}, "version1": {tool: malformedProfdata}, "version2": {tool: validProfdata}},
-			sinks:      map[string][]runtests.DataSink{"version2": {{File: "sink1"}, {File: "sink2"}}, "version3": {{File: "sink0"}}},
-			wantErr:    true,
-		},
-		{
-			name:       "missing buildid returns error",
-			partitions: map[string]*partition{"": {tool: validProfdata}, "version1": {tool: missingProfdata}, "version2": {tool: validProfdata}},
-			sinks:      map[string][]runtests.DataSink{"version1": {{File: "sink1"}}},
-			wantErr:    true,
-		},
-		{
-			name:       "malformed profile returns error",
-			partitions: map[string]*partition{"": {tool: malformedProfdata}, "version1": {tool: missingProfdata}, "version2": {tool: validProfdata}},
-			sinks:      map[string][]runtests.DataSink{"": {{File: "sink1"}}, "version2": {{File: "sink2"}}},
-			wantErr:    true,
-		},
-		{
-			name:            "use version fetcher if not specifying summary version",
-			partitions:      map[string]*partition{"": {tool: validProfdata}, "5": {tool: malformedProfdata}, "7": {tool: validProfdata}},
-			sinks:           map[string][]runtests.DataSink{"": {{File: "sink1"}, {File: "sink7"}}},
-			expectedEntries: []profileEntry{{Profile: "sink1", Module: validModule}, {Profile: "sink7", Module: validModule}},
-			expectedSinks:   map[string][]string{"": {"sink1"}, "7": {"sink7"}},
-		},
-		{
-			name:       "use default partition with version fetcher",
-			partitions: map[string]*partition{"": {tool: validProfdata}, "5": {tool: malformedProfdata}, "7": {tool: validProfdata}},
-			sinks:      map[string][]runtests.DataSink{"": {{File: "sink1"}, {File: "sink2"}, {File: "sink7"}}},
-			expectedEntries: []profileEntry{
-				{Profile: "sink1", Module: validModule},
-				{Profile: "sink2", Module: validModule},
-				{Profile: "sink7", Module: validModule},
-			},
-			expectedSinks: map[string][]string{"": {"sink1", "sink2"}, "7": {"sink7"}},
-		},
-	}
-	for _, tc := range cases {
-		tc := tc
-		t.Run(tc.name, func(t *testing.T) {
-			versionedSummaries := make(map[string]runtests.DataSinkMap)
-			for version, sinks := range tc.sinks {
-				versionedSummaries[version] = runtests.DataSinkMap{llvmProfileSinkType: sinks}
-			}
-			entries, err := mergeEntries(ctx, &mockVersionFetcher{}, versionedSummaries, tc.partitions)
-			if tc.wantErr != (err != nil) {
-				t.Errorf("got err: %s, want err: %v", err, tc.wantErr)
-			}
-			sortSlicesOpt := cmpopts.SortSlices(func(a, b profileEntry) bool { return a.Profile < b.Profile })
-			if diff := cmp.Diff(entries, tc.expectedEntries, sortSlicesOpt); diff != "" {
-				t.Errorf("unexpected entries: (-got +want):\n%s", diff)
-			}
-
-			if !tc.wantErr {
-				sortSlicesOpt = cmpopts.SortSlices(func(a, b string) bool { return a < b })
-				for version, partition := range tc.partitions {
-					if diff := cmp.Diff(partition.profiles, tc.expectedSinks[version], sortSlicesOpt); diff != "" {
-						t.Errorf("unexpected profiles for version %q: (-got +want):\n%s", version, diff)
-					}
-				}
-			}
-		})
 	}
 }
