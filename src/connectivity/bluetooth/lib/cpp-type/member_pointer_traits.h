5bbbd75dc6b90d8c335c064b8bf18ce8b9e60968
Tamir Duberstein <tamird@google.com>
2023-03-06 19:57:20 +0000

    [bt] Remove sanitizer suppressions
    Return a pointer from `MutableByteBuffer::AsMutable` to avoid forming
    references to misaligned memory.
    
    Use simpler pointer arithmetic in `MemberPointerTraits::offset` to avoid
    UB. Mark the function constexpr which C++14 allows.
    
    Fixed: 46637
    Change-Id: Iabfd58dff75463a7f8e84fb010de9d1a2476ecea
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/814427
    Reviewed-by: Leonard Chan <leonardchan@google.com>
    Fuchsia-Auto-Submit: Tamir Duberstein <tamird@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/connectivity/bluetooth/lib/cpp-type/member_pointer_traits.h
+++ src/connectivity/bluetooth/lib/cpp-type/member_pointer_traits.h
@@ -30,7 +30,7 @@ class MemberPointerTraits<PointerToMember> {
   // This is necessary because pointers-to-data-member can not be used with the offsetof macro (see
   // http://wg21.link/p0908r0 for a description and proposed standard fix). Note this constructs and
   // value-initializes an object of ClassT, so care should be taken if that has side effects.
-  static size_t offset() {
+  static size_t constexpr offset() {
     // Pointer subtraction is only defined for array objects per ISO/IEC 14882:2017(E) § 8.7
     // [expr.add] ¶ 2.5, so we have to actually construct an object rather than use a nullptr
     // pointer or std::declval (whose rules disallow odr-use).
@@ -44,20 +44,7 @@ class MemberPointerTraits<PointerToMember> {
     // this function than it uses elsewhere. For now, it's safest to use this _only_ with
     // [[gnu::packed]] classes.
     const ClassType temporary{};  // Value initialization in case of const members
-
-    // Conversion to void* then char* is excepted from UB by § 6.8 [basic.life] ¶ 6.4.
-    const void* const base = std::addressof(temporary);
-
-    // TODO(https://fxbug.dev/46637): this may form a reference to a misaligned field, which is UB.
-    const void* const member =
-        [](const ClassType* temporary) __attribute__((no_sanitize("alignment"))) {
-      return std::addressof(temporary->*PointerToMember);
-    }
-    (&temporary);
-
-    // This void* to pointer-to-object conversion is undefined behavior if this function were
-    // constexpr per § 8.20 [expr.const] ¶ 2.13.
-    return static_cast<const char*>(member) - static_cast<const char*>(base);
+    return size_t(&(temporary.*PointerToMember)) - size_t(&temporary);
   }
 };
 
