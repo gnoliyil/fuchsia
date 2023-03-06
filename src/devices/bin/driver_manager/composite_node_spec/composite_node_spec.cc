af8819d348d3644d188ac41b7167ebb709e20caf
Novin Changizi <novinc@google.com>
2023-03-06 22:32:54 +0000

    [fdf] Check for expired composite parent
    Checks for expired nodes in composite node spec manager when looking to
    bind a parent node. This will allow us to do reloading in a later change
    without breaking composite node specs. This is dfv2 specific.
    
    Bug: 104954
    Tests: updated unit tests to pass non-expired nodes where this is
    checked
    
    Change-Id: I652ba47afc783e55684d81163704019d36d4e051
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/814087
    Commit-Queue: Novin Changizi <novinc@google.com>
    Reviewed-by: Sarah Chan <spqchan@google.com>

--- src/devices/bin/driver_manager/composite_node_spec/composite_node_spec.cc
+++ src/devices/bin/driver_manager/composite_node_spec/composite_node_spec.cc
@@ -6,7 +6,7 @@
 
 CompositeNodeSpec::CompositeNodeSpec(CompositeNodeSpecCreateInfo create_info)
     : name_(create_info.name) {
-  parent_specs_ = std::vector<bool>(create_info.size, false);
+  parent_specs_ = std::vector<std::optional<DeviceOrNode>>(create_info.size, std::nullopt);
 }
 
 zx::result<std::optional<DeviceOrNode>> CompositeNodeSpec::BindParent(
@@ -18,13 +18,21 @@ zx::result<std::optional<DeviceOrNode>> CompositeNodeSpec::BindParent(
     return zx::error(ZX_ERR_OUT_OF_RANGE);
   }
 
-  if (parent_specs_[node_index]) {
-    return zx::error(ZX_ERR_ALREADY_BOUND);
+  const std::optional<DeviceOrNode>& current_at_index = parent_specs_[node_index];
+  if (current_at_index.has_value()) {
+    const DeviceOrNode& existing = current_at_index.value();
+    const std::weak_ptr<dfv2::Node>* existing_node =
+        std::get_if<std::weak_ptr<dfv2::Node>>(&existing);
+    // If the dfv2::Node no longer exists (for example because of a reload) then we don't
+    // want to return an ALREADY_BOUND error.
+    if (!existing_node || !existing_node->expired()) {
+      return zx::error(ZX_ERR_ALREADY_BOUND);
+    }
   }
 
   auto result = BindParentImpl(info, device_or_node);
   if (result.is_ok()) {
-    parent_specs_[node_index] = true;
+    parent_specs_[node_index] = device_or_node;
   }
 
   return result;
