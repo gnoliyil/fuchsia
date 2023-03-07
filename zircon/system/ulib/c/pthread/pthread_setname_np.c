3d35ee9413815616dbed1d1c54a97545b383f552
Alex Brachet <abrachet@google.com>
2023-03-07 07:03:55 +0000

    [libc] Correctly handle completed threads in pthread_{get,set}name
    Fixed: 121867
    Change-Id: I6b1b21e84eb9128350b89bc73e2900db162a972f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/803167
    Fuchsia-Auto-Submit: Alex Brachet <abrachet@google.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- zircon/system/ulib/c/pthread/pthread_setname_np.c
+++ zircon/system/ulib/c/pthread/pthread_setname_np.c
@@ -4,6 +4,7 @@
 
 #include <string.h>
 #include <zircon/compiler.h>
+#include <zircon/status.h>
 
 #include "libc.h"
 #include "threads_impl.h"
@@ -11,6 +12,14 @@
 __EXPORT int pthread_setname_np(pthread_t thread, const char *name) {
   zx_handle_t handle = zxr_thread_get_handle(&thread->zxr_thread);
   zx_status_t status = zx_object_set_property(handle, ZX_PROP_NAME, name, strlen(name));
-  ZX_ASSERT(status == ZX_OK);
-  return 0;
+  switch (status) {
+    case ZX_OK:
+      return 0;
+    // The thread has exited but has not been joined so the internal thread object has died but not
+    // this pthread_t.
+    case ZX_ERR_BAD_STATE:
+      return ESRCH;
+    default:
+      ZX_PANIC("unexpected status '%s'", zx_status_get_string(status));
+  }
 }
