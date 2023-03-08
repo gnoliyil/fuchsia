9414008d2682447d8c22da150960812cdcb9f7d3
Kendal Harland <kjharland@google.com>
2023-03-08 22:56:50 +0000

    [test_realm_proxy] Add initial service and client helpers.
    Change (2/3)
    
    Adds two Rust modules at //src/testing/realm_proxy for
    writing RealmProxy clients and services.
    
    For services:
    - The RealmProxy trait describes an struct that handles business logic
      for a fuchsia.testing.harness.RealmProxy service. A default
      RealmInstanceProxy implementation is included.
    - handle_request_stream() serves all RealmProxyRequests received in
      a stream by delegating to a RealmProxy implementation.
    
    For clients:
    - RealmProxyClient simplifies connecting to a RealmProxy service and
      the services that are exposed by that proxy.
    
    Bug: 121395
    Change-Id: I0aa30d02b30373793dae510efb93a2cb4aa75d2a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/813747
    Commit-Queue: Kendal Harland <kjharland@google.com>
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Reviewed-by: Marc-Antoine Ruel <maruel@google.com>

---
+++ src/testing/realm_proxy/src/client.rs
@@ -0,0 +1,55 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {
+    anyhow::{bail, Error, Result},
+    fidl::endpoints::{create_endpoints, ClientEnd, DiscoverableProtocolMarker},
+    fidl_fuchsia_testing_harness::{RealmProxy_Marker, RealmProxy_Proxy},
+    fuchsia_component::client::connect_to_protocol,
+};
+
+// RealmProxyClient is a client for fuchsia.testing.harness.RealmProxy.
+//
+// The calling component must have a handle to the RealmProxy protocol in
+// order to use this struct. Once the caller has connected to the RealmProxy
+// service, they can access the other services in the proxied test realm by
+// calling [connect_to_protocol].
+//
+// # Example Usage
+//
+// ```
+// let realm_proxy = RealmProxyClient::connect()?;
+// let echo = realm_proxy.connect_to_protocol::<EchoMarker>().await?;
+// ```
+pub struct RealmProxyClient {
+    inner: RealmProxy_Proxy,
+}
+
+impl RealmProxyClient {
+    // Connects to the RealmProxy service.
+    pub fn connect() -> Result<Self, Error> {
+        let inner = connect_to_protocol::<RealmProxy_Marker>()?;
+        Ok(Self { inner })
+    }
+
+    // Connects to the protocol marked by [T] via the proxy.
+    //
+    // Returns an error if the connection fails.
+    pub async fn connect_to_protocol<T: DiscoverableProtocolMarker>(
+        &self,
+    ) -> Result<ClientEnd<T>, Error> {
+        let (client, server) = create_endpoints::<T>();
+        let res = self
+            .inner
+            .connect_to_named_protocol(T::PROTOCOL_NAME, server.into_channel())
+            .await
+            .expect("connect_to_named_protocol");
+
+        if let Some(op_err) = res.err() {
+            bail!("{:?}", op_err);
+        }
+
+        Ok(client)
+    }
+}
