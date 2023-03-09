727702b862652724ca184796c5ee66abd4abf031
Victor Costan <costan@google.com>
2023-03-09 19:31:45 +0000

    [fusb302][vim3] Type-safe definitions for USB PD concepts.
    This CL adds definitions for general concepts used in the USB Type C and
    USB PD specs. The definitions will be used in future CLs.
    
    Bug: 122113
    Test: fx test fusb302-test (no functional changes)
    Change-Id: Id640fbf9abf6f9a180123de3dfdd455cab0ad604
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/815187
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Ruby Zhuang <rdzhuang@google.com>
    Fuchsia-Auto-Submit: Victor Costan <costan@google.com>

---
+++ src/devices/power/drivers/fusb302/usb-pd-defs.cc
@@ -0,0 +1,90 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/devices/power/drivers/fusb302/usb-pd-defs.h"
+
+#include <zircon/assert.h>
+
+#include <cinttypes>
+
+namespace usb_pd {
+
+const char* DebugStringFromPowerRole(PowerRole power_role) {
+  switch (power_role) {
+    case PowerRole::kSink:
+      return "Sink";
+    case PowerRole::kSource:
+      return "Source";
+  }
+  ZX_DEBUG_ASSERT_MSG(false, "Invalid PowerRole: %" PRId8, power_role);
+  return nullptr;
+}
+
+const char* DebugStringFromDataRole(DataRole data_role) {
+  switch (data_role) {
+    case DataRole::kDownstreamFacingPort:
+      return "DFP";
+    case DataRole::kUpstreamFacingPort:
+      return "UFP";
+  }
+  ZX_DEBUG_ASSERT_MSG(false, "Invalid DataRole: %" PRId8, data_role);
+  return nullptr;
+}
+
+const char* DebugStringFromConfigChannelPinId(ConfigChannelPinId pin_id) {
+  switch (pin_id) {
+    case ConfigChannelPinId::kCc1:
+      return "CC1";
+    case ConfigChannelPinId::kCc2:
+      return "CC2";
+  }
+  ZX_DEBUG_ASSERT_MSG(false, "Invalid ConfigChannelPinId: %" PRId8, pin_id);
+  return nullptr;
+}
+
+const char* DebugStringFromConfigChannelPinSwitch(ConfigChannelPinSwitch cc_switch) {
+  switch (cc_switch) {
+    case ConfigChannelPinSwitch::kNone:
+      return "(not connected)";
+    case ConfigChannelPinSwitch::kCc1:
+      return "CC1";
+    case ConfigChannelPinSwitch::kCc2:
+      return "CC2";
+  }
+  ZX_DEBUG_ASSERT_MSG(false, "Invalid ConfigChannelPinSwitch: %" PRId8, cc_switch);
+  return nullptr;
+}
+
+const char* DebugStringFromConfigChannelTermination(ConfigChannelTermination termination) {
+  switch (termination) {
+    case ConfigChannelTermination::kUnknown:
+      return "(unknown)";
+    case ConfigChannelTermination::kOpen:
+      return "Open";
+    case ConfigChannelTermination::kRa:
+      return "Ra (Accessory)";
+    case ConfigChannelTermination::kRd:
+      return "Rd (Sink)";
+    case ConfigChannelTermination::kRpStandardUsb:
+      return "Rp (Standard USB Power)";
+    case ConfigChannelTermination::kRp1500mA:
+      return "Rp (1.5A Type C Power / SinkTxOK)";
+    case ConfigChannelTermination::kRp3000mA:
+      return "Rp (3.0A Type C Power / SinkTxNG)";
+  }
+  ZX_DEBUG_ASSERT_MSG(false, "Invalid ConfigChannelTermination: %" PRId8, termination);
+  return nullptr;
+}
+
+// Tests for ConfigChannelPinIdFromInverse().
+static_assert(ConfigChannelPinIdFromInverse(ConfigChannelPinId::kCc1) == ConfigChannelPinId::kCc2);
+static_assert(ConfigChannelPinIdFromInverse(ConfigChannelPinId::kCc2) == ConfigChannelPinId::kCc1);
+
+// Tests for ConfigChannelPinIdFromSwitch().
+static_assert(static_cast<ConfigChannelPinId>(ConfigChannelPinSwitch::kCc1) ==
+              ConfigChannelPinId::kCc1);
+static_assert(static_cast<ConfigChannelPinId>(ConfigChannelPinSwitch::kCc2) ==
+              ConfigChannelPinId::kCc2);
+
+}  // namespace usb_pd
---
+++ src/devices/power/drivers/fusb302/usb-pd-defs.h
@@ -0,0 +1,107 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_DEVICES_POWER_DRIVERS_FUSB302_USB_PD_DEFS_H_
+#define SRC_DEVICES_POWER_DRIVERS_FUSB302_USB_PD_DEFS_H_
+
+// The comments in this file reference the USB Power Delivery Specification,
+// downloadable at https://usb.org/document-library/usb-power-delivery
+//
+// The section and page numbers reference Revision 3.1, Version 1.7, published
+// January 2023.
+
+#include <zircon/assert.h>
+
+#include <cstdint>
+
+namespace usb_pd {
+
+// Type-safe selector for a USB power role.
+//
+// The values match Section 6.2.1.1.4 "Port Power Role" in the USB PD spec. So,
+// instances can be bit-copied into PD message headers.
+enum class PowerRole : int8_t {
+  kSink = 0,
+  kSource = 1,
+};
+
+// Descriptor for logging and debugging.
+const char* DebugStringFromPowerRole(PowerRole power_role);
+
+// Specification Revision
+//
+// The values match Section 6.2.1.1.5 "Specification Revision" in the USB PD
+// spec. So, instances can be bit-copied into PD message headers.
+enum class SpecRevision : uint8_t {
+  kRev1 = 0b00,
+  kRev2 = 0b01,
+  kRev3 = 0b10,
+};
+
+// Type-safe selector for a USB data role.
+//
+// The values match Section 6.2.1.1.6 "Port Data Role" in the USB PD spec. So,
+// instances can be bit-copied into PD message headers.
+enum class DataRole : uint8_t {
+  kUpstreamFacingPort = 0,    // UFP
+  kDownstreamFacingPort = 1,  // DFP
+};
+
+// Descriptor for logging and debugging.
+const char* DebugStringFromDataRole(DataRole data_role);
+
+// Type-safe selector for one of the two CC (Configuration Channel) pins.
+//
+// The Configuration Channel is described by the USB PD spec. It is introduced
+// in Section 2.1 "Introduction", and its role is summarized in in Section 2.3
+// "Configuration Process".
+enum class ConfigChannelPinId : int8_t {
+  kCc1 = 1,
+  kCc2 = 2,
+};
+
+// Descriptor for logging and debugging.
+const char* DebugStringFromConfigChannelPinId(ConfigChannelPinId pin_id);
+
+// State of a switch that can either point to a CC pin or be open.
+//
+// See `ConfigChannelPinId` for a description of the CC (Configuration Channel)
+// pins.
+enum class ConfigChannelPinSwitch : int8_t {
+  kNone = 0,
+  kCc1 = 1,
+  kCc2 = 2,
+};
+
+// Descriptor for logging and debugging.
+const char* DebugStringFromConfigChannelPinSwitch(ConfigChannelPinSwitch cc_switch);
+
+// Measured voltage of a CC pin.
+enum class ConfigChannelTermination : int8_t {
+  kUnknown = 0,
+  kOpen = 1,
+  kRa = 2,
+  kRd = 3,
+  kRpStandardUsb = 4,
+  kRp1500mA = 5,
+  kRp3000mA = 6,
+};
+
+// Descriptor for logging and debugging.
+const char* DebugStringFromConfigChannelTermination(ConfigChannelTermination termination);
+
+// Inverts a ConfigChannelPinId.
+constexpr ConfigChannelPinId ConfigChannelPinIdFromInverse(ConfigChannelPinId id) {
+  return (id == ConfigChannelPinId::kCc1) ? ConfigChannelPinId::kCc2 : ConfigChannelPinId::kCc1;
+}
+
+// `cc_switch` must not be `kNone`.
+constexpr ConfigChannelPinId ConfigChannelPinIdFromSwitch(ConfigChannelPinSwitch cc_switch) {
+  ZX_DEBUG_ASSERT(cc_switch != ConfigChannelPinSwitch::kNone);
+  return static_cast<ConfigChannelPinId>(cc_switch);
+}
+
+}  // namespace usb_pd
+
+#endif  // SRC_DEVICES_POWER_DRIVERS_FUSB302_USB_PD_DEFS_H_
