90da8baeaeb579078ca26e1eed64882a7db0e5a4
Andrew Davies <awdavies@google.com>
2023-03-09 02:40:15 +0000

    [fuchsia-controller][ffx] Add Python build rules.
    Adds some Python wrapper rules to make it possible to point to the
    fuchsia controller library, making it importable for binaries using the
    controller.
    
    Change-Id: I3cb2e5d6f128279bb097b6d9ecfdc834ca36efb7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/807847
    Reviewed-by: Jay Zhuang <jayzhuang@google.com>
    Commit-Queue: Andrew Davies <awdavies@google.com>
    Reviewed-by: Casey Dahlin <sadmac@google.com>
    Reviewed-by: Anirudh Mathukumilli <rudymathu@google.com>

---
+++ src/developer/ffx/lib/fuchsia-controller/python/example.py
@@ -0,0 +1,39 @@
+# Copyright 2023 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+from fuchsia_controller_py import Context, open_handle_notifier
+import os
+import asyncio
+
+
+async def read_handle(fd, handle):
+    while True:
+        try:
+            return handle.read()
+        except RuntimeError:
+            loop = asyncio.get_event_loop()
+            await loop.run_in_executor(None, lambda: os.read(fd, 4))
+
+
+async def async_main():
+    os.putenv('FFX_BIN', 'host_x64/ffx')
+    ctx = Context({})
+    fd = open_handle_notifier()
+    hdl = ctx.open_daemon_protocol('fuchsia.developer.ffx.Echo')
+    # For now this is just a raw encoded FIDL message.
+    out = bytearray(
+        b'\x01\x00\x00\x00\x02\x00\x00\x01%z\xcet\x90g/\x00\x06\x00\x00\x00\x00\x00\x00\x00\xff\xff\xff\xff\xff\xff\xff\xfffoobar\x00\x00'
+    )
+    hdl.write((out, []))
+    print(f'Was able to write: {out}')
+    (b, _) = await read_handle(fd, hdl)
+    print(f'Read: {b}')
+    assert out == b
+
+
+def main():
+    asyncio.run(async_main())
+
+
+if __name__ == '__main__':
+    main()
