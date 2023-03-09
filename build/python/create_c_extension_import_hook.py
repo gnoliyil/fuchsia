b5d958649ee6819900bc55881072cfcd7dc157ed
Andrew Davies <awdavies@google.com>
2023-03-09 02:40:15 +0000

    [fuchsia-controller][ffx] Add Python build rules.
    Adds some Python wrapper rules to make it possible to point to the
    fuchsia controller library, making it importable for binaries using the
    controller.
    
    Change-Id: I3cb2e5d6f128279bb097b6d9ecfdc834ca36efb7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/807847
    Reviewed-by: Jay Zhuang <jayzhuang@google.com>
    Commit-Queue: Andrew Davies <awdavies@google.com>
    Reviewed-by: Casey Dahlin <sadmac@google.com>
    Reviewed-by: Anirudh Mathukumilli <rudymathu@google.com>

---
+++ build/python/create_c_extension_import_hook.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3.8
+"""Creates a Python library for a C extension."""
+
+# Copyright 2023 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import os
+import sys
+
+
+def main():
+    """Creates library wrapper"""
+
+    parser = argparse.ArgumentParser(
+        'Creates a Python library wrapper for a C extension.')
+    parser.add_argument(
+        '--target_name',
+        help='Name of the build target',
+        required=True,
+    )
+    parser.add_argument(
+        '--shlib_source',
+        help='Path to the shared library',
+        required=True,
+    )
+    parser.add_argument(
+        '--gen_dir',
+        help='Path to gen directory, used to stage temp directories',
+        required=True,
+    )
+    parser.add_argument('--output', help='Path to output', required=True)
+    parser.add_argument(
+        '--depfile',
+        help='Path to the depfile to generate',
+        type=argparse.FileType('w'),
+        required=True,
+    )
+
+    args = parser.parse_args()
+    app_dir = os.path.join(args.gen_dir, args.target_name)
+    os.makedirs(app_dir, exist_ok=True)
+    main_file = os.path.join(app_dir, "__init__.py")
+    shlib_source_dir = os.path.dirname(args.shlib_source)
+    if not shlib_source_dir:
+        shlib_source_dir = os.path.join('.')
+    shlib_source = os.path.basename(args.shlib_source)
+
+    # Making this a list in the event that there are more deps.
+    deps = ' '.join([args.shlib_source])
+    args.depfile.write(f'{args.output}: {deps}\n')
+    with open(main_file, 'w', encoding='utf-8') as main_file_out:
+        main_file_out.write(
+            f'''
+import importlib.util
+import importlib.machinery
+
+def _init():
+    finder = importlib.machinery.PathFinder()
+    spec = finder.find_spec('{shlib_source}', path=['{shlib_source_dir}'])
+    if spec is None:
+        raise Exception('Couldn\\'t load library "{shlib_source}" from {shlib_source_dir}')
+    mod = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(mod)
+    return mod
+
+__all__ = _init().__dict__
+''')
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
