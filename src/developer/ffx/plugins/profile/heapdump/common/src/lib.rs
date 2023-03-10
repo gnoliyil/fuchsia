ae1074bdd5abf3037f6d189cb5340d1fd5072ea9
Fabio D'Urso <fdurso@google.com>
2023-03-10 10:27:16 +0000

    [heapdump] Connect to the collector from a new ffx plugin
    This patch adds the ffx client plugin, which was the last missing piece
    of infrastructure for the heapdump memory profiler.
    
    Test: fx test heapdump, manual testing for ffx client plugin
    Change-Id: I3c67a63a25c3712ab42cee86c9a12f45831220f1
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/802287
    API-Review: Miguel Flores <miguelfrde@google.com>
    Reviewed-by: Étienne J. Membrives <etiennej@google.com>
    Reviewed-by: Miguel Flores <miguelfrde@google.com>
    Commit-Queue: Fabio D'Urso <fdurso@google.com>

---
+++ src/developer/ffx/plugins/profile/heapdump/common/src/lib.rs
@@ -0,0 +1,47 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use errors::ffx_bail;
+use fidl_fuchsia_memory_heapdump_client as fheapdump_client;
+
+/// Builds a ProcessSelector value from command-line arguments.
+pub fn build_process_selector(
+    by_name: Option<String>,
+    by_koid: Option<u64>,
+) -> anyhow::Result<fheapdump_client::ProcessSelector> {
+    match (by_name, by_koid) {
+        (Some(selected_name), None) => Ok(fheapdump_client::ProcessSelector::ByName(selected_name)),
+        (None, Some(selected_koid)) => Ok(fheapdump_client::ProcessSelector::ByKoid(selected_koid)),
+        _ => anyhow::bail!("Please use either --by-name or --by-koid"),
+    }
+}
+
+/// Converts a CollectorError into a user-friendly error.
+///
+/// The returned error is meant to be returned by the ffx plugin's main function.
+pub fn check_collector_error<T>(
+    value: Result<T, fheapdump_client::CollectorError>,
+) -> anyhow::Result<T> {
+    // Match known errors and return an FfxError (which is simply printed as-is).
+    // For unknown errors, return a regular anyhow Error so that the full context with a "BUG"
+    // banner is printed instead.
+    match value {
+        Ok(inner) => Ok(inner),
+        Err(fheapdump_client::CollectorError::ProcessSelectorUnsupported) => {
+            ffx_bail!("Unsupported process filter")
+        }
+        Err(fheapdump_client::CollectorError::ProcessSelectorNoMatch) => {
+            ffx_bail!("No process matches the requested filter")
+        }
+        Err(fheapdump_client::CollectorError::ProcessSelectorAmbiguous) => {
+            ffx_bail!("More than one process matches the requested filter")
+        }
+        Err(fheapdump_client::CollectorError::LiveSnapshotFailed) => {
+            ffx_bail!("Failed to take a snapshot of the current live allocations in the process")
+        }
+        Err(other) => {
+            anyhow::bail!("Unrecognized CollectorError: {:?}", other)
+        }
+    }
+}
--- src/performance/memory/heapdump/collector/src/registry.rs
+++ src/performance/memory/heapdump/collector/src/registry.rs
@@ -2,13 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use fidl_fuchsia_memory_heapdump_client::{self as fheapdump_client, CollectorError};
 use fidl_fuchsia_memory_heapdump_process as fheapdump_process;
 use fuchsia_zircon::Koid;
 use futures::lock::Mutex;
 use futures::StreamExt;
 use std::collections::hash_map::{Entry, HashMap};
 use std::sync::Arc;
-use tracing::info;
+use tracing::{info, warn};
 
 use crate::process::Process;
 use crate::process_v1::ProcessV1;
@@ -22,6 +23,57 @@ pub struct Registry {
 impl Registry {
     pub fn new() -> Registry {
         Registry { processes: Mutex::new(HashMap::new()) }
+    }
+
+    async fn find_process_by_name(&self, name: &str) -> Result<Arc<dyn Process>, CollectorError> {
+        let processes = self.processes.lock().await;
+        let mut iterator = processes.values().filter(|p| p.get_name() == name);
+        match (iterator.next(), iterator.next()) {
+            (Some(process), None) => Ok(process.clone()),
+            (None, _) => Err(CollectorError::ProcessSelectorNoMatch),
+            (Some(_), Some(_)) => Err(CollectorError::ProcessSelectorAmbiguous),
+        }
+    }
+
+    async fn find_process_by_koid(&self, koid: &Koid) -> Result<Arc<dyn Process>, CollectorError> {
+        let result = self.processes.lock().await.get(koid).cloned();
+        result.ok_or(CollectorError::ProcessSelectorNoMatch)
+    }
+
+    pub async fn serve_client_stream(
+        &self,
+        mut stream: fheapdump_client::CollectorRequestStream,
+    ) -> Result<(), anyhow::Error> {
+        while let Some(request) = stream.next().await.transpose()? {
+            match request {
+                fheapdump_client::CollectorRequest::TakeLiveSnapshot {
+                    process_selector,
+                    responder,
+                } => {
+                    let process = match process_selector {
+                        fheapdump_client::ProcessSelector::ByName(name) => {
+                            self.find_process_by_name(&name).await
+                        }
+                        fheapdump_client::ProcessSelector::ByKoid(koid) => {
+                            self.find_process_by_koid(&Koid::from_raw(koid)).await
+                        }
+                        fheapdump_client::ProcessSelectorUnknown!() => {
+                            warn!(ordinal = process_selector.ordinal(), "Unknown process selector");
+                            Err(CollectorError::ProcessSelectorUnsupported)
+                        }
+                    };
+
+                    match process {
+                        Ok(_process) => {
+                            // Not implemented yet, pretend that an error happened.
+                            responder.send(&mut Err(CollectorError::LiveSnapshotFailed))?;
+                        }
+                        Err(e) => responder.send(&mut Err(e))?,
+                    };
+                }
+            }
+        }
+        Ok(())
     }
 
     pub async fn serve_process_stream(
@@ -83,9 +135,39 @@ impl Registry {
 mod tests {
     use super::*;
     use async_trait::async_trait;
+    use fidl::endpoints::create_proxy_and_stream;
     use fuchsia_async as fasync;
     use futures::{channel::oneshot, pin_mut};
     use test_case::test_case;
+
+    fn create_registry_and_proxy(
+        initial_processes: impl IntoIterator<Item = Arc<dyn Process>>,
+    ) -> (Arc<Registry>, fheapdump_client::CollectorProxy) {
+        // Create a new Registry and register the given processes.
+        let registry = Arc::new(Registry::new());
+        {
+            let mut processes = registry.processes.try_lock().unwrap();
+            for process in initial_processes {
+                let koid = process.get_koid();
+                let insert_result = processes.insert(koid, process);
+                assert!(insert_result.is_none(), "found duplicate koid in initial_processes");
+            }
+        }
+
+        // Create a client and start serving its stream from a detached task.
+        let proxy = {
+            let registry = registry.clone();
+            let (proxy, stream) =
+                create_proxy_and_stream::<fheapdump_client::CollectorMarker>().unwrap();
+
+            let worker_fn = async move { registry.serve_client_stream(stream).await.unwrap() };
+            fasync::Task::local(worker_fn).detach();
+
+            proxy
+        };
+
+        (registry, proxy)
+    }
 
     struct FakeProcess {
         name: String,
@@ -95,14 +177,17 @@ mod tests {
 
     impl FakeProcess {
         /// Returns a new FakeProcess and a oneshot channel to make it exit with a given result.
-        pub fn new(name: &str, koid: Koid) -> (FakeProcess, oneshot::Sender<anyhow::Result<()>>) {
+        pub fn new(
+            name: &str,
+            koid: Koid,
+        ) -> (Arc<dyn Process>, oneshot::Sender<anyhow::Result<()>>) {
             let (sender, receiver) = oneshot::channel();
             let fake_process = FakeProcess {
                 name: name.to_string(),
                 koid,
                 exit_signal: Mutex::new(Some(receiver)),
             };
-            (fake_process, sender)
+            (Arc::new(fake_process), sender)
         }
     }
 
@@ -132,7 +217,7 @@ mod tests {
         let name = "fake";
         let koid = Koid::from_raw(1234);
         let (process, signal) = FakeProcess::new(name, koid);
-        let serve_fut = registry.serve_process(Arc::new(process));
+        let serve_fut = registry.serve_process(process);
         pin_mut!(serve_fut);
         assert!(ex.run_until_stalled(&mut serve_fut).is_pending());
 
@@ -160,7 +245,7 @@ mod tests {
         let (process2, _signal2) = FakeProcess::new(name2, koid);
 
         // Register the first process.
-        let serve1_fut = registry.serve_process(Arc::new(process1));
+        let serve1_fut = registry.serve_process(process1);
         pin_mut!(serve1_fut);
         assert!(ex.run_until_stalled(&mut serve1_fut).is_pending());
 
@@ -170,12 +255,42 @@ mod tests {
         // Verify that the second process cannot be registered:
         // - serve_process should exit immediately
         // - list_process_koids should not list the koid twice
-        let serve2_fut = registry.serve_process(Arc::new(process2));
+        let serve2_fut = registry.serve_process(process2);
         pin_mut!(serve2_fut);
         assert!(ex.run_until_stalled(&mut serve2_fut).is_ready());
         assert_eq!(ex.run_singlethreaded(registry.list_processes()), [(koid, name1.to_string())]);
 
         // Verify that the first process stayed registered as if nothing happened.
         assert!(ex.run_until_stalled(&mut serve1_fut).is_pending());
+    }
+
+    #[test_case(fheapdump_client::ProcessSelector::ByName("foo".to_string()),
+        CollectorError::LiveSnapshotFailed ; "valid name, snapshot fails")]
+    #[test_case(fheapdump_client::ProcessSelector::ByName("bar".to_string()),
+        CollectorError::ProcessSelectorAmbiguous ; "ambiguous name")]
+    #[test_case(fheapdump_client::ProcessSelector::ByName("baz".to_string()),
+        CollectorError::ProcessSelectorNoMatch ; "no matching name")]
+    #[test_case(fheapdump_client::ProcessSelector::ByKoid(2),
+        CollectorError::LiveSnapshotFailed ; "valid koid, snapshot fails")]
+    #[test_case(fheapdump_client::ProcessSelector::ByKoid(99),
+        CollectorError::ProcessSelectorNoMatch ; "no matching koid")]
+    #[fasync::run_singlethreaded(test)]
+    async fn test_take_live_snapshot(
+        mut process_selector: fheapdump_client::ProcessSelector,
+        expect_error: CollectorError,
+    ) {
+        // Create three FakeProcess instances, two of which with the same name.
+        let (process1, _signal1) = FakeProcess::new("foo", Koid::from_raw(1));
+        let (process2, _signal2) = FakeProcess::new("bar", Koid::from_raw(2));
+        let (process3, _signal3) = FakeProcess::new("bar", Koid::from_raw(3));
+
+        // Create a Registry and a client connected to it.
+        let (_registry, proxy) = create_registry_and_proxy([process1, process2, process3]);
+
+        // Execute the request.
+        let result =
+            proxy.take_live_snapshot(&mut process_selector).await.expect("FIDL channel error");
+
+        assert_eq!(result.expect_err("request should fail"), expect_error);
     }
 }
