fb3598592df704d31abd1fdc18112d41655262ff
Kevin Cho <chok@google.com>
2023-03-14 20:51:46 +0000

    [lacewing] Repurpose example test as Mobly driver smoke test.
    * Created mobly_driver/tests/functional_tests dir
    * Updated test logic to assert on Mobly Driver functionalities.
    * Updated README.md
    * Updated botanist.json example to include new "type" field
    
    Change-Id: Ia7a17dfe43678bb04813a05f42229cefcb92450a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/818987
    Commit-Queue: Kevin Cho <chok@google.com>
    Reviewed-by: Pavan Kumar Juturu <jpavankumar@google.com>

---
+++ src/testing/end_to_end/mobly_driver/tests/functional_tests/smoke_test/mobly_driver_smoke_test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env fuchsia-vendored-python
+# Copyright 2023 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Mobly Driver smoke test.
+
+This E2E smoke test exercises the Mobly Driver. Specifically, it tests the
+following functionalities:
+
+1) Execution-environment dependent logic.
+2) Device config generation.
+3) Mobly test execution.
+
+The successful execution of this test in Fuchsia infra also proves the following
+infra integration points:
+1) Test parsing.
+2) Test artifact reporting.
+"""
+
+from mobly import asserts
+from mobly import base_test
+import fuchsia_device
+
+
+class MoblyDriverSmokeTest(base_test.BaseTestClass):
+    """Mobly Driver smoke tests."""
+
+    def test_mobly_controller_init(self):
+        """Test case to ensure Mobly controller initializes successfully"""
+        # Use Mobly's built-in register_controller() to parse configs and create
+        # devices controllers for test usage.
+        fuchsia_devices = self.register_controller(fuchsia_device)
+        asserts.assert_true(
+            fuchsia_devices, 'Expect at least 1 created controller.')
+
+    def test_params_exist(self):
+        """Test case to ensure test params are included in the Mobly config"""
+        asserts.assert_true(self.user_params, 'Test params are missing.')
