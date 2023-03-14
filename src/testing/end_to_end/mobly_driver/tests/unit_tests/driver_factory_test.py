29de8093d1a74a17539d8e5d43b0d2ad3e38ff04
Kevin Cho <chok@google.com>
2023-03-14 23:16:44 +0000

    [lacewing] Ensure pylint compliance for Mobly driver unit tests.
    Change-Id: I7489f6683f4fad9e2c2cc0f36fd66123440c3087
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/819487
    Commit-Queue: Kevin Cho <chok@google.com>
    Reviewed-by: Pavan Kumar Juturu <jpavankumar@google.com>

--- src/testing/end_to_end/mobly_driver/tests/unit_tests/driver_factory_test.py
+++ src/testing/end_to_end/mobly_driver/tests/unit_tests/driver_factory_test.py
@@ -2,6 +2,7 @@
 # Copyright 2023 The Fuchsia Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+"""Unit tests for Mobly driver's driver_factory.py."""
 
 import os
 import unittest
@@ -17,6 +18,7 @@ import local_driver
 
 
 class DriverFactoryTest(unittest.TestCase):
+    """Driver Factory tests"""
 
     @parameterized.expand(
         [
@@ -27,17 +29,20 @@ class DriverFactoryTest(unittest.TestCase):
                     api_infra.BOT_ENV_TEST_OUTDIR: '/tmp/log',
                 }, infra_driver.InfraDriver),
         ])
-    def test_get_driver_success(self, name, test_env, expected_driver_type):
+    def test_get_driver_success(
+            self, unused_name, test_env, expected_driver_type):
+        """Test case to ensure driver resolution success"""
         factory = driver_factory.DriverFactory()
         with mock.patch.dict(os.environ, test_env, clear=True):
             driver = factory.get_driver()
         self.assertEqual(type(driver), expected_driver_type)
 
     def test_get_driver_unexpected_env_raises_exception(self):
+        """Test case to ensure exception is raised on unexpected env"""
         factory = driver_factory.DriverFactory()
 
         # Undefined "api_infra.BOT_ENV_TEST_OUTDIR".
         invalid_infra_env = {api_infra.BOT_ENV_TESTBED_CONFIG: 'botanist.json'}
         with mock.patch.dict(os.environ, invalid_infra_env, clear=True):
             with self.assertRaises(common.DriverException):
-                driver = factory.get_driver()
+                factory.get_driver()
