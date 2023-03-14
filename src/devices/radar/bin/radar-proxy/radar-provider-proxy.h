8dd22d94197d6913b1b1e69f1c5e3f56097239f3
Braden Kell <bradenkell@google.com>
2023-03-14 14:31:36 +0000

    [radar-proxy] Refactor to prepare for burst injection changes
    - Make RadarProxy an abstract class, and add RadarProviderProxy to hold
      the implementation. A new RadarReaderProxy class will be added later,
      and will be used instead of RadarProviderProxy when injection is
      enabled.
    - Make RadarDeviceConnector take a callback in order to validate
      available radar devices. This will be helpful when the reader protocol
      is proxied and more steps are required to connect to devices.
    
    Bug: 100020
    Test: fx test radar-proxy-test
    Test: fx test radar-integration-test
    Change-Id: Ifcdd60f67c8c6b5223980d3d1762553a43859276
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/816707
    Commit-Queue: Braden Kell <bradenkell@google.com>
    Reviewed-by: Sam Hansen <hansens@google.com>

--- src/devices/radar/bin/radar-proxy/radar-provider-proxy.h
+++ src/devices/radar/bin/radar-proxy/radar-provider-proxy.h
@@ -1,58 +1,30 @@
-// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef SRC_DEVICES_RADAR_BIN_RADAR_PROXY_RADAR_PROXY_H_
-#define SRC_DEVICES_RADAR_BIN_RADAR_PROXY_RADAR_PROXY_H_
+#ifndef SRC_DEVICES_RADAR_BIN_RADAR_PROXY_RADAR_PROVIDER_PROXY_H_
+#define SRC_DEVICES_RADAR_BIN_RADAR_PROXY_RADAR_PROVIDER_PROXY_H_
 
-#include <fuchsia/hardware/radar/cpp/fidl.h>
-#include <lib/async/task.h>
-#include <lib/zx/result.h>
-
-#include <memory>
+#include "radar-proxy.h"
 
 namespace radar {
 
-class RadarDeviceConnector {
+class RadarProviderProxy : public RadarProxy {
  public:
-  // Synchronously connects to the given radar device and returns the client end. Calling threads
-  // must have a default dispatcher.
-  virtual fuchsia::hardware::radar::RadarBurstReaderProviderPtr ConnectToRadarDevice(
-      int dir_fd, const std::string& filename) = 0;
-
-  // Calls ConnectToRadarDevice() on all available devices, and returns the first one that is able
-  // to connect successfully. Calling threads must have a default dispatcher.
-  virtual fuchsia::hardware::radar::RadarBurstReaderProviderPtr ConnectToFirstRadarDevice() = 0;
-};
-
-class RadarProviderProxy : public fuchsia::hardware::radar::RadarBurstReaderProvider {
- public:
-  static constexpr char kRadarDeviceDirectory[] = "/dev/class/radar";
-
-  explicit RadarProviderProxy(RadarDeviceConnector* connector = nullptr);
+  explicit RadarProviderProxy(RadarDeviceConnector* connector);
 
   void Connect(fidl::InterfaceRequest<fuchsia::hardware::radar::RadarBurstReader> server,
                ConnectCallback callback) override;
 
-  // Called by a DeviceWatcher when /dev/class/radar has a new device.
-  void DeviceAdded(int dir_fd, const std::string& filename);
+  void DeviceAdded(int dir_fd, const std::string& filename) override;
 
  private:
-  class DefaultRadarDeviceConnector : public RadarDeviceConnector {
-   public:
-    fuchsia::hardware::radar::RadarBurstReaderProviderPtr ConnectToRadarDevice(
-        int dir_fd, const std::string& filename) override;
-    fuchsia::hardware::radar::RadarBurstReaderProviderPtr ConnectToFirstRadarDevice() override;
-  };
-
   void ErrorHandler(zx_status_t status);
 
-  RadarDeviceConnector* connector_;
+  RadarDeviceConnector* const connector_;
   fuchsia::hardware::radar::RadarBurstReaderProviderPtr radar_client_;
-  DefaultRadarDeviceConnector default_connector_;
-  bool new_devices_ = false;
 };
 
 }  // namespace radar
 
-#endif  // SRC_DEVICES_RADAR_BIN_RADAR_PROXY_RADAR_PROXY_H_
+#endif  // SRC_DEVICES_RADAR_BIN_RADAR_PROXY_RADAR_PROVIDER_PROXY_H_
---
+++ src/devices/radar/bin/radar-proxy/radar-proxy.h
@@ -0,0 +1,55 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_DEVICES_RADAR_BIN_RADAR_PROXY_RADAR_PROXY_H_
+#define SRC_DEVICES_RADAR_BIN_RADAR_PROXY_RADAR_PROXY_H_
+
+#include <fuchsia/hardware/radar/cpp/fidl.h>
+#include <lib/fit/function.h>
+
+#include <filesystem>
+#include <string>
+
+#include "src/lib/fsl/io/device_watcher.h"
+
+namespace radar {
+
+class RadarDeviceConnector {
+ public:
+  // Called on zero or more eligible radar devices. If true is returned, the device is usable and
+  // the callback should not be invoked again. Otherwise, the device is not usable, and the search
+  // for a suitable device should continue.
+  using ConnectDeviceCallback = fit::function<bool(
+      fidl::InterfaceHandle<fuchsia::hardware::radar::RadarBurstReaderProvider>)>;
+
+  // Synchronously connects to the given radar device and passes the client end to the callback. The
+  // callback is not called if the connection could not be made. The callback return value is
+  // ignored.
+  virtual void ConnectToRadarDevice(int dir_fd, const std::filesystem::path& path,
+                                    ConnectDeviceCallback connect_device) = 0;
+
+  // Calls ConnectToRadarDevice() on available devices until the callback returns true.
+  virtual void ConnectToFirstRadarDevice(ConnectDeviceCallback connect_device) = 0;
+};
+
+class RadarProxy : public fuchsia::hardware::radar::RadarBurstReaderProvider {
+ public:
+  static constexpr char kRadarDeviceDirectory[] = "/dev/class/radar";
+
+  RadarProxy()
+      : device_watcher_(fsl::DeviceWatcher::Create(
+            kRadarDeviceDirectory,
+            [&](int dir_fd, const std::string& filename) { DeviceAdded(dir_fd, filename); })) {}
+
+  // Called by a DeviceWatcher when /dev/class/radar has a new device, and for each existing device
+  // during construction.
+  virtual void DeviceAdded(int dir_fd, const std::string& filename) = 0;
+
+ private:
+  std::unique_ptr<fsl::DeviceWatcher> device_watcher_;
+};
+
+}  // namespace radar
+
+#endif  // SRC_DEVICES_RADAR_BIN_RADAR_PROXY_RADAR_PROXY_H_
