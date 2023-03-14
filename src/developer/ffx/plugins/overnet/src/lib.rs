7613d4531afea57fe792cd1c31dabffb139e9d26
Megan Batty <mgnb@google.com>
2023-03-14 23:31:00 +0000

    [ffx][overnet] Migrate to dual-mode subtool
    Change-Id: I7779c53bb750f725e86e784d1c893a9625f24ebe
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/803817
    Reviewed-by: Casey Dahlin <sadmac@google.com>
    Fuchsia-Auto-Submit: Megan Batty <mgnb@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/developer/ffx/plugins/overnet/src/lib.rs
+++ src/developer/ffx/plugins/overnet/src/lib.rs
@@ -2,11 +2,27 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use anyhow::Error;
+use anyhow as _; // all plugins are currently forced to include anyhow, so void it.
+use ffx_overnet_plugin_args::OvernetCommand;
+use fho::{FfxMain, FfxTool, SimpleWriter};
 
-#[ffx_core::ffx_plugin()]
-pub async fn onet(cmd: ffx_overnet_plugin_args::OvernetCommand) -> Result<(), Error> {
-    // todo(fxb/108692) remove this use of the global hoist when we put the main one in the environment context
-    // instead.
-    onet_tool::run_onet(hoist::hoist(), onet_tool::Opts { command: cmd.command }).await
+#[derive(FfxTool)]
+pub struct OvernetTool {
+    #[command]
+    cmd: OvernetCommand,
+}
+
+fho::embedded_plugin!(OvernetTool);
+
+#[async_trait::async_trait(?Send)]
+impl FfxMain for OvernetTool {
+    type Writer = SimpleWriter;
+
+    async fn main(self, _writer: Self::Writer) -> fho::Result<()> {
+        // todo(fxb/108692) remove this use of the global hoist when we put the main one in the environment context
+        // instead.
+        onet_tool::run_onet(hoist::hoist(), onet_tool::Opts { command: self.cmd.command })
+            .await
+            .map_err(Into::into)
+    }
 }
