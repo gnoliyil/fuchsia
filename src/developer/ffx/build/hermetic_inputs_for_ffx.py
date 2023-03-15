cf107d1b01b2e59c873f9655cc0b398eb853758b
Megan Batty <mgnb@google.com>
2023-03-15 18:03:18 +0000

    [ffx] Allow an `ffx_action` to not use the sdk
    The ffx action in `ffx_tool` not only doesn't need the sdk, the files it
    produces are inputs *to* the SDK. This makes it so that it can still use
    the `ffx_action` template but not pull in the entire (modular) sdk in to
    do so, since it doesn't need it.
    
    Fixed: 123388
    Change-Id: I3385a5d2fbd16df656760aa8379a1a36073cf99c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/815082
    Reviewed-by: Andrew Davies <awdavies@google.com>
    Reviewed-by: David Turner <digit@google.com>
    Commit-Queue: Megan Batty <mgnb@google.com>

--- src/developer/ffx/build/hermetic_inputs_for_ffx.py
+++ src/developer/ffx/build/hermetic_inputs_for_ffx.py
@@ -17,7 +17,6 @@ def main():
     parser.add_argument(
         '--sdk-manifest',
         type=argparse.FileType('r'),
-        required=True,
         help='The path to the SDK manifest')
     parser.add_argument(
         '--additional-hermetic-inputs',
@@ -31,12 +30,13 @@ def main():
     args = parser.parse_args()
 
     inputs = []
-    sdk_manifest = json.load(args.sdk_manifest)
-    sdk_atoms = sdk_manifest.get('atoms', [])
-    for atom in sdk_atoms:
-        files = atom.get('files', [])
-        files = [entry['source'] for entry in files]
-        inputs.extend(files)
+    if args.sdk_manifest:
+        sdk_manifest = json.load(args.sdk_manifest)
+        sdk_atoms = sdk_manifest.get('atoms', [])
+        for atom in sdk_atoms:
+            files = atom.get('files', [])
+            files = [entry['source'] for entry in files]
+            inputs.extend(files)
     if args.additional_hermetic_inputs:
         additional_inputs = args.additional_hermetic_inputs.readlines()
         additional_inputs = [i.strip() for i in additional_inputs]
