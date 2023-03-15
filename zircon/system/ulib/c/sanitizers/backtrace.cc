7fe5f909c47c101c47108321887c5aa8cfde9297
Roland McGrath <mcgrathr@google.com>
2023-03-15 21:09:03 +0000

    [libc] Use <lib/arch/backtrace.h> for __sanitizer_fast_backtrace
    The API used for kernel code is not kernel-specific and handles
    the arch nuances in one place for all the machines.
    
    Bug: 122877
    Change-Id: Ia265130ddfc97a0134273f63d03d2d800220b8ba
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/818050
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Reviewed-by: Paul Kirth <paulkirth@google.com>

--- zircon/system/ulib/c/sanitizers/backtrace.cc
+++ zircon/system/ulib/c/sanitizers/backtrace.cc
@@ -4,85 +4,41 @@
 
 #include "backtrace.h"
 
+#include <lib/arch/backtrace.h>
+
 #include "threads_impl.h"
 
 namespace __libc_sanitizer {
 
 size_t BacktraceByFramePointer(cpp20::span<uintptr_t> pcs) {
-  struct FramePointer {
-    const FramePointer* fp;
-    uintptr_t pc;
-  };
-
-  auto on_stack = [&stack = __pthread_self()->safe_stack](const FramePointer* fp) -> bool {
-    uintptr_t address = reinterpret_cast<uintptr_t>(fp);
-    return address >= reinterpret_cast<uintptr_t>(stack.iov_base) &&
-           address < reinterpret_cast<uintptr_t>(stack.iov_base) + stack.iov_len;
-  };
-
-  uintptr_t ra = reinterpret_cast<uintptr_t>(__builtin_return_address(0));
-  auto fp = reinterpret_cast<const FramePointer*>(__builtin_frame_address(0));
-  size_t i = 0;
-  while (i < pcs.size() && on_stack(fp) && fp->pc != 0) {
-    if (i == 0 && fp->pc != ra) {
-      pcs[i++] = ra;
-    } else {
-      pcs[i++] = fp->pc;
-      fp = fp->fp;
+  struct IsOnStack {
+    bool operator()(const arch::CallFrame* fp) const {
+      const iovec& stack = __pthread_self()->safe_stack;
+      if (stack.iov_len < sizeof(*fp)) [[unlikely]] {
+        // This should be impossible, but assume nothing in a critical
+        // error-reporting path since this might be used after clobberation.
+        return false;
+      }
+      const uintptr_t base = reinterpret_cast<uintptr_t>(stack.iov_base);
+      const uintptr_t frame = reinterpret_cast<uintptr_t>(fp);
+      return frame >= base && frame - base <= stack.iov_len - sizeof(*fp);
     }
-  }
-  if (i == 0 && i < pcs.size()) {
-    pcs[i++] = ra;
-  }
+  };
+  using FpBacktrace = arch::FramePointerBacktrace<IsOnStack>;
 
-  return i;
+  return arch::StoreBacktrace(FpBacktrace::BackTrace(), pcs, __builtin_return_address(0));
 }
 
 #if __has_feature(shadow_call_stack)
 
-namespace {
-
-// This is actually defined with internal linkage in asm, below.
-// It's defined inside the function so it can travel in its section group.
-extern "C" uintptr_t GetShadowCallStackPointer();
-
-}  // namespace
-
 size_t BacktraceByShadowCallStack(cpp20::span<uintptr_t> pcs) {
-  // Fetch the current shadow call stack pointer.  This isn't done
-  // with direct inline asm so that we can be sure that the compiler
-  // has pushed our own frame's return address before we collect it.
-  uintptr_t sp = GetShadowCallStackPointer();
-#ifdef __aarch64__
-  __asm__(
-      R"""(
-      .pushsection .text.GetShadowCallStackPointer, "ax?", %progbits
-      .type GetShadowCallStackPointer, %function
-      GetShadowCallStackPointer:
-        mov x0, x18
-        ret
-      .size GetShadowCallStackPointer, . - GetShadowCallStackPointer
-      .popsection
-      )""");
-#else
-#error "what machine?"
-#endif
-
-  const iovec& stack_block = __pthread_self()->shadow_call_stack;
-  uintptr_t stack_base = reinterpret_cast<uintptr_t>(stack_block.iov_base);
-  uintptr_t stack_limit = stack_base + stack_block.iov_len;
-
-  if (sp < stack_base || sp > stack_limit || sp % sizeof(uintptr_t) != 0) {
-    return 0;
-  }
-
-  const uintptr_t* next_pc = reinterpret_cast<const uintptr_t*>(sp);
-  const uintptr_t* last_pc = reinterpret_cast<const uintptr_t*>(stack_base);
-  size_t i = 0;
-  while (i < pcs.size() && next_pc > last_pc && *--next_pc != 0) {
-    pcs[i++] = *next_pc;
-  }
-  return i;
+  const iovec& shadow_call_stack_block = __pthread_self()->shadow_call_stack;
+  return arch::StoreBacktrace(
+      arch::ShadowCallStackBacktrace{
+          {static_cast<const uintptr_t*>(shadow_call_stack_block.iov_base),
+           shadow_call_stack_block.iov_len / sizeof(uintptr_t)},
+          arch::GetShadowCallStackPointer()},
+      pcs, __builtin_return_address(0));
 }
 
 #endif  // __has_feature(shadow_call_stack)
