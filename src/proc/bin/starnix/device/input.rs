db4fd5fbc4744763fc8216523b821abb38550acd
Mukesh Agrawal <quiche@google.com>
2023-03-15 00:36:27 +0000

    [starnix] start filling in input implementation
    1. Recognize common input ioctls from guest system.
    2. Allow guest to `epoll()` the input node. This initial
       implementation leaving the caller waiting forever.
    3. Add `framebuffer` feature to chromiumos syscalls test,
       to enable writing integration tests which exercise
       `input.rs`.
    4. Add integration tests which verify that `input.rs`:
       a) responds to `ioctl()`-s about the input device's
          features/properties with data indicating that
          the device is a touchscreen
       b) allows clients to register the device with `epoll()`
    
    Bug: 123430
    Bug: 123091
    Test: fx test test_chromiumos_syscalls --test-filter="Input*"
    Change-Id: Ib4ce0b5e9c1173b2f6d3a32e6c134936f8780b7b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/816488
    Reviewed-by: Vickie Cheng <vickiecheng@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Mukesh Agrawal <quiche@google.com>

--- src/proc/bin/starnix/device/input.rs
+++ src/proc/bin/starnix/device/input.rs
@@ -6,17 +6,55 @@ use crate::device::DeviceOps;
 use crate::fs::buffers::{InputBuffer, OutputBuffer};
 use crate::fs::*;
 use crate::logging::*;
+use crate::mm::MemoryAccessorExt;
 use crate::syscalls::SyscallResult;
-use crate::task::CurrentTask;
+use crate::syscalls::SUCCESS;
+use crate::task::{CurrentTask, EventHandler, WaitKey, Waiter};
 use crate::types::*;
 
 use std::sync::Arc;
 
-pub struct InputFile {}
+pub struct InputFile {
+    driver_version: u32,
+    input_id: uapi::input_id,
+    supported_keys: BitSet<{ min_bytes(KEY_CNT) }>,
+    supported_position_attributes: BitSet<{ min_bytes(ABS_CNT) }>, // ABSolute position
+    supported_motion_attributes: BitSet<{ min_bytes(REL_CNT) }>,   // RELative motion
+    supported_switches: BitSet<{ min_bytes(SW_CNT) }>,
+    supported_leds: BitSet<{ min_bytes(LED_CNT) }>,
+    supported_haptics: BitSet<{ min_bytes(FF_CNT) }>, // 'F'orce 'F'eedback
+    supported_misc_features: BitSet<{ min_bytes(MSC_CNT) }>,
+    properties: BitSet<{ min_bytes(INPUT_PROP_CNT) }>,
+    waiter: Waiter,
+}
 
 impl InputFile {
+    // Per https://www.linuxjournal.com/article/6429, the driver version is 32-bits wide,
+    // and interpreted as:
+    // * [31-16]: version
+    // * [15-08]: minor
+    // * [07-00]: patch level
+    const DRIVER_VERSION: u32 = 0;
+
+    // Per https://www.linuxjournal.com/article/6429, the bus type should be populated with a
+    // sensible value, but other fields may not be.
+    const INPUT_ID: uapi::input_id =
+        uapi::input_id { bustype: BUS_VIRTUAL as u16, product: 0, vendor: 0, version: 0 };
+
     pub fn new() -> Arc<Self> {
-        Arc::new(Self {})
+        Arc::new(Self {
+            driver_version: Self::DRIVER_VERSION,
+            input_id: Self::INPUT_ID,
+            supported_keys: touch_key_attributes(),
+            supported_position_attributes: touch_position_attributes(),
+            supported_motion_attributes: BitSet::new(), // None supported, not a mouse.
+            supported_switches: BitSet::new(),          // None supported
+            supported_leds: BitSet::new(),              // None supported
+            supported_haptics: BitSet::new(),           // None supported
+            supported_misc_features: BitSet::new(),     // None supported
+            properties: touch_properties(),
+            waiter: Waiter::new(),
+        })
     }
 }
 
@@ -39,11 +77,71 @@ impl FileOps for Arc<InputFile> {
         &self,
         _file: &FileObject,
         current_task: &CurrentTask,
-        _request: u32,
-        _user_addr: UserAddress,
+        request: u32,
+        user_addr: UserAddress,
     ) -> Result<SyscallResult, Errno> {
-        not_implemented!(current_task, "ioctl() on input device");
-        error!(EOPNOTSUPP)
+        match request {
+            uapi::EVIOCGVERSION => {
+                current_task.mm.write_object(UserRef::new(user_addr), &self.driver_version)?;
+                Ok(SUCCESS)
+            }
+            uapi::EVIOCGID => {
+                current_task.mm.write_object(UserRef::new(user_addr), &self.input_id)?;
+                Ok(SUCCESS)
+            }
+            uapi::EVIOCGBIT_EV_KEY => {
+                current_task
+                    .mm
+                    .write_object(UserRef::new(user_addr), &self.supported_keys.bytes)?;
+                Ok(SUCCESS)
+            }
+            uapi::EVIOCGBIT_EV_ABS => {
+                current_task.mm.write_object(
+                    UserRef::new(user_addr),
+                    &self.supported_position_attributes.bytes,
+                )?;
+                Ok(SUCCESS)
+            }
+            uapi::EVIOCGBIT_EV_REL => {
+                current_task.mm.write_object(
+                    UserRef::new(user_addr),
+                    &self.supported_motion_attributes.bytes,
+                )?;
+                Ok(SUCCESS)
+            }
+            uapi::EVIOCGBIT_EV_SW => {
+                current_task
+                    .mm
+                    .write_object(UserRef::new(user_addr), &self.supported_switches.bytes)?;
+                Ok(SUCCESS)
+            }
+            uapi::EVIOCGBIT_EV_LED => {
+                current_task
+                    .mm
+                    .write_object(UserRef::new(user_addr), &self.supported_leds.bytes)?;
+                Ok(SUCCESS)
+            }
+            uapi::EVIOCGBIT_EV_FF => {
+                current_task
+                    .mm
+                    .write_object(UserRef::new(user_addr), &self.supported_haptics.bytes)?;
+                Ok(SUCCESS)
+            }
+            uapi::EVIOCGBIT_EV_MSC => {
+                current_task
+                    .mm
+                    .write_object(UserRef::new(user_addr), &self.supported_misc_features.bytes)?;
+                Ok(SUCCESS)
+            }
+            uapi::EVIOCGPROP => {
+                current_task.mm.write_object(UserRef::new(user_addr), &self.properties.bytes)?;
+                Ok(SUCCESS)
+            }
+            _ => {
+                not_implemented!(current_task, "ioctl() {} on input device", request);
+                error!(EOPNOTSUPP)
+            }
+        }
     }
 
     fn read(
@@ -65,4 +163,69 @@ impl FileOps for Arc<InputFile> {
         not_implemented!(current_task, "write() on input device");
         error!(EOPNOTSUPP)
     }
+
+    fn wait_async(
+        &self,
+        _file: &FileObject,
+        current_task: &CurrentTask,
+        _waiter: &Waiter,
+        _events: FdEvents,
+        _handler: EventHandler,
+    ) -> WaitKey {
+        not_implemented!(current_task, "wait_async() on input device will block forever");
+        self.waiter.fake_wait()
+    }
+
+    fn cancel_wait(&self, current_task: &CurrentTask, _waiter: &Waiter, _key: WaitKey) {
+        not_implemented!(current_task, "cancel_wait() on input device");
+    }
+
+    fn query_events(&self, current_task: &CurrentTask) -> FdEvents {
+        not_implemented!(current_task, "query_events() on input device");
+        FdEvents::empty()
+    }
+}
+
+struct BitSet<const NUM_BYTES: usize> {
+    bytes: [u8; NUM_BYTES],
+}
+
+impl<const NUM_BYTES: usize> BitSet<{ NUM_BYTES }> {
+    const fn new() -> Self {
+        Self { bytes: [0; NUM_BYTES] }
+    }
+
+    fn set(&mut self, bitnum: u32) {
+        let bitnum = bitnum as usize;
+        let byte = bitnum / 8;
+        let bit = bitnum % 8;
+        self.bytes[byte] |= 1 << bit;
+    }
+}
+
+/// Returns the minimum number of bytes required to store `n_bits` bits.
+const fn min_bytes(n_bits: u32) -> usize {
+    ((n_bits as usize) + 7) / 8
+}
+
+/// Returns appropriate `KEY`-board related flags for a touchscreen device.
+fn touch_key_attributes() -> BitSet<{ min_bytes(KEY_CNT) }> {
+    let mut attrs = BitSet::new();
+    attrs.set(BTN_TOUCH);
+    attrs
+}
+
+/// Returns appropriate `ABS`-olute position related flags for a touchscreen device.
+fn touch_position_attributes() -> BitSet<{ min_bytes(ABS_CNT) }> {
+    let mut attrs = BitSet::new();
+    attrs.set(ABS_X);
+    attrs.set(ABS_Y);
+    attrs
+}
+
+/// Returns appropriate `INPUT_PROP`-erties for a touchscreen device.
+fn touch_properties() -> BitSet<{ min_bytes(INPUT_PROP_CNT) }> {
+    let mut attrs = BitSet::new();
+    attrs.set(INPUT_PROP_DIRECT);
+    attrs
 }
