6836efaee32c9c1fb42e05c890f1fe4342d3e836
Alex Konradi <akonradi@google.com>
2023-03-15 13:50:26 +0000

    [netstack3] Document lock_ordering module
    Add a module-level doc comment explaining how the lock_ordering types
    are used and why the lock ordering for Netstack3 is linearized.
    
    Bug: 120973
    Change-Id: Ie73081c47ff8090b5ad673d4a0be4a3d534f36a5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/818588
    Commit-Queue: Alex Konradi <akonradi@google.com>
    Reviewed-by: Ghanan Gowripalan <ghanan@google.com>
    Fuchsia-Auto-Submit: Alex Konradi <akonradi@google.com>
    Reviewed-by: Evan Wildenhain <wildenhain@google.com>

--- src/connectivity/network/netstack3/core/src/lock_ordering.rs
+++ src/connectivity/network/netstack3/core/src/lock_ordering.rs
@@ -2,6 +2,104 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+//! Lock ordering for Netstack3 core.
+//!
+//! This module contains the "lock ordering" for Netstack3 core: it describes
+//! the order in which additional locks can be acquired while other locks are
+//! held. In general, code that is written to avoid deadlocks must respect the
+//! same lock ordering.
+//!
+//! Netstack3 core relies on the [`lock_order`] crate to ensure that all
+//! possible code paths respect the lock ordering defined here. By leveraging
+//! the types and traits from `lock_order`, we can guarantee at compile time
+//! that there are no opportunities for deadlock. The way this works is that
+//! each lock in [`SyncCtx`] and its associated per-device state gets assigned a
+//! type in this module. Then, for a pair of locks `A` and `B`, where `B` is
+//! allowed to be acquired while `A` is locked, there is a corresponding
+//! declaration of the [`lock_order::relation::LockAfter`] trait (done via the
+//! [`impl_lock_after`] macro).
+//!
+//! Notionally, the lock ordering forms a [directed acyclic graph (DAG)], where
+//! the nodes in the graph are locks that can be acquired, and each edge
+//! represents a `LockAfter` implementation. For a lock `B` to be acquired while
+//! some lock `A` is held, the node for `B` in the graph must be reachable from
+//! `A`, either via direct edge or via an indirect path that traverses
+//! other nodes. This can also be thought of as a [strict partial order] between
+//! locks, where `A < B` means `B` can be acquired with `A` held.
+//!
+//! Ideally we'd represent the lock ordering here that way too, but it doesn't
+//! work well with the blanket impls of `LockAfter`. The problem can be
+//! illustrated by this reduced example. Suppose we have a lock ordering DAG
+//! like this:
+//!
+//! ```text
+//! ┌────────────────────┐
+//! │LoopbackRx          │
+//! └┬──────────────────┬┘
+//! ┌▽─────────────┐  ┌─▽────────────┐
+//! │IpState<Ipv4> │  │IpState<Ipv6> │
+//! └┬─────────────┘  └┬─────────────┘
+//! ┌▽─────────────────▽┐
+//! │DevicesState       │
+//! └───────────────────┘
+//!```
+//!
+//! With the blanket `LockAfter` impls, we'd get this:
+//! ```
+//! impl<C: LockAfter<IpState<Ipv4>>> LockAfter<LoopbackRx> for C {}
+//! // and
+//! impl<C: LockAfter<IpState<Ipv6>>> LockAfter<LoopbackRx> for C {}
+//! ```
+//!
+//! Since `C` could be `DevicesState`, we'd have duplicate impls of
+//! `LockAfter<LoopbackRx> for DevicesState`.
+//!
+//! To work around this, we pick a different graph for the lock ordering that
+//! won't produce duplicate blanket impls. The property we need is that every
+//! path in the original graph is also present in our alternate graph. Luckily,
+//! graph theory proves that this is always possible: a [topological sort] has
+//! precisely this property, and every DAG has at least one topological sort.
+//! For the graph above, that could look something like this:
+//!
+//! ```text
+//! ┌────────────────────┐
+//! │LoopbackRx          │
+//! └┬───────────────────┘
+//! ┌▽─────────────┐
+//! │IpState<Ipv4> │
+//! └┬─────────────┘
+//! ┌▽─────────────┐
+//! │IpState<Ipv6> │
+//! └┬─────────────┘
+//! ┌▽──────────────────┐
+//! │DevicesState       │
+//! └───────────────────┘
+//! ```
+//!
+//! Note that every possible lock ordering path in the original graph is present
+//! (directly or transitively) in the second graph. There are additional paths,
+//! like `IpState<Ipv4> -> IpState<Ipv6>`, but those don't matter since
+//!   a) if they become load-bearing, they need to be in the original DAG, and
+//!   b) load-bearing or not, the result is still a valid lock ordering graph.
+//!
+//! The lock ordering described in this module is likewise a modification of
+//! the ideal graph. Instead of specifying the actual DAG, we make nice to the
+//! Rust compiler so we can use the transitive blanket impls instead. Since the
+//! graph doesn't have multiple paths between any two nodes (that's what causes
+//! the duplicate blanket impls), it ends up being a [multitree]. While we could
+//! turn it into a (linear) total ordering, we keep the tree structure so that
+//! we can more faithfully represent that some locks can't be held at the same
+//! time by putting them on different branches.
+//!
+//! If, in the future, someone comes up with a better way to declare these, we
+//! should be able to migrate the definitions here without affecting the usages.
+//!
+//! [`SyncCtx`]: crate::SyncCtx
+//! [directed acyclic graph (DAG)]: https://en.wikipedia.org/wiki/Directed_acyclic_graph
+//! [strict partial order]: https://en.wikipedia.org/wiki/Partially_ordered_set#Strict_partial_orders
+//! [topological sort]: https://en.wikipedia.org/wiki/Topological_sorting
+//! [multitree]: https://en.wikipedia.org/wiki/Multitree
+
 pub(crate) use lock_order::Unlocked;
 
 use core::{convert::Infallible as Never, marker::PhantomData};
