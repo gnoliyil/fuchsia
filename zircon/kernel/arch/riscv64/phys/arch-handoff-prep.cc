5415d9eb5f7b77bd10780b21e66324af8bb9a245
Travis Geiselbrecht <travisg@google.com>
2023-03-16 02:44:28 +0000

    [kernel][riscv][phys] pass PLIC and Generic timer through ArchPhysHandoff
    Wire through ZBI entries concerning the PLIC and RISCV generic timer
    through physboot to the kernel handoff struct.
    
    Bug: 122873
    Change-Id: I6a8a8335cce07a157bda5d3fa1e4594fac328982
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/820287
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Fuchsia-Auto-Submit: Travis Geiselbrecht <travisg@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- zircon/kernel/arch/riscv64/phys/arch-handoff-prep.cc
+++ zircon/kernel/arch/riscv64/phys/arch-handoff-prep.cc
@@ -23,4 +23,25 @@ void HandoffPrep::ArchHandoff() {
 void HandoffPrep::ArchSummarizeMiscZbiItem(const zbi_header_t& header,
                                            ktl::span<const ktl::byte> payload) {
   ZX_DEBUG_ASSERT(handoff_);
+  ArchPhysHandoff& arch_handoff = handoff_->arch_handoff;
+
+  switch (header.type) {
+    case ZBI_TYPE_KERNEL_DRIVER: {
+      switch (header.extra) {
+        case ZBI_KERNEL_DRIVER_RISCV_PLIC:
+          ZX_ASSERT(payload.size() >= sizeof(zbi_dcfg_riscv_plic_driver_t));
+          arch_handoff.plic_driver =
+              *reinterpret_cast<const zbi_dcfg_riscv_plic_driver_t*>(payload.data());
+          SaveForMexec(header, payload);
+          break;
+        case ZBI_KERNEL_DRIVER_RISCV_GENERIC_TIMER:
+          ZX_ASSERT(payload.size() >= sizeof(zbi_dcfg_riscv_generic_timer_driver_t));
+          arch_handoff.generic_timer_driver =
+              *reinterpret_cast<const zbi_dcfg_riscv_generic_timer_driver_t*>(payload.data());
+          SaveForMexec(header, payload);
+          break;
+      }
+      break;
+    }
+  }
 }
--- zircon/kernel/arch/riscv64/phys/include/phys/arch/arch-handoff.h
+++ zircon/kernel/arch/riscv64/phys/include/phys/arch/arch-handoff.h
@@ -7,12 +7,21 @@
 #ifndef ZIRCON_KERNEL_ARCH_RISCV64_PHYS_INCLUDE_PHYS_ARCH_ARCH_HANDOFF_H_
 #define ZIRCON_KERNEL_ARCH_RISCV64_PHYS_INCLUDE_PHYS_ARCH_ARCH_HANDOFF_H_
 
-#include <stdint.h>
+#include <zircon/boot/driver-config.h>
+
+#include <ktl/optional.h>
+#include <ktl/variant.h>
 
 // This holds (or points to) all riscv64-specific data that is handed off from
 // physboot to the kernel proper at boot time.
 struct ArchPhysHandoff {
   uint64_t boot_hart_id;
+
+  // (ZBI_TYPE_KERNEL_DRIVER, ZBI_KERNEL_DRIVER_RISCV_PLIC) payload.
+  ktl::optional<zbi_dcfg_riscv_plic_driver_t> plic_driver;
+
+  // (ZBI_TYPE_KERNEL_DRIVER, ZBI_KERNEL_DRIVER_RISCV_GENERIC_TIMER) payload.
+  ktl::optional<zbi_dcfg_riscv_generic_timer_driver_t> generic_timer_driver;
 };
 
 #endif  // ZIRCON_KERNEL_ARCH_RISCV64_PHYS_INCLUDE_PHYS_ARCH_ARCH_HANDOFF_H_
