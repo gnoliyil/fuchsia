b9938e48c287ac6140a0ce928cb1f69aa4464c3b
Louis Yung-Chieh Lo <yjlou@google.com>
2023-03-17 02:09:35 +0000

    [iwlwifi][uprev] null pointer check in iwl_pcie_txq_unmap()
    Bug: 106615
    
    Change-Id: I5318b8bde7b723ebdbf4a9951d62526fb25875de
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/732729
    Reviewed-by: John Tan <johntan@google.com>
    Commit-Queue: Louis Lo <yjlou@google.com>

--- src/connectivity/wlan/drivers/third_party/intel/iwlwifi/pcie/tx.c
+++ src/connectivity/wlan/drivers/third_party/intel/iwlwifi/pcie/tx.c
@@ -577,6 +577,11 @@ void iwl_pcie_txq_unmap(struct iwl_trans* trans, int txq_id) {
   struct iwl_trans_pcie* trans_pcie = IWL_TRANS_GET_PCIE_TRANS(trans);
   struct iwl_txq* txq = trans_pcie->txq[txq_id];
 
+  if (!txq) {
+    IWL_ERR(trans, "Trying to free a queue that wasn't allocated?\n");
+    return;
+  }
+
   mtx_lock(&txq->lock);
   while (txq->write_ptr != txq->read_ptr) {
     IWL_DEBUG_TX_REPLY(trans, "Q %d Free %d\n", txq_id, txq->read_ptr);
