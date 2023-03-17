dc3af462df2fddbdf17210889dce281f099063de
Roland McGrath <mcgrathr@google.com>
2023-03-17 19:11:01 +0000

    [kernel][userabi] Define libc++ internal panic function in userboot
    Bug: 123630
    Change-Id: If5ea212d711d1d5b245c9a7e8c850de3f7e0bb4a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/820829
    Reviewed-by: Daniel Thornburgh <dthorn@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>

--- zircon/kernel/lib/userabi/userboot/zx_panic.cc
+++ zircon/kernel/lib/userabi/userboot/zx_panic.cc
@@ -4,23 +4,42 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT
 
+#include <stdarg.h>
 #include <zircon/assert.h>
 #include <zircon/compiler.h>
 
+#include <__verbose_abort>
+
 #include "util.h"
 
-extern "C" __NO_RETURN __PRINTFLIKE(1, 2) void __zx_panic(const char* fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
+namespace {
+
+[[noreturn]] void vpanic(const char* fmt, va_list ap) {
   // This uses zx_debug_write().  The log handle isn't plumbed to __zx_panic.
   vprintl(zx::debuglog{}, fmt, ap);
   va_end(ap);
 
-  // Because the zx::debuglog{} is default constructed above, vprintl() will use zx_debug_write(),
-  // and in that path vprintl() will explicitly output a \n after whatever is written via vprintl()
-  // above.  So we don't need/want a printl(zx::debuglog{}, "\n") here (at least for now).  This
-  // intentionally differs from other __zx_panic() implementations that have the \n added in
+  // Because the zx::debuglog{} is default constructed above, vprintl() will
+  // use zx_debug_write(), and in that path vprintl() will explicitly output a
+  // \n after whatever is written via vprintl() above.  So we don't need/want a
+  // printl(zx::debuglog{}, "\n") here (at least for now).  This intentionally
+  // differs from other __zx_panic() implementations that have the \n added in
   // __zx_panic().
 
   __builtin_trap();
+}
+
+}  // namespace
+
+extern "C" __NO_RETURN __PRINTFLIKE(1, 2) void __zx_panic(const char* fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vpanic(fmt, ap);
+}
+
+// Inline functions in libc++ headers call this.
+[[noreturn]] void std::__libcpp_verbose_abort(const char* format, ...) {
+  va_list ap;
+  va_start(ap, format);
+  vpanic(format, ap);
 }
