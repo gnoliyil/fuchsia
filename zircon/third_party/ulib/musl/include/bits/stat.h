567a41bbb978d4ba2a647a01e745894b19d529cb
Alex Brachet <abrachet@google.com>
2023-03-17 20:16:58 +0000

    [libc] Remove per-arch headers for stat.h
    We aren't bound by any kernel definition and no existing
    code depends on the layout of struct stat for riscv on
    Fuchsia.
    
    Bug: 123052
    Change-Id: Ife254f1ef5b4dcedeae7429ba8dd3cffbef445bf
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/820267
    Fuchsia-Auto-Submit: Alex Brachet <abrachet@google.com>
    Commit-Queue: Alex Brachet <abrachet@google.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    API-Review: Roland McGrath <mcgrathr@google.com>

--- zircon/third_party/ulib/musl/include/bits/stat.h
+++ zircon/third_party/ulib/musl/include/bits/stat.h
@@ -1,7 +1,40 @@
-#if defined(__x86_64__)
-#include "x86_64/stat.h"
-#elif defined(__aarch64__)
-#include "aarch64/stat.h"
+#ifdef __aarch64__
+struct stat {
+  dev_t st_dev;
+  ino_t st_ino;
+  mode_t st_mode;
+  nlink_t st_nlink;
+  uid_t st_uid;
+  gid_t st_gid;
+  dev_t st_rdev;
+  unsigned long __pad;
+  off_t st_size;
+  blksize_t st_blksize;
+  int __pad2;
+  blkcnt_t st_blocks;
+  struct timespec st_atim;
+  struct timespec st_mtim;
+  struct timespec st_ctim;
+  unsigned __unused1[2];
+};
 #else
-#error Unsupported architecture!
+struct stat {
+  dev_t st_dev;
+  ino_t st_ino;
+  nlink_t st_nlink;
+
+  mode_t st_mode;
+  uid_t st_uid;
+  gid_t st_gid;
+  unsigned int __pad0;
+  dev_t st_rdev;
+  off_t st_size;
+  blksize_t st_blksize;
+  blkcnt_t st_blocks;
+
+  struct timespec st_atim;
+  struct timespec st_mtim;
+  struct timespec st_ctim;
+  long __unused1[3];
+};
 #endif
