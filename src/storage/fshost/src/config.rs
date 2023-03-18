7cff3c6a50f936498ee0d6ca7ee2b371fbd94e13
James Sullivan <jfsulliv@google.com>
2023-03-18 00:25:35 +0000

    [fshost] Remove use_native_fxfs_crypto
    This flag is only used on workstation.
    
    This is a breaking change that will result in data loss on Workstation
    devices.
    
    Fixed: 121502
    Change-Id: I8881289046c951b64948f8edd8729d8f0f955c5a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/820496
    Commit-Queue: James Sullivan <jfsulliv@google.com>
    Reviewed-by: Aidan Wolter <awolter@google.com>
    Reviewed-by: Stephen Demos <sdemos@google.com>

--- src/storage/fshost/src/config.rs
+++ src/storage/fshost/src/config.rs
@@ -33,7 +33,6 @@ pub fn default_config() -> fshost_config::Config {
         no_zxcrypt: false,
         sandbox_decompression: false,
         use_disk_based_minfs_migration: false,
-        use_native_fxfs_crypto: true,
         ramdisk_prefix: "/dev/sys/platform/00:00:2d/ramctl".to_owned(),
     }
 }
