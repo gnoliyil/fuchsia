b41f24614f9a87244f7280409cf358d125513fd2
Caslyn Tonelli <caslyn@google.com>
2023-03-18 00:27:34 +0000

    [libc] Reduce/remove per-arch signal.h definitions
    This CL removes aarch64/x64_64 signal.h headers.
    
    - arch-specific definitions that are still needed are put in
    bits/signal.h.
    - shared macros that don't require an `#if defined` are moved to
    include/signal.h.
    - these aarch64-specific symbols are deleted because they are not used:
    -#define FPSIMD_MAGIC 0x46508001
    -#define ESR_MAGIC 0x45535201
    -struct _aarch64_ctx
    -struct fpsimd_context
    -struct esr_context
    -typedef unsigned long greg_t;
    -typedef unsigned long gregset_t[34];
    -typedef struct .. fpregset_t;
    -non-gnu/bsd version of mcontext_t
    
    Bug: 123052
    Change-Id: I143216eb04dffdc93be4579fe33be90b09ff40b3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/819295
    API-Review: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Caslyn Tonelli <caslyn@google.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>

--- zircon/third_party/ulib/musl/include/bits/signal.h
+++ zircon/third_party/ulib/musl/include/bits/signal.h
@@ -1,7 +1,107 @@
+// TODO(fxbug.dev/123876): Remove these types since no libc interfaces use them.
+#if defined(_POSIX_SOURCE) || defined(_POSIX_C_SOURCE) || defined(_XOPEN_SOURCE) || \
+    defined(_GNU_SOURCE) || defined(_BSD_SOURCE)
+
+struct sigaltstack {
+  void* ss_sp;
+  int ss_flags;
+  size_t ss_size;
+};
+
+#define SA_NOCLDSTOP 1
+#define SA_NOCLDWAIT 2
+#define SA_SIGINFO 4
+#define SA_ONSTACK 0x08000000
+#define SA_RESTART 0x10000000
+#define SA_NODEFER 0x40000000
+#define SA_RESETHAND 0x80000000
+#define SA_RESTORER 0x04000000
+
+#define SIGSTKSZ 12288
+#define MINSIGSTKSZ 6144
+
 #if defined(__x86_64__)
-#include "x86_64/signal.h"
+
+#ifdef _GNU_SOURCE
+#define REG_R8 0
+#define REG_R9 1
+#define REG_R10 2
+#define REG_R11 3
+#define REG_R12 4
+#define REG_R13 5
+#define REG_R14 6
+#define REG_R15 7
+#define REG_RDI 8
+#define REG_RSI 9
+#define REG_RBP 10
+#define REG_RBX 11
+#define REG_RDX 12
+#define REG_RAX 13
+#define REG_RCX 14
+#define REG_RSP 15
+#define REG_RIP 16
+#define REG_EFL 17
+#define REG_CSGSFS 18
+#define REG_ERR 19
+#define REG_TRAPNO 20
+#define REG_OLDMASK 21
+#define REG_CR2 22
+#endif
+typedef long long greg_t, gregset_t[23];
+typedef struct _fpstate {
+  unsigned short cwd, swd, ftw, fop;
+  unsigned long long rip, rdp;
+  unsigned mxcsr, mxcr_mask;
+  struct {
+    unsigned short significand[4], exponent, padding[3];
+  } _st[8];
+  struct {
+    unsigned element[4];
+  } _xmm[16];
+  unsigned padding[24];
+} * fpregset_t;
+typedef struct {
+  gregset_t gregs;
+  fpregset_t fpregs;
+  unsigned long long __reserved1[8];
+} mcontext_t;
+typedef struct __ucontext {
+  unsigned long uc_flags;
+  struct __ucontext* uc_link;
+  stack_t uc_stack;
+  mcontext_t uc_mcontext;
+  sigset_t uc_sigmask;
+  unsigned long __fpregs_mem[64];
+} ucontext_t;
+
 #elif defined(__aarch64__)
-#include "aarch64/signal.h"
+
+typedef struct sigcontext {
+  unsigned long fault_address;
+  unsigned long regs[31];
+  unsigned long sp, pc, pstate;
+  long double __reserved[256];
+} mcontext_t;
+
+typedef struct __ucontext {
+  unsigned long uc_flags;
+  struct ucontext* uc_link;
+  stack_t uc_stack;
+  sigset_t uc_sigmask;
+  mcontext_t uc_mcontext;
+} ucontext_t;
+
 #else
-#error Unsupported architecture!
+
+typedef struct sigcontext {};
+typedef struct __ucontext {
+  unsigned long uc_flags;
+  struct ucontext* uc_link;
+  stack_t uc_stack;
+  sigset_t uc_sigmask;
+  mcontext_t uc_mcontext;
+} ucontext_t;
+
+#endif
+
 #endif
