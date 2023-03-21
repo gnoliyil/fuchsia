84cb175b19693862fa93858e7a38e47714af6220
Caslyn Tonelli <caslyn@google.com>
2023-03-21 18:59:28 +0000

    [libc] Remove sys/timex.h
    libc does not define any functions in this header and referencing any of
    these declarations will produce a link error, so the header is deleted.
    
    Bug: 100615
    Change-Id: I7ff648a421e7857c9d698967acbdadb162cf6b5a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/821318
    API-Review: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Caslyn Tonelli <caslyn@google.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>

--- zircon/third_party/ulib/musl/src/linux/adjtime.c
+++ zircon/third_party/ulib/musl/src/linux/adjtime.c
@@ -1,7 +1,6 @@
 #define _GNU_SOURCE
 #include <errno.h>
 #include <sys/time.h>
-#include <sys/timex.h>
 
 int adjtime(const struct timeval* in, struct timeval* out) {
   // TODO(kulakowski) implement adjtime(x)
