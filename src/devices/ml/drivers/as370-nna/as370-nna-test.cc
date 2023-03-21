e731addbfa69e5d8c1432e277c8110f1100eaf3e
Novin Changizi <novinc@google.com>
2023-03-21 22:26:10 +0000

    [as370][nna] Use service for registers
    Uses the FIDL service to connect to the fuchsia.hardware.registers
    protocol, instead of going through the banjo connector.
    
    Bug: 123422
    Change-Id: I2bf1345cf17d8fb81a5538ec6a030b10320e4897
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/821180
    Commit-Queue: Novin Changizi <novinc@google.com>
    Reviewed-by: Sarah Chan <spqchan@google.com>

--- src/devices/ml/drivers/as370-nna/as370-nna-test.cc
+++ src/devices/ml/drivers/as370-nna/as370-nna-test.cc
@@ -4,8 +4,10 @@
 
 #include "as370-nna.h"
 
-#include <fuchsia/hardware/registers/cpp/banjo-mock.h>
+#include <fidl/fuchsia.hardware.registers/cpp/wire.h>
 #include <lib/async-loop/cpp/loop.h>
+
+#include <zxtest/zxtest.h>
 
 #include "src/devices/lib/as370/include/soc/as370/as370-nna.h"
 #include "src/devices/registers/testing/mock-registers/mock-registers.h"
@@ -17,8 +19,7 @@ class MockRegisters {
  public:
   MockRegisters() {
     loop_.StartThread();
-    global_registers_mock_ =
-        std::make_unique<mock_registers::MockRegistersDevice>(loop_.dispatcher());
+    global_registers_mock_ = std::make_unique<mock_registers::MockRegisters>(loop_.dispatcher());
   }
 
   // The caller should set the mock expectations before calling this.
@@ -26,7 +27,7 @@ class MockRegisters {
     auto endpoints = fidl::CreateEndpoints<fuchsia_hardware_registers::Device>();
     EXPECT_OK(endpoints.status_value());
 
-    global_registers_mock_->RegistersConnect(endpoints->server.TakeChannel());
+    global_registers_mock_->Init(std::move(endpoints->server));
 
     ddk::PDev pdev;
     auto device = std::make_unique<As370NnaDevice>(
@@ -34,33 +35,29 @@ class MockRegisters {
     ASSERT_NOT_NULL(device);
     EXPECT_OK(device->Init());
 
-    EXPECT_OK(GlobalRegisters()->VerifyAll());
+    EXPECT_OK(global_registers_mock_->VerifyAll());
 
     loop_.Shutdown();
   }
 
-  mock_registers::MockRegisters* GlobalRegisters() {
-    return global_registers_mock_->fidl_service();
-  }
-
   std::shared_ptr<MockDevice> fake_parent_ = MockDevice::FakeRootParent();
   async::Loop loop_{&kAsyncLoopConfigNeverAttachToThread};
-  std::unique_ptr<mock_registers::MockRegistersDevice> global_registers_mock_;
+  std::unique_ptr<mock_registers::MockRegisters> global_registers_mock_;
 };
 
 TEST(As370NnaTest, Init) {
   MockRegisters mock_regs;
 
-  mock_regs.GlobalRegisters()->ExpectWrite<uint32_t>(as370::kNnaResetOffset, as370::kNnaResetMask,
-                                                     0);
-  mock_regs.GlobalRegisters()->ExpectWrite<uint32_t>(as370::kNnaResetOffset, as370::kNnaResetMask,
-                                                     1);
-  mock_regs.GlobalRegisters()->ExpectWrite<uint32_t>(as370::kNnaPowerOffset, as370::kNnaPowerMask,
-                                                     0);
-  mock_regs.GlobalRegisters()->ExpectWrite<uint32_t>(as370::kNnaClockSysOffset,
-                                                     as370::kNnaClockSysMask, 0x299);
-  mock_regs.GlobalRegisters()->ExpectWrite<uint32_t>(as370::kNnaClockCoreOffset,
-                                                     as370::kNnaClockCoreMask, 0x299);
+  mock_regs.global_registers_mock_->ExpectWrite<uint32_t>(as370::kNnaResetOffset,
+                                                          as370::kNnaResetMask, 0);
+  mock_regs.global_registers_mock_->ExpectWrite<uint32_t>(as370::kNnaResetOffset,
+                                                          as370::kNnaResetMask, 1);
+  mock_regs.global_registers_mock_->ExpectWrite<uint32_t>(as370::kNnaPowerOffset,
+                                                          as370::kNnaPowerMask, 0);
+  mock_regs.global_registers_mock_->ExpectWrite<uint32_t>(as370::kNnaClockSysOffset,
+                                                          as370::kNnaClockSysMask, 0x299);
+  mock_regs.global_registers_mock_->ExpectWrite<uint32_t>(as370::kNnaClockCoreOffset,
+                                                          as370::kNnaClockCoreMask, 0x299);
 
   ASSERT_NO_FATAL_FAILURE(mock_regs.CreateDeviceAndVerify());
 }
