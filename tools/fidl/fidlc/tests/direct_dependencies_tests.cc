f13c1fa67d43140d824fde9d39f06c4c4bd03229
Mitchell Kember <mkember@google.com>
2023-03-21 18:00:59 +0000

    [fidlc] Visit result unions when calculating composed dependencies
    This CL fixes the "direct and composed dependencies" logic in fidlc.
    Previously it only visited the top-level request and response types.
    Now, it visits the success and error types for methods that have a
    result union (i.e. two-way and flexible or uses error syntax).
    
    Test: fx test fidlc-test -- -f 'DirectDependenciesTests.*'
    Fixed: 124008
    Change-Id: I1cbb1bdb44c87032110c0dbb60a89874ca76b475
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/822447
    Fuchsia-Auto-Submit: Mitchell Kember <mkember@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Ian McKellar <ianloic@google.com>

--- tools/fidl/fidlc/tests/direct_dependencies_tests.cc
+++ tools/fidl/fidlc/tests/direct_dependencies_tests.cc
@@ -37,8 +37,8 @@ library dep1;
 
 using dep2;
 
-protocol Foo {
-  UsesDepType(resource struct { data )FIDL" +
+protocol ComposedProtocol {
+  UsesDep2(resource struct { data )FIDL" +
                          type_usage + R"FIDL(; });
 };
 )FIDL");
@@ -50,7 +50,7 @@ library example;
 using dep1;
 
 protocol CapturesDependencyThroughCompose {
-  compose dep1.Foo;
+  compose dep1.ComposedProtocol;
 };
 )FIDL");
     ASSERT_COMPILED(lib);
@@ -63,7 +63,9 @@ protocol CapturesDependencyThroughCompose {
   }
 }
 
-TEST(DirectDependenciesTests, GoodDoesNotCaptureTransitiveDeps) {
+// TODO(fxbug.dev/7807): Reconsider this. Will likely need to follow aliases,
+// which would mean expecting the result to include both dep1 and dep2 here.
+TEST(DirectDependenciesTests, GoodDoesNotFollowAlias) {
   SharedAmongstLibraries shared;
   TestLibrary dep2(&shared, "dep2.fidl", R"FIDL(
 library dep2;
@@ -79,8 +81,8 @@ using dep2;
 
 alias Bar = dep2.Foo;
 
-protocol Baz {
-  UsesDepConst(struct { foo vector<Bar>; });
+protocol ComposedProtocol {
+  UsesDep2InAlias(struct { foo vector<Bar>; });
 };
 )FIDL");
   ASSERT_COMPILED(dep1);
@@ -91,7 +93,7 @@ library example;
 using dep1;
 
 protocol CapturesDependencyThroughCompose {
-  compose dep1.Baz;
+  compose dep1.ComposedProtocol;
 };
 )FIDL");
   ASSERT_COMPILED(lib);
@@ -100,6 +102,162 @@ protocol CapturesDependencyThroughCompose {
   ASSERT_EQ(deps.size(), 1);
   auto iter = deps.cbegin();
   EXPECT_EQ(fidl::NameLibrary((*iter++).library->name), "dep1");
+}
+
+TEST(DirectDependenciesTests, GoodDoesNotFollowNestedStruct) {
+  SharedAmongstLibraries shared;
+  TestLibrary dep2(&shared, "dep2.fidl", R"FIDL(
+library dep2;
+
+type Foo = struct {};
+)FIDL");
+  ASSERT_COMPILED(dep2);
+
+  TestLibrary dep1(&shared, "dep1.fidl", R"FIDL(
+library dep1;
+
+using dep2;
+
+type Bar = struct {
+  foo dep2.Foo;
+};
+
+protocol ComposedProtocol {
+  UsesDep2InNestedStruct(struct { foo vector<Bar>; });
+};
+)FIDL");
+  ASSERT_COMPILED(dep1);
+
+  TestLibrary lib(&shared, "example.fidl", R"FIDL(
+library example;
+
+using dep1;
+
+protocol CapturesDependencyThroughCompose {
+  compose dep1.ComposedProtocol;
+};
+)FIDL");
+  ASSERT_COMPILED(lib);
+
+  auto deps = lib.direct_and_composed_dependencies();
+  ASSERT_EQ(deps.size(), 1);
+  auto iter = deps.cbegin();
+  EXPECT_EQ(fidl::NameLibrary((*iter++).library->name), "dep1");
+}
+
+TEST(DirectDependenciesTests, GoodErrorSyntaxSuccessType) {
+  SharedAmongstLibraries shared;
+  TestLibrary dep2(&shared, "dep2.fidl", R"FIDL(
+library dep2;
+
+type Foo = struct {};
+)FIDL");
+  ASSERT_COMPILED(dep2);
+
+  TestLibrary dep1(&shared, "dep1.fidl", R"FIDL(
+library dep1;
+
+using dep2;
+
+protocol ComposedProtocol {
+  UsesDep2InSuccessType() -> (struct { foo vector<dep2.Foo>; }) error uint32;
+};
+)FIDL");
+  ASSERT_COMPILED(dep1);
+
+  TestLibrary lib(&shared, "example.fidl", R"FIDL(
+library example;
+
+using dep1;
+
+protocol CapturesDependencyThroughCompose {
+  compose dep1.ComposedProtocol;
+};
+)FIDL");
+  ASSERT_COMPILED(lib);
+
+  auto deps = lib.direct_and_composed_dependencies();
+  ASSERT_EQ(deps.size(), 2);
+  auto iter = deps.cbegin();
+  EXPECT_EQ(fidl::NameLibrary((*iter++).library->name), "dep1");
+  EXPECT_EQ(fidl::NameLibrary((*iter++).library->name), "dep2");
+}
+
+TEST(DirectDependenciesTests, GoodErrorSyntaxErrorType) {
+  SharedAmongstLibraries shared;
+  TestLibrary dep2(&shared, "dep2.fidl", R"FIDL(
+library dep2;
+
+type Foo = flexible enum : uint32 {};
+)FIDL");
+  ASSERT_COMPILED(dep2);
+
+  TestLibrary dep1(&shared, "dep1.fidl", R"FIDL(
+library dep1;
+
+using dep2;
+
+protocol ComposedProtocol {
+  UsesDep2InErrorType() -> () error dep2.Foo;
+};
+)FIDL");
+  ASSERT_COMPILED(dep1);
+
+  TestLibrary lib(&shared, "example.fidl", R"FIDL(
+library example;
+
+using dep1;
+
+protocol CapturesDependencyThroughCompose {
+  compose dep1.ComposedProtocol;
+};
+)FIDL");
+  ASSERT_COMPILED(lib);
+
+  auto deps = lib.direct_and_composed_dependencies();
+  ASSERT_EQ(deps.size(), 2);
+  auto iter = deps.cbegin();
+  EXPECT_EQ(fidl::NameLibrary((*iter++).library->name), "dep1");
+  EXPECT_EQ(fidl::NameLibrary((*iter++).library->name), "dep2");
+}
+
+TEST(DirectDependenciesTests, GoodFlexibleResponse) {
+  SharedAmongstLibraries shared;
+  shared.EnableFlag(fidl::ExperimentalFlags::Flag::kUnknownInteractions);
+  TestLibrary dep2(&shared, "dep2.fidl", R"FIDL(
+library dep2;
+
+type Foo = struct {};
+)FIDL");
+  ASSERT_COMPILED(dep2);
+
+  TestLibrary dep1(&shared, "dep1.fidl", R"FIDL(
+library dep1;
+
+using dep2;
+
+open protocol ComposedProtocol {
+  flexible UsesDep2InFlexibleResponse() -> (struct { foo vector<dep2.Foo>; });
+};
+)FIDL");
+  ASSERT_COMPILED(dep1);
+
+  TestLibrary lib(&shared, "example.fidl", R"FIDL(
+library example;
+
+using dep1;
+
+open protocol CapturesDependencyThroughCompose {
+  compose dep1.ComposedProtocol;
+};
+)FIDL");
+  ASSERT_COMPILED(lib);
+
+  auto deps = lib.direct_and_composed_dependencies();
+  ASSERT_EQ(deps.size(), 2);
+  auto iter = deps.cbegin();
+  EXPECT_EQ(fidl::NameLibrary((*iter++).library->name), "dep1");
+  EXPECT_EQ(fidl::NameLibrary((*iter++).library->name), "dep2");
 }
 
 }  // namespace
