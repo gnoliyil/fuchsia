4738c0301cf255863eaa867bc38c4f39c940d249
Andres Oportus <andresoportus@google.com>
2023-03-22 14:25:05 +0000

    [hwstress] Remove references to hardcoded devfs device number 000
    Bug: 113834
    Test: hwstress lights on Nelson
    Change-Id: I273084a9f14a1e56bfeae872a4253b7338c62d4a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/822710
    Reviewed-by: Tamir Duberstein <tamird@google.com>
    Commit-Queue: Andres Oportus <andresoportus@google.com>

--- src/zircon/bin/hwstress/device.cc
+++ src/zircon/bin/hwstress/device.cc
@@ -9,24 +9,28 @@
 #include <lib/zx/result.h>
 
 #include <cstdio>
+#include <filesystem>
 #include <string>
 
 namespace hwstress {
 
 // Open the given path as a FIDL channel.
-zx::result<zx::channel> OpenDeviceChannel(std::string_view path) {
+zx::result<zx::channel> OpenDeviceChannel(std::string_view folder_path) {
   zx::channel client, server;
   zx_status_t status = zx::channel::create(0, &client, &server);
   if (status != ZX_OK) {
     return zx::error(status);
   }
 
-  status = fdio_service_connect(std::string(path).c_str(), server.release());
-  if (status != ZX_OK) {
-    return zx::error(status);
+  std::error_code error_code;  // Used such that we use the noexcept version of exists.
+  if (!std::filesystem::exists(folder_path, error_code)) {
+    return zx::error(ZX_ERR_INVALID_ARGS);
   }
-
-  return zx::success(std::move(client));
+  for (const auto& entry : std::filesystem::directory_iterator(folder_path)) {
+    return zx::make_result(fdio_service_connect(entry.path().c_str(), server.release()),
+                           std::move(client));
+  }
+  return zx::error(ZX_ERR_NOT_FOUND);
 }
 
 }  // namespace hwstress
