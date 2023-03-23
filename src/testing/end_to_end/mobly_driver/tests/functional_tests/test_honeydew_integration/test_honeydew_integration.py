d18212365af9079597c1c0fc3c516fad5bc2d30a
Kevin Cho <chok@google.com>
2023-03-23 01:22:37 +0000

    [lacewing] Add test for Mobly x Honeydew integration.
    Change-Id: I8cef02e0852dab162fcfbadc28201468bd5656b7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/822769
    Reviewed-by: Pavan Kumar Juturu <jpavankumar@google.com>
    Commit-Queue: Kevin Cho <chok@google.com>

---
+++ src/testing/end_to_end/mobly_driver/tests/functional_tests/test_honeydew_integration/test_honeydew_integration.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env fuchsia-vendored-python
+# Copyright 2023 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Mobly Driver honeydew integration test.
+
+Test that Mobly driver's Mobly config interfaces with HoneyDew correctly.
+"""
+
+from mobly import asserts
+from mobly import base_test
+from mobly import test_runner
+
+from honeydew.mobly_controller import fuchsia_device
+
+
+class MoblyDriverHoneydewIntegrationTest(base_test.BaseTestClass):
+    """Mobly Driver Honeydew integration tests."""
+
+    def test_mobly_controller_init(self):
+        """Test case to ensure Mobly controller initializes successfully"""
+        fuchsia_devices = self.register_controller(fuchsia_device)
+        asserts.assert_true(
+            fuchsia_devices, 'Expect at least 1 created controller.')
+
+
+if __name__ == '__main__':
+    test_runner.main()
