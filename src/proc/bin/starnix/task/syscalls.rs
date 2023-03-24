d9cef03465967f567cc397e346909a34eab6a618
Kevin Lindkvist <lindkvist@google.com>
2023-03-24 17:39:47 +0000

    [starnix] Switch clone parameter order on aarch64
    Change-Id: I506d72290c7ab545ce755e3f515eee68f344f2de
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/824959
    Fuchsia-Auto-Submit: Kevin Lindkvist <lindkvist@google.com>
    Reviewed-by: Adam Barth <abarth@google.com>
    Commit-Queue: Kevin Lindkvist <lindkvist@google.com>

--- src/proc/bin/starnix/task/syscalls.rs
+++ src/proc/bin/starnix/task/syscalls.rs
@@ -58,6 +58,7 @@ fn do_clone(current_task: &CurrentTask, args: &clone_args) -> Result<pid_t, Errn
     Ok(tid)
 }
 
+#[cfg(target_arch = "x86_64")]
 pub fn sys_clone(
     current_task: &CurrentTask,
     flags: u64,
@@ -65,6 +66,31 @@ pub fn sys_clone(
     user_parent_tid: UserRef<pid_t>,
     user_child_tid: UserRef<pid_t>,
     user_tls: UserAddress,
+) -> Result<pid_t, Errno> {
+    // Our flags parameter uses the low 8 bits (CSIGNAL mask) of flags to indicate the exit
+    // signal. The CloneArgs struct separates these as `flags` and `exit_signal`.
+    do_clone(
+        current_task,
+        &clone_args {
+            flags: flags & !(CSIGNAL as u64),
+            child_tid: user_child_tid.addr().ptr() as u64,
+            parent_tid: user_parent_tid.addr().ptr() as u64,
+            exit_signal: flags & (CSIGNAL as u64),
+            stack: user_stack.ptr() as u64,
+            tls: user_tls.ptr() as u64,
+            ..Default::default()
+        },
+    )
+}
+
+#[cfg(target_arch = "aarch64")]
+pub fn sys_clone(
+    current_task: &CurrentTask,
+    flags: u64,
+    user_stack: UserAddress,
+    user_parent_tid: UserRef<pid_t>,
+    user_tls: UserAddress,
+    user_child_tid: UserRef<pid_t>,
 ) -> Result<pid_t, Errno> {
     // Our flags parameter uses the low 8 bits (CSIGNAL mask) of flags to indicate the exit
     // signal. The CloneArgs struct separates these as `flags` and `exit_signal`.
