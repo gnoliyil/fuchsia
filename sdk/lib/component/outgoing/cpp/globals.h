ca071aedfec5c7122af20cfe23e7466e464b8499
Novin Changizi <novinc@google.com>
2023-03-24 21:12:42 +0000

    [sdk][outgoing] Sync check use weak symbol
    Changes the outgoing directory to use a weak symbol to indicate if the
    synchronization checker task should run. This should enable using both
    versions of the library without running into linker duplicate symbols.
    
    Change-Id: I0f3679edebf178c12b00a7dd1dc78f6217273c24
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/824638
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Commit-Queue: Novin Changizi <novinc@google.com>
    Reviewed-by: Yifei Teng <yifeit@google.com>
    API-Review: Yifei Teng <yifeit@google.com>

---
+++ sdk/lib/component/outgoing/cpp/globals.h
@@ -0,0 +1,14 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef LIB_COMPONENT_OUTGOING_CPP_GLOBALS_H_
+#define LIB_COMPONENT_OUTGOING_CPP_GLOBALS_H_
+
+namespace component {
+
+extern bool enable_synchronization_check_dispatcher_task;
+
+}  // namespace component
+
+#endif  // LIB_COMPONENT_OUTGOING_CPP_GLOBALS_H_
