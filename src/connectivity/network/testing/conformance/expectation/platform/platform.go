4f69733a6992afd5e8d07ac5ff5e214f7f8bc9df
Nick Brown <nickbrow@google.com>
2023-03-28 20:54:07 +0000

    [netconform] Add NS3 as a platform enum
    This enum will be used by the orchestrator to control the version of the
    netstack brought up in NTR.
    
    Also, add a method for exposing the flag needed to request that version
    over ffx.
    
    Bug: 121477
    Change-Id: Ief0291c0c1f49a3ded2fedb7e89dac3c7497def7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/826316
    Reviewed-by: Evan Wildenhain <wildenhain@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Nick Brown <nickbrow@google.com>

--- src/connectivity/network/testing/conformance/expectation/platform/platform.go
+++ src/connectivity/network/testing/conformance/expectation/platform/platform.go
@@ -11,12 +11,26 @@ type Platform int
 const (
 	_ Platform = iota
 	NS2
+	NS3
 )
 
 func (p Platform) String() string {
 	switch p {
 	case NS2:
 		return "NS2"
+	case NS3:
+		return "NS3"
+	default:
+		panic(fmt.Sprintf("Unrecognized Platform: %d", p))
+	}
+}
+
+func (p Platform) FfxFlag() string {
+	switch p {
+	case NS2:
+		return "v2"
+	case NS3:
+		return "v3"
 	default:
 		panic(fmt.Sprintf("Unrecognized Platform: %d", p))
 	}
