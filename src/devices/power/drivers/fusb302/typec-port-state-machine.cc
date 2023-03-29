2c56b90796d82c99b246d09e28142cd830c93f1c
Victor Costan <costan@google.com>
2023-03-29 04:58:48 +0000

    [fusb302][vim3] Add revised Type C Port state machine implementation.
    Bug: 122113
    Test: manual, in I815dfc481851b78e90b901d29699b0cde90ca962
    Change-Id: I37772a5d5a874393afee6b258e113cc2d89d7f67
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/824010
    Reviewed-by: Sam Hansen <hansens@google.com>
    Commit-Queue: Victor Costan <costan@google.com>

---
+++ src/devices/power/drivers/fusb302/typec-port-state-machine.cc
@@ -0,0 +1,80 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/devices/power/drivers/fusb302/typec-port-state-machine.h"
+
+#include <lib/inspect/cpp/vmo/types.h>
+
+#include <cinttypes>
+#include <utility>
+
+#include "src/devices/power/drivers/fusb302/fusb302.h"
+#include "src/devices/power/drivers/fusb302/usb-pd-defs.h"
+
+namespace fusb302 {
+
+void TypeCPortStateMachine::EnterState(TypeCPortState state) {
+  switch (state) {
+    case TypeCPortState::kSinkUnattached:
+    case TypeCPortState::kSinkAttached:
+      return;
+    case TypeCPortState::kSourceAttached:
+      zxlogf(ERROR, "SourceAttached not implemented");
+      return;
+  }
+
+  zxlogf(ERROR, "Invalid state: %d", state);
+}
+
+void TypeCPortStateMachine::ExitState(TypeCPortState state) {}
+
+TypeCPortState TypeCPortStateMachine::NextState(TypeCPortInput input,
+                                                TypeCPortState current_state) {
+  switch (current_state) {
+    case TypeCPortState::kSinkUnattached:
+      if (input != TypeCPortInput::kPortStateChanged) {
+        return current_state;
+      }
+      if (device_.sensors().detected_wired_cc_pin() == usb_pd::ConfigChannelPinSwitch::kNone) {
+        return current_state;
+      }
+      if (device_.sensors().detected_power_role() != usb_pd::PowerRole::kSink) {
+        zxlogf(DEBUG, "Sink-only, ignoring Source power state");
+        return current_state;
+      }
+      return TypeCPortState::kSinkAttached;
+
+    case TypeCPortState::kSinkAttached:
+      if (input != TypeCPortInput::kPortStateChanged) {
+        return current_state;
+      }
+      if (device_.sensors().detected_wired_cc_pin() != usb_pd::ConfigChannelPinSwitch::kNone) {
+        return current_state;
+      }
+      return TypeCPortState::kSinkUnattached;
+
+    case TypeCPortState::kSourceAttached:
+      // Only sink is currently implemented
+      zxlogf(ERROR, "SourceAttached not implemented");
+      return current_state;
+  }
+
+  zxlogf(ERROR, "Invalid state: %" PRId32, current_state);
+}
+
+const char* TypeCPortStateMachine::StateToString(TypeCPortState state) const {
+  switch (state) {
+    case TypeCPortState::kSinkUnattached:
+      return "SinkUnattached";
+    case TypeCPortState::kSinkAttached:
+      return "SinkAttached";
+    case TypeCPortState::kSourceAttached:
+      return "SourceAttached";
+  }
+
+  ZX_DEBUG_ASSERT_MSG(false, "Invalid TypeCPortStateMachine: %" PRId32, state);
+  return nullptr;
+}
+
+}  // namespace fusb302
