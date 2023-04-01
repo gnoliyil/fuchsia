ffe8349b6f7b091a57212a5e834cd7066b57dfe5
Casey Dahlin <sadmac@google.com>
2023-04-01 00:35:50 +0000

    [fasync] Add shutdown call for emulated handles
    Because emulated handles live in the process using them, not the Zircon
    kernel, writes buffered in emulated handles go away when the process
    dies.
    
    This patch adds a call that ensures all emulated handles are flushed.
    It at least guarantees whatever subsystem is on the other end of the
    handles (i.e. Overnet) has seen whatever was in the buffers.
    
    Bug: 124614
    Change-Id: Iec70a9036a58970e075ca41d59eb6f612798ed60
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/828159
    Commit-Queue: Casey Dahlin <sadmac@google.com>
    Reviewed-by: Adam Perry <adamperry@google.com>
    Reviewed-by: Steven Grady <slgrady@google.com>

--- src/lib/fuchsia-async/src/lib.rs
+++ src/lib/fuchsia-async/src/lib.rs
@@ -48,9 +48,9 @@ pub use self::handle::{
 #[cfg(not(target_os = "fuchsia"))]
 pub mod emulated_handle {
     pub use super::handle::{
-        AsHandleRef, Channel, ChannelProxyProtocol, EmulatedHandleRef, Event, EventPair, Handle,
-        HandleBased, HandleDisposition, HandleInfo, HandleOp, HandleRef, MessageBuf, MessageBufEtc,
-        ObjectType, Peered, Rights, Signals, Socket, SocketOpts,
+        shut_down_handles, AsHandleRef, Channel, ChannelProxyProtocol, EmulatedHandleRef, Event,
+        EventPair, Handle, HandleBased, HandleDisposition, HandleInfo, HandleOp, HandleRef,
+        MessageBuf, MessageBufEtc, ObjectType, Peered, Rights, Signals, Socket, SocketOpts,
     };
 }
 
