85a48519df683f0379276858ac4fdb59202ee81b
Jiaming Li <lijiaming@google.com>
2023-04-04 01:06:42 +0000

    [bazel][sdk] Merge generated_constants.bzl
    Change-Id: I78c1533b7fbda8130479c527cf83147151ea20dd
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/829456
    Reviewed-by: Jay Zhuang <jayzhuang@google.com>
    Commit-Queue: Jiaming Li <lijiaming@google.com>
    Fuchsia-Auto-Submit: Jiaming Li <lijiaming@google.com>

--- scripts/sdk/bazel/merge_bazel_wrapper.py
+++ scripts/sdk/bazel/merge_bazel_wrapper.py
@@ -1,12 +1,10 @@
 # Copyright 2023 The Fuchsia Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
 """Recursively copy all files from --source-dir to the dest-dir therein.
 If a meta/manifest.json is found, the arch target will be merged into that file
 (rather than replacing it).
 """
-
 
 import argparse
 import filecmp
@@ -14,6 +12,17 @@ import os
 import shutil
 import json
 import sys
+import difflib
+
+GENERATED_CONTANTS_TEMPLATE = """# AUTO-GENERATED - DO NOT EDIT!
+
+# This file is re-generated in //scripts/sdk/bazel/merge_bazel_wrapper.py
+
+# The following list of CPU names use Fuchsia conventions.
+constants = struct(host_cpus = ["x64"], target_cpus = {target_cpu})
+"""
+
+WHITELIST_DIFF_LISTS = []
 
 
 def merge_new_manifest(manifest_json, new_manifest):
@@ -25,13 +34,26 @@ def merge_new_manifest(manifest_json, new_manifest):
     return manifest_json
 
 
+def print_file_difference(source_file, dest_file, relpath):
+    print("=" * 40)
+    print(relpath)
+    print("=" * 40)
+    with open(source_file) as sf, open(dest_file) as df:
+        # Only print the difference if the files is utf-8 encoded
+        try:
+            for line in difflib.unified_diff(sf.readlines(), df.readlines()):
+                print(line)
+        except UnicodeDecodeError:
+            return
+    # assert relpath in WHITELIST_DIFF_LISTS
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--source-dir")
     parser.add_argument("--dest-dir")
     args = parser.parse_args()
     manifest_json = None
-    unmergable_files = []
 
     for directory in os.listdir(args.source_dir):
         directory = os.path.join(args.source_dir, directory)
@@ -46,15 +68,19 @@ def main():
                     manifest_json = merge_new_manifest(
                         manifest_json, new_manifest)
                     continue
+
+                # Skip generated_constants.bzl file as we have to update the targets section with merged targets.
+                if relpath == "generated_constants.bzl":
+                    continue
+
                 if os.path.exists(dest_file):
                     # Coherence check: Ensure that files with the same path have equal content.
                     if not filecmp.cmp(source_file, dest_file):
-                        unmergable_files.append(relpath)
+                        print_file_difference(source_file, dest_file, relpath)
                     continue
                 if not os.path.exists(os.path.dirname(dest_file)):
                     os.makedirs(os.path.dirname(dest_file))
                 shutil.copy(source_file, dest_file)
-    print(unmergable_files)
 
     # Write meta/manifest.json
     dest_file = os.path.join(args.dest_dir, "meta/manifest.json")
@@ -62,6 +88,13 @@ def main():
         os.makedirs(os.path.dirname(dest_file))
     with open(dest_file, "w") as f:
         json.dump(manifest_json, f, indent=2)
+
+    # Write generated_constants.bzl
+    dest_file = os.path.join(args.dest_dir, "generated_constants.bzl")
+    with open(dest_file, "w") as f:
+        f.write(
+            GENERATED_CONTANTS_TEMPLATE.format(
+                target_cpu=manifest_json["arch"]["target"]))
 
 
 if __name__ == "__main__":
