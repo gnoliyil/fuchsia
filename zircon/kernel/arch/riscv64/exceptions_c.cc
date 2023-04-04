af5b72e5d0ab886af01c0cd416c4cce12cb5e478
Travis Geiselbrecht <travisg@google.com>
2023-04-04 09:33:55 +0000

    [kernel][riscv] implement exception and IRQ handling
    RISC-V kernel fixup patch 3/12
    
    Somewhat rewritten exception handling logic:
    -Previous exception handler only saved callee trashed regs, but this is
    insufficient for full exception handling. Save all regs now using the
    new iframe_t format.
    -Remove code to save fpu registers on exception entry
    -Move exception handling assembly out of asm.S into exceptions.S
    -Stub out anything to do with handling an exception from user space
    -Stub out syscall handling logic. Old logic is still commented out for
    future reference.
    -Add logic to print exception frames and start to modernize exception
    related C code.
    
    Bug: 122873
    Change-Id: I843c6b2cb295945c7bcc4d5b88c24ff55b108d02
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/822190
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Fuchsia-Auto-Submit: Travis Geiselbrecht <travisg@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- zircon/kernel/arch/riscv64/exceptions_c.cc
+++ zircon/kernel/arch/riscv64/exceptions_c.cc
@@ -57,12 +57,48 @@ bool arch_install_exception_context(Thread* thread, const arch_exception_context
 
 void arch_remove_exception_context(Thread* thread) {}
 
+void PrintFrame(FILE* f, const iframe_t& frame) {
+  fprintf(f, "iframe %p:\n", &frame);
+  fprintf(f,
+          "epc    %#18" PRIx64 " x1/ra  %#18" PRIx64 " x2/sp   %#18" PRIx64 " x3/gp   %#18" PRIx64
+          "\n",
+          frame.regs.pc, frame.regs.ra, frame.regs.sp, frame.regs.gp);
+  fprintf(f,
+          "x4/tp  %#18" PRIx64 " x5/t0  %#18" PRIx64 " x6/t1   %#18" PRIx64 " x7/t2   %#18" PRIx64
+          "\n",
+          frame.regs.tp, frame.regs.t0, frame.regs.t1, frame.regs.t2);
+  fprintf(f,
+          "x8/s0  %#18" PRIx64 " x9/s1  %#18" PRIx64 " x10/a0  %#18" PRIx64 " x11/a1  %#18" PRIx64
+          "\n",
+          frame.regs.s0, frame.regs.s1, frame.regs.a0, frame.regs.a1);
+  fprintf(f,
+          "x12/a2 %#18" PRIx64 " x13/a3 %#18" PRIx64 " x14/a4  %#18" PRIx64 " x15/a5  %#18" PRIx64
+          "\n",
+          frame.regs.a2, frame.regs.a3, frame.regs.a4, frame.regs.a5);
+  fprintf(f,
+          "x16/a6 %#18" PRIx64 " x17/a7 %#18" PRIx64 " x18/s2  %#18" PRIx64 " x19/s3  %#18" PRIx64
+          "\n",
+          frame.regs.a6, frame.regs.a7, frame.regs.s2, frame.regs.s3);
+  fprintf(f,
+          "x20/s4 %#18" PRIx64 " x21/s5 %#18" PRIx64 " x22/s6  %#18" PRIx64 " x23/s7  %#18" PRIx64
+          "\n",
+          frame.regs.s4, frame.regs.s5, frame.regs.s6, frame.regs.s7);
+  fprintf(f,
+          "x24/s8 %#18" PRIx64 " x25/s9 %#18" PRIx64 " x26/s10 %#18" PRIx64 " x27/s11 %#18" PRIx64
+          "\n",
+          frame.regs.s8, frame.regs.s9, frame.regs.s10, frame.regs.s11);
+  fprintf(f,
+          "x28/t3 %#18" PRIx64 " x29/t4 %#18" PRIx64 " x30/t5  %#18" PRIx64 " x31/t6  %#18" PRIx64
+          "\n",
+          frame.regs.t3, frame.regs.t4, frame.regs.t5, frame.regs.t6);
+  fprintf(f, "status %#18" PRIx64 "\n", frame.status);
+}
+
 static const char* cause_to_string(long cause) {
   if (cause < 0) {
     switch (cause & LONG_MAX) {
       case RISCV64_INTERRUPT_SSWI:
         return "Software interrupt";
-        break;
       case RISCV64_INTERRUPT_STIM:
         return "Timer interrupt";
       case RISCV64_INTERRUPT_SEXT:
@@ -103,23 +139,38 @@ static const char* cause_to_string(long cause) {
   return "Unknown";
 }
 
+// Prints exception details and then panics.
+__NO_RETURN __NO_INLINE static void exception_die(iframe_t* iframe, const char* format, ...) {
+  platform_panic_start();
+
+  va_list args;
+  va_start(args, format);
+  vprintf(format, args);
+  va_end(args);
+
+  /* fatal exception, die here */
+  PrintFrame(stdout, *iframe);
+
+  platform_halt(HALT_ACTION_HALT, ZirconCrashReason::Panic);
+}
+
 __NO_RETURN __NO_INLINE static void fatal_exception(long cause, struct iframe_t* frame) {
   if (cause < 0) {
-    panic("unhandled interrupt cause %#lx, epc %#lx, tval %#lx cpu %u\n", cause, frame->regs.pc,
-          riscv64_csr_read(RISCV64_CSR_STVAL), arch_curr_cpu_num());
+    exception_die(frame, "unhandled interrupt cause %#lx, epc %#lx, tval %#lx cpu %u\n", cause,
+                  frame->regs.pc, riscv64_csr_read(RISCV64_CSR_STVAL), arch_curr_cpu_num());
   } else {
-    panic("unhandled exception cause %#lx (%s), epc %#lx, tval %#lx, cpu %u\n", cause,
-          cause_to_string(cause), frame->regs.pc, riscv64_csr_read(RISCV64_CSR_STVAL),
-          arch_curr_cpu_num());
+    exception_die(frame, "unhandled exception cause %#lx (%s), epc %#lx, tval %#lx, cpu %u\n",
+                  cause, cause_to_string(cause), frame->regs.pc,
+                  riscv64_csr_read(RISCV64_CSR_STVAL), arch_curr_cpu_num());
   }
 }
 
-static void riscv64_page_fault_handler(long cause, struct iframe_t* frame) {
+static void riscv64_page_fault_handler(long cause, struct iframe_t* frame, bool user) {
   vaddr_t tval = riscv64_csr_read(RISCV64_CSR_STVAL);
   uint pf_flags = VMM_PF_FLAG_NOT_PRESENT;
-  pf_flags |= cause == RISCV64_EXCEPTION_STORE_PAGE_FAULT ? VMM_PF_FLAG_WRITE : 0;
-  pf_flags |= cause == RISCV64_EXCEPTION_INS_PAGE_FAULT ? VMM_PF_FLAG_INSTRUCTION : 0;
-  pf_flags |= !(frame->sstatus & RISCV64_CSR_SSTATUS_PP) ? VMM_PF_FLAG_USER : 0;
+  pf_flags |= (cause == RISCV64_EXCEPTION_STORE_PAGE_FAULT) ? VMM_PF_FLAG_WRITE : 0;
+  pf_flags |= (cause == RISCV64_EXCEPTION_INS_PAGE_FAULT) ? VMM_PF_FLAG_INSTRUCTION : 0;
+  pf_flags |= user ? VMM_PF_FLAG_USER : 0;
 
   zx_status_t pf_status = vmm_page_fault_handler(tval, pf_flags);
 
@@ -138,23 +189,38 @@ static void riscv64_page_fault_handler(long cause, struct iframe_t* frame) {
         return;
       }
     } else {
-      panic("Kernel page fault");
+      exception_die(frame, "Page fault in kernel: %s, %s, address %#lx\n",
+                    (pf_flags & VMM_PF_FLAG_INSTRUCTION) ? "instruction" : "data",
+                    (pf_flags & VMM_PF_FLAG_WRITE) ? "write" : "read", tval);
     }
   }
 }
 
-static void riscv64_illegal_instruction_handler(long cause, struct iframe_t* frame) {
+static void riscv64_illegal_instruction_handler(long cause, struct iframe_t* frame, bool user) {
+  // TODO-rvbringup: actually implement lazy FPU context switch
+  exception_die(frame, "unimplemented illegal instruction handler: from_user %u\n", user);
+#if 0
   // If the FPU is already enabled this is bad.
-  if ((frame->sstatus & RISCV64_CSR_SSTATUS_FS) != RISCV64_CSR_SSTATUS_FS_OFF) {
-    try_dispatch_user_data_fault_exception(ZX_EXCP_UNDEFINED_INSTRUCTION, frame);
+  if ((frame->status & RISCV64_CSR_SSTATUS_FS) != RISCV64_CSR_SSTATUS_FS_OFF) {
+    try_dispatch_user_data_fault_exception(ZX_EXCP_UNDEFINED_INSTRUCTION,
+        frame);
   }
   // Otherwise we just try to enable the FPU.
-  frame->sstatus |= RISCV64_CSR_SSTATUS_FS_INITIAL;
+  frame->status |= RISCV64_CSR_SSTATUS_FS_INITIAL;
+#endif
 }
 
-extern "C" syscall_result riscv64_syscall_dispatcher(struct iframe_t* frame);
+extern "C" syscall_result riscv64_syscall_dispatcher(struct iframe_t* frame) {
+  // TODO-rvbringup: implement in exceptions.S
+  PANIC_UNIMPLEMENTED;
+}
 
-static void riscv64_syscall_handler(struct iframe_t* frame) {
+static void riscv64_syscall_handler(struct iframe_t* frame, bool user) {
+  if (unlikely(!user)) {
+    exception_die(frame, "syscall from supervisor mode\n");
+  }
+
+  // TODO-rvbringup: double check that need to do this skip
   frame->regs.pc = frame->regs.pc + 0x4;  // Skip the ecall instruction
 
   struct syscall_result ret = riscv64_syscall_dispatcher(frame);
@@ -165,8 +231,12 @@ static void riscv64_syscall_handler(struct iframe_t* frame) {
 }
 
 extern "C" void riscv64_exception_handler(long cause, struct iframe_t* frame) {
-  LTRACEF("hart %u cause %s epc %#lx status %#lx\n", arch_curr_cpu_num(), cause_to_string(cause),
-          frame->regs.pc, frame->sstatus);
+  const bool user = (frame->status & RISCV64_CSR_SSTATUS_PP) == 0;
+
+  LTRACEF("hart %u cause %s epc %#lx status %#lx user %u\n", arch_curr_cpu_num(),
+          cause_to_string(cause), frame->regs.pc, frame->status, user);
+
+  // TODO-rvbringup: add some kcounters
 
   // top bit of the cause register determines if it's an interrupt or not
   if (cause < 0) {
@@ -188,6 +258,7 @@ extern "C" void riscv64_exception_handler(long cause, struct iframe_t* frame) {
     }
 
     bool do_preempt = int_handler_finish(&state);
+    // TODO-rvbringup: add arch_iframe_process_pending_signals here if from user space
     if (do_preempt) {
       Thread::Current::Preempt();
     }
@@ -197,13 +268,13 @@ extern "C" void riscv64_exception_handler(long cause, struct iframe_t* frame) {
       case RISCV64_EXCEPTION_INS_PAGE_FAULT:
       case RISCV64_EXCEPTION_LOAD_PAGE_FAULT:
       case RISCV64_EXCEPTION_STORE_PAGE_FAULT:
-        riscv64_page_fault_handler(cause, frame);
+        riscv64_page_fault_handler(cause, frame, user);
         break;
       case RISCV64_EXCEPTION_ILLEGAL_INS:
-        riscv64_illegal_instruction_handler(cause, frame);
+        riscv64_illegal_instruction_handler(cause, frame, user);
         break;
       case RISCV64_EXCEPTION_ENV_CALL_U_MODE:
-        riscv64_syscall_handler(frame);
+        riscv64_syscall_handler(frame, user);
         break;
       default:
         fatal_exception(cause, frame);
--- zircon/kernel/arch/riscv64/include/arch/regs.h
+++ zircon/kernel/arch/riscv64/include/arch/regs.h
@@ -7,6 +7,41 @@
 #ifndef ZIRCON_KERNEL_ARCH_RISCV64_INCLUDE_ARCH_REGS_H_
 #define ZIRCON_KERNEL_ARCH_RISCV64_INCLUDE_ARCH_REGS_H_
 
+#define RISCV64_IFRAME_OFFSET_PC (0 * 8)    // in the x0 slot
+#define RISCV64_IFRAME_OFFSET_RA (1 * 8)    // x1
+#define RISCV64_IFRAME_OFFSET_SP (2 * 8)    // x2
+#define RISCV64_IFRAME_OFFSET_GP (3 * 8)    // x3
+#define RISCV64_IFRAME_OFFSET_TP (4 * 8)    // x4
+#define RISCV64_IFRAME_OFFSET_T0 (5 * 8)    // x5
+#define RISCV64_IFRAME_OFFSET_T1 (6 * 8)    // x6
+#define RISCV64_IFRAME_OFFSET_T2 (7 * 8)    // x7
+#define RISCV64_IFRAME_OFFSET_S0 (8 * 8)    // x8
+#define RISCV64_IFRAME_OFFSET_S1 (9 * 8)    // x9
+#define RISCV64_IFRAME_OFFSET_A0 (10 * 8)   // x10
+#define RISCV64_IFRAME_OFFSET_A1 (11 * 8)   // x11
+#define RISCV64_IFRAME_OFFSET_A2 (12 * 8)   // x12
+#define RISCV64_IFRAME_OFFSET_A3 (13 * 8)   // x13
+#define RISCV64_IFRAME_OFFSET_A4 (14 * 8)   // x14
+#define RISCV64_IFRAME_OFFSET_A5 (15 * 8)   // x15
+#define RISCV64_IFRAME_OFFSET_A6 (16 * 8)   // x16
+#define RISCV64_IFRAME_OFFSET_A7 (17 * 8)   // x17
+#define RISCV64_IFRAME_OFFSET_S2 (18 * 8)   // x18
+#define RISCV64_IFRAME_OFFSET_S3 (19 * 8)   // x19
+#define RISCV64_IFRAME_OFFSET_S4 (20 * 8)   // x20
+#define RISCV64_IFRAME_OFFSET_S5 (21 * 8)   // x21
+#define RISCV64_IFRAME_OFFSET_S6 (22 * 8)   // x22
+#define RISCV64_IFRAME_OFFSET_S7 (23 * 8)   // x23
+#define RISCV64_IFRAME_OFFSET_S8 (24 * 8)   // x24
+#define RISCV64_IFRAME_OFFSET_S9 (25 * 8)   // x25
+#define RISCV64_IFRAME_OFFSET_S10 (26 * 8)  // x26
+#define RISCV64_IFRAME_OFFSET_S11 (27 * 8)  // x27
+#define RISCV64_IFRAME_OFFSET_T3 (28 * 8)   // x28
+#define RISCV64_IFRAME_OFFSET_T4 (29 * 8)   // x29
+#define RISCV64_IFRAME_OFFSET_T5 (30 * 8)   // x30
+#define RISCV64_IFRAME_OFFSET_T6 (31 * 8)   // x31
+#define RISCV64_IFRAME_OFFSET_STATUS (32 * 8)
+#define RISCV64_IFRAME_SIZE ((32 + 2) * 8)
+
 #ifndef __ASSEMBLER__
 
 #include <stdint.h>
@@ -16,7 +51,8 @@
 // Registers saved on entering the kernel via architectural exception.
 struct alignas(16) iframe_t {
   zx_riscv64_thread_state_general_regs_t regs;
-  uint64_t sstatus;
+  uint64_t status;
+  uint64_t pad;
 };
 
 struct arch_exception_context {
@@ -27,10 +63,47 @@ struct arch_exception_context {
   bool is_page_fault;
 };
 
-void PrintFrame(FILE* file, const iframe_t& frame);
+static_assert(sizeof(iframe_t) % 16u == 0u);
+static_assert(sizeof(iframe_t) == RISCV64_IFRAME_SIZE);
+
+static_assert(__offsetof(iframe_t, regs.pc) == RISCV64_IFRAME_OFFSET_PC);
+static_assert(__offsetof(iframe_t, regs.ra) == RISCV64_IFRAME_OFFSET_RA);
+static_assert(__offsetof(iframe_t, regs.sp) == RISCV64_IFRAME_OFFSET_SP);
+static_assert(__offsetof(iframe_t, regs.gp) == RISCV64_IFRAME_OFFSET_GP);
+static_assert(__offsetof(iframe_t, regs.tp) == RISCV64_IFRAME_OFFSET_TP);
+static_assert(__offsetof(iframe_t, regs.t0) == RISCV64_IFRAME_OFFSET_T0);
+static_assert(__offsetof(iframe_t, regs.t1) == RISCV64_IFRAME_OFFSET_T1);
+static_assert(__offsetof(iframe_t, regs.t2) == RISCV64_IFRAME_OFFSET_T2);
+static_assert(__offsetof(iframe_t, regs.s0) == RISCV64_IFRAME_OFFSET_S0);
+static_assert(__offsetof(iframe_t, regs.s1) == RISCV64_IFRAME_OFFSET_S1);
+static_assert(__offsetof(iframe_t, regs.a0) == RISCV64_IFRAME_OFFSET_A0);
+static_assert(__offsetof(iframe_t, regs.a1) == RISCV64_IFRAME_OFFSET_A1);
+static_assert(__offsetof(iframe_t, regs.a2) == RISCV64_IFRAME_OFFSET_A2);
+static_assert(__offsetof(iframe_t, regs.a3) == RISCV64_IFRAME_OFFSET_A3);
+static_assert(__offsetof(iframe_t, regs.a4) == RISCV64_IFRAME_OFFSET_A4);
+static_assert(__offsetof(iframe_t, regs.a5) == RISCV64_IFRAME_OFFSET_A5);
+static_assert(__offsetof(iframe_t, regs.a6) == RISCV64_IFRAME_OFFSET_A6);
+static_assert(__offsetof(iframe_t, regs.a7) == RISCV64_IFRAME_OFFSET_A7);
+static_assert(__offsetof(iframe_t, regs.s2) == RISCV64_IFRAME_OFFSET_S2);
+static_assert(__offsetof(iframe_t, regs.s3) == RISCV64_IFRAME_OFFSET_S3);
+static_assert(__offsetof(iframe_t, regs.s4) == RISCV64_IFRAME_OFFSET_S4);
+static_assert(__offsetof(iframe_t, regs.s5) == RISCV64_IFRAME_OFFSET_S5);
+static_assert(__offsetof(iframe_t, regs.s6) == RISCV64_IFRAME_OFFSET_S6);
+static_assert(__offsetof(iframe_t, regs.s7) == RISCV64_IFRAME_OFFSET_S7);
+static_assert(__offsetof(iframe_t, regs.s8) == RISCV64_IFRAME_OFFSET_S8);
+static_assert(__offsetof(iframe_t, regs.s9) == RISCV64_IFRAME_OFFSET_S9);
+static_assert(__offsetof(iframe_t, regs.s10) == RISCV64_IFRAME_OFFSET_S10);
+static_assert(__offsetof(iframe_t, regs.s11) == RISCV64_IFRAME_OFFSET_S11);
+static_assert(__offsetof(iframe_t, regs.t3) == RISCV64_IFRAME_OFFSET_T3);
+static_assert(__offsetof(iframe_t, regs.t4) == RISCV64_IFRAME_OFFSET_T4);
+static_assert(__offsetof(iframe_t, regs.t5) == RISCV64_IFRAME_OFFSET_T5);
+static_assert(__offsetof(iframe_t, regs.t6) == RISCV64_IFRAME_OFFSET_T6);
+static_assert(__offsetof(iframe_t, status) == RISCV64_IFRAME_OFFSET_STATUS);
 
 // Registers saved on entering the kernel via syscall.
 using syscall_regs_t = iframe_t;
+
+void PrintFrame(FILE* file, const iframe_t& frame);
 
 #endif  // !__ASSEMBLER__
 
