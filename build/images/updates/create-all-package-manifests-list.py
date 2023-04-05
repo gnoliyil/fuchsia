a4ff08c5cc86551fc72bf0715ede9ccab222c279
Erick Tryzelaar <etryzelaar@google.com>
2023-04-05 22:00:30 +0000

    [build] Merge bazel product bundle into all_package_manifests.list
    This changes the `use_bazel_images_only` GN argument to merge in the
    bazel built packages from a product bundle into
    `all_package_manifests.list`. This allows the GN/Bazel hybrid config
    to work with the traditional `fx` tools.
    
    In order to do this, we need to switch from using `build/cat.sh` to
    a custom script to create `all_package_manifests.list`, because the
    paths inside a package manifest list are relative to the package
    manifest list's location.
    
    Change-Id: I0ee90079342f22ed6701d23a55756479995209a4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/830822
    Commit-Queue: Erick Tryzelaar <etryzelaar@google.com>
    Reviewed-by: Jiaming Li <lijiaming@google.com>
    Reviewed-by: Aaron Wood <aaronwood@google.com>
    Fuchsia-Auto-Submit: Erick Tryzelaar <etryzelaar@google.com>

---
+++ build/images/updates/create-all-package-manifests-list.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3.8
+# Copyright 2023 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import os.path
+import sys
+import tempfile
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('output', help='Write to this file')
+    parser.add_argument('paths', nargs='+', help='package manifest lists')
+    args = parser.parse_args()
+
+    out = tempfile.NamedTemporaryFile(
+        'w', dir=os.path.dirname(args.output), delete=False)
+    try:
+        for path in args.paths:
+            with open(path) as f:
+                # Package manifests in a package manifest list are relative to
+                # the directory containing the list.
+                parent = os.path.dirname(path)
+                if parent == '.':
+                    for line in f:
+                        out.write(line)
+                else:
+                    for line in f:
+                        out.write(os.path.join(parent, line))
+
+        out.close()
+
+        os.replace(out.name, args.output)
+    finally:
+        try:
+            os.unlink(out.name)
+        except FileNotFoundError:
+            pass
+
+
+if __name__ == "__main__":
+    sys.exit(main())
