fbdadb49410fa72b61ac880ecc96fb367ea18719
Eric Rahm <erahm@google.com>
2023-04-05 22:11:00 +0000

    [fdr trigger] Remove syslog dependency
    Switch over to using `fuchsia::main` to initialize logging and remove
    dependency on deprecated `fuchsia_syslog`.
    
    Bug: 89292
    Test: fx build
    Change-Id: I7164255ef4e9230ba0cbde18d27c96606b5e373a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/830177
    Commit-Queue: Eric Rahm <erahm@google.com>
    Reviewed-by: Prashanth Swaminathan <prashanthsw@google.com>

--- src/recovery/factory-reset-trigger/src/main.rs
+++ src/recovery/factory-reset-trigger/src/main.rs
@@ -2,13 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use anyhow::{Context, Error};
+use anyhow::Error;
 use forced_fdr::perform_fdr_if_necessary;
-use fuchsia_async as fasync;
 
-#[fasync::run(1)]
+#[fuchsia::main(logging_tags = ["factory-reset-trigger"])]
 async fn main() -> Result<(), Error> {
-    fuchsia_syslog::init_with_tags(&["factory-reset-trigger"]).context("syslog init failed")?;
     perform_fdr_if_necessary().await;
 
     Ok(())
