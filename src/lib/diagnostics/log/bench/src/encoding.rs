f04dea624ee9f1e3ace396c8f904ca5810114a05
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2023-04-05 15:15:37 +0000

    [log][rust] Add benchmarks skeleton
    Bug: 124921
    
    Change-Id: Ib06819f1d19bc7da060390b1710b3f3b80b6e743
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/831179
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>
    Reviewed-by: Brian Bosak <bbosak@google.com>

---
+++ src/lib/diagnostics/log/bench/src/encoding.rs
@@ -0,0 +1,22 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use fuchsia_criterion::{criterion, FuchsiaCriterion};
+use std::time::Duration;
+
+fn main() {
+    // TODO(fxbug.dev/124921): add benchmarks.
+    let mut c = FuchsiaCriterion::default();
+    let internal_c: &mut criterion::Criterion = &mut c;
+    *internal_c = std::mem::take(internal_c)
+        .warm_up_time(Duration::from_millis(1))
+        .measurement_time(Duration::from_millis(100))
+        // We must reduce the sample size from the default of 100, otherwise
+        // Criterion will sometimes override the 1ms + 500ms suggested times
+        // and run for much longer.
+        .sample_size(10);
+
+    let bench = criterion::Benchmark::new("TODO", move |_b| {});
+    c.bench("fuchsia.diagnostics_log.encoding_benchmarks", bench);
+}
