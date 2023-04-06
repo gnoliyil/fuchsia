7ad6219cb191ba3d83b63baf6c2e78c8f6c57903
Travis Geiselbrecht <travisg@google.com>
2023-04-06 02:22:49 +0000

    [kernel][riscv] Stub out Guest and Vcpu objects
    Hypervisor is currently unimplemented on riscv, so stub these objects
    out so that they perform no function but allow the Guest and
    VcpuDispatcher to be constructed.
    
    Bug: 122871
    Bug: 122880
    Change-Id: Ifab1fd397d58f9f375105110530204668ef0204b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/831433
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Travis Geiselbrecht <travisg@google.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>

--- zircon/kernel/arch/riscv64/include/arch/hypervisor.h
+++ zircon/kernel/arch/riscv64/include/arch/hypervisor.h
@@ -7,11 +7,16 @@
 #ifndef ZIRCON_KERNEL_ARCH_RISCV64_INCLUDE_ARCH_HYPERVISOR_H_
 #define ZIRCON_KERNEL_ARCH_RISCV64_INCLUDE_ARCH_HYPERVISOR_H_
 
+#include <debug.h>
 #include <lib/zx/result.h>
 #include <zircon/syscalls/hypervisor.h>
 
 #include <fbl/ref_ptr.h>
 #include <ktl/unique_ptr.h>
+
+// Nulled out RISC-V implementation of Guest and Vcpu objects. There is currently
+// no support for this architecture, so this is the minimum to cleanly compile the
+// layers above.
 
 typedef struct zx_port_packet zx_port_packet_t;
 class PortDispatcher;
@@ -20,8 +25,9 @@ class VmAddressRegion;
 // Represents a guest within the hypervisor.
 class Guest {
  public:
-  static zx::result<ktl::unique_ptr<Guest>> Create();
-  ~Guest();
+  static zx::result<ktl::unique_ptr<Guest>> Create() { return zx::error(ZX_ERR_NOT_SUPPORTED); }
+
+  ~Guest() = default;
 
   Guest(Guest&&) = delete;
   Guest& operator=(Guest&&) = delete;
@@ -29,9 +35,11 @@ class Guest {
   Guest& operator=(const Guest&) = delete;
 
   zx::result<> SetTrap(uint32_t kind, zx_vaddr_t addr, size_t len, fbl::RefPtr<PortDispatcher> port,
-                       uint64_t key);
+                       uint64_t key) {
+    return zx::error(ZX_ERR_NOT_SUPPORTED);
+  }
 
-  fbl::RefPtr<VmAddressRegion> RootVmar() const;
+  fbl::RefPtr<VmAddressRegion> RootVmar() const { return nullptr; }
 };
 
 using NormalGuest = Guest;
@@ -39,22 +47,24 @@ using NormalGuest = Guest;
 // Represents a virtual CPU within a guest.
 class Vcpu {
  public:
-  static zx::result<ktl::unique_ptr<Vcpu>> Create(Guest& guest, zx_vaddr_t entry);
-  ~Vcpu();
+  static zx::result<ktl::unique_ptr<Vcpu>> Create(Guest& guest, zx_vaddr_t entry) {
+    return zx::error(ZX_ERR_NOT_SUPPORTED);
+  }
+  ~Vcpu() = default;
 
   Vcpu(Vcpu&&) = delete;
   Vcpu& operator=(Vcpu&&) = delete;
   Vcpu(const Vcpu&) = delete;
   Vcpu& operator=(const Vcpu&) = delete;
 
-  zx::result<> Enter(zx_port_packet_t& packet);
-  void Kick();
-  void Interrupt(uint32_t vector);
-  zx::result<> ReadState(zx_vcpu_state_t& state) const;
-  zx::result<> WriteState(const zx_vcpu_state_t& state);
-  zx::result<> WriteState(const zx_vcpu_io_t& io_state) { return zx::error(ZX_ERR_INVALID_ARGS); }
+  zx::result<> Enter(zx_port_packet_t& packet) { return zx::error(ZX_ERR_NOT_SUPPORTED); }
+  void Kick() { PANIC_UNIMPLEMENTED; }
+  void Interrupt(uint32_t vector) { PANIC_UNIMPLEMENTED; }
+  zx::result<> ReadState(zx_vcpu_state_t& state) const { return zx::error(ZX_ERR_NOT_SUPPORTED); }
+  zx::result<> WriteState(const zx_vcpu_state_t& state) { return zx::error(ZX_ERR_NOT_SUPPORTED); }
+  zx::result<> WriteState(const zx_vcpu_io_t& io_state) { return zx::error(ZX_ERR_NOT_SUPPORTED); }
 
-  void GetInfo(zx_info_vcpu_t* info);
+  void GetInfo(zx_info_vcpu_t* info) { *info = {}; }
 };
 
 using NormalVcpu = Vcpu;
