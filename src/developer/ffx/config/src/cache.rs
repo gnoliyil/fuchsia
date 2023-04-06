e0ad269aac3d6248ee71a6f4b77ac5af69ab6fc4
Megan Batty <mgnb@google.com>
2023-04-06 17:55:53 +0000

    [ffx][config] Make the context own the cache
    Instead of dueling globals between the cache and the environment
    context, just have the environment context own the cache. This will
    avoid cross-contamination of multiple contexts in one process.
    
    Fixed: 124465
    Change-Id: I214f439a9a8bd9fe6b51fb083242b56031db741e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/828877
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Steven Grady <slgrady@google.com>
    Fuchsia-Auto-Submit: Megan Batty <mgnb@google.com>

--- src/developer/ffx/config/src/cache.rs
+++ src/developer/ffx/config/src/cache.rs
@@ -2,155 +2,32 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use crate::{
-    environment::{Environment, EnvironmentContext, ExecutableKind},
-    storage::{Config, ConfigMap},
-    BuildOverride,
-};
-use anyhow::{bail, Context, Result};
+use crate::{environment::Environment, storage::Config, BuildOverride};
+use anyhow::Result;
 use async_lock::{RwLock, RwLockUpgradableReadGuard};
 use std::{
     collections::HashMap,
     path::{Path, PathBuf},
-    sync::{Arc, Mutex},
+    sync::Arc,
     time::{Duration, Instant},
 };
-use tempfile::{NamedTempFile, TempDir};
 
 // Timeout for the config cache.
 pub const CONFIG_CACHE_TIMEOUT: Duration = Duration::from_secs(3);
 
+#[derive(Debug)]
 struct CacheItem {
     created: Instant,
     config: Arc<RwLock<Config>>,
 }
 
-type Cache = RwLock<HashMap<Option<PathBuf>, CacheItem>>;
-
-lazy_static::lazy_static! {
-    static ref ENV: Mutex<Option<EnvironmentContext>> = Mutex::default();
-    static ref CACHE: Cache = RwLock::default();
-}
-
-pub fn global_env_context() -> Option<EnvironmentContext> {
-    ENV.lock().unwrap().clone()
-}
-
-pub async fn global_env() -> Result<Environment> {
-    let context =
-        global_env_context().context("Tried to load global environment before configuration")?;
-
-    match context.load().await {
-        Err(err) => {
-            tracing::error!("failed to load environment, reverting to default: {}", err);
-            Environment::new_empty(context).await
-        }
-        Ok(ctx) => Ok(ctx),
-    }
-}
-
-/// A structure that holds information about the test config environment for the duration
-/// of a test. This object must continue to exist for the duration of the test, or the test
-/// may fail.
-#[must_use = "This object must be held for the duration of a test (ie. `let _env = ffx_config::test_init()`) for it to operate correctly."]
-pub struct TestEnv {
-    pub env_file: NamedTempFile,
-    pub context: EnvironmentContext,
-    pub isolate_root: TempDir,
-    pub user_file: NamedTempFile,
-    _guard: async_lock::MutexGuardArc<()>,
-}
-
-impl TestEnv {
-    async fn new(_guard: async_lock::MutexGuardArc<()>) -> Result<Self> {
-        let env_file = NamedTempFile::new().context("tmp access failed")?;
-        let user_file = NamedTempFile::new().context("tmp access failed")?;
-        let isolate_root = tempfile::tempdir()?;
-
-        Environment::init_env_file(env_file.path()).await.context("initializing env file")?;
-
-        // Point the user config at a temporary file.
-        let user_file_path = user_file.path().to_owned();
-        let context = EnvironmentContext::isolated(
-            ExecutableKind::Test,
-            isolate_root.path().to_owned(),
-            HashMap::from_iter(std::env::vars()),
-            ConfigMap::default(),
-            Some(env_file.path().to_owned()),
-        );
-        let test_env = TestEnv { env_file, context, user_file, isolate_root, _guard };
-
-        let mut env = test_env.load().await;
-        env.set_user(Some(&user_file_path));
-        env.save().await.context("saving env file")?;
-
-        Ok(test_env)
-    }
-
-    pub async fn load(&self) -> Environment {
-        self.context.load().await.expect("opening test env file")
-    }
-}
-
-impl Drop for TestEnv {
-    fn drop(&mut self) {
-        // after the test, wipe out all the test configuration we set up. Explode if things aren't as we
-        // expect them.
-        let mut env = ENV.lock().expect("Poisoned lock");
-        let env_prev = env.clone();
-        *env = None;
-        drop(env);
-
-        if let Some(env_prev) = env_prev {
-            assert_eq!(
-                env_prev,
-                self.context,
-                "environment context changed from isolated environment to {other:?} during test run somehow.",
-                other = env_prev
-            );
-        }
-
-        // since we're not running in async context during drop, we can't clear the cache unfortunately.
-    }
-}
-
-/// When running tests we usually just want to initialize a blank slate configuration, so
-/// use this for tests. You must hold the returned object object for the duration of the test, not doing so
-/// will result in strange behaviour.
-pub async fn test_init() -> Result<TestEnv> {
-    lazy_static::lazy_static! {
-        static ref TEST_LOCK: Arc<async_lock::Mutex<()>> = Arc::default();
-    }
-    let env = TestEnv::new(TEST_LOCK.lock_arc().await).await?;
-
-    // force an overwrite of the configuration setup
-    init(&env.context).await?;
-    // force clearing the cache as well
-    invalidate().await;
-
-    Ok(env)
-}
+#[derive(Debug, Default)]
+pub(crate) struct Cache(RwLock<HashMap<Option<PathBuf>, CacheItem>>);
 
 /// Invalidate the cache. Call this if you do anything that might make a cached config go stale
 /// in a critical way, like changing the environment.
-pub async fn invalidate() {
-    CACHE.write().await.clear();
-}
-
-/// Initialize the configuration. Only the first call in a process runtime takes effect, so users must
-/// call this early with the required values, such as in main() in the ffx binary.
-pub async fn init(context: &EnvironmentContext) -> Result<()> {
-    let env = context.env_file_path()?;
-    if !env.is_file() {
-        tracing::debug!("initializing environment {}", env.display());
-        Environment::init_env_file(&env).await?;
-    }
-    let mut env_lock = ENV.lock().unwrap();
-    if env_lock.is_some() {
-        bail!("Attempted to set the global environment more than once in a process invocation, outside of a test");
-    }
-    env_lock.replace(context.clone());
-    Ok(())
+pub(crate) async fn invalidate(cache: &Cache) {
+    cache.0.write().await.clear();
 }
 
 fn is_cache_item_expired(item: &CacheItem, now: Instant) -> bool {
@@ -172,7 +49,13 @@ pub(crate) async fn load_config(
     environment: Environment,
     build_override: Option<BuildOverride<'_>>,
 ) -> Result<Arc<RwLock<Config>>> {
-    load_config_with_instant(&environment, build_override, Instant::now(), &CACHE).await
+    load_config_with_instant(
+        &environment,
+        build_override,
+        Instant::now(),
+        &environment.context().cache,
+    )
+    .await
 }
 
 async fn load_config_with_instant(
@@ -181,7 +64,7 @@ async fn load_config_with_instant(
     now: Instant,
     cache: &Cache,
 ) -> Result<Arc<RwLock<Config>>> {
-    let guard = cache.upgradable_read().await;
+    let guard = cache.0.upgradable_read().await;
     let build_dir = env.override_build_dir(build_override);
     let cache_hit = read_cache(&guard, build_dir, now).await;
     match cache_hit {
@@ -204,6 +87,7 @@ async fn load_config_with_instant(
 #[cfg(test)]
 mod test {
     use super::*;
+    use crate::EnvironmentContext;
     use futures::future::join_all;
     use std::time::Duration;
 
@@ -236,12 +120,12 @@ mod test {
         cache: &Cache,
     ) {
         {
-            let read_guard = cache.read().await;
+            let read_guard = cache.0.read().await;
             assert_eq!(expected_len_before, (*read_guard).len());
         }
         load(now, key, cache).await;
         {
-            let read_guard = cache.read().await;
+            let read_guard = cache.0.read().await;
             assert_eq!(expected_len_after, (*read_guard).len());
         }
     }
@@ -256,12 +140,12 @@ mod test {
     }
 
     fn setup(tests: usize) -> (Instant, Vec<Option<PathBuf>>, Cache) {
-        (Instant::now(), setup_build_dirs(tests), RwLock::new(HashMap::new()))
+        (Instant::now(), setup_build_dirs(tests), Cache::default())
     }
 
     #[fuchsia_async::run_singlethreaded(test)]
     async fn test_config_one_at_a_time() {
-        let _test_env = test_init().await.unwrap();
+        let _test_env = crate::test_init().await.unwrap();
         let tests = 10;
         let (now, build_dirs, cache) = setup(tests);
         for x in 0..tests {
@@ -271,19 +155,19 @@ mod test {
 
     #[fuchsia_async::run_singlethreaded(test)]
     async fn test_config_many_at_a_time() {
-        let _test_env = test_init().await.unwrap();
+        let _test_env = crate::test_init().await.unwrap();
         let tests = 25;
         let (now, build_dirs, cache) = setup(tests);
         let futures = build_dirs.iter().map(|x| load(now, x, &cache));
         let result = join_all(futures).await;
         assert_eq!(tests, result.len());
-        let read_guard = cache.read().await;
+        let read_guard = cache.0.read().await;
         assert_eq!(tests, (*read_guard).len());
     }
 
     #[fuchsia_async::run_singlethreaded(test)]
     async fn test_config_timeout() {
-        let _test_env = test_init().await.unwrap();
+        let _test_env = crate::test_init().await.unwrap();
         let tests = 1;
         let (now, build_dirs, cache) = setup(tests);
         load_and_test(now, 0, 1, &build_dirs[0], &cache).await;
