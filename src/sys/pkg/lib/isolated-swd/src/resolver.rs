e81e38b36295a3b8a220bfb46ce7de6e6a6a587e
Sen Jiang <senj@google.com>
2023-04-07 19:33:50 +0000

    [isolated-swd] Generate delivery blob in repository
    These tests use production pkg-resolver config, so they don't fallback
    to uncompressed when offline compression is enabled, always make
    delivery blobs available in these tests to ease transition to offline
    compression.
    
    Change-Id: Ia8498ad70dbcd64be4cd26aa9bf2631155e78f33
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/832663
    Reviewed-by: Erick Tryzelaar <etryzelaar@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Mike Comfoltey <comfoltey@google.com>
    Fuchsia-Auto-Submit: Sen Jiang <senj@google.com>

--- src/sys/pkg/lib/isolated-swd/src/resolver.rs
+++ src/sys/pkg/lib/isolated-swd/src/resolver.rs
@@ -261,6 +261,7 @@ pub mod tests {
         let repo = Arc::new(
             RepositoryBuilder::from_template_dir(EMPTY_REPO_PATH)
                 .add_package(&package)
+                .delivery_blob_type(1)
                 .build()
                 .await
                 .context("Building repo")
@@ -305,6 +306,7 @@ pub mod tests {
         let repo = Arc::new(
             RepositoryBuilder::from_template_dir(EMPTY_REPO_PATH)
                 .add_package(&package)
+                .delivery_blob_type(1)
                 .build()
                 .await
                 .context("Building repo")
