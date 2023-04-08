f23a271ec236d18574b63bd4efb4a365c19e09b4
Tamir Duberstein <tamird@google.com>
2023-04-08 01:07:36 +0000

    [fuchsia.io] Remove RightsRequest
    This was an over-engineered solution to needing POSIX semantics on Open
    calls that are not known a priori to be reaching a directory.
    
    The same information is now in the directory variant of NodeProtocols,
    which is now a table.
    
    Bug: 77623
    Change-Id: Ib8c5a5b2736377a42a5080468c54599bf0bcfcf3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/832777
    Reviewed-by: Yifei Teng <yifeit@google.com>
    Reviewed-by: Adam Barth <abarth@google.com>
    Fuchsia-Auto-Submit: Tamir Duberstein <tamird@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    API-Review: Adam Barth <abarth@google.com>

--- sdk/lib/vfs/cpp/internal/file_connection.h
+++ sdk/lib/vfs/cpp/internal/file_connection.h
@@ -52,8 +52,8 @@ class FileConnection final : public Connection, public fuchsia::io::File {
                      GetAttributesCallback callback) override {
     callback(fuchsia::io::Node2_GetAttributes_Result::WithErr(ZX_ERR_NOT_SUPPORTED));
   }
-  void Reopen(std::unique_ptr<::fuchsia::io::RightsRequest> rights_request,
-              fidl::InterfaceRequest<::fuchsia::io::Node> object_request) override {
+  void Reopen(fuchsia::io::RightsRequest rights_request,
+              fidl::InterfaceRequest<fuchsia::io::Node> object_request) override {
     object_request.Close(ZX_ERR_NOT_SUPPORTED);
   }
   void UpdateAttributes(fuchsia::io::MutableNodeAttributes MutableNodeAttributes,
