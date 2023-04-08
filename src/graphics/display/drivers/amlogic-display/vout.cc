d59bd31134143abb9cf952812119c32712181577
Yilong Li <liyl@google.com>
2023-04-08 02:39:12 +0000

    [vim3] Support display capture.
    This adds the display capture interrupt to vim3-display device.
    The display capture is independent of the display frontend
    encoder and the programming is the same as Astro / Sherlock /
    Nelson.
    
    Test: display-test --capture --num-frames 100 on VIM3
    Fixed: 124947
    Change-Id: I9c003f4ca9a134284ccce68c4b3b107ad7099e8f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/832831
    Reviewed-by: Victor Costan <costan@google.com>
    Commit-Queue: Victor Costan <costan@google.com>
    Reviewed-by: Sarah Chan <spqchan@google.com>
    Fuchsia-Auto-Submit: Victor Costan <costan@google.com>

--- src/graphics/display/drivers/amlogic-display/vout.cc
+++ src/graphics/display/drivers/amlogic-display/vout.cc
@@ -36,7 +36,7 @@ constexpr supported_features_t kDsiSupportedFeatures = supported_features_t{
 
 constexpr supported_features_t kHdmiSupportedFeatures = supported_features_t{
     .afbc = false,
-    .capture = false,
+    .capture = true,
     .hpd = true,
 };
 
