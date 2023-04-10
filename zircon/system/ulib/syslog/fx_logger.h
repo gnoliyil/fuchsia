f5a251d42d4422b253a8447c0e29d231cf9e9861
Roland McGrath <mcgrathr@google.com>
2023-04-10 23:13:05 +0000

    [syslog] Don't assume va_list is safe to use uninitialized
    It's not really kosher to use va_list without va_start
    intializing it. But in practice it's sufficient to explicitly
    default-initialize it. It's never OK to just use an uninitialized
    value, even just for passing it to be ignored elsewhere.
    
    Bug: 122875
    Change-Id: Icbe8332e09c9d6007373d1cc36ef8846c999113e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/833318
    Reviewed-by: Ankur Mittal <anmittal@google.com>
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Reviewed-by: Paul Kirth <paulkirth@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- zircon/system/ulib/syslog/fx_logger.h
+++ zircon/system/ulib/syslog/fx_logger.h
@@ -57,8 +57,7 @@ struct fx_logger {
 
   zx_status_t LogWrite(fx_log_severity_t severity, const char* tag, const char* msg,
                        const char* file = nullptr, uint32_t line = 0) {
-    va_list empty_args;
-    return VLogWrite(severity, tag, file, line, msg, empty_args, false);
+    return VLogWrite(severity, tag, file, line, msg, va_list{}, false);
   }
 
   zx_status_t SetSeverity(fx_log_severity_t log_severity) {
