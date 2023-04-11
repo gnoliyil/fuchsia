af6e8182d1219977733c70911487b886a7cf2671
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2023-04-11 12:09:01 +0000

    [log][rust] Add benchmarks for Encoder/write_event
    Benchmarks asserting the following:
    
    1) A message containing an argument of each type
    2) A message of a reasonable length containing no arguments
    3) A message of a reasaonble length containing some arguments
    4) The same message from (3) but with the arguments formatted into the
       string.
    
    fuchsia.diagnostics_log_rust.encoding_benchmarks: Encoder/WriteEvent/AllArguments              added             -              -                       4730 +/- 588 ns
    fuchsia.diagnostics_log_rust.encoding_benchmarks: Encoder/WriteEvent/MessageAsString           added             -              -                       1632 +/- 15 ns
    fuchsia.diagnostics_log_rust.encoding_benchmarks: Encoder/WriteEvent/MessageWithSomeArguments  added             -              -                       2767 +/- 41 ns
    fuchsia.diagnostics_log_rust.encoding_benchmarks: Encoder/WriteEvent/NoArguments               added             -              -                       1627 +/- 13 ns
    
    Bug: 124921
    
    Change-Id: I8aec0d281ee363503d11798a3989bd085085d4fe
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/831198
    Reviewed-by: Mark Seaborn <mseaborn@google.com>
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/lib/diagnostics/log/bench/src/encoding.rs
+++ src/lib/diagnostics/log/bench/src/encoding.rs
@@ -6,8 +6,78 @@ use diagnostics_log_encoding::encode::Encoder;
 use fidl_fuchsia_diagnostics_stream::{Argument, Value};
 use fidl_fuchsia_logger::MAX_DATAGRAM_LEN_BYTES;
 use fuchsia_criterion::{criterion, FuchsiaCriterion};
-use std::io::Cursor;
-use std::time::Duration;
+use fuchsia_zircon::{self as zx, AsHandleRef};
+use once_cell::sync::Lazy;
+use std::{collections::HashSet, io::Cursor, mem::MaybeUninit, time::Duration};
+use tracing::{Callsite, Event, Level, Metadata};
+use tracing_core::{field, identify_callsite, subscriber::Interest, Kind};
+
+static PROCESS_ID: Lazy<zx::Koid> =
+    Lazy::new(|| fuchsia_runtime::process_self().get_koid().unwrap());
+
+static THREAD_ID: Lazy<zx::Koid> = Lazy::new(|| fuchsia_runtime::thread_self().get_koid().unwrap());
+
+struct NullCallsite;
+static NULL_CALLSITE: NullCallsite = NullCallsite;
+impl Callsite for NullCallsite {
+    fn set_interest(&self, _: Interest) {
+        unreachable!("unused by the encoder")
+    }
+
+    fn metadata(&self) -> &Metadata<'_> {
+        unreachable!("unused by the encoder")
+    }
+}
+
+macro_rules! len {
+    () => (0usize);
+    ($head:ident $($tail:ident)*) => (1usize + len!($($tail)*));
+}
+
+// Generates the metadata required to create a tracing::Event with the lifetimes that it expects.
+macro_rules! make_event_metadata {
+    ($($key:ident: $value:expr),*) => {{
+        static METADATA: Metadata<'static> = Metadata::new(
+            /* name */ "benching",
+            /* target */ "bench",
+            Level::INFO,
+            Some(file!()),
+            Some(line!()),
+            Some(module_path!()),
+            field::FieldSet::new(
+                &[$( stringify!($key),)*],
+                identify_callsite!(&NULL_CALLSITE),
+            ),
+            Kind::EVENT,
+        );
+        const N : usize = len!($($key)*);
+        let fields : [field::Field; N] = [$(METADATA.fields().field(stringify!($key)).unwrap(),)*];
+        let values : [&dyn field::Value; N] = [$((&$value as &dyn field::Value),)*];
+        (&METADATA, fields, values)
+    }};
+}
+
+type ValueSetItem<'a> = (&'a field::Field, Option<&'a (dyn field::Value + 'a)>);
+
+// TODO: this will be much cleaner when Array zip is stable.
+// https://github.com/rust-lang/rust/issues/80094
+fn make_value_set<'a, const N: usize>(
+    fields: &'a [field::Field; N],
+    values: &[&'a (dyn field::Value + 'a); N],
+) -> [ValueSetItem<'a>; N] {
+    // Safety: assume_init is safe because the type is MaybeUninit which
+    // doesn't need to be initialized.
+    // Use uninit_array when stabilized.
+    // https://doc.rust-lang.org/stable/src/core/mem/maybe_uninit.rs.html#350
+    let mut data: [MaybeUninit<ValueSetItem<'a>>; N] =
+        unsafe { MaybeUninit::uninit().assume_init() };
+    for (i, slot) in data.iter_mut().enumerate() {
+        slot.write((&fields[i], Some(values[i])));
+    }
+    // See https://github.com/rust-lang/rust/issues/61956 for why we can't use transmute.
+    // Safety: everything has been initialized now.
+    unsafe { std::mem::transmute_copy::<_, [ValueSetItem<'a>; N]>(&data) }
+}
 
 #[inline]
 fn encoder() -> Encoder<Cursor<[u8; MAX_DATAGRAM_LEN_BYTES as usize]>> {
@@ -24,6 +94,85 @@ fn bench_argument(value: Value) -> impl FnMut(&mut criterion::Bencher) + 'static
             criterion::BatchSize::SmallInput,
         );
     }
+}
+
+// `tracing_core::field::private::ValidLen<'_>` is not implemented for `[(&tracing_core::Field,
+// std::option::Option<&dyn tracing::Value>); N]`
+// Therefore we cannot use const generics for doing this.
+macro_rules! impl_bench_write_event {
+    ($($n:tt),+) => {
+        $(
+            paste::paste! {
+                fn [< bench_write_event_with_ $n _args>](
+                    b: &mut criterion::Bencher,
+                    metadata: &'static Metadata<'static>,
+                    fields: [field::Field; $n],
+                    values: [&dyn field::Value; $n],
+                ) {
+                    let value_set_entries = make_value_set(&fields, &values);
+                    let value_set = metadata.fields().value_set(&value_set_entries);
+                    let event = Event::new(metadata, &value_set);
+                    b.iter_batched_ref(
+                        || encoder(),
+                        |encoder| encoder.write_event(
+                            &event,
+                            &["some-tag"],
+                            &HashSet::new(),
+                            *PROCESS_ID,
+                            *THREAD_ID,
+                            1
+                        ),
+                        criterion::BatchSize::SmallInput,
+                    )
+                }
+            }
+        )+
+    }
+}
+
+impl_bench_write_event!(1, 5, 7);
+
+fn setup_write_event_benchmarks(bench: criterion::Benchmark) -> criterion::Benchmark {
+    bench
+        .with_function("Encoder/WriteEvent/AllArguments", |b| {
+            let (metadata, fields, values) = make_event_metadata!(
+                message: "this is a log emitted from the benchmark",
+                tag: "logbench",
+                boolean: true,
+                float: 1234.5678,
+                int: -123456,
+                string: "foobarbaz",
+                uint: 123456
+            );
+            bench_write_event_with_7_args(b, metadata, fields, values);
+        })
+        .with_function("Encoder/WriteEvent/NoArguments", |b| {
+            let (metadata, fields, values) = make_event_metadata!(
+                message: "this is a log emitted from the benchmark"
+            );
+            bench_write_event_with_1_args(b, metadata, fields, values);
+        })
+        .with_function("Encoder/WriteEvent/MessageAsString", |b| {
+            let (metadata, fields, values) = make_event_metadata!(
+                message:
+                    // NOTE: the arguments here should match the bench below.
+                    concat!(
+                        "this is a log emitted from the benchmark boolean=true ",
+                        "int=98765 string=foobarbaz"
+                    )
+            );
+            bench_write_event_with_1_args(b, metadata, fields, values);
+        })
+        .with_function("Encoder/WriteEvent/MessageWithSomeArguments", |b| {
+            let (metadata, fields, values) = make_event_metadata!(
+                message: "this is a log emitted from the benchmark",
+                tag: "log-benching",
+                boolean: true,
+                int: 98765,
+                string: "foobarbaz"
+            );
+            bench_write_event_with_5_args(b, metadata, fields, values);
+        })
 }
 
 fn main() {
@@ -51,6 +200,8 @@ fn main() {
             bench_argument(Value::Text("x".repeat(size))),
         )
     }
+
+    bench = setup_write_event_benchmarks(bench);
 
     c.bench("fuchsia.diagnostics_log_rust.encoding", bench);
 }
