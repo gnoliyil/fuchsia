551e72fd01233c8371bcbf9dcee7f5608aa71bff
James Sullivan <jfsulliv@google.com>
2023-04-11 16:56:49 +0000

    [fxfs] Support resizing Fxfs images on qemu boot
    Bug: 122056
    Change-Id: Ie158418f75f98b22710c988ee4e7ce1c1cf2f15b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/830256
    Reviewed-by: Joshua Seaton <joshuaseaton@google.com>
    Commit-Queue: James Sullivan <jfsulliv@google.com>

--- tools/devshell/lib/disktools.sh
+++ tools/devshell/lib/disktools.sh
@@ -3,6 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# Portable function to truncate a file to a given length.
 fx-truncate() {
   local -r file="$1"
   local -r size="$2"
@@ -22,6 +23,103 @@ fx-truncate() {
       ;;
     *)
       head -c "${size}" /dev/zero > "${file}"
+      ;;
+  esac
+  return $?
+}
+
+# Converts human-readable numbers to an integer.  Uses power-of-two abbreviations (K == 1024, M ==
+# 1048576, etc).
+#   1314K   => 1345536
+#   13M     => 13631488
+#   1314000 => 1314000
+convert_human_readable_num() {
+  if [[ -z "${1}" ]]; then
+    return 1
+  fi
+  conv=1
+  val="$1"
+  case ${1: -1} in
+    "K")
+      conv=1024
+      val="${val%?}"
+      ;;
+    "M")
+      conv=1048576
+      val="${val%?}"
+      ;;
+    "G")
+      conv=1073741824
+      val="${val%?}"
+      ;;
+  esac
+  echo "$(( ${val} * ${conv} ))"
+}
+
+# Compares $1 to $2 where either can be a human-readable number (e.g. 12K).
+# Prints -1, 0, or 1.
+compare_human_readable_nums() {
+  left="$(convert_human_readable_num $1)"
+  right="$(convert_human_readable_num $2)"
+  if [[ -z "${left}" || -z "${right}" ]]; then
+    return 1
+  fi
+
+  if [[ "$left" == "$right" ]]; then
+    echo "0"
+  else
+    least=$(echo -e "$left\\n$right" | sort -n | head -n1)
+    if [[ "$least" == "$left" ]]; then
+      echo "-1"
+    else
+      echo "1"
+    fi
+  fi
+}
+
+_fx-extend-generic() {
+  local -r file="$1"
+  local -r size="$2"
+
+  len="$(wc -c < ${file} | awk '{print $1}')"
+  target="$(convert_human_readable_num ${size})"
+  if [[ "$len" -lt "$target" ]]; then
+    rem="$(( $target - $len ))"
+    head -c "${rem}" /dev/zero >> "${file}"
+  fi
+}
+
+# Like fx-truncate, but only ever extends a file (if the file would be truncated, this is a NOP).
+fx-extend() {
+  local -r file="$1"
+  local -r size="$2"
+  touch "${file}"
+
+  if [[ -z "${size}" ]]; then
+    echo >&2 "fx-extend: size \"${size}\" not given"
+    return 1
+  fi
+
+  case $(uname) in
+    Darwin)
+      # mkfile isn't always available on macos.
+      if [[ -n "$(which mkfile)" ]]; then
+        len="$(stat -f %z ${file})"
+        if [[ "$(compare_human_readable_nums ${len} ${size})" -lt "0" ]]; then
+          mkfile -n "${size}" "${file}"
+        fi
+      else
+        _fx-extend-generic "${file}" "${size}"
+      fi
+      ;;
+    Linux)
+      len="$(stat --format=%s ${file})"
+      if [[ "$(compare_human_readable_nums ${len} ${size})" -lt "0" ]]; then
+        truncate -s "${size}" "${file}"
+      fi
+      ;;
+    *)
+      _fx-extend-generic "${file}" "${size}"
       ;;
   esac
   return $?
