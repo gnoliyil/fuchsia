3b732d27b2ec9fd5fbd96b3ead124c3c7b425ba7
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2023-04-11 10:15:50 +0000

    [log][rust] Add initial benchmarks for arguments
    Initial results:
    
    ```
    fuchsia.diagnostics_log_rust.encoding_benchmarks: Encoder/Argument/Boolean         added             -              -                    5196 +/- 350 ns
    fuchsia.diagnostics_log_rust.encoding_benchmarks: Encoder/Argument/Floating        added             -              -                    4355 +/- 253 ns
    fuchsia.diagnostics_log_rust.encoding_benchmarks: Encoder/Argument/SignedInt       added             -              -                    4358 +/- 262 ns
    fuchsia.diagnostics_log_rust.encoding_benchmarks: Encoder/Argument/Text/1024       added             -              -                    4655 +/- 148 ns
    fuchsia.diagnostics_log_rust.encoding_benchmarks: Encoder/Argument/Text/128        added             -              -                    4480 +/- 221 ns
    fuchsia.diagnostics_log_rust.encoding_benchmarks: Encoder/Argument/Text/16         added             -              -                    4450 +/- 234 ns
    fuchsia.diagnostics_log_rust.encoding_benchmarks: Encoder/Argument/Text/256        added             -              -                    4540 +/- 188 ns
    fuchsia.diagnostics_log_rust.encoding_benchmarks: Encoder/Argument/Text/32000      added             -              -                    6857 +/- 370 ns
    fuchsia.diagnostics_log_rust.encoding_benchmarks: Encoder/Argument/Text/512        added             -              -                    4628 +/- 159 ns
    fuchsia.diagnostics_log_rust.encoding_benchmarks: Encoder/Argument/UnsignedInt     added             -              -                    4351 +/- 238 ns
    fuchsia.diagnostics_log_rust.encoding_benchmarks: Encoder/Create                   added             -              -                    36.6 +/- 4.9 ns
    ```
    
    Bug: 124921
    
    Change-Id: I8e89cc7fb120b09a1b07ab79df8e1b718b9c8ba9
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/831217
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Reviewed-by: Mark Seaborn <mseaborn@google.com>
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/lib/diagnostics/log/bench/src/encoding.rs
+++ src/lib/diagnostics/log/bench/src/encoding.rs
@@ -2,21 +2,55 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use diagnostics_log_encoding::encode::Encoder;
+use fidl_fuchsia_diagnostics_stream::{Argument, Value};
+use fidl_fuchsia_logger::MAX_DATAGRAM_LEN_BYTES;
 use fuchsia_criterion::{criterion, FuchsiaCriterion};
+use std::io::Cursor;
 use std::time::Duration;
 
+#[inline]
+fn encoder() -> Encoder<Cursor<[u8; MAX_DATAGRAM_LEN_BYTES as usize]>> {
+    let buffer = [0u8; MAX_DATAGRAM_LEN_BYTES as usize];
+    Encoder::new(Cursor::new(buffer))
+}
+
+fn bench_argument(value: Value) -> impl FnMut(&mut criterion::Bencher) + 'static {
+    move |b: &mut criterion::Bencher| {
+        let arg = Argument { name: "foo".to_string(), value: value.clone() };
+        b.iter_batched_ref(
+            || encoder(),
+            |encoder| encoder.write_argument(&arg),
+            criterion::BatchSize::SmallInput,
+        );
+    }
+}
+
 fn main() {
-    // TODO(fxbug.dev/124921): add benchmarks.
     let mut c = FuchsiaCriterion::default();
     let internal_c: &mut criterion::Criterion = &mut c;
     *internal_c = std::mem::take(internal_c)
         .warm_up_time(Duration::from_millis(1))
         .measurement_time(Duration::from_millis(100))
         // We must reduce the sample size from the default of 100, otherwise
-        // Criterion will sometimes override the 1ms + 500ms suggested times
+        // Criterion will sometimes override the 1ms + 100ms suggested times
         // and run for much longer.
         .sample_size(10);
 
-    let bench = criterion::Benchmark::new("TODO", move |_b| {});
-    c.bench("fuchsia.diagnostics_log.encoding_benchmarks", bench);
+    let mut bench = criterion::Benchmark::new("Encoder/Create", move |b| {
+        b.iter_with_large_drop(|| encoder());
+    })
+    .with_function("Encoder/Argument/Boolean", bench_argument(Value::Boolean(true)))
+    .with_function("Encoder/Argument/Floating", bench_argument(Value::Floating(1234.5678)))
+    .with_function("Encoder/Argument/UnsignedInt", bench_argument(Value::UnsignedInt(12345)))
+    .with_function("Encoder/Argument/SignedInt", bench_argument(Value::SignedInt(-12345)));
+
+    for size in [16, 128, 256, 512, 1024, 32000] {
+        bench = bench.with_function(
+            &format!("Encoder/Argument/Text/{}", size),
+            bench_argument(Value::Text("x".repeat(size))),
+        )
+    }
+
+    c.bench("fuchsia.diagnostics_log_rust.encoding", bench);
 }
---
+++ src/tests/end_to_end/perf/test/diagnostics_log_rust_benchmarks_test.dart
@@ -0,0 +1,23 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// TODO(https://fxbug.dev/84961): Fix null safety and remove this language version.
+// @dart=2.9
+
+import 'package:test/test.dart';
+
+import 'helpers.dart';
+
+void main() {
+  enableLoggingOutput();
+
+  test('diagnostics_log_rust_encoding_benchmarks', () async {
+    await runTestComponent(
+        packageName: 'diagnostics-log-rust-benchmarks',
+        componentName: 'encoding.cm',
+        commandArgs: PerfTestHelper.componentOutputPath,
+        expectedMetricNamesFile:
+            'fuchsia.diagnostics_log_rust.encoding.txt');
+  }, timeout: Timeout.none);
+}
