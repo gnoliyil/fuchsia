6e01b7d935488e581ef9bebbbcbf00b8f992b44d
Kendal Harland <kjharland@google.com>
2023-04-12 18:04:42 +0000

    [detect] Remove unnecessary Arc & RwLock.
    Removes unecessary Arc and RwLock from FakeCrashReporter
    (the child UnboundedSenderInner is threadsafe on its own) and makes
    send_test_event sync since it no longer needs to await any futures.
    
    Change-Id: I9f5fdf4d804932f9e248eef82c9d355a22e5dfc6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/834664
    Fuchsia-Auto-Submit: Kendal Harland <kjharland@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Reviewed-by: Miguel Flores <miguelfrde@google.com>

--- src/diagnostics/detect/integration/fake_crash_reporter.rs
+++ src/diagnostics/detect/integration/fake_crash_reporter.rs
@@ -9,7 +9,6 @@ use {
     fcrash::FileReportResults,
     fidl_fuchsia_feedback as fcrash,
     fidl_server::*,
-    std::sync::{Arc, RwLock},
     tracing::*,
 };
 
@@ -18,7 +17,7 @@ const REPORT_PROGRAM_NAME: &str = "triage_detect";
 /// FakeCrashReporter can be injected to capture Detect's crash report requests.
 #[derive(Clone)]
 pub struct FakeCrashReporter {
-    event_sender: Arc<RwLock<TestEventSender>>,
+    event_sender: TestEventSender,
     done_signaler: DoneSignaler,
 }
 
@@ -42,16 +41,11 @@ fn evaluate_report(report: &fcrash::CrashReport) -> Result<String, Error> {
 
 impl FakeCrashReporter {
     pub fn new(event_sender: TestEventSender, done_signaler: DoneSignaler) -> Self {
-        let event_sender = Arc::new(RwLock::new(event_sender));
         Self { event_sender, done_signaler }
     }
 
-    async fn send_test_event(&self, event: Result<TestEvent, Error>) -> Result<(), Error> {
-        self.event_sender
-            .write()
-            .expect("failed to acquire lock on event sender")
-            .unbounded_send(event)?;
-        Ok(())
+    fn send_test_event(&self, event: Result<TestEvent, Error>) -> Result<(), Error> {
+        Ok(self.event_sender.unbounded_send(event)?)
     }
 }
 
@@ -63,12 +57,12 @@ impl AsyncRequestHandler<fcrash::CrashReporterMarker> for FakeCrashReporter {
                 match evaluate_report(&report) {
                     Ok(signature) => {
                         info!("Received crash report: {}", signature);
-                        self.send_test_event(Ok(TestEvent::CrashReport(signature))).await?;
+                        self.send_test_event(Ok(TestEvent::CrashReport(signature)))?;
                         responder.send(&mut Ok(())).context("failed to send response to client")?;
                     }
                     Err(problem) => {
                         error!("Problem in report: {}", problem);
-                        self.send_test_event(Err(problem)).await?;
+                        self.send_test_event(Err(problem))?;
                         self.done_signaler.signal_done().await;
                     }
                 }
@@ -77,14 +71,14 @@ impl AsyncRequestHandler<fcrash::CrashReporterMarker> for FakeCrashReporter {
                 match evaluate_report(&report) {
                     Ok(signature) => {
                         info!("Received crash report: {}", signature);
-                        self.send_test_event(Ok(TestEvent::CrashReport(signature))).await?;
+                        self.send_test_event(Ok(TestEvent::CrashReport(signature)))?;
                         responder
                             .send(&mut Ok(FileReportResults::EMPTY))
                             .context("failed to send response to client")?;
                     }
                     Err(problem) => {
                         error!("Problem in report: {}", problem);
-                        self.send_test_event(Err(problem)).await?;
+                        self.send_test_event(Err(problem))?;
                         self.done_signaler.signal_done().await;
                     }
                 }
