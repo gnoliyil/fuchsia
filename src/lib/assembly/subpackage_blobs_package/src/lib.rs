959827065e98dbaca64b7e5315b95364fac77975
Erick Tryzelaar <etryzelaar@google.com>
2023-04-12 23:52:46 +0000

    [assembly] Add subpackage_blobs_package builder
    This adds a helper that generates the subpackage blobs package, which
    will enable an old build that doesn't understand subpackages to download
    the subpackage blobs during an OTA.
    
    Bug: 123712
    
    Change-Id: I68c31a789b4ee2bd639b412fc021cbfb42c947f7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/826577
    Fuchsia-Auto-Submit: Erick Tryzelaar <etryzelaar@google.com>
    Reviewed-by: Aaron Wood <aaronwood@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

---
+++ src/lib/assembly/subpackage_blobs_package/src/lib.rs
@@ -0,0 +1,11 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#![deny(missing_docs)]
+
+//! Library for constructing the Subpackage Blobs Package.
+
+mod subpackage_blobs_package;
+
+pub use subpackage_blobs_package::SubpackageBlobsPackageBuilder;
