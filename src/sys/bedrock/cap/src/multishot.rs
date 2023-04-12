4b07dd5d3d8a29ffc89150ae9e518186d9e018b7
Yegor Pomortsev <ypomortsev@google.com>
2023-04-12 23:35:42 +0000

    [bedrock] Implement Remote for multishot::Receiver
    This makes it possible for Senders to be used over FIDL in programs.
    
    For example, a program can use this to serve a FIDL protocol by
    receiving a server end for that protocol.
    
    Bug: 122024
    
    Change-Id: Idcc8a49bf9d588dbd0d0a41a3b85faaa1ecf69f2
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/834456
    Commit-Queue: Yegor Pomortsev <ypomortsev@google.com>
    Reviewed-by: Claire Gonyeo <cgonyeo@google.com>
    API-Review: Yegor Pomortsev <ypomortsev@google.com>

--- src/sys/bedrock/cap/src/multishot.rs
+++ src/sys/bedrock/cap/src/multishot.rs
@@ -5,10 +5,10 @@ use {
     crate::cap::{Capability, Remote},
     anyhow::{Context, Error},
     fidl::endpoints::{create_request_stream, ControlHandle, Responder},
-    fidl_fuchsia_component_bedrock as fbedrock, fuchsia_zircon as zx,
+    fidl_fuchsia_component_bedrock as fbedrock, fuchsia_async as fasync, fuchsia_zircon as zx,
     fuchsia_zircon::HandleBased,
     futures::channel::mpsc,
-    futures::{future::BoxFuture, FutureExt, TryStreamExt},
+    futures::{future::BoxFuture, FutureExt, StreamExt, TryStreamExt},
 };
 
 #[derive(Debug)]
@@ -71,9 +71,47 @@ pub struct Receiver<T: Capability>(pub mpsc::UnboundedReceiver<T>);
 
 impl<T: Capability> Capability for Receiver<T> {}
 
+impl<T: Capability> Receiver<T> {
+    async fn serve_receiver(
+        mut self,
+        mut stream: fbedrock::ReceiverRequestStream,
+    ) -> Result<(), Error> {
+        // Tasks that serve the zx handles for values received from this Receiver.
+        let mut value_tasks = vec![];
+
+        while let Some(request) =
+            stream.try_next().await.context("failed to read request from stream")?
+        {
+            match request {
+                fbedrock::ReceiverRequest::Receive { responder, .. } => match self.0.next().await {
+                    Some(value) => {
+                        let (handle, fut) = Box::new(value).to_zx_handle();
+                        if let Some(fut) = fut {
+                            value_tasks.push(fasync::Task::spawn(fut));
+                        }
+                        responder.send(handle).context("failed to send response")?;
+                    }
+                    None => {
+                        responder.control_handle().shutdown_with_epitaph(zx::Status::OK);
+                    }
+                },
+            }
+        }
+
+        Ok(())
+    }
+}
+
 impl<T: Capability> Remote for Receiver<T> {
     fn to_zx_handle(self: Box<Self>) -> (zx::Handle, Option<BoxFuture<'static, ()>>) {
-        todo!()
+        let (receiver_client_end, receiver_stream) =
+            create_request_stream::<fbedrock::ReceiverMarker>().unwrap();
+
+        let fut = async move {
+            (*self).serve_receiver(receiver_stream).await.expect("failed to serve Receiver");
+        };
+
+        (receiver_client_end.into_handle(), Some(fut.boxed()))
     }
 }
 
@@ -123,6 +161,39 @@ mod tests {
         let handle = receiver.0.next().await.expect("receiver should have a handle");
         let got_koid = handle.get_koid().unwrap();
         assert_eq!(got_koid, expected_koid);
+
+        Ok(())
+    }
+
+    /// Tests that a capability sent through a Sender can be received through the FIDL method
+    /// `Receiver.Receive`.
+    #[fuchsia::test]
+    async fn serve_receiver_receive() -> Result<(), Error> {
+        let (sender, receiver) = multishot::<Handle>();
+
+        let (receiver_proxy, receiver_stream) =
+            create_proxy_and_stream::<fbedrock::ReceiverMarker>()?;
+        let server = receiver.serve_receiver(receiver_stream);
+
+        // Create an event and get its koid.
+        let event = zx::Event::create();
+        let expected_koid = event.get_koid().unwrap();
+
+        // Send the event through the Sender.
+        let handle = Handle::from(event.into_handle());
+        sender.0.unbounded_send(handle).unwrap();
+
+        let client = async move {
+            let handle = receiver_proxy.receive().await.context("failed to call Receive")?;
+
+            // The sent handle should be available on the receiving end.
+            let got_koid = handle.get_koid().unwrap();
+            assert_eq!(got_koid, expected_koid);
+
+            Ok(())
+        };
+
+        try_join!(client, server).map(|_| ())?;
 
         Ok(())
     }
