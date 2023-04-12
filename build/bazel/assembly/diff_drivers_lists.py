9030da70b4ea3588df32375ee07c784eb653c889
Jay Zhuang <jayzhuang@google.com>
2023-04-12 10:06:48 +0000

    [drivers_list] Diff lists between GN and Bazel
    ... to ensure that Bazel assembly doesn't introduce any regression in
    driver checks.
    
    Change-Id: Ia718efb5d2c66a4ccb7c23bc4ef42aee047821af
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/828976
    Reviewed-by: David Turner <digit@google.com>
    Reviewed-by: Yaar Schnitman <yaar@google.com>
    Fuchsia-Auto-Submit: Jay Zhuang <jayzhuang@google.com>
    Commit-Queue: Jay Zhuang <jayzhuang@google.com>

---
+++ build/bazel/assembly/diff_drivers_lists.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3.8
+# Copyright 2023 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import difflib
+import json
+import sys
+
+
+def main():
+  parser = argparse.ArgumentParser(
+      description="Compares drivers lists and outputs diffs"
+  )
+  parser.add_argument(
+      "--drivers-list1", type=argparse.FileType("r"), required=True
+  )
+  parser.add_argument(
+      "--drivers-list2", type=argparse.FileType("r"), required=True
+  )
+  parser.add_argument("--output", type=argparse.FileType("w"), required=True)
+  args = parser.parse_args()
+
+  drivers_list1 = sorted(args.drivers_list1.readlines())
+  drivers_list2 = sorted(args.drivers_list2.readlines())
+  diff = difflib.unified_diff(
+      drivers_list1,
+      drivers_list2,
+      args.drivers_list1.name,
+      args.drivers_list2.name,
+      lineterm="",
+  )
+  diffstr = "\n".join(diff)
+  args.output.write(diffstr)
+
+  if len(diffstr) != 0:
+    print(f"Error: non-empty diff\n\n{diffstr}", file=sys.stderr)
+    return 1
+
+
+if __name__ == "__main__":
+  sys.exit(main())
