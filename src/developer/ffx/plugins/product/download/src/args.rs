56265a3fc5a60057e9a140a4e89d0a18f2fec876
Dave Schuyler <dschuyler@chromium.org>
2023-04-13 01:09:20 +0000

    [product] add product download + https support
    Move current product get functionality into product download.
    Also adds http/s download support and unit test for http
    download.
    
    Bug: 125131
    Change-Id: I8aa41d3a1924ed2d2cd5ee78ce8503746a6301fb
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/832828
    Reviewed-by: Erick Tryzelaar <etryzelaar@google.com>
    Commit-Queue: Dave Schuyler <dschuyler@google.com>

---
+++ src/developer/ffx/plugins/product/download/src/args.rs
@@ -0,0 +1,44 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use argh::FromArgs;
+use ffx_core::ffx_command;
+use pbms::AuthFlowChoice;
+use std::path::PathBuf;
+
+/// Retrieve image data.
+#[ffx_command()]
+#[derive(FromArgs, Debug, PartialEq)]
+#[argh(
+    subcommand,
+    name = "download",
+    example = "\
+    Auth flow choices for --auth include:\
+    \n  `--auth no-auth` do not use auth.\
+    \n  `--auth oob` to use Out-of-Band auth (deprecated).\
+    \n  `--auth pkce` to use PKCE auth flow (requires GUI browser).\
+    \n  `--auth device-experimental` to use device flow.\
+    \n  `--auth <path/to/exe>` run tool at given path which will print an \
+    access token to stdout and exit 0.
+    \n  `--auth default` let the tool decide which auth flow to use.\
+    "
+)]
+pub struct DownloadCommand {
+    /// get the data again, even if it's already present locally.
+    #[argh(switch)]
+    pub force: bool,
+
+    /// use specific auth flow for oauth2 (see examples; default: pkce).
+    #[argh(option, default = "AuthFlowChoice::Default")]
+    pub auth: AuthFlowChoice,
+
+    /// url to the transfer manifest of the product bundle to download.
+    #[argh(positional)]
+    pub manifest_url: String,
+
+    /// local directory to download the product bundle into (default:
+    /// "local_pb").
+    #[argh(positional, default = "PathBuf::from(\"local_pb\")")]
+    pub out_dir: PathBuf,
+}
