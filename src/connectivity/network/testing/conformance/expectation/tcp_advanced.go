c99889505c3c45adef7560f975e6adbf77aa53d1
Nick Brown <nickbrow@google.com>
2023-04-13 22:40:32 +0000

    [netconform] Add tcp-advanced expectations for NS3
    Bug: 121477
    Change-Id: I43f02184bcd3c7035e47db5188865ae096aa21e6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/836078
    Reviewed-by: Amanda Tait <atait@google.com>
    Commit-Queue: Nick Brown <nickbrow@google.com>

--- src/connectivity/network/testing/conformance/expectation/tcp_advanced.go
+++ src/connectivity/network/testing/conformance/expectation/tcp_advanced.go
@@ -21,3 +21,19 @@ var tcpAdvancedExpectations map[AnvlCaseNumber]outcome.Outcome = map[AnvlCaseNum
 	{4, 17}: Pass,
 	{6, 17}: Pass,
 }
+
+var tcpAdvancedExpectationsNS3 map[AnvlCaseNumber]outcome.Outcome = map[AnvlCaseNumber]outcome.Outcome{
+	{1, 17}: Fail,
+	{1, 18}: Fail,
+	{2, 18}: Fail,
+	{2, 19}: Pass,
+	{2, 20}: Fail,
+	{2, 21}: Fail,
+	{2, 22}: Fail,
+	{2, 23}: Fail,
+	{2, 24}: Fail,
+	{2, 25}: Fail,
+	{3, 17}: Flaky, // TODO(https://fxbug.dev/105170): Fix flake.
+	{4, 17}: Pass,
+	{6, 17}: Pass,
+}
