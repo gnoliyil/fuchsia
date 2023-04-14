d16cb7bf65aee398c02ff6401b2e42ce2b223abc
Colin Nelson <colnnelson@google.com>
2023-04-14 22:03:12 +0000

    [tools][funnel]: add support for multiple devices
    Bug: 122612
    Change-Id: I9b00e706f83fd01e5ffef279ba754e72f721f6e6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/833348
    Fuchsia-Auto-Submit: Colin Nelson <colnnelson@google.com>
    Reviewed-by: Megan Batty <mgnb@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- tools/funnel/src/target.rs
+++ tools/funnel/src/target.rs
@@ -2,8 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
-use anyhow::Result;
+use anyhow::{anyhow, bail, Result};
 use fidl_fuchsia_developer_ffx::TargetInfo;
+use std::io::{stdin, stdout, BufRead, Write};
 
 pub(crate) async fn choose_target(
     targets: Vec<TargetInfo>,
@@ -23,11 +24,108 @@ pub(crate) async fn choose_target(
         return Ok(found_target.cloned().unwrap());
     }
 
-    // TODO(colnnelson): If there is more than one target available, and they have not specified
-    // the target to use, prompt user to choose it
-    let first = targets.first();
-    match first {
-        Some(f) => Ok(f.clone()),
-        None => anyhow::bail!("No targets"),
+    match targets.len() {
+        0 => {
+            bail!("No targets discovered")
+        }
+        1 => {
+            let first = targets.first();
+            match first {
+                Some(f) => Ok(f.clone()),
+                None => bail!("No targets"),
+            }
+        }
+        _ => {
+            // Okay there is more than one target available and they haven't
+            // specified a target to use, lets prompt them for one
+            let stdin = stdin().lock();
+            prompt_for_target(stdin, stdout(), targets).await
+        }
+    }
+}
+
+async fn prompt_for_target<R, W>(
+    mut input: R,
+    mut output: W,
+    targets: Vec<TargetInfo>,
+) -> Result<TargetInfo>
+where
+    R: BufRead,
+    W: Write,
+{
+    writeln!(output, "Multiple devices detected. Please choose the target by number:")?;
+    for (i, t) in targets.iter().enumerate() {
+        let name = t.clone().nodename.unwrap_or("unknown".to_string());
+        writeln!(output, "  {i}: {name}")?;
+    }
+    write!(output, "Choice: ")?;
+    output.flush()?;
+
+    let mut choice = String::new();
+    input.read_line(&mut choice)?;
+    let idx: usize = choice
+        .trim()
+        .parse()
+        .map_err(|_| anyhow!("Choice: {} was not an unsigned integer", choice))?;
+    if idx == 0 || idx > targets.len() {
+        bail!("Invalid choice: {}", idx);
+    }
+
+    targets
+        .get(idx - 1)
+        .ok_or_else(|| anyhow!("Could not get target {} from collection", (idx - 1)))
+        .cloned()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn test_prompt_for_target() -> Result<()> {
+        let input = b"1";
+        let output = Vec::new();
+        let targets = vec![
+            TargetInfo { nodename: Some("cytherera".to_string()), ..TargetInfo::EMPTY },
+            TargetInfo { nodename: Some("alecto".to_string()), ..TargetInfo::EMPTY },
+        ];
+
+        let res = prompt_for_target(&input[..], output, targets).await?;
+        assert_eq!(
+            res,
+            TargetInfo { nodename: Some("cytherera".to_string()), ..TargetInfo::EMPTY }
+        );
+        Ok(())
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn test_prompt_for_target_not_usize_error() -> Result<()> {
+        let input = b"asdf";
+        let output = Vec::new();
+        let targets = vec![
+            TargetInfo { nodename: Some("cytherera".to_string()), ..TargetInfo::EMPTY },
+            TargetInfo { nodename: Some("alecto".to_string()), ..TargetInfo::EMPTY },
+        ];
+
+        let res = prompt_for_target(&input[..], output, targets).await;
+        assert!(res.is_err());
+        assert_eq!(format!("{}", res.unwrap_err()), "Choice: asdf was not an unsigned integer");
+        Ok(())
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn test_prompt_for_target_out_of_range_error() -> Result<()> {
+        let input = b"4";
+        let output = Vec::new();
+        let targets = vec![
+            TargetInfo { nodename: Some("cytherera".to_string()), ..TargetInfo::EMPTY },
+            TargetInfo { nodename: Some("alecto".to_string()), ..TargetInfo::EMPTY },
+        ];
+
+        let res = prompt_for_target(&input[..], output, targets).await;
+        assert!(res.is_err());
+        assert_eq!(format!("{}", res.unwrap_err()), "Invalid choice: 4");
+        Ok(())
     }
 }
