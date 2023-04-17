975277330825e0d8e885eca07aefc6fab549c6c1
Ghanan Gowripalan <ghanan@google.com>
2023-04-17 17:55:31 +0000

    [async-utils] Support keeping hanging-get server hydrated
    ...with an in-flight request.
    
    This change updates `async_utils::hanging_get::client::HangingGetStream`
    to be configured to eagerly send a (hanging-get) request so that the
    server is able to serve a response as quickly as possible so the
    `Future` is able to do meaningful work as soon as possible.
    
    Before this change, a request was sent during a poll following:
      1) the creation of the `HangingGetStream`
      2) the completion of a previous in-flight request/`Future`
    
    Now, a request is sent immediately after the above situations (before
    polling) if configured to do so.
    
    Change-Id: I3580232ffe6e56f9065d634d2900ef0d03e0b84c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/837056
    Fuchsia-Auto-Submit: Ghanan Gowripalan <ghanan@google.com>
    Reviewed-by: Marie Janssen <jamuraa@google.com>
    Commit-Queue: Ghanan Gowripalan <ghanan@google.com>

--- src/lib/async-utils/src/hanging_get/client.rs
+++ src/lib/async-utils/src/hanging_get/client.rs
@@ -21,6 +21,7 @@ pub struct HangingGetStream<P, O, Q = fn(&P) -> QueryResponseFut<O>> {
     proxy: P,
     query: Q,
     response: QueryResponseFut<O>,
+    eager: bool,
 }
 
 impl<P, O, Q> Stream for HangingGetStream<P, O, Q>
@@ -32,10 +33,36 @@ where
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let mut this = self.project();
-        if this.response.is_terminated() {
-            *this.response = (&mut this.query)(&this.proxy);
+        if *this.eager {
+            match this.response.poll_unpin(cx) {
+                Poll::Ready(o) => {
+                    let QueryResponseFut(prev_response) =
+                        std::mem::replace(this.response, (&mut this.query)(&this.proxy));
+
+                    if cfg!(debug_assertions) {
+                        match prev_response {
+                            MaybeDone::Gone => {}
+                            MaybeDone::Future(_) => {
+                                panic!("previous uncompleted future still exists")
+                            }
+                            MaybeDone::Done(_) => {
+                                panic!("previous completed future's result not taken")
+                            }
+                        }
+                    } else {
+                        let _: MaybeDone<_> = prev_response;
+                    }
+
+                    Poll::Ready(Some(o))
+                }
+                Poll::Pending => Poll::Pending,
+            }
+        } else {
+            if this.response.is_terminated() {
+                *this.response = (&mut this.query)(&this.proxy);
+            }
+            this.response.poll_unpin(cx).map(Some)
         }
-        this.response.poll_unpin(cx).map(Some)
     }
 }
 
@@ -54,19 +81,35 @@ where
     Q: FnMut(&P) -> QueryResponseFut<O>,
     O: Unpin,
 {
-    /// Creates a new hanging-get stream.
+    fn new_inner(proxy: P, mut query: Q, eager: bool) -> Self {
+        let response = if eager { query(&proxy) } else { QueryResponseFut(MaybeDone::Gone) };
+
+        Self { proxy, query, response, eager }
+    }
+
+    /// Creates a new lazily-polled hanging-get stream.
     pub fn new(proxy: P, query: Q) -> Self {
-        Self { proxy, query, response: QueryResponseFut(MaybeDone::Gone) }
+        Self::new_inner(proxy, query, false /* eager */)
+    }
+
+    /// Creates a new eagerly-polled hanging-get stream.
+    pub fn new_eager(proxy: P, query: Q) -> Self {
+        Self::new_inner(proxy, query, true /* eager */)
     }
 }
 
-impl<P, O> HangingGetStream<P, O>
+impl<P, O> HangingGetStream<P, O, fn(&P) -> QueryResponseFut<O>>
 where
     O: Unpin,
 {
-    /// Creates a new hanging-get stream with a function pointer.
+    /// Creates a new lazily-polled hanging-get stream with a function pointer.
     pub fn new_with_fn_ptr(proxy: P, query: fn(&P) -> QueryResponseFut<O>) -> Self {
         Self::new(proxy, query)
+    }
+
+    /// Creates a new eagerly-polled hanging-get stream with a function pointer.
+    pub fn new_eager_with_fn_ptr(proxy: P, query: fn(&P) -> QueryResponseFut<O>) -> Self {
+        Self::new_eager(proxy, query)
     }
 }
 
@@ -89,13 +132,28 @@ mod tests {
     }
 
     #[fasync::run_singlethreaded(test)]
-    async fn generates_items() {
+    async fn generates_items_lazily() {
         let proxy = TestProxy { state: Cell::new(0) };
         let mut watcher = HangingGetStream::new(proxy, TestProxy::watch);
 
         const ITERS: usize = 3;
         for i in 0..ITERS {
             assert!(watcher.response.is_terminated());
+            assert_eq!(watcher.try_next().await.expect("failed to get next item"), Some(i));
+        }
+    }
+
+    #[fasync::run_singlethreaded(test)]
+    async fn generates_items_eagerly() {
+        let proxy = TestProxy { state: Cell::new(0) };
+        let mut watcher = HangingGetStream::new_eager_with_fn_ptr(proxy, TestProxy::watch);
+
+        const ITERS: usize = 3;
+        for i in 0..ITERS {
+            assert!(
+                !watcher.response.is_terminated(),
+                "should keep the server hydrated with an in-flight request",
+            );
             assert_eq!(watcher.try_next().await.expect("failed to get next item"), Some(i));
         }
     }
