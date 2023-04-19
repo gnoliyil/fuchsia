a0d03b207d5838bc6bb2c968d5462ea43f8b33ff
Casey Dahlin <sadmac@google.com>
2023-04-19 01:16:18 +0000

    [drivers][overnet] Introduce Overnet USB driver
    This is a driver that will sustain using the Overnet link protocol over
    USB. The driver itself is fairly minimal; it publishes a bulk transport
    in either direction and forwards traffic to a FIDL client. It does
    essentially no actual protocol interpretation.
    
    Fixed: 123598
    
    Change-Id: Ia3653dad97e23da3b5f1ff1b6ee23dbfce73fd52
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/705127
    API-Review: Dave Schuyler <dschuyler@google.com>
    Commit-Queue: Casey Dahlin <sadmac@google.com>
    Reviewed-by: Sam Hansen <hansens@google.com>
    Reviewed-by: Aaron Wood <aaronwood@google.com>

--- src/lib/ddk/include/lib/ddk/protodefs.h
+++ src/lib/ddk/include/lib/ddk/protodefs.h
@@ -183,5 +183,6 @@ DDK_PROTOCOL_DEF(ADB,                     165, "adb", 0)
 DDK_PROTOCOL_DEF(USB_INTERFACE_ASSOCIATION, 166, "usb-interface-association", PF_NOPUB)
 DDK_PROTOCOL_DEF(FASTBOOT, 167, "fastboot", 0)
 DDK_PROTOCOL_DEF(WLAN_FACTORY,            168,   "wlan-factory", 0)
+DDK_PROTOCOL_DEF(OVERNET,                 169, "overnet-usb", 0)
 #undef DDK_PROTOCOL_DEF
 #endif
