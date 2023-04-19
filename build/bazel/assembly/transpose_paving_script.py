b68e252251a88a216c56b42ebbd03167c7c2f95a
David 'Digit' Turner <digit@google.com>
2023-04-19 09:22:29 +0000

    [fx] Make `fx pave` work with product bundles.
    This CL ensures that if the build configuration uses a
    Bazel product bundle, then `fx pave` will work correctly.
    
    In this case, the script will pick up the pave.sh script
    that comes with the produc bundle, which will be in a
    different location.
    
    Note that this requires fixing the GN bazel_product_bundle()
    to properly transpose the pave.sh script generated by Bazel.
    The script contains relative directory paths like
    "product_bundle_eng_out" that needs to be transposed to
    their equivalent in the Ninja output directory, i.e.
    "product_bundle".
    
    This CL only fixes `fx pave`. A future CL will handle
    `fx pave-remote` as well to fix the bug.
    
    Bug: 125445
    Change-Id: I31461054047059712322d0b5b76a11de31852c95
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/838838
    Commit-Queue: David Turner <digit@google.com>
    Reviewed-by: Jay Zhuang <jayzhuang@google.com>

---
+++ build/bazel/assembly/transpose_paving_script.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env fuchsia-vendored-python
+# Copyright 2023 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+'''Transpose a Fuchsia paving script, replacing SRC_DIR with DST_DIR in
+its commands.'''
+
+import argparse
+import os
+import sys
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        '--input',
+        type=argparse.FileType('r'),
+        required=True,
+        help='Path to input script.')
+    parser.add_argument(
+        '--output', required=True, help='Path to output script.')
+    parser.add_argument(
+        '--src-dir', required=True, help='Name of source directory.')
+    parser.add_argument(
+        '--dst-dir',
+        default='product_bundle',
+        help='Name of destination directory.')
+
+    args = parser.parse_args()
+
+    input_data = args.input.read()
+
+    # Replace {dir}/<src_dir>/ with {dir}/<dst_dir>/
+    output_data = input_data.replace(
+        '{dir}/' + args.src_dir, '{dir}/' + args.dst_dir)
+
+    with open(args.output, 'w') as f:
+        f.write(output_data)
+
+    # Make the output executable
+    os.chmod(args.output, 0o755)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
