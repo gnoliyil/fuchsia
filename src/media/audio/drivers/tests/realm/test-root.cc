e6bd7d6b52624b3b1e9b6d6d25b2f34983b68714
David Gilhooley <dgilhooley@google.com>
2023-04-21 17:21:14 +0000

    [audio][tests] Add root driver
    Add a root driver that binds to the test root and creates a node
    for each child driver.
    
    This is preferred because the driver framework is about to stop
    allowing multiple drivers to bind to the test root.
    
    Bug: 125832
    Change-Id: I57230ff454335b649c4c235f1ec87d1641310fcb
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/842396
    Fuchsia-Auto-Submit: David Gilhooley <dgilhooley@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

---
+++ src/media/audio/drivers/tests/realm/test-root.cc
@@ -0,0 +1,78 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <bind/fuchsia/test/audio/cpp/bind.h>
+#include <bind/fuchsia/test/cpp/bind.h>
+#include <ddktl/device.h>
+
+#include "src/media/audio/drivers/tests/realm/test_root-bind.h"
+
+namespace audio {
+
+class Child : public ddk::Device<Child> {
+ public:
+  explicit Child(zx_device_t* parent) : ddk::Device<Child>(parent) {}
+
+  static zx_status_t Create(zx_device_t* parent, const char* name, const char* bind_name) {
+    std::unique_ptr<Child> child = std::make_unique<Child>(parent);
+    zx_device_str_prop_t str_props[] = {
+        {bind_fuchsia_test::TEST_CHILD.c_str(), str_prop_str_val(bind_name)},
+    };
+    zx_status_t status = child->DdkAdd(ddk::DeviceAddArgs(name).set_str_props(str_props));
+    if (status != ZX_OK) {
+      return status;
+    }
+
+    child.release();
+    return ZX_OK;
+  }
+  void DdkRelease() { delete this; }
+};
+
+class Test : public ddk::Device<Test> {
+ public:
+  explicit Test(zx_device_t* parent) : ddk::Device<Test>(parent) {}
+
+  static zx_status_t Bind(void* ctx, zx_device_t* parent) {
+    std::unique_ptr<Test> test = std::make_unique<Test>(parent);
+    zx_status_t status =
+        test->DdkAdd(ddk::DeviceAddArgs("audio_test_root").set_flags(DEVICE_ADD_NON_BINDABLE));
+    if (status != ZX_OK) {
+      return status;
+    }
+
+    status = Child::Create(test->zxdev(), "codec-parent",
+                           bind_fuchsia_test_audio::TEST_CHILD_CODEC.c_str());
+    if (status != ZX_OK) {
+      return status;
+    }
+
+    status = Child::Create(test->zxdev(), "codec2-parent",
+                           bind_fuchsia_test_audio::TEST_CHILD_CODEC2.c_str());
+    if (status != ZX_OK) {
+      return status;
+    }
+
+    status =
+        Child::Create(test->zxdev(), "dai-parent", bind_fuchsia_test_audio::TEST_CHILD_DAI.c_str());
+    if (status != ZX_OK) {
+      return status;
+    }
+
+    test.release();
+    return ZX_OK;
+  }
+  void DdkRelease() { delete this; }
+};
+
+static constexpr zx_driver_ops_t driver_ops = []() {
+  zx_driver_ops_t ops = {};
+  ops.version = DRIVER_OPS_VERSION;
+  ops.bind = Test::Bind;
+  return ops;
+}();
+
+}  // namespace audio
+
+ZIRCON_DRIVER(audio_test_root, audio::driver_ops, "zircon", "0.1");
