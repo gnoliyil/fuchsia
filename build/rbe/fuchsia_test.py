3b3866ff19e14262316c4b5489101643af70266a
David Fang <fangism@google.com>
2023-04-21 07:25:56 +0000

    [rbe][gcc] Add missing support tools (workaround)
    gcc needs related tools like 'as', 'cc1', 'cc1plus' to work.
    
    Bug: b/239101612
    Bug: 96250
    Bug: 109078
    Change-Id: Ieffc0573323c38bb6996f6b0fe75b41c6023933d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/840823
    Reviewed-by: Jay Zhuang <jayzhuang@google.com>
    Commit-Queue: David Fang <fangism@google.com>

--- build/rbe/fuchsia_test.py
+++ build/rbe/fuchsia_test.py
@@ -32,6 +32,19 @@ class RemoteExecutableTests(unittest.TestCase):
         )
 
 
+class GCCSupportToolsTests(unittest.TestCase):
+
+    def test_partial_paths(self):
+        gcc_install_base = Path('../../some/where/gcc')
+        with mock.patch.object(Path, 'glob',
+                               return_value=iter(['0.99'])) as mock_version:
+            tools = list(
+                fuchsia.gcc_support_tools(gcc_install_base / 'bin/g++'))
+        self.assertEqual({t.name for t in tools}, {'as', 'cc1', 'cc1plus'})
+        for t in tools:
+            self.assertIn(gcc_install_base, t.parents)
+
+
 class RustStdlibDirTests(unittest.TestCase):
 
     def test_substitution(self):
