3c1b1c439eed95a6417a652f4bcf75b127e708e8
Amanda Tait <atait@google.com>
2023-04-24 14:59:00 +0000

    [netconform] Add ipv6-pmtu NS3 expectations
    Bug: 125778
    Bug: 121477
    Change-Id: I39063258cb0aaffbf2f7f596c59bca3af66f4385
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/842519
    Reviewed-by: Nick Brown <nickbrow@google.com>
    Commit-Queue: Amanda Tait <atait@google.com>

--- src/connectivity/network/testing/conformance/expectation/ipv6_pmtu.go
+++ src/connectivity/network/testing/conformance/expectation/ipv6_pmtu.go
@@ -17,3 +17,15 @@ var ipv6PmtuExpectations map[AnvlCaseNumber]outcome.Outcome = map[AnvlCaseNumber
 	{4, 2}: Fail,
 	{5, 1}: Fail,
 }
+
+var ipv6PmtuExpectationsNS3 map[AnvlCaseNumber]outcome.Outcome = map[AnvlCaseNumber]outcome.Outcome{
+	// TODO(https://fxbug.dev/123534): Expect pass after next release with ANVL fix.
+	{2, 1}: Flaky,
+	{2, 2}: Fail,
+	{3, 1}: Fail,
+	{3, 2}: Fail,
+	{3, 3}: Fail,
+	{3, 4}: Fail,
+	{4, 2}: Fail,
+	{5, 1}: Fail,
+}
