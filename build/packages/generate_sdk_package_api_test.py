81939d37a641bf8dc16da8fe5039eb588956275c
Andrew Pollack <andrewpollack@google.com>
2023-04-25 22:49:34 +0000

    [swd] `fuchsia_sdk_package()` GN template
    New `fuchsia_sdk_package()` GN template as described in
    https://fuchsia.googlesource.com/fuchsia/+/refs/changes/43/724843/25/docs/contribute/governance/rfcs/NNNN_distributing_packages_with_the_sdk.md#selection
    
    Takes as input a `fuchsia_package()` target to distribute,
    and outputs an `.api` file, capturing the contract of files
    along with their dispositions.
    
    If no golden api file is passed, or mismatch between the golden
    and generated api file, it will print a command to allow users
    to copy the generated api file into the golden location.
    
    Fixed: 120030
    Test: `fx test build/packages`
    Change-Id: Ie937c0813436a24822328b9c3383b09eb883ac98
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/841298
    Commit-Queue: Andrew Pollack <andrewpollack@google.com>
    Reviewed-by: David Turner <digit@google.com>

---
+++ build/packages/generate_sdk_package_api_test.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3.8
+# Copyright 2023 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+import tempfile
+import generate_sdk_package_api
+import os
+import sys
+import json
+from parameterized import parameterized, param
+
+
+class ConvertTest(unittest.TestCase):
+
+    @parameterized.expand(
+        [
+            param(
+                exit_code=0,
+                manifest={
+                    "version":
+                        "1",
+                    "repository":
+                        "fuchsia.com",
+                    "package": {
+                        "name": "foo",
+                        "version": "0"
+                    },
+                    "blobs":
+                        [
+                            {
+                                "source_path": "path/to/meta.far",
+                                "path": "meta/",
+                                "merkle": "000",
+                                "size": 1
+                            },
+                            {
+                                "source_path": "my/bar",
+                                "path": "bin/bar",
+                                "merkle": "111",
+                                "size": 1
+                            },
+                            {
+                                "source_path": "my/baz",
+                                "path": "bin/baz",
+                                "merkle": "222",
+                                "size": 1
+                            },
+                        ],
+                },
+                expected_files_exact=["bin/bar"],
+                expected_files_internal=[],
+                reference={
+                    "version": "1",
+                    "content":
+                        {
+                            "files":
+                                {
+                                    "meta/": {
+                                        "internal": True,
+                                    },
+                                    "bin/bar": {
+                                        "hash": "111",
+                                    },
+                                    "bin/baz": {
+                                        "internal": True,
+                                    }
+                                }
+                        }
+                },
+                warn=False,
+            ),
+            param(
+                exit_code=1,
+                manifest={
+                    "version":
+                        "1",
+                    "repository":
+                        "fuchsia.com",
+                    "package": {
+                        "name": "foo",
+                        "version": "0"
+                    },
+                    "blobs":
+                        [
+                            {
+                                "source_path": "path/to/meta.far",
+                                "path": "meta/",
+                                "merkle": "000",
+                                "size": 1
+                            },
+                            {
+                                "source_path": "my/bar",
+                                "path": "bin/bar",
+                                "merkle": "222",
+                                "size": 1
+                            },
+                        ],
+                },
+                expected_files_exact=["bin/bar"],
+                expected_files_internal=[],
+                reference={
+                    "version": "1",
+                    "content":
+                        {
+                            "files":
+                                {
+                                    "meta/": {
+                                        "internal": True
+                                    },
+                                    "bin/bar": {
+                                        "hash": "111",
+                                    },
+                                }
+                        }
+                },
+                warn=False,
+            )
+        ])
+    def test_run_main(
+            self, exit_code, manifest, expected_files_exact,
+            expected_files_internal, reference, warn):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            package_manifest_path = os.path.join(
+                tmpdir, "package-manifest.json")
+            with open(package_manifest_path, "w") as file:
+                file.write(json.dumps(manifest, indent=2))
+
+            reference_path = os.path.join(tmpdir, "package-golden.api")
+            with open(reference_path, "w") as file:
+                file.write(json.dumps(reference, indent=2))
+
+            output_path = os.path.join(tmpdir, "package.api")
+            sys.argv = [
+                "", "--manifest", package_manifest_path, "--output",
+                output_path, "--reference", reference_path
+            ]
+
+            for expected_file in expected_files_exact:
+                sys.argv += ["--expected-files-exact", expected_file]
+            for expected_file in expected_files_internal:
+                sys.argv += ["--expected-files-internal", expected_file]
+
+            if warn:
+                sys.argv += ["--warn"]
+
+            result = generate_sdk_package_api.main()
+            self.assertEqual(exit_code, result)
