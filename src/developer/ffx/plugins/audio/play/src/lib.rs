0dafb4986a00e7cdc9c6176b99a59a2af2938e6c
camlloyd <camlloyd@google.com>
2023-04-25 20:59:53 +0000

    [ffx][audio] Migrate play and record plugins to SubTools
    Change-Id: I3cbf44dce90f91340b4dfff1e2c33e78d3505237
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/838236
    Reviewed-by: Megan Batty <mgnb@google.com>
    Commit-Queue: Cam Lloyd <camlloyd@google.com>

--- src/developer/ffx/plugins/audio/play/src/lib.rs
+++ src/developer/ffx/plugins/audio/play/src/lib.rs
@@ -4,53 +4,61 @@
 
 use {
     anyhow::Result,
+    async_trait::async_trait,
     ffx_audio_play_args::{
         AudioRenderUsageExtended::{
             Background, Communication, Interruption, Media, SystemAgent, Ultrasound,
         },
         PlayCommand,
     },
-    ffx_core::ffx_plugin,
+    fho::{selector, FfxMain, FfxTool, SimpleWriter},
     fidl_fuchsia_audio_ffxdaemon::{AudioDaemonPlayRequest, AudioDaemonProxy},
 };
 
-#[ffx_plugin(
-    "audio",
-    AudioDaemonProxy = "core/audio_ffx_daemon:expose:fuchsia.audio.ffxdaemon.AudioDaemon"
-)]
-pub async fn play_cmd(audio_proxy: AudioDaemonProxy, cmd: PlayCommand) -> Result<()> {
-    let (play_remote, play_local) = fidl::Socket::create_datagram();
+#[derive(FfxTool)]
+pub struct PlayTool {
+    #[command]
+    cmd: PlayCommand,
+    #[with(selector("core/audio_ffx_daemon:expose:fuchsia.audio.ffxdaemon.AudioDaemon"))]
+    audio_proxy: AudioDaemonProxy,
+}
 
-    let renderer = match cmd.usage {
-        Ultrasound => fidl_fuchsia_audio_ffxdaemon::RendererType::UltrasoundRenderer(
-            fidl_fuchsia_audio_ffxdaemon::UltrasoundRenderer {
-                packet_count: cmd.packet_count,
-                ..fidl_fuchsia_audio_ffxdaemon::UltrasoundRenderer::EMPTY
-            },
-        ),
+fho::embedded_plugin!(PlayTool);
+#[async_trait(?Send)]
+impl FfxMain for PlayTool {
+    type Writer = SimpleWriter;
+    async fn main(self, _writer: Self::Writer) -> fho::Result<()> {
+        let (play_remote, play_local) = fidl::Socket::create_datagram();
 
-        Background(usage) | Media(usage) | SystemAgent(usage) | Communication(usage)
-        | Interruption(usage) => fidl_fuchsia_audio_ffxdaemon::RendererType::StandardRenderer(
-            fidl_fuchsia_audio_ffxdaemon::RendererConfig {
-                usage: Some(usage),
-                clock: Some(cmd.clock),
-                packet_count: cmd.packet_count,
-                ..fidl_fuchsia_audio_ffxdaemon::RendererConfig::EMPTY
-            },
-        ),
-    };
+        let renderer = match self.cmd.usage {
+            Ultrasound => fidl_fuchsia_audio_ffxdaemon::RendererType::UltrasoundRenderer(
+                fidl_fuchsia_audio_ffxdaemon::UltrasoundRenderer {
+                    packet_count: self.cmd.packet_count,
+                    ..fidl_fuchsia_audio_ffxdaemon::UltrasoundRenderer::EMPTY
+                },
+            ),
 
-    let request = AudioDaemonPlayRequest {
-        socket: Some(play_remote),
-        location: Some(fidl_fuchsia_audio_ffxdaemon::PlayLocation::Renderer(renderer)),
-        gain_settings: Some(fidl_fuchsia_audio_ffxdaemon::GainSettings {
-            mute: Some(cmd.mute),
-            gain: Some(cmd.gain),
-            ..fidl_fuchsia_audio_ffxdaemon::GainSettings::EMPTY
-        }),
-        ..AudioDaemonPlayRequest::EMPTY
-    };
+            Background(usage) | Media(usage) | SystemAgent(usage) | Communication(usage)
+            | Interruption(usage) => fidl_fuchsia_audio_ffxdaemon::RendererType::StandardRenderer(
+                fidl_fuchsia_audio_ffxdaemon::RendererConfig {
+                    usage: Some(usage),
+                    clock: Some(self.cmd.clock),
+                    ..fidl_fuchsia_audio_ffxdaemon::RendererConfig::EMPTY
+                },
+            ),
+        };
 
-    ffx_audio_common::play(request, audio_proxy, play_local).await?;
-    Ok(())
+        let request = AudioDaemonPlayRequest {
+            socket: Some(play_remote),
+            location: Some(fidl_fuchsia_audio_ffxdaemon::PlayLocation::Renderer(renderer)),
+            gain_settings: Some(fidl_fuchsia_audio_ffxdaemon::GainSettings {
+                mute: Some(self.cmd.mute),
+                gain: Some(self.cmd.gain),
+                ..fidl_fuchsia_audio_ffxdaemon::GainSettings::EMPTY
+            }),
+            ..AudioDaemonPlayRequest::EMPTY
+        };
+
+        ffx_audio_common::play(request, self.audio_proxy, play_local).await.map_err(Into::into)
+    }
 }
