910d76cf59bd17f91d4ba4875942f68ec3da5ae6
Adam Barth <abarth@google.com>
2023-04-25 11:47:50 +0000

    [ffx][starnix] Convert to subtool
    Convert `ffx starnix` from a plugin to a subtool. Subtools are the new
    way of writing ffx commands.
    
    Change-Id: Ie854c54622e01a218eac10432021dfd8a6653b09
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/844656
    Fuchsia-Auto-Submit: Adam Barth <abarth@google.com>
    Reviewed-by: Kevin Lindkvist <lindkvist@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/developer/ffx/tools/starnix/src/main.rs
+++ src/developer/ffx/tools/starnix/src/main.rs
@@ -2,7 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use ffx_starnix::StarnixTool;
+use fho::FfxTool;
+
 #[fuchsia_async::run_singlethreaded]
 async fn main() {
-    ffx_starnix_suite::fho_suite_main().await
+    StarnixTool::execute_tool().await
 }
