90b6e529e8f68b2e63698f259c126da6fbd19758
Josh Gargus <jjosh@google.com>
2023-04-25 00:15:37 +0000

    [async] Small doc comment improvement.
    RunLoopUntilIdle() runs tasks on the current thread, not on the other
    existing threads associated with the loop.  Document this more clearly.
    
    Bug: 124887
    Change-Id: I60f075c9a30a078e27864bedc77e943880748adf
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/844296
    Commit-Queue: Josh Gargus <jjosh@google.com>
    Reviewed-by: Hunter Freyer <hjfreyer@google.com>
    Fuchsia-Auto-Submit: Josh Gargus <jjosh@google.com>
    API-Review: Hunter Freyer <hjfreyer@google.com>
    Reviewed-by: Corey Tabaka <eieio@google.com>

--- zircon/system/ulib/async-loop/include/lib/async-loop/cpp/loop.h
+++ zircon/system/ulib/async-loop/include/lib/async-loop/cpp/loop.h
@@ -20,16 +20,17 @@ namespace async {
 class Loop {
  public:
   /// Creates a message loop.
+  ///
   /// All operations on the message loop are thread-safe (except destroy).
   ///
-  /// Note that it's ok to run the loop on a different thread from the one
-  /// upon which it was created.
+  /// Note that it's ok to run the loop on a different thread from the one on which it was created.
   ///
   /// |config| provides configuration for the message loop.  Must not be null.
   ///
-  /// See also |kAsyncLoopConfigNeverAttachToThread|,
-  /// |kAsyncLoopConfigAttachToCurrentThread|, and
-  /// |kAsyncLoopConfigNoAttachToCurrentThread|.
+  /// See also:
+  ///   |kAsyncLoopConfigNeverAttachToThread|
+  ///   |kAsyncLoopConfigAttachToCurrentThread|
+  ///   |kAsyncLoopConfigNoAttachToCurrentThread|
   explicit Loop(const async_loop_config_t* config);
 
   Loop(const Loop&) = delete;
@@ -37,8 +38,7 @@ class Loop {
   Loop& operator=(const Loop&) = delete;
   Loop& operator=(Loop&&) = delete;
 
-  /// Destroys the message loop.
-  /// Implicitly calls |Shutdown()|.
+  /// Destroys the message loop.  Implicitly calls |Shutdown()|.
   ~Loop();
 
   /// Gets the underlying message loop structure.
@@ -47,25 +47,23 @@ class Loop {
   /// Gets the loop's asynchronous dispatch interface.
   async_dispatcher_t* dispatcher() const { return async_loop_get_dispatcher(loop_); }
 
-  /// Shuts down the message loop, notifies handlers which asked to handle shutdown.
-  /// The message loop must not currently be running on any threads other than
-  /// those started by |StartThread()| which this function will join.
+  /// Shuts down the message loop, notifies handlers which asked to handle shutdown. The message
+  /// loop must not currently be running on any threads other than those started by |StartThread()|
+  /// which this function will join.
   ///
-  /// Any tasks still queued when |Shutdown()| is called will be immediately
-  /// completed with the ZX_ERR_CANCELLED status. Callbacks will called on one
-  /// of the loop's worker threads (i.e., a thread created by |StartThread()|)
-  /// if such a thread exists. If no worker threads have been created,
-  /// callbacks will be called by the current thread.
+  /// Any tasks still queued when |Shutdown()| is called will be immediately completed with the
+  /// ZX_ERR_CANCELLED status. Callbacks will called on one of the loop's worker threads (i.e., a
+  /// thread created by |StartThread()|) if such a thread exists. If no worker threads have been
+  /// created, callbacks will be called by the current thread.
   ///
   /// Does nothing if already shutting down.
   void Shutdown();
 
-  /// Runs the message loop on the current thread.
-  /// This function can be called on multiple threads to setup a multi-threaded
-  /// dispatcher.
+  /// Runs the message loop on the current thread. This function can be called on multiple threads
+  /// to setup a multi-threaded dispatcher.
   ///
-  /// Dispatches events until the |deadline| expires or the loop is quitted.
-  /// Use |ZX_TIME_INFINITE| to dispatch events indefinitely.
+  /// Dispatches events until the |deadline| expires or the loop is quitted. Use |ZX_TIME_INFINITE|
+  /// to dispatch events indefinitely.
   ///
   /// If |once| is true, performs a single unit of work then returns.
   ///
@@ -75,19 +73,18 @@ class Loop {
   /// Returns |ZX_ERR_BAD_STATE| if the loop was shut down with |Shutdown()|.
   zx_status_t Run(zx::time deadline = zx::time::infinite(), bool once = false);
 
-  /// Dispatches events until there are none remaining, and then returns
-  /// without waiting. This is useful for unit testing, because the behavior
+  /// Runs the message loop on the current thread. Dispatches events until there are none remaining,
+  /// and then returns without waiting. This is useful for unit testing, because the behavior
   /// doesn't depend on time.
   ///
-  /// WARNING: Calling RunUntilIdle() with a dispatcher that runs on a separate
-  /// thread or a multi-threaded dispatcher (common outside single-threaded
-  /// tests) can have subtle unexpected consequences:
-  ///   1) This may return before newly queued work is completed, because the
-  ///      work may be dispatched to a different thread. The calling thread will
-  ///      not wait on that work to complete, only for it to be removed from the
-  ///      pending work queue (roughly speaking).
-  ///   2) Work that was previously queued, prior to calling RunUntilIdle(), is
-  ///      also not guaranteed to have completed when RunUntilIdle() finishes.
+  /// WARNING: Calling RunUntilIdle() with a dispatcher that runs on a separate thread or a
+  /// multi-threaded dispatcher (common outside single-threaded tests) can have subtle unexpected
+  /// consequences:
+  ///   1) This may return before newly queued work is completed, because the work may be dispatched
+  ///      to a different thread. The calling thread will not wait on that work to complete, only
+  ///      for it to be removed from the pending work queue (roughly speaking).
+  ///   2) Work that was previously queued, prior to calling RunUntilIdle(), is also not guaranteed
+  ///      to have completed when RunUntilIdle() finishes.
   ///
   /// Returns |ZX_OK| if the dispatcher reaches an idle state.
   /// Returns |ZX_ERR_CANCELED| if the loop quitted.
@@ -95,30 +92,30 @@ class Loop {
   zx_status_t RunUntilIdle();
 
   /// Quits the message loop.
-  /// Active invocations of |Run()| and threads started using |StartThread()|
-  /// will eventually terminate upon completion of their current unit of work.
   ///
-  /// Subsequent calls to |Run()| or |StartThread()| will return immediately
-  /// until |ResetQuit()| is called.
+  /// Active invocations of |Run()| and threads started using |StartThread()| will eventually
+  /// terminate upon completion of their current unit of work.
+  ///
+  /// Subsequent calls to |Run()| or |StartThread()| will return immediately until |ResetQuit()| is
+  /// called.
   void Quit();
 
-  /// Resets the quit state of the message loop so that it can be restarted
-  /// using |Run()| or |StartThread()|.
+  /// Resets the quit state of the message loop so that it can be restarted using |Run()| or
+  /// |StartThread()|.
   ///
-  /// This function must only be called when the message loop is not running.
-  /// The caller must ensure all active invocations of |Run()| and threads
-  /// started using |StartThread()| have terminated before resetting the quit state.
+  /// This function must only be called when the message loop is not running. The caller must ensure
+  /// all active invocations of |Run()| and threads started using |StartThread()| have terminated
+  /// before resetting the quit state.
   ///
   /// Returns |ZX_OK| if the loop's state was |ASYNC_LOOP_RUNNABLE| or |ASYNC_LOOP_QUIT|.
-  /// Returns |ZX_ERR_BAD_STATE| if the loop's state was |ASYNC_LOOP_SHUTDOWN| or if
-  /// the message loop is currently active on one or more threads.
+  /// Returns |ZX_ERR_BAD_STATE| if the loop's state was |ASYNC_LOOP_SHUTDOWN| or if the message
+  /// loop is currently active on one or more threads.
   zx_status_t ResetQuit();
 
   /// Returns the current state of the message loop.
   async_loop_state_t GetState() const;
 
-  /// Starts a message loop running on a new thread.
-  /// The thread will run until the loop quits.
+  /// Starts a message loop running on a new thread. The thread will run until the loop quits.
   ///
   /// |name| is the desired name for the new thread, may be NULL.
   /// If |out_thread| is not NULL, it is set to the new thread identifier.
@@ -128,8 +125,7 @@ class Loop {
   /// Returns |ZX_ERR_NO_MEMORY| if allocation or thread creation failed.
   zx_status_t StartThread(const char* name = nullptr, thrd_t* out_thread = nullptr);
 
-  /// Blocks until all dispatch threads started with |StartThread()|
-  /// have terminated.
+  /// Blocks until all dispatch threads started with |StartThread()| have terminated.
   void JoinThreads();
 
  private:
