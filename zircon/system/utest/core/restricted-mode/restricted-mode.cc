780384c15ab8afa960e0f13e4d2d98e27e4a7fd1
Tim Detwiler <tjdetwiler@google.com>
2023-04-26 21:42:22 +0000

    [restricted] Add test for channel-based exceptions.
    This just adds some back-fill test coverage for the channel-based
    exception-delivery mechanism. We also update Bench to include a crude
    performance test:
    
      > restricted call 421 ns per round trip (1136486268 raw ticks)
      > test syscall 71 ns per call (192591108 raw ticks)
      > channel-based exceptions 8597 ns per round trip (231718131 raw ticks)
    
    Change-Id: I98955055a6f1597c7df46b832a1a4281f03e593d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/845839
    Reviewed-by: Nick Maniscalco <maniscalco@google.com>
    Commit-Queue: Tim Detwiler <tjdetwiler@google.com>

--- zircon/system/utest/core/restricted-mode/restricted-mode.cc
+++ zircon/system/utest/core/restricted-mode/restricted-mode.cc
@@ -3,25 +3,47 @@
 // found in the LICENSE file.
 
 #include <lib/fit/defer.h>
+#include <lib/zx/exception.h>
+#include <lib/zx/process.h>
+#include <lib/zx/thread.h>
 #include <lib/zx/vmar.h>
 #include <lib/zx/vmo.h>
 #include <zircon/syscalls-next.h>
+#include <zircon/syscalls/debug.h>
+#include <zircon/syscalls/exception.h>
 #include <zircon/testonly-syscalls.h>
+
+#include <thread>
 
 #include <zxtest/zxtest.h>
 
 #if defined(__x86_64__) || defined(__aarch64__)
 
 extern "C" void vectab();
-extern "C" void bounce();
-extern "C" void bounce_post_syscall();
+extern "C" void syscall_bounce();
+extern "C" void syscall_bounce_post_syscall();
+extern "C" void exception_bounce();
+extern "C" void exception_bounce_exception_address();
+// TODO(https://fxb/121511): We need this to break out of exception_bounce. We
+// should be able to remove this once kick is implemented.
+extern "C" void exception_bounce_exit();
 extern "C" zx_status_t restricted_enter_wrapper(uint32_t options, uintptr_t vector_table,
                                                 zx_restricted_reason_t* reason_code);
+
+// The normal-mode view of restricted-mode state will change slightly depending on
+// if the exit to normal-mode was caused by a syscall or an exception.
+enum class RegisterMutation {
+  kFromSyscall,
+  kFromException,
+};
 
 #if defined(__x86_64__)
 
 class ArchRegisterState {
  public:
+  static void WritePcToGeneralRegs(zx_thread_state_general_regs_t& regs, uint64_t rip) {
+    regs.rip = rip;
+  }
   void InitializeRegisters() {
     state_.flags = 0;
     state_.rax = 0x0101010101010101;
@@ -46,13 +68,40 @@ class ArchRegisterState {
     gs_val_ = 0;
   }
 
-  void VerifyTwiddledRestrictedState() const {
+  void InitializeFromThreadState(const zx_thread_state_general_regs_t& regs) {
+    state_.flags = regs.rflags;
+    state_.rax = regs.rax;
+    state_.rbx = regs.rbx;
+    state_.rcx = regs.rcx;
+    state_.rdx = regs.rdx;
+    state_.rsi = regs.rsi;
+    state_.rdi = regs.rdi;
+    state_.rbp = regs.rbp;
+    state_.rsp = regs.rsp;
+    state_.r8 = regs.r8;
+    state_.r9 = regs.r9;
+    state_.r10 = regs.r10;
+    state_.r11 = regs.r11;
+    state_.r12 = regs.r12;
+    state_.r13 = regs.r13;
+    state_.r14 = regs.r14;
+    state_.r15 = regs.r15;
+    state_.fs_base = regs.fs_base;
+    state_.gs_base = regs.gs_base;
+    state_.ip = regs.rip;
+  }
+
+  void VerifyTwiddledRestrictedState(RegisterMutation mutation) const {
     // Validate the state of the registers is what was written inside restricted mode.
     //
     // NOTE: Each of the registers was incremented by one before exiting restricted mode.
     EXPECT_EQ(0x0101010101010102, state_.rax);
     EXPECT_EQ(0x0202020202020203, state_.rbx);
-    EXPECT_EQ(0, state_.rcx);  // RCX is trashed by the syscall and set to zero
+    if (mutation == RegisterMutation::kFromSyscall) {
+      EXPECT_EQ(0, state_.rcx);  // RCX is trashed by the syscall and set to zero
+    } else {
+      EXPECT_EQ(0x0303030303030304, state_.rcx);
+    }
     EXPECT_EQ(0x0404040404040405, state_.rdx);
     EXPECT_EQ(0x0505050505050506, state_.rsi);
     EXPECT_EQ(0x0606060606060607, state_.rdi);
@@ -61,7 +110,11 @@ class ArchRegisterState {
     EXPECT_EQ(0x090909090909090a, state_.r8);
     EXPECT_EQ(0x0a0a0a0a0a0a0a0b, state_.r9);
     EXPECT_EQ(0x0b0b0b0b0b0b0b0c, state_.r10);
-    EXPECT_EQ(0, state_.r11);  // r11 is trashed by the syscall and set to zero
+    if (mutation == RegisterMutation::kFromSyscall) {
+      EXPECT_EQ(0, state_.r11);  // r11 is trashed by the syscall and set to zero
+    } else {
+      EXPECT_EQ(0x0c0c0c0c0c0c0c0d, state_.r11);
+    }
     EXPECT_EQ(0x0d0d0d0d0d0d0d0e, state_.r12);
     EXPECT_EQ(0x0e0e0e0e0e0e0e0f, state_.r13);
     EXPECT_EQ(0x0f0f0f0f0f0f0f10, state_.r14);
@@ -110,8 +163,8 @@ vectab:
 )");
 
 asm(R"(
-.globl bounce
-bounce:
+.globl syscall_bounce
+syscall_bounce:
   // do something to all the registers so we can read
   // the state on the way out
   inc  %rax
@@ -139,9 +192,46 @@ bounce:
 
 0:
   syscall
-.globl bounce_post_syscall
-bounce_post_syscall:
+.globl syscall_bounce_post_syscall
+syscall_bounce_post_syscall:
   jmp 0b
+)");
+
+asm(R"(
+.globl exception_bounce
+exception_bounce:
+  // do something to all the registers so we can read
+  // the state on the way out
+  inc  %rax
+  inc  %rbx
+  inc  %rcx
+  inc  %rdx
+  inc  %rsi
+  inc  %rdi
+  inc  %rbp
+  inc  %rsp
+  inc  %r8
+  inc  %r9
+  inc  %r10
+  inc  %r11
+  inc  %r12
+  inc  %r13
+  inc  %r14
+  inc  %r15
+
+  // write rcx and r11 to fs and gs base since they are both
+  // trashed by the syscall. also tests that fs and gs base are
+  // set properly.
+  mov   %rcx, %fs:0
+  mov   %r11, %gs:0
+
+.globl exception_bounce_exception_address
+exception_bounce_exception_address:
+  ud2
+  jmp exception_bounce_exception_address
+.globl exception_bounce_exit
+exception_bounce_exit:
+  syscall
 )");
 
 asm(R"(
@@ -177,6 +267,10 @@ restricted_enter_wrapper:
 
 class ArchRegisterState {
  public:
+  static void WritePcToGeneralRegs(zx_thread_state_general_regs_t& regs, uint64_t pc) {
+    regs.pc = pc;
+  }
+
   void InitializeRegisters() {
     // Initialize all standard registers to arbitrary values.
     state_.x[0] = 0x0101010101010101;
@@ -217,11 +311,21 @@ class ArchRegisterState {
     state_.tpidr_el0 = reinterpret_cast<uintptr_t>(&tls_val_);
   }
 
-  void VerifyTwiddledRestrictedState() const {
+  void InitializeFromThreadState(const zx_thread_state_general_regs_t& regs) {
+    static_assert(sizeof(regs.r) <= sizeof(state_.x));
+    memcpy(state_.x, regs.r, sizeof(regs.r));
+    state_.x[30] = regs.lr;
+    state_.pc = regs.pc;
+    state_.tpidr_el0 = regs.tpidr;
+    state_.sp = regs.sp;
+    state_.cpsr = static_cast<uint32_t>(regs.cpsr);
+  }
+
+  void VerifyTwiddledRestrictedState(RegisterMutation mutation) const {
     // Validate the state of the registers is what was written inside restricted mode.
     //
     // NOTE: Each of the registers was incremented by one before exiting restricted mode.
-    // x0 was used as temp space by bounce, so skip that one.
+    // x0 was used as temp space by syscall_bounce, so skip that one.
     EXPECT_EQ(0x0202020202020203, state_.x[1]);
     EXPECT_EQ(0x0303030303030304, state_.x[2]);
     EXPECT_EQ(0x0404040404040405, state_.x[3]);
@@ -237,7 +341,11 @@ class ArchRegisterState {
     EXPECT_EQ(0x0e0e0e0e0e0e0e0f, state_.x[13]);
     EXPECT_EQ(0x0f0f0f0f0f0f0f10, state_.x[14]);
     EXPECT_EQ(0x0101010101010102, state_.x[15]);
-    EXPECT_EQ(0x40, state_.x[16]);  // bounce ran syscall 0x40
+    if (mutation == RegisterMutation::kFromSyscall) {
+      EXPECT_EQ(0x40, state_.x[16]);  // syscall_bounce ran syscall 0x40
+    } else {
+      EXPECT_EQ(0x0202020202020203, state_.x[16]);
+    }
     EXPECT_EQ(0x0303030303030304, state_.x[17]);
     EXPECT_EQ(0x0404040404040405, state_.x[18]);
     EXPECT_EQ(0x0505050505050506, state_.x[19]);
@@ -299,8 +407,8 @@ vectab:
 )");
 
 asm(R"(
-.globl bounce
-bounce:
+.globl syscall_bounce
+syscall_bounce:
   // do something to all the registers so we can read
   // the state on the way out
   add x0, x0, #1
@@ -343,9 +451,63 @@ bounce:
   mov x16, xzr
   add x16, x16, #64
   svc #0
-.globl bounce_post_syscall
-bounce_post_syscall:
-  bl bounce
+.globl syscall_bounce_post_syscall
+syscall_bounce_post_syscall:
+  bl syscall_bounce
+)");
+
+asm(R"(
+.globl exception_bounce
+exception_bounce:
+  // do something to all the registers so we can read
+  // the state on the way out
+  add x0, x0, #1
+  add x1, x1, #1
+  add x2, x2, #1
+  add x3, x3, #1
+  add x4, x4, #1
+  add x5, x5, #1
+  add x6, x6, #1
+  add x7, x7, #1
+  add x8, x8, #1
+  add x9, x9, #1
+  add x10, x10, #1
+  add x11, x11, #1
+  add x12, x12, #1
+  add x13, x13, #1
+  add x14, x14, #1
+  add x15, x15, #1
+  add x16, x16, #1
+  add x17, x17, #1
+  add x18, x18, #1
+  add x19, x19, #1
+  add x20, x20, #1
+  add x21, x21, #1
+  add x22, x22, #1
+  add x23, x23, #1
+  add x24, x24, #1
+  add x25, x25, #1
+  add x26, x26, #1
+  add x27, x27, #1
+  add x28, x28, #1
+  add x29, x29, #1
+  add x30, x30, #1
+  add sp, sp, #1
+
+  // Save the contents of x16 to TLS prior to running a syscall.
+  mrs x0, tpidr_el0
+  str x16, [x0]
+.globl exception_bounce_exception_address
+exception_bounce_exception_address:
+  udf #0
+  b exception_bounce_exception_address
+.globl exception_bounce_exit
+exception_bounce_exit:
+  // Perform a syscall to exit. This is needed if we're using channel-based
+  // exception handlers.
+  mov x16, xzr
+  add x16, x16, #64
+  svc #0
 )");
 
 asm(R"(
@@ -463,9 +625,9 @@ TEST(RestrictedMode, Basic) {
   ArchRegisterState state;
   state.InitializeRegisters();
 
-  // Set the PC to the bounce routine, as the PC is where zx_restricted_enter
+  // Set the PC to the syscall_bounce routine, as the PC is where zx_restricted_enter
   // will jump to.
-  state.set_ip(reinterpret_cast<uint64_t>(bounce));
+  state.set_ip(reinterpret_cast<uint64_t>(syscall_bounce));
 
   // Write the state to the state VMO.
   ASSERT_OK(vmo.write(&state.restricted_state(), 0, sizeof(state.restricted_state())));
@@ -480,8 +642,45 @@ TEST(RestrictedMode, Basic) {
   ASSERT_OK(vmo.read(&state.restricted_state(), 0, sizeof(state.restricted_state())));
 
   // Validate that the instruction pointer is right after the syscall instruction.
-  EXPECT_EQ((uintptr_t)&bounce_post_syscall, state.ip());
-  state.VerifyTwiddledRestrictedState();
+  EXPECT_EQ((uintptr_t)&syscall_bounce_post_syscall, state.ip());
+  state.VerifyTwiddledRestrictedState(RegisterMutation::kFromSyscall);
+}
+
+void ReadExceptionFromChannel(const zx::channel& exception_channel,
+                              zx_thread_state_general_regs_t& general_regs,
+                              std::optional<uintptr_t> new_pc) {
+  zx_signals_t pending;
+  ASSERT_OK(exception_channel.wait_one(ZX_CHANNEL_READABLE | ZX_CHANNEL_PEER_CLOSED,
+                                       zx::time::infinite(), &pending));
+  ASSERT_NE(0, pending & ZX_CHANNEL_READABLE, "exception channel peer closed (pending 0x%08x)",
+            pending);
+
+  // This test will cover the 'worst case' exception latency, so we read all the data that we
+  // can also access when using in-thread exception handlers. In other words we read all the
+  // data that we would otherwise get from the restricted mode state VMO when using in-thread
+  // exception handling.
+  zx::exception exception;
+  zx_exception_info_t exc_info;
+  uint32_t nbytes, nhandles;
+  ASSERT_OK(exception_channel.read(0, &exc_info, exception.reset_and_get_address(),
+                                   sizeof(exc_info), 1, &nbytes, &nhandles));
+
+  zx::thread thread;
+  ASSERT_OK(exception.get_thread(&thread));
+
+  zx_exception_report_t report = {};
+  ASSERT_OK(
+      thread.get_info(ZX_INFO_THREAD_EXCEPTION_REPORT, &report, sizeof(report), nullptr, nullptr));
+  ASSERT_OK(thread.read_state(ZX_THREAD_STATE_GENERAL_REGS, &general_regs, sizeof(general_regs)));
+
+  // Update the PC if necessary.
+  if (new_pc.has_value()) {
+    ArchRegisterState::WritePcToGeneralRegs(general_regs, *new_pc);
+  }
+  ASSERT_OK(thread.write_state(ZX_THREAD_STATE_GENERAL_REGS, &general_regs, sizeof(general_regs)));
+  constexpr uint32_t kExceptionState = ZX_EXCEPTION_STATE_HANDLED;
+  ASSERT_OK(
+      exception.set_property(ZX_PROP_EXCEPTION_STATE, &kExceptionState, sizeof(kExceptionState)));
 }
 
 // This is a simple benchmark test that prints some rough performance numbers.
@@ -495,32 +694,107 @@ TEST(RestrictedMode, Bench) {
     // Set the state.
     ArchRegisterState state;
     state.InitializeRegisters();
-    state.set_ip(reinterpret_cast<uintptr_t>(bounce));
+    state.set_ip(reinterpret_cast<uintptr_t>(syscall_bounce));
     ASSERT_OK(vmo.write(&state.restricted_state(), 0, sizeof(state.restricted_state())));
 
     // Go through a full restricted syscall entry/exit cycle iter times and show the time.
-    constexpr int iter = 1000000;  // about a second worth of iterations on a mid range x86
-    zx_restricted_reason_t reason_code;
-    auto t = zx::ticks::now();
-    for (int i = 0; i < iter; i++) {
+    {
+      constexpr int iter = 1000000;  // about a second worth of iterations on a mid range x86
+      zx_restricted_reason_t reason_code;
+      auto t = zx::ticks::now();
+      for (int i = 0; i < iter; i++) {
+        ASSERT_OK(restricted_enter_wrapper(ZX_RESTRICTED_OPT_EXCEPTION_CHANNEL, (uintptr_t)vectab,
+                                           &reason_code));
+      }
+      t = zx::ticks::now() - t;
+
+      printf("restricted call %ld ns per round trip (%ld raw ticks)\n",
+             t / iter * ZX_SEC(1) / zx::ticks::per_second(), t.get());
+    }
+
+    {
+      // For way of comparison, time a null syscall.
+      constexpr int iter = 1000000;
+      auto t = zx::ticks::now();
+      for (int i = 0; i < iter; i++) {
+        ASSERT_OK(zx_syscall_test_0());
+      }
+      t = zx::ticks::now() - t;
+
+      printf("test syscall %ld ns per call (%ld raw ticks)\n",
+             t / iter * ZX_SEC(1) / zx::ticks::per_second(), t.get());
+    }
+
+    // Perform channel-based exception handling.
+    {
+      constexpr int iter = 10000;
+      auto t = zx::ticks::now();
+      zx_restricted_reason_t reason_code;
+
+      zx::channel exception_channel;
+      ZX_ASSERT(zx::process::self()->create_exception_channel(0, &exception_channel) == ZX_OK);
+      std::thread exception_handler([&]() {
+        for (int i = 0; i < iter; ++i) {
+          zx_thread_state_general_regs_t general_regs = {};
+          ReadExceptionFromChannel(
+              exception_channel, general_regs,
+              (i == iter - 1)
+                  ? std::make_optional(reinterpret_cast<uintptr_t>(exception_bounce_exit))
+                  : std::nullopt);
+        }
+      });
+      state.set_ip(reinterpret_cast<uintptr_t>(exception_bounce_exception_address));
+      ASSERT_OK(vmo.write(&state.restricted_state(), 0, sizeof(state.restricted_state())));
+      t = zx::ticks::now();
+      // Iteration happens in the handler thread; we only have a single restricted_enter
+      // when using channel-based exception handling.
       ASSERT_OK(restricted_enter_wrapper(ZX_RESTRICTED_OPT_EXCEPTION_CHANNEL, (uintptr_t)vectab,
                                          &reason_code));
+      t = zx::ticks::now() - t;
+      exception_handler.join();
+
+      printf("channel-based exceptions %ld ns per round trip (%ld raw ticks)\n",
+             t / iter * ZX_SEC(1) / zx::ticks::per_second(), t.get());
     }
-    t = zx::ticks::now() - t;
-
-    printf("restricted call %ld ns per round trip (%ld raw ticks)\n",
-           t / iter * ZX_SEC(1) / zx::ticks::per_second(), t.get());
-
-    // For way of comparison, time a null syscall.
-    t = zx::ticks::now();
-    for (int i = 0; i < iter; i++) {
-      ASSERT_OK(zx_syscall_test_0());
-    }
-    t = zx::ticks::now() - t;
-
-    printf("test syscall %ld ns per call (%ld raw ticks)\n",
-           t / iter * ZX_SEC(1) / zx::ticks::per_second(), t.get());
   }
+}
+
+// Verify we can receive restricted exceptions via exception channels.
+TEST(RestrictedMode, ExceptionChannel) {
+  zx::vmo vmo;
+  ASSERT_OK(zx_restricted_bind_state(0, vmo.reset_and_get_address()));
+  auto cleanup = fit::defer([]() { EXPECT_OK(zx_restricted_unbind_state(0)); });
+
+  // Configure the state for x86.
+  ArchRegisterState state;
+  state.InitializeRegisters();
+  state.set_ip(reinterpret_cast<uint64_t>(exception_bounce));
+
+  // Spawn a thread to receive exceptions.
+  zx::channel exception_channel;
+  ZX_ASSERT(zx::process::self()->create_exception_channel(0, &exception_channel) == ZX_OK);
+
+  auto restricted_thread = zx::thread::self();
+
+  bool exception_handled = false;
+  std::thread exception_thread([&]() {
+    zx_thread_state_general_regs_t general_regs = {};
+    ReadExceptionFromChannel(exception_channel, general_regs,
+                             reinterpret_cast<uintptr_t>(exception_bounce_exit));
+
+    state.InitializeFromThreadState(general_regs);
+    state.VerifyTwiddledRestrictedState(RegisterMutation::kFromException);
+    exception_handled = true;
+  });
+
+  // Enter restricted mode. The restricted code will twiddle some registers
+  // and generate an exception.
+  ASSERT_OK(vmo.write(&state.restricted_state(), 0, sizeof(state.restricted_state())));
+  zx_restricted_reason_t reason_code = 99;
+  ASSERT_OK(restricted_enter_wrapper(ZX_RESTRICTED_OPT_EXCEPTION_CHANNEL, (uintptr_t)vectab,
+                                     &reason_code));
+  exception_thread.join();
+  ASSERT_TRUE(exception_handled);
 }
 
 // Verify that restricted_enter fails on invalid zx_restricted_state_t values.
@@ -546,24 +820,24 @@ TEST(RestrictedMode, EnterBadStateStruct) {
 
 #ifdef __x86_64__
   state.InitializeRegisters();
-  state.set_ip((uint64_t)bounce);
+  state.set_ip((uint64_t)syscall_bounce);
   state.restricted_state().flags = (1UL << 31);  // set an invalid flag
   set_state_and_enter();
 
   state.InitializeRegisters();
-  state.set_ip((uint64_t)bounce);
+  state.set_ip((uint64_t)syscall_bounce);
   state.restricted_state().fs_base = (1UL << 63);  // invalid fs (non canonical)
   set_state_and_enter();
 
   state.InitializeRegisters();
-  state.set_ip((uint64_t)bounce);
+  state.set_ip((uint64_t)syscall_bounce);
   state.restricted_state().gs_base = (1UL << 63);  // invalid gs (non canonical)
   set_state_and_enter();
 #endif
 
 #ifdef __aarch64__
   state.InitializeRegisters();
-  state.set_ip((uint64_t)bounce);
+  state.set_ip((uint64_t)syscall_bounce);
   state.restricted_state().cpsr = 0x1;  // CPSR contains non-user settable flags.
   set_state_and_enter();
 #endif
