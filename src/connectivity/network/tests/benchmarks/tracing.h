2fcb695b8eef7bdd8ac12821413858310b0bcab2
Tony Gong <gongt@google.com>
2023-04-26 15:46:20 +0000

    [network] Add socket benchmarks with tracing
    Add variants of the existing socket benchmarks to run against NS2 with
    tracing enabled, and collect traces (on the target side inside the
    test logic itself using the `fuchsia.tracing.controller/Controller`
    FIDL directly) to be included in the test artifacts.
    
    Note that although these tests do not rely on any host-side machinery,
    they are run as a perf test as there is no other way to include the
    trace as a test artifact currently due to builders not using ffx in
    infra.
    
    Test: fx test --e2e netstack_benchmarks_test
    Fixed: 109959
    Change-Id: I00411f263cf6181ccdce98f291b4ef984d231b51
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/815080
    Reviewed-by: Mark Seaborn <mseaborn@google.com>
    Reviewed-by: Peter Johnston <peterjohnston@google.com>
    Commit-Queue: Tony Gong <gongt@google.com>

---
+++ src/connectivity/network/tests/benchmarks/tracing.h
@@ -0,0 +1,23 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_CONNECTIVITY_NETWORK_TESTS_BENCHMARKS_TRACING_H_
+#define SRC_CONNECTIVITY_NETWORK_TESTS_BENCHMARKS_TRACING_H_
+
+#include <fidl/fuchsia.tracing.controller/cpp/fidl.h>
+#include <lib/component/incoming/cpp/protocol.h>
+
+#include <future>
+
+inline constexpr const char* kSocketBenchmarksTracingCategory = "socket_benchmarks";
+
+struct Tracer {
+  fidl::SyncClient<fuchsia_tracing_controller::Controller> controller;
+  std::future<zx_status_t> future;
+};
+
+fit::result<fit::failed, Tracer> StartTracing();
+fit::result<fit::failed> StopTracing(Tracer tracer);
+
+#endif  // SRC_CONNECTIVITY_NETWORK_TESTS_BENCHMARKS_TRACING_H_
