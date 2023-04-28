d337dc294b4d10f3a99569b8e3d09cccba36379a
Martin Lindsay <mlindsay@google.com>
2023-04-28 16:00:28 +0000

    [fxfs] Add seek table to persistent layer file blocks.
    Reserves space for an offset seek table at the end of the block while
    writing it out. Binary searches within the block by only deserializing
    the key and comparing it to further narrow the search space.
    
    Will migrate cleanly.
    
    Bug: 123089
    
    Change-Id: Id5bf684ee67eb6595f25a771ef35a1b90dc29c64
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/830658
    Reviewed-by: Aaron Drew <ripper@google.com>
    Commit-Queue: Martin Lindsay <mlindsay@google.com>

--- src/storage/fxfs/src/lsm_tree/simple_persistent_layer.rs
+++ src/storage/fxfs/src/lsm_tree/simple_persistent_layer.rs
@@ -13,7 +13,9 @@ use {
         },
         object_handle::{ReadObjectHandle, WriteBytes},
         round::{round_down, round_up},
-        serialized_types::{Version, Versioned, VersionedLatest, LATEST_VERSION},
+        serialized_types::{
+            Version, Versioned, VersionedLatest, LATEST_VERSION, PER_BLOCK_SEEK_VERSION,
+        },
     },
     anyhow::{bail, ensure, Context, Error},
     async_trait::async_trait,
@@ -71,6 +73,9 @@ impl std::io::Read for BufferCursor<'_> {
     }
 }
 
+const BLOCK_HEADER_SIZE: usize = 2;
+const BLOCK_SEEK_ENTRY_SIZE: usize = 2;
+
 struct Iterator<'iter, K: Key, V: Value> {
     // Allocated out of |layer|.
     buffer: BufferCursor<'iter>,
@@ -104,6 +109,29 @@ impl<K: Key, V: Value> Iterator<'_, K, V> {
             item_count: 0,
             item: None,
         }
+    }
+
+    // Find the offset in the block for a particular object index. Returns error if the index is
+    // out of range or the resulting offset contains an obviously invalid value.
+    fn offset_for_index<'a>(&'a mut self, index: usize) -> Result<u16, Error> {
+        ensure!(index < usize::from(self.item_count), FxfsError::OutOfRange);
+        // First entry isn't actually recorded, it is at the start of the block.
+        if index == 0 {
+            return Ok(BLOCK_HEADER_SIZE.try_into().unwrap());
+        }
+
+        let old_buffer_pos = self.buffer.pos;
+        self.buffer.pos = self.layer.layer_info.block_size as usize
+            - (BLOCK_SEEK_ENTRY_SIZE * (usize::from(self.item_count) - index));
+        let res = self.buffer.read_u16::<LittleEndian>();
+        self.buffer.pos = old_buffer_pos;
+        let offset = res?;
+        ensure!(
+            u64::from(offset) < self.layer.layer_info.block_size
+                && usize::try_from(offset).unwrap() > BLOCK_HEADER_SIZE,
+            FxfsError::Inconsistent
+        );
+        Ok(offset)
     }
 }
 
@@ -224,6 +252,7 @@ impl<K: Key, V: Value> Layer<K, V> for SimplePersistentLayer {
                 Ordering::Less => {}
             },
         }
+        let mut right = Iterator::new(self, right_offset);
         while right_offset - left_offset > self.layer_info.block_size as u64 {
             // Pick a block midway.
             let mid_offset = round_down(
@@ -234,7 +263,10 @@ impl<K: Key, V: Value> Layer<K, V> for SimplePersistentLayer {
             iterator.advance().await?;
             let item: ItemRef<'_, K, V> = iterator.get().unwrap();
             match item.key.cmp_upper_bound(key) {
-                Ordering::Greater => right_offset = mid_offset,
+                Ordering::Greater => {
+                    right_offset = mid_offset;
+                    right = iterator;
+                }
                 Ordering::Equal => {
                     if excluded {
                         iterator.advance().await?;
@@ -247,22 +279,83 @@ impl<K: Key, V: Value> Layer<K, V> for SimplePersistentLayer {
                 }
             }
         }
-        // At this point, we know that left_key < key and right_key >= key, so we have to iterate
-        // through left_key to find the key we want.
-        loop {
-            left.advance().await?;
-            match left.get() {
-                None => return Ok(Box::new(left)),
-                Some(item) => match item.key.cmp_upper_bound(key) {
-                    Ordering::Greater => return Ok(Box::new(left)),
+
+        if self.layer_info.key_value_version >= PER_BLOCK_SEEK_VERSION {
+            // We have a seek table, use it to binary search.
+            let mut left_index = 0;
+            let mut right_index = left.item_count;
+            // If the size is zero then we don't touch the iterator.
+            while left_index < (right_index - 1) {
+                let mid_index = left_index + ((right_index - left_index) / 2);
+                left.buffer.pos = left.offset_for_index(mid_index.into())? as usize;
+                left.item_index = u16::try_from(mid_index).unwrap();
+                // Only deserialize the key while searching.
+                let current_key = K::deserialize_from_version(
+                    left.buffer.by_ref(),
+                    left.layer.layer_info.key_value_version,
+                )
+                .context("Corrupt layer (key)")?;
+                match current_key.cmp_upper_bound(key) {
+                    Ordering::Greater => {
+                        right_index = mid_index;
+                    }
                     Ordering::Equal => {
+                        // Already deserialized the key, get the rest of the item.
+                        let current_value = V::deserialize_from_version(
+                            left.buffer.by_ref(),
+                            left.layer.layer_info.key_value_version,
+                        )
+                        .context("Corrupt layer (value)")?;
+                        let current_sequence = left
+                            .buffer
+                            .read_u64::<LittleEndian>()
+                            .context("Corrupt layer (seq)")?;
+                        left.item_index += 1;
                         if excluded {
                             left.advance().await?;
+                        } else {
+                            left.item = Some(Item {
+                                key: current_key,
+                                value: current_value,
+                                sequence: current_sequence,
+                            });
                         }
                         return Ok(Box::new(left));
                     }
-                    Ordering::Less => {}
-                },
+                    Ordering::Less => {
+                        left_index = mid_index;
+                    }
+                }
+            }
+            // When we don't find an exact match, we need to return with the first entry *after* the
+            // the target key. Which might be the first one in the next block, currently already
+            // pointed to by the "right" buffer, but usually it's just the result of the right index
+            // within the "left" buffer.
+            if right_index < left.item_count {
+                right = left;
+                right.buffer.pos = right.offset_for_index(right_index.into())? as usize;
+                right.item_index = u16::try_from(right_index).unwrap();
+                right.advance().await?;
+            };
+            return Ok(Box::new(right));
+        } else {
+            // At this point, we know that left_key < key and right_key >= key, so we have to
+            // iterate through left_key to find the key we want.
+            loop {
+                left.advance().await?;
+                match left.get() {
+                    None => return Ok(Box::new(left)),
+                    Some(item) => match item.key.cmp_upper_bound(key) {
+                        Ordering::Greater => return Ok(Box::new(left)),
+                        Ordering::Equal => {
+                            if excluded {
+                                left.advance().await?;
+                            }
+                            return Ok(Box::new(left));
+                        }
+                        Ordering::Less => {}
+                    },
+                }
             }
         }
     }
@@ -292,6 +385,7 @@ pub struct SimplePersistentLayerWriter<W: WriteBytes, K: Key, V: Value> {
     block_size: u64,
     buf: Vec<u8>,
     item_count: u16,
+    block_offsets: Vec<u16>,
     _key: PhantomData<K>,
     _value: PhantomData<V>,
 }
@@ -302,21 +396,21 @@ impl<W: WriteBytes, K: Key, V: Value> SimplePersistentLayerWriter<W, K, V> {
     pub async fn new(mut writer: W, block_size: u64) -> Result<Self, Error> {
         ensure!(block_size <= MAX_BLOCK_SIZE, FxfsError::NotSupported);
         let layer_info = LayerInfo { block_size, key_value_version: LATEST_VERSION };
-        let mut buf: Vec<u8> = Vec::new();
+        let mut buf: Vec<u8> = vec![0u8; block_size as usize];
         let len;
         {
-            buf.resize(layer_info.block_size as usize, 0);
             let mut cursor = std::io::Cursor::new(&mut buf);
             layer_info.serialize_with_version(&mut cursor)?;
             len = cursor.position();
         }
         writer.write_bytes(&buf[..len as usize]).await?;
-        writer.skip(block_size as u64 - len as u64).await?;
+        writer.skip(block_size - len).await?;
         Ok(SimplePersistentLayerWriter {
             writer,
             block_size,
-            buf: vec![0; 2],
+            buf: vec![0; BLOCK_HEADER_SIZE],
             item_count: 0,
+            block_offsets: Vec::new(),
             _key: PhantomData,
             _value: PhantomData,
         })
@@ -326,12 +420,32 @@ impl<W: WriteBytes, K: Key, V: Value> SimplePersistentLayerWriter<W, K, V> {
         if self.item_count == 0 {
             return Ok(());
         }
-        LittleEndian::write_u16(&mut self.buf[0..2], self.item_count);
+        LittleEndian::write_u16(&mut self.buf[0..BLOCK_HEADER_SIZE], self.item_count);
         self.writer.write_bytes(&self.buf[..len]).await?;
-        self.writer.skip(self.block_size as u64 - len as u64).await?;
+        // Leave a gap and write the seek table to the end.
+        self.writer
+            .skip(
+                self.block_size
+                    - len as u64
+                    - (self.block_offsets.len() * BLOCK_SEEK_ENTRY_SIZE) as u64,
+            )
+            .await?;
+        // Write the seek table overwriting the buffer. It must be smaller, entries are 2 bytes each
+        // and items are always at least 10,
+        let mut pointer = 0;
+        for offset in &self.block_offsets {
+            LittleEndian::write_u16(
+                &mut self.buf[pointer..pointer + BLOCK_SEEK_ENTRY_SIZE],
+                *offset,
+            );
+            pointer += BLOCK_SEEK_ENTRY_SIZE;
+        }
+        self.writer.write_bytes(&self.buf[..pointer]).await?;
         debug!(item_count = self.item_count, byte_count = len, "wrote items");
-        self.buf.drain(..len - 2); // 2 bytes are used for the next item count.
+        // Leave space to prepend the next header.
+        self.buf.drain(..len - BLOCK_HEADER_SIZE);
         self.item_count = 0;
+        self.block_offsets.clear();
         Ok(())
     }
 }
@@ -346,10 +460,23 @@ impl<W: WriteBytes + Send, K: Key, V: Value> LayerWriter<K, V>
         item.key.serialize_into(&mut self.buf)?;
         item.value.serialize_into(&mut self.buf)?;
         self.buf.write_u64::<LittleEndian>(item.sequence)?;
+        let mut added_offset = false;
+        // Never record the first item. The offset is always the same.
+        if self.item_count > 0 {
+            self.block_offsets.push(u16::try_from(len).unwrap());
+            added_offset = true;
+        }
 
         // If writing the item took us over a block, flush the bytes in the buffer prior to this
         // item.
-        if self.buf.len() > self.block_size as usize - 1 {
+        if self.buf.len() + (self.block_offsets.len() * BLOCK_SEEK_ENTRY_SIZE)
+            > self.block_size as usize - 1
+        {
+            if added_offset {
+                // Drop the recently added offset from the list. The latest item will be the first
+                // on the next block and have a known offset there.
+                self.block_offsets.pop();
+            }
             self.write_some(len).await?;
         }
 
@@ -373,12 +500,14 @@ impl<W: WriteBytes, K: Key, V: Value> Drop for SimplePersistentLayerWriter<W, K,
 
 #[cfg(test)]
 mod tests {
+
     use {
         super::{SimplePersistentLayer, SimplePersistentLayerWriter},
         crate::{
             filesystem::MAX_BLOCK_SIZE,
             lsm_tree::types::{DefaultOrdUpperBound, Item, ItemRef, Layer, LayerWriter},
             object_handle::Writer,
+            round::round_up,
             testing::fake_object::{FakeObject, FakeObjectHandle},
         },
         std::{fmt::Debug, ops::Bound, sync::Arc},
@@ -426,7 +555,7 @@ mod tests {
     #[fuchsia::test]
     async fn test_seek_after_write() {
         const BLOCK_SIZE: u64 = 512;
-        const ITEM_COUNT: i32 = 10000;
+        const ITEM_COUNT: i32 = 5000;
 
         let handle = FakeObjectHandle::new(Arc::new(FakeObject::new()));
         {
@@ -437,24 +566,38 @@ mod tests {
             .await
             .expect("writer new");
             for i in 0..ITEM_COUNT {
-                writer.write(Item::new(i, i).as_item_ref()).await.expect("write failed");
+                // Populate every other value as an item.
+                writer.write(Item::new(i * 2, i * 2).as_item_ref()).await.expect("write failed");
             }
             writer.flush().await.expect("flush failed");
         }
         let layer = SimplePersistentLayer::open(handle).await.expect("new failed");
-        for i in 0..ITEM_COUNT {
+        // Search for all values to check the in-between values.
+        for i in 0..ITEM_COUNT * 2 {
+            // We've written every other value, we expect to get either the exact value searched
+            // for, or the next one after it. So round up to the nearest multiple of 2.
+            let expected = round_up(i, 2).unwrap();
             let mut iterator = layer.seek(Bound::Included(&i)).await.expect("failed to seek");
-            let ItemRef { key, value, .. } = iterator.get().expect("missing item");
-            assert_eq!((key, value), (&i, &i));
-
-            // Check that we can advance to the next item.
-            iterator.advance().await.expect("failed to advance");
-            if i == ITEM_COUNT - 1 {
+            // We've written values up to (N-1)*2=2*N-2, so when looking for 2*N-1 we'll go off the
+            // end of the layer and get back no item.
+            if i >= (ITEM_COUNT * 2) - 1 {
                 assert!(iterator.get().is_none());
             } else {
                 let ItemRef { key, value, .. } = iterator.get().expect("missing item");
-                let j = i + 1;
-                assert_eq!((key, value), (&j, &j));
+                assert_eq!((key, value), (&expected, &expected));
+            }
+
+            // Check that we can advance to the next item.
+            iterator.advance().await.expect("failed to advance");
+            // The highest value is 2*N-2, searching for 2*N-3 will find the last value, and
+            // advancing will go off the end of the layer and return no item. If there was
+            // previously no item, then it will latch and always return no item.
+            if i >= (ITEM_COUNT * 2) - 3 {
+                assert!(iterator.get().is_none());
+            } else {
+                let ItemRef { key, value, .. } = iterator.get().expect("missing item");
+                let next = expected + 2;
+                assert_eq!((key, value), (&next, &next));
             }
         }
     }
