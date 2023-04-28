70461f23605409560bd73e0777ef264fcb202984
Vickie Cheng <vickiecheng@google.com>
2023-04-28 18:56:14 +0000

    [starnix] Only run reported gVisor benchmarks
    The starnix test runner filters gbenchmark results down to the first
    five variants of each metric. This adds filters so we don't run those
    benchmarks at all.
    
    Change-Id: Ice96e86a43519b6e4c6ce1f900067e8f53475c41
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/835856
    Reviewed-by: Mark Seaborn <mseaborn@google.com>
    Fuchsia-Auto-Submit: Vickie Cheng <vickiecheng@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/tests/end_to_end/perf/test/starnix_gvisor_benchmarks_test.dart
+++ src/tests/end_to_end/perf/test/starnix_gvisor_benchmarks_test.dart
@@ -20,39 +20,18 @@ void main(List<String> args) {
   enableLoggingOutput();
 
   const benchmarks = {
-    'clock_getres_benchmark.cm':
-        'fuchsia.starnix.gvisor_benchmarks.clock_getres.txt',
-    'clock_gettime_benchmark.cm':
-        'fuchsia.starnix.gvisor_benchmarks.clock_gettime.txt',
-    // 'death_benchmark' - not passing
-    'dup_benchmark.cm': 'fuchsia.starnix.gvisor_benchmarks.dup.txt',
-    'epoll_benchmark.cm': 'fuchsia.starnix.gvisor_benchmarks.epoll.txt',
-    'fork_benchmark.cm': 'fuchsia.starnix.gvisor_benchmarks.fork.txt',
-    'futex_benchmark.cm': 'fuchsia.starnix.gvisor_benchmarks.futex.txt',
-    'getdents_benchmark.cm': 'fuchsia.starnix.gvisor_benchmarks.getdents.txt',
-    'getpid_benchmark.cm': 'fuchsia.starnix.gvisor_benchmarks.getpid.txt',
-    'gettid_benchmark.cm': 'fuchsia.starnix.gvisor_benchmarks.gettid.txt',
-    'mapping_benchmark.cm': 'fuchsia.starnix.gvisor_benchmarks.mapping.txt',
-    'open_benchmark.cm': 'fuchsia.starnix.gvisor_benchmarks.open.txt',
-    'open_read_close_benchmark.cm':
-        'fuchsia.starnix.gvisor_benchmarks.open_read_close.txt',
-    'pipe_benchmark.cm': 'fuchsia.starnix.gvisor_benchmarks.pipe.txt',
-    'randread_benchmark.cm': 'fuchsia.starnix.gvisor_benchmarks.randread.txt',
-    'read_benchmark.cm': 'fuchsia.starnix.gvisor_benchmarks.read.txt',
-    'sched_yield_benchmark.cm':
-        'fuchsia.starnix.gvisor_benchmarks.sched_yield.txt',
-    'send_recv_benchmark.cm': 'fuchsia.starnix.gvisor_benchmarks.send_recv.txt',
-    'seqwrite_benchmark.cm': 'fuchsia.starnix.gvisor_benchmarks.seqwrite.txt',
-    'signal_benchmark.cm': 'fuchsia.starnix.gvisor_benchmarks.signal.txt',
-    'sleep_benchmark.cm': 'fuchsia.starnix.gvisor_benchmarks.sleep.txt',
-    'stat_benchmark.cm': 'fuchsia.starnix.gvisor_benchmarks.stat.txt',
-    'unlink_benchmark.cm': 'fuchsia.starnix.gvisor_benchmarks.unlink.txt',
-    'write_benchmark.cm': 'fuchsia.starnix.gvisor_benchmarks.write.txt',
-  };
-
-  const filters = {
-    // TODO(b/275745984): Remove when BM_ThreadSwitch passes.
-    'fork_benchmark.cm': '--benchmark_filter_internal="'
+    'clock_getres': '--benchmark_filter_internal=all',
+    'clock_gettime': '--benchmark_filter_internal=all',
+    // 'death': '--benchmark_filter_internal=all', not passing
+    'epoll': '--benchmark_filter_internal=all',
+    'getdents': '--benchmark_filter_internal=all',
+    'getpid': '--benchmark_filter_internal=all',
+    'open': '--benchmark_filter_internal=all',
+    'open_read_close': '--benchmark_filter_internal=all',
+    'signal': '--benchmark_filter_internal=all',
+    'stat': '--benchmark_filter_internal=all',
+    // TODO(b/275745984): Update to all when BM_ThreadSwitch passes.
+    'fork': '--benchmark_filter_internal="'
         'BM_CPUBoundSymmetric'
         '|BM_CPUBoundUniprocess'
         '|BM_CPUBoundAsymmetric'
@@ -61,14 +40,20 @@ void main(List<String> args) {
         '|BM_ProcessLifecycle"',
     // TODO(b/275745984): Add BM_RecvmsgWithControlBuf and
     // variants of BM_SendmsgTCP when they pass.
-    'send_recv_benchmark.cm': '--benchmark_filter_internal="'
+    'send_recv': '--benchmark_filter_internal="'
         'BM_Recvmsg/real_time'
         '|BM_Sendmsg/real_time'
         '|BM_Recvfrom/real_time'
         '|BM_Sendto/real_time"',
     // The following benchmarks run with large ranges of arguments.
     // Run a subset of them.
-    'futex_benchmark.cm': '--benchmark_filter_internal="'
+    'dup': '--benchmark_filter_internal="'
+        'BM_Dup/1/real_time'
+        '|BM_Dup/8/real_time'
+        '|BM_Dup/64/real_time'
+        '|BM_Dup/512/real_time'
+        '|BM_Dup/4096/real_time"',
+    'futex': '--benchmark_filter_internal="'
         'BM_FutexRoundtripDelayed/0/min_time'
         '|BM_FutexRoundtripDelayed/10/min_time'
         '|BM_FutexWaitMonotonicDeadline'
@@ -76,33 +61,89 @@ void main(List<String> args) {
         '|BM_FutexWakeNop'
         '|BM_FutexWaitNop'
         '|BM_FutexWaitRealtimeDeadline"',
-    'gettid_benchmark.cm': '--benchmark_filter_internal="'
+    'gettid': '--benchmark_filter_internal="'
         r'(BM_Gettid/real_time/threads:1'
         r'|BM_Gettid/real_time/threads:2'
         r'|BM_Gettid/real_time/threads:4'
         r'|BM_Gettid/real_time/threads:8'
         r'|BM_Gettid/real_time/threads:16)$"',
-    'sched_yield_benchmark.cm': '--benchmark_filter_internal="'
+    'mapping': '--benchmark_filter_internal="'
+        'BM_MapUnmap/1/real_time'
+        '|BM_MapUnmap/8/real_time'
+        '|BM_MapUnmap/64/real_time'
+        '|BM_MapUnmap/512/real_time'
+        '|BM_MapUnmap/4096/real_time'
+        '|BM_MapTouchUnmap/1/real_time'
+        '|BM_MapTouchUnmap/8/real_time'
+        '|BM_MapTouchUnmap/64/real_time'
+        '|BM_MapTouchUnmap/512/real_time'
+        '|BM_MapTouchUnmap/4096/real_time'
+        '|BM_MapTouchMany/1/real_time'
+        '|BM_MapTouchMany/8/real_time'
+        '|BM_MapTouchMany/64/real_time'
+        '|BM_MapTouchMany/512/real_time'
+        '|BM_MapTouchMany/4096/real_time'
+        '|BM_PageFault/real_time"',
+    'pipe': '--benchmark_filter_internal="'
+        'BM_Pipe/8/real_time'
+        '|BM_Pipe/1/real_time'
+        '|BM_Pipe/64/real_time'
+        '|BM_Pipe/512/real_time'
+        '|BM_Pipe/4096/real_time"',
+    'randread': '--benchmark_filter_internal="'
+        'BM_RandRead/1/real_time'
+        '|BM_RandRead/8/real_time'
+        '|BM_RandRead/64/real_time'
+        '|BM_RandRead/512/real_time'
+        '|BM_RandRead/4096/real_time"',
+    'read': '--benchmark_filter_internal="'
+        'BM_Read/1/real_time'
+        '|BM_Read/8/real_time'
+        '|BM_Read/64/real_time'
+        '|BM_Read/512/real_time'
+        '|BM_Read/4096/real_time"',
+    'sched_yield': '--benchmark_filter_internal="'
         r'(BM_Sched_yield/real_time/threads:1'
         r'|BM_Sched_yield/real_time/threads:2'
         r'|BM_Sched_yield/real_time/threads:4'
         r'|BM_Sched_yield/real_time/threads:8'
         r'|BM_Sched_yield/real_time/threads:16)$"',
+    'seqwrite': '--benchmark_filter_internal="'
+        'BM_SeqWrite/1/real_time'
+        '|BM_SeqWrite/8/real_time'
+        '|BM_SeqWrite/64/real_time'
+        '|BM_SeqWrite/512/real_time'
+        '|BM_SeqWrite/4096/real_time"',
+    'sleep': '--benchmark_filter_internal="'
+        'BM_Sleep/0/real_time'
+        '|BM_Sleep/1/real_time'
+        '|BM_Sleep/1000/real_time'
+        '|BM_Sleep/1000000/real_time'
+        '|BM_Sleep/10000000/real_time"',
+    'unlink': '--benchmark_filter_internal="'
+        'BM_Unlink/1/real_time'
+        '|BM_Unlink/8/real_time'
+        '|BM_Unlink/64/real_time'
+        '|BM_Unlink/512/real_time'
+        '|BM_Unlink/4096/real_time"',
+    'write': '--benchmark_filter_internal="'
+        'BM_Write/1/real_time'
+        '|BM_Write/8/real_time'
+        '|BM_Write/64/real_time'
+        '|BM_Write/512/real_time'
+        '|BM_Write/4096/real_time'
+        '|BM_Append"',
   };
 
-  benchmarks.forEach((String componentName, String expectedMetricNamesFile) {
-    var commandArgs = '';
-    if (filters.containsKey(componentName)) {
-      commandArgs = filters[componentName];
-    }
-
+  benchmarks.forEach((String componentName, String commandArgs) {
     _tests.add(() {
       test(starnix_gvisor_benchmarks, () async {
         await runTestComponent(
             packageName: starnix_gvisor_benchmarks,
-            componentName: componentName,
+            componentName: '$componentName\_benchmark.cm',
             commandArgs: commandArgs,
-            expectedMetricNamesFile: expectedMetricNamesFile);
+            expectedMetricNamesFile:
+                'fuchsia.starnix.gvisor_benchmarks.$componentName.txt');
       }, timeout: Timeout.none);
     });
   });
