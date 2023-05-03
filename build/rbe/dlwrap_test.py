8886269964bd6189688bb0f1d8afbd331bd8d8ef
David Fang <fangism@google.com>
2023-05-03 17:15:06 +0000

    [rbe] download tool command prefix
    For commands that might need to use remote outputs that
    were not downloaded due to rewrapper --download_outputs=false:
    
      build/rbe/dlwrap.py --download ... -- command
    
    or
    
      build/rbe/dlwrap.py --download ... && command
    
    (depending on which is more suitable).
    
    --download arguments are paths that point to places that would
    normally would be real files, but could be download stubs.
    Real files are ignored and left as-is, stubs (links) are downloaded
    and written over the same locations.
    
    Note: this does not require any xattrs because reproxy is not involved.
    
    Bug: 123178
    Change-Id: I0351dd07bfb5b34b101a125ed32d58dde6fb74e5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/851319
    Reviewed-by: David Turner <digit@google.com>
    Fuchsia-Auto-Submit: David Fang <fangism@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

---
+++ build/rbe/dlwrap_test.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env fuchsia-vendored-python
+# Copyright 2023 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import subprocess
+import sys
+import tempfile
+import unittest
+from pathlib import Path
+from unittest import mock
+
+import dlwrap
+import remote_action
+
+
+class MainArgParserTests(unittest.TestCase):
+
+    def test_defaults(self):
+        args = dlwrap._MAIN_ARG_PARSER.parse_args([])
+        self.assertFalse(args.verbose)
+        self.assertFalse(args.dry_run)
+        self.assertEqual(args.download, [])
+        self.assertEqual(args.command, [])
+
+    def test_verbose(self):
+        args = dlwrap._MAIN_ARG_PARSER.parse_args(['--verbose'])
+        self.assertTrue(args.verbose)
+
+    def test_dry_run(self):
+        args = dlwrap._MAIN_ARG_PARSER.parse_args(['--dry-run'])
+        self.assertTrue(args.dry_run)
+
+    def test_download_list(self):
+        args = dlwrap._MAIN_ARG_PARSER.parse_args(
+            ['--download', 'aa.o', 'bb.o'])
+        self.assertEqual(args.download, [Path('aa.o'), Path('bb.o')])
+
+    def test_command(self):
+        args = dlwrap._MAIN_ARG_PARSER.parse_args(['--', 'cat', 'dog.txt'])
+        self.assertEqual(args.command, ['cat', 'dog.txt'])
+
+
+class ReadDownloadStubInfosTests(unittest.TestCase):
+
+    def test_is_a_stub(self):
+        path = 'path/to/no/where.obj'
+        stub_info = remote_action.DownloadStubInfo(
+            path=path,
+            type="file",
+            blob_digest='feeeeeeedfaaaaace/124',
+            action_digest='87ac8eb9865d/43',
+            build_id='10293-ab8e-bc72',
+        )
+        with mock.patch.object(remote_action, 'get_download_stub_info',
+                               return_value=stub_info) as mock_get_infos:
+            stub_infos = list(dlwrap.read_download_stub_infos([path]))
+        self.assertEqual(stub_infos, [stub_info])
+        mock_get_infos.assert_called_with(path)
+
+    def test_not_a_stub(self):
+        path = 'path/to/already/downloaded.obj'
+        with mock.patch.object(remote_action, 'get_download_stub_info',
+                               return_value=None) as mock_get_infos:
+            stub_infos = list(dlwrap.read_download_stub_infos([path]))
+        self.assertEqual(stub_infos, [])
+        mock_get_infos.assert_called_with(path)
+
+
+class DownloadArtifactsTests(unittest.TestCase):
+
+    def _stub_info(self, path: Path) -> remote_action.DownloadStubInfo:
+        return remote_action.DownloadStubInfo(
+            path=path,
+            type="file",
+            blob_digest='f33333df44444ce/124',
+            action_digest='47ac8eb38351/41',
+            build_id='50f93-a38e-b112',
+        )
+
+    def test_success(self):
+        path = 'road/to/perdition.obj'
+        exec_root = Path('/exec/root')
+        working_dir = exec_root / 'work'
+        download_status = 0
+        with mock.patch.object(dlwrap, 'read_download_stub_infos',
+                               return_value=iter([self._stub_info(path)
+                                                 ])) as mock_read_stubs:
+            with mock.patch.object(
+                    remote_action.DownloadStubInfo, 'download',
+                    return_value=download_status) as mock_download:
+                status = dlwrap.download_artifacts(
+                    [path], exec_root=exec_root, working_dir=working_dir)
+        self.assertEqual(status, download_status)
+        mock_read_stubs.assert_called_once()
+        mock_download.assert_called_with(
+            exec_root=exec_root, working_dir=working_dir)
+
+    def test_failure(self):
+        path = 'highway/to/hell.obj'
+        exec_root = Path('/exec/root')
+        working_dir = exec_root / 'work'
+        download_status = 1
+        with mock.patch.object(dlwrap, 'read_download_stub_infos',
+                               return_value=iter([self._stub_info(path)
+                                                 ])) as mock_read_stubs:
+            with mock.patch.object(
+                    remote_action.DownloadStubInfo, 'download',
+                    return_value=download_status) as mock_download:
+                status = dlwrap.download_artifacts(
+                    [path], exec_root=exec_root, working_dir=working_dir)
+        self.assertEqual(status, download_status)
+        mock_read_stubs.assert_called_once()
+        mock_download.assert_called_with(
+            exec_root=exec_root, working_dir=working_dir)
+
+
+class MainTests(unittest.TestCase):
+
+    def test_dry_run(self):
+        path = 'dir/file.o'
+        exec_root = Path('/exec/root')
+        working_dir = exec_root / 'work'
+        with mock.patch.object(dlwrap, 'download_artifacts',
+                               return_value=0) as mock_download:
+            with mock.patch.object(subprocess, 'call') as mock_run:
+                status = dlwrap._main(
+                    ['--dry-run', '--', 'cat', 'foo'],
+                    exec_root=exec_root,
+                    working_dir=working_dir)
+        self.assertEqual(status, 0)
+        mock_run.assert_not_called()
+
+    def test_download_fail(self):
+        path = 'dir/file.o'
+        exec_root = Path('/exec/root')
+        working_dir = exec_root / 'work'
+        with mock.patch.object(dlwrap, 'download_artifacts',
+                               return_value=1) as mock_download:
+            with mock.patch.object(subprocess, 'call') as mock_run:
+                status = dlwrap._main(
+                    ['--', 'cat', 'foo'],
+                    exec_root=exec_root,
+                    working_dir=working_dir)
+        self.assertEqual(status, 1)
+        mock_run.assert_not_called()
+
+    def test_no_command(self):
+        path = 'dir/file.o'
+        exec_root = Path('/exec/root')
+        working_dir = exec_root / 'work'
+        with mock.patch.object(dlwrap, 'download_artifacts',
+                               return_value=0) as mock_download:
+            with mock.patch.object(subprocess, 'call',
+                                   return_value=0) as mock_run:
+                status = dlwrap._main(
+                    [], exec_root=exec_root, working_dir=working_dir)
+        self.assertEqual(status, 0)
+        mock_run.assert_not_called()
+
+    def test_success(self):
+        path = 'dir/file.o'
+        exec_root = Path('/exec/root')
+        working_dir = exec_root / 'work'
+        with mock.patch.object(dlwrap, 'download_artifacts',
+                               return_value=0) as mock_download:
+            with mock.patch.object(subprocess, 'call',
+                                   return_value=0) as mock_run:
+                status = dlwrap._main(
+                    ['--', 'cat', 'foo'],
+                    exec_root=exec_root,
+                    working_dir=working_dir)
+        self.assertEqual(status, 0)
+        mock_run.assert_called_with(['cat', 'foo'])
+
+
+if __name__ == '__main__':
+    unittest.main()
