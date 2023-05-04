f2e467f6f36e05c4f55cb06d9c4c24edbf862209
Adam Perry <adamperry@google.com>
2023-05-04 23:48:30 +0000

    [sc] Stub out parent overrides integration test.
    Start with tests that passes without any overrides.
    
    Bug: 96254
    Change-Id: I994a96b029d443205b53f223af830538c90f6e3f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/797800
    API-Review: Gary Bressler <geb@google.com>
    Fuchsia-Auto-Submit: Adam Perry <adamperry@google.com>
    Reviewed-by: Gary Bressler <geb@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

---
+++ src/sys/component_manager/tests/structured_config/parent_overrides/config_receiver.rs
@@ -0,0 +1,37 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use fidl_test_config_parentoverrides::{ReporterRequest, ReporterRequestStream};
+use fuchsia_component::server::ServiceFs;
+use futures::StreamExt;
+use received_config::Config;
+
+enum IncomingClient {
+    Reporter(ReporterRequestStream),
+}
+
+#[fuchsia::main]
+async fn main() {
+    let Config { parent_provided } = Config::take_from_startup_handle();
+
+    let mut service_fs = ServiceFs::new();
+    service_fs.dir("svc").add_fidl_service(IncomingClient::Reporter);
+    service_fs.take_and_serve_directory_handle().unwrap();
+
+    service_fs
+        .for_each_concurrent(None, |client: IncomingClient| async {
+            match client {
+                IncomingClient::Reporter(mut requests) => {
+                    while let Some(request) = requests.next().await {
+                        match request.unwrap() {
+                            ReporterRequest::GetParentProvidedConfigString { responder } => {
+                                responder.send(&parent_provided).unwrap();
+                            }
+                        }
+                    }
+                }
+            }
+        })
+        .await;
+}
---
+++ src/sys/component_manager/tests/structured_config/parent_overrides/integration_test.rs
@@ -0,0 +1,93 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use fidl::endpoints::DiscoverableProtocolMarker;
+use fidl_fuchsia_component::{CreateChildArgs, RealmMarker};
+use fidl_fuchsia_component_decl::{Child, CollectionRef, StartupMode};
+use fidl_test_config_parentoverrides::ReporterMarker;
+use fuchsia_component::client::{
+    connect_to_protocol, connect_to_protocol_at_dir_root, open_childs_exposed_directory,
+};
+use fuchsia_component_test::{Capability, ChildOptions, RealmBuilder, Ref, Route};
+
+const RECEIVER_URL: &str = "#meta/config_receiver.cm";
+
+#[fuchsia::test]
+async fn realm_api_without_override_returns_default() {
+    let realm = connect_to_protocol::<RealmMarker>().unwrap();
+
+    // TODO(https://fxbug.dev/96254) set a dynamic-child-specific config value here
+    let expected_value = "default value";
+
+    let child_name = String::from("dynamic_child_realm_api");
+    let collection_name = String::from("realm_api_collection");
+
+    let mut collection_ref = CollectionRef { name: collection_name.clone() };
+    let child_decl = Child {
+        name: Some(child_name.clone()),
+        url: Some(String::from(RECEIVER_URL)),
+        startup: Some(StartupMode::Lazy),
+        ..Child::default()
+    };
+
+    realm
+        .create_child(&mut collection_ref, child_decl, CreateChildArgs::default())
+        .await
+        .unwrap()
+        .unwrap();
+
+    let exposed_dir =
+        open_childs_exposed_directory(child_name, Some(collection_name)).await.unwrap();
+
+    let reporter = connect_to_protocol_at_dir_root::<ReporterMarker>(&exposed_dir).unwrap();
+
+    let value = reporter.get_parent_provided_config_string().await.unwrap();
+    assert_eq!(value, expected_value);
+}
+
+#[fuchsia::test]
+async fn realm_builder_without_override_returns_default() {
+    let builder = RealmBuilder::new().await.unwrap();
+
+    // TODO(https://fxbug.dev/102211) set a realm-builder-specific config value here
+    let expected_value = "default value";
+
+    let config_receiver = builder
+        .add_child("realm_builder_config_receiver", RECEIVER_URL, ChildOptions::new())
+        .await
+        .unwrap();
+    builder
+        .add_route(
+            Route::new()
+                .capability(Capability::protocol_by_name("fuchsia.logger.LogSink"))
+                .from(Ref::parent())
+                .to(&config_receiver),
+        )
+        .await
+        .unwrap();
+    builder
+        .add_route(
+            Route::new()
+                .capability(Capability::protocol_by_name(ReporterMarker::PROTOCOL_NAME))
+                .from(&config_receiver)
+                .to(Ref::parent()),
+        )
+        .await
+        .unwrap();
+    let realm = builder.build().await.unwrap();
+
+    let reporter = realm.root.connect_to_protocol_at_exposed_dir::<ReporterMarker>().unwrap();
+    let value = reporter.get_parent_provided_config_string().await.unwrap();
+    assert_eq!(value, expected_value);
+}
+
+#[fuchsia::test]
+async fn static_child_without_overrides_returns_default() {
+    // TODO(https://fxbug.dev/126578) set a static-child-specific config value here
+    let expected_value = "default value";
+
+    let reporter = connect_to_protocol::<ReporterMarker>().unwrap();
+    let value = reporter.get_parent_provided_config_string().await.unwrap();
+    assert_eq!(value, expected_value);
+}
