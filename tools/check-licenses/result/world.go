242939ff3c63dc52862fb8d52017e50fce49fb96
Jerry Belton <jcecil@google.com>
2023-05-05 20:57:26 +0000

    [check-licenses] Updates to check-license collection behavior.
    Collect CREDITS.fuchsia files by default
    Fix a trailing "/" issue in target cleaning
    Dedup all licenses based on license text.
    
    Change-Id: Ib77d548740076002ca2f7ecf40b0a5140cfde946
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/852597
    Reviewed-by: Yaar Schnitman <yaar@google.com>
    Commit-Queue: Jerry Belton <jcecil@google.com>

--- tools/check-licenses/result/world.go
+++ tools/check-licenses/result/world.go
@@ -5,12 +5,19 @@
 package result
 
 import (
+	"fmt"
+	"sort"
+
 	"go.fuchsia.dev/fuchsia/tools/check-licenses/directory"
 	"go.fuchsia.dev/fuchsia/tools/check-licenses/file"
 	"go.fuchsia.dev/fuchsia/tools/check-licenses/license"
 	"go.fuchsia.dev/fuchsia/tools/check-licenses/project"
 )
 
+// World is used to store all of the current run's information in a single object
+// that can be processed by go templates.
+//
+// TODO(fxbug.dev/126804): consider moving this into a separate build step.
 type world struct {
 	// file
 	AllFiles map[string]*file.File
@@ -23,7 +30,8 @@ type world struct {
 	RootProject        *project.Project
 	AllProjects        map[string]*project.Project
 	FilteredProjects   map[string]*project.Project
-	DedupedLicenseData [][]*file.FileData
+	LicenseData        []*DedupedLicense
+	DedupedLicenseData []*DedupedLicense
 
 	// license
 	AllPatterns                 []*license.Pattern
@@ -35,7 +43,55 @@ type world struct {
 	Empty                       *license.Pattern
 }
 
+type DedupedLicense struct {
+	LibraryNames []string
+	Text         string
+}
+
 func getWorldStruct() *world {
+	dedupedLicenseDataMap := make(map[string]*DedupedLicense, 0)
+	licenseDataList := make([]*DedupedLicense, 0)
+	dedupedLicenseDataList := make([]*DedupedLicense, 0)
+
+	// Dedup all license texts by putting them in a hashmap.
+	for _, p := range project.FilteredProjects {
+		for _, l := range p.LicenseFiles {
+			data, err := l.Data()
+			if err != nil {
+				fmt.Printf("Failed to get data for %s\n", l.RelPath())
+				continue
+			}
+
+			for _, d := range data {
+				hash := d.Hash()
+				dl := &DedupedLicense{
+					LibraryNames: []string{d.LibraryName()},
+					Text:         string(d.Data()),
+				}
+				licenseDataList = append(licenseDataList, dl)
+
+				if value, ok := dedupedLicenseDataMap[hash]; !ok {
+					dedupedLicenseDataMap[hash] = dl
+				} else {
+					value.LibraryNames = append(value.LibraryNames, d.LibraryName())
+				}
+			}
+		}
+	}
+
+	// Deterministically loop over the deduped data by storing the results
+	// in a sorted list.
+	for _, v := range dedupedLicenseDataMap {
+		sort.Strings(v.LibraryNames)
+		dedupedLicenseDataList = append(dedupedLicenseDataList, v)
+	}
+	sort.Slice(licenseDataList, func(i, j int) bool {
+		return licenseDataList[i].Text < licenseDataList[j].Text
+	})
+	sort.Slice(dedupedLicenseDataList, func(i, j int) bool {
+		return dedupedLicenseDataList[i].Text < dedupedLicenseDataList[j].Text
+	})
+
 	return &world{
 		AllFiles: file.AllFiles,
 
@@ -45,7 +101,8 @@ func getWorldStruct() *world {
 		RootProject:        project.RootProject,
 		AllProjects:        project.AllProjects,
 		FilteredProjects:   project.FilteredProjects,
-		DedupedLicenseData: project.DedupedLicenseData,
+		LicenseData:        licenseDataList,
+		DedupedLicenseData: dedupedLicenseDataList,
 
 		AllPatterns:                 license.AllPatterns,
 		AllCopyrightPatterns:        license.AllCopyrightPatterns,
