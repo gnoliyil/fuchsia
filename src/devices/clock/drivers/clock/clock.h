86aedf4c8e083d977506d0a94e8d58053994d38c
Nick Eaton <nickeaton@google.com>
2023-05-08 20:07:36 +0000

    [clock][fidl] Create clock FIDL protocol
    Create clock FIDL protocol. Make clock driver serve clock FIDL protocol.
    This protocol will replace the clock banjo protocol.
    
    Bug: 126069
    Change-Id: Ib3105863e13eb9cfc9a8d4947f000f69d9586abe
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/852356
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Reviewed-by: David Gilhooley <dgilhooley@google.com>
    API-Review: Jocelyn Dang <jocelyndang@google.com>
    Commit-Queue: Nick Eaton <nickeaton@google.com>

--- src/devices/clock/drivers/clock/clock.h
+++ src/devices/clock/drivers/clock/clock.h
@@ -5,8 +5,10 @@
 #ifndef SRC_DEVICES_CLOCK_DRIVERS_CLOCK_CLOCK_H_
 #define SRC_DEVICES_CLOCK_DRIVERS_CLOCK_CLOCK_H_
 
+#include <fidl/fuchsia.hardware.clock/cpp/wire.h>
 #include <fuchsia/hardware/clock/cpp/banjo.h>
 #include <fuchsia/hardware/clockimpl/cpp/banjo.h>
+#include <lib/component/outgoing/cpp/outgoing_directory.h>
 #include <lib/ddk/platform-defs.h>
 
 #include <ddktl/device.h>
@@ -15,13 +17,17 @@ class ClockDevice;
 using ClockDeviceType = ddk::Device<ClockDevice>;
 
 class ClockDevice : public ClockDeviceType,
-                    public ddk::ClockProtocol<ClockDevice, ddk::base_protocol> {
+                    // TODO(fxbug.dev/126069): Remove banjo clock protocol when
+                    // there aren't any banjo clock protocol clients.
+                    public ddk::ClockProtocol<ClockDevice, ddk::base_protocol>,
+                    public fidl::WireServer<fuchsia_hardware_clock::Clock> {
  public:
   ClockDevice(zx_device_t* parent, clock_impl_protocol_t* clock, uint32_t id)
       : ClockDeviceType(parent), clock_(clock), id_(id) {}
 
   static zx_status_t Create(void* ctx, zx_device_t* parent);
 
+  // Device protocol implementation
   void DdkRelease();
 
   zx_status_t ClockEnable();
@@ -36,9 +42,27 @@ class ClockDevice : public ClockDeviceType,
   zx_status_t ClockGetNumInputs(uint32_t* out);
   zx_status_t ClockGetInput(uint32_t* out);
 
+  zx_status_t ServeOutgoing(fidl::ServerEnd<fuchsia_io::Directory> server_end);
+
  private:
+  // fuchsia.hardware.clock/Clock protocol implementation
+  void Enable(EnableCompleter::Sync& completer) override;
+  void Disable(DisableCompleter::Sync& completer) override;
+  void IsEnabled(IsEnabledCompleter::Sync& completer) override;
+  void SetRate(SetRateRequestView request, SetRateCompleter::Sync& completer) override;
+  void QuerySupportedRate(QuerySupportedRateRequestView request,
+                          QuerySupportedRateCompleter::Sync& completer) override;
+  void GetRate(GetRateCompleter::Sync& completer) override;
+  void SetInput(SetInputRequestView request, SetInputCompleter::Sync& completer) override;
+  void GetNumInputs(GetNumInputsCompleter::Sync& completer) override;
+  void GetInput(GetInputCompleter::Sync& completer) override;
+
   const ddk::ClockImplProtocolClient clock_;
   const uint32_t id_;
+
+  async_dispatcher_t* dispatcher_{fdf::Dispatcher::GetCurrent()->async_dispatcher()};
+  component::OutgoingDirectory outgoing_{dispatcher_};
+  fidl::ServerBindingGroup<fuchsia_hardware_clock::Clock> bindings_;
 };
 
 #endif  // SRC_DEVICES_CLOCK_DRIVERS_CLOCK_CLOCK_H_
