81072dce6d522a34226bf169d7e27c55d89eac8b
Puneetha Ramachandra <puneetha@google.com>
2023-05-08 16:23:39 +0000

    Reland "[vim3-usb-phy] Add usb3phy settings"
    This is a reland of commit a043b32fde0228a5b67f9c65aba148672dbdc5a1
    
    Original change's description:
    > [vim3-usb-phy] Add usb3phy settings
    >
    > Fixed: 86839
    > Change-Id: Ie6986092521964826fee62fa168fc576788963a8
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/832742
    > Fuchsia-Auto-Submit: Puneetha Ramachandra <puneetha@google.com>
    > Reviewed-by: Sam Hansen <hansens@google.com>
    > Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com
    
    Bug: 86839, 125699
    Change-Id: Id6bc763906c594d3e5aaacfe83cc2cf9e148885e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/852253
    Reviewed-by: Sam Hansen <hansens@google.com>
    Commit-Queue: Puneetha Ramachandra <puneetha@google.com>

--- src/devices/usb/drivers/vim3-usb-phy/usb-phy-regs.h
+++ src/devices/usb/drivers/vim3-usb-phy/usb-phy-regs.h
@@ -29,6 +29,11 @@ constexpr uint32_t USB_R5_OFFSET = 0x94;
 
 constexpr uint32_t USB_PHY_REG21_OFFSET = 0x54;
 
+constexpr uint32_t PHY3_R1_OFFSET = 0x4;
+constexpr uint32_t PHY3_R2_OFFSET = 0x8;
+constexpr uint32_t PHY3_R4_OFFSET = 0x10;
+constexpr uint32_t PHY3_R5_OFFSET = 0x14;
+
 class U2P_R0_V2 : public hwreg::RegisterBase<U2P_R0_V2, uint32_t> {
  public:
   DEF_BIT(0, host_device);
@@ -143,6 +148,51 @@ class PLL_REGISTER_40 : public hwreg::RegisterBase<PLL_REGISTER_40, uint32_t> {
   DEF_BIT(28, enable);
   DEF_BIT(29, reset);
   static auto Get() { return hwreg::RegisterAddr<PLL_REGISTER_40>(0x40); }
+};
+
+// USB3 PHY Registers
+
+class PHY3_R1 : public hwreg::RegisterBase<PHY3_R1, uint32_t> {
+ public:
+  DEF_FIELD(4, 0, phy_tx1_term_offset);
+  DEF_FIELD(9, 5, phy_tx0_term_offset);
+  DEF_FIELD(12, 10, phy_rx1_eq);
+  DEF_FIELD(15, 13, phy_rx0_eq);
+  DEF_FIELD(20, 16, phy_los_level);
+  DEF_FIELD(23, 21, phy_los_bias);
+  DEF_BIT(24, phy_ref_clkdiv2);
+  DEF_FIELD(31, 25, phy_mpll_multiplier);
+  static auto Get() { return hwreg::RegisterAddr<PHY3_R1>(PHY3_R1_OFFSET); }
+};
+
+class PHY3_R2 : public hwreg::RegisterBase<PHY3_R2, uint32_t> {
+ public:
+  DEF_FIELD(5, 0, pcs_tx_deemph_gen2_6db);
+  DEF_FIELD(11, 6, pcs_tx_deemph_gen2_3p5db);
+  DEF_FIELD(17, 12, pcs_tx_deemph_gen1);
+  DEF_FIELD(20, 18, phy_tx_vboost_lvl);
+  DEF_FIELD(31, 21, reserved);
+  static auto Get() { return hwreg::RegisterAddr<PHY3_R2>(PHY3_R2_OFFSET); }
+};
+
+class PHY3_R4 : public hwreg::RegisterBase<PHY3_R4, uint32_t> {
+ public:
+  DEF_BIT(0, phy_cr_write);
+  DEF_BIT(1, phy_cr_read);
+  DEF_FIELD(17, 2, phy_cr_data_in);
+  DEF_BIT(18, phy_cr_cap_data);
+  DEF_BIT(19, phy_cr_cap_addr);
+  DEF_FIELD(31, 20, reserved);
+  static auto Get() { return hwreg::RegisterAddr<PHY3_R4>(PHY3_R4_OFFSET); }
+};
+
+class PHY3_R5 : public hwreg::RegisterBase<PHY3_R5, uint32_t> {
+ public:
+  DEF_FIELD(15, 0, phy_cr_data_out);
+  DEF_BIT(16, phy_cr_ack);
+  DEF_BIT(17, phy_bs_out);
+  DEF_FIELD(31, 18, reserved);
+  static auto Get() { return hwreg::RegisterAddr<PHY3_R5>(PHY3_R5_OFFSET); }
 };
 
 }  // namespace vim3_usb_phy
