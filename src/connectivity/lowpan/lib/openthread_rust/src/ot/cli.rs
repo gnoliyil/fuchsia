f7847f9860e1027555231a71ce82d30cce53c640
Robert Quattlebaum <rquattle@google.com>
2023-05-08 21:09:39 +0000

    [openthread_rust] Make `Cli::cli_init()` safe to call more than once.
    It turns out that the OpenThread call `otCliInit()` must only ever
    be called once. Calling it multiple times to change the line callback
    results in undefined behavior and crashes.
    
    This change adjusts `Cli::cli_init()` to only call `otCliInit()` once
    and makes the intermediate callback `_ot_cli_output_callback` fetch
    the closure directly from the backing instead of getting it directly
    from the context argument.
    
    Bug: 280647789
    Change-Id: Ie7887af75e35a1853bb087cd6be896ff2b47814d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/853797
    Commit-Queue: Robert Quattlebaum <rquattle@google.com>
    Fuchsia-Auto-Submit: Robert Quattlebaum <rquattle@google.com>
    Reviewed-by: Jiaming (Charlie) Wang <jiamingw@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/connectivity/lowpan/lib/openthread_rust/src/ot/cli.rs
+++ src/connectivity/lowpan/lib/openthread_rust/src/ot/cli.rs
@@ -38,7 +38,7 @@ impl Cli for Instance {
     where
         F: FnMut(&CStr) + 'a,
     {
-        unsafe extern "C" fn _ot_cli_output_callback<'a, F: FnMut(&CStr) + 'a>(
+        unsafe extern "C" fn _ot_cli_output_callback(
             context: *mut ::std::os::raw::c_void,
             line: *const c_char,
             _: *mut otsys::__va_list_tag,
@@ -47,33 +47,66 @@ impl Cli for Instance {
 
             trace!("_ot_cli_output_callback: {:?}", line);
 
-            // Reconstitute a reference to our closure.
-            let sender = &mut *(context as *mut F);
+            // Reconstitute a reference to our instance
+            let instance = Instance::ref_from_ot_ptr(context as *mut otInstance).unwrap();
 
-            sender(line);
+            // Get a reference to our instance backing.
+            let backing = instance.borrow_backing();
+
+            // Remove our line sender from the cell in the backing.
+            // We will replace it once we are done.
+            let mut sender_option = backing.cli_output_fn.replace(None);
+
+            if let Some(sender) = sender_option.as_mut() {
+                sender(line);
+            } else {
+                info!("_ot_cli_output_callback: Nothing to handle CLI output");
+            }
+
+            // Put the sender back into the backing so that we can
+            // use it again later.
+            if let Some(new_sender) = backing.cli_output_fn.replace(sender_option) {
+                // In this case someone called the init function while
+                // they were in the callback. We want to restore that
+                // callback.
+                backing.cli_output_fn.set(Some(new_sender));
+            }
 
             line.to_bytes().len().try_into().unwrap()
         }
 
-        let mut x = Box::new(f);
-        let (fn_ptr, fn_box, cb): (_, _, otCliOutputCallback) = (
-            x.as_mut() as *mut F as *mut ::std::os::raw::c_void,
-            Some(x as Box<dyn FnMut(&CStr) + 'a>),
-            Some(_ot_cli_output_callback::<F>),
-        );
+        // Put our sender closure in a box.
+        // This will go into the backing.
+        let fn_box = Box::new(f) as Box<dyn FnMut(&CStr) + 'a>;
+
+        // Prepare a function pointer for our callback closure.
+        let cb: otCliOutputCallback = Some(_ot_cli_output_callback);
+
+        // Go ahead and get our context pointer ready for the callback.
+        // In this case our context pointer is the `otInstance` pointer.
+        let fn_ptr = self.as_ot_ptr() as *mut ::std::os::raw::c_void;
 
         unsafe {
-            otCliInit(self.as_ot_ptr(), cb, fn_ptr);
-
             // Make sure our object eventually gets cleaned up.
-            // Here we must also transmute our closure to have a 'static lifetime.
-            // We need to do this because the borrow checker cannot infer the
-            // proper lifetime for the singleton instance backing, but
-            // this is guaranteed by the API.
-            self.borrow_backing().cli_output_fn.set(std::mem::transmute::<
-                Option<Box<dyn FnMut(&'_ CStr) + 'a>>,
-                Option<Box<dyn FnMut(&'_ CStr) + 'static>>,
-            >(fn_box));
+            // Here we must also transmute our closure to have a 'static
+            // lifetime.
+            // SAFETY: We need to do this because the borrow checker
+            //         cannot infer the proper lifetime for the
+            //         singleton instance backing, but this is guaranteed
+            //         by the API.
+            let prev_callback =
+                self.borrow_backing().cli_output_fn.replace(std::mem::transmute::<
+                    Option<Box<dyn FnMut(&'_ CStr) + 'a>>,
+                    Option<Box<dyn FnMut(&'_ CStr) + 'static>>,
+                >(Some(fn_box)));
+
+            // Check to see if there was a previous callback registered.
+            // We only want to call `otCliInit()` if there was no
+            // previously registered callback.
+            if prev_callback.is_none() {
+                // SAFETY: This must only be called once.
+                otCliInit(self.as_ot_ptr(), cb, fn_ptr);
+            }
         }
     }
 }
