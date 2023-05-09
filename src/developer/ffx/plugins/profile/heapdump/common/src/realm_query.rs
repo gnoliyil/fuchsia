30749f1a825b775cf6d4902efe5abe6be7e059df
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2023-05-09 12:56:10 +0000

    [heapdump] Use ConnectCapability instead of Connect
    Removes the unnecessary complexity of selectors in the RemoteControl
    API.
    
    Bug: 126721
    
    Change-Id: I61803d2205f82a309eac50ea780a13e0fb89a24f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/852218
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>
    Reviewed-by: Fabio D'Urso <fdurso@google.com>
    Commit-Queue: Miguel Flores <miguelfrde@google.com>
    Reviewed-by: Étienne J. Membrives <etiennej@google.com>

--- src/developer/ffx/plugins/profile/heapdump/common/src/realm_query.rs
+++ src/developer/ffx/plugins/profile/heapdump/common/src/realm_query.rs
@@ -7,7 +7,7 @@ use component_debug::capability::{get_all_route_segments, RouteSegment};
 use errors::{ffx_bail, ffx_error};
 use fidl::{endpoints::DiscoverableProtocolMarker, Status};
 use fidl_fuchsia_developer_remotecontrol::RemoteControlProxy;
-use fidl_fuchsia_diagnostics::Selector;
+use fidl_fuchsia_io::OpenFlags;
 use fidl_fuchsia_memory_heapdump_client as fheapdump_client;
 use fidl_fuchsia_sys2::RealmQueryProxy;
 
@@ -23,29 +23,11 @@ async fn list_collectors(query_proxy: &RealmQueryProxy) -> anyhow::Result<Vec<St
             RouteSegment::DeclareBy { moniker, capability: CapabilityDecl::Protocol(protocol) }
                 if protocol.name() == COLLECTOR_CAPABILITY =>
             {
-                Some(
-                    moniker
-                        .to_string()
-                        .strip_prefix('/')
-                        .expect("should start with '/'")
-                        .to_string(),
-                )
+                Some(moniker.to_string())
             }
             _ => None,
         })
         .collect())
-}
-
-/// Given a collector's moniker, create a selector for its COLLECTOR_CAPABILITY and parse it.
-fn build_collector_selector(moniker: &str) -> anyhow::Result<(Selector, String)> {
-    let selector_str = format!(
-        "{}:expose:{}",
-        selectors::sanitize_moniker_for_selectors(moniker),
-        COLLECTOR_CAPABILITY
-    );
-    let selector = selectors::parse_selector::<selectors::VerboseError>(&selector_str)
-        .map_err(|err| ffx_error!("Failed to parse selector {}: {}", selector_str, err))?;
-    Ok((selector, selector_str))
 }
 
 /// Connects to the collector.
@@ -77,50 +59,19 @@ pub async fn connect_to_collector(
         }
     };
 
-    let (selector, selector_str) = build_collector_selector(&moniker)?;
     let (collector_proxy, collector_server) =
         fidl::endpoints::create_proxy::<fheapdump_client::CollectorMarker>()?;
-    remote_control.connect(selector, collector_server.into_channel()).await?.map_err(|err| {
-        ffx_error!(
-            "Attempting to connect to moniker {} (full selector {}) failed with {:?}",
-            moniker,
-            selector_str,
-            err
+    remote_control
+        .connect_capability(
+            &moniker,
+            fheapdump_client::CollectorMarker::PROTOCOL_NAME,
+            collector_server.into_channel(),
+            OpenFlags::RIGHT_READABLE,
         )
-    })?;
+        .await?
+        .map_err(|err| {
+            ffx_error!("Attempting to connect to moniker {moniker} failed with {err:?}",)
+        })?;
 
     Ok(collector_proxy)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use fidl_fuchsia_diagnostics::{PropertySelector, StringSelector::ExactMatch, TreeSelector};
-    use test_case::test_case;
-
-    // Verifies that build_collector_selector is not confused by the presence of collections, whose
-    // ':' character needs to be internally escaped.
-    #[test_case("core/foo/bar" ; "no collections")]
-    #[test_case("core/ffx-laboratory:foo/heapdump-collector" ; "parent is in a collection")]
-    #[test_case("core/ffx-laboratory/foo:bar" ; "leaf is in a collection")]
-    fn test_build_collector_selector(moniker: &str) {
-        let (selector, _) = build_collector_selector(moniker).unwrap();
-
-        // The moniker must have been split at '/' without any special handling for ':'
-        let expected_segments: Vec<_> =
-            moniker.split('/').map(|segment| ExactMatch(segment.to_string())).collect();
-        assert_eq!(
-            selector.component_selector.unwrap().moniker_segments.unwrap(),
-            expected_segments
-        );
-
-        // The tree selector should always point to the exposed COLLECTOR_CAPABILITY
-        assert_eq!(
-            selector.tree_selector.unwrap(),
-            TreeSelector::PropertySelector(PropertySelector {
-                node_path: vec![ExactMatch("expose".to_string())],
-                target_properties: ExactMatch(COLLECTOR_CAPABILITY.to_string()),
-            })
-        );
-    }
 }
