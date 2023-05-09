24190449c677311eecbb4f0fe5c57cb0b38acd27
Joseph Ryan <josephry@google.com>
2023-05-09 00:57:57 +0000

    [fx][rustdoc] cleanup formatting
    Change-Id: I6b855785a4cef686d41f40cc0b94d7e4ceaa9e91
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/854076
    Commit-Queue: Dan Johnson <computerdruid@google.com>
    Reviewed-by: Dan Johnson <computerdruid@google.com>
    Fuchsia-Auto-Submit: Dan Johnson <computerdruid@google.com>
    Reviewed-by: Joseph Ryan <josephry@google.com>

--- tools/devshell/contrib/lib/rust/rustdoc.py
+++ tools/devshell/contrib/lib/rust/rustdoc.py
@@ -7,12 +7,12 @@
 
 import argparse
 import os
-import platform
 import subprocess
 import sys
 
 import rust
-from rust import ROOT_PATH
+from rust import HOST_PLATFORM, ROOT_PATH
+
 
 def manifest_path_from_path_or_gn_target(arg):
     if arg.endswith("Cargo.toml"):
@@ -22,35 +22,33 @@ def manifest_path_from_path_or_gn_target(arg):
         gn_target.label_name += ".actual"
         return gn_target.manifest_path()
 
+
 def main():
     parser = argparse.ArgumentParser("fx rustdoc")
-    parser.add_argument("manifest_path",
-                        metavar="gn_target",
-                        type=manifest_path_from_path_or_gn_target,
-                        help="GN target to document. \
-                              Use '.[:target]' to discover the cargo target \
-                              for the current directory or use the \
-                              absolute path to the target \
-                              (relative to $FUCHSIA_DIR). \
-                              For example: //garnet/bin/foo/bar:baz. \
-                              Alternatively, this can be a path to a \
-                              Cargo.toml file of a package for which to \
-                              generate docs.")
-    parser.add_argument("--target",
-                        help="Target triple for which this crate is being compiled",
-                        default="x86_64-fuchsia")
-    parser.add_argument("--out-dir",
-                        help="Path to the Fuchsia build directory",
-                        required=False)
-    parser.add_argument("--no-deps",
-                        action="store_true",
-                        help="Disable building of docs for dependencies")
-    parser.add_argument("--doc-private",
-                        action="store_true",
-                        help="Document private items")
-    parser.add_argument("--open",
-                        action="store_true",
-                        help="Open the generated documentation")
+    parser.add_argument(
+        "manifest_path",
+        metavar="gn_target",
+        type=manifest_path_from_path_or_gn_target,
+        help="GN target to document. Use '.[:target]' to discover the cargo \
+                target for the current directory or use the absolute path to \
+                the target (relative to $FUCHSIA_DIR). For example: \
+                //garnet/bin/foo/bar:baz. Alternatively, this can be a path \
+                to a Cargo.toml file of a package for which to generate docs.",
+    )
+    parser.add_argument(
+        "--target",
+        help="Target triple for which this crate is being compiled",
+        default="x86_64-fuchsia")
+    parser.add_argument("--out-dir", help="Path to the Fuchsia build directory")
+    parser.add_argument(
+        "--no-deps",
+        action="store_true",
+        help="Disable building of docs for dependencies",
+    )
+    parser.add_argument(
+        "--doc-private", action="store_true", help="Document private items")
+    parser.add_argument(
+        "--open", action="store_true", help="Open the generated documentation")
 
     args = parser.parse_args()
 
@@ -59,71 +57,49 @@ def main():
     else:
         build_dir = os.environ["FUCHSIA_BUILD_DIR"]
 
+    rust_dir = ROOT_PATH / "prebuilt/third_party/rust" / HOST_PLATFORM / "bin"
+    buildtools_dir = ROOT_PATH / "prebuilt/third_party"
+    clang_prefix = buildtools_dir / "clang" / HOST_PLATFORM / "bin"
+    cmake_dir = buildtools_dir / "cmake" / HOST_PLATFORM / "bin"
+    clang = str(clang_prefix / "clang")
+    shared_libs_root = ROOT_PATH / build_dir
+    sysroot = (
+        ROOT_PATH / build_dir /
+        "zircon_toolchain/obj/zircon/public/sysroot/sysroot")
+
     env = os.environ.copy()
 
-    host_platform = "%s-%s" % (
-        platform.system().lower().replace("darwin", "mac"),
-        {
-            "x86_64": "x64",
-            "aarch64": "arm64",
-        }[platform.machine()],
-    )
-
-    target_cpu = {
-        "x86_64-fuchsia": "x64",
-        "aarch64-fuchsia": "aarch64",
-        "x86_64-unknown-linux-gnu": "x64",
-        "aarch64-unknown-linux-gnu": "aarch64",
-    }[args.target]
-
-    # run cargo from third_party/rust_crates which has an appropriate .cargo/config
-    cwd = os.path.join(ROOT_PATH, "third_party", "rust_crates")
-    buildtools_dir = os.path.join(ROOT_PATH, "prebuilt", "third_party")
-    clang_prefix = os.path.join(buildtools_dir, "clang", host_platform, "bin")
-    cmake_dir = os.path.join(buildtools_dir, "cmake", host_platform, "bin")
-    rust_dir = os.path.join(buildtools_dir, "rust", host_platform, "bin")
-    cargo = os.path.join(rust_dir, "cargo")
-    rustc = os.path.join(rust_dir, "rustc")
-    rustdoc = os.path.join(ROOT_PATH, "scripts", "rust", "rustdoc_no_ld_library_path.sh")
-
-    shared_libs_root = os.path.join(ROOT_PATH, build_dir)
-    sysroot = os.path.join(
-        ROOT_PATH, build_dir, "zircon_toolchain", "obj", "zircon", "public",
-        "sysroot", "sysroot")
-
-    clang_c_compiler = os.path.join(clang_prefix, "clang")
-
-    env["CARGO_TARGET_LINKER"] = clang_c_compiler
-    env["CARGO_TARGET_X86_64_APPLE_DARWIN_LINKER"] = clang_c_compiler
-    env["CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER"] = clang_c_compiler
-    env["CARGO_TARGET_%s_LINKER" % args.target.replace("-", "_").upper()] = clang_c_compiler
+    env["CARGO_TARGET_LINKER"] = clang
+    env["CARGO_TARGET_X86_64_APPLE_DARWIN_LINKER"] = clang
+    env["CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER"] = clang
+    env["CARGO_TARGET_%s_LINKER" %
+        args.target.replace("-", "_").upper()] = clang
     if "fuchsia" in args.target:
-        env["CARGO_TARGET_%s_RUSTFLAGS" % args.target.replace("-", "_").upper()] = (
-            "-Clink-arg=--target=" + args.target +
-            " -Clink-arg=--sysroot=" + sysroot +
-            " -Lnative=" + shared_libs_root
-        )
+        env["CARGO_TARGET_%s_RUSTFLAGS" %
+            args.target.replace("-", "_").upper()] = (
+                "-Clink-arg=--target=" + args.target +
+                " -Clink-arg=--sysroot=" + str(sysroot) + " -Lnative=" +
+                str(shared_libs_root))
     else:
-        env["CARGO_TARGET_%s_RUSTFLAGS" % args.target.replace("-", "_").upper()] = (
-            "-Clink-arg=--target=" + args.target
-        )
-    env["RUSTC"] = rustc
-    env["RUSTDOC"] = rustdoc
+        env["CARGO_TARGET_%s_RUSTFLAGS" %
+            args.target.replace("-", "_").upper()] = (
+                "-Clink-arg=--target=" + args.target)
+    env["CC"] = clang
+    env["CXX"] = str(clang_prefix / "clang++")
+    env["AR"] = str(clang_prefix / "llvm-ar")
+    env["RANLIB"] = str(clang_prefix / "llvm-ranlib")
+    env["RUSTC"] = str(rust_dir / "rustc")
+    env["RUSTDOC"] = str(
+        ROOT_PATH / "scripts/rust/rustdoc_no_ld_library_path.sh")
     env["RUSTDOCFLAGS"] = "-Z unstable-options --enable-index-page"
     env["RUST_BACKTRACE"] = "1"
-    env["CC"] = clang_c_compiler
     if "fuchsia" in args.target:
         env["CFLAGS"] = "--sysroot=%s -L %s" % (sysroot, shared_libs_root)
-    env["CXX"] = os.path.join(clang_prefix, "clang++")
-    env["AR"] = os.path.join(clang_prefix, "llvm-ar")
-    env["RANLIB"] = os.path.join(clang_prefix, "llvm-ranlib")
     env["PATH"] = "%s:%s" % (env["PATH"], cmake_dir)
 
     call_args = [
-        cargo,
-        "doc",
-        "--manifest-path=%s" % args.manifest_path,
-        "--target=%s" % args.target,
+        rust_dir / "cargo", "doc", "--manifest-path=" + str(args.manifest_path),
+        "--target=%s" % args.target
     ]
 
     if args.no_deps:
@@ -135,7 +111,10 @@ def main():
     if args.doc_private:
         call_args.append("--document-private-items")
 
-    return subprocess.call(call_args, env=env, cwd=cwd)
+    # run cargo from third_party/rust_crates which has an appropriate .cargo/config
+    return subprocess.call(
+        call_args, env=env, cwd=ROOT_PATH / "third_party/rust_crates")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     sys.exit(main())
