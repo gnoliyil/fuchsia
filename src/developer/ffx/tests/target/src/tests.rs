8f59a6da0b53b266f6ec7760b5e5190962c7b0ba
Dwayne Slater <dwayneslater@google.com>
2023-05-09 22:33:23 +0000

    [ffx][testing] Add test for list and reboot in fastboot
    Change-Id: I90968f0937f4bd2ff5b24755d7c3aad3715cd08f
    Bug: 124396
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/839698
    Commit-Queue: Dwayne Slater <dwayneslater@google.com>
    Reviewed-by: Steven Grady <slgrady@google.com>

--- src/developer/ffx/tests/target/src/tests.rs
+++ src/developer/ffx/tests/target/src/tests.rs
@@ -9,7 +9,7 @@ use fixture::fixture;
 use fuchsia_async as _;
 use futures::{
     io::{AsyncBufReadExt, AsyncReadExt, BufReader},
-    AsyncWriteExt, StreamExt,
+    AsyncRead, AsyncWrite, AsyncWriteExt, Stream, StreamExt,
 };
 
 /// Test `ffx target flash` by bringing up an emulator in fastboot.
@@ -22,21 +22,12 @@ async fn test_target_flash_gigaboot(ctx: TestContext) {
 
     let emu = Emu::start(&ctx);
 
-    let serial = emu.serial().await;
-
-    // On initial boot, press `f` to enter fastboot.
     {
-        let (reader, mut writer) = serial.split();
-        let reader = BufReader::new(reader);
-        let mut lines = reader.lines();
-        while let Some(res) = lines.next().await {
-            let line = res.unwrap();
-            eprintln!("{}", line);
-            if line.contains("Press f to enter fastboot.") {
-                writer.write_all(b"f").await.expect("failed to press f");
-                break;
-            }
-        }
+        let serial = emu.serial().await;
+        let (mut serial_lines, mut serial_writer) = serial_lines(serial).await;
+
+        // On initial boot, press `f` to enter fastboot.
+        enter_fastboot(&mut serial_lines, &mut serial_writer).await;
     }
 
     std::thread::sleep(Duration::from_secs(5));
@@ -73,5 +64,62 @@ async fn test_target_flash_gigaboot(ctx: TestContext) {
         assert!(out.stdout.contains("Product:"), "stdout is unexpected: {:?}", out);
         assert!(out.stderr.lines().count() == 0, "stderr is unexpected: {:?}", out);
         break
+    }
+}
+
+#[fixture(base_fixture)]
+#[fuchsia::test]
+async fn test_target_reboot_to_bootloader_gigaboot(ctx: TestContext) {
+    let isolate = ctx.isolate();
+
+    let emu = Emu::start(&ctx);
+
+    {
+        let serial = emu.serial().await;
+        let (mut serial_lines, mut serial_writer) = serial_lines(serial).await;
+
+        // On initial boot, press `f` to enter fastboot.
+        enter_fastboot(&mut serial_lines, &mut serial_writer).await;
+    }
+
+    std::thread::sleep(Duration::from_secs(5));
+
+    let output = isolate
+        .ffx(&["--target", emu.nodename(), "target", "reboot", "-b"])
+        .await
+        .expect("reboot to bootloader");
+    assert!(output.status.success(), "Failed to run command: {}\n{}", output.stdout, output.stderr);
+
+    // ffx waits for the bootloader to re-enter fastboot before returning.
+
+    let out = isolate
+        .ffx(&["--target", emu.nodename(), "target", "list", emu.nodename()])
+        .await
+        .expect("target show");
+
+    assert!(out.status.success(), "status is unexpected: {:?}", out);
+    assert!(out.stdout.contains("Fastboot"), "stdout is unexpected: {:?}", out);
+    assert!(out.stderr.lines().count() == 0, "stderr is unexpected: {:?}", out);
+}
+
+async fn serial_lines(
+    serial: impl AsyncRead + AsyncWrite,
+) -> (impl Stream<Item = std::io::Result<String>>, impl AsyncWrite) {
+    let (reader, writer) = serial.split();
+    let reader = BufReader::new(reader);
+    (reader.lines(), writer)
+}
+
+async fn enter_fastboot(
+    lines: &mut (dyn Stream<Item = std::io::Result<String>> + Unpin),
+    writer: &mut (dyn AsyncWrite + Unpin),
+) {
+    while let Some(res) = lines.next().await {
+        let line = res.unwrap();
+        eprintln!("{}", line);
+        if line.contains("Press f to enter fastboot.") {
+            writer.write_all(b"f").await.expect("failed to press f");
+            break;
+        }
     }
 }
