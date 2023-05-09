0cf5a44bd72b37155edda294309461718e6d471c
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2023-05-09 12:20:59 +0000

    [net-cli] Use RemoteControl/ConnectCapability
    Removes the unnecessary complexity of selectors in the RemoteControl
    API.
    
    Bug: 126721
    
    Change-Id: Ibeae997becadfdee492b19aed91cef373279049d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/852219
    Commit-Queue: Miguel Flores <miguelfrde@google.com>
    Reviewed-by: Evan Wildenhain <wildenhain@google.com>
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>

--- src/connectivity/network/net-cli/ffx/src/lib.rs
+++ src/connectivity/network/net-cli/ffx/src/lib.rs
@@ -10,6 +10,7 @@ use errors::FfxError;
 use ffx_core::ffx_plugin;
 use fidl::endpoints::ProtocolMarker;
 use fidl_fuchsia_developer_remotecontrol as fremotecontrol;
+use fidl_fuchsia_io as fio;
 use fidl_fuchsia_net_debug as fdebug;
 use fidl_fuchsia_net_dhcp as fdhcp;
 use fidl_fuchsia_net_filter as ffilter;
@@ -19,19 +20,10 @@ use fidl_fuchsia_net_neighbor as fneighbor;
 use fidl_fuchsia_net_routes as froutes;
 use fidl_fuchsia_net_stack as fstack;
 
-const DEBUG_SELECTOR_SUFFIX: &str = "/netstack:expose:fuchsia.net.debug.Interfaces";
-const DHCPD_SELECTOR_SUFFIX: &str = "/dhcpd:expose:fuchsia.net.dhcp.Server";
-const DNS_SELECTOR_SUFFIX: &str = "/dns-resolver:expose:fuchsia.net.name.Lookup";
-const FILTER_SELECTOR_SUFFIX: &str = "/netstack:expose:fuchsia.net.filter.Filter";
-const INTERFACES_SELECTOR_SUFFIX: &str = "/netstack:expose:fuchsia.net.interfaces.State";
-const NEIGHBOR_CONTROLLER_SELECTOR_SUFFIX: &str =
-    "/netstack:expose:fuchsia.net.neighbor.Controller";
-const NEIGHBOR_VIEW_SELECTOR_SUFFIX: &str = "/netstack:expose:fuchsia.net.neighbor.View";
-const LOG_SELECTOR_SUFFIX: &str = "/netstack:expose:fuchsia.net.stack.Log";
-const STACK_SELECTOR_SUFFIX: &str = "/netstack:expose:fuchsia.net.stack.Stack";
-const ROUTES_V4_SELECTOR_SUFFIX: &str = "/netstack:expose:fuchsia.net.routes.StateV4";
-const ROUTES_V6_SELECTOR_SUFFIX: &str = "/netstack:expose:fuchsia.net.routes.StateV6";
-const NETWORK_REALM: &str = "core/network";
+const NETSTACK_MONIKER_SUFFIX: &str = "/netstack";
+const DHCPD_MONIKER_SUFFIX: &str = "/dhcpd";
+const DNS_MONIKER_SUFFIX: &str = "/dns-resolver";
+const NETWORK_REALM: &str = "/core/network";
 
 struct FfxConnector<'a> {
     remote_control: fremotecontrol::RemoteControlProxy,
@@ -41,22 +33,28 @@ struct FfxConnector<'a> {
 impl FfxConnector<'_> {
     async fn remotecontrol_connect<S: ProtocolMarker>(
         &self,
-        selector_suffix: &str,
+        moniker_suffix: &str,
     ) -> Result<S::Proxy, anyhow::Error> {
         let Self { remote_control, realm } = &self;
-        let unparsed_selector = format!("{}{}", realm, selector_suffix);
+        let mut moniker = format!("{}{}", realm, moniker_suffix);
+        // TODO: remove once all clients of this tool are passing monikers instead of selectors for
+        // `realm`.
+        if !moniker.starts_with("/") {
+            moniker = moniker.replace("\\:", ":");
+            moniker = format!("/{moniker}");
+        }
         let (proxy, server_end) = fidl::endpoints::create_proxy::<S>()
             .with_context(|| format!("failed to create proxy to {}", S::DEBUG_NAME))?;
-        let selector = selectors::parse_selector::<selectors::VerboseError>(&unparsed_selector)
-            .with_context(|| format!("failed to parse selector {}", &unparsed_selector))?;
-        let _: fremotecontrol::ServiceMatch =
-            remote_control.connect(selector, server_end.into_channel()).await?.map_err(|e| {
-                anyhow::anyhow!(
-                    "failed to connect to {} as {}: {:?}",
-                    S::DEBUG_NAME,
-                    unparsed_selector,
-                    e
-                )
+        remote_control
+            .connect_capability(
+                &moniker,
+                S::DEBUG_NAME,
+                server_end.into_channel(),
+                fio::OpenFlags::RIGHT_READABLE,
+            )
+            .await?
+            .map_err(|e| {
+                anyhow::anyhow!("failed to connect to {} at {}: {:?}", S::DEBUG_NAME, moniker, e)
             })?;
         Ok(proxy)
     }
@@ -67,7 +65,7 @@ impl net_cli::ServiceConnector<fdebug::InterfacesMarker> for FfxConnector<'_> {
     async fn connect(
         &self,
     ) -> Result<<fdebug::InterfacesMarker as ProtocolMarker>::Proxy, anyhow::Error> {
-        self.remotecontrol_connect::<fdebug::InterfacesMarker>(DEBUG_SELECTOR_SUFFIX).await
+        self.remotecontrol_connect::<fdebug::InterfacesMarker>(NETSTACK_MONIKER_SUFFIX).await
     }
 }
 
@@ -76,7 +74,7 @@ impl net_cli::ServiceConnector<fdhcp::Server_Marker> for FfxConnector<'_> {
     async fn connect(
         &self,
     ) -> Result<<fdhcp::Server_Marker as ProtocolMarker>::Proxy, anyhow::Error> {
-        self.remotecontrol_connect::<fdhcp::Server_Marker>(DHCPD_SELECTOR_SUFFIX).await
+        self.remotecontrol_connect::<fdhcp::Server_Marker>(DHCPD_MONIKER_SUFFIX).await
     }
 }
 
@@ -85,7 +83,7 @@ impl net_cli::ServiceConnector<ffilter::FilterMarker> for FfxConnector<'_> {
     async fn connect(
         &self,
     ) -> Result<<ffilter::FilterMarker as ProtocolMarker>::Proxy, anyhow::Error> {
-        self.remotecontrol_connect::<ffilter::FilterMarker>(FILTER_SELECTOR_SUFFIX).await
+        self.remotecontrol_connect::<ffilter::FilterMarker>(NETSTACK_MONIKER_SUFFIX).await
     }
 }
 
@@ -94,7 +92,7 @@ impl net_cli::ServiceConnector<finterfaces::StateMarker> for FfxConnector<'_> {
     async fn connect(
         &self,
     ) -> Result<<finterfaces::StateMarker as ProtocolMarker>::Proxy, anyhow::Error> {
-        self.remotecontrol_connect::<finterfaces::StateMarker>(INTERFACES_SELECTOR_SUFFIX).await
+        self.remotecontrol_connect::<finterfaces::StateMarker>(NETSTACK_MONIKER_SUFFIX).await
     }
 }
 
@@ -103,10 +101,7 @@ impl net_cli::ServiceConnector<fneighbor::ControllerMarker> for FfxConnector<'_>
     async fn connect(
         &self,
     ) -> Result<<fneighbor::ControllerMarker as ProtocolMarker>::Proxy, anyhow::Error> {
-        self.remotecontrol_connect::<fneighbor::ControllerMarker>(
-            NEIGHBOR_CONTROLLER_SELECTOR_SUFFIX,
-        )
-        .await
+        self.remotecontrol_connect::<fneighbor::ControllerMarker>(NETSTACK_MONIKER_SUFFIX).await
     }
 }
 
@@ -115,14 +110,14 @@ impl net_cli::ServiceConnector<fneighbor::ViewMarker> for FfxConnector<'_> {
     async fn connect(
         &self,
     ) -> Result<<fneighbor::ViewMarker as ProtocolMarker>::Proxy, anyhow::Error> {
-        self.remotecontrol_connect::<fneighbor::ViewMarker>(NEIGHBOR_VIEW_SELECTOR_SUFFIX).await
+        self.remotecontrol_connect::<fneighbor::ViewMarker>(NETSTACK_MONIKER_SUFFIX).await
     }
 }
 
 #[async_trait::async_trait]
 impl net_cli::ServiceConnector<fstack::LogMarker> for FfxConnector<'_> {
     async fn connect(&self) -> Result<<fstack::LogMarker as ProtocolMarker>::Proxy, anyhow::Error> {
-        self.remotecontrol_connect::<fstack::LogMarker>(LOG_SELECTOR_SUFFIX).await
+        self.remotecontrol_connect::<fstack::LogMarker>(NETSTACK_MONIKER_SUFFIX).await
     }
 }
 
@@ -131,7 +126,7 @@ impl net_cli::ServiceConnector<fstack::StackMarker> for FfxConnector<'_> {
     async fn connect(
         &self,
     ) -> Result<<fstack::StackMarker as ProtocolMarker>::Proxy, anyhow::Error> {
-        self.remotecontrol_connect::<fstack::StackMarker>(STACK_SELECTOR_SUFFIX).await
+        self.remotecontrol_connect::<fstack::StackMarker>(NETSTACK_MONIKER_SUFFIX).await
     }
 }
 
@@ -140,7 +135,7 @@ impl net_cli::ServiceConnector<froutes::StateV4Marker> for FfxConnector<'_> {
     async fn connect(
         &self,
     ) -> Result<<froutes::StateV4Marker as ProtocolMarker>::Proxy, anyhow::Error> {
-        self.remotecontrol_connect::<froutes::StateV4Marker>(ROUTES_V4_SELECTOR_SUFFIX).await
+        self.remotecontrol_connect::<froutes::StateV4Marker>(NETSTACK_MONIKER_SUFFIX).await
     }
 }
 
@@ -149,7 +144,7 @@ impl net_cli::ServiceConnector<froutes::StateV6Marker> for FfxConnector<'_> {
     async fn connect(
         &self,
     ) -> Result<<froutes::StateV6Marker as ProtocolMarker>::Proxy, anyhow::Error> {
-        self.remotecontrol_connect::<froutes::StateV6Marker>(ROUTES_V6_SELECTOR_SUFFIX).await
+        self.remotecontrol_connect::<froutes::StateV6Marker>(NETSTACK_MONIKER_SUFFIX).await
     }
 }
 
@@ -158,7 +153,7 @@ impl net_cli::ServiceConnector<fname::LookupMarker> for FfxConnector<'_> {
     async fn connect(
         &self,
     ) -> Result<<fname::LookupMarker as ProtocolMarker>::Proxy, anyhow::Error> {
-        self.remotecontrol_connect::<fname::LookupMarker>(DNS_SELECTOR_SUFFIX).await
+        self.remotecontrol_connect::<fname::LookupMarker>(DNS_MONIKER_SUFFIX).await
     }
 }
 
