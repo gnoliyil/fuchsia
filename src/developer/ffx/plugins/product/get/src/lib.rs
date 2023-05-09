8116dd4732ffbe949805dea5d3cb1f7d440368a9
Dave Schuyler <dschuyler@chromium.org>
2023-05-09 19:33:32 +0000

    [product] wrap lookup and download in get; disable old get
    The prior `ffx product get` is renamed to  `ffx product download`.
    The `get` command is now essentially removed.
    
    Adds `ffx product get` which does the function of both lookup and
    download. This is behind an --experimental flag which bypasses
    the message about the tool being renamed.
    
    The experimental version is not for production use and is a proof
    of concept example.
    
    Change-Id: I3fa2b6b713116877a7745127412aa3a31be61d88
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/841896
    Commit-Queue: Dave Schuyler <dschuyler@google.com>
    Reviewed-by: Jiaming Li <lijiaming@google.com>

--- src/developer/ffx/plugins/product/get/src/lib.rs
+++ src/developer/ffx/plugins/product/get/src/lib.rs
@@ -6,92 +6,127 @@
 //! - acquire and display product bundle information (metadata)
 //! - acquire related data files, such as disk partition images (data)
 
-use ::gcs::client::{Client, ProgressResponse, ProgressState};
-use anyhow::{anyhow, Context, Result};
-use async_fs::rename;
-use errors::ffx_bail;
+use ::gcs::client::Client;
+use anyhow::{Context, Result};
 use ffx_core::ffx_plugin;
+use ffx_product_download::pb_download_impl;
 use ffx_product_get_args::GetCommand;
-use pbms::{make_way_for_output, transfer_download};
-use std::io::{stderr, stdin, stdout};
+use ffx_product_lookup::pb_lookup_impl;
+use std::{
+    io::{stderr, stdin, stdout},
+    path::Path,
+};
 use structured_ui;
 
 /// `ffx product get` sub-command.
 #[ffx_plugin("product.experimental")]
 pub async fn pb_get(cmd: GetCommand) -> Result<()> {
+    if !cmd.experimental {
+        // The --experimental flag is intentionally not advertised to the user,
+        // because the intent is for the user to call `download` instead.
+        println!("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+        println!("@");
+        println!("@  The `get` subcommand is renamed");
+        println!("@");
+        println!("@  Please use `ffx product download ...` instead.");
+        println!("@");
+        println!("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+    }
     let mut input = stdin();
     let mut output = stdout();
     let mut err_out = stderr();
     let mut ui = structured_ui::TextUi::new(&mut input, &mut output, &mut err_out);
-    pb_get_impl(&cmd, &mut ui).await
-}
-
-fn deprecated() {
-    println!("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-    println!("@");
-    println!("@  The `get` subcommand is deprecated/renamed");
-    println!("@");
-    println!("@  Please use `ffx product download ...` instead.");
-    println!("@");
-    println!("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+    let client = Client::initial()?;
+    pb_get_impl(
+        &cmd.auth,
+        &cmd.context_uri,
+        cmd.force,
+        cmd.latest,
+        &cmd.out_dir,
+        &cmd.product_name,
+        &cmd.version,
+        &client,
+        &mut ui,
+    )
+    .await
 }
 
 async fn pb_get_impl<I: structured_ui::Interface + Sync>(
-    cmd: &GetCommand,
-    ui: &mut I,
+    auth: &pbms::AuthFlowChoice,
+    context_uri: &str,
+    force: bool,
+    look_in_latest: bool,
+    out_dir: &Path,
+    product_name: &Option<String>,
+    product_version: &Option<String>,
+    client: &Client,
+    ui: &I,
 ) -> Result<()> {
     let start = std::time::Instant::now();
-    deprecated();
-    tracing::info!("---------------------- Begin ----------------------------");
-    tracing::debug!("transfer_manifest_url Url::parse");
-    let transfer_manifest_url = match url::Url::parse(&cmd.manifest_url) {
-        Ok(p) => p,
-        _ => ffx_bail!("The source location must be a URL, failed to parse {:?}", cmd.manifest_url),
+    tracing::info!("------------------ Begin get PBv2 -------------------");
+    // There are three forms of input and each should resolve to a transfer url.
+    let transfer_url = if context_uri.contains(":") && !context_uri.starts_with("pb:") {
+        context_uri.to_string()
+    } else {
+        let (bucket, name, version) = if context_uri.starts_with("pb:") {
+            let mut iter = context_uri.split(":");
+            assert_eq!(iter.next(), Some("pb:"));
+            let bucket = iter.next().expect("pb URI must have three colons").to_string();
+            let name = iter.next().expect("pb URI must have three colons");
+            let version = iter.next().expect("pb URI must have three colons");
+            assert_eq!(iter.next(), None);
+            (bucket, name, version)
+        } else {
+            let name = product_name
+                .as_ref()
+                .expect("when not passing a URI, a <name> is required.")
+                .as_str();
+            let version = product_version.as_ref().unwrap().as_str();
+            (context_uri.to_string(), name, version)
+        };
+
+        let version = if look_in_latest {
+            #[cfg(target_os = "linux")]
+            let latest_url = url::Url::parse(&format!("gs://{}/development/LATEST_LINUX", bucket))?;
+            #[cfg(target_os = "macos")]
+            let latest_url = url::Url::parse(&format!("gs://{}/development/LATEST_MAC", bucket))?;
+            let version = pbms::string_from_url(
+                &latest_url,
+                auth,
+                &|state| {
+                    let mut progress = structured_ui::Progress::builder();
+                    progress.title("Getting product latest version string");
+                    progress.entry(&state.name, state.at, state.of, state.units);
+                    ui.present(&structured_ui::Presentation::Progress(progress))?;
+                    Ok(gcs::client::ProgressResponse::Continue)
+                },
+                ui,
+                client,
+            )
+            .await
+            .context("getting latest version string.")?;
+            let version = version.trim();
+            assert!(version.len() > 0, "latest version string is empty!");
+            version.to_string()
+        } else {
+            version.to_string()
+        };
+
+        tracing::debug!("pb_lookup_impl {} {} {}", bucket, name, version);
+        println!("pb_lookup_impl {} {} {}", bucket, name, version);
+        pb_lookup_impl(
+            &auth,
+            &format!("gs://{}/development/{}", bucket, version),
+            name,
+            &version,
+            ui,
+        )
+        .await?
     };
-    let local_dir = &cmd.out_dir;
-    tracing::debug!("make_way_for_output {:?}", local_dir);
-    make_way_for_output(&local_dir, cmd.force).await?;
 
-    let parent_dir = local_dir.parent().ok_or_else(|| anyhow!("local dir has no parent"))?;
-    let temp_dir = tempfile::TempDir::new_in(&parent_dir)?;
-    let client = Client::initial()?;
-    tracing::debug!("transfer_manifest, transfer_manifest_url {:?}", transfer_manifest_url);
-    transfer_download(
-        &transfer_manifest_url,
-        &temp_dir.path(),
-        &cmd.auth,
-        &|layers| {
-            let mut progress = structured_ui::Progress::builder();
-            progress.title("Transfer download");
-            progress.entry("Transfer manifest", /*at=*/ 1, /*of=*/ 2, "steps");
-            for layer in layers {
-                progress.entry(&layer.name, layer.at, layer.of, layer.units);
-            }
-            ui.present(&structured_ui::Presentation::Progress(progress))?;
-            Ok(ProgressResponse::Continue)
-        },
-        ui,
-        &client,
-    )
-    .await
-    .context("downloading via transfer manifest")?;
+    println!("pb_download_impl {}", transfer_url);
+    pb_download_impl(&auth, force, &transfer_url, out_dir, &client, ui).await?;
 
-    // Workaround for having the product bundle nested in a sub-dir.
-    let extra_dir = temp_dir.path().join("product_bundle");
-    let pb_dir = if extra_dir.exists() { extra_dir } else { temp_dir.path().to_path_buf() };
-    rename(&pb_dir, &local_dir)
-        .await
-        .with_context(|| format!("moving dir {:?} to {:?}", pb_dir, local_dir))?;
-
-    let layers = vec![ProgressState { name: "complete", at: 2, of: 2, units: "steps" }];
-    let mut progress = structured_ui::Progress::builder();
-    progress.title("Transfer download");
-    for layer in layers {
-        progress.entry(&layer.name, layer.at, layer.of, layer.units);
-    }
-    ui.present(&structured_ui::Presentation::Progress(progress))?;
-
-    deprecated();
     tracing::debug!("Total ffx product get runtime {} seconds.", start.elapsed().as_secs_f32());
     tracing::debug!("End");
     Ok(())
@@ -99,21 +134,50 @@ async fn pb_get_impl<I: structured_ui::Interface + Sync>(
 
 #[cfg(test)]
 mod test {
-    use super::*;
-    use tempfile;
+    use {
+        super::*,
+        fuchsia_hyper_test_support::{
+            handler::{ForPath, StaticResponse},
+            TestServer,
+        },
+        tempfile,
+    };
 
-    #[ignore]
-    #[should_panic(expected = "downloading via transfer manifest")]
     #[fuchsia_async::run_singlethreaded(test)]
     async fn test_pb_get_impl() {
         let test_dir = tempfile::TempDir::new().expect("temp dir");
-        let cmd = GetCommand {
-            auth: pbms::AuthFlowChoice::Default,
-            force: false,
-            out_dir: test_dir.path().to_path_buf(),
-            manifest_url: "gs://example/fake/transfer.json".to_string(),
-        };
-        let mut ui = structured_ui::MockUi::new();
-        pb_get_impl(&cmd, &mut ui).await.expect("testing get");
+        let server = TestServer::builder()
+            .handler(ForPath::new(
+                "/example/fake/transfer.json",
+                StaticResponse::ok_body(
+                    r#"
+            {
+                "version": "1",
+                "entries": [{
+                    "type": "files",
+                    "local": "foo",
+                    "remote": "data",
+                    "entries": [{ "name": "payload.txt"}]
+                }]
+            }"#,
+                ),
+            ))
+            .handler(ForPath::new("/api/b/example/o", StaticResponse::ok_body(r#"{}"#)))
+            .start()
+            .await;
+        let auth = pbms::AuthFlowChoice::Default;
+        let force = false;
+        let latest = false;
+        let manifest_url = "gs://example/fake/transfer.json".to_string();
+        let out_dir = test_dir.path().join("download");
+        let client = Client::initial_with_urls(
+            &server.local_url_for_path("api"),
+            &server.local_url_for_path("storage"),
+        )
+        .expect("creating client");
+        let ui = structured_ui::MockUi::new();
+        pb_get_impl(&auth, &manifest_url, force, latest, &out_dir, &None, &None, &client, &ui)
+            .await
+            .expect("testing download");
     }
 }
