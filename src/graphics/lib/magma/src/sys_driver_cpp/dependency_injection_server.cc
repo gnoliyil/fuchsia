1d937a9404496678e2c836e85c59f39a44eaccb3
John Bauman <jbauman@google.com>
2023-05-11 17:50:50 +0000

    [msd-arm-mali][dfv2] Implement fuchsia.gpu.magma/DependencyInjection
    Bug: 126333
    Change-Id: I77cf3d88292832016f631eefec904568c818a257
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/853996
    Fuchsia-Auto-Submit: John Bauman <jbauman@google.com>
    Reviewed-by: Craig Stout <cstout@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

---
+++ src/graphics/lib/magma/src/sys_driver_cpp/dependency_injection_server.cc
@@ -0,0 +1,75 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "dependency_injection_server.h"
+
+zx::result<> DependencyInjectionServer::Create(
+    fidl::WireSyncClient<fuchsia_driver_framework::Node>& node_client) {
+  fidl::Arena arena;
+  zx::result connector = devfs_connector_.Bind(fdf::Dispatcher::GetCurrent()->async_dispatcher());
+  if (connector.is_error()) {
+    return connector.take_error();
+  }
+
+  auto devfs = fuchsia_driver_framework::wire::DevfsAddArgs::Builder(arena)
+                   .connector(std::move(connector.value()))
+                   .class_name("gpu-dependency-injection");
+
+  auto args = fuchsia_driver_framework::wire::NodeAddArgs::Builder(arena)
+                  .name(arena, "gpu-dependency-injection")
+                  .devfs_args(devfs.Build())
+                  .Build();
+
+  zx::result controller_endpoints =
+      fidl::CreateEndpoints<fuchsia_driver_framework::NodeController>();
+  ZX_ASSERT_MSG(controller_endpoints.is_ok(), "Failed: %s", controller_endpoints.status_string());
+  zx::result node_endpoints = fidl::CreateEndpoints<fuchsia_driver_framework::Node>();
+  ZX_ASSERT_MSG(node_endpoints.is_ok(), "Failed: %s", node_endpoints.status_string());
+
+  fidl::WireResult result = node_client->AddChild(args, std::move(controller_endpoints->server),
+                                                  std::move(node_endpoints->server));
+  node_controller_.Bind(std::move(controller_endpoints->client));
+  node_.Bind(std::move(node_endpoints->client));
+  return zx::ok();
+}
+
+void DependencyInjectionServer::SetMemoryPressureProvider(
+    fuchsia_gpu_magma::wire::DependencyInjectionSetMemoryPressureProviderRequest* request,
+    SetMemoryPressureProviderCompleter::Sync& completer) {
+  if (pressure_server_) {
+    return;
+  }
+  auto endpoints = fidl::CreateEndpoints<fuchsia_memorypressure::Watcher>();
+  if (!endpoints.is_ok()) {
+    MAGMA_LOG(WARNING, "Failed to create fidl Endpoints");
+    return;
+  }
+  pressure_server_ = fidl::BindServer(fdf::Dispatcher::GetCurrent()->async_dispatcher(),
+                                      std::move(endpoints->server), this);
+
+  fidl::WireSyncClient provider{std::move(request->provider)};
+  // TODO(fxbug.dev/97955) Consider handling the error instead of ignoring it.
+  (void)provider->RegisterWatcher(std::move(endpoints->client));
+}
+
+void DependencyInjectionServer::OnLevelChanged(OnLevelChangedRequestView request,
+                                               OnLevelChangedCompleter::Sync& completer) {
+  owner_->SetMemoryPressureLevel(GetMagmaLevel(request->level));
+  completer.Reply();
+}
+
+// static
+MagmaMemoryPressureLevel DependencyInjectionServer::GetMagmaLevel(
+    fuchsia_memorypressure::wire::Level level) {
+  switch (level) {
+    case fuchsia_memorypressure::wire::Level::kNormal:
+      return MAGMA_MEMORY_PRESSURE_LEVEL_NORMAL;
+    case fuchsia_memorypressure::wire::Level::kWarning:
+      return MAGMA_MEMORY_PRESSURE_LEVEL_WARNING;
+    case fuchsia_memorypressure::wire::Level::kCritical:
+      return MAGMA_MEMORY_PRESSURE_LEVEL_CRITICAL;
+    default:
+      return MAGMA_MEMORY_PRESSURE_LEVEL_NORMAL;
+  }
+}
---
+++ src/graphics/lib/magma/src/sys_driver_cpp/dependency_injection_server.h
@@ -0,0 +1,53 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#ifndef SRC_GRAPHICS_LIB_MAGMA_SRC_SYS_DRIVER_CPP_DEPENDENCY_INJECTION_SERVER_H_
+#define SRC_GRAPHICS_LIB_MAGMA_SRC_SYS_DRIVER_CPP_DEPENDENCY_INJECTION_SERVER_H_
+
+#include <fidl/fuchsia.driver.framework/cpp/fidl.h>
+#include <fidl/fuchsia.gpu.magma/cpp/wire.h>
+#include <fidl/fuchsia.memorypressure/cpp/wire.h>
+#include <lib/driver/devfs/cpp/connector.h>
+#include <lib/fdf/cpp/dispatcher.h>
+
+#include "magma_util/macros.h"
+#include "src/graphics/lib/magma/include/msd/msd_defs.h"
+
+class DependencyInjectionServer : public fidl::WireServer<fuchsia_gpu_magma::DependencyInjection>,
+                                  public fidl::WireServer<fuchsia_memorypressure::Watcher> {
+ public:
+  class Owner {
+   public:
+    virtual void SetMemoryPressureLevel(MagmaMemoryPressureLevel level) = 0;
+  };
+
+  explicit DependencyInjectionServer(Owner* owner)
+      : owner_(owner),
+        devfs_connector_(fit::bind_member<&DependencyInjectionServer::BindConnector>(this)) {}
+
+  zx::result<> Create(fidl::WireSyncClient<fuchsia_driver_framework::Node>& node_client);
+
+  // fuchsia:gpu::magma::DependencyInjection implementation.
+  void SetMemoryPressureProvider(
+      fuchsia_gpu_magma::wire::DependencyInjectionSetMemoryPressureProviderRequest* request,
+      SetMemoryPressureProviderCompleter::Sync& completer) override;
+
+  // fuchsia::memorypressure::Watcher implementation.
+  void OnLevelChanged(OnLevelChangedRequestView request,
+                      OnLevelChangedCompleter::Sync& completer) override;
+
+ private:
+  void BindConnector(fidl::ServerEnd<fuchsia_gpu_magma::DependencyInjection> server) {
+    fidl::BindServer(fdf::Dispatcher::GetCurrent()->async_dispatcher(), std::move(server), this);
+  }
+
+  static MagmaMemoryPressureLevel GetMagmaLevel(fuchsia_memorypressure::wire::Level level);
+
+  Owner* owner_;
+  driver_devfs::Connector<fuchsia_gpu_magma::DependencyInjection> devfs_connector_;
+  fidl::WireSyncClient<fuchsia_driver_framework::Node> node_;
+  fidl::WireSyncClient<fuchsia_driver_framework::NodeController> node_controller_;
+  std::optional<fidl::ServerBindingRef<fuchsia_memorypressure::Watcher>> pressure_server_;
+};
+
+#endif  // SRC_GRAPHICS_LIB_MAGMA_SRC_SYS_DRIVER_CPP_DEPENDENCY_INJECTION_SERVER_H_
