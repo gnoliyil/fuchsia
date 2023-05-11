54ac1c61d45223c06193ce1fac24e330c15972bb
John Bauman <jbauman@google.com>
2023-05-11 15:44:10 +0000

    [msd-arm-mali][dfv2] Add performance counter support
    Bug: 126333
    Change-Id: Icda483d592c7f8f8982f9c8d80a8dcd95ccb7f55
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/853125
    Fuchsia-Auto-Submit: John Bauman <jbauman@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Craig Stout <cstout@google.com>

---
+++ src/graphics/lib/magma/src/sys_driver_cpp/performance_counters_server.h
@@ -0,0 +1,37 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#ifndef SRC_GRAPHICS_LIB_MAGMA_SRC_SYS_DRIVER_CPP_PERFORMANCE_COUNTERS_SERVER_H_
+#define SRC_GRAPHICS_LIB_MAGMA_SRC_SYS_DRIVER_CPP_PERFORMANCE_COUNTERS_SERVER_H_
+
+#include <fidl/fuchsia.driver.framework/cpp/fidl.h>
+#include <fidl/fuchsia.gpu.magma/cpp/wire.h>
+#include <lib/driver/devfs/cpp/connector.h>
+#include <lib/driver/runtime/include/lib/fdf/cpp/dispatcher.h>
+
+#include "magma_util/macros.h"
+
+class PerformanceCountersServer
+    : public fidl::WireServer<fuchsia_gpu_magma::PerformanceCounterAccess> {
+ public:
+  PerformanceCountersServer()
+      : devfs_connector_(fit::bind_member<&PerformanceCountersServer::BindConnector>(this)) {}
+
+  zx::result<> Create(fidl::WireSyncClient<fuchsia_driver_framework::Node>& node_client);
+
+  zx_koid_t GetEventKoid();
+
+ private:
+  void BindConnector(fidl::ServerEnd<fuchsia_gpu_magma::PerformanceCounterAccess> server) {
+    fidl::BindServer(fdf::Dispatcher::GetCurrent()->async_dispatcher(), std::move(server), this);
+  }
+
+  void GetPerformanceCountToken(GetPerformanceCountTokenCompleter::Sync& completer) override;
+
+  zx::event event_;
+  driver_devfs::Connector<fuchsia_gpu_magma::PerformanceCounterAccess> devfs_connector_;
+  fidl::WireSyncClient<fuchsia_driver_framework::Node> node_;
+  fidl::WireSyncClient<fuchsia_driver_framework::NodeController> node_controller_;
+};
+
+#endif  // SRC_GRAPHICS_LIB_MAGMA_SRC_SYS_DRIVER_CPP_PERFORMANCE_COUNTERS_SERVER_H_
