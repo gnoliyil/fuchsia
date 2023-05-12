2e68d49b36eb9b522528017c08b1525656f002a8
Brian Bosak <bbosak@google.com>
2023-05-12 18:02:03 +0000

    [ffx] Migrate ffx component run to direct mode logging
    This CL migrates ffx component run to direct logging
    
    Bug: 121413
    Change-Id: Idc62acaa244d19bb5b2cd359a3c9bbc706086aab
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/855616
    Reviewed-by: Miguel Flores <miguelfrde@google.com>
    Reviewed-by: Steven Grady <slgrady@google.com>
    Commit-Queue: Brian Bosak <bbosak@google.com>

--- src/developer/ffx/plugins/component/run/src/lib.rs
+++ src/developer/ffx/plugins/component/run/src/lib.rs
@@ -4,22 +4,23 @@
 
 use anyhow::Result;
 use component_debug::cli::run_cmd;
-use errors::FfxError;
 use ffx_component::rcs::connect_to_lifecycle_controller;
 use ffx_component_run_args::RunComponentCommand;
 use ffx_core::ffx_plugin;
-use ffx_log::{log_impl, LogOpts};
+use ffx_log::{error::LogError, log_impl, LogOpts};
 use ffx_log_args::LogCommand;
-use fidl_fuchsia_developer_ffx::DiagnosticsProxy;
+use ffx_log_frontend::RemoteDiagnosticsBridgeProxyWrapper;
+use fidl_fuchsia_developer_ffx::TargetCollectionProxy;
 use fidl_fuchsia_developer_remotecontrol as rc;
+use std::sync::Arc;
 
-#[ffx_plugin(DiagnosticsProxy = "daemon::protocol")]
-pub async fn cmd(
-    diagnostics_proxy: DiagnosticsProxy,
+async fn cmd_impl(
+    target_collection_proxy: TargetCollectionProxy,
     rcs_proxy: rc::RemoteControlProxy,
     args: RunComponentCommand,
-) -> Result<()> {
+) -> Result<(), LogError> {
     let lifecycle_controller = connect_to_lifecycle_controller(&rcs_proxy).await?;
+    let host = rcs_proxy.identify_host().await??;
 
     // All errors from component_debug library are user-visible.
     run_cmd(
@@ -30,15 +31,17 @@ pub async fn cmd(
         lifecycle_controller,
         std::io::stdout(),
     )
-    .await
-    .map_err(|e| FfxError::Error(e, 1))?;
+    .await?;
 
     if args.follow_logs {
         let log_filter = args.moniker.to_string().strip_prefix("/").unwrap().to_string();
         let log_cmd = LogCommand { filter: vec![log_filter], ..LogCommand::default() };
 
         log_impl(
-            diagnostics_proxy,
+            Arc::new(RemoteDiagnosticsBridgeProxyWrapper::new(
+                target_collection_proxy,
+                host.nodename.ok_or(LogError::NoHostname)?,
+            )),
             Some(rcs_proxy),
             &None,
             log_cmd,
@@ -47,6 +50,15 @@ pub async fn cmd(
         )
         .await?;
     }
+    Ok(())
+}
 
+#[ffx_plugin(TargetCollectionProxy = "daemon::protocol")]
+pub async fn cmd(
+    target_collection_proxy: TargetCollectionProxy,
+    rcs_proxy: rc::RemoteControlProxy,
+    args: RunComponentCommand,
+) -> Result<()> {
+    cmd_impl(target_collection_proxy, rcs_proxy, args).await?;
     Ok(())
 }
--- src/developer/ffx/plugins/log/src/lib.rs
+++ src/developer/ffx/plugins/log/src/lib.rs
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-mod error;
+pub mod error;
 
 use anyhow::{anyhow, Context, Error, Result};
 use async_trait::async_trait;
