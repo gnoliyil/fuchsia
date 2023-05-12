2acc1102d41dc0309e187c2c560432cf75c422a4
camlloyd <camlloyd@google.com>
2023-05-12 16:48:24 +0000

    [ffx][audio] Add tests for ListDevices Command
    Test:  fx test host_x64/ffx_audio_listdevices_lib_test
    
    Change-Id: If673eef7da4a3927fc61fb8a1416294a43cfeea1
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/855436
    Commit-Queue: Cam Lloyd <camlloyd@google.com>
    Reviewed-by: Andres Oportus <andresoportus@google.com>

--- src/developer/ffx/plugins/audio/list-devices/src/lib.rs
+++ src/developer/ffx/plugins/audio/list-devices/src/lib.rs
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use fho::FfxContext;
-
 use {
     anyhow::{Context, Result},
     async_trait::async_trait,
@@ -47,59 +45,154 @@ fho::embedded_plugin!(ListDevicesTool);
 #[async_trait(?Send)]
 impl FfxMain for ListDevicesTool {
     type Writer = MachineWriter<ListDeviceResult>;
-    async fn main(self, mut writer: Self::Writer) -> fho::Result<()> {
-        let response = self
-            .audio_proxy
-            .list_devices()
-            .await
-            .context("List devices failed")?
-            .map_err(Status::from_raw)
-            .context("Error from daemon for list devices request")?;
+    async fn main(self, writer: Self::Writer) -> fho::Result<()> {
+        list_devices_impl(self.audio_proxy, writer).await.map_err(Into::into)
+    }
+}
 
-        if let Some(devices) = response.devices {
-            writer
-                .machine_or_else(
-                    &ListDeviceResult {
-                        devices: devices
-                            .clone()
-                            .into_iter()
-                            .map(|device| DeviceSelectorWrapper {
-                                device_id: device.id.clone(),
-                                device_type: DeviceTypeWrapper::STREAMCONFIG,
-                                is_input: device.is_input,
-                                path: format_utils::path_for_selector(&device)
-                                    .unwrap_or(format!("Path not available")),
-                            })
-                            .collect(),
-                    },
-                    || {
-                        devices
-                            .iter()
-                            .map(|device| {
-                                let in_out = match device.is_input {
-                                    Some(is_input) => {
-                                        if is_input {
-                                            format!("Input")
-                                        } else {
-                                            format!("Output")
-                                        }
+async fn list_devices_impl(
+    audio_proxy: AudioDaemonProxy,
+    mut writer: MachineWriter<ListDeviceResult>,
+) -> Result<(), anyhow::Error> {
+    let response = audio_proxy
+        .list_devices()
+        .await
+        .context("List devices failed")?
+        .map_err(Status::from_raw)
+        .context("Error from daemon for list devices request")?;
+    if let Some(devices) = response.devices {
+        writer
+            .machine_or_else(
+                &ListDeviceResult {
+                    devices: devices
+                        .clone()
+                        .into_iter()
+                        .map(|device| DeviceSelectorWrapper {
+                            device_id: device.id.clone(),
+                            device_type: DeviceTypeWrapper::STREAMCONFIG,
+                            is_input: device.is_input,
+                            path: format_utils::path_for_selector(&device)
+                                .unwrap_or(format!("Path not available")),
+                        })
+                        .collect(),
+                },
+                || {
+                    devices
+                        .iter()
+                        .map(|device| {
+                            let in_out = match device.is_input {
+                                Some(is_input) => {
+                                    if is_input {
+                                        format!("Input")
+                                    } else {
+                                        format!("Output")
                                     }
-                                    None => format!("Input/Output not specified"),
-                                };
+                                }
+                                None => format!("Input/Output not specified"),
+                            };
 
-                                format!(
-                                    "{:?} Device id: {:?}, Device type: {:?}, {in_out}",
-                                    format_utils::path_for_selector(&device),
-                                    device.id,
-                                    device.device_type
-                                )
-                            })
-                            .join("\n")
-                    },
-                )
-                .map_err(Into::into)
-        } else {
-            writeln!(writer, "No devices found.").bug()
-        }
+                            format!(
+                                "{:?} Device id: {:?}, Device type: {:?}, {in_out}",
+                                format_utils::path_for_selector(&device),
+                                device.id,
+                                device.device_type
+                            )
+                        })
+                        .join("\n")
+                },
+            )
+            .map_err(Into::into)
+    } else {
+        writeln!(writer, "No devices found.").map_err(Into::into)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ffx_writer::{Format, TestBuffers};
+    use fidl_fuchsia_audio_ffxdaemon::{
+        AudioDaemonListDevicesResponse, AudioDaemonProxy, AudioDaemonRequest, DeviceSelector,
+    };
+    use fidl_fuchsia_virtualaudio::DeviceType;
+
+    fn fake_audio_daemon() -> AudioDaemonProxy {
+        let devices = vec![
+            DeviceSelector {
+                is_input: Some(true),
+                id: Some("abc123".to_string()),
+                device_type: Some(DeviceType::StreamConfig),
+                ..Default::default()
+            },
+            DeviceSelector {
+                is_input: Some(false),
+                id: Some("abc123".to_string()),
+                device_type: Some(DeviceType::StreamConfig),
+                ..Default::default()
+            },
+        ];
+        let callback = move |req| match req {
+            AudioDaemonRequest::ListDevices { responder, .. } => {
+                let response = AudioDaemonListDevicesResponse {
+                    devices: Some(devices.clone()),
+                    ..Default::default()
+                };
+                responder.send(&mut Ok(response)).unwrap();
+            }
+            _ => {}
+        };
+        fho::testing::fake_proxy(callback)
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    pub async fn test_list_devices() -> Result<(), fho::Error> {
+        let audio_daemon = fake_audio_daemon();
+        let test_buffers = TestBuffers::default();
+        let writer: MachineWriter<ListDeviceResult> = MachineWriter::new_test(None, &test_buffers);
+        let result = list_devices_impl(audio_daemon, writer).await;
+        result.unwrap();
+
+        let stdout = test_buffers.into_stdout_str();
+        let stdout_expected = format!(
+            "Ok(\"/dev/class/audio-input/abc123\") Device id: Some(\"abc123\"), Device type: Some(StreamConfig), Input\n\
+            Ok(\"/dev/class/audio-output/abc123\") Device id: Some(\"abc123\"), Device type: Some(StreamConfig), Output\n");
+
+        assert_eq!(stdout, stdout_expected);
+        Ok(())
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    pub async fn test_list_devices_machine() -> Result<(), fho::Error> {
+        let audio_daemon = fake_audio_daemon();
+        let test_buffers = TestBuffers::default();
+        let writer: MachineWriter<ListDeviceResult> =
+            MachineWriter::new_test(Some(Format::Json), &test_buffers);
+        let result = list_devices_impl(audio_daemon, writer).await;
+        result.unwrap();
+
+        let stdout = test_buffers.into_stdout_str();
+        let stdout_content = format!(
+            "\
+        {{\
+            \"devices\":[\
+                {{\
+                    \"device_id\":\"abc123\",\
+                    \"is_input\":true,\
+                    \"device_type\":\"STREAMCONFIG\",\
+                    \"path\":\"/dev/class/audio-input/abc123\"\
+                }},\
+                {{\
+                    \"device_id\":\"abc123\",\
+                    \"is_input\":false,\
+                    \"device_type\":\"STREAMCONFIG\",\
+                    \"path\":\"/dev/class/audio-output/abc123\"\
+                }}\
+            ]\
+        }}"
+        );
+
+        let stdout_expected = format!("{}\n", stdout_content);
+        assert_eq!(stdout, stdout_expected);
+        Ok(())
     }
 }
