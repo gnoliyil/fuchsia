20d2e466ca9d47d2f22e35861e741e0b679ffa6e
Jeff Martin <martinjeffrey@google.com>
2023-05-12 19:56:56 +0000

    [netlink] Support specifying multicast groups
    Introduce a pair of clients: the external client will be held by
    Starnix, and the internal client will be held by the Netlink async
    worker. A client corresponds one to one with a Netlink socket.
    
    Allow the external client to specify the multicast group memberships,
    and allow the internal client to observe the current memberships.
    
    Bug: https://issuetracker.google.com/280483454
    
    Change-Id: I15a37956e4f4c3c34ae91b5f775e222ebb9aee3e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/853196
    Reviewed-by: Adam Perry <adamperry@google.com>
    Reviewed-by: Antonio Linhart <antoniolinhart@google.com>
    Commit-Queue: Jeff Martin <martinjeffrey@google.com>
    Reviewed-by: Ghanan Gowripalan <ghanan@google.com>

--- src/connectivity/network/netlink/src/lib.rs
+++ src/connectivity/network/netlink/src/lib.rs
@@ -9,14 +9,27 @@
 //! protocol families each offering different functionality. This crate targets
 //! the implementation of families related to networking.
 
-use futures::future::Future;
+#![deny(unused, missing_docs)]
+
+mod client;
+mod multicast_groups;
+pub mod protocol_family;
+
+use futures::{
+    channel::mpsc::{self, UnboundedReceiver, UnboundedSender},
+    future::Future,
+    StreamExt as _,
+};
+
+use crate::{
+    client::InternalClient,
+    protocol_family::route::{NetlinkRoute, NetlinkRouteClient},
+};
 
 /// The implementation of the Netlink protocol suite.
 pub struct Netlink {
-    /// A temporary private field to ensure that `Netlink` cannot be member
-    /// initialized by external users. This can be removed once the struct has
-    /// actual private fields.
-    _private: (),
+    /// Sender to attach new `NETLINK_ROUTE` clients to the event loop.
+    route_client_sender: UnboundedSender<InternalClient<NetlinkRoute>>,
 }
 
 impl Netlink {
@@ -25,17 +38,45 @@ impl Netlink {
     /// Callers are responsible for polling the event loop, which drives
     /// the Netlink implementation's asynchronous work. The event loop will
     /// never complete.
-    pub fn new() -> (Self, impl Future<Output = ()>) {
-        (Netlink { _private: () }, run_event_loop())
+    pub fn new() -> (Self, impl Future<Output = ()> + Send) {
+        let (route_client_sender, route_client_receiver) = mpsc::unbounded();
+        (Netlink { route_client_sender }, run_event_loop(EventLoopParams { route_client_receiver }))
     }
+
+    /// Creates a new client of the `NETLINK_ROUTE` protocol family.
+    pub fn new_route_client(&self) -> Result<NetlinkRouteClient, NewClientError> {
+        let (external_client, internal_client) = client::new_client_pair::<NetlinkRoute>();
+        self.route_client_sender.unbounded_send(internal_client).map_err(|e| {
+            // Sending on an `UnboundedSender` can never fail with `is_full()`.
+            debug_assert!(e.is_disconnected());
+            NewClientError::Disconnected
+        })?;
+        Ok(NetlinkRouteClient(external_client))
+    }
+}
+
+/// The possible error types when instantiating a new client.
+pub enum NewClientError {
+    /// The [`Netlink`] is disconnected from its associated event loop, perhaps
+    /// as a result of dropping the event loop.
+    Disconnected,
+}
+
+/// Parameters used to start the event loop.
+struct EventLoopParams {
+    /// Receiver of newly created `NETLINK_ROUTE` clients.
+    route_client_receiver: UnboundedReceiver<InternalClient<NetlinkRoute>>,
 }
 
 /// The event loop encompassing all asynchronous Netlink work.
 ///
 /// The event loop is never expected to complete.
-async fn run_event_loop() {
-    // Temporary to prevent the event loop from terminating.
-    futures::future::pending::<()>().await;
+async fn run_event_loop(params: EventLoopParams) {
+    let EventLoopParams { route_client_receiver } = params;
+
+    // TODO(https://issuetracker.google.com/280483454): Notify route clients of
+    // multicast group events.
+    let _route_clients = route_client_receiver.collect::<Vec<_>>().await;
 }
 
 #[cfg(test)]
@@ -47,6 +88,7 @@ mod tests {
     // Placeholder test to ensure the build targets are setup properly.
     #[test]
     fn test_event_loop() {
-        assert_eq!(run_event_loop().now_or_never(), None);
+        let (_route_client_sender, route_client_receiver) = mpsc::unbounded();
+        assert_eq!(run_event_loop(EventLoopParams { route_client_receiver }).now_or_never(), None);
     }
 }
---
+++ src/connectivity/network/netlink/src/multicast_groups.rs
@@ -0,0 +1,323 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//! A module for managing Netlink multicast group memberships.
+//!
+//! A Netlink socket can subscribe to any number of multicast groups, as defined
+//! by the Netlink protocol family that the socket is connected to. There are
+//! two modes of specifying the multicast group memberships. Mode 1 is referred
+//! to as "legacy" throughout this module because it was replaced by mode 2,
+//! referred to as "modern", in Linux 2.6.14.
+//!     Mode 1: Specifying `nl_groups`, a 32 bit bitmask, when binding the
+//!             the socket.
+//!     Mode 2: Setting the `NETLINK_ADD_MEMBERSHIP` or
+//!            `NETLINK_DROP_MEMBERSHIP` socket option.
+//!
+//! Note that both mode 1 and mode 2 are supported (for backwards
+//! compatibility), and the two modes operate over different sets of constants.
+//! The underlying memberships are tracked independently of the mode via which
+//! they were set. For example, a `NETLINK_ROUTE` client could bind to
+//! `RTMGRP_IPV6_IFADDR` (256), to start receiving IPv6 address events, and
+//! later set the `NETLINK_DROP_MEMBERSHIP` socket option to
+//! `RTNLGRP_IPV6_IFADDR` (9), to stop receiving events.
+
+use std::marker::PhantomData;
+
+use bit_set::BitSet;
+
+/// A modern (non-legacy) multicast group. Interpreted as a single group.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct ModernGroup(pub u32);
+
+impl Into<usize> for ModernGroup {
+    fn into(self) -> usize {
+        let ModernGroup(group) = self;
+        group.try_into().expect("expected usize >= u32")
+    }
+}
+
+/// A set of legacy multicast groups. Interpreted as a bit mask, where each set
+/// bit corresponds to a different group membership.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct LegacyGroups(pub u32);
+
+/// A single legacy multicast group membership. At most 1 bit is set.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub(crate) struct SingleLegacyGroup(u32);
+
+impl SingleLegacyGroup {
+    pub(crate) fn inner(&self) -> u32 {
+        let SingleLegacyGroup(inner) = self;
+        *inner
+    }
+}
+
+/// Error returned when attempting to convert a u32 into [`SingleLegacyGroup`].
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub(crate) struct MultipleBitsSetError;
+
+impl TryFrom<u32> for SingleLegacyGroup {
+    type Error = MultipleBitsSetError;
+    fn try_from(value: u32) -> Result<Self, Self::Error> {
+        (value.count_ones() <= 1).then_some(SingleLegacyGroup(value)).ok_or(MultipleBitsSetError)
+    }
+}
+
+/// Multicast group semantics that are specific to a particular protocol family.
+pub(crate) trait MulticastCapableNetlinkFamily {
+    /// Translate the given legacy group membership to a modern membership.
+    ///
+    /// Returns `None` if the given legacy_group does not exist in this family.
+    fn legacy_to_modern(legacy_group: SingleLegacyGroup) -> Option<ModernGroup>;
+
+    /// Returns true if the given [`ModernGroup`] is a valid multicast group.
+    fn is_valid_group(group: &ModernGroup) -> bool;
+}
+
+/// Manages the current multicast group memberships of a single connection to
+/// Netlink.
+///
+/// Memberships are stored entirely using the modern set of constants. Legacy
+/// memberships are translated to their modern equivalent before being stored.
+#[derive(Debug)]
+pub(crate) struct MulticastGroupMemberships<F: MulticastCapableNetlinkFamily> {
+    /// Aspects of multicast group memberships that are family specific.
+    family: PhantomData<F>,
+
+    // The current multicast group memberships, stored as modern memberships.
+    // Membership in multicast group "N" is determined by whether the "Nth" bit
+    // of the `BitSet` is set.
+    memberships: BitSet,
+}
+
+/// Error returned when attempting to join an invalid [`ModernGroup`].
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct InvalidModernGroupError;
+
+/// Error returned when attempting to join invalid [`LegacyGroups`].
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct InvalidLegacyGroupsError;
+
+impl<F: MulticastCapableNetlinkFamily> MulticastGroupMemberships<F> {
+    /// Instantiate a new [`MulticastGroupMemberships`].
+    pub(crate) fn new() -> MulticastGroupMemberships<F> {
+        MulticastGroupMemberships { family: PhantomData, memberships: Default::default() }
+    }
+
+    /// Returns `True` if `self` is a member of `group`.
+    pub(crate) fn member_of_group(&self, group: ModernGroup) -> bool {
+        self.memberships.contains(group.into())
+    }
+
+    /// Adds the given multicast group membership.
+    pub(crate) fn add_membership(
+        &mut self,
+        group: ModernGroup,
+    ) -> Result<(), InvalidModernGroupError> {
+        let MulticastGroupMemberships { family: _, memberships } = self;
+        if !F::is_valid_group(&group) {
+            return Err(InvalidModernGroupError);
+        }
+        let _was_absent: bool = memberships.insert(group.into());
+        return Ok(());
+    }
+
+    /// Deletes the given multicast group membership.
+    pub(crate) fn del_membership(
+        &mut self,
+        group: ModernGroup,
+    ) -> Result<(), InvalidModernGroupError> {
+        let MulticastGroupMemberships { family: _, memberships } = self;
+        if !F::is_valid_group(&group) {
+            return Err(InvalidModernGroupError);
+        }
+        let _was_present: bool = memberships.remove(group.into());
+        return Ok(());
+    }
+
+    /// Sets the legacy multicast group memberships.
+    ///
+    /// Legacy memberships are translated into their modern equivalent before
+    /// being written.
+    pub(crate) fn set_legacy_memberships(
+        &mut self,
+        LegacyGroups(requested_groups): LegacyGroups,
+    ) -> Result<(), InvalidLegacyGroupsError> {
+        let MulticastGroupMemberships { family: _, memberships } = self;
+        // Safe "as" conversion because u32::BITS (32) will fit into any usize.
+        const U32_BITS_USIZE: usize = u32::BITS as usize;
+        #[derive(Clone, Copy)]
+        enum Mutation {
+            None,
+            Add(ModernGroup),
+            Del(ModernGroup),
+        }
+        let mut mutations = [Mutation::None; U32_BITS_USIZE];
+        // Validate and record all the mutations that will need to be applied.
+        for i in 0..U32_BITS_USIZE {
+            let raw_legacy_group = 1 << i;
+            let legacy_group = raw_legacy_group
+                .try_into()
+                .expect("raw_legacy_group unexpectedly had multiple bits set");
+            let modern_group = F::legacy_to_modern(legacy_group);
+            let is_member_of_group = requested_groups & raw_legacy_group != 0;
+            mutations[i] = match (modern_group, is_member_of_group) {
+                (Some(modern_group), true) => Mutation::Add(modern_group),
+                (Some(modern_group), false) => Mutation::Del(modern_group),
+                (None, true) => return Err(InvalidLegacyGroupsError),
+                (None, false) => Mutation::None,
+            };
+        }
+        // Apply all of the mutations.
+        for mutation in mutations {
+            match mutation {
+                Mutation::None => {}
+                Mutation::Add(group) => {
+                    let _was_absent: bool = memberships.insert(group.into());
+                }
+                Mutation::Del(group) => {
+                    let _was_present: bool = memberships.remove(group.into());
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::protocol_family::testutil::{
+        FakeProtocolFamily, INVALID_LEGACY_GROUP, INVALID_MODERN_GROUP, LEGACY_GROUP1,
+        LEGACY_GROUP2, LEGACY_GROUP3, MODERN_GROUP1, MODERN_GROUP2, MODERN_GROUP3,
+    };
+
+    #[test]
+    fn test_single_legacy_groups() {
+        assert_eq!(0.try_into(), Ok(SingleLegacyGroup(0)));
+        assert_eq!(0x00010000.try_into(), Ok(SingleLegacyGroup(0x00010000)));
+        assert_eq!(
+            <u32 as TryInto<SingleLegacyGroup>>::try_into(0x00010100),
+            Err(MultipleBitsSetError {})
+        );
+    }
+
+    #[test]
+    fn test_add_del_membership() {
+        let mut memberships = MulticastGroupMemberships::<FakeProtocolFamily>::new();
+
+        assert!(!memberships.member_of_group(MODERN_GROUP1));
+        assert!(!memberships.member_of_group(MODERN_GROUP2));
+        assert!(!memberships.member_of_group(MODERN_GROUP3));
+
+        // Add one membership, and verify the others are unaffected.
+        memberships.add_membership(MODERN_GROUP1).expect("failed to add");
+        assert!(memberships.member_of_group(MODERN_GROUP1));
+        assert!(!memberships.member_of_group(MODERN_GROUP2));
+        assert!(!memberships.member_of_group(MODERN_GROUP3));
+        // Add a second & third membership.
+        memberships.add_membership(MODERN_GROUP2).expect("failed to add");
+        memberships.add_membership(MODERN_GROUP3).expect("failed to add");
+        assert!(memberships.member_of_group(MODERN_GROUP1));
+        assert!(memberships.member_of_group(MODERN_GROUP2));
+        assert!(memberships.member_of_group(MODERN_GROUP3));
+        // Remove one membership, and verify the others are unaffected.
+        memberships.del_membership(MODERN_GROUP1).expect("failed to del");
+        assert!(!memberships.member_of_group(MODERN_GROUP1));
+        assert!(memberships.member_of_group(MODERN_GROUP2));
+        assert!(memberships.member_of_group(MODERN_GROUP3));
+        // Remove the second & third membership.
+        memberships.del_membership(MODERN_GROUP2).expect("failed to del");
+        memberships.del_membership(MODERN_GROUP3).expect("failed to del");
+        assert!(!memberships.member_of_group(MODERN_GROUP1));
+        assert!(!memberships.member_of_group(MODERN_GROUP2));
+        assert!(!memberships.member_of_group(MODERN_GROUP3));
+        // Verify Adding/Deleting an invalid group fails.
+        assert_eq!(
+            memberships.add_membership(INVALID_MODERN_GROUP),
+            Err(InvalidModernGroupError {})
+        );
+        assert_eq!(
+            memberships.del_membership(INVALID_MODERN_GROUP),
+            Err(InvalidModernGroupError {})
+        );
+    }
+
+    #[test]
+    fn test_legacy_memberships() {
+        let mut memberships = MulticastGroupMemberships::<FakeProtocolFamily>::new();
+
+        assert!(!memberships.member_of_group(MODERN_GROUP1));
+        assert!(!memberships.member_of_group(MODERN_GROUP2));
+        assert!(!memberships.member_of_group(MODERN_GROUP3));
+
+        // Add one membership and verify the others are unaffected.
+        memberships
+            .set_legacy_memberships(LegacyGroups(LEGACY_GROUP1))
+            .expect("failed to set legacy groups");
+        assert!(memberships.member_of_group(MODERN_GROUP1));
+        assert!(!memberships.member_of_group(MODERN_GROUP2));
+        assert!(!memberships.member_of_group(MODERN_GROUP3));
+        // Add a second & third membership.
+        memberships
+            .set_legacy_memberships(LegacyGroups(LEGACY_GROUP1 | LEGACY_GROUP2 | LEGACY_GROUP3))
+            .expect("failed to set legacy groups");
+        assert!(memberships.member_of_group(MODERN_GROUP1));
+        assert!(memberships.member_of_group(MODERN_GROUP2));
+        assert!(memberships.member_of_group(MODERN_GROUP3));
+        // Remove one membership and verify the others are unaffected.
+        memberships
+            .set_legacy_memberships(LegacyGroups(LEGACY_GROUP2 | LEGACY_GROUP3))
+            .expect("failed to set legacy_groups");
+        assert!(!memberships.member_of_group(MODERN_GROUP1));
+        assert!(memberships.member_of_group(MODERN_GROUP2));
+        assert!(memberships.member_of_group(MODERN_GROUP3));
+        // Remove the second & third membership.
+        memberships.set_legacy_memberships(LegacyGroups(0)).expect("failed to set legacy groups");
+        assert!(!memberships.member_of_group(MODERN_GROUP1));
+        assert!(!memberships.member_of_group(MODERN_GROUP2));
+        assert!(!memberships.member_of_group(MODERN_GROUP3));
+        // Verify that setting an invalid group fails.
+        assert_eq!(
+            memberships.set_legacy_memberships(INVALID_LEGACY_GROUP),
+            Err(InvalidLegacyGroupsError {})
+        );
+    }
+
+    #[test]
+    fn test_legacy_and_modern_memberships() {
+        let mut memberships = MulticastGroupMemberships::<FakeProtocolFamily>::new();
+
+        assert!(!memberships.member_of_group(MODERN_GROUP1));
+        assert!(!memberships.member_of_group(MODERN_GROUP2));
+
+        // Add memberships by their legacy group and drop by their modern group.
+        memberships
+            .set_legacy_memberships(LegacyGroups(LEGACY_GROUP1 | LEGACY_GROUP2))
+            .expect("failed to set legacy groups");
+        assert!(memberships.member_of_group(MODERN_GROUP1));
+        assert!(memberships.member_of_group(MODERN_GROUP2));
+        memberships.del_membership(MODERN_GROUP1).expect("failed to del");
+        assert!(!memberships.member_of_group(MODERN_GROUP1));
+        assert!(memberships.member_of_group(MODERN_GROUP2));
+        memberships.del_membership(MODERN_GROUP2).expect("failed to del");
+        assert!(!memberships.member_of_group(MODERN_GROUP1));
+        assert!(!memberships.member_of_group(MODERN_GROUP2));
+
+        // Add memberships by their modern group and drop by their legacy group.
+        memberships.add_membership(MODERN_GROUP1).expect("failed to add");
+        memberships.add_membership(MODERN_GROUP2).expect("failed to add");
+        assert!(memberships.member_of_group(MODERN_GROUP1));
+        assert!(memberships.member_of_group(MODERN_GROUP2));
+        memberships
+            .set_legacy_memberships(LegacyGroups(LEGACY_GROUP2))
+            .expect("failed to set legacy groups");
+        assert!(!memberships.member_of_group(MODERN_GROUP1));
+        assert!(memberships.member_of_group(MODERN_GROUP2));
+        memberships.set_legacy_memberships(LegacyGroups(0)).expect("failed to set legacy groups");
+        assert!(!memberships.member_of_group(MODERN_GROUP1));
+        assert!(!memberships.member_of_group(MODERN_GROUP2));
+    }
+}
