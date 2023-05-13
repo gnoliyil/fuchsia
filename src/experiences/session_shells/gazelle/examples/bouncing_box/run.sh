ad804f3c7f3312f25aec6f7ab23e31aca6cca53e
David Worsham <dworsham@google.com>
2023-05-13 01:12:01 +0000

    [workstation] Delete experiences
    Workstation uses tiles-session now, and the ermine and gazelle shells
    are defunct.
    
    Bug: 124937
    Change-Id: I7ac574e0ce750c9f18afbd93e0a6b7ad2f1c7a69
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/834498
    Reviewed-by: Jason Campbell <jasoncampbell@google.com>
    Reviewed-by: Marc-Antoine Ruel <maruel@google.com>
    Commit-Queue: David Worsham <dworsham@google.com>
    Reviewed-by: Chase Latta <chaselatta@google.com>
    API-Review: Chase Latta <chaselatta@google.com>

--- src/experiences/session_shells/gazelle/examples/bouncing_box/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Copyright 2022 The Fuchsia Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-ffx session add fuchsia-pkg://fuchsia.com/bouncing_box#meta/bouncing_box.cm
