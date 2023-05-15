11c6de68f88ee1bedc880c096e1ce39ae74336d5
Adam Barth <abarth@google.com>
2023-05-15 20:14:11 +0000

    [starnix] Implement /proc/self/{cwd,root}
    Test: ProcSelfCwd.*, ProcSelfRoot.*
    Change-Id: I8f5500d17c196b4dbef4c81098513ce0f54b306c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/856676
    Fuchsia-Auto-Submit: Adam Barth <abarth@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Theodore Dubois <tbodt@google.com>

--- src/starnix/kernel/fs/symlink_node.rs
+++ src/starnix/kernel/fs/symlink_node.rs
@@ -17,7 +17,7 @@ pub struct SymlinkNode {
 
 impl SymlinkNode {
     pub fn new(target: &FsStr) -> Self {
-        SymlinkNode { target: target.to_owned(), xattrs: MemoryXattrStorage::default() }
+        SymlinkNode { target: target.to_owned(), xattrs: Default::default() }
     }
 }
 
@@ -31,5 +31,39 @@ impl FsNodeOps for SymlinkNode {
         _current_task: &CurrentTask,
     ) -> Result<SymlinkTarget, Errno> {
         Ok(SymlinkTarget::Path(self.target.clone()))
+    }
+}
+
+/// A SymlinkNode that uses a callback.
+pub struct CallbackSymlinkNode<F>
+where
+    F: Fn() -> Result<SymlinkTarget, Errno> + Send + Sync + 'static,
+{
+    callback: F,
+    xattrs: MemoryXattrStorage,
+}
+
+impl<F> CallbackSymlinkNode<F>
+where
+    F: Fn() -> Result<SymlinkTarget, Errno> + Send + Sync + 'static,
+{
+    pub fn new(callback: F) -> CallbackSymlinkNode<F> {
+        CallbackSymlinkNode { callback, xattrs: Default::default() }
+    }
+}
+
+impl<F> FsNodeOps for CallbackSymlinkNode<F>
+where
+    F: Fn() -> Result<SymlinkTarget, Errno> + Send + Sync + 'static,
+{
+    fs_node_impl_symlink!();
+    fs_node_impl_xattr_delegate!(self, self.xattrs);
+
+    fn readlink(
+        &self,
+        _node: &FsNode,
+        _current_task: &CurrentTask,
+    ) -> Result<SymlinkTarget, Errno> {
+        (self.callback)()
     }
 }
