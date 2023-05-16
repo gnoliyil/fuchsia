d095e892cbb542be8aaddf9a779471462d368ed1
Travis Geiselbrecht <travisg@google.com>
2023-05-16 01:00:28 +0000

    [kernel][riscv][spinlock] Reimplement spinlocks in c++
    -Drop the spinlock type to 32bit since it's just storing the cpu number.
    -Switch to a C++ CAS loop.
    -Lower the implied memory order to just acquire on lock path, instead of
    a full seq_cst that was encoded into the existing assembly.
    -Remove the extraneous typedef of arch_spin_lock to arch_spin_lock_t on
    all of the architectures.
    
    Validated the codegen is exactly the same except for the size and
    lowered memory order.
    
    Bug: 122876
    Change-Id: Ie3e5e2259273d5f34ccfec6eb109fe48401fce63
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/856022
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: John Grossman <johngro@google.com>
    Fuchsia-Auto-Submit: Travis Geiselbrecht <travisg@google.com>

--- zircon/kernel/arch/riscv64/spinlock.cc
+++ zircon/kernel/arch/riscv64/spinlock.cc
@@ -6,49 +6,40 @@
 
 #include <arch/ops.h>
 #include <arch/spinlock.h>
+#include <ktl/atomic.h>
 
 // We need to disable thread safety analysis in this file, since we're
 // implementing the locks themselves.  Without this, the header-level
 // annotations cause Clang to detect violations.
 
-// TODO-rvbringup: check to see if we need release semantics
-// TODO-rvbringup: see if C++ implementation works well
+// Simple spinning lock, using LR/SC CAS instructions. Stores the current cpu
+// number + 1 for debugging purposes.
 
 void arch_spin_lock(arch_spin_lock_t* lock) TA_NO_THREAD_SAFETY_ANALYSIS {
-  uint64_t new_value = arch_curr_cpu_num() + 1, existing_value, sc_failed;
-
-  __asm__ volatile(
-      "0: lr.d.aq  %0, %2;"       // Load the current lock value
-      "   bne      %0, x0, 0b;"   // Spin if it is already locked (non-zero value)
-      "   sc.d.rl  %1, %z3, %2;"  // Try to store new_value (cpu_num+1) in the lock
-      "   bnez     %1, 0b"        // If the lock had changed behind our back, retry
-      : "=&r"(existing_value), "=&r"(sc_failed), "+A"(lock->value)
-      : "r"(new_value)
-      : "memory");
+  const uint32_t new_value = arch_curr_cpu_num() + 1;
+  for (;;) {
+    uint32_t expected = 0;
+    if (lock->value.compare_exchange_weak(expected, new_value, ktl::memory_order_acquire,
+                                          ktl::memory_order_relaxed)) {
+      break;
+    }
+  }
 
   WRITE_PERCPU_FIELD32(num_spinlocks, READ_PERCPU_FIELD32(num_spinlocks) + 1);
 }
 
 bool arch_spin_trylock(arch_spin_lock_t* lock) TA_NO_THREAD_SAFETY_ANALYSIS {
-  uint64_t new_value = arch_curr_cpu_num() + 1, existing_value, sc_failed;
-
-  __asm__ volatile(
-      "0: lr.d.aq  %0, %2;"       // Load the current lock value
-      "   bne      %0, x0, 1f;"   // Bail out if already locked (non-zero value)
-      "   sc.d.rl  %1, %z3, %2;"  // Try to store new_value (cpu_num+1) in the lock
-      "   bnez     %1, 0b;"       // If the lock had changed behind our back, retry
-      "1:"
-      : "=&r"(existing_value), "=&r"(sc_failed), "+A"(lock->value)
-      : "r"(new_value)
-      : "memory");
-
-  if (existing_value == 0) {
+  const uint32_t new_value = arch_curr_cpu_num() + 1;
+  uint32_t expected = 0;
+  if (lock->value.compare_exchange_strong(expected, new_value, ktl::memory_order_acquire,
+                                          ktl::memory_order_relaxed)) {
+    // success
     WRITE_PERCPU_FIELD32(num_spinlocks, READ_PERCPU_FIELD32(num_spinlocks) + 1);
   }
-  return existing_value;
+  return expected;  // actual old value
 }
 
 void arch_spin_unlock(arch_spin_lock_t* lock) TA_NO_THREAD_SAFETY_ANALYSIS {
   WRITE_PERCPU_FIELD32(num_spinlocks, READ_PERCPU_FIELD32(num_spinlocks) - 1);
-  __atomic_store_n(&lock->value, 0UL, __ATOMIC_RELEASE);
+  lock->value.store(0, ktl::memory_order_release);
 }
