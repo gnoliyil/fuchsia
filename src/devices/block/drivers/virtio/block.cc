b0869b4f0f7769a719dfd5e859ce4424c3733187
HanBin Yoon <hanbinyoon@google.com>
2023-05-18 21:02:56 +0000

    [virtio-blk] Perform feature negotiation during initialization.
    This is a required step as per the virtio spec:
    https://docs.oasis-open.org/virtio/virtio/v1.1/csprd01/virtio-v1.1-csprd01.html#x1-920001
    
    Bug: 123776
    Change-Id: I4abde50e82d10f6e6ca81f88d90352722863952c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/858296
    Reviewed-by: Christopher Anderson <cja@google.com>
    Commit-Queue: HanBin Yoon <hanbinyoon@google.com>

--- src/devices/block/drivers/virtio/block.cc
+++ src/devices/block/drivers/virtio/block.cc
@@ -106,7 +106,16 @@ zx_status_t BlockDevice::Init() {
 
   DriverStatusAck();
 
-  // TODO: Check features bits and ack/nak them
+  if (!DeviceFeatureSupported(VIRTIO_F_VERSION_1)) {
+    // Declaring non-support until there is a need in the future.
+    zxlogf(ERROR, "Legacy virtio interface is not supported by this driver");
+    return ZX_ERR_NOT_SUPPORTED;
+  }
+  DriverFeatureAck(VIRTIO_F_VERSION_1);
+  if (zx_status_t status = DeviceStatusFeaturesOk(); status != ZX_OK) {
+    zxlogf(ERROR, "Feature negotiation failed: %s", zx_status_get_string(status));
+    return status;
+  }
 
   // Allocate the main vring.
   auto err = vring_.Init(0, ring_size);
