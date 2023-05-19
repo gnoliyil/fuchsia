035858426e909c08373433e2c3141e723c05d640
Joseph Ryan <josephry@google.com>
2023-05-19 00:32:29 +0000

    [docs][rust] Improve rustdoc index page
    Change-Id: I99832cb1e87d45367f180e065ea386e87223a4fe
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/857596
    Commit-Queue: Joseph Ryan <josephry@google.com>
    Fuchsia-Auto-Submit: Joseph Ryan <josephry@google.com>
    Reviewed-by: Nick Van der Auwermeulen <nickvander@google.com>
    Reviewed-by: Tyler Mandry <tmandry@google.com>

--- build/rust/docs/rustdoc-index/src/lib.rs
+++ build/rust/docs/rustdoc-index/src/lib.rs
@@ -1,4 +1,5 @@
 // Copyright 2023 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-#![doc = include_str!("rustdoc-index.md")]
+#![doc = include_str!("../header.md")]
+#![doc = include_str!("../../../../../docs/reference/rust/crates.md")]
