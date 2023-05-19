678173923483f6da93e6f608eeb39c94e33cd2b9
Nick Van der Auwermeulen <nickvander@google.com>
2023-05-19 18:12:14 +0000

    [docs] Update the includes to improve rustdoc index page.
    Change-Id: I18c1932d7dcaa384a368751c305425478e5c448f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/858345
    Fuchsia-Auto-Submit: Nick Van der Auwermeulen <nickvander@google.com>
    Reviewed-by: Joseph Ryan <josephry@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- build/rust/docs/rustdoc-index/src/lib.rs
+++ build/rust/docs/rustdoc-index/src/lib.rs
@@ -2,4 +2,4 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 #![doc = include_str!("../header.md")]
-#![doc = include_str!("../../../../../docs/reference/rust/crates.md")]
+#![doc = include_str!("../../../../../docs/reference/rust/_common/_crates_list.md")]
