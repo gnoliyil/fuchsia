33c607bc14c82d080b2df422a74e413a64c02c63
Kevin Lindkvist <lindkvist@google.com>
2023-05-23 15:45:01 +0000

    [starnix] Remove exception executor
    The restricted executor has now reached feature parity with the
    exception executor, and is significantly faster.
    
    Maintaining two different executors is a maintenance burden: right now
    the exception executor is not even compiled in CQ/CI.
    
    Given that Zircon considers restricted mode part of the kernel's base
    functionality, and is committed to supporting it on new architectures
    moving forward, the exception executor is unlikely to be useful in the
    future.
    
    Change-Id: Ia510a9243060ee160cbdd0d7a8ac23f008d77d28
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/849157
    Commit-Queue: Kevin Lindkvist <lindkvist@google.com>
    Reviewed-by: Benjamin Lerman <qsr@google.com>

--- src/starnix/kernel/arch/aarch64/execution.rs
+++ src/starnix/kernel/arch/aarch64/execution.rs
@@ -91,16 +91,3 @@ impl Syscall {
         }
     }
 }
-
-pub fn generate_interrupt_instructions(_current_task: &CurrentTask) -> Vec<u8> {
-    const INTERRUPT_AND_JUMP: [u8; 8] = [
-        0x00, 0x00, 0x20, 0xd4, // 0xd4200000 = brk 0 (the argument is ignored by us).
-        // TODO(fxbug.dev/121659): Write this code. This issues an undefined instruction so it's
-        // obvious something is not implemented.
-        //
-        // I believe ARM lacks a single indirect-from-memory jump instruction like x86 so this
-        // will need some research on what registers we can clobber.
-        0x00, 0x00, 0x00, 0x00, // udf = "Undefined instruction"
-    ];
-    INTERRUPT_AND_JUMP.to_vec()
-}
--- src/starnix/kernel/execution/mod.rs
+++ src/starnix/kernel/execution/mod.rs
@@ -4,16 +4,12 @@
 
 mod component_runner;
 mod container;
-mod exception_executor;
 mod restricted_executor;
 mod serve_protocols;
 mod shared;
 
 pub use component_runner::*;
 pub use container::*;
-#[cfg(not(feature = "restricted_mode"))]
-pub use exception_executor::*;
-#[cfg(feature = "restricted_mode")]
 pub use restricted_executor::*;
 pub use serve_protocols::*;
 pub use shared::*;
