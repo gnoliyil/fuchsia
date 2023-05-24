a832ccb76538811a62d3948bf5f26d472de8bea7
Matthew Kehrt <kehrt@google.com>
2023-05-24 18:00:01 +0000

    [bt][test] Move ProfileTestServer to a library.
    Moves the ProfileTestServer from HID to a the profile_client library so
    clients can use this mock to test their code.  Also renames some things.
    
    Change-Id: Ia79a8bd0d88e6270434796aaa647dd533fa6f12d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/852538
    Commit-Queue: Matthew Kehrt <kehrt@google.com>
    Reviewed-by: Ani Ramakrishnan <aniramakri@google.com>

--- src/connectivity/bluetooth/lib/profile-client/src/test/lib.rs
+++ src/connectivity/bluetooth/lib/profile-client/src/test/lib.rs
@@ -1,8 +1,6 @@
-// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
-// TODO(fxb/84459) Move this to a common location for all BT profiles.
 
 use fidl;
 use fidl_fuchsia_bluetooth_bredr as bredr;
@@ -13,70 +11,66 @@ use profile_client::ProfileClient;
 pub struct TestProfileServerEndpoints {
     pub proxy: bredr::ProfileProxy,
     pub client: ProfileClient,
-    pub test_server: LocalProfileTestServer,
+    pub test_server: TestProfileServer,
 }
 
-// TODO(fxb/126738) Reomved this when this is moved to a library.
-#[allow(unused)]
+/// Used to specify the channel to expect on an incoming Connect message
 #[derive(Debug)]
-// Used to specify the channel to expect on an incoming Connect message
 pub enum ConnectChannel {
     L2CapPsm(u16),
     RfcommChannel(u8), // Valid channels are 1-30
-}
-
-/// Create a new Profile proxy and stream, and create a profile client that wraps the proxy and a
-/// test server that wraps the stream.
-///
-/// If service_class_profile_id is Some, add a search for that service class.
-///
-/// If service_definition is Some, advertise with that service definition.
-///
-/// Returns a struct containing the proxy, profile client and test server.
-pub fn new(
-    service_definition: Option<bredr::ServiceDefinition>,
-    service_class_profile_id: Option<bredr::ServiceClassProfileIdentifier>,
-) -> TestProfileServerEndpoints {
-    let (proxy, stream) = fidl::endpoints::create_proxy_and_stream::<bredr::ProfileMarker>()
-        .expect("Create new profile connection");
-
-    let mut client = match service_definition {
-        None => ProfileClient::new(proxy.clone()),
-        Some(service_definition) => {
-            let channel_params = bredr::ChannelParameters::default();
-            ProfileClient::advertise(proxy.clone(), &[service_definition], channel_params)
-                .expect("Failed to advertise.")
-        }
-    };
-
-    if let Some(service_class_profile_id) = service_class_profile_id {
-        client.add_search(service_class_profile_id, &[]).expect("Failed to search for peers.");
-    }
-
-    let test_server = LocalProfileTestServer::from(stream);
-
-    TestProfileServerEndpoints { proxy, client, test_server }
 }
 
 /// Holds all the server side resources associated with a `Profile`'s connection to
 /// fuchsia.bluetooth.bredr.Profile. Provides helper methods for common test related tasks.
 /// Some fields are optional because they are not populated until the Profile has completed
 /// registration.
-pub struct LocalProfileTestServer {
+pub struct TestProfileServer {
     profile_request_stream: bredr::ProfileRequestStream,
     search_results_proxy: Option<bredr::SearchResultsProxy>,
-    #[allow(unused)]
     connection_receiver_proxy: Option<bredr::ConnectionReceiverProxy>,
 }
 
-impl From<bredr::ProfileRequestStream> for LocalProfileTestServer {
+impl From<bredr::ProfileRequestStream> for TestProfileServer {
     fn from(profile_request_stream: bredr::ProfileRequestStream) -> Self {
         Self { profile_request_stream, search_results_proxy: None, connection_receiver_proxy: None }
     }
 }
 
-impl LocalProfileTestServer {
-    /// Run through the registration process of a new `Profile`.
+impl TestProfileServer {
+    /// Create a new Profile proxy and stream, and create a profile client that wraps the proxy and a
+    /// test server that wraps the stream.
+    ///
+    /// If service_class_profile_id is Some, add a search for that service class.
+    ///
+    /// If service_definition is Some, advertise with that service definition.
+    ///
+    /// Returns a struct containing the proxy, profile client and test server.
+    pub fn new(
+        service_definition: Option<bredr::ServiceDefinition>,
+        service_class_profile_id: Option<bredr::ServiceClassProfileIdentifier>,
+    ) -> TestProfileServerEndpoints {
+        let (proxy, stream) = fidl::endpoints::create_proxy_and_stream::<bredr::ProfileMarker>()
+            .expect("Create new profile connection");
+
+        let mut client = match service_definition {
+            None => ProfileClient::new(proxy.clone()),
+            Some(service_definition) => {
+                let channel_params = bredr::ChannelParameters::default();
+                ProfileClient::advertise(proxy.clone(), &[service_definition], channel_params)
+                    .expect("Failed to advertise.")
+            }
+        };
+
+        if let Some(service_class_profile_id) = service_class_profile_id {
+            client.add_search(service_class_profile_id, &[]).expect("Failed to search for peers.");
+        }
+
+        let test_server = TestProfileServer::from(stream);
+
+        TestProfileServerEndpoints { proxy, client, test_server }
+    }
+
     pub async fn expect_search(&mut self) {
         let request = self.profile_request_stream.next().await;
         match request {
@@ -90,7 +84,6 @@ impl LocalProfileTestServer {
         }
     }
 
-    #[allow(unused)]
     pub async fn expect_advertise(&mut self) {
         let request = self.profile_request_stream.next().await;
         match request {
@@ -104,7 +97,6 @@ impl LocalProfileTestServer {
         }
     }
 
-    #[allow(unused)]
     pub async fn expect_connect(
         &mut self,
         expected_channel: Option<ConnectChannel>,
@@ -153,17 +145,14 @@ impl LocalProfileTestServer {
         search_results_proxy.service_found(&peer_id.into(), protocol_list.as_deref(), &attributes)
     }
 
-    #[allow(unused)]
     pub fn send_connected(
         &mut self,
         peer_id: PeerId,
         protocol_list: Vec<bredr::ProtocolDescriptor>,
     ) -> bt::Channel {
-        let (near_socket, far_socket) = fidl::Socket::create_stream();
-        let near_bt_channel =
-            bt::Channel::from_socket(near_socket, 1).expect("failed to create chennel from socket");
-        let far_bredr_channel =
-            bredr::Channel { socket: Some(far_socket), ..bredr::Channel::default() };
+        let (near_bt_channel, far_bt_channel) = bt::Channel::create();
+        let far_bredr_channel: bredr::Channel =
+            far_bt_channel.try_into().expect("BT Channel into FIDL BREDR Channel");
 
         let connection_receiver_proxy =
             self.connection_receiver_proxy.as_ref().expect("Connection receiver proxy");
