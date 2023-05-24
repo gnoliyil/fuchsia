92355b0ab23bfac5c51dd227b0f07c5d27c0a93e
David Fang <fangism@google.com>
2023-05-24 19:08:41 +0000

    [rbe] support working w/ logs instead of log-dirs
    Sometimes, users will download two reproxy logs to
    compare.  This makes it convenient to operate
    on two logs instead two log dirs, or a mix.
    The logs are copied as needed to a cached location
    based on a hash of the log's contents.
    
    Helps with debugging cache misses between similar builds.
    
    Change-Id: I2b892895be0de8e9efed47fbdff9a9659d0234d2
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/857617
    Reviewed-by: Aaron Wood <aaronwood@google.com>
    Commit-Queue: David Fang <fangism@google.com>

--- build/rbe/reproxy_logs.py
+++ build/rbe/reproxy_logs.py
@@ -7,6 +7,9 @@
 This requires python protobufs for reproxy LogDump.
 """
 
+import hashlib
+import os
+import shutil
 import subprocess
 
 from api.proxy import log_pb2
@@ -15,39 +18,77 @@ from typing import Dict
 
 _SCRIPT_BASENAME = Path(__file__).name
 
+
 def msg(text: str):
     print(f'[{_SCRIPT_BASENAME}] {text}')
 
 
 class ReproxyLog(object):
-  """Contains a LogDump proto and indexes information internally.
+    """Contains a LogDump proto and indexes information internally.
 
   The output_file paths used are relative to the working dir.
   """
 
-  def __init__(self, log_dump: log_pb2.LogDump):
-      self._proto : log_pb2.LogDump = log_dump
+    def __init__(self, log_dump: log_pb2.LogDump):
+        self._proto: log_pb2.LogDump = log_dump
 
-      self._records_by_output_file = self._index_records_by_output_file()
+        self._records_by_output_file = self._index_records_by_output_file()
 
-  def _index_records_by_output_file(self) -> Dict[Path, log_pb2.LogRecord]:
-      """Map files to the records of the actions that created them."""
-      records : Dict[Path, log_pb2.LogRecord] = {}
-      for record in self.proto.records:
-          for output_file in record.command.output.output_files:
-              records[Path(output_file)] = record
-      return records
+    def _index_records_by_output_file(self) -> Dict[Path, log_pb2.LogRecord]:
+        """Map files to the records of the actions that created them."""
+        records: Dict[Path, log_pb2.LogRecord] = {}
+        for record in self.proto.records:
+            for output_file in record.command.output.output_files:
+                records[Path(output_file)] = record
+        return records
 
-  @property
-  def proto(self) -> log_pb2.LogDump:
-      return self._proto
+    @property
+    def proto(self) -> log_pb2.LogDump:
+        return self._proto
 
-  @property
-  def records_by_output_file(self) -> Dict[Path, log_pb2.LogRecord]:
-      return self._records_by_output_file
+    @property
+    def records_by_output_file(self) -> Dict[Path, log_pb2.LogRecord]:
+        return self._records_by_output_file
+
+
+def setup_logdir_for_logdump(path: Path, verbose: bool = False) -> Path:
+    """Automatically setup a log dir for logdump, if needed.
+
+    The `logdump` tool expects a log directory, but sometimes we
+    are only given a log file.  When we are given a log file, copy it
+    to a directory.  The created directory is cached by the hash
+    of the log file, so that it can be re-used for multiple queries.
+
+    Args:
+      path: path to either a .rrpl/.rpl log, or an reproxy log dir
+        containing a .rrpl/.rpl log.
+
+    Returns:
+      The original path, or path to a newly created cached directory
+      containing a copy of the log.
+    """
+    if path.is_dir():
+        # This is a directory containing a .rrpl or .rpl file.
+        return path
+
+    # Otherwise, this is assumed to be a .rrpl or .rpl file.
+    # Copy it to a cached location.
+    tmpdir = Path(os.environ.get('TMPDIR', '/tmp'))
+    with open(path, "rb") as f:
+        contents = f.read()
+    readable_hash = hashlib.sha256(contents).hexdigest()
+    cached_log_dir = tmpdir / 'action_diff_py' / 'cache' / readable_hash
+    if verbose:
+        msg(f'Copying log to {cached_log_dir} for logdump processing.')
+    cached_log_dir.mkdir(parents=True, exist_ok=True)
+    shutil.copy2(path, cached_log_dir)
+    return cached_log_dir
+
 
 def convert_reproxy_actions_log(
-    reproxy_logdir: Path, reclient_bindir: Path, verbose: bool = False) -> log_pb2.LogDump:
+        reproxy_logdir: Path,
+        reclient_bindir: Path,
+        verbose: bool = False) -> log_pb2.LogDump:
     log_pb_file = reproxy_logdir / "reproxy_log.pb"
 
     if not log_pb_file.exists():
@@ -73,4 +114,3 @@ def convert_reproxy_actions_log(
         log_dump.ParseFromString(logf.read())
 
     return log_dump
-
