d06bad8fd8c8855e7165bd55b7102a24e4ee8ab2
Sarah Chan <spqchan@google.com>
2023-05-25 16:55:54 +0000

    [reboot] Kill shutdown-shim in kill_critical_process_test
    Update kill_critical_process_test so that it
    kills the shutdown-shim process instead of the
    driver_manager
    
    Multiply: kill_critical_process_test.sh: 10
    Bug: 127767, 127646
    Change-Id: I28870bf1c6b4d7a9cdd975e5ab20dca1a0021f09
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/861403
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Commit-Queue: Sarah Chan <spqchan@google.com>
    Reviewed-by: Jocelyn Dang <jocelyndang@google.com>

--- src/tests/reboot/kill_critical_process/kill_critical_process_test.go
+++ src/tests/reboot/kill_critical_process/kill_critical_process_test.go
@@ -10,10 +10,10 @@ import (
 	"go.fuchsia.dev/fuchsia/src/tests/reboot/reboottest"
 )
 
-// Test that "killall driver_manager.cm" will reboot the system (because driver_manager is
+// Test that "killall shutdown-shim.cm" will reboot the system (because shutdown-shim is
 // marked as a critical process; see also |zx_job_set_critical|).
 func TestKillCriticalProcess(t *testing.T) {
 	// Killing a critical process will result in an "unclean reboot" because,
 	// among other things, the filesystem won't be shutdown cleanly.
-	reboottest.RebootWithCommand(t, "killall driver_manager.cm", reboottest.UncleanReboot, reboottest.Reboot, reboottest.UserspaceRootJobTermination)
+	reboottest.RebootWithCommand(t, "killall shutdown-shim.cm", reboottest.UncleanReboot, reboottest.Reboot, reboottest.UserspaceRootJobTermination)
 }
