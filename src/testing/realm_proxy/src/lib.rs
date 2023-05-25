904eaa946280e732b27ad4ec91733fdc8431b5da
Kendal Harland <kjharland@google.com>
2023-05-25 00:46:39 +0000

    [realm_proxy] Add Error type to help unwrap OperationErrors
    Change-Id: I8137c547dae0349fdd2c2aa811c9aec9ac53f399
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/860380
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Kendal Harland <kjharland@google.com>

--- src/testing/realm_proxy/src/lib.rs
+++ src/testing/realm_proxy/src/lib.rs
@@ -4,3 +4,44 @@
 
 pub mod client;
 pub mod service;
+
+use fidl_fuchsia_testing_harness as fharness;
+
+/// A wrapper for errors returned by fuchsia.testing.harness FIDL protocol methods.
+/// This implements std::error::Error.
+///
+/// Example usage:
+///
+/// ```
+/// realm_factory.create_realm(..)
+///     .await?
+///     .map_err(realm_proxy::Error::from)?;
+/// ```
+#[derive(Debug)]
+pub enum Error {
+    OperationError(fharness::OperationError),
+}
+
+impl From<fharness::OperationError> for Error {
+    fn from(value: fharness::OperationError) -> Self {
+        Error::OperationError(value)
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::OperationError(e) => write!(f, "{:?}", e),
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn description(&self) -> &str {
+        "description() is deprecated; use Display"
+    }
+
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
+    }
+}
