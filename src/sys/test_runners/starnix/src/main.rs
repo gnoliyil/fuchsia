c5e19d23810a27bc8bfc88556b906a408bba176c
Adam Barth <abarth@google.com>
2023-05-25 15:47:14 +0000

    [starnix] Modernize starnix_test_runner main
    We have much better tools for writing these main functions now.
    
    Change-Id: I4f008999d1e4e3caf1ec28f45acdacd080558ac6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/861682
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Kevin Lindkvist <lindkvist@google.com>
    Fuchsia-Auto-Submit: Adam Barth <abarth@google.com>

--- src/sys/test_runners/starnix/src/main.rs
+++ src/sys/test_runners/starnix/src/main.rs
@@ -10,44 +10,29 @@ mod results_parser;
 mod runner;
 mod test_suite;
 
-use anyhow::{anyhow, Error};
-use fidl::endpoints::DiscoverableProtocolMarker;
+use anyhow::Error;
 use fidl_fuchsia_component_runner as frunner;
-use fidl_fuchsia_io as fio;
-use fuchsia_zircon as zx;
+use fuchsia_component::server::ServiceFs;
+use futures::StreamExt;
 use tracing::debug;
-use vfs::directory::{entry::DirectoryEntry, helper::DirectlyMutable};
+
+enum Services {
+    ComponentRunner(frunner::ComponentRunnerRequestStream),
+}
 
 #[fuchsia::main(logging_tags=["starnix_test_runner"])]
 async fn main() -> Result<(), Error> {
     debug!("starnix test runner started");
-    const SVC_DIRECTORY: &str = "svc";
-
-    let outgoing_dir_handle =
-        fuchsia_runtime::take_startup_handle(fuchsia_runtime::HandleType::DirectoryRequest.into())
-            .ok_or(anyhow!("Failed to get startup handle"))?;
-    let outgoing_dir_server_end =
-        fidl::endpoints::ServerEnd::new(zx::Channel::from(outgoing_dir_handle));
-
-    let outgoing_dir = vfs::directory::immutable::simple();
-
-    let svc_dir = vfs::directory::immutable::simple();
-    svc_dir.add_entry(
-        frunner::ComponentRunnerMarker::PROTOCOL_NAME,
-        vfs::service::host(move |requests| async move {
-            runner::handle_runner_requests(requests).await.expect("Error serving runner requests.")
-        }),
-    )?;
-    outgoing_dir.add_entry(SVC_DIRECTORY, svc_dir.clone())?;
-
-    let execution_scope = vfs::execution_scope::ExecutionScope::new();
-    outgoing_dir.open(
-        execution_scope.clone(),
-        fio::OpenFlags::RIGHT_READABLE | fio::OpenFlags::RIGHT_WRITABLE,
-        vfs::path::Path::dot(),
-        outgoing_dir_server_end,
-    );
-    execution_scope.wait().await;
-
+    let mut fs = ServiceFs::new_local();
+    fs.dir("svc").add_fidl_service(Services::ComponentRunner);
+    fs.take_and_serve_directory_handle()?;
+    fs.for_each_concurrent(None, |request| async {
+        match request {
+            Services::ComponentRunner(stream) => runner::handle_runner_requests(stream)
+                .await
+                .expect("Error serving runner requests."),
+        }
+    })
+    .await;
     Ok(())
 }
