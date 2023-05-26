7902ee5d78501020f2aec389001639a91db3d13c
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2023-05-26 22:02:57 +0000

    [ffx][product_bundle] Migrate to fho + FFxMain instead of ffx_plugin
    Change-Id: I7d362433f8a0c439fcaea99fdd0151e48bb971bc
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/862736
    Reviewed-by: Megan Batty <mgnb@google.com>
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

---
+++ src/developer/ffx/plugins/product_bundle/src/main.rs
@@ -0,0 +1,10 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use fho::FfxTool;
+
+#[fuchsia_async::run_singlethreaded]
+async fn main() {
+    ffx_product_bundle::ProductBundleTool::execute_tool().await
+}
