7d58967e4b9a2e1ff0152bf89968aef5dab764b8
Jocelyn Dang <jocelyndang@google.com>
2023-05-31 00:39:14 +0000

    [aml-pwm-regulator] Migrate from pwm banjo proxying to FIDL.
    Bug: 126924
    Change-Id: I11d68e4db94460d1e96cb74204a54131b29e65d9
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/862436
    Commit-Queue: Jocelyn Dang <jocelyndang@google.com>
    Reviewed-by: Sarah Chan <spqchan@google.com>

--- src/devices/power/drivers/aml-pwm-regulator/aml-pwm-regulator.h
+++ src/devices/power/drivers/aml-pwm-regulator/aml-pwm-regulator.h
@@ -5,8 +5,8 @@
 #ifndef SRC_DEVICES_POWER_DRIVERS_AML_PWM_REGULATOR_AML_PWM_REGULATOR_H_
 #define SRC_DEVICES_POWER_DRIVERS_AML_PWM_REGULATOR_AML_PWM_REGULATOR_H_
 
+#include <fidl/fuchsia.hardware.pwm/cpp/wire.h>
 #include <fidl/fuchsia.hardware.vreg/cpp/wire.h>
-#include <fuchsia/hardware/pwm/cpp/banjo.h>
 #include <lib/component/outgoing/cpp/outgoing_directory.h>
 #include <lib/ddk/debug.h>
 
@@ -24,7 +24,7 @@ class AmlPwmRegulator : public AmlPwmRegulatorType,
                         public fidl::WireServer<fuchsia_hardware_vreg::Vreg> {
  public:
   explicit AmlPwmRegulator(zx_device_t* parent, const PwmVregMetadataEntry& vreg_range,
-                           ddk::PwmProtocolClient pwm)
+                           fidl::WireSyncClient<fuchsia_hardware_pwm::Pwm> pwm_proto_client)
       : AmlPwmRegulatorType(parent),
         pwm_index_(vreg_range.pwm_index()),
         period_ns_(vreg_range.period_ns()),
@@ -32,7 +32,7 @@ class AmlPwmRegulator : public AmlPwmRegulatorType,
         voltage_step_uv_(vreg_range.voltage_step_uv()),
         num_steps_(vreg_range.num_steps()),
         current_step_(vreg_range.num_steps()),
-        pwm_(pwm) {}
+        pwm_proto_client_(std::move(pwm_proto_client)) {}
   static zx_status_t Create(void* ctx, zx_device_t* parent);
 
   // Device Protocol Implementation
@@ -56,7 +56,7 @@ class AmlPwmRegulator : public AmlPwmRegulatorType,
 
   uint32_t current_step_;
 
-  ddk::PwmProtocolClient pwm_;
+  fidl::WireSyncClient<fuchsia_hardware_pwm::Pwm> pwm_proto_client_;
 
   std::optional<component::OutgoingDirectory> outgoing_;
   fidl::ServerEnd<fuchsia_io::Directory> outgoing_server_end_;
