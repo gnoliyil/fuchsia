06e8fc6c18a0224571e5ea876bd1366959a07a4a
Jeff Martin <martinjeffrey@google.com>
2023-06-01 20:38:02 +0000

    [netlink] Add ClientId to improve debugability
    This gives us a way to associate disparate log messages that originate
    on behalf of a single socket. While here, add log messages for
    joining/leaving multicast groups.
    
    For now, ClientIds are monotonically increasing identifiers that never
    get repurposed.
    
    While here, introduce an `InnerClient` to hold shared fields/logic for
    the `InternalClient` and `ExternalClient`.
    
    Change-Id: I5ff647e25fddbd27f83e9065047a890b3327aeac
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/864061
    Reviewed-by: Antonio Linhart <antoniolinhart@google.com>
    Commit-Queue: Jeff Martin <martinjeffrey@google.com>
    Reviewed-by: Ghanan Gowripalan <ghanan@google.com>

--- src/connectivity/network/netlink/src/client.rs
+++ src/connectivity/network/netlink/src/client.rs
@@ -4,10 +4,16 @@
 
 //! A module for managing individual clients (aka sockets) of Netlink.
 
-use std::sync::{Arc, Mutex};
+use std::{
+    fmt::{Display, Formatter},
+    sync::{
+        atomic::{AtomicU64, Ordering},
+        Arc, Mutex,
+    },
+};
 
 use derivative::Derivative;
-use tracing::debug;
+use tracing::{debug, warn};
 
 use crate::{
     messaging::Sender,
@@ -19,21 +25,47 @@ use crate::{
     NETLINK_LOG_TAG,
 };
 
+/// A unique identifier for a client.
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub(crate) struct ClientId(u64);
+
+/// The aspects of a client shared by [`InternalClient`] and [`ExternalClient`].
+struct InnerClient<F: ProtocolFamily> {
+    /// The unique ID for this client.
+    id: ClientId,
+    /// The client's current multicast group memberships.
+    group_memberships: Mutex<MulticastGroupMemberships<F>>,
+}
+
+impl<F: ProtocolFamily> Display for InnerClient<F> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let InnerClient { id: ClientId(id), group_memberships: _ } = self;
+        write!(f, "Client[{}, {}]", id, F::NAME)
+    }
+}
+
 /// The internal half of a Netlink client, with the external half being provided
 /// by ['ExternalClient'].
 #[derive(Derivative)]
 #[derivative(Clone(bound = ""))]
 pub(crate) struct InternalClient<F: ProtocolFamily, S: Sender<F::Message>> {
-    /// The client's current multicast group memberships.
-    group_memberships: Arc<Mutex<MulticastGroupMemberships<F>>>,
+    /// The inner client.
+    inner: Arc<InnerClient<F>>,
     /// The [`Sender`] of messages from Netlink to the Client.
     sender: S,
+}
+
+impl<F: ProtocolFamily, S: Sender<F::Message>> Display for InternalClient<F, S> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let InternalClient { inner, sender: _ } = self;
+        write!(f, "{}", inner)
+    }
 }
 
 impl<F: ProtocolFamily, S: Sender<F::Message>> InternalClient<F, S> {
     /// Returns true if this client is a member of the provided group.
     pub(crate) fn member_of_group(&self, group: ModernGroup) -> bool {
-        self.group_memberships.lock().unwrap().member_of_group(group)
+        self.inner.group_memberships.lock().unwrap().member_of_group(group)
     }
 
     /// Sends the given message to the external half of this client.
@@ -45,19 +77,42 @@ impl<F: ProtocolFamily, S: Sender<F::Message>> InternalClient<F, S> {
 /// The external half of a Netlink client, with the internal half being provided
 /// by ['InternalClient'].
 pub(crate) struct ExternalClient<F: ProtocolFamily> {
-    /// The client's current multicast group memberships
-    group_memberships: Arc<Mutex<MulticastGroupMemberships<F>>>,
+    /// The inner client.
+    inner: Arc<InnerClient<F>>,
+}
+
+impl<F: ProtocolFamily> Display for ExternalClient<F> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let ExternalClient { inner } = self;
+        write!(f, "{}", inner)
+    }
 }
 
 impl<F: ProtocolFamily> ExternalClient<F> {
     /// Adds the given multicast group membership.
     pub(crate) fn add_membership(&self, group: ModernGroup) -> Result<(), InvalidModernGroupError> {
-        self.group_memberships.lock().unwrap().add_membership(group)
+        let res = self.inner.group_memberships.lock().unwrap().add_membership(group);
+        match res {
+            Ok(()) => debug!(tag = NETLINK_LOG_TAG, "{} joined multicast group: {:?}", self, group),
+            Err(InvalidModernGroupError) => warn!(
+                tag = NETLINK_LOG_TAG,
+                "{} failed to join invalid multicast group: {:?}", self, group
+            ),
+        };
+        res
     }
 
     /// Deletes the given multicast group membership.
     pub(crate) fn del_membership(&self, group: ModernGroup) -> Result<(), InvalidModernGroupError> {
-        self.group_memberships.lock().unwrap().del_membership(group)
+        let res = self.inner.group_memberships.lock().unwrap().del_membership(group);
+        match res {
+            Ok(()) => debug!(tag = NETLINK_LOG_TAG, "{} left multicast group: {:?}", self, group),
+            Err(InvalidModernGroupError) => warn!(
+                tag = NETLINK_LOG_TAG,
+                "{} failed to leave invalid multicast group: {:?}", self, group
+            ),
+        };
+        res
     }
 
     /// Sets the legacy multicast group memberships.
@@ -65,19 +120,46 @@ impl<F: ProtocolFamily> ExternalClient<F> {
         &self,
         legacy_memberships: LegacyGroups,
     ) -> Result<(), InvalidLegacyGroupsError> {
-        self.group_memberships.lock().unwrap().set_legacy_memberships(legacy_memberships)
+        let res =
+            self.inner.group_memberships.lock().unwrap().set_legacy_memberships(legacy_memberships);
+        match res {
+            Ok(()) => debug!(
+                tag = NETLINK_LOG_TAG,
+                "{} updated multicast groups: {:?}", self, legacy_memberships
+            ),
+            Err(InvalidLegacyGroupsError) => warn!(
+                tag = NETLINK_LOG_TAG,
+                "{} failed to update multicast groups: {:?}", self, legacy_memberships
+            ),
+        };
+        res
     }
 }
 
 // Instantiate a new client pair.
 pub(crate) fn new_client_pair<F: ProtocolFamily, S: Sender<F::Message>>(
+    id: ClientId,
     sender: S,
 ) -> (ExternalClient<F>, InternalClient<F, S>) {
-    let group_memberships = Arc::new(Mutex::new(MulticastGroupMemberships::new()));
-    (
-        ExternalClient { group_memberships: group_memberships.clone() },
-        InternalClient { group_memberships, sender: sender },
-    )
+    let inner = Arc::new(InnerClient {
+        id,
+        group_memberships: Mutex::new(MulticastGroupMemberships::new()),
+    });
+    (ExternalClient { inner: inner.clone() }, InternalClient { inner, sender: sender })
+}
+
+/// A generator of [`ClientId`].
+#[derive(Default)]
+pub(crate) struct ClientIdGenerator(AtomicU64);
+
+impl ClientIdGenerator {
+    /// Returns a unique [`ClientId`].
+    pub(crate) fn new_id(&self) -> ClientId {
+        let ClientIdGenerator(next_id) = self;
+        let id = next_id.fetch_add(1, Ordering::Relaxed);
+        assert_ne!(id, u64::MAX, "exhausted client IDs");
+        ClientId(id)
+    }
 }
 
 /// The table of connected clients for a given ProtocolFamily.
@@ -122,12 +204,19 @@ pub(crate) mod testutil {
         protocol_family::ProtocolFamily,
     };
 
+    pub(crate) const CLIENT_ID_1: ClientId = ClientId(1);
+    pub(crate) const CLIENT_ID_2: ClientId = ClientId(2);
+    pub(crate) const CLIENT_ID_3: ClientId = ClientId(2);
+    pub(crate) const CLIENT_ID_4: ClientId = ClientId(4);
+    pub(crate) const CLIENT_ID_5: ClientId = ClientId(5);
+
     /// Creates a new client with memberships to the given groups.
     pub(crate) fn new_fake_client<F: ProtocolFamily>(
+        id: ClientId,
         group_memberships: &[ModernGroup],
     ) -> (FakeSenderSink<F::Message>, InternalClient<F, FakeSender<F::Message>>) {
         let (sender, sender_sink) = crate::messaging::testutil::fake_sender_with_sink();
-        let (external_client, internal_client) = new_client_pair(sender);
+        let (external_client, internal_client) = new_client_pair(id, sender);
         for group in group_memberships {
             external_client.add_membership(*group).expect("add group membership");
         }
@@ -138,6 +227,8 @@ pub(crate) mod testutil {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    use std::collections::HashSet;
 
     use crate::{
         messaging::testutil::FakeSender,
@@ -151,7 +242,7 @@ mod tests {
     #[test]
     fn test_group_memberships() {
         let (external_client, internal_client) =
-            new_client_pair::<FakeProtocolFamily, _>(FakeSender::default());
+            new_client_pair::<FakeProtocolFamily, _>(testutil::CLIENT_ID_1, FakeSender::default());
 
         assert!(!internal_client.member_of_group(MODERN_GROUP1));
         assert!(!internal_client.member_of_group(MODERN_GROUP2));
@@ -177,12 +268,19 @@ mod tests {
     #[test]
     fn test_send_message_to_group() {
         let clients = ClientTable::default();
-        let (mut sink_group1, client_group1) =
-            testutil::new_fake_client::<FakeProtocolFamily>(&[MODERN_GROUP1]);
-        let (mut sink_group2, client_group2) =
-            testutil::new_fake_client::<FakeProtocolFamily>(&[MODERN_GROUP2]);
+        let (mut sink_group1, client_group1) = testutil::new_fake_client::<FakeProtocolFamily>(
+            testutil::CLIENT_ID_1,
+            &[MODERN_GROUP1],
+        );
+        let (mut sink_group2, client_group2) = testutil::new_fake_client::<FakeProtocolFamily>(
+            testutil::CLIENT_ID_2,
+            &[MODERN_GROUP2],
+        );
         let (mut sink_both_groups, client_both_groups) =
-            testutil::new_fake_client::<FakeProtocolFamily>(&[MODERN_GROUP1, MODERN_GROUP2]);
+            testutil::new_fake_client::<FakeProtocolFamily>(
+                testutil::CLIENT_ID_3,
+                &[MODERN_GROUP1, MODERN_GROUP2],
+            );
         clients.add_client(client_group1);
         clients.add_client(client_group2);
         clients.add_client(client_both_groups);
@@ -200,5 +298,17 @@ mod tests {
         assert_eq!(&sink_group1.take_messages()[..], &[]);
         assert_eq!(&sink_group2.take_messages()[..], &[FakeNetlinkMessage]);
         assert_eq!(&sink_both_groups.take_messages()[..], &[FakeNetlinkMessage]);
+    }
+
+    #[test]
+    fn test_client_id_generator() {
+        let generator = ClientIdGenerator::default();
+        const NUM_IDS: u32 = 1000;
+        let mut ids = HashSet::new();
+        for _ in 0..NUM_IDS {
+            // `insert` returns false if the ID is already present in the set,
+            // indicating that the generator returned a non-unique ID.
+            assert!(ids.insert(generator.new_id()));
+        }
     }
 }
