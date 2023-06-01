f68ef08a55a859a6c02ff3bb684b34111a4ed7b3
Adam Barth <abarth@google.com>
2023-06-01 15:22:10 +0000

    [starnix] Share code between `starnix_runner` and `starnix_test_runner`
    This CL refactors the common code from `starnix_runner` and
    `starnix_test_runner` into a `kernel_manager` crate. The new
    `StarnixKernel` object is responsible for creating and destroying the
    `starnix_kernel` component and starting the Starnix container within
    that component.
    
    Change-Id: Ibaca38a4e0861d7fef5f6f35ec1713c4062fc6e1
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/864339
    Fuchsia-Auto-Submit: Adam Barth <abarth@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Kevin Lindkvist <lindkvist@google.com>

--- src/sys/test_runners/starnix/src/test_container.rs
+++ src/sys/test_runners/starnix/src/test_container.rs
@@ -5,42 +5,20 @@
 use {
     anyhow::{anyhow, Error},
     fidl::endpoints::{create_proxy, Proxy},
-    fidl_fuchsia_component as fcomponent, fidl_fuchsia_component_decl as fdecl,
-    fidl_fuchsia_component_runner as frunner, fidl_fuchsia_io as fio,
-    fuchsia_component::client as fclient,
+    fidl_fuchsia_component as fcomponent, fidl_fuchsia_component_runner as frunner,
     fuchsia_zircon as zx,
+    kernel_manager::StarnixKernel,
     tracing::debug,
 };
-
-/// The name of the collection in which the starnix kernel is instantiated.
-const RUNNERS_COLLECTION: &str = "runners";
 
 /// The component URL of the Starnix kernel.
 const KERNEL_URL: &str = "starnix_kernel#meta/starnix_kernel.cm";
 
-/// The protocol name the kernel uses to expose the container runner.
-const CONTAINER_RUNNER_PROTOCOL: &str = "fuchsia.starnix.container.Runner";
-
-fn generate_kernel_name(start_info: &frunner::ComponentStartInfo) -> Result<String, Error> {
-    let container_url = start_info.resolved_url.clone().ok_or(anyhow!("Missing resolved URL"))?;
-    let container_name = container_url
-        .split('/')
-        .last()
-        .expect("Could not find last path component in resolved URL");
-    Ok(kernel_config::generate_kernel_name(container_name))
-}
-
 pub struct TestContainer {
-    /// The realm in which the test runs.
-    realm: fcomponent::RealmProxy,
+    /// The Starnix kernel that is running the container.
+    kernel: StarnixKernel,
 
-    /// The name of the kernel used to run the test.
-    kernel_name: String,
-
-    /// The component controller for the container.
-    _container_controller: frunner::ComponentControllerProxy,
-
-    /// The runner use to run the test.
+    /// The runner used to run the test.
     ///
     /// This runner runs the test inside the container.
     pub component_runner: frunner::ComponentRunnerProxy,
@@ -54,60 +32,26 @@ impl TestContainer {
     ///   - `start_info`: The component start info used to create the configuration for the
     ///                   starnix container that will run the test.
     pub async fn create(mut start_info: frunner::ComponentStartInfo) -> Result<Self, Error> {
-        let kernel_name = generate_kernel_name(&start_info)?;
-
         // Grab the realm from the component's namespace.
         let realm =
             get_realm(start_info.ns.as_mut().ok_or(anyhow!("Couldn't get realm from namespace"))?)?;
 
-        realm
-            .create_child(
-                &fdecl::CollectionRef { name: RUNNERS_COLLECTION.into() },
-                &fdecl::Child {
-                    name: Some(kernel_name.clone()),
-                    url: Some(KERNEL_URL.to_string()),
-                    startup: Some(fdecl::StartupMode::Lazy),
-                    ..Default::default()
-                },
-                Default::default(),
-            )
-            .await?
-            .map_err(|e| anyhow::anyhow!("failed to create runner child: {:?}", e))?;
-
-        let kernel_outgoing =
-            open_exposed_directory(&realm, &kernel_name, RUNNERS_COLLECTION).await?;
-
-        let (container_controller, container_controller_server_end) =
+        let (_container_controller, container_controller_server_end) =
             create_proxy::<frunner::ComponentControllerMarker>()?;
 
-        let container_runner = fclient::connect_to_named_protocol_at_dir_root::<
-            frunner::ComponentRunnerMarker,
-        >(&kernel_outgoing, CONTAINER_RUNNER_PROTOCOL)?;
-        container_runner.start(start_info, container_controller_server_end)?;
+        let kernel =
+            StarnixKernel::create(realm, KERNEL_URL, start_info, container_controller_server_end)
+                .await?;
 
-        let component_runner = fclient::connect_to_protocol_at_dir_root::<
-            frunner::ComponentRunnerMarker,
-        >(&kernel_outgoing)?;
+        let component_runner = kernel.connect_to_protocol::<frunner::ComponentRunnerMarker>()?;
 
-        debug!(%kernel_name, "instantiated starnix test kernel");
-        Ok(Self {
-            realm,
-            kernel_name,
-            _container_controller: container_controller,
-            component_runner,
-        })
+        debug!(%kernel.name, "instantiated starnix test kernel");
+        Ok(Self { kernel, component_runner })
     }
 
-    /// Destroys the Starnix kernel that is running the given test.
+    /// Destroys this Starnix container.
     pub async fn destroy(&self) -> Result<(), Error> {
-        self.realm
-            .destroy_child(&fdecl::ChildRef {
-                name: self.kernel_name.clone(),
-                collection: Some(RUNNERS_COLLECTION.into()),
-            })
-            .await?
-            .map_err(|e| anyhow::anyhow!("failed to destory runner child: {:?}", e))?;
-        Ok(())
+        self.kernel.destroy().await
     }
 }
 
@@ -137,27 +81,4 @@ fn get_realm(
         })
         .next()
         .ok_or_else(|| anyhow!("Unable to find /svc"))?
-}
-
-async fn open_exposed_directory(
-    realm: &fcomponent::RealmProxy,
-    child_name: &str,
-    collection_name: &str,
-) -> Result<fio::DirectoryProxy, Error> {
-    let (directory_proxy, server_end) = fidl::endpoints::create_proxy::<fio::DirectoryMarker>()?;
-    realm
-        .open_exposed_dir(
-            &fdecl::ChildRef { name: child_name.into(), collection: Some(collection_name.into()) },
-            server_end,
-        )
-        .await?
-        .map_err(|e| {
-            anyhow!(
-                "failed to bind to child {} in collection {:?}: {:?}",
-                child_name,
-                collection_name,
-                e
-            )
-        })?;
-    Ok(directory_proxy)
 }
