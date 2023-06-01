96033e5591ba0ba791760faa8e68a59a3c49d94b
David Worsham <dworsham@google.com>
2023-06-01 06:01:01 +0000

    [lacewing] Allow passing -v to mobly test
    This is extremely helpful for debugging.
    
    Usage: `fx test run -- -v <lacewing test target>`
    
    Bug: b/276726514
    
    Change-Id: I5998bae584cd60593dfcfbe82afc4d6b9e28b5a5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/862580
    Reviewed-by: Pavan Kumar Juturu <jpavankumar@google.com>
    Commit-Queue: David Worsham <dworsham@google.com>
    Reviewed-by: Kevin Cho <chok@google.com>

--- src/testing/end_to_end/mobly_driver/tests/unit_tests/mobly_driver_lib_test.py
+++ src/testing/end_to_end/mobly_driver/tests/unit_tests/mobly_driver_lib_test.py
@@ -17,6 +17,7 @@ class MoblyDriverLibTest(unittest.TestCase):
     """Mobly Driver lib tests"""
 
     def setUp(self):
+        self.mock_tmp = mock.Mock()
         self.mock_process = mock.Mock()
         self.mock_driver = mock.Mock()
         self.mock_driver.generate_test_config.return_value = ''
@@ -37,10 +38,13 @@ class MoblyDriverLibTest(unittest.TestCase):
 
     @parameterized.expand(
         [
-            ('invalid_driver', None, '/py/path', '/test/path', 0),
-            ('invalid_python_path', mock.Mock(), '', '/test/path', 0),
-            ('invalid_test_path', mock.Mock(), '/py/path', '', 0),
-            ('invalid_timeout', mock.Mock(), '/py/path', '/test/path', -1)
+            ['invalid_driver', None, '/py/path', '/test/path', 0],
+            ['invalid_python_path',
+             mock.Mock(), '', '/test/path', 0],
+            ['invalid_test_path',
+             mock.Mock(), '/py/path', '', 0],
+            ['invalid_timeout',
+             mock.Mock(), '/py/path', '/test/path', -1]
         ])
     def test_run_invalid_argument_raises_exception(
             self, unused_name, driver, python_path, test_path, timeout_sec):
@@ -89,6 +93,30 @@ class MoblyDriverLibTest(unittest.TestCase):
         self.mock_driver.teardown.assert_called()
         self.assertIn(
             mock.call('MOCK_FAILURE_OUTPUT'), mock_print.call_args_list)
+
+    @parameterized.expand([[True], [False]])
+    @mock.patch('builtins.print')
+    @mock.patch('subprocess.Popen')
+    @mock.patch('mobly_driver_lib.NamedTemporaryFile')
+    def test_run_passes_params_to_popen(
+            self, verbose, mock_tempfile, mock_popen, *unused_args):
+        """Test case to ensure correct params are passed to Popen"""
+        tmp_path = '/tmp/path'
+        py_path = '/py/path'
+        test_path = '/test/path'
+        self.mock_tmp.name = tmp_path
+        self.mock_process.poll.side_effect = [None, 0]
+        mock_tempfile.return_value.__enter__.return_value = self.mock_tmp
+        mock_popen.return_value.__enter__.return_value = self.mock_process
+
+        mobly_driver_lib.run(
+            self.mock_driver, py_path, test_path, verbose=verbose)
+        mock_popen.assert_called_once_with(
+            [py_path, test_path, '-c', tmp_path] + (['-v'] if verbose else []),
+            stdout=mock.ANY,
+            stderr=mock.ANY,
+            universal_newlines=mock.ANY,
+            env=mock.ANY)
 
     @mock.patch('builtins.print')
     @mock.patch('subprocess.Popen')
