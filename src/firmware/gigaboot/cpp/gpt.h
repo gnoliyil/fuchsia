d62e6d041eb2ff7bed269fe1da2373b0c2d8bb3a
Victor Costan <costan@google.com>
2023-06-03 21:12:08 +0000

    [gigaboot] Fix const-safety issue in fbl::Vector iterator usage.
    Bug: 128479
    Change-Id: I258b9bf2fc05937c7891ac14ee68cc1eba6576e7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/865876
    Reviewed-by: Yecheng Zhao <zyecheng@google.com>
    Commit-Queue: Victor Costan <costan@google.com>
    Fuchsia-Auto-Submit: Victor Costan <costan@google.com>

--- src/firmware/gigaboot/cpp/gpt.h
+++ src/firmware/gigaboot/cpp/gpt.h
@@ -37,7 +37,7 @@ class EfiGptBlockDevice {
                                          size_t length);
 
   gpt_header_t const &GptHeader() const { return gpt_header_; }
-  cpp20::span<std::array<char, GPT_NAME_LEN / 2>> ListPartitionNames() const;
+  cpp20::span<const std::array<char, GPT_NAME_LEN / 2>> ListPartitionNames() const;
 
   size_t BlockSize() { return block_io_protocol_->Media->BlockSize; }
   uint64_t LastBlock() { return block_io_protocol_->Media->LastBlock; }
