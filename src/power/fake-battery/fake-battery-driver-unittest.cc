34623ae526fd483733125fafa9d2ac02bb88f9b9
Hai Bi <bihai@google.com>
2023-06-06 22:53:53 +0000

    [power] Add fake-battery-driver
    The fake battery driver can help with testing of the battery manager.
    
    Bug: b/283805532
    Test:
      fx set *.x64 --release --with-base=//src/power:battery
          --with=//src/power/fake-battery:fake_battery_component
      fx test fake-battery-driver-tests --test-filter=FakeBatteryDriver*.C*
    
    Change-Id: I67f20103cb0af7bf5acf6a7c678fb238670e6ffe
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/859787
    Commit-Queue: Hai Bi <bihai@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Reviewed-by: James Robinson <jamesr@google.com>
    Reviewed-by: Michael Brunson <mbrunson@google.com>

---
+++ src/power/fake-battery/fake-battery-driver-unittest.cc
@@ -0,0 +1,113 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "fake-battery-driver.h"
+
+#include <lib/async-loop/loop.h>
+#include <lib/async_patterns/testing/cpp/dispatcher_bound.h>
+#include <lib/driver/component/cpp/driver_cpp.h>
+#include <lib/driver/testing/cpp/driver_lifecycle.h>
+#include <lib/driver/testing/cpp/driver_runtime_env.h>
+#include <lib/driver/testing/cpp/test_environment.h>
+#include <lib/driver/testing/cpp/test_node.h>
+#include <lib/fdf/env.h>
+#include <lib/fdf/testing.h>
+#include <lib/syslog/cpp/macros.h>
+
+#include <gtest/gtest.h>
+
+using fuchsia_hardware_powersource::wire::BatteryUnit;
+using fuchsia_hardware_powersource::wire::PowerType;
+
+// If the environment needs to run on a managed driver dispatcher (for example if the driver needs
+// to make sync FIDL calls), we need to run the environment on a managed dispatcher while keeping
+// the driver on the main thread.
+class FakeBatteryDriverTest : public ::testing::Test {
+ public:
+  static void RunSyncClientTask(fit::closure task) {
+    // Spawn a separate thread to run the client task using an async::Loop.
+    async::Loop loop{&kAsyncLoopConfigNeverAttachToThread};
+    loop.StartThread();
+    zx::result result = fdf::RunOnDispatcherSync(loop.dispatcher(), std::move(task));
+    ASSERT_EQ(ZX_OK, result.status_value());
+  }
+  void SetUp() override {
+    // Create start args
+    zx::result start_args = node_server_.SyncCall(&fdf_testing::TestNode::CreateStartArgsAndServe);
+    EXPECT_EQ(ZX_OK, start_args.status_value());
+
+    // Start the test environment
+    zx::result init_result =
+        test_environment_.SyncCall(&fdf_testing::TestEnvironment::Initialize,
+                                   std::move(start_args->incoming_directory_server));
+    EXPECT_EQ(ZX_OK, init_result.status_value());
+    zx::result driver = fdf_testing::StartDriver<fake_battery::Driver>(
+        std::move(start_args->start_args), test_driver_dispatcher_);
+    EXPECT_EQ(ZX_OK, driver.status_value());
+    driver_ = driver.value();
+  }
+
+  void TearDown() override {
+    zx::result result = fdf_testing::TeardownDriver(driver_, test_driver_dispatcher_);
+    EXPECT_EQ(ZX_OK, result.status_value());
+  }
+
+  fake_battery::Driver* driver() { return driver_; }
+
+  async_dispatcher_t* env_dispatcher() { return test_env_dispatcher_.dispatcher(); }
+
+  async_patterns::TestDispatcherBound<fdf_testing::TestNode>& node_server() { return node_server_; }
+
+ private:
+  // This starts up the initial managed thread. It must come before the dispatcher.
+  fdf_testing::DriverRuntimeEnv managed_runtime_env_;
+
+  // Driver dispatcher. Started as the current thread's default dispatcher and not ran on the
+  // managed runtime thread pool.
+  fdf::TestSynchronizedDispatcher test_driver_dispatcher_{fdf::kDispatcherDefault};
+
+  // Env dispatcher. Managed by driver runtime threads because we need to make sync calls into it.
+  fdf::TestSynchronizedDispatcher test_env_dispatcher_{fdf::kDispatcherManaged};
+
+  async_patterns::TestDispatcherBound<fdf_testing::TestNode> node_server_{
+      env_dispatcher(), std::in_place, std::string("root")};
+
+  // The env_dispatcher is an fdf_dispatcher so we can add driver transport FIDL servers into this
+  // environment.
+  async_patterns::TestDispatcherBound<fdf_testing::TestEnvironment> test_environment_{
+      env_dispatcher(), std::in_place};
+
+  fake_battery::Driver* driver_;
+};
+
+TEST_F(FakeBatteryDriverTest, CanGetInfo) {
+  // Safe to touch the driver from here since the driver_dispatcher is the default.
+  // Dispatcher allows sync calls from the driver so we use the sync version.
+  zx::result device_result = node_server().SyncCall([](fdf_testing::TestNode* root_node) {
+    return root_node->children().at("fake-battery").ConnectToDevice();
+  });
+  ASSERT_EQ(ZX_OK, device_result.status_value());
+
+  fidl::ClientEnd<fuchsia_hardware_powersource::Source> device_client_end(
+      std::move(device_result.value()));
+
+  fidl::WireSyncClient<fuchsia_hardware_powersource::Source> client(std::move(device_client_end));
+  RunSyncClientTask([this_client = std::move(client)]() {
+    {
+      fidl::WireResult result = this_client->GetPowerInfo();
+      ASSERT_EQ(result.status(), ZX_OK);
+      ASSERT_EQ(result.value().status, ZX_OK);
+      const auto& info = result.value().info;
+      ASSERT_EQ(PowerType::kBattery, info.type);
+      ASSERT_EQ(fuchsia_hardware_powersource::kPowerStateCharging, info.state);
+    }
+    {
+      fidl::WireResult result = this_client->GetBatteryInfo();
+      ASSERT_EQ(result.status(), ZX_OK);
+      ASSERT_EQ(result.value().status, ZX_OK);
+      const auto& info = result.value().info;
+      ASSERT_EQ(info.present_rate, 2);
+    }
+  });
+}
