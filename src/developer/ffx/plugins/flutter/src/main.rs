62c71d2757f431862edd67504a7a16039f93f3d1
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2023-06-06 19:11:08 +0000

    [ffx][flutter] Delete
    Change-Id: I241b927ce98fc3c5553dcd064fab6931d302d2fb
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/862536
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>
    Reviewed-by: David Worsham <dworsham@google.com>
    Reviewed-by: Megan Batty <mgnb@google.com>

--- src/developer/ffx/plugins/flutter/src/main.rs
+++
@@ -1,8 +0,0 @@
-// Copyright 2022 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#[fuchsia_async::run_singlethreaded]
-async fn main() {
-    ffx_flutter_suite::fho_suite_main().await
-}
