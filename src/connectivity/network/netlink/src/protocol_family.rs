fd7dea34060f7c636a83b7d10a918a173862735c
Jeff Martin <martinjeffrey@google.com>
2023-06-06 02:02:46 +0000

    [netlink] Translate Legacy Groups to Modern Groups
    Evidently, legacy multicast groups have a power-of-two relationship to
    their modern counterpart. This means we no longer need to declare a
    static mapping between the two sets of constants for each protocol
    family, and can instead automatically convert between the two.
    
    Bug: b/280483454
    Change-Id: I7ca308b9cb89966f50d012c3bf31b820691eafeb
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/866377
    Reviewed-by: Ghanan Gowripalan <ghanan@google.com>
    Fuchsia-Auto-Submit: Jeff Martin <martinjeffrey@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/connectivity/network/netlink/src/protocol_family.rs
+++ src/connectivity/network/netlink/src/protocol_family.rs
@@ -20,7 +20,7 @@ use crate::{
     messaging::Sender,
     multicast_groups::{
         InvalidLegacyGroupsError, InvalidModernGroupError, LegacyGroups, ModernGroup,
-        MulticastCapableNetlinkFamily, SingleLegacyGroup,
+        MulticastCapableNetlinkFamily,
     },
 };
 
@@ -72,45 +72,6 @@ pub mod route {
     pub(crate) enum NetlinkRoute {}
 
     impl MulticastCapableNetlinkFamily for NetlinkRoute {
-        fn legacy_to_modern(group: SingleLegacyGroup) -> Option<ModernGroup> {
-            // Legacy NETLINK_ROUTE multicast groups.
-            const RTMGRP_LINK: u32 = 0x1;
-            const RTMGRP_NOTIFY: u32 = 0x2;
-            const RTMGRP_NEIGH: u32 = 0x4;
-            const RTMGRP_TC: u32 = 0x8;
-            const RTMGRP_IPV4_IFADDR: u32 = 0x10;
-            const RTMGRP_IPV4_MROUTE: u32 = 0x20;
-            const RTMGRP_IPV4_ROUTE: u32 = 0x40;
-            const RTMGRP_IPV4_RULE: u32 = 0x80;
-            const RTMGRP_IPV6_IFADDR: u32 = 0x100;
-            const RTMGRP_IPV6_MROUTE: u32 = 0x200;
-            const RTMGRP_IPV6_ROUTE: u32 = 0x400;
-            const RTMGRP_IPV6_RULE: u32 = 0x800;
-            const RTMGRP_DECNET_IFADDR: u32 = 0x1000;
-            const RTMGRP_DECNET_ROUTE: u32 = 0x2000;
-            const RTMGRP_IPV6_PREFIX: u32 = 0x20000;
-
-            match group.inner() {
-                RTMGRP_LINK => Some(RTNLGRP_LINK),
-                RTMGRP_NOTIFY => Some(RTNLGRP_NOTIFY),
-                RTMGRP_NEIGH => Some(RTNLGRP_NEIGH),
-                RTMGRP_TC => Some(RTNLGRP_TC),
-                RTMGRP_IPV4_IFADDR => Some(RTNLGRP_IPV4_IFADDR),
-                RTMGRP_IPV4_MROUTE => Some(RTNLGRP_IPV4_MROUTE),
-                RTMGRP_IPV4_ROUTE => Some(RTNLGRP_IPV4_ROUTE),
-                RTMGRP_IPV4_RULE => Some(RTNLGRP_IPV4_RULE),
-                RTMGRP_IPV6_IFADDR => Some(RTNLGRP_IPV6_IFADDR),
-                RTMGRP_IPV6_MROUTE => Some(RTNLGRP_IPV6_MROUTE),
-                RTMGRP_IPV6_ROUTE => Some(RTNLGRP_IPV6_ROUTE),
-                RTMGRP_IPV6_RULE => Some(RTNLGRP_IPV6_RULE),
-                RTMGRP_DECNET_IFADDR => Some(RTNLGRP_DECNET_IFADDR),
-                RTMGRP_DECNET_ROUTE => Some(RTNLGRP_DECNET_ROUTE),
-                RTMGRP_IPV6_PREFIX => Some(RTNLGRP_IPV6_PREFIX),
-                _ => None,
-            }
-            .map(ModernGroup)
-        }
-
         fn is_valid_group(ModernGroup(group): &ModernGroup) -> bool {
             match *group {
                 RTNLGRP_DCB
@@ -287,26 +248,18 @@ pub(crate) mod testutil {
     use netlink_packet_utils::Emitable;
 
     pub(crate) const LEGACY_GROUP1: u32 = 0x00000001;
-    pub(crate) const LEGACY_GROUP2: u32 = 0x00000010;
-    pub(crate) const LEGACY_GROUP3: u32 = 0x00000100;
-    pub(crate) const INVALID_LEGACY_GROUP: LegacyGroups = LegacyGroups(0x00001000);
+    pub(crate) const LEGACY_GROUP2: u32 = 0x00000002;
+    pub(crate) const LEGACY_GROUP3: u32 = 0x00000004;
+    pub(crate) const INVALID_LEGACY_GROUP: u32 = 0x00000008;
     pub(crate) const MODERN_GROUP1: ModernGroup = ModernGroup(1);
     pub(crate) const MODERN_GROUP2: ModernGroup = ModernGroup(2);
-    pub(crate) const MODERN_GROUP3: ModernGroup = ModernGroup(10);
-    pub(crate) const INVALID_MODERN_GROUP: ModernGroup = ModernGroup(20);
+    pub(crate) const MODERN_GROUP3: ModernGroup = ModernGroup(3);
+    pub(crate) const INVALID_MODERN_GROUP: ModernGroup = ModernGroup(4);
 
     #[derive(Debug)]
     pub(crate) enum FakeProtocolFamily {}
 
     impl MulticastCapableNetlinkFamily for FakeProtocolFamily {
-        fn legacy_to_modern(legacy_group: SingleLegacyGroup) -> Option<ModernGroup> {
-            match legacy_group.inner() {
-                LEGACY_GROUP1 => Some(MODERN_GROUP1),
-                LEGACY_GROUP2 => Some(MODERN_GROUP2),
-                LEGACY_GROUP3 => Some(MODERN_GROUP3),
-                _ => None,
-            }
-        }
         fn is_valid_group(group: &ModernGroup) -> bool {
             match *group {
                 MODERN_GROUP1 | MODERN_GROUP2 | MODERN_GROUP3 => true,
