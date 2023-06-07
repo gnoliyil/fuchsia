a93efd5d02bd14140716c3d2216e4c182f811248
Darren Chan <chandarren@google.com>
2023-06-07 18:08:50 +0000

    [fx] Refactor `fx publish` to python.
    Refactor the majority of `fx publish` to python.
    
    This will allow us to test this tool easier.
    
    Change-Id: I18b4953965869daea9bebddba6ef09fa0db09046
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/862399
    Reviewed-by: Erick Tryzelaar <etryzelaar@google.com>
    Reviewed-by: Aaron Wood <aaronwood@google.com>
    Commit-Queue: Darren Chan <chandarren@google.com>

---
+++ tools/devshell/contrib/terminal.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3.8
+# Copyright 2023 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""
+A utility class for python scripts writing to terminal output.
+"""
+
+import os
+import sys
+
+
+class Terminal:
+    suppress = False
+
+    def bold(text: str) -> str:
+        return Terminal._style(text, 1)
+
+    def red(text: str) -> str:
+        return Terminal._style(text, 91)
+
+    def green(text: str) -> str:
+        return Terminal._style(text, 92)
+
+    def yellow(text: str) -> str:
+        return Terminal._style(text, 93)
+
+    def supports_color() -> bool:
+        return sys.stdout.isatty() and sys.stderr.isatty(
+        ) and not os.environ.get('NO_COLOR')
+
+    def _print(*args, **kwargs) -> None:
+        if not Terminal.suppress:
+            print(*args, **kwargs)
+
+    def fatal(text: str) -> int:
+        Terminal._print(
+            Terminal.red(Terminal.bold('FATAL: ')) + text, file=sys.stderr)
+        sys.exit(1)
+
+    def error(text: str) -> None:
+        Terminal._print(
+            Terminal.red(Terminal.bold('ERROR: ')) + text, file=sys.stderr)
+
+    def warn(text: str) -> None:
+        Terminal._print(
+            Terminal.yellow(Terminal.bold('WARNING: ')) + text, file=sys.stderr)
+
+    def info(text: str) -> None:
+        Terminal._print(Terminal.green(Terminal.bold('INFO: ')) + text)
+
+    def _style(text: str, escape_code: int) -> str:
+        if Terminal.supports_color():
+            return f'\033[{escape_code}m{text}\033[0m'
+        else:
+            # If neither stdout nor stderr is not a tty then any styles likely
+            # won't get rendered correctly when the text is eventually printed,
+            # so don't apply the style.
+            return text
