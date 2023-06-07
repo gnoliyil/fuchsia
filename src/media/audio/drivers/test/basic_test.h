cb5045d8098457910c2b0a17d91c3b742917b5e6
Martin Puryear <mpuryear@google.com>
2023-06-07 23:40:06 +0000

    [audio] Make BasicTest purely non-destructive
    Restore the previous gain state, so that all BasicTest cases
    are purely non-destructive (if they change the device's
    state, they subsequently restore it).
    
    Clarify the definition of BasicTest versus AdminTest.
    
    Add checks to ensure that a client can run any subset of
    test cases without depending on previous cases.
    
    Multiply: audio_driver_tests
    
    Change-Id: Ic0b579d6ca0b21fadce7e6d9b243e3b5f573d9db
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/867696
    Commit-Queue: Martin Puryear <mpuryear@google.com>
    Reviewed-by: Andres Oportus <andresoportus@google.com>

--- src/media/audio/drivers/test/basic_test.h
+++ src/media/audio/drivers/test/basic_test.h
@@ -6,19 +6,29 @@
 #define SRC_MEDIA_AUDIO_DRIVERS_TEST_BASIC_TEST_H_
 
 #include <fuchsia/hardware/audio/cpp/fidl.h>
-#include <fuchsia/media/cpp/fidl.h>
 #include <zircon/device/audio.h>
+
+#include <optional>
 
 #include "src/lib/fsl/io/device_watcher.h"
 #include "src/media/audio/drivers/test/test_base.h"
 
 namespace media::audio::drivers::test {
 
+// BasicTest cases must run in environments where an audio driver may already have an active client.
+// For most hardware functions, testing is limited to validating the correctness and consistency of
+// the declared capabilities and current state. BasicTest cases CAN _change_ a device's state, but
+// only if it fully restores the previous state afterward (as it does when testing SetGain).
+//
+// A driver can have only one RingBuffer client connection at any time, so BasicTest avoids any
+// usage of the RingBuffer interface. (Note: AdminTest is not limited to RingBuffer cases.)
 class BasicTest : public TestBase {
  public:
   explicit BasicTest(const DeviceEntry& dev_entry) : TestBase(dev_entry) {}
 
  protected:
+  void TearDown() override;
+
   void RequestHealthState();
   void GetHealthState(fuchsia::hardware::audio::Health::GetHealthStateCallback cb);
 
@@ -38,9 +48,12 @@ class BasicTest : public TestBase {
  private:
   static constexpr size_t kUniqueIdLength = 16;
 
-  fuchsia::hardware::audio::StreamProperties stream_props_;
-  fuchsia::hardware::audio::GainState gain_state_;
-  fuchsia::hardware::audio::GainState set_gain_state_;
+  // BasicTest cannot permanently change device state. Optionals ensure we fetch stream props and
+  // initial gain state (to later restore it), before calling any method that alters device state.
+  std::optional<fuchsia::hardware::audio::StreamProperties> stream_props_;
+  std::optional<fuchsia::hardware::audio::GainState> previous_gain_state_;
+
+  bool changed_gain_state_ = false;
   fuchsia::hardware::audio::PlugState plug_state_;
 };
 
