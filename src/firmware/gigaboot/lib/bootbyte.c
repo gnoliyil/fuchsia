93cf1ed835df06d03fe6460027c6d344673d34e2
Leonard Chan <leonardchan@google.com>
2023-06-07 19:14:08 +0000

    [firmware] Temporarily suppress function type mismatch on Get/SetVariable
    UBSan was detecting a function type mismatch on the Get/SetVariable
    functions used in fastboot_test.cc. Namely, UBSan says the function type
    of efi_runtime_services::Get/SetVariable doesn't match that of
    EfiBootbyteOwner::Get/SetVariable. To unblock the clang roll, let's
    temporarily disable where this called for now, then we'll come back to
    investigate further.
    
    Bug: 128274
    Change-Id: Ia830d8d03e11257e3dd9ce45ea1c53bb96aa564a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/867706
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Leonard Chan <leonardchan@google.com>

--- src/firmware/gigaboot/lib/bootbyte.c
+++ src/firmware/gigaboot/lib/bootbyte.c
@@ -13,7 +13,13 @@
 char16_t kBootbyteVariableName[] = ZIRCON_BOOTBYTE_EFIVAR;
 efi_guid kZirconVendorGuid = ZIRCON_VENDOR_GUID;
 
-efi_status get_bootbyte(efi_runtime_services* runtime, uint8_t* bootbyte) {
+// FIXME(fxbug.dev/128274): UBSan is detecting a function type mismatch between
+// this and EfiBootbyOwner::GetVariable in fastboot_test.cc. I'm not exactly
+// sure where the mismatch occurs since both functions have the same attribute,
+// return type, and argument types. But for now, let's suppress the warning to
+// allow the clang roll to proceed.
+__attribute__((no_sanitize("function"))) efi_status get_bootbyte(efi_runtime_services* runtime,
+                                                                 uint8_t* bootbyte) {
   size_t size = sizeof(*bootbyte);
   efi_status status = runtime->GetVariable((char16_t*)kBootbyteVariableName, &kZirconVendorGuid,
                                            NULL, &size, bootbyte);
@@ -23,7 +29,13 @@ efi_status get_bootbyte(efi_runtime_services* runtime, uint8_t* bootbyte) {
   return status;
 }
 
-efi_status set_bootbyte(efi_runtime_services* runtime, uint8_t bootbyte) {
+// FIXME(fxbug.dev/128274): UBSan is detecting a function type mismatch between
+// this and EfiBootbyOwner::SetVariable in fastboot_test.cc. I'm not exactly
+// sure where the mismatch occurs since both functions have the same attribute,
+// return type, and argument types. But for now, let's suppress the warning to
+// allow the clang roll to proceed.
+__attribute__((no_sanitize("function"))) efi_status set_bootbyte(efi_runtime_services* runtime,
+                                                                 uint8_t bootbyte) {
   return runtime->SetVariable((char16_t*)kBootbyteVariableName, &kZirconVendorGuid,
                               ZIRCON_BOOTBYTE_EFIATTR, sizeof(bootbyte), &bootbyte);
 }
