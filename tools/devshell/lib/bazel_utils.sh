268b5054a1a20441ef227b399f3e2841893c626e
David 'Digit' Turner <digit@google.com>
2023-06-09 16:46:44 +0000

    [fx][build] Add `bazel-tool` tool
    Add a new script named `bazel-tool` that provides command to perform
    useful operations inside Bazel workspaces. For now this only implements
    the `target_dump` command which shows the final target definitions
    after all macro expansions. More commands will follow in the future.
    
    This script is intended to be used out-of-tree one day, so should
    not depend on non-standard Python modules, and should not invoke
    fuchsia-vendored-python, intentionally.
    
    + Add `fx bazel-tool` command to invoke the tool inside the Fuchsia
      build's internal Bazel workspace.
    
    + Refactor tools/devshell bazel-related files to make it easier
      to locate Bazel-related items and invoke our Bazel wrapper
      script.
    
    Bug: None
    Change-Id: I76e7148aeee72917bcc5df34cc151f5a353868e1
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/868556
    Reviewed-by: Jiaming Li <lijiaming@google.com>
    Commit-Queue: David Turner <digit@google.com>

--- tools/devshell/lib/bazel_utils.sh
+++ tools/devshell/lib/bazel_utils.sh
@@ -5,7 +5,6 @@
 # Return the top-directory of a given Bazel workspace used by the platform
 # build. A TOPDIR contains several files and directories like workspace/
 # or output_base/
-#
 fx-bazel-top-dir () {
   # See //build/bazel/config/README.md
   #
@@ -14,4 +13,26 @@ fx-bazel-top-dir () {
   local INPUT_FILE="${FUCHSIA_DIR}/build/bazel/config/${1:-main}_workspace_top_dir"
   local TOPDIR=$(cat "${INPUT_FILE}")
   echo "${FUCHSIA_BUILD_DIR}/${TOPDIR}"
+}
+
+# Return path to Bazel workspace.
+fx-get-bazel-workspace () {
+  printf %s/workspace "$(fx-bazel-top-dir)"
+}
+
+# Return the path to the Bazel wrapper script.
+fx-get-bazel () {
+   printf %s/bazel "$(fx-bazel-top-dir)"
+}
+
+# Regenerate Bazel workspace and launcher script if needed.
+# Note that this also regenerates the Ninja build plan if necessary.
+fx-update-bazel-workspace () {
+  "${FUCHSIA_DIR}"/build/bazel/scripts/update_workspace.py
+}
+
+# Run bazel command in the Fuchsia workspace, after ensuring it is up-to-date.
+fx-bazel () {
+  fx-update-bazel-workspace
+  "$(fx-get-bazel)" "$@"
 }
