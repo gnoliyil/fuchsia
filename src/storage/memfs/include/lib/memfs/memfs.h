15c93ba48d1cfbecc1f1e90efc2ef4c5db8ceb8a
Tamir Duberstein <tamird@gmail.com>
2023-06-10 14:10:24 +0000

    [memfs] Remove memfs_create_filesystem
    Change-Id: Ie8583bc98eb89b5845a3a044da2a11f4a34c0c9e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/867599
    Reviewed-by: Adam Barth <abarth@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    API-Review: Adam Barth <abarth@google.com>
    Fuchsia-Auto-Submit: Tamir Duberstein <tamird@google.com>

--- src/storage/memfs/include/lib/memfs/memfs.h
+++ src/storage/memfs/include/lib/memfs/memfs.h
@@ -14,23 +14,9 @@ __BEGIN_CDECLS
 
 typedef struct memfs_filesystem memfs_filesystem_t;
 
-// Create an in-memory filesystem. It will run on the given dispatcher.
+// Creates an in-memory filesystem and installs it into the local namespace at the given path.
 //
 // The number of pages in this memfs is bounded by the amount of available physical memory.
-//
-// Returns the MemFS filesystem object in |out_fs|. This object must be freed by
-// memfs_free_filesystem().
-//
-// Returns a handle to the root directory in |out_root|. The caller can install it in the local
-// namespace (mount it at a given path for the current process) or use it directly for file
-// operations. Many callers will want to install it in the local namespace and can use
-// memfs_install_at() for convenience instead of this function.
-__EXPORT zx_status_t memfs_create_filesystem(async_dispatcher_t* dispatcher,
-                                             memfs_filesystem_t** out_fs, zx_handle_t* out_root);
-
-// Creates an in-memory filesystem and installs it into the local namespace at the given path.
-// This is an alternative to memfs_create_filesystem() for convenience; see that function for
-// more.
 //
 // Returns the MemFS filesystem object in |out_fs|. This object must be freed by
 // memfs_free_filesystem().
@@ -42,8 +28,8 @@ __EXPORT zx_status_t memfs_install_at(async_dispatcher_t* dispatcher, const char
 // Frees a MemFS filesystem, unmounting any sub-filesystems that may exist. If memfs_install_at()
 // was used, the installation in the local namespace will be removed before this function returns.
 //
-// Requires that the async handler dispatcher provided to memfs_create_filesystem() or
-// memfs_install_at() still be running. Otherwise the completion argument will never be signaled.
+// Requires that the async handler dispatcher provided to memfs_install_at() still be running.
+// Otherwise the completion argument will never be signaled.
 //
 // Signals the optional argument |unmounted| when memfs has torn down. If this is null the
 // filesystem will still be torn down asynchronously (possibly in the future) which requires that
