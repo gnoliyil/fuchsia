5d643235fb2249a0fda23f7ddfa49fa12c574506
Benjamin Lerman <qsr@chromium.org>
2023-06-12 16:51:40 +0000

    [starnix] Implements mknod on FUSE fs.
    Bug: 125496
    Change-Id: Ie377da3ecb6dab199fed4ce934de8c2e2b0d3c78
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/869777
    Fuchsia-Auto-Submit: Benjamin Lerman <qsr@google.com>
    Reviewed-by: Kevin Lindkvist <lindkvist@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/starnix/tests/syscalls/fuse_test.cc
+++ src/starnix/tests/syscalls/fuse_test.cc
@@ -157,4 +157,12 @@ TEST_F(FuseTest, NoFile) {
   ASSERT_FALSE(fd.is_valid());
   ASSERT_EQ(errno, ENOENT);
 }
+
+TEST_F(FuseTest, Mknod) {
+  ASSERT_TRUE(Mount());
+  std::string filename = GetMountDir() + "/file";
+  ScopedFD fd(open(filename.c_str(), O_WRONLY | O_CREAT));
+  ASSERT_TRUE(fd.is_valid());
+}
+
 #endif  // SRC_STARNIX_TESTS_SYSCALLS_PROC_TEST_H_
