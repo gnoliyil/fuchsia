5a5c85932db11b71e4d16f8e42bc3baec313abf7
Tamir Duberstein <tamird@google.com>
2023-06-13 17:06:26 +0000

    [logs] Improve CTS test for legacy logs
    Address a few problems with this test:
    - Always call async::Loop::Quit, including on failure. Previously this
      test would hang on assertion failure.
    - Avoid repeating message and tag strings.
    - Avoid hardcoding the log line number by using `__LINE__`.
    
    Change-Id: If206326a0bcbedd104c0a83d5614035580ada247
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/870516
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Tamir Duberstein <tamird@google.com>
    Reviewed-by: Brian Bosak <bbosak@google.com>

--- sdk/ctf/tests/fidl/fuchsia.diagnostics/logging_test.cc
+++ sdk/ctf/tests/fidl/fuchsia.diagnostics/logging_test.cc
@@ -5,13 +5,15 @@
 #include <fuchsia/logger/cpp/fidl.h>
 #include <lib/async-loop/cpp/loop.h>
 #include <lib/async-loop/default.h>
+#include <lib/fidl/cpp/binding.h>
+#include <lib/fit/defer.h>
 #include <lib/fit/function.h>
 #include <lib/sys/cpp/component_context.h>
+#include <lib/syslog/cpp/logging_backend.h>
 #include <lib/syslog/cpp/macros.h>
 
+#include <fbl/string_printf.h>
 #include <zxtest/zxtest.h>
-
-#include "lib/syslog/cpp/logging_backend.h"
 
 class LogTest : public zxtest::Test {};
 
@@ -19,9 +21,9 @@ namespace {
 
 class LogListener : public fuchsia::logger::LogListenerSafe {
  public:
-  explicit LogListener(fit::function<void(::fuchsia::logger::LogMessage)> message_callback,
-                       zx::channel channel)
-      : binding_(this), message_callback_(std::move(message_callback)) {
+  explicit LogListener(fit::function<void(const fuchsia::logger::LogMessage&)> message_callback,
+                       fidl::InterfaceRequest<fuchsia::logger::LogListenerSafe> channel)
+      : message_callback_(std::move(message_callback)) {
     binding_.Bind(std::move(channel));
   }
 
@@ -29,8 +31,8 @@ class LogListener : public fuchsia::logger::LogListenerSafe {
   ///
   /// The return value is used for flow control, and implementers should acknowledge receipt of
   /// each message in order to continue receiving future messages.
-  void Log(::fuchsia::logger::LogMessage log, LogCallback callback) override {
-    message_callback_(std::move(log));
+  void Log(fuchsia::logger::LogMessage log, LogCallback callback) override {
+    message_callback_(log);
     callback();
   }
 
@@ -40,10 +42,9 @@ class LogListener : public fuchsia::logger::LogListenerSafe {
   ///
   /// The return value is used for flow control, and implementers should acknowledge receipt of
   /// each batch in order to continue receiving future messages.
-  void LogMany(::std::vector<::fuchsia::logger::LogMessage> log,
-               LogManyCallback callback) override {
-    for (auto& msg : log) {
-      message_callback_(std::move(msg));
+  void LogMany(std::vector<fuchsia::logger::LogMessage> log, LogManyCallback callback) override {
+    for (const fuchsia::logger::LogMessage& msg : log) {
+      message_callback_(msg);
     }
     callback();
   }
@@ -52,31 +53,33 @@ class LogListener : public fuchsia::logger::LogListenerSafe {
   void Done() override {}
 
  private:
-  fidl::Binding<fuchsia::logger::LogListenerSafe> binding_;
-  fit::function<void(::fuchsia::logger::LogMessage)> message_callback_;
+  fidl::Binding<fuchsia::logger::LogListenerSafe> binding_{this};
+  fit::function<void(const fuchsia::logger::LogMessage&)> message_callback_;
 };
 
 TEST_F(LogTest, LegacyFormat) {
   async::Loop loop(&kAsyncLoopConfigAttachToCurrentThread);
-  FX_SLOG(INFO, "Test message", KV("tag", "test"), KV("tag", "test2"));
+  constexpr const char payload[] = "test message";
+  constexpr const char tag1[] = "test1";
+  constexpr const char tag2[] = "test2";
+  constexpr size_t line = __LINE__ + 1;
+  FX_SLOG(INFO, payload, KV("tag", tag1), KV("tag", tag2));
   fuchsia::logger::LogPtr accessor;
-  auto context = sys::ComponentContext::Create();
-  context->svc()->Connect(accessor.NewRequest());
-  zx::channel local, remote;
-  zx::channel::create(0, &local, &remote);
-  fidl::InterfaceHandle<fuchsia::logger::LogListenerSafe> listener_interface(std::move(remote));
-  accessor->ListenSafe(std::move(listener_interface),
-                       std::unique_ptr<fuchsia::logger::LogFilterOptions>(nullptr));
+  std::unique_ptr context = sys::ComponentContext::Create();
+  ASSERT_OK(context->svc()->Connect(accessor.NewRequest()));
+  fidl::InterfaceHandle<fuchsia::logger::LogListenerSafe> handle;
+  fidl::InterfaceRequest request = handle.NewRequest();
+  accessor->ListenSafe(std::move(handle), nullptr);
   LogListener listener(
-      [&](::fuchsia::logger::LogMessage message) {
-        ASSERT_EQ(message.msg,
-                  "[sdk/ctf/tests/fidl/fuchsia.diagnostics/logging_test.cc(61)] Test message");
-        ASSERT_EQ(message.tags.size(), 2);
-        ASSERT_EQ(message.tags[0], "test");
-        ASSERT_EQ(message.tags[1], "test2");
-        loop.Quit();
+      [&](const fuchsia::logger::LogMessage& message) {
+        auto quit = fit::defer([&] { loop.Quit(); });
+        const fbl::String msg = fbl::StringPrintf(
+            "[sdk/ctf/tests/fidl/fuchsia.diagnostics/logging_test.cc(%zu)] %s", line, payload);
+        ASSERT_EQ(message.msg, msg);
+        const std::vector<std::string> tags = {tag1, tag2};
+        ASSERT_EQ(message.tags, tags);
       },
-      std::move(local));
+      std::move(request));
   loop.Run();
 }
 }  // namespace
