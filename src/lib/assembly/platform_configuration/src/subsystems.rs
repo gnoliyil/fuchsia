bedbeb91d284869da18793fbafa69917a314d509
Justin Mattson <jmatt@google.com>
2023-06-13 16:57:58 +0000

    [assembly][swd] Add structured config support
    Put assembly in control of what system update checker pieces are
    in the build.
    
    This hollows out //src/sys/pkg:* groups so that depending on
    those no longer adds things to the build output.
    
    Bug: 123798
    
    Change-Id: Idee61888bf950fb6ae076f08a411cbcc8c977b3f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/867699
    Reviewed-by: Aaron Wood <aaronwood@google.com>
    Commit-Queue: Justin Mattson <jmatt@google.com>
    Reviewed-by: Sen Jiang <senj@google.com>

--- src/lib/assembly/platform_configuration/src/subsystems.rs
+++ src/lib/assembly/platform_configuration/src/subsystems.rs
@@ -164,7 +164,6 @@ fn configure_subsystems(
     }
 
     // The real platform subsystems
-
     connectivity::ConnectivitySubsystemConfig::define_configuration(
         context,
         &config.platform.connectivity,
@@ -242,6 +241,13 @@ fn configure_subsystems(
         builder,
     )
     .context("Configuring the 'storage' subsystem")?;
+
+    swd::SwdSubsystemConfig::define_configuration(
+        context,
+        &config.platform.software_delivery,
+        builder,
+    )
+    .context("Configuring the 'software_delivery' subsystem")?;
 
     thermal::ThermalSubsystem::define_configuration(context, &(), builder)
         .context("Configuring the 'thermal' subsystem")?;
