1e7e8514c48740d0b4760a3985eee4f173fa60ea
Jocelyn Dang <jocelyndang@google.com>
2023-06-13 00:33:20 +0000

    [driver_runtime] Deprecate CreateWithLoop.
    I moved the ShutdownProcessTest into a new file, as it affects
    the process loop.
    
    Multiply: fuchsia-pkg://fuchsia.com/driver_runtime_tests#meta/driver_runtime_tests.cm : 50
    Multiply: fuchsia-pkg://fuchsia.com/driver_runtime_tests#meta/driver_runtime_shutdown_process_tests.cm : 50
    
    Bug: 123943
    
    Change-Id: I8c25747c1afcda7f554bbfd71f38c9f0663f29d0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/869242
    Reviewed-by: Novin Changizi <novinc@google.com>
    Commit-Queue: Jocelyn Dang <jocelyndang@google.com>

---
+++ src/devices/bin/driver_runtime/shutdown_process_test.cc
@@ -0,0 +1,71 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/driver/testing/cpp/driver_runtime_env.h>
+#include <lib/fdf/channel.h>
+#include <lib/fdf/cpp/channel_read.h>
+#include <lib/fdf/cpp/dispatcher.h>
+#include <lib/fdf/cpp/env.h>
+#include <lib/fdf/testing.h>
+#include <lib/fit/defer.h>
+#include <lib/sync/cpp/completion.h>
+#include <lib/zx/event.h>
+#include <zircon/errors.h>
+
+#include <zxtest/zxtest.h>
+
+#include "lib/fdf/dispatcher.h"
+#include "src/devices/bin/driver_runtime/dispatcher.h"
+#include "src/devices/bin/driver_runtime/driver_context.h"
+#include "src/devices/bin/driver_runtime/runtime_test_case.h"
+
+namespace driver_runtime {
+extern DispatcherCoordinator& GetDispatcherCoordinator();
+}
+
+// This is done separately to the DispatcherTest as it shuts down the process
+// wide async loop backing the dispatcher, and that can't be restarted.
+class ShutdownProcessTest : public RuntimeTestCase {
+ protected:
+  fdf_testing::DriverRuntimeEnv managed_env;
+};
+
+// Tests shutting down the process async loop while requests are still pending.
+TEST_F(ShutdownProcessTest, ShutdownProcessAsyncLoop) {
+  const void* driver = CreateFakeDriver();
+  libsync::Completion shutdown_completion;
+  auto shutdown_handler = [&](fdf_dispatcher_t* dispatcher) { shutdown_completion.Signal(); };
+
+  auto dispatcher =
+      fdf_env::DispatcherBuilder::CreateUnsynchronizedWithOwner(driver, {}, "", shutdown_handler);
+  ASSERT_FALSE(dispatcher.is_error());
+
+  auto channels = fdf::ChannelPair::Create(0);
+  ASSERT_OK(channels.status_value());
+  auto local = std::move(channels->end0);
+  auto remote = std::move(channels->end1);
+
+  libsync::Completion entered_read;
+  auto channel_read = std::make_unique<fdf::ChannelRead>(
+      local.get(), 0,
+      [&](fdf_dispatcher_t* dispatcher, fdf::ChannelRead* channel_read, zx_status_t status) {
+        entered_read.Signal();
+        // Do not let the read callback complete until the loop has entered a shutdown state.
+        auto* loop = driver_runtime::GetDispatcherCoordinator().default_thread_pool()->loop();
+        while (loop->GetState() != ASYNC_LOOP_SHUTDOWN) {
+        }
+      });
+  ASSERT_OK(channel_read->Begin(dispatcher->get()));
+
+  // Call is reentrant, so the read will be queued on the async loop.
+  ASSERT_EQ(ZX_OK, fdf_channel_write(remote.get(), 0, nullptr, nullptr, 0, nullptr, 0));
+  // This will queue the wait to run |Dispatcher::CompleteShutdown|.
+  dispatcher->ShutdownAsync();
+
+  ASSERT_OK(entered_read.Wait(zx::time::infinite()));
+
+  driver_runtime::GetDispatcherCoordinator().default_thread_pool()->loop()->Shutdown();
+
+  shutdown_completion.Wait();
+}
