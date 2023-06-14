f27988760e9dc8b589545c5b73bdb0c27ed9ff7e
Yilong Li <liyl@google.com>
2023-06-14 02:35:48 +0000

    [display][api-types-cpp] Make conversion methods constexpr inline wherever possible.
    For StrongInt-based value wrappers, we make conversion methods
    from / to C++ fundamental types constexpr inline so that they
    can be used for constexpr expressions which are useful in tests.
    For more complicated conversion methods where ZX_DEBUG_ASSERT()
    or ZX_ASSERT() are involved we don't add the constexpr specifier.
    
    Bug: 128286
    Change-Id: Ic1c4e9dc7f3b4a1b2c9e6666374015f1a0012d34
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/868713
    Fuchsia-Auto-Submit: Yilong Li <liyl@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Victor Costan <costan@google.com>

--- src/graphics/display/lib/api-types-cpp/config-stamp.h
+++ src/graphics/display/lib/api-types-cpp/config-stamp.h
@@ -17,16 +17,18 @@ namespace display {
 // More useful representation of `fuchsia.hardware.display/ConfigStampValue`.
 DEFINE_STRONG_INT(ConfigStamp, uint64_t);
 
-inline ConfigStamp ToConfigStamp(config_stamp_t banjo_config_stamp) {
+constexpr inline ConfigStamp ToConfigStamp(config_stamp_t banjo_config_stamp) {
   return ConfigStamp(banjo_config_stamp.value);
 }
-inline ConfigStamp ToConfigStamp(fuchsia_hardware_display::wire::ConfigStamp fidl_config_stamp) {
+constexpr inline ConfigStamp ToConfigStamp(
+    fuchsia_hardware_display::wire::ConfigStamp fidl_config_stamp) {
   return ConfigStamp(fidl_config_stamp.value);
 }
-inline config_stamp_t ToBanjoConfigStamp(ConfigStamp config_stamp) {
+constexpr inline config_stamp_t ToBanjoConfigStamp(ConfigStamp config_stamp) {
   return config_stamp_t{.value = config_stamp.value()};
 }
-inline fuchsia_hardware_display::wire::ConfigStamp ToFidlConfigStamp(ConfigStamp config_stamp) {
+constexpr inline fuchsia_hardware_display::wire::ConfigStamp ToFidlConfigStamp(
+    ConfigStamp config_stamp) {
   return fuchsia_hardware_display::wire::ConfigStamp{.value = config_stamp.value()};
 }
 
