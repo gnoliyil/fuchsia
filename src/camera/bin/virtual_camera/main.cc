ca722556f522c6d8be18913844faeb1dcead8f39
Yegor Pomortsev <ypomortsev@google.com>
2023-06-15 21:16:24 +0000

    [modular] Delete //sdk/lib/modular
    Bug: 116651
    Change-Id: Iaa1e23285d4a183651fb68ba18adaf3f8b379a5b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/871566
    Reviewed-by: Miguel Flores <miguelfrde@google.com>
    API-Review: Adam Barth <abarth@google.com>
    Commit-Queue: Yegor Pomortsev <ypomortsev@google.com>
    Reviewed-by: Adam Barth <abarth@google.com>

--- src/camera/bin/virtual_camera/main.cc
+++ src/camera/bin/virtual_camera/main.cc
@@ -4,18 +4,15 @@
 
 #include <lib/async-loop/cpp/loop.h>
 #include <lib/async-loop/default.h>
-#include <lib/modular/cpp/agent.h>
 #include <lib/sys/cpp/component_context.h>
 
 #include "src/camera/bin/virtual_camera/stream_storage.h"
 #include "src/camera/bin/virtual_camera/virtual_camera_agent.h"
 
 int main(int argc, char** argv) {
-  // Create the VirtualCameraAgent which will serve as the agent entrypoint.
-  // Start the async::Loop until the modular::Agent quits.
+  // Create the VirtualCameraAgent which will serve protocols.
   async::Loop loop(&kAsyncLoopConfigAttachToCurrentThread);
   auto component_context = sys::ComponentContext::CreateAndServeOutgoingDirectory();
-  modular::Agent agent(component_context->outgoing(), [&loop] { loop.Quit(); });
   camera::StreamStorage stream_storage;
   camera::VirtualCameraAgent virtual_camera_agent(component_context.get(), stream_storage);
   loop.Run();
