12e3bb94608e289b160c2c33f84af6d8316e892b
Sergey Ulanov <sergeyu@google.com>
2023-06-15 06:26:05 +0000

    [Starnix] Implement generate_cfi_directives for RISC-V
    Bug: 128554
    Change-Id: I687757823ee5dd6106b82c59711d7bf82b8ebde6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/871416
    Fuchsia-Auto-Submit: Sergey Ulanov <sergeyu@google.com>
    Commit-Queue: Sergey Ulanov <sergeyu@google.com>
    Reviewed-by: Kevin Lindkvist <lindkvist@google.com>

--- src/starnix/kernel/arch/riscv64/execution.rs
+++ src/starnix/kernel/arch/riscv64/execution.rs
@@ -15,7 +15,53 @@ use crate::{
 #[macro_export]
 macro_rules! generate_cfi_directives {
     ($state:expr) => {
-        // TODO(fxbug.dev/128554): implement riscv support.
+        unsafe {
+            let state_addr = std::ptr::addr_of!($state);
+            // The base address that the unwinder will use is stored in t6. Then it will look for
+            // each register value at an offset specified below. These offsets match the offsets of
+            // the register values in the `zx_restricted_state_t` struct.
+            std::arch::asm!(
+                ".cfi_remember_state",
+                ".cfi_def_cfa t6, 0",
+                ".cfi_offset 32, 0",  // GNU Assembler doesn't recognize pc.
+                ".cfi_offset ra, 0x08",
+                ".cfi_offset sp, 0x10",
+                ".cfi_offset gp, 0x18",
+                ".cfi_offset tp, 0x20",
+                ".cfi_offset t0, 0x28",
+                ".cfi_offset t1, 0x30",
+                ".cfi_offset t2, 0x38",
+                ".cfi_offset s0, 0x40",
+                ".cfi_offset s1, 0x48",
+                ".cfi_offset a0, 0x50",
+                ".cfi_offset a1, 0x58",
+                ".cfi_offset a2, 0x60",
+                ".cfi_offset a3, 0x68",
+                ".cfi_offset a4, 0x70",
+                ".cfi_offset a5, 0x78",
+                ".cfi_offset a6, 0x80",
+                ".cfi_offset a7, 0x88",
+                ".cfi_offset s2, 0x90",
+                ".cfi_offset s3, 0x98",
+                ".cfi_offset s4, 0xA0",
+                ".cfi_offset s5, 0xA8",
+                ".cfi_offset s6, 0xB0",
+                ".cfi_offset s7, 0xB8",
+                ".cfi_offset s8, 0xC0",
+                ".cfi_offset s9, 0xC8",
+                ".cfi_offset s10, 0xD0",
+                ".cfi_offset s11, 0xD8",
+                ".cfi_offset t3, 0xE0",
+                ".cfi_offset t4, 0xE8",
+                ".cfi_offset t5, 0xF0",
+                ".cfi_offset t6, 0xF8",
+
+                // t6 could technically get clobbered between here and `execute_syscall`. We should
+                // use a method for computing `.cfi_def_cfa` that can't fail (e.g., sp offset).
+                in("t6") state_addr,
+                options(nomem, preserves_flags, nostack),
+            );
+        }
     };
 }
 
