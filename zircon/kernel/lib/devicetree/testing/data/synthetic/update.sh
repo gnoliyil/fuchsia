f0f14beb4b1e955b18ed3388e9d231a19530c631
Gianfranco Valentino <gevalentino@google.com>
2023-06-16 23:32:29 +0000

    [devicetree] Fix conflict in script
    Change-Id: Iefc21a8a658fd39f498abab14a1cc02e0ec3cc22
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/872671
    Commit-Queue: Gianfranco Valentino <gevalentino@google.com>
    Reviewed-by: Tamir Duberstein <tamird@google.com>

--- zircon/kernel/lib/devicetree/testing/data/synthetic/update.sh
+++ zircon/kernel/lib/devicetree/testing/data/synthetic/update.sh
@@ -5,25 +5,16 @@
 
 set -o errexit nounset
 set -u
-<<<<<<< HEAD
 
 readonly DTS_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 readonly IN_FORMAT="dts"
 readonly OUT_FORMAT="dtb"
 
-=======
-readonly DTS_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-readonly IN_FORMAT="dts"
-readonly OUT_FORMAT="dtb"
->>>>>>> 45c1e0c9dff ([boot-shim] Bootstrap '/chosen')
 if ! command -v dtc &>/dev/null; then
   echo "error: dtc is not installed"
   exit 1
 fi
-<<<<<<< HEAD
 
-=======
->>>>>>> 45c1e0c9dff ([boot-shim] Bootstrap '/chosen')
 for source in "${DTS_DIR}"/*."${IN_FORMAT}"
 do
   dest="${source%.${IN_FORMAT}}.${OUT_FORMAT}"
