390a07223130cb53ae7867c34ff9f2febafd2571
Justin Kobler <jkobler@google.com>
2023-06-16 22:19:51 +0000

    [scenic] Define default Scenic structured config values
    Bug: 125067
    Change-Id: Idcdc58a999147945c59c4ea3d775da6460706c1a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/864058
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Aaron Wood <aaronwood@google.com>
    Fuchsia-Auto-Submit: Justin Kobler <jkobler@google.com>

--- src/lib/assembly/config_schema/src/platform_config/ui_config.rs
+++ src/lib/assembly/config_schema/src/platform_config/ui_config.rs
@@ -6,7 +6,7 @@ use camino::Utf8PathBuf;
 use serde::{Deserialize, Serialize};
 
 /// Platform configuration options for the UI area.
-#[derive(Debug, Default, Deserialize, Serialize, PartialEq)]
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
 #[serde(deny_unknown_fields)]
 pub struct PlatformUiConfig {
     /// Whether UI should be enabled on the product.
@@ -21,4 +21,25 @@ pub struct PlatformUiConfig {
     /// The sensor config to provide to the input pipeline.
     #[serde(default)]
     pub sensor_config: Option<Utf8PathBuf>,
+
+    /// The minimum frame duration for frame scheduler.
+    #[serde(default)]
+    pub frame_scheduler_min_predicted_frame_duration_in_us: u64,
+
+    // Scenic shifts focus from view to view as the user interacts with the UI.
+    // Set to false for Smart displays, as they use a different programmatic focus change scheme.
+    #[serde(default)]
+    pub pointer_auto_focus: bool,
+}
+
+impl Default for PlatformUiConfig {
+    fn default() -> Self {
+        Self {
+            enabled: Default::default(),
+            legacy: Default::default(),
+            sensor_config: Default::default(),
+            frame_scheduler_min_predicted_frame_duration_in_us: Default::default(),
+            pointer_auto_focus: true,
+        }
+    }
 }
