651c0eb098ace2be4803a96028b437b73d3b656b
Yegor Pomortsev <ypomortsev@google.com>
2023-06-16 22:56:27 +0000

    [ffx][session] Add `ffx session stop` command
    Fixed: 89084
    Change-Id: Iebefb72baa2e37a867fe1e4527b11e9f86b343c6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/871569
    Reviewed-by: Yifei Teng <yifeit@google.com>
    Commit-Queue: Yegor Pomortsev <ypomortsev@google.com>

---
+++ src/developer/ffx/plugins/session/stop/src/args.rs
@@ -0,0 +1,10 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {argh::FromArgs, ffx_core::ffx_command};
+
+#[ffx_command()]
+#[derive(FromArgs, Debug, PartialEq)]
+#[argh(subcommand, name = "stop", description = "Stop the session component.")]
+pub struct SessionStopCommand {}
