aed2b903b5278b75b4543f0cdfff9676c9628e50
Christopher Anderson <cja@google.com>
2023-06-21 21:35:57 +0000

    [lib][mmio] Add fdf_testing::CreateMmioBuffer for tests
    Bug: 115988
    Change-Id: I7e689b9eda2bdc095ad5f4c4846e4463aa2f378a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/872584
    Fuchsia-Auto-Submit: Christopher Anderson <cja@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

---
+++ src/devices/lib/mmio/test-helper.h
@@ -0,0 +1,30 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/mmio/mmio-buffer.h>
+#include <lib/mmio/mmio.h>
+#include <lib/zx/vmo.h>
+#include <zircon/assert.h>
+#include <zircon/errors.h>
+#include <zircon/syscalls.h>
+#include <zircon/types.h>
+
+namespace fdf_testing {
+// This provides a helper to quickly create an MmioBuffer for use in tests.
+// TODO(fxb/115988): This use of mmio_buffer_t is temporary to ease the
+// transition of clients over to MmioBuffer, and can be switched over once we
+// have no unmigrated users touching MmioBufferOps.
+[[maybe_unused]] static fdf::MmioBuffer CreateMmioBuffer(
+    size_t size, uint32_t cache_policy = ZX_CACHE_POLICY_UNCACHED,
+    const ::fdf::internal::MmioBufferOps* ops = &::fdf::internal::kDefaultOps,
+    void* ctx = nullptr) {
+  zx::vmo vmo;
+  zx_status_t status = zx::vmo::create(/*size=*/size, 0, &vmo);
+  ZX_ASSERT(status == ZX_OK);
+  mmio_buffer_t mmio{};
+  status = mmio_buffer_init(&mmio, 0, size, vmo.release(), cache_policy);
+  ZX_ASSERT(status == ZX_OK);
+  return fdf::MmioBuffer(mmio, ops, ctx);
+}
+}  // namespace fdf_testing
