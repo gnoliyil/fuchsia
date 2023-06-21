25f9c0b1898ebb3343ab0eedf26dacda6982a1c2
Yilong Li <liyl@google.com>
2023-06-21 06:32:42 +0000

    [devfs-factory] Migrate to new C++ binding.
    This migrates the devfs-factory to the new C++ binding.
    The filename will be changed in a separate CL to preserve
    change history.
    
    Test: flatland_display_compositor_pixeltest (with real display)
    Bug: 129273
    
    Change-Id: Ie6165a4c9358fcc486e641284c680ee09d325e4f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/873037
    Reviewed-by: Victor Costan <costan@google.com>
    Reviewed-by: Emircan Uysaler <emircan@google.com>
    Commit-Queue: Yilong Li <liyl@google.com>

--- src/graphics/display/testing/coordinator-provider-lib/devfs-factory-hlcpp.h
+++ src/graphics/display/testing/coordinator-provider-lib/devfs-factory-hlcpp.h
@@ -5,8 +5,10 @@
 #ifndef SRC_GRAPHICS_DISPLAY_TESTING_COORDINATOR_PROVIDER_LIB_DEVFS_FACTORY_HLCPP_H_
 #define SRC_GRAPHICS_DISPLAY_TESTING_COORDINATOR_PROVIDER_LIB_DEVFS_FACTORY_HLCPP_H_
 
-#include <fuchsia/hardware/display/cpp/fidl.h>
+#include <fidl/fuchsia.hardware.display/cpp/fidl.h>
+#include <lib/component/outgoing/cpp/outgoing_directory.h>
 #include <lib/fidl/cpp/binding_set.h>
+#include <lib/zx/result.h>
 
 #include <cstdint>
 #include <map>
@@ -14,42 +16,60 @@
 
 #include "src/lib/fsl/io/device_watcher.h"
 
-namespace sys {
-class ComponentContext;
-}  // namespace sys
-
 namespace display {
 
-// Implements the FIDL fuchsia.hardware.display.Provider API.  Only provides access to the primary
-// controller, not the Virtcon controller.
-class DevFsCoordinatorFactoryHlcpp : public fuchsia::hardware::display::Provider {
+// Implements the FIDL fuchsia.hardware.display.Provider API.  Only provides
+// access to the coordinator as a primary client, but not a virtcon client.
+//
+// This class is thread unsafe. All FIDL calls must be dispatched onto the same
+// dispatcher (i.e. the `dispatcher` argument in ctor) to which DeviceWatcher
+// async tasks are dispatched.
+class DevFsCoordinatorFactory : public fidl::Server<fuchsia_hardware_display::Provider> {
  public:
-  // |app_context| is used to publish this service.
-  explicit DevFsCoordinatorFactoryHlcpp(sys::ComponentContext* app_context);
+  // Creates a DevFsCoordinatorFactory which issues async tasks to `dispatcher`
+  // and publishes the service to the `outgoing` directory.
+  //
+  // For thread safety, the `outgoing` directory must be also created using
+  // `dispatcher`.
+  static zx::result<> CreateAndPublishService(component::OutgoingDirectory& outgoing,
+                                              async_dispatcher_t* dispatcher);
 
-  // |fuchsia::hardware::display::Provider|.
-  void OpenCoordinatorForVirtcon(
-      ::fidl::InterfaceRequest<fuchsia::hardware::display::Coordinator> coordinator,
-      OpenCoordinatorForVirtconCallback callback) override {
-    callback(ZX_ERR_NOT_SUPPORTED);
-  }
+  // Creates a DevFsCoordinatorFactory which issues async tasks to `dispatcher`.
+  explicit DevFsCoordinatorFactory(async_dispatcher_t* dispatcher);
+  ~DevFsCoordinatorFactory() override = default;
 
-  // |fuchsia::hardware::display::Provider|.
-  void OpenCoordinatorForPrimary(
-      ::fidl::InterfaceRequest<fuchsia::hardware::display::Coordinator> coordinator,
-      OpenCoordinatorForPrimaryCallback callback) override;
+  // Disallow copying, moving and assignment.
+  DevFsCoordinatorFactory(const DevFsCoordinatorFactory&) = delete;
+  DevFsCoordinatorFactory(DevFsCoordinatorFactory&&) = delete;
+  DevFsCoordinatorFactory& operator=(const DevFsCoordinatorFactory&) = delete;
+  DevFsCoordinatorFactory& operator=(DevFsCoordinatorFactory&&) = delete;
 
-  void BindDisplayProvider(fidl::InterfaceRequest<fuchsia::hardware::display::Provider> request);
+  // `fidl::Server<fuchsia_hardware_display::Provider>`
+  void OpenCoordinatorForVirtcon(OpenCoordinatorForVirtconRequest& request,
+                                 OpenCoordinatorForVirtconCompleter::Sync& completer) override;
+  void OpenCoordinatorForPrimary(OpenCoordinatorForPrimaryRequest& request,
+                                 OpenCoordinatorForPrimaryCompleter::Sync& completer) override;
 
  private:
-  fidl::BindingSet<fuchsia::hardware::display::Provider> bindings_;
+  // Opens fuchsia.hardware.display.Coordinator service for a primary client
+  // at service located at "dir/filename" using the server end channel
+  // `coordinator_server`.
+  static zx_status_t OpenCoordinatorForPrimaryOnDevice(
+      const fidl::ClientEnd<fuchsia_io::Directory>& dir, const std::string& filename,
+      fidl::ServerEnd<fuchsia_hardware_display::Coordinator> coordinator_server);
+
+  // The dispatcher where all async tasks are dispatched onto by the
+  // `DevFsCoordinatorFactory`.
+  async_dispatcher_t* dispatcher_;
 
   // The currently outstanding DeviceWatcher closures.  The closures will remove
   // themselves from here if they are invoked before shutdown.  Any closures
   // still outstanding will be handled by the destructor.
-  // This approach assumes that the event loop is attached to
-  // the main thread, else race conditions may occur.
-  std::map<uint64_t, std::unique_ptr<fsl::DeviceWatcher>> holders_;
+  std::map<int64_t, std::unique_ptr<fsl::DeviceWatcher>> pending_device_watchers_;
+
+  // ID of the next connected display client to identify queued display
+  // coordinator client connections.
+  int64_t next_display_client_id_ = 1;
 };
 
 }  // namespace display
