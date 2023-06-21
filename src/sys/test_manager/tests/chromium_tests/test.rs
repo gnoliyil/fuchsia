450bc6e05035177f222617dd2781281b3580e8e5
David Dorwin <ddorwin@google.com>
2023-06-21 01:45:26 +0000

    Reland "[chromium][test_manager] Remove buildinfo & intl from chromium realm"
    This is a reland of commit e416e3ddbdba166c73b6cfe1f994c892d2a6f591
    
    Original change's description:
    > [chromium][test_manager] Remove buildinfo & intl from chromium realm
    >
    > Now that Chromium tests launch fakes for "fuchsia.buildinfo.Provider"
    > and "fuchsia.intl.PropertyProvider", the real services no longer need
    > to be provided to the `chromium` test realm.
    >
    > Remove the test that checks for access to /build_info from this realm
    > because access is no longer necessary or available.
    >
    > Bug: 121700, 91934, chromium:1326674
    > Change-Id: I28844456e7f600e3350d04eb9d3c3bdf07d02c0c
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/792216
    > Reviewed-by: Wez <wez@google.com>
    > Reviewed-by: Christopher Johnson <crjohns@google.com>
    > Commit-Queue: David Dorwin <ddorwin@google.com>
    
    Bug: 121700, 91934, chromium:1326674
    Change-Id: Ia8c4bf15919419423faa91588eacac047e6479c9
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/872577
    Reviewed-by: Wez <wez@google.com>
    Commit-Queue: David Dorwin <ddorwin@google.com>
    Reviewed-by: Christopher Johnson <crjohns@google.com>

--- src/sys/test_manager/tests/chromium_tests/test.rs
+++ src/sys/test_manager/tests/chromium_tests/test.rs
@@ -6,11 +6,3 @@
 // routed.
 // TODO(fxbug.dev/91934): once we can define the chromium realm out of tree, we should move
 // the definition and remove these tests.
-
-const BUILD_DIR: &str = "/build_info";
-
-#[test]
-fn can_read_build_info() {
-    let entries: Vec<_> = std::fs::read_dir(BUILD_DIR).expect("read build directory").collect();
-    assert!(!entries.is_empty());
-}
