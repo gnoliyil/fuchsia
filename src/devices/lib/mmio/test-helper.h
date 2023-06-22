4b2297519a9514f74b77e519bb6c82819d7ce0b6
Christopher Anderson <cja@google.com>
2023-06-22 01:28:12 +0000

    [lib][mmio] Add missing header guard
    Change-Id: I6460585b97b32a7382a63e661f295aa5ba33db6b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/874559
    Reviewed-by: Jocelyn Dang <jocelyndang@google.com>
    Fuchsia-Auto-Submit: Christopher Anderson <cja@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/devices/lib/mmio/test-helper.h
+++ src/devices/lib/mmio/test-helper.h
@@ -1,6 +1,8 @@
 // Copyright 2023 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+#ifndef SRC_DEVICES_LIB_MMIO_TEST_HELPER_H_
+#define SRC_DEVICES_LIB_MMIO_TEST_HELPER_H_
 
 #include <lib/mmio/mmio-buffer.h>
 #include <lib/mmio/mmio.h>
@@ -28,3 +30,5 @@ namespace fdf_testing {
   return fdf::MmioBuffer(mmio, ops, ctx);
 }
 }  // namespace fdf_testing
+
+#endif  // SRC_DEVICES_LIB_MMIO_TEST_HELPER_H_
