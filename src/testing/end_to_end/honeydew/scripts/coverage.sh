6e2e8602dd61e59a1f2a8fc58aa5427b21f0fd19
David Worsham <dworsham@google.com>
2023-06-22 13:38:28 +0000

    [lacewing] Fix honeydew coverage
    Add instructions for how to run honeydew in the python REPL.
    
    Add a script and instructions for how to run the coverage tool taking
    into account the fuchsia controller requirements (PYTHONPATH and running
    from build directory).
    
    Change-Id: Iadc818f232296e908be4546225778956fb79f5d9
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/874078
    Reviewed-by: Pavan Kumar Juturu <jpavankumar@google.com>
    Commit-Queue: David Worsham <dworsham@google.com>
    Fuchsia-Auto-Submit: David Worsham <dworsham@google.com>

---
+++ src/testing/end_to_end/honeydew/scripts/coverage.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Copyright 2022 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Formats the HoneyDew code as per coding guidelines
+
+LACEWING_SRC="$FUCHSIA_DIR/src/testing/end_to_end"
+HONEYDEW_SRC="$LACEWING_SRC/honeydew"
+BUILD_DIR=$(cat "$FUCHSIA_DIR"/.fx-build-dir)
+
+VENV_ROOT_PATH="$LACEWING_SRC/.venvs"
+VENV_NAME="fuchsia_python_venv"
+VENV_PATH="$VENV_ROOT_PATH/$VENV_NAME"
+
+if [ -d $VENV_PATH ]
+then
+    echo "Activating the virtual environment..."
+    source $VENV_PATH/bin/activate
+else
+    echo "Directory '$VENV_PATH' does not exists. Run the 'install.sh' script first..."
+    exit 1
+fi
+
+echo "Configuring environment..."
+pushd $FUCHSIA_DIR/$BUILD_DIR > /dev/null
+OLD_PYTHONPATH=$PYTHONPATH
+PYTHONPATH=$FUCHSIA_DIR/$BUILD_DIR/host_x64:$FUCHSIA_DIR/src/developer/ffx/lib/fuchsia-controller/python:$PYTHONPATH
+
+echo "Running coverage tool..."
+coverage \
+    run -m unittest discover \
+    --top-level-directory $HONEYDEW_SRC \
+    --start-directory $HONEYDEW_SRC/tests/unit_tests \
+    --pattern "*_test.py"
+
+echo "Generating coverage stats..."
+coverage report -m
+rm -rf .coverage
+
+echo "Restoring environment..."
+PYTHONPATH=$OLD_PYTHONPATH
+popd > /dev/null
