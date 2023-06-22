5ba1e856a325a2748abd1862bd3f42f726a04195
Ina Huh <ihuh@google.com>
2023-06-22 23:19:57 +0000

    [kernel_zbi_test] Add cuckoo kernel test for collecting early boot coverage.
    Bug: 109879
    Change-Id: I2398d71046faa9a8110bc38793a63138416b48e5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/806036
    Commit-Queue: Ina Huh <ihuh@google.com>
    Reviewed-by: Joshua Seaton <joshuaseaton@google.com>
    Reviewed-by: Gianfranco Valentino <gevalentino@google.com>

---
+++ src/sys/early_boot_instrumentation/test/collector-test.cc
@@ -0,0 +1,9 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <gtest/gtest.h>
+
+// The purpose of this test is to be included in the cuckoo test, to trigger run scoped artifact
+// collections.
+TEST(CollectorTest, StubForEarlyBootdata) {}
