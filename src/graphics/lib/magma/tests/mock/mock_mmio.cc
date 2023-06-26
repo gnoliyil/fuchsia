ea44b2ee48a900a5f3779404045da4c91d34fd2d
John Bauman <jbauman@google.com>
2023-06-26 20:17:22 +0000

    [magma] Use MMIO_PTR with PlatformMmio
    Only specific load/store instructions may be used with MMIOs,
    particularly when using virtualized hardware. The mmio-ptr library has
    functions that use those instructions to implement read/write, and an
    MMIO_PTR annotation to ensure that the mmio pointers aren't dereferenced
    directly.
    
    Bug: 56253
    Change-Id: I7e01faf793939e606dac7b7cb36919fdaeb7ff89
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/873759
    Reviewed-by: Craig Stout <cstout@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: John Bauman <jbauman@google.com>

--- src/graphics/lib/magma/tests/mock/mock_mmio.cc
+++ src/graphics/lib/magma/tests/mock/mock_mmio.cc
@@ -13,9 +13,11 @@ std::unique_ptr<MockMmio> MockMmio::Create(uint64_t size) {
   return std::unique_ptr<MockMmio>(new MockMmio(addr, size));
 }
 
-MockMmio::MockMmio(void* addr, uint64_t size) : magma::PlatformMmio(addr, size) {}
+MockMmio::MockMmio(void* addr, uint64_t size)
+    : magma::PlatformMmio(reinterpret_cast<MMIO_PTR void*>(reinterpret_cast<uintptr_t>(addr)),
+                          size) {}
 
 MockMmio::~MockMmio() {
   MAGMA_DLOG("MockMmio dtor");
-  free(addr());
+  free(reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(addr())));
 }
