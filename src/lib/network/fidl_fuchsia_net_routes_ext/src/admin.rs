01cd14c664b8536384d8d9453ec98877550d47ed
Evan Wildenhain <wildenhain@google.com>
2023-06-26 19:27:11 +0000

    [tests/routes-admin] Ip-generify
    Makes the existing routes-admin integration tests generic over IP.
    
    Bug: 129220
    Change-Id: Ieba8192bffd94f354aba4a7ea9f8f7200c59907d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/875877
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Antonio Linhart <antoniolinhart@google.com>
    Reviewed-by: Jeff Martin <martinjeffrey@google.com>
    Fuchsia-Auto-Submit: Evan Wildenhain <wildenhain@google.com>

--- src/lib/network/fidl_fuchsia_net_routes_ext/src/admin.rs
+++ src/lib/network/fidl_fuchsia_net_routes_ext/src/admin.rs
@@ -6,8 +6,11 @@
 
 use fidl::endpoints::{DiscoverableProtocolMarker, ProtocolMarker};
 use fidl_fuchsia_net_routes_admin as fnet_routes_admin;
+use futures::future::Either;
 use net_types::ip::{GenericOverIp, Ip, IpInvariant, Ipv4, Ipv6};
 use thiserror::Error;
+
+use crate::FidlRouteIpExt;
 
 /// Route set creation errors.
 #[derive(Clone, Debug, Error)]
@@ -38,10 +41,10 @@ impl FidlRouteAdminIpExt for Ipv6 {
     type RouteSetMarker = fnet_routes_admin::RouteSetV6Marker;
 }
 
-/// Dispatches `new_route_set` on either the `RouteSetV4`
-/// or `RouteSetV6` proxy.
+/// Dispatches `new_route_set` on either the `SetProviderV4`
+/// or `SetProviderV6` proxy.
 pub fn new_route_set<I: Ip + FidlRouteAdminIpExt>(
-    set_provider_proxy: <I::SetProviderMarker as ProtocolMarker>::Proxy,
+    set_provider_proxy: &<I::SetProviderMarker as ProtocolMarker>::Proxy,
 ) -> Result<<I::RouteSetMarker as ProtocolMarker>::Proxy, RouteSetCreationError> {
     let (route_set_proxy, route_set_server_end) =
         fidl::endpoints::create_proxy::<I::RouteSetMarker>()
@@ -53,7 +56,7 @@ pub fn new_route_set<I: Ip + FidlRouteAdminIpExt>(
         set_provider_proxy: &'a <I::SetProviderMarker as ProtocolMarker>::Proxy,
     }
     let IpInvariant(result) = I::map_ip::<NewRouteSetInput<'_, I>, _>(
-        NewRouteSetInput::<'_, I> { route_set_server_end, set_provider_proxy: &set_provider_proxy },
+        NewRouteSetInput::<'_, I> { route_set_server_end, set_provider_proxy },
         |NewRouteSetInput { route_set_server_end, set_provider_proxy }| {
             IpInvariant(set_provider_proxy.new_route_set(route_set_server_end))
         },
@@ -64,4 +67,46 @@ pub fn new_route_set<I: Ip + FidlRouteAdminIpExt>(
 
     result.map_err(RouteSetCreationError::RouteSet)?;
     Ok(route_set_proxy)
+}
+
+/// Dispatches `add_route` on either the `RouteSetV4` or `RouteSetV6` proxy.
+pub async fn add_route<I: Ip + FidlRouteAdminIpExt + FidlRouteIpExt>(
+    route_set: &<I::RouteSetMarker as ProtocolMarker>::Proxy,
+    route: &I::Route,
+) -> Result<Result<bool, fnet_routes_admin::RouteSetError>, fidl::Error> {
+    #[derive(GenericOverIp)]
+    struct AddRouteInput<'a, I: Ip + FidlRouteAdminIpExt + FidlRouteIpExt> {
+        route_set: &'a <I::RouteSetMarker as ProtocolMarker>::Proxy,
+        route: &'a I::Route,
+    }
+
+    let IpInvariant(result_fut) = I::map_ip(
+        AddRouteInput { route_set, route },
+        |AddRouteInput { route_set, route }| IpInvariant(Either::Left(route_set.add_route(route))),
+        |AddRouteInput { route_set, route }| IpInvariant(Either::Right(route_set.add_route(route))),
+    );
+    result_fut.await
+}
+
+/// Dispatches `remove_route` on either the `RouteSetV4` or `RouteSetV6` proxy.
+pub async fn remove_route<I: Ip + FidlRouteAdminIpExt + FidlRouteIpExt>(
+    route_set: &<I::RouteSetMarker as ProtocolMarker>::Proxy,
+    route: &I::Route,
+) -> Result<Result<bool, fnet_routes_admin::RouteSetError>, fidl::Error> {
+    #[derive(GenericOverIp)]
+    struct RemoveRouteInput<'a, I: Ip + FidlRouteAdminIpExt + FidlRouteIpExt> {
+        route_set: &'a <I::RouteSetMarker as ProtocolMarker>::Proxy,
+        route: &'a I::Route,
+    }
+
+    let IpInvariant(result_fut) = I::map_ip(
+        RemoveRouteInput { route_set, route },
+        |RemoveRouteInput { route_set, route }| {
+            IpInvariant(Either::Left(route_set.remove_route(route)))
+        },
+        |RemoveRouteInput { route_set, route }| {
+            IpInvariant(Either::Right(route_set.remove_route(route)))
+        },
+    );
+    result_fut.await
 }
