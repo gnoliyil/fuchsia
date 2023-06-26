a2288b71bec814017054381187b6aba4e00f58d0
Tamir Duberstein <tamird@google.com>
2023-06-26 20:09:19 +0000

    [fdio-caller] Reduce boilerplate
    Use a test fixture.
    
    Change-Id: Id803d7fb4523a26795515b7e63fb30dbc51fbce6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/876777
    Fuchsia-Auto-Submit: Tamir Duberstein <tamird@google.com>
    Reviewed-by: Ben Keller <galbanum@google.com>
    Reviewed-by: David Gilhooley <dgilhooley@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- zircon/system/ulib/fdio-caller/test/fdio.cc
+++ zircon/system/ulib/fdio-caller/test/fdio.cc
@@ -31,13 +31,13 @@ void TryFilesystemOperations(fidl::UnownedClientEnd<fio::File> client_end) {
               fidl::VectorView<uint8_t>::FromExternal(
                   reinterpret_cast<uint8_t*>(const_cast<char*>(payload.data())), payload.size()),
               0);
-  ASSERT_EQ(write_result.status(), ZX_OK);
+  ASSERT_OK(write_result);
   const fit::result write_response = write_result.value();
   ASSERT_TRUE(write_response.is_ok(), "%s", zx_status_get_string(write_response.error_value()));
   ASSERT_EQ(write_response.value()->actual_count, payload.size());
 
   const fidl::WireResult read_result = fidl::WireCall(client_end)->ReadAt(256, 0);
-  ASSERT_OK(read_result.status());
+  ASSERT_OK(read_result);
   const fit::result read_response = read_result.value();
   ASSERT_TRUE(read_response.is_ok(), "%s", zx_status_get_string(read_response.error_value()));
   const fidl::VectorView data = read_result->value()->data;
@@ -60,14 +60,9 @@ void TryFilesystemOperations(const fdio_cpp::UnownedFdioCaller& caller) {
   TryFilesystemOperations(caller.channel());
 }
 
-class Harness {
- public:
-  Harness() = default;
-  ~Harness() = default;
-
-  void Setup() {
-    ASSERT_EQ(loop_.StartThread(), ZX_OK);
-
+class FdioCallerTest : public zxtest::Test {
+ protected:
+  void SetUp() override {
     zx::result result = memfs::Memfs::Create(loop_.dispatcher(), "<tmp>");
     ASSERT_OK(result);
     auto& [memfs, root] = result.value();
@@ -79,13 +74,19 @@ class Harness {
     ASSERT_OK(memfs->ServeDirectory(std::move(root), std::move(server)));
     memfs_ = std::move(memfs);
 
-    fbl::unique_fd dir;
-    ASSERT_EQ(fdio_fd_create(client.TakeChannel().release(), dir.reset_and_get_address()), ZX_OK);
-    fd_.reset(openat(dir.get(), "my-file", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR));
-    ASSERT_TRUE(fd_);
+    zx::channel ch0, ch1;
+    ASSERT_OK(zx::channel::create(0, &ch0, &ch1));
+
+    ASSERT_OK(fidl::WireCall(client)->Open(
+        fio::OpenFlags::kCreate | fio::OpenFlags::kRightReadable | fio::OpenFlags::kRightWritable,
+        {}, "my-file", fidl::ServerEnd<fio::Node>{std::move(ch0)}));
+
+    ASSERT_OK(loop_.StartThread());
+
+    ASSERT_OK(fdio_fd_create(ch1.release(), fd_.reset_and_get_address()));
   }
 
-  fbl::unique_fd fd() { return std::move(fd_); }
+  fbl::unique_fd& fd() { return fd_; }
 
  private:
   async::Loop loop_ = async::Loop(&kAsyncLoopConfigNoAttachToCurrentThread);
@@ -93,51 +94,35 @@ class Harness {
   fbl::unique_fd fd_;
 };
 
-TEST(FdioCallTests, FdioCallerFile) {
-  Harness harness;
-  ASSERT_NO_FATAL_FAILURE(harness.Setup());
-  auto fd = harness.fd();
-
+TEST_F(FdioCallerTest, FdioCallerFile) {
   // Try some filesystem operations.
-  fdio_cpp::FdioCaller caller(std::move(fd));
+  fdio_cpp::FdioCaller caller(std::move(fd()));
   ASSERT_TRUE(caller);
   ASSERT_NO_FATAL_FAILURE(TryFilesystemOperations(caller));
 
   // Re-acquire the underlying fd.
-  fd = caller.release();
+  fbl::unique_fd fd = caller.release();
   ASSERT_EQ(close(fd.release()), 0);
 }
 
-TEST(FdioCallTests, FdioCallerMoveAssignment) {
-  Harness harness;
-  ASSERT_NO_FATAL_FAILURE(harness.Setup());
-  auto fd = harness.fd();
-
-  fdio_cpp::FdioCaller caller(std::move(fd));
+TEST_F(FdioCallerTest, FdioCallerMoveAssignment) {
+  fdio_cpp::FdioCaller caller(std::move(fd()));
   fdio_cpp::FdioCaller move_assignment_caller = std::move(caller);
   ASSERT_TRUE(move_assignment_caller);
   ASSERT_FALSE(caller);
   ASSERT_NO_FATAL_FAILURE(TryFilesystemOperations(move_assignment_caller));
 }
 
-TEST(FdioCallTests, FdioCallerMoveConstructor) {
-  Harness harness;
-  ASSERT_NO_FATAL_FAILURE(harness.Setup());
-  auto fd = harness.fd();
-
-  fdio_cpp::FdioCaller caller(std::move(fd));
+TEST_F(FdioCallerTest, FdioCallerMoveConstructor) {
+  fdio_cpp::FdioCaller caller(std::move(fd()));
   fdio_cpp::FdioCaller move_ctor_caller(std::move(caller));
   ASSERT_TRUE(move_ctor_caller);
   ASSERT_FALSE(caller);
   ASSERT_NO_FATAL_FAILURE(TryFilesystemOperations(move_ctor_caller));
 }
 
-TEST(FdioCallTests, FdioCallerBorrow) {
-  Harness harness;
-  ASSERT_NO_FATAL_FAILURE(harness.Setup());
-  auto fd = harness.fd();
-
-  fdio_cpp::FdioCaller caller(std::move(fd));
+TEST_F(FdioCallerTest, FdioCallerBorrow) {
+  fdio_cpp::FdioCaller caller(std::move(fd()));
   zx::unowned_channel channel = caller.channel();
   ASSERT_TRUE(channel->is_valid());
   ASSERT_TRUE(caller);
@@ -148,12 +133,8 @@ TEST(FdioCallTests, FdioCallerBorrow) {
   ASSERT_NO_FATAL_FAILURE(TryFilesystemOperations(caller.directory().channel()));
 }
 
-TEST(FdioCallTests, FdioCallerClone) {
-  Harness harness;
-  ASSERT_NO_FATAL_FAILURE(harness.Setup());
-  auto fd = harness.fd();
-
-  fdio_cpp::FdioCaller caller(std::move(fd));
+TEST_F(FdioCallerTest, FdioCallerClone) {
+  fdio_cpp::FdioCaller caller(std::move(fd()));
   auto channel = caller.clone_channel();
   ASSERT_OK(channel.status_value());
   ASSERT_TRUE(channel->is_valid());
@@ -166,12 +147,8 @@ TEST(FdioCallTests, FdioCallerClone) {
   ASSERT_NO_FATAL_FAILURE(TryFilesystemOperations(caller.clone_directory()->channel()));
 }
 
-TEST(FdioCallTests, FdioCallerTake) {
-  Harness harness;
-  ASSERT_NO_FATAL_FAILURE(harness.Setup());
-  auto fd = harness.fd();
-
-  fdio_cpp::FdioCaller caller(std::move(fd));
+TEST_F(FdioCallerTest, FdioCallerTake) {
+  fdio_cpp::FdioCaller caller(std::move(fd()));
   auto channel = caller.take_channel();
   ASSERT_OK(channel.status_value());
   ASSERT_TRUE(channel->is_valid());
@@ -179,12 +156,8 @@ TEST(FdioCallTests, FdioCallerTake) {
   ASSERT_NO_FATAL_FAILURE(TryFilesystemOperations(channel->borrow()));
 }
 
-TEST(FdioCallTests, FdioCallerTakeAs) {
-  Harness harness;
-  ASSERT_NO_FATAL_FAILURE(harness.Setup());
-  auto fd = harness.fd();
-
-  fdio_cpp::FdioCaller caller(std::move(fd));
+TEST_F(FdioCallerTest, FdioCallerTakeAs) {
+  fdio_cpp::FdioCaller caller(std::move(fd()));
   auto channel = caller.take_as<fio::File>();
   ASSERT_OK(channel.status_value());
   ASSERT_TRUE(channel->is_valid());
@@ -192,22 +165,15 @@ TEST(FdioCallTests, FdioCallerTakeAs) {
   ASSERT_NO_FATAL_FAILURE(TryFilesystemOperations(channel->borrow()));
 }
 
-TEST(FdioCallTests, UnownedFdioCaller) {
-  Harness harness;
-  ASSERT_NO_FATAL_FAILURE(harness.Setup());
-  auto fd = harness.fd();
-  fdio_cpp::UnownedFdioCaller caller(fd);
+TEST_F(FdioCallerTest, UnownedFdioCaller) {
+  fdio_cpp::UnownedFdioCaller caller(fd());
   ASSERT_TRUE(caller);
-  ASSERT_TRUE(fd);
+  ASSERT_TRUE(fd());
   ASSERT_NO_FATAL_FAILURE(TryFilesystemOperations(caller));
 }
 
-TEST(FdioCallTests, UnownedFdioCallerBorrow) {
-  Harness harness;
-  ASSERT_NO_FATAL_FAILURE(harness.Setup());
-  auto fd = harness.fd();
-
-  fdio_cpp::UnownedFdioCaller caller(fd);
+TEST_F(FdioCallerTest, UnownedFdioCallerBorrow) {
+  fdio_cpp::UnownedFdioCaller caller(fd());
   zx::unowned_channel channel = caller.channel();
   ASSERT_TRUE(channel->is_valid());
   ASSERT_TRUE(caller);
