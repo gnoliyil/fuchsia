3c989bcd01e0a0cb68ce44e7c797402a1a86e3f4
Ali Saeed <saeedali@google.com>
2023-06-27 18:13:22 +0000

    [sl4f] Add mock Media Player service
    Add three commands to the MediaSessionFacade: PublishMockPlayer,
    StopMockPlayer, and ListReceivedRequests.
    
    Fixed: b/285011179, b/288339273
    Test: Connect an AVRCP controller (e.g. Bluetooth speaker) to
    the Fuchsia device, issue media_session_facade.PublishMockPlayer
    command, issue playback commands on the AVRCP controller, and
    validate that a list of the commands are returned by
    media_session_facade.ListReceivedRequests.
    
    Change-Id: I0cbad0f37910ea319933aa21204c1f8ae97b560a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/873760
    Commit-Queue: Ali Saeed <saeedali@google.com>
    Reviewed-by: Ani Ramakrishnan <aniramakri@google.com>
    Reviewed-by: Dale Sather <dalesat@google.com>

--- src/testing/sl4f/src/media_session/facade.rs
+++ src/testing/sl4f/src/media_session/facade.rs
@@ -5,17 +5,31 @@
 use crate::common_utils::common::macros::{fx_err_and_bail, with_line};
 use crate::media_session::types::PlayerStatusWrapper;
 use anyhow::Error;
-use fidl_fuchsia_media_sessions2::{ActiveSessionMarker, ActiveSessionProxy};
+use fidl::endpoints::create_request_stream;
+use fidl_fuchsia_media_sessions2::{
+    self as sessions2, ActiveSessionMarker, ActiveSessionProxy, PlayerRegistration, PublisherMarker,
+};
+use fuchsia_async as fasync;
 use fuchsia_component::client::connect_to_protocol;
+use futures::StreamExt;
+use parking_lot::RwLock;
+use std::sync::Arc;
+use tracing::info;
 
 #[derive(Debug)]
 pub struct MediaSessionFacade {
     active_session_proxy: Option<ActiveSessionProxy>,
+    player_task: RwLock<Option<fasync::Task<()>>>,
+    requests_received: Arc<RwLock<Vec<String>>>,
 }
 
 impl MediaSessionFacade {
     pub fn new() -> Self {
-        Self { active_session_proxy: None }
+        Self {
+            active_session_proxy: None,
+            player_task: RwLock::new(None),
+            requests_received: Arc::new(RwLock::new(Vec::new())),
+        }
     }
 
     /// Returns the active session proxy.
@@ -54,5 +68,88 @@ impl MediaSessionFacade {
         } else {
             Ok(None)
         }
+    }
+
+    /// Publishes a mock Media Player via the fuchsia.media.sessions2 Publisher protocol.
+    /// Clients can watch Media Sessions via the fuchsia.media.sessions2 Discovery protocol.
+    /// This mock player does not route requests; it just records the requests it receives.
+    /// The list of requests can be accessed through this facade's ListReceivedRequests command.
+    /// The Bluetooth AVRCP component forwards commands to the most recently activated player.
+    pub async fn publish_mock_player(&self) -> Result<(), Error> {
+        let requests_received = self.requests_received.clone();
+        let fut = async move {
+            let publisher = fuchsia_component::client::connect_to_protocol::<PublisherMarker>()
+                .expect("Failed to connect to Publisher");
+
+            let (player_client, mut player_request_stream) =
+                create_request_stream().expect("Failed to create request stream");
+
+            publisher
+                .publish(
+                    player_client,
+                    &PlayerRegistration {
+                        domain: Some("domain://sl4f".to_string()),
+                        ..Default::default()
+                    },
+                )
+                .await
+                .expect("Failed to publish Player");
+
+            let mut _player_info_change_responder = None;
+            let mut sent_response = false;
+
+            loop {
+                match player_request_stream.next().await {
+                    Some(Ok(request)) => match request {
+                        sessions2::PlayerRequest::WatchInfoChange { responder } => {
+                            if !sent_response {
+                                let _ = responder.send(&sessions2::PlayerInfoDelta::default());
+                                sent_response = true;
+                            } else {
+                                // Save responder so request stream is not dropped
+                                _player_info_change_responder = Some(responder);
+                            }
+                        }
+                        request => {
+                            requests_received.write().push(request.method_name().to_string());
+                        }
+                    },
+                    Some(Err(e)) => {
+                        info!(?e, "Mock player request stream error");
+                        break;
+                    }
+                    None => {
+                        info!("Mock player request stream terminated");
+                        break;
+                    }
+                }
+            }
+        };
+
+        if let Some(_task) = self.player_task.write().take() {
+            info!("Dropping old mock player");
+        }
+        *self.player_task.write() = Some(fasync::Task::spawn(fut));
+        info!("Publishing mock player");
+
+        Ok(())
+    }
+
+    /// Unpublishes a mock Media Player and resets the list of received requests.
+    /// Returns true if a mock Media Player was running and is now stopped.
+    pub async fn stop_mock_player(&self) -> Result<bool, Error> {
+        if let Some(_task) = self.player_task.write().take() {
+            info!("Stopping mock player");
+            self.requests_received.write().clear();
+            return Ok(true);
+        }
+        info!("Mock player stoppage requested, but no session exists");
+        Ok(false)
+    }
+
+    /// Returns a list of the player requests received by mock Media Players spawned by this
+    /// facade's PublishMockPlayer command.
+    pub async fn list_received_requests(&self) -> Result<Vec<String>, Error> {
+        Ok(self.requests_received.read().clone())
     }
 }
