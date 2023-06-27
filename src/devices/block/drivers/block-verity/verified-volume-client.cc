bac89e32c5b01225f97ae9c4f253be3455d0b7ca
David Gilhooley <dgilhooley@google.com>
2023-06-27 22:13:20 +0000

    [block-verity] Remove FIDL multiplexing
    Bug: 112484
    Change-Id: I034deef04c578da6d7fea057b52e0de4421b29ec
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/876697
    Fuchsia-Auto-Submit: David Gilhooley <dgilhooley@google.com>
    Reviewed-by: Tamir Duberstein <tamird@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/devices/block/drivers/block-verity/verified-volume-client.cc
+++ src/devices/block/drivers/block-verity/verified-volume-client.cc
@@ -36,66 +36,75 @@ zx_status_t BindVerityDriver(fidl::UnownedClientEnd<fuchsia_device::Controller> 
   return ZX_OK;
 }
 
-zx_status_t RelativeTopologicalPath(fidl::UnownedClientEnd<fuchsia_device::Controller> channel,
-                                    fbl::String* out) {
+zx::result<fbl::String> RelativeTopologicalPath(
+    fidl::UnownedClientEnd<fuchsia_device::Controller> channel) {
   const fidl::WireResult result = fidl::WireCall(channel)->GetTopologicalPath();
   if (!result.ok()) {
-    return result.status();
+    return zx::error(result.status());
   }
   const fit::result response = result.value();
   if (response.is_error()) {
-    return response.error_value();
+    return zx::error(response.error_value());
   }
   std::string_view path = response.value()->path.get();
 
   constexpr std::string_view kSlashDevSlash = "/dev/";
   if (!cpp20::starts_with(path, kSlashDevSlash)) {
-    return ZX_ERR_INTERNAL;
+    return zx::error(ZX_ERR_INTERNAL);
   }
-  *out = path.substr(kSlashDevSlash.size());
-  return ZX_OK;
+  return zx::ok(path.substr(kSlashDevSlash.size()));
 }
 
 }  // namespace
 
 VerifiedVolumeClient::VerifiedVolumeClient(
     fidl::ClientEnd<fuchsia_hardware_block_verified::DeviceManager> verity_chan,
-    fbl::unique_fd devfs_root_fd)
-    : verity_chan_(std::move(verity_chan)), devfs_root_fd_(std::move(devfs_root_fd)) {}
+    fidl::ClientEnd<fuchsia_device::Controller> verity_controller, fbl::unique_fd devfs_root_fd)
+    : verity_chan_(std::move(verity_chan)),
+      verity_controller_(std::move(verity_controller)),
+      devfs_root_fd_(std::move(devfs_root_fd)) {}
 
-zx_status_t VerifiedVolumeClient::CreateFromBlockDevice(
+zx::result<std::unique_ptr<VerifiedVolumeClient>> VerifiedVolumeClient::CreateFromBlockDevice(
     fidl::UnownedClientEnd<fuchsia_device::Controller> device, fbl::unique_fd devfs_root_fd,
-    Disposition disposition, const zx::duration& timeout,
-    std::unique_ptr<VerifiedVolumeClient>* out) {
+    Disposition disposition, const zx::duration& timeout) {
   // Bind the driver if called for by `disposition`.
   if (disposition == kDriverNeedsBinding) {
     if (zx_status_t status = BindVerityDriver(device); status != ZX_OK) {
       printf("VerifiedVolumeClient: couldn't bind driver: %s", zx_status_get_string(status));
-      return status;
+      return zx::error(status);
     }
   }
 
   // Compute the path at which we expect to see the verity child device appear.
-  fbl::String block_dev_path;
-  if (zx_status_t status = RelativeTopologicalPath(device, &block_dev_path); status != ZX_OK) {
+  zx::result block_dev_path = RelativeTopologicalPath(device);
+  if (block_dev_path.is_error()) {
     printf("VerifiedVolumeClient: could not compute relative path: %s\n",
-           zx_status_get_string(status));
-    return status;
+           block_dev_path.status_string());
+    return block_dev_path.take_error();
   }
-  fbl::String verity_path = fbl::String::Concat({block_dev_path, "/verity"});
+  fbl::String verity_path = fbl::String::Concat({block_dev_path.value(), "/verity"});
 
   // Wait for the device to appear.
   zx::result channel =
       device_watcher::RecursiveWaitForFile(devfs_root_fd.get(), verity_path.c_str(), timeout);
   if (channel.is_error()) {
     printf("VerifiedVolumeClient: verity device failed to appear: %s\n", channel.status_string());
-    return channel.error_value();
+    return channel.take_error();
   }
 
-  *out = std::make_unique<VerifiedVolumeClient>(
+  fbl::String controller_path = fbl::String::Concat({verity_path, "/device_controller"});
+  zx::result controller_channel =
+      device_watcher::RecursiveWaitForFile(devfs_root_fd.get(), controller_path.c_str(), timeout);
+  if (controller_channel.is_error()) {
+    printf("VerifiedVolumeClient: verity controller failed to appear: %s\n",
+           controller_channel.status_string());
+    return controller_channel.take_error();
+  }
+
+  return zx::ok(std::make_unique<VerifiedVolumeClient>(
       fidl::ClientEnd<fuchsia_hardware_block_verified::DeviceManager>{std::move(channel.value())},
-      std::move(devfs_root_fd));
-  return ZX_OK;
+      fidl::ClientEnd<fuchsia_device::Controller>{std::move(controller_channel.value())},
+      std::move(devfs_root_fd)));
 }
 
 zx_status_t VerifiedVolumeClient::OpenForAuthoring(const zx::duration& timeout,
@@ -119,17 +128,13 @@ zx_status_t VerifiedVolumeClient::OpenForAuthoring(const zx::duration& timeout,
   }
 
   // Compute path of expected `mutable` child device via relative topological path
-  fbl::String verity_path;
-  // TODO(https://fxbug.dev/112484): this relies on multiplexing.
-  if (zx_status_t status = RelativeTopologicalPath(
-          fidl::UnownedClientEnd<fuchsia_device::Controller>(verity_chan_.channel().borrow()),
-          &verity_path);
-      status != ZX_OK) {
+  zx::result verity_path = RelativeTopologicalPath(verity_controller_);
+  if (verity_path.is_error()) {
     printf("VerifiedVolumeClient: could not compute relative path: %s\n",
-           zx_status_get_string(status));
-    return status;
+           verity_path.status_string());
+    return verity_path.error_value();
   }
-  fbl::String mutable_path = fbl::String::Concat({verity_path, "/mutable"});
+  fbl::String mutable_path = fbl::String::Concat({verity_path.value(), "/mutable"});
 
   // Wait for the `mutable` child device to appear
   if (zx::result channel =
@@ -221,17 +226,13 @@ zx_status_t VerifiedVolumeClient::OpenForVerifiedRead(const digest::Digest& expe
   }
 
   // Compute path of expected `verified` child device via relative topological path
-  fbl::String verity_path;
-  // TODO(https://fxbug.dev/112484): this relies on multiplexing.
-  if (zx_status_t status = RelativeTopologicalPath(
-          fidl::UnownedClientEnd<fuchsia_device::Controller>(verity_chan_.channel().borrow()),
-          &verity_path);
-      status != ZX_OK) {
+  zx::result verity_path = RelativeTopologicalPath(verity_controller_);
+  if (verity_path.is_error()) {
     printf("VerifiedVolumeClient: could not compute relative path: %s\n",
-           zx_status_get_string(status));
-    return status;
+           verity_path.status_string());
+    return verity_path.error_value();
   }
-  fbl::String verified_path = fbl::String::Concat({verity_path, "/verified"});
+  fbl::String verified_path = fbl::String::Concat({verity_path.value(), "/verified"});
 
   // Wait for the `verified` child device to appear
   if (zx::result channel = device_watcher::RecursiveWaitForFile(devfs_root_fd_.get(),
