abcb5324943f351156ccf6ab8a4ac06477383763
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2023-06-28 16:19:20 +0000

    [dart][fuchsia_component_test] Delete
    Fixed: b/240153809
    
    Change-Id: I0e8f95c6cf060a08792e0144f12ac6f59f5c252d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/878372
    Reviewed-by: David Worsham <dworsham@google.com>
    API-Review: Gary Bressler <geb@google.com>
    Commit-Queue: Miguel Flores <miguelfrde@google.com>
    Reviewed-by: Gabe Schine <thatguy@google.com>
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>

--- sdk/dart/fuchsia_component_test/lib/realm_builder.dart
+++
@@ -1,8 +0,0 @@
-// Copyright 2022 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-/// A framework for testing Fuchsia components.
-export 'src/error.dart';
-export 'src/local_component_handles.dart';
-export 'src/realm_builder.dart';
