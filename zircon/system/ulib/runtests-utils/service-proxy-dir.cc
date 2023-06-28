7f427c910907e5eaf2b711d2e50a0287b25718cf
Tamir Duberstein <tamird@google.com>
2023-06-28 21:50:11 +0000

    [runtests] Connect without rights
    Bug: 101092
    Change-Id: I8981e8a7b798ac07bee54d9d083c83179357e9b0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/878434
    Reviewed-by: Stephen Demos <sdemos@google.com>
    Fuchsia-Auto-Submit: Tamir Duberstein <tamird@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- zircon/system/ulib/runtests-utils/service-proxy-dir.cc
+++ zircon/system/ulib/runtests-utils/service-proxy-dir.cc
@@ -54,15 +54,13 @@ zx_status_t ServiceProxyDir::Lookup(std::string_view name, fbl::RefPtr<fs::Vnode
     return ZX_OK;
   }
 
-  entries_.emplace(
-      entry_name,
-      *out =
-          fbl::MakeRefCounted<fs::Service>([this, entry_name](fidl::ServerEnd<fio::Node> request) {
-            return fidl::WireCall(proxy_dir_)
-                ->Open(fio::wire::OpenFlags::kRightReadable | fio::wire::OpenFlags::kRightWritable,
-                       {}, fidl::StringView::FromExternal(entry_name), std::move(request))
-                .status();
-          }));
+  entries_.emplace(entry_name, *out = fbl::MakeRefCounted<fs::Service>(
+                                   [this, entry_name](fidl::ServerEnd<fio::Node> request) {
+                                     return fidl::WireCall(proxy_dir_)
+                                         ->Open({}, {}, fidl::StringView::FromExternal(entry_name),
+                                                std::move(request))
+                                         .status();
+                                   }));
 
   return ZX_OK;
 }
