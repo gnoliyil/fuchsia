9bfb1c44f285f6d57f75f7e71cebbecbc88a7b51
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2023-06-28 10:37:02 +0000

    [dart][fuchsia_services] Refactor test to not use on RealmBuilder
    This is the only user of the library left and doesn't seem to need it
    now that we "use from child" exists, which maybe didn't exist at the
    time this test was written.
    
    Bug: b/240153925
    
    Change-Id: I3a95b2acecdcf52d1bc4ecdd20455e03fb694d93
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/878371
    Reviewed-by: David Worsham <dworsham@google.com>
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- sdk/dart/fuchsia_services/test/launch_flow_test.dart
+++ sdk/dart/fuchsia_services/test/launch_flow_test.dart
@@ -2,9 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:fidl_fuchsia_sys/fidl_async.dart';
 import 'package:fidl_test_fuchsia_service_foo/fidl_async.dart';
-import 'package:fuchsia_component_test/realm_builder.dart';
 import 'package:fuchsia_logger/logger.dart';
 import 'package:fuchsia_services/services.dart';
 import 'package:test/test.dart';
@@ -14,18 +12,7 @@ const String serverUrl = '#meta/fuchsia-services-foo-test-server.cm';
 void main() {
   setupLogger(name: 'fuchsia-services-test');
   test('launching and connecting to the foo service', () async {
-    final builder = await RealmBuilder.create();
-    final server = await builder.addChild(
-      'fuchsia-services-foo-test-server',
-      serverUrl,
-    );
-    await builder.addRoute(Route()
-      ..capability(ProtocolCapability(Foo.$serviceName))
-      ..from(Ref.child(server))
-      ..to(Ref.parent()));
-    final instance = await builder.build();
-
-    final incoming = Incoming.withDirectory(instance.root.exposedDir);
+    final incoming = Incoming.fromSvcPath();
     final fooProxy = FooProxy();
     incoming.connectToService(fooProxy);
     final response = await fooProxy.echo('foo');
