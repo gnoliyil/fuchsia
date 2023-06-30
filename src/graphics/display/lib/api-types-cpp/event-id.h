a02826a5b0282e7de07a1f61f659f176438efb14
Yilong Li <liyl@google.com>
2023-06-30 03:59:32 +0000

    [display][api-types-cpp] Add type EventId.
    This adds "EventId" convenient type to api-types-cpp.
    Display coordinator will switch to EventId to identify
    imported zircon events.
    
    Test: added event-id-test.cc
    Bug: 128286
    Change-Id: If30bfd4dcf2ab1336d2f475156158978c15bb5a9
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/879211
    Reviewed-by: Victor Costan <costan@google.com>
    Commit-Queue: Yilong Li <liyl@google.com>

---
+++ src/graphics/display/lib/api-types-cpp/event-id.h
@@ -0,0 +1,29 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_GRAPHICS_DISPLAY_LIB_API_TYPES_CPP_EVENT_ID_H_
+#define SRC_GRAPHICS_DISPLAY_LIB_API_TYPES_CPP_EVENT_ID_H_
+
+#include <fidl/fuchsia.hardware.display/cpp/wire.h>
+#include <fuchsia/hardware/display/controller/c/banjo.h>
+
+#include <cstdint>
+
+#include <fbl/strong_int.h>
+
+namespace display {
+
+// More useful representation of `fuchsia.hardware.display/EventId`.
+DEFINE_STRONG_INT(EventId, uint64_t);
+
+constexpr inline EventId ToEventId(uint64_t fidl_event_id_value) {
+  return EventId(fidl_event_id_value);
+}
+constexpr inline uint64_t ToFidlEventIdValue(EventId event_id) { return event_id.value(); }
+
+constexpr EventId kInvalidEventId(fuchsia_hardware_display::wire::kInvalidDispId);
+
+}  // namespace display
+
+#endif  // SRC_GRAPHICS_DISPLAY_LIB_API_TYPES_CPP_EVENT_ID_H_
