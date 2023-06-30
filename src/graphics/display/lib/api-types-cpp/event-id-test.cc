96eacde1bea9d150c22a46d929a0cf18d0943b61
Yilong Li <liyl@google.com>
2023-06-30 03:59:32 +0000

    [display][api-types-cpp] Add type EventId.
    This adds "EventId" convenient type to api-types-cpp.
    Display coordinator will switch to EventId to identify
    imported zircon events.
    
    Test: added event-id-test.cc
    Bug: 128286
    Change-Id: If30bfd4dcf2ab1336d2f475156158978c15bb5a9
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/879211
    Reviewed-by: Victor Costan <costan@google.com>
    Commit-Queue: Yilong Li <liyl@google.com>

---
+++ src/graphics/display/lib/api-types-cpp/event-id-test.cc
@@ -0,0 +1,72 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/graphics/display/lib/api-types-cpp/event-id.h"
+
+#include <fidl/fuchsia.hardware.display/cpp/wire.h>
+#include <fuchsia/hardware/display/controller/c/banjo.h>
+
+#include <cstdint>
+
+#include <gtest/gtest.h>
+
+namespace display {
+
+namespace {
+
+constexpr EventId kOne(1);
+constexpr EventId kAnotherOne(1);
+constexpr EventId kTwo(2);
+
+constexpr uint64_t kLargeIdValue = uint64_t{1} << 63;
+constexpr EventId kLargeId(kLargeIdValue);
+
+TEST(EventIdTest, EqualityIsReflexive) {
+  EXPECT_EQ(kOne, kOne);
+  EXPECT_EQ(kAnotherOne, kAnotherOne);
+  EXPECT_EQ(kTwo, kTwo);
+  EXPECT_EQ(kInvalidEventId, kInvalidEventId);
+}
+
+TEST(EventIdTest, EqualityIsSymmetric) {
+  EXPECT_EQ(kOne, kAnotherOne);
+  EXPECT_EQ(kAnotherOne, kOne);
+}
+
+TEST(EventIdTest, EqualityForDifferentValues) {
+  EXPECT_NE(kOne, kTwo);
+  EXPECT_NE(kAnotherOne, kTwo);
+  EXPECT_NE(kTwo, kOne);
+  EXPECT_NE(kTwo, kAnotherOne);
+
+  EXPECT_NE(kOne, kInvalidEventId);
+  EXPECT_NE(kTwo, kInvalidEventId);
+  EXPECT_NE(kInvalidEventId, kOne);
+  EXPECT_NE(kInvalidEventId, kTwo);
+}
+
+TEST(EventIdTest, ToFidlEventIdValue) {
+  EXPECT_EQ(1u, ToFidlEventIdValue(kOne));
+  EXPECT_EQ(2u, ToFidlEventIdValue(kTwo));
+  EXPECT_EQ(kLargeIdValue, ToFidlEventIdValue(kLargeId));
+  EXPECT_EQ(fuchsia_hardware_display::wire::kInvalidDispId, ToFidlEventIdValue(kInvalidEventId));
+}
+
+TEST(EventIdTest, ToEventIdWithFidlValue) {
+  EXPECT_EQ(kOne, ToEventId(1));
+  EXPECT_EQ(kTwo, ToEventId(2));
+  EXPECT_EQ(kLargeId, ToEventId(kLargeIdValue));
+  EXPECT_EQ(kInvalidEventId, ToEventId(fuchsia_hardware_display::wire::kInvalidDispId));
+}
+
+TEST(EventIdTest, FidlEventIdValueConversionRoundtrip) {
+  EXPECT_EQ(kOne, ToEventId(ToFidlEventIdValue(kOne)));
+  EXPECT_EQ(kTwo, ToEventId(ToFidlEventIdValue(kTwo)));
+  EXPECT_EQ(kLargeId, ToEventId(ToFidlEventIdValue(kLargeId)));
+  EXPECT_EQ(kInvalidEventId, ToEventId(ToFidlEventIdValue(kInvalidEventId)));
+}
+
+}  // namespace
+
+}  // namespace display
