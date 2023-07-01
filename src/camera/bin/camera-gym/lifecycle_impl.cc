3bbaa6492a503a72709b5b3c9ad900bd1c196455
Yegor Pomortsev <ypomortsev@google.com>
2023-07-01 01:02:07 +0000

    [camera-gym] Replace Modular lifecycle with process lifecycle
    Serve the fuchsia.process.lifecycle.Lifecycle protocol instead
    of fuchsia.modular.Lifecycle.
    
    * Remove dep on fuchsia.modular APIs
    * Refactor manifests into a common shard
      * Only manual mode needs /tmp because frame capture is only enabled
        in manual mode.
      * camera-gym-gfx does not `use` Flatland protocols
    
    Change-Id: I2f8d9f0d5688da0b438fa708d70f1d496585451a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/879421
    Reviewed-by: Gabe Schine <thatguy@google.com>
    Commit-Queue: Yegor Pomortsev <ypomortsev@google.com>

--- src/camera/bin/camera-gym/lifecycle_impl.cc
+++ src/camera/bin/camera-gym/lifecycle_impl.cc
@@ -5,20 +5,25 @@
 #include "src/camera/bin/camera-gym/lifecycle_impl.h"
 
 #include <lib/async-loop/default.h>
+#include <lib/syslog/cpp/macros.h>
+#include <zircon/processargs.h>
 
 LifecycleImpl::LifecycleImpl(fit::closure on_terminate)
     : loop_(&kAsyncLoopConfigNoAttachToCurrentThread), on_terminate_(std::move(on_terminate)) {
   loop_.StartThread("Lifecycle Thread");
+
+  zx::channel channel = zx::channel(zx_take_startup_handle(PA_LIFECYCLE));
+  if (!channel.is_valid()) {
+    FX_LOGS(FATAL) << "PA_LIFECYCLE startup handle is required.";
+  }
+
+  bindings_.AddBinding(
+      this, fidl::InterfaceRequest<fuchsia::process::lifecycle::Lifecycle>(std::move(channel)),
+      loop_.dispatcher());
 }
 
-LifecycleImpl::~LifecycleImpl() { loop_.Shutdown(); }
-
-fidl::InterfaceRequestHandler<fuchsia::modular::Lifecycle> LifecycleImpl::GetHandler() {
-  return fit::bind_member(this, &LifecycleImpl::OnNewRequest);
+void LifecycleImpl::Stop() {
+  on_terminate_();
+  loop_.Shutdown();
+  bindings_.CloseAll();
 }
-
-void LifecycleImpl::OnNewRequest(fidl::InterfaceRequest<fuchsia::modular::Lifecycle> request) {
-  bindings_.AddBinding(this, std::move(request), loop_.dispatcher());
-}
-
-void LifecycleImpl::Terminate() { on_terminate_(); }
