0d2570e270dee9c2696adeb5ee1e2c7549cfbb3f
Adam Perry <adamperry@google.com>
2023-07-10 23:38:07 +0000

    [starnix] Populate /sys/block with virtual devices.
    We'll need to also include non-virtual devices but this is all that's needed to
    unblock a roller.
    
    Bug: 129705
    Bug: 121327
    Bug: 130408
    Change-Id: I6d15b025452a01cb0353a487034e0c64f6ae4873
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/881237
    Reviewed-by: Guocheng Wei <guochengwei@google.com>
    Fuchsia-Auto-Submit: Adam Perry <adamperry@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/starnix/kernel/fs/sysfs/fs.rs
+++ src/starnix/kernel/fs/sysfs/fs.rs
@@ -46,6 +46,16 @@ impl SysFs {
             dir.subdir(b"tracing", 0o755, |_| ());
         });
 
+        // TODO(https://fxbug.dev/130408) include concrete block devices
+        let virtual_block_class = {
+            kernel.device_registry.read().virtual_bus().get_or_create_child(
+                b"block",
+                KType::Class,
+                SysFsDirectory::new,
+            )
+        };
+        dir.entry(b"block", SysFsDirectory::new(Arc::downgrade(&virtual_block_class)), dir_mode);
+
         dir.entry(
             b"devices",
             SysFsDirectory::new(Arc::downgrade(&kernel.device_registry.read().root_kobject())),
