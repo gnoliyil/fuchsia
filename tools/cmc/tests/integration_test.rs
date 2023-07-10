d99957d03e1db1baf85ceca2c9546832eea17e90
Clayton McCray <claytonmccray@google.com>
2023-07-10 19:59:38 +0000

    [inspect] Route InspectSink in //scripts, //src/session, tools
    This is in service of RFC 168:
    https://fuchsia.dev/fuchsia-src/contribute/governance/rfcs/0168_exposing_inspect_through_inspectsink
    
    InspectSink will replace the out/diagnostics directory.
    For an easier transition, InspectSink is routed by
    the diagnostics team before a compiler flag is added
    to cmc enforcing the offering of InspectSink.
    
    These changes are primarily automated and either add
    manual routing of InspectSink to children and collections
    in shards, or add the inspect/client.shard.cml if the
    CML represents a final component (ie non-shard).
    
    Bug: 93344
    Change-Id: I1629a837e9d9f6428072483e137b73bdb45b208d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/881760
    Fuchsia-Auto-Submit: Clayton McCray <claytonmccray@google.com>
    Reviewed-by: Yifei Teng <yifeit@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- tools/cmc/tests/integration_test.rs
+++ tools/cmc/tests/integration_test.rs
@@ -304,6 +304,51 @@ fn example_cml_integration_test() {
         }),
         Offer::Protocol(OfferProtocol {
             source: Some(Ref::Parent(ParentRef)),
+            source_name: Some("fuchsia.inspect.InspectSink".to_string()),
+            target: Some(Ref::Child(ChildRef { name: "logger".to_string(), collection: None })),
+            target_name: Some("fuchsia.inspect.InspectSink".to_string()),
+            dependency_type: Some(DependencyType::Strong),
+            availability: Some(Availability::Required),
+            ..Default::default()
+        }),
+        Offer::Protocol(OfferProtocol {
+            source: Some(Ref::Parent(ParentRef)),
+            source_name: Some("fuchsia.inspect.InspectSink".to_string()),
+            target: Some(Ref::Collection(CollectionRef { name: "explicit_dynamic".to_string() })),
+            target_name: Some("fuchsia.inspect.InspectSink".to_string()),
+            dependency_type: Some(DependencyType::Strong),
+            availability: Some(Availability::Required),
+            ..Default::default()
+        }),
+        Offer::Protocol(OfferProtocol {
+            source: Some(Ref::Parent(ParentRef)),
+            source_name: Some("fuchsia.inspect.InspectSink".to_string()),
+            target: Some(Ref::Collection(CollectionRef { name: "explicit_static".to_string() })),
+            target_name: Some("fuchsia.inspect.InspectSink".to_string()),
+            dependency_type: Some(DependencyType::Strong),
+            availability: Some(Availability::Required),
+            ..Default::default()
+        }),
+        Offer::Protocol(OfferProtocol {
+            source: Some(Ref::Parent(ParentRef)),
+            source_name: Some("fuchsia.inspect.InspectSink".to_string()),
+            target: Some(Ref::Collection(CollectionRef { name: "long_child_names".to_string() })),
+            target_name: Some("fuchsia.inspect.InspectSink".to_string()),
+            dependency_type: Some(DependencyType::Strong),
+            availability: Some(Availability::Required),
+            ..Default::default()
+        }),
+        Offer::Protocol(OfferProtocol {
+            source: Some(Ref::Parent(ParentRef)),
+            source_name: Some("fuchsia.inspect.InspectSink".to_string()),
+            target: Some(Ref::Collection(CollectionRef { name: "modular".to_string() })),
+            target_name: Some("fuchsia.inspect.InspectSink".to_string()),
+            dependency_type: Some(DependencyType::Strong),
+            availability: Some(Availability::Required),
+            ..Default::default()
+        }),
+        Offer::Protocol(OfferProtocol {
+            source: Some(Ref::Parent(ParentRef)),
             source_name: Some("fuchsia.logger.LogSink".to_string()),
             target: Some(Ref::Child(ChildRef { name: "logger".to_string(), collection: None })),
             target_name: Some("fuchsia.logger.LogSink".to_string()),
