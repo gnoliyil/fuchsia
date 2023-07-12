c183d678e38c1c87194e185146b0934b7643e954
Yifei Teng <yifeit@google.com>
2023-07-12 21:59:47 +0000

    [starnix] gen-android-starnix-container adds init.rc files
    When the HAL package has a `system/init.rc` file, add that to the
    container image.
    
    Bug: 129576
    Change-Id: Ie602c185ddd07585ed0db8a3963b8753e46ce277
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/883353
    Reviewed-by: Adam Perry <adamperry@google.com>
    Fuchsia-Auto-Submit: Yifei Teng <yifeit@google.com>
    Commit-Queue: Yifei Teng <yifeit@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Aidan Wolter <awolter@google.com>

--- src/starnix/tools/gen_android_starnix_container/src/main.rs
+++ src/starnix/tools/gen_android_starnix_container/src/main.rs
@@ -52,6 +52,9 @@ fn generate(cmd: Command) -> Result<()> {
     builder.name(&cmd.name);
     builder.published_name(&cmd.name);
 
+    // Track inputs for producing a depfile for incremental build correctness.
+    let mut inputs_for_depfile: Vec<String> = Vec::new();
+
     // Add all the HALs as subpackages.
     for hal in &cmd.hal {
         let manifest = PackageManifest::try_load_from(&hal)
@@ -60,6 +63,21 @@ fn generate(cmd: Command) -> Result<()> {
         builder
             .add_subpackage(&name, manifest.hash(), hal.into())
             .with_context(|| format!("Adding subpackage from manifest: {}", &hal))?;
+
+        // If a HAL package contains `system/init.rc`, copy that file to
+        // `data/init/${hal_package_name}.rc` in the resulting container package.
+        const HAL_INIT_RC_PATH: &str = "system/init.rc";
+        if let Some(init_rc_blob) =
+            manifest.blobs().iter().find(|blob| blob.path == HAL_INIT_RC_PATH)
+        {
+            let hal_package_name = manifest.name().to_string();
+            let src = &init_rc_blob.source_path;
+            let dst = format!("data/init/{hal_package_name}.rc");
+            builder.add_file_as_blob(&dst, src).with_context(|| {
+                format!("Adding init.rc for {hal_package_name} from {src} to {dst}")
+            })?;
+            inputs_for_depfile.push(src.clone());
+        }
     }
 
     // Put all the system image files into the container.
@@ -73,6 +91,7 @@ fn generate(cmd: Command) -> Result<()> {
         builder
             .add_file_as_blob(dst, &src)
             .with_context(|| format!("Adding blob from file: {}", &src))?;
+        inputs_for_depfile.push(src.clone());
     }
 
     // Build the starnix container.
@@ -92,7 +111,7 @@ fn generate(cmd: Command) -> Result<()> {
         .iter()
         .map(|p| p.to_string())
         .collect();
-        deps.extend(system_files.into_values());
+        deps.extend(inputs_for_depfile);
         let mut deps: String = deps.iter().map(|p| format!("{p} ")).collect();
         deps += &format!(": {}", cmd.system);
         std::fs::write(&depfile, &deps).with_context(|| format!("Writing depfile: {}", depfile))?;
@@ -110,17 +129,21 @@ mod tests {
 
     const EXT4_IMAGE_PATH: &str = "host_x64/test_data/gen-android-starnix-container/test.img";
 
-    #[fuchsia_async::run_singlethreaded(test)]
-    async fn test_generate() {
-        let tmp = TempDir::new().unwrap();
-        let outdir = Utf8Path::from_path(tmp.path()).unwrap();
-
+    fn fake_base(outdir: &Utf8Path) -> Utf8PathBuf {
         // Build a fake "base".
         let base_manifest_path = outdir.join("base_package_manifest.json");
         let mut builder = PackageBuilder::new("test-base");
         builder.add_contents_as_blob("data/test", "test-base-blob", &outdir).unwrap();
         builder.manifest_path(&base_manifest_path);
         let _ = builder.build(&outdir, outdir.join("base-meta.far")).unwrap();
+        base_manifest_path
+    }
+
+    #[test]
+    fn test_generate() {
+        let tmp = TempDir::new().unwrap();
+        let outdir = Utf8Path::from_path(tmp.path()).unwrap();
+        let base_manifest_path = fake_base(outdir);
 
         // Build a fake HAL.
         let hal_manifest_path = outdir.join("hal_package_manifest.json");
@@ -160,5 +183,50 @@ mod tests {
             ]
         );
         assert_eq!(subpackage_names, vec!["test-hal".to_string(),]);
+    }
+
+    #[test]
+    fn test_hal_init_rc() {
+        let tmp = TempDir::new().unwrap();
+        let outdir = Utf8Path::from_path(tmp.path()).unwrap();
+        let base_manifest_path = fake_base(outdir);
+
+        // Build a fake HAL with an init.rc file.
+        let hal_manifest_path = outdir.join("hal_package_manifest.json");
+        let mut builder = PackageBuilder::new("test-hal");
+        builder.add_contents_as_blob("data/hal", "test-hal-blob", &outdir).unwrap();
+        builder.add_contents_as_blob("system/init.rc", "service foo bar", &outdir).unwrap();
+        builder.manifest_path(&hal_manifest_path);
+        let _ = builder.build(&outdir, outdir.join("hal-meta.far")).unwrap();
+
+        // Run the generator.
+        let cmd = Command {
+            name: "test-name".into(),
+            outdir: outdir.to_owned(),
+            base: base_manifest_path,
+            system: Utf8PathBuf::from_str(EXT4_IMAGE_PATH).unwrap(),
+            hal: vec![hal_manifest_path],
+            depfile: None,
+        };
+        generate(cmd).unwrap();
+
+        // Read the package manifest, and ensure the correct files are present as blobs, and
+        // there is an additional `.rc` file corresponding to `test-hal`.
+        let manifest_path = outdir.join("package_manifest.json");
+        let manifest = PackageManifest::try_load_from(manifest_path).unwrap();
+        assert_eq!(manifest.name().as_ref(), "test-name");
+        let (blobs, _subpackages) = manifest.into_blobs_and_subpackages();
+        assert_eq!(blobs.len(), 5);
+        let blob_filenames: Vec<String> = blobs.into_iter().map(|b| b.path).collect();
+        assert_eq!(
+            blob_filenames,
+            vec![
+                "meta/".to_string(),
+                "data/init/test-hal.rc".to_string(),
+                "data/system/13".to_string(),
+                "data/system/metadata.v1".to_string(),
+                "data/test".to_string(),
+            ]
+        );
     }
 }
