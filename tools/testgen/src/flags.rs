b352e8b3bb9e6c22fd5ce976860149199756a114
Kendal Harland <kjharland@google.com>
2023-07-12 20:46:51 +0000

    [testgen] Only show error messages by default
    Bug: 130473
    Change-Id: I6de7b43acf988224046d63b234935be30f4f5412
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/883078
    Reviewed-by: Ankur Mittal <anmittal@google.com>
    Commit-Queue: Kendal Harland <kjharland@google.com>
    Fuchsia-Auto-Submit: Kendal Harland <kjharland@google.com>

--- tools/testgen/src/flags.rs
+++ tools/testgen/src/flags.rs
@@ -6,16 +6,34 @@
 /// `fx testgen`. Please run that command to make sure the output looks correct before
 /// submitting changes.
 use argh::FromArgs;
+use tracing::Level;
 
 /// testgen generates a Fuchsia test.
 #[derive(FromArgs, Debug)]
 pub(crate) struct Flags {
     #[argh(subcommand)]
     pub subcommand: Subcommand,
+
+    /// if true, all logs are printed. Otherwise only errors are shown.
+    #[argh(option, short = 'v', default = "false")]
+    pub verbose_logging: bool,
 }
 
 #[derive(FromArgs, PartialEq, Debug)]
 #[argh(subcommand)]
 pub(crate) enum Subcommand {
     IntegrationTest(crate::cmd_integration_test::IntegrationTestCmd),
+}
+
+impl Flags {
+    #[must_use]
+    pub fn setup_logging(&self) -> impl Drop {
+        let subscriber = tracing_subscriber::fmt::Subscriber::builder()
+            .with_max_level(match self.verbose_logging {
+                true => Level::INFO,
+                false => Level::ERROR,
+            })
+            .finish();
+        tracing::subscriber::set_default(subscriber)
+    }
 }
--- tools/testgen/src/main.rs
+++ tools/testgen/src/main.rs
@@ -10,6 +10,12 @@ use anyhow::{bail, Error, Result};
 #[fuchsia::main(logging_tags = ["testgen"])]
 async fn main() -> Result<(), Error> {
     let flags: flags::Flags = argh::from_env();
+
+    // _ignore is an `impl Drop` that sets the log level back to it's default
+    // value when dropped. We hold onto it to prevent that from happening until
+    // the program exits, and setup logging here rather than in each subcommand.
+    let _ignore = flags.setup_logging();
+
     let result = match flags.subcommand {
         flags::Subcommand::IntegrationTest(ref cmd) => cmd.run(&flags).await,
     };
