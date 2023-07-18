dfeadddcd5b7d04f61f024a26e88c1b34bd3b9c8
Bob Evans <bobevans@google.com>
2023-07-18 15:49:04 +0000

    [analytics][rust] Fix for 72654 analytics data collection page
    Bug: 72654
    
    Change-Id: I4dbf5167c5f28508675c6ffd501fae3e6c86481a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/884693
    Reviewed-by: Rundong Du <rundongdu@google.com>
    Commit-Queue: Bob Evans <bobevans@google.com>

--- src/lib/analytics/rust/src/notice.rs
+++ src/lib/analytics/rust/src/notice.rs
@@ -13,7 +13,7 @@ Analytics are not sent on this very first run. To disable reporting, type
 
     ffx config analytics disable
 
-To display the current setting and what is collected, type
+To display the current setting, type
 
     ffx config analytics show
 
@@ -24,8 +24,8 @@ By using Fuchsia developer tools, you agree to the Google Terms of Service.
 Note: The Google Privacy Policy describes how data is handled in your use of
 this service.
 
-Read about data we send with crash reports:
-https://fuchsia.dev/reference/crash-reporting
+Read about the data we send:
+https://fuchsia.dev/fuchsia-src/contribute/governance/policy/analytics_collected_fuchsia_tools?hl=en
 
 See Google's privacy policy:
 https://policies.google.com/privacy
@@ -42,16 +42,15 @@ To disable reporting, type
 
     ffx config analytics disable
 
-To display the current setting, a full list of tools sharing this setting, and
-what is collected, type
+To display the current setting, type
 
     ffx config analytics show
 
 If you opt out of analytics, an opt-out event will be sent, and then no further
 information will be sent by the developer tools to Google.
 
-Read about data we send with crash reports:
-https://fuchsia.dev/reference/crash-reporting
+Read about the data we send :
+https://fuchsia.dev/fuchsia-src/contribute/governance/policy/analytics_collected_fuchsia_tools?hl=en
 
 See Google's privacy policy:
 https://policies.google.com/privacy
