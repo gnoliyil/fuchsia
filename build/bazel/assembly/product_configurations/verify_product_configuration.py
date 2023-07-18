75222fabe7d85427b70c0655b0a41877246b2425
Aidan Wolter <awolter@google.com>
2023-07-18 19:57:07 +0000

    [assembly] Rebase forensics.cobalt.registry
    Support forensics.cobalt.registry in product assembly.
    
    Bug: b/290987899
    Change-Id: Ifce7372c6aedab94b5b06b4e39144e992cab40c1
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/885757
    Reviewed-by: Jay Zhuang <jayzhuang@google.com>
    Fuchsia-Auto-Submit: Aidan Wolter <awolter@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- build/bazel/assembly/product_configurations/verify_product_configuration.py
+++ build/bazel/assembly/product_configurations/verify_product_configuration.py
@@ -101,6 +101,7 @@ def normalize_platform(config, root_dir):
     files_to_normalize.append(
         "development_support.authorized_ssh_ca_certs_path")
     files_to_normalize.append("ui.sensor_config")
+    files_to_normalize.append("forensics.cobalt.registry")
     normalize_files_in_config(platform, files_to_normalize, root_dir)
 
     # Due to how some optional configs are routed to Bazel, there may be empty
