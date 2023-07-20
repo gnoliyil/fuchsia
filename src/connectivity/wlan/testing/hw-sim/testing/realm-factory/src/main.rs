ce3b7808e754bf619a4d2ddd67a32478c9243afe
John Tan <johntan@google.com>
2023-07-20 17:38:27 +0000

    [wlan][hw-sim] Add generated realm factory and FIDL
    This change adds the test realm factory and FIDL protocol generated by
    fx testgen. This additionally adds a test the realm factory based on the
    main.rs created by fx testgen.
    
    This does not introduce any functional changes to the existing hw-sim
    tests.
    
    Bug: 125740
    Test: fx test realm-factory-test
    Change-Id: I12fda2a97568c53d08c4a57fe85928d150345e8d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/871376
    Reviewed-by: David Worsham <dworsham@google.com>
    Reviewed-by: Kendal Harland <kjharland@google.com>
    Reviewed-by: Dylan Swiggett <swiggett@google.com>
    Commit-Queue: John Tan <johntan@google.com>

---
+++ src/connectivity/wlan/testing/hw-sim/testing/realm-factory/src/main.rs
@@ -0,0 +1,40 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+mod realm_factory_impl;
+mod realm_factory_server;
+use crate::realm_factory_impl::*;
+use crate::realm_factory_server::*;
+
+use {
+    anyhow::{Error, Result},
+    fidl_server::*,
+    fidl_test_wlan_realm::RealmFactoryRequestStream,
+    fuchsia_component::server::ServiceFs,
+    futures::StreamExt,
+    tracing::*,
+};
+
+enum IncomingService {
+    RealmFactory(RealmFactoryRequestStream),
+}
+
+#[fuchsia::main(logging = true)]
+async fn main() -> Result<(), Error> {
+    info!("starting");
+
+    let mut fs = ServiceFs::new();
+    fs.dir("svc").add_fidl_service(IncomingService::RealmFactory);
+    fs.take_and_serve_directory_handle()?;
+    fs.for_each_concurrent(0, |service: IncomingService| async {
+        match service {
+            IncomingService::RealmFactory(stream) => {
+                let server = RealmFactoryServer::new(RealmFactoryImpl::new());
+                serve_async(stream, server).await.expect("serve RealmFactory");
+            }
+        };
+    })
+    .await;
+    Ok(())
+}
---
+++ src/connectivity/wlan/testing/hw-sim/testing/realm-factory/src/test.rs
@@ -0,0 +1,30 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use anyhow::Result;
+use fidl::endpoints::create_endpoints;
+use fidl_test_wlan_realm as ftest;
+use fuchsia_component::client::connect_to_protocol;
+use realm_proxy::client::RealmProxyClient;
+use tracing::info;
+
+async fn create_realm(options: ftest::RealmOptions) -> Result<RealmProxyClient> {
+    let realm_factory = connect_to_protocol::<ftest::RealmFactoryMarker>()?;
+    let (client, server) = create_endpoints();
+
+    realm_factory.set_realm_options(options).await?.map_err(realm_proxy::Error::OperationError)?;
+    realm_factory.create_realm(server).await?.map_err(realm_proxy::Error::OperationError)?;
+
+    Ok(RealmProxyClient::from(client))
+}
+
+#[fuchsia::test]
+async fn test_example() -> Result<()> {
+    let realm_options = ftest::RealmOptions { ..Default::default() };
+    let _realm = create_realm(realm_options).await?;
+
+    info!("connected to the test realm!");
+
+    Ok(())
+}
