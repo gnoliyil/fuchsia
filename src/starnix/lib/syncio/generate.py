9fecb07d5debc8e82b02692ed3f10ea7be19085b
Mahesh Saripalli <maheshsr@google.com>
2023-07-20 23:29:35 +0000

    [py] Update all python shebang to use fuchsia-vendored-python
    We updated the GN build and test config to 3.11, update the remaining python
    scripts shebang from 3.* to fuchsia-vendored-python.
    
    BUG=b/280829652
    
    Change-Id: Id351de46c8bb01ad9986596eebd3e306a4ace0d6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/887554
    Fuchsia-Auto-Submit: Mahesh Saripalli <maheshsr@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Marc-Antoine Ruel <maruel@google.com>
    Commit-Queue: Mahesh Saripalli <maheshsr@google.com>

--- src/starnix/lib/syncio/generate.py
+++ src/starnix/lib/syncio/generate.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S python3 -B
+#!/usr/bin/env -S fuchsia-vendored-python -B
 # Copyright 2023 The Fuchsia Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
