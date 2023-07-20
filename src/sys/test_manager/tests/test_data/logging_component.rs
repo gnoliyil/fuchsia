3edd6efbd46eb257684241ac39766d4367e48e04
David Gilhooley <dgilhooley@google.com>
2023-07-20 23:17:57 +0000

    [build] Remove cmx template
    CFv1 components can no longer be run, so remove this support.
    This cleans up some dead code.
    
    Bug: 109012
    Change-Id: Ic816850e58da6035418df197fbcd7ab80c5c954f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/887597
    Fuchsia-Auto-Submit: David Gilhooley <dgilhooley@google.com>
    Reviewed-by: Yegor Pomortsev <ypomortsev@google.com>
    Reviewed-by: Gary Bressler <geb@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/sys/test_manager/tests/test_data/logging_component.rs
+++
@@ -1,9 +0,0 @@
-// Copyright 2020 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
-
-#[fuchsia::main(logging_minimum_severity = "debug")]
-async fn main() {
-    tracing::debug!("my debug message.");
-    tracing::info!("my info message.");
-    tracing::warn!("my warn message.");
-}
