3dd47bd605d97a024d249462efe668ccc5624661
John Tan <johntan@google.com>
2023-07-21 18:03:25 +0000

    [wlan][hw-sim] Implement realm factory for hw-sim
    This change:
    
    * Implements `build_realm` in `realm_factory_impl.rs` to actually build
      `wlan-hw-sim` and routes capabilities into/out of the component
    
    * Adds `memfs` as a child to `test-root`, which provides the backing
      directory for `data` and `cache `storage
    
    * Adds archivist to the test realm
    
    * Extends realm factory FIDL protocol to expose devfs through
      `RealmOptions`
    
    * Updates the realm factory test to connect to the exposed
      protocols/directories
    
    Bug: 125740
    Test: fx test realm-factory-test
    Change-Id: I41ce4febf3e391bac4b6355a794ccd5911f787e5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/871477
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Commit-Queue: John Tan <johntan@google.com>
    Reviewed-by: Dylan Swiggett <swiggett@google.com>
    Reviewed-by: Kendal Harland <kjharland@google.com>

--- src/connectivity/wlan/testing/hw-sim/testing/realm-factory/src/realm_factory_impl.rs
+++ src/connectivity/wlan/testing/hw-sim/testing/realm-factory/src/realm_factory_impl.rs
@@ -4,8 +4,11 @@
 
 use {
     anyhow::*,
-    fidl_test_wlan_realm as ftest,
-    fuchsia_component_test::{RealmBuilder, RealmInstance},
+    fidl::endpoints::ServerEnd,
+    fidl_fuchsia_io as fio, fidl_test_wlan_realm as ftest,
+    fuchsia_component_test::{
+        Capability, ChildOptions, ChildRef, RealmBuilder, RealmInstance, Ref, Route,
+    },
     tracing::info,
 };
 
@@ -23,22 +26,99 @@ impl RealmFactoryImpl {
             None => bail!("the realm has already been created"),
             Some(_) => self.realm_options.replace(options),
         };
+
+        info!("set_realm_options: {:?}", self.realm_options);
         Ok(())
     }
 
     pub async fn create_realm(&mut self) -> Result<RealmInstance, Error> {
         let realm_options = self.realm_options.take().unwrap();
-        build_realm(realm_options).await
+        let inst = build_realm(realm_options).await?;
+
+        Ok(inst)
     }
 }
 
-async fn build_realm(options: ftest::RealmOptions) -> Result<RealmInstance, Error> {
+async fn setup_archivist(builder: &RealmBuilder, wlan_components: &ChildRef) -> Result<(), Error> {
+    let archivist = builder
+        .add_child("archivist", "#meta/archivist-for-embedding.cm", ChildOptions::new())
+        .await?;
+
+    builder
+        .add_route(
+            Route::new()
+                .capability(Capability::protocol_by_name("fuchsia.diagnostics.ArchiveAccessor"))
+                .capability(Capability::protocol_by_name("fuchsia.logger.Log"))
+                .from(&archivist)
+                .to(Ref::parent())
+                .to(wlan_components),
+        )
+        .await?;
+
+    builder
+        .add_route(
+            Route::new()
+                .capability(Capability::protocol_by_name("fuchsia.logger.LogSink"))
+                .capability(Capability::protocol_by_name("fuchsia.sys2.EventSource"))
+                .capability(Capability::event_stream("directory_ready"))
+                .capability(Capability::event_stream("capability_requested"))
+                .from(Ref::parent())
+                .to(&archivist),
+        )
+        .await?;
+
+    Ok(())
+}
+
+async fn build_realm(mut options: ftest::RealmOptions) -> Result<RealmInstance, Error> {
     info!("building the realm using options {:?}", options);
 
     let builder = RealmBuilder::new().await?;
 
-    // FIXME: Copy realm builder code here.
+    let wlan_components = builder
+        .add_child("wlan-hw-sim", "#meta/wlan-hw-sim.cm", ChildOptions::new().eager())
+        .await?;
+
+    setup_archivist(&builder, &wlan_components).await?;
+
+    builder
+        .add_route(
+            Route::new()
+                .capability(Capability::protocol_by_name("fuchsia.driver.test.Realm"))
+                .capability(Capability::protocol_by_name("fuchsia.wlan.policy.ClientProvider"))
+                .capability(Capability::protocol_by_name("fuchsia.wlan.policy.AccessPointProvider"))
+                .capability(Capability::protocol_by_name(
+                    "fuchsia.wlan.device.service.DeviceMonitor",
+                ))
+                .capability(Capability::directory("dev-topological"))
+                .capability(Capability::directory("dev-class"))
+                .from(&wlan_components)
+                .to(Ref::parent()),
+        )
+        .await?;
+
+    builder
+        .add_route(
+            Route::new()
+                .capability(Capability::protocol_by_name("fuchsia.logger.LogSink"))
+                .capability(Capability::protocol_by_name("fuchsia.process.Launcher"))
+                .capability(Capability::storage("data"))
+                .capability(Capability::storage("cache"))
+                .from(Ref::parent())
+                .to(&wlan_components),
+        )
+        .await?;
 
     let realm = builder.build().await?;
+
+    let devfs = options.devfs_server_end.take().unwrap();
+
+    realm.root.get_exposed_dir().open(
+        fio::OpenFlags::DIRECTORY,
+        fio::ModeType::empty(),
+        "dev-topological",
+        ServerEnd::new(devfs.into_channel()),
+    )?;
+
     Ok(realm)
 }
