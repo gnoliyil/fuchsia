da8dedbdbcca6ff3fbd5b4522d1b286803d2d370
Mark Dittmer <markdittmer@google.com>
2023-07-21 15:50:48 +0000

    [scrutiny_x] Remove unboxed API
    This is the first of two changes that promotes the `boxed` API to the
    official Scrutiny X API. This change deletes the "unboxed "API and its
    smoke test. The second change will move the remaining code out of its
    `boxed` submodule.
    
    Change-Id: I560a29fda6913a1162413c17adda1b9c7fa02abc
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/887598
    Reviewed-by: Aidan Wolter <awolter@google.com>
    Reviewed-by: Laura Peskin <pesk@google.com>
    Commit-Queue: Mark Dittmer <markdittmer@google.com>

--- src/security/lib/scrutiny/x/src/lib.rs
+++ src/security/lib/scrutiny/x/src/lib.rs
@@ -2,30 +2,4 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-pub mod api;
-pub mod blob;
 pub mod boxed;
-pub mod component;
-pub mod component_capability;
-pub mod component_instance;
-pub mod component_instance_capability;
-pub mod component_manager;
-pub mod component_resolver;
-pub mod data_source;
-pub mod hash;
-pub mod package;
-pub mod package_resolver;
-pub mod product_bundle;
-pub mod scrutiny;
-pub mod system;
-pub mod todo;
-pub mod update_package;
-pub mod zbi;
-
-// Directly expose all API types from top-level crate.
-pub use api::*;
-
-// Expose select configuration types needed to instantiate scrutiny objects.
-pub use product_bundle::ProductBundleBuilder;
-pub use product_bundle::SystemSlot;
-pub use scrutiny::ScrutinyBuilder;
