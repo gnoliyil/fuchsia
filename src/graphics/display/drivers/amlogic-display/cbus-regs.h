27f093ac91a980d45dc64d7f46895e31bbd3a3a0
Victor Costan <costan@google.com>
2023-07-24 17:32:18 +0000

    [amlogic-display] Document the GPIO Multiplex registers used.
    Bug: 130970
    Test: fx build (no functional changes)
    Change-Id: Ib91053c101627bdcbff8a6b70500232fdfcb6f8c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/887565
    Fuchsia-Auto-Submit: Victor Costan <costan@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: John Bauman <jbauman@google.com>

--- src/graphics/display/drivers/amlogic-display/cbus-regs.h
+++ src/graphics/display/drivers/amlogic-display/cbus-regs.h
@@ -8,7 +8,17 @@
 #define READ32_CBUS_REG(a) cbus_mmio_->Read32(0x400 + a)
 #define WRITE32_CBUS_REG(a, v) cbus_mmio_->Write32(v, 0x400 + a)
 
-// Offsets
+// Register offsets from the A311D datasheet section 8.9.2 "GPIO Multiplex
+// Function", Table 8-23 "Pin Mux Registers".
+//
+// Section 8.9.2 states that all the registers below are on CBUS (regular
+// power-gated domain), and their offsets are relative to 0xff63'4400. Section
+// 8.1 "System" > "Memory Map" lists this range as PERIPHS_REGS.
+//
+// The following datasheets have matching information.
+// * S905D2, Section 6.8.2 "GPIO Multiplex Function", Section 6.1 "Memory Map"
+// * S905D3, Section 6.9.2 "GPIO Multiplex Function", Section 6.1 "Memory Map"
+// * T931, Section 6.9.2 "GPIO Multiplex Function", Section 6.1 "Memory Map"
 #define PAD_PULL_UP_EN_REG3 (0x4b << 2)
 #define PAD_PULL_UP_REG3 (0x3d << 2)
 #define P_PREG_PAD_GPIO3_EN_N (0x19 << 2)
