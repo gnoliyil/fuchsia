43d9a06d2870145bb7eab7b3570622d0a5ccc521
Tamir Duberstein <tamird@google.com>
2023-07-24 23:48:28 +0000

    Reland "[core] Remove core_proxy"
    This is a reland of commit 4aa4195bbc2cebd933aca9d6ccd20a56234bcbee
    
    The original change caused flakes in generic_fuchsia_device_test and
    soft_reboot_test due to https://fxbug.dev/130845. These tests launch
    SL4F by running `launch_sl4f` from a shell that connects to SL4F's
    Binder protocol.
    
    Since shells now run as separate components and are destroyed when the
    client disconnects, SL4F failed to start because the shell component was
    destroyed before routing completed. This was not a problem previously
    because the routing target was sshd-host itself, not the shell process,
    and the sshd-host component was never destroyed.
    
    This change has the following differences from the original change:
    
      * sshd-host logs "Waiting for next connection" with DEBUG severity,
        like the original description says. The original code left it
        as INFO severity.
    
    Original change's description:
    > [core] Remove core_proxy
    >
    > Launches the shell in a collection.
    >
    > All protocols used by shell binaries are now routed explicitly
    > to #ssh-host and the sshd component, instead of `use`s in core shards.
    >
    > * Remove some protocols from the shell namespace:
    >   - `fuchsia.intl.TimeZones`: This is likely unused and can't easily be
    >     routed because it both originates from a different child (#intl vs
    >     #time-zone-info-service) so we can't use
    >     `source_availability: "unknown"` and #sshd-host does not exist
    >     on all builds, so we can't route it from other shards.
    > * sshd-host now logs "Waiting for next connection" with debug severity.
    >   This message is logspam because sshd-host normally always waits for
    >   connections.
    > * Remove usb_webcam.core_shard.cml and replaced it with the regular
    >   camera.core_shard.cml. Both contain the same routes.
    > * Remove "testonly" bluetooth shards. The test protocols are now
    >   always routed to the shell. If this is undesirable, BT components
    >   should expose the protocol from void. e.g. see bt-init
    >   vs bt-init-testonly.
    > * network-basic.cml now exposes `fuchsia.net.virtualization.Control`
    >   from void.
    >
    > Bug: 104628, 97617, 130576, b/236962910
    > Change-Id: I64e690faaed960e6337737a714ae543b55343009
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/881091
    > Reviewed-by: Marie Janssen <jamuraa@google.com>
    > Reviewed-by: Mark Dittmer <markdittmer@google.com>
    > Reviewed-by: Gabe Schine <thatguy@google.com>
    > Commit-Queue: Yegor Pomortsev <ypomortsev@google.com>
    
    Multiply: host_x64/obj/src/testing/end_to_end/examples/test_soft_reboot/soft_reboot_test.sh
    Bug: 104628, 97617, 130576, b/236962910
    Change-Id: I8cd244a44d3e221ad4885a418072f278e474028f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/886471
    Reviewed-by: Gabe Schine <thatguy@google.com>
    Reviewed-by: Marina Ciocea <marinaciocea@google.com>
    Commit-Queue: Yegor Pomortsev <ypomortsev@google.com>

--- src/developer/sshd-host/service.h
+++ src/developer/sshd-host/service.h
@@ -5,36 +5,37 @@
 #ifndef SRC_DEVELOPER_SSHD_HOST_SERVICE_H_
 #define SRC_DEVELOPER_SSHD_HOST_SERVICE_H_
 
+#include <fuchsia/component/cpp/fidl.h>
+#include <lib/async/dispatcher.h>
 #include <lib/sys/cpp/service_directory.h>
-#include <lib/zx/job.h>
 #include <lib/zx/process.h>
 #include <sys/socket.h>
 
+#include <cstdint>
+#include <memory>
 #include <optional>
 #include <string>
 #include <vector>
 
-#include "constants.h"
-#include "fbl/unique_fd.h"
+#include <fbl/unique_fd.h>
+
 #include "src/lib/fsl/tasks/fd_waiter.h"
 
 namespace sshd_host {
 
-constexpr zx_rights_t kChildJobRights =
-    ZX_RIGHTS_BASIC | ZX_RIGHT_DESTROY | ZX_RIGHT_MANAGE_PROCESS | ZX_RIGHT_MANAGE_JOB;
-
-constexpr char kSshDirectory[] = "/data/ssh";
-constexpr char kAuthorizedKeysPath[] = "/data/ssh/authorized_keys";
+inline constexpr char kSshDirectory[] = "/data/ssh";
+inline constexpr char kAuthorizedKeysPath[] = "/data/ssh/authorized_keys";
+// Name of the collection that contains sshd shell child components.
+inline constexpr std::string_view kShellCollection = "shell";
 
 zx_status_t provision_authorized_keys_from_bootloader_file(
-    std::shared_ptr<sys::ServiceDirectory> service_directory);
-
-zx_status_t make_child_job(const zx::job& parent, std::string name, zx::job* job);
+    const std::shared_ptr<sys::ServiceDirectory>& service_directory);
 
 // Service relies on the default async dispatcher and is not thread safe.
 class Service {
  public:
-  explicit Service(uint16_t port);
+  Service(async_dispatcher_t* dispatcher, std::shared_ptr<sys::ServiceDirectory> service_directory,
+          uint16_t port);
   ~Service();
 
  private:
@@ -45,19 +46,20 @@ class Service {
     fsl::FDWaiter waiter;
   };
 
-  static fbl::unique_fd MakeSocket(IpVersion ip_version, uint16_t port);
+  static Socket MakeSocket(async_dispatcher_t* dispatcher, IpVersion ip_version, uint16_t port);
 
   void Wait(std::optional<IpVersion> ip_version);
-  void Launch(int conn, const std::string& peer_name);
-  void ProcessTerminated(zx::process process, zx::job job);
+  void Launch(fbl::unique_fd conn);
 
+  async_dispatcher_t* dispatcher_;
+  std::shared_ptr<sys::ServiceDirectory> service_directory_;
   uint16_t port_;
   // TODO(https://fxbug.dev/21198): Replace these with a single dual-stack
   // socket once Netstack3 supports that.
   Socket v4_socket_, v6_socket_;
-  zx::job job_;
+  uint64_t next_child_num_ = 0;
 
-  std::vector<std::unique_ptr<async::Wait>> process_waiters_;
+  std::vector<fuchsia::component::ExecutionControllerPtr> controllers_;
 };
 
 }  // namespace sshd_host
