485e5587f46bea87b83b41dc2d4f66bd9da065ea
Aidan Wolter <awolter@google.com>
2023-07-27 19:17:36 +0000

    [assembly] Normalize paths in board during verify
    The board configs now have paths for the zbi/vbmeta. We must normalize
    these paths before comparing between GN and Bazel.
    
    Bug: b/291976241
    Change-Id: Icada8e49b7aee07df61a7710467ad3e6c054bfc8
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/891200
    Reviewed-by: Jiaming Li <lijiaming@google.com>
    Fuchsia-Auto-Submit: Aidan Wolter <awolter@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- build/bazel/assembly/board_configurations/verify_board_configuration.py
+++ build/bazel/assembly/board_configurations/verify_board_configuration.py
@@ -6,10 +6,48 @@
 
 import argparse
 import difflib
+import hashlib
 import json
 import json5
 import os
 import sys
+
+
+def file_sha1(path):
+    sha1 = hashlib.sha1()
+    with open(path, "rb") as f:
+        sha1.update(f.read())
+    return sha1.hexdigest()
+
+
+def replace_with_file_hash(dict, key, root_dir, extra_files_read):
+    p = os.path.join(root_dir, dict[key])
+    dict[key] = file_sha1(p)
+    # Follow links for depfile entry. See https://fxbug.dev/122513.
+    p = os.path.relpath(os.path.realpath(p))
+    extra_files_read.append(p)
+
+
+def normalize(config, root_dir, extra_files_read):
+    if "filesystems" in config:
+        filesystems = config["filesystems"]
+
+        if "vbmeta" in filesystems:
+            vbmeta = filesystems["vbmeta"]
+            for key in ("key", "key_metadata"):
+                if key in vbmeta:
+                    replace_with_file_hash(
+                        vbmeta, key, root_dir, extra_files_read)
+
+        if "zbi" in filesystems:
+            zbi = filesystems["zbi"]
+            if "postprocessing_script" in zbi:
+                replace_with_file_hash(
+                    zbi["postprocessing_script"],
+                    "path",
+                    root_dir,
+                    extra_files_read,
+                )
 
 
 def main():
@@ -18,12 +56,27 @@ def main():
     parser.add_argument(
         "--generated_board_config", type=argparse.FileType("r"), required=True)
     parser.add_argument(
+        "--generated_root_dir",
+        help="Directory where paths in --generated_board_config are relative to",
+        required=True,
+    )
+    parser.add_argument(
         "--golden_json5", type=argparse.FileType("r"), required=True)
+    parser.add_argument(
+        "--golden_root_dir",
+        help="Directory where paths in --golden_json5 are relative to",
+        required=True,
+    )
+    parser.add_argument("--depfile", type=argparse.FileType("w"), required=True)
     parser.add_argument("--output", type=argparse.FileType("w"), required=True)
     args = parser.parse_args()
 
     generated = json.load(args.generated_board_config)
     golden = json5.load(args.golden_json5)
+
+    extra_files_read = []
+    normalize(generated, args.generated_root_dir, extra_files_read)
+    normalize(golden, args.golden_root_dir, extra_files_read)
 
     generated_str = json.dumps(generated, sort_keys=True, indent=2).splitlines()
     golden_str = json.dumps(golden, sort_keys=True, indent=2).splitlines()
@@ -39,10 +92,14 @@ def main():
     diffstr = "\n".join(diff)
     args.output.write(diffstr)
 
+    args.depfile.write(
+        "{}: {}".format(args.output.name, " ".join(extra_files_read)))
+
     if len(diffstr) != 0:
         print(
             "Error: non-empty diff between board configurations"
-            f" representations:\n{diffstr}", file=sys.stderr)
+            f" representations:\n{diffstr}",
+            file=sys.stderr)
         return 1
 
     return 0
