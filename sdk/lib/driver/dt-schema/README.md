d9ed20fdaf6fe9cac6054380496fefa037d4ab2e
Puneetha Ramachandra <puneetha@google.com>
2023-08-10 22:42:24 +0000

    [dt-schema] Add initial dt-schema tool.
    First commit for dt-schema.
    
    Co-authored-by: simonshields@google.com
    Bug: 112722
    Change-Id: I4b0e8044a7fec410dcf41b805e83b4e03874ef51
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/766760
    Commit-Queue: Puneetha Ramachandra <puneetha@google.com>
    Reviewed-by: Adam Perry <adamperry@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>

---
+++ sdk/lib/driver/dt-schema/README.md
@@ -0,0 +1,6 @@
+# dt-schema
+
+dt-schema is a devicetree schema validator.
+
+**NOTE** This is planned to be released as a separate open source rust crate eventually.
+Please do not add dependencies on fuchsia rust crates to this module.
---
+++ sdk/lib/driver/dt-schema/bin/validator.rs
@@ -0,0 +1,9 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use dt_schema_lib as _;
+
+fn main() {
+    println!("Hello, world!");
+}
