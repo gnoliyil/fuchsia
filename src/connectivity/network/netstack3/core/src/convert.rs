b3bc2ab2f924f5e7fb390e3fc407efb7a7414ff7
Alex Konradi <akonradi@google.com>
2023-08-15 19:55:16 +0000

    [netstack3] Add bidirectional converter trait
    This will be used to provide a simple API for converting between socket
    address representations.
    
    Bug: 21198
    Change-Id: I2c0b0e2583bed071f309f9ba40dbc56034812e29
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/901202
    Reviewed-by: Bruno Dal Bo <brunodalbo@google.com>
    Reviewed-by: Jeff Martin <martinjeffrey@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Alex Konradi <akonradi@google.com>

--- src/connectivity/network/netstack3/core/src/convert.rs
+++ src/connectivity/network/netstack3/core/src/convert.rs
@@ -4,6 +4,10 @@
 
 //! General utilities for converting types.
 
+use core::{convert::Infallible as Never, marker::PhantomData};
+
+use explicit::UnreachableExt as _;
+
 /// Produces an owned value `T` from Self.
 ///
 /// This trait is useful for implementing functions that take a value that may
@@ -28,9 +32,57 @@ impl<'s, T: Clone> OwnedOrCloned<T> for &'s T {
     }
 }
 
+/// Provides functions for converting infallibly between types.
+///
+/// This trait can be implemented on types that allow converting between two
+/// related types. It has two blanket implementations: `()` for identity
+/// conversions, i.e. `Input=Output`, and [`UninstantiableConverter`] as an
+/// uninstantiable type that implements the trait for any input and output.
+pub(crate) trait BidirectionalConverter<Input> {
+    /// The output type for the given `Input` type.
+    ///
+    /// An instance of this type is produced by `convert` and consumed by
+    /// `convert_back`.
+    type Output;
+
+    /// Converts an instance of `Input` into an instance of `Output`.
+    fn convert(&self, a: Input) -> Self::Output;
+
+    /// Converts an instance of `Output` into an instance of `Input`.
+    fn convert_back(&self, b: Self::Output) -> Input;
+}
+
+impl<I> BidirectionalConverter<I> for () {
+    type Output = I;
+    fn convert_back(&self, value: I) -> Self::Output {
+        value
+    }
+    fn convert(&self, value: I) -> Self::Output {
+        value
+    }
+}
+
+pub(crate) struct UninstantiableConverter<Output>(Never, PhantomData<Output>);
+
+impl<Output> AsRef<Never> for UninstantiableConverter<Output> {
+    fn as_ref(&self) -> &Never {
+        &self.0
+    }
+}
+
+impl<I, O> BidirectionalConverter<I> for UninstantiableConverter<O> {
+    type Output = O;
+    fn convert_back(&self, _: O) -> I {
+        self.uninstantiable_unreachable()
+    }
+    fn convert(&self, _: I) -> O {
+        self.uninstantiable_unreachable()
+    }
+}
+
 #[cfg(test)]
 mod test {
-    use super::OwnedOrCloned;
+    use super::*;
 
     #[derive(Debug, Eq, PartialEq)]
     struct Uncloneable(u8);
@@ -49,5 +101,12 @@ mod test {
         let reference = &cloneable;
         let cloned: Cloneable = reference.into_owned();
         assert_eq!(cloned, cloneable);
+    }
+
+    #[test]
+    fn bidirectional_converter_identity() {
+        let a = ();
+        assert_eq!(a.convert(123), 123);
+        assert_eq!(a.convert_back(123), 123);
     }
 }
