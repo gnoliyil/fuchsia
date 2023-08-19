c04f80149f9d8d200fa8efeb6495640056f73221
Paul Kirth <paulkirth@google.com>
2023-08-19 03:25:22 +0000

    [kernel][lib/arch] Replace AsBytes with cpp20::as_writable_bytes
    Also make expected values constexpr.
    
    Bug: 132007
    
    Change-Id: Id2d751cf6d61eb7e89dde897c2543200b8b68b2b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/904340
    Reviewed-by: Gianfranco Valentino <gevalentino@google.com>
    Reviewed-by: Joshua Seaton <joshuaseaton@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Fuchsia-Auto-Submit: Paul Kirth <paulkirth@google.com>

--- zircon/kernel/lib/arch/test/nop-tests.cc
+++ zircon/kernel/lib/arch/test/nop-tests.cc
@@ -13,9 +13,6 @@ namespace {
 
 using ::testing::ElementsAreArray;
 
-cpp20::span<std::byte> AsBytes(uint8_t* ptr, size_t size) {
-  return {reinterpret_cast<std::byte*>(ptr), size};
-}
 
 TEST(NopFillTests, Arm64) {
   constexpr size_t kInsnSize = 4;
@@ -23,28 +20,28 @@ TEST(NopFillTests, Arm64) {
   // 1 instruction.
   {
     constexpr size_t kSize = kInsnSize;
-    uint8_t expected[kSize] = {0x1f, 0x20, 0x03, 0xd5};
+    constexpr uint8_t kExpected[kSize] = {0x1f, 0x20, 0x03, 0xd5};
     uint8_t actual[kSize];
-    arch::NopFill<arch::Arm64NopTraits>(AsBytes(actual, kSize));
-    EXPECT_THAT(actual, ElementsAreArray(expected));
+    arch::NopFill<arch::Arm64NopTraits>(cpp20::as_writable_bytes(cpp20::span{actual}));
+    EXPECT_THAT(actual, ElementsAreArray(kExpected));
   }
 
   // 2 instructions.
   {
     constexpr size_t kSize = 2 * kInsnSize;
-    uint8_t expected[kSize] = {
+    constexpr uint8_t kExpected[kSize] = {
         0x1f, 0x20, 0x03, 0xd5,  //
         0x1f, 0x20, 0x03, 0xd5,
     };
     uint8_t actual[kSize];
-    arch::NopFill<arch::Arm64NopTraits>(AsBytes(actual, kSize));
-    EXPECT_THAT(actual, ElementsAreArray(expected));
+    arch::NopFill<arch::Arm64NopTraits>(cpp20::as_writable_bytes(cpp20::span{actual}));
+    EXPECT_THAT(actual, ElementsAreArray(kExpected));
   }
 
   // 5 instructions.
   {
     constexpr size_t kSize = 5 * kInsnSize;
-    uint8_t expected[kSize] = {
+    constexpr uint8_t kExpected[kSize] = {
         0x1f, 0x20, 0x03, 0xd5,  //
         0x1f, 0x20, 0x03, 0xd5,  //
         0x1f, 0x20, 0x03, 0xd5,  //
@@ -52,14 +49,14 @@ TEST(NopFillTests, Arm64) {
         0x1f, 0x20, 0x03, 0xd5,
     };
     uint8_t actual[kSize];
-    arch::NopFill<arch::Arm64NopTraits>(AsBytes(actual, kSize));
-    EXPECT_THAT(actual, ElementsAreArray(expected));
+    arch::NopFill<arch::Arm64NopTraits>(cpp20::as_writable_bytes(cpp20::span{actual}));
+    EXPECT_THAT(actual, ElementsAreArray(kExpected));
   }
 
   // 10 instructions.
   {
     constexpr size_t kSize = 10 * kInsnSize;
-    uint8_t expected[kSize] = {
+    constexpr uint8_t kExpected[kSize] = {
         0x1f, 0x20, 0x03, 0xd5,  //
         0x1f, 0x20, 0x03, 0xd5,  //
         0x1f, 0x20, 0x03, 0xd5,  //
@@ -72,8 +69,8 @@ TEST(NopFillTests, Arm64) {
         0x1f, 0x20, 0x03, 0xd5,
     };
     uint8_t actual[kSize];
-    arch::NopFill<arch::Arm64NopTraits>(AsBytes(actual, kSize));
-    EXPECT_THAT(actual, ElementsAreArray(expected));
+    arch::NopFill<arch::Arm64NopTraits>(cpp20::as_writable_bytes(cpp20::span{actual}));
+    EXPECT_THAT(actual, ElementsAreArray(kExpected));
   }
 }
 
@@ -83,146 +80,148 @@ TEST(NopFillTests, X86) {
   // 1 instruction.
   {
     constexpr size_t kSize = kInsnSize;
-    uint8_t expected[kSize] = {0x90};
+    constexpr uint8_t kExpected[kSize] = {0x90};
     uint8_t actual[kSize];
-    arch::NopFill<arch::X86NopTraits>(AsBytes(actual, kSize));
-    EXPECT_THAT(actual, ElementsAreArray(expected));
+    arch::NopFill<arch::X86NopTraits>(cpp20::as_writable_bytes(cpp20::span{actual}));
+    EXPECT_THAT(actual, ElementsAreArray(kExpected));
   }
 
   // 2 instructions.
   {
     constexpr size_t kSize = 2 * kInsnSize;
-    uint8_t expected[kSize] = {0x66, 0x90};
+    constexpr uint8_t kExpected[kSize] = {0x66, 0x90};
     uint8_t actual[kSize];
-    arch::NopFill<arch::X86NopTraits>(AsBytes(actual, kSize));
-    EXPECT_THAT(actual, ElementsAreArray(expected));
+    arch::NopFill<arch::X86NopTraits>(cpp20::as_writable_bytes(cpp20::span{actual}));
+    EXPECT_THAT(actual, ElementsAreArray(kExpected));
   }
 
   // 3 instructions.
   {
     constexpr size_t kSize = 3 * kInsnSize;
-    uint8_t expected[kSize] = {0x0f, 0x1f, 0x00};
+    constexpr uint8_t kExpected[kSize] = {0x0f, 0x1f, 0x00};
     uint8_t actual[kSize];
-    arch::NopFill<arch::X86NopTraits>(AsBytes(actual, kSize));
-    EXPECT_THAT(actual, ElementsAreArray(expected));
+    arch::NopFill<arch::X86NopTraits>(cpp20::as_writable_bytes(cpp20::span{actual}));
+    EXPECT_THAT(actual, ElementsAreArray(kExpected));
   }
 
   // 4 instructions.
   {
     constexpr size_t kSize = 4 * kInsnSize;
-    uint8_t expected[kSize] = {0x0f, 0x1f, 0x40, 0x00};
+    constexpr uint8_t kExpected[kSize] = {0x0f, 0x1f, 0x40, 0x00};
     uint8_t actual[kSize];
-    arch::NopFill<arch::X86NopTraits>(AsBytes(actual, kSize));
-    EXPECT_THAT(actual, ElementsAreArray(expected));
+    arch::NopFill<arch::X86NopTraits>(cpp20::as_writable_bytes(cpp20::span{actual}));
+    EXPECT_THAT(actual, ElementsAreArray(kExpected));
   }
 
   // 5 instructions.
   {
     constexpr size_t kSize = 5 * kInsnSize;
-    uint8_t expected[kSize] = {0x0f, 0x1f, 0x44, 0x00, 0x00};
+    constexpr uint8_t kExpected[kSize] = {0x0f, 0x1f, 0x44, 0x00, 0x00};
     uint8_t actual[kSize];
-    arch::NopFill<arch::X86NopTraits>(AsBytes(actual, kSize));
-    EXPECT_THAT(actual, ElementsAreArray(expected));
+    arch::NopFill<arch::X86NopTraits>(cpp20::as_writable_bytes(cpp20::span{actual}));
+    EXPECT_THAT(actual, ElementsAreArray(kExpected));
   }
 
   // 6 instructions.
   {
     constexpr size_t kSize = 6 * kInsnSize;
-    uint8_t expected[kSize] = {0x66, 0x0f, 0x1f, 0x44, 0x00, 0x00};
+    constexpr uint8_t kExpected[kSize] = {0x66, 0x0f, 0x1f, 0x44, 0x00, 0x00};
     uint8_t actual[kSize];
-    arch::NopFill<arch::X86NopTraits>(AsBytes(actual, kSize));
-    EXPECT_THAT(actual, ElementsAreArray(expected));
+    arch::NopFill<arch::X86NopTraits>(cpp20::as_writable_bytes(cpp20::span{actual}));
+    EXPECT_THAT(actual, ElementsAreArray(kExpected));
   }
 
   // 7 instructions.
   {
     constexpr size_t kSize = 7 * kInsnSize;
-    uint8_t expected[kSize] = {0x0f, 0x1f, 0x80, 0x00, 0x00, 0x00, 0x00};
+    constexpr uint8_t kExpected[kSize] = {0x0f, 0x1f, 0x80, 0x00, 0x00, 0x00, 0x00};
     uint8_t actual[kSize];
-    arch::NopFill<arch::X86NopTraits>(AsBytes(actual, kSize));
-    EXPECT_THAT(actual, ElementsAreArray(expected));
+    arch::NopFill<arch::X86NopTraits>(cpp20::as_writable_bytes(cpp20::span{actual}));
+    EXPECT_THAT(actual, ElementsAreArray(kExpected));
   }
 
   // 8 instructions.
   {
     constexpr size_t kSize = 8 * kInsnSize;
-    uint8_t expected[kSize] = {0x0f, 0x1f, 0x84, 0x00, 0x00, 0x00, 0x00, 0x00};
+    constexpr uint8_t kExpected[kSize] = {0x0f, 0x1f, 0x84, 0x00, 0x00, 0x00, 0x00, 0x00};
     uint8_t actual[kSize];
-    arch::NopFill<arch::X86NopTraits>(AsBytes(actual, kSize));
-    EXPECT_THAT(actual, ElementsAreArray(expected));
+    arch::NopFill<arch::X86NopTraits>(cpp20::as_writable_bytes(cpp20::span{actual}));
+    EXPECT_THAT(actual, ElementsAreArray(kExpected));
   }
 
   // 9 instructions.
   {
     constexpr size_t kSize = 9 * kInsnSize;
-    uint8_t expected[kSize] = {0x66, 0x0f, 0x1f, 0x84, 0x00, 0x00, 0x00, 0x00, 0x00};
+    constexpr uint8_t kExpected[kSize] = {0x66, 0x0f, 0x1f, 0x84, 0x00, 0x00, 0x00, 0x00, 0x00};
     uint8_t actual[kSize];
-    arch::NopFill<arch::X86NopTraits>(AsBytes(actual, kSize));
-    EXPECT_THAT(actual, ElementsAreArray(expected));
+    arch::NopFill<arch::X86NopTraits>(cpp20::as_writable_bytes(cpp20::span{actual}));
+    EXPECT_THAT(actual, ElementsAreArray(kExpected));
   }
 
   // 10 instructions.
   {
     constexpr size_t kSize = 10 * kInsnSize;
-    uint8_t expected[kSize] = {0x66, 0x66, 0x0f, 0x1f, 0x84, 0x00, 0x00, 0x00, 0x00, 0x00};
+    constexpr uint8_t kExpected[kSize] = {0x66, 0x66, 0x0f, 0x1f, 0x84,
+                                          0x00, 0x00, 0x00, 0x00, 0x00};
     uint8_t actual[kSize];
-    arch::NopFill<arch::X86NopTraits>(AsBytes(actual, kSize));
-    EXPECT_THAT(actual, ElementsAreArray(expected));
+    arch::NopFill<arch::X86NopTraits>(cpp20::as_writable_bytes(cpp20::span{actual}));
+    EXPECT_THAT(actual, ElementsAreArray(kExpected));
   }
 
   // 11 instructions.
   {
     constexpr size_t kSize = 11 * kInsnSize;
-    uint8_t expected[kSize] = {0x66, 0x66, 0x66, 0x0f, 0x1f, 0x84, 0x00, 0x00, 0x00, 0x00, 0x00};
+    constexpr uint8_t kExpected[kSize] = {0x66, 0x66, 0x66, 0x0f, 0x1f, 0x84,
+                                          0x00, 0x00, 0x00, 0x00, 0x00};
     uint8_t actual[kSize];
-    arch::NopFill<arch::X86NopTraits>(AsBytes(actual, kSize));
-    EXPECT_THAT(actual, ElementsAreArray(expected));
+    arch::NopFill<arch::X86NopTraits>(cpp20::as_writable_bytes(cpp20::span{actual}));
+    EXPECT_THAT(actual, ElementsAreArray(kExpected));
   }
 
   // 12 instructions.
   {
     constexpr size_t kSize = 12 * kInsnSize;
-    uint8_t expected[kSize] = {0x66, 0x66, 0x66, 0x66, 0x0f, 0x1f,
-                               0x84, 0x00, 0x00, 0x00, 0x00, 0x00};
+    constexpr uint8_t kExpected[kSize] = {0x66, 0x66, 0x66, 0x66, 0x0f, 0x1f,
+                                          0x84, 0x00, 0x00, 0x00, 0x00, 0x00};
     uint8_t actual[kSize];
-    arch::NopFill<arch::X86NopTraits>(AsBytes(actual, kSize));
-    EXPECT_THAT(actual, ElementsAreArray(expected));
+    arch::NopFill<arch::X86NopTraits>(cpp20::as_writable_bytes(cpp20::span{actual}));
+    EXPECT_THAT(actual, ElementsAreArray(kExpected));
   }
 
   // 13 instructions.
   {
     constexpr size_t kSize = 13 * kInsnSize;
-    uint8_t expected[kSize] = {0x66, 0x66, 0x66, 0x66, 0x66, 0x0f, 0x1f,
-                               0x84, 0x00, 0x00, 0x00, 0x00, 0x00};
+    constexpr uint8_t kExpected[kSize] = {0x66, 0x66, 0x66, 0x66, 0x66, 0x0f, 0x1f,
+                                          0x84, 0x00, 0x00, 0x00, 0x00, 0x00};
     uint8_t actual[kSize];
-    arch::NopFill<arch::X86NopTraits>(AsBytes(actual, kSize));
-    EXPECT_THAT(actual, ElementsAreArray(expected));
+    arch::NopFill<arch::X86NopTraits>(cpp20::as_writable_bytes(cpp20::span{actual}));
+    EXPECT_THAT(actual, ElementsAreArray(kExpected));
   }
 
   // 14 instructions.
   {
     constexpr size_t kSize = 14 * kInsnSize;
-    uint8_t expected[kSize] = {0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x0f,
-                               0x1f, 0x84, 0x00, 0x00, 0x00, 0x00, 0x00};
+    constexpr uint8_t kExpected[kSize] = {0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x0f,
+                                          0x1f, 0x84, 0x00, 0x00, 0x00, 0x00, 0x00};
     uint8_t actual[kSize];
-    arch::NopFill<arch::X86NopTraits>(AsBytes(actual, kSize));
-    EXPECT_THAT(actual, ElementsAreArray(expected));
+    arch::NopFill<arch::X86NopTraits>(cpp20::as_writable_bytes(cpp20::span{actual}));
+    EXPECT_THAT(actual, ElementsAreArray(kExpected));
   }
 
   // 15 instructions.
   {
     constexpr size_t kSize = 15 * kInsnSize;
-    uint8_t expected[kSize] = {0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x0f,
-                               0x1f, 0x84, 0x00, 0x00, 0x00, 0x00, 0x00};
+    constexpr uint8_t kExpected[kSize] = {0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x0f,
+                                          0x1f, 0x84, 0x00, 0x00, 0x00, 0x00, 0x00};
     uint8_t actual[kSize];
-    arch::NopFill<arch::X86NopTraits>(AsBytes(actual, kSize));
-    EXPECT_THAT(actual, ElementsAreArray(expected));
+    arch::NopFill<arch::X86NopTraits>(cpp20::as_writable_bytes(cpp20::span{actual}));
+    EXPECT_THAT(actual, ElementsAreArray(kExpected));
   }
 
   // 50 instructions.
   {
     constexpr size_t kSize = 50 * kInsnSize;
-    uint8_t expected[kSize] = {
+    constexpr uint8_t kExpected[kSize] = {
         0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x0f,
         0x1f, 0x84, 0x00, 0x00, 0x00, 0x00, 0x00,  // Size-15 nop.
         0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x0f,
@@ -232,8 +231,8 @@ TEST(NopFillTests, X86) {
         0x0f, 0x1f, 0x44, 0x00, 0x00,              // Size-5 nop.
     };
     uint8_t actual[kSize];
-    arch::NopFill<arch::X86NopTraits>(AsBytes(actual, kSize));
-    EXPECT_THAT(actual, ElementsAreArray(expected));
+    arch::NopFill<arch::X86NopTraits>(cpp20::as_writable_bytes(cpp20::span{actual}));
+    EXPECT_THAT(actual, ElementsAreArray(kExpected));
   }
 }
 
