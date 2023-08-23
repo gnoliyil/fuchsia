b713324a401744a833371e661a878ccdc782163e
Roland McGrath <mcgrathr@google.com>
2023-08-23 01:18:13 +0000

    [utest/core] Modernize thread-initial-state-test
    Use libzx.  Make the assembly protocol more simple so it doesn't
    need to call back into C(++) code in the test thread.  Explicitly
    test the second argument register value and the SP value.
    
    Bug: 132495
    Change-Id: I82e82eb4c9efba2198ee998971c0a05f21f95ffd
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/906484
    Reviewed-by: Nick Maniscalco <maniscalco@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>

--- zircon/system/utest/core/thread-initial-state/init-state.cc
+++ zircon/system/utest/core/thread-initial-state/init-state.cc
@@ -2,54 +2,58 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <zircon/compiler.h>
-#include <zircon/process.h>
-#include <zircon/syscalls.h>
+#include <lib/zx/process.h>
+#include <lib/zx/thread.h>
+
+#include <array>
+#include <cstdint>
+#include <string_view>
 
 #include <zxtest/zxtest.h>
 
 namespace {
 
-extern "C" void thread_entry(uintptr_t arg);
+// This checks every other expected starting register value (all zeros),
+// then does `*ptr = value;` to check the two argument registers, and
+// pushes kStackTopValue right below the starting SP value to check that.
+extern "C" [[noreturn]] void InitialStateTestThread(uint64_t* ptr, uint64_t value);
 
-extern "C" int print_fail(void) {
-  EXPECT_TRUE(false, "Failed");
-  zx_thread_exit();
-  return 1;  // Not reached
-}
+constexpr uint64_t kStackTopValue = 0x1234567890abcdef;
 
-// create a thread using the raw zircon api.
-// cannot use a higher level api because they'll use trampoline functions that'll trash
-// registers on entry.
-zx_status_t raw_thread_create(void (*thread_entry)(uintptr_t arg), uintptr_t arg,
-                              zx_handle_t* out) {
-  // preallocated stack to satisfy the thread we create
-  static uint8_t stack[1024] __ALIGNED(16);
+constexpr uint64_t kArgumentValue = 0xabcdef1234567890;
 
-  zx_handle_t handle;
-  zx_status_t status = zx_thread_create(zx_process_self(), "", 0, 0, &handle);
-  if (status < 0)
-    return status;
+alignas(16) std::array<uint64_t, 128> gThreadStack;
 
-  status =
-      zx_thread_start(handle, (uintptr_t)thread_entry, (uintptr_t)stack + sizeof(stack), arg, 0);
-  if (status < 0) {
-    zx_handle_close(handle);
-    return status;
-  }
+TEST(ThreadInitialStateTests, RegisterValues) {
+  constexpr std::string_view kThreadName = "thread-initial-state-test-thread";
+  zx::thread thread;
+  ASSERT_OK(
+      zx::thread::create(*zx::process::self(), kThreadName.data(), kThreadName.size(), 0, &thread));
 
-  *out = handle;
-  return ZX_OK;
-}
+  // Start the thread at the exact PC of the assembly entry point,
+  // with the SP exactly at the top of the stack.  Thus every register
+  // including PC, SP, and the two argument registers has a known value.
 
-TEST(TisTests, tis_test) {
-  uintptr_t arg = 0x1234567890abcdef;
-  zx_handle_t handle = ZX_HANDLE_INVALID;
-  zx_status_t status = raw_thread_create(thread_entry, arg, &handle);
-  ASSERT_EQ(status, ZX_OK, "Error while thread creation");
+  const uintptr_t pc = reinterpret_cast<uintptr_t>(&InitialStateTestThread);
+  const uintptr_t sp = reinterpret_cast<uintptr_t>(std::end(gThreadStack));
+  uint64_t arg1_points_to = 0;
+  const uint64_t arg1 = reinterpret_cast<uintptr_t>(&arg1_points_to);
+  const uint64_t arg2 = kArgumentValue;
 
-  status = zx_object_wait_one(handle, ZX_THREAD_TERMINATED, ZX_TIME_INFINITE, NULL);
-  ASSERT_GE(status, 0, "Error while thread wait");
+  ASSERT_OK(thread.start(pc, sp, arg1, arg2));
+
+  constexpr zx_signals_t kWaitFor = ZX_THREAD_TERMINATED;
+  zx_signals_t signals;
+  ASSERT_OK(thread.wait_one(kWaitFor, zx::time::infinite(), &signals));
+  EXPECT_TRUE(signals & ZX_THREAD_TERMINATED);
+
+  // The thread exits no matter what.  The last things it does are write the
+  // kStackTopValue, so we know the SP was where we put it; and store the
+  // second argument register's value in the first argument register's pointer,
+  // so we know both argument registers were what we passed to the syscall.
+
+  EXPECT_EQ(arg1_points_to, kArgumentValue);
+  EXPECT_EQ(gThreadStack.back(), kStackTopValue);
 }
 
 }  // namespace
