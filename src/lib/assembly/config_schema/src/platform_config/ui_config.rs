445ff98bc21ef0b6a748a75b20bf65feef4bb057
Justin Kobler <jkobler@google.com>
2023-08-24 20:07:34 +0000

    [scene-manager] Merge UI and Input Platform Configuration
    We would like to merge PlatformUiConfig and PlatformInputConfig to
    centralize all UI config.
    
    This CL tackles the first step which is adding existing Input config
    field to PlatformUiConfig. We must update smart_product_config after
    this CL lands before we can complete subsequent steps needed to
    completely merge UI and Input's platform config
    
    Bug: 132464
    Change-Id: I9e9bef812b253d316529de0cd88b11d039bd4312
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/906844
    Fuchsia-Auto-Submit: Justin Kobler <jkobler@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Aidan Wolter <awolter@google.com>

--- src/lib/assembly/config_schema/src/platform_config/ui_config.rs
+++ src/lib/assembly/config_schema/src/platform_config/ui_config.rs
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 use camino::Utf8PathBuf;
+use input_device_constants::InputDeviceType as PlatformInputDeviceType;
 use serde::{Deserialize, Serialize};
 
 /// Platform configuration options for the UI area.
@@ -30,6 +31,11 @@ pub struct PlatformUiConfig {
     /// GPU/Vulkan composition as the fallback. If false, GPU/Vulkan composition is always used.
     #[serde(default)]
     pub enable_display_composition: bool,
+
+    /// The relevant input device bindings from which to install appropriate
+    /// input handlers. Default to an empty set.
+    #[serde(default)]
+    pub supported_input_devices: Vec<InputDeviceType>,
 }
 
 impl Default for PlatformUiConfig {
@@ -40,6 +46,33 @@ impl Default for PlatformUiConfig {
             frame_scheduler_min_predicted_frame_duration_in_us: Default::default(),
             pointer_auto_focus: true,
             enable_display_composition: false,
+            supported_input_devices: Default::default(),
         }
     }
 }
+
+// LINT.IfChange
+/// Options for input devices that may be supported.
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
+#[serde(rename_all = "lowercase", deny_unknown_fields)]
+pub enum InputDeviceType {
+    Button,
+    Keyboard,
+    LightSensor,
+    Mouse,
+    Touchscreen,
+}
+
+// This impl verifies that the platform and assembly enums are kept in sync.
+impl From<InputDeviceType> for PlatformInputDeviceType {
+    fn from(src: InputDeviceType) -> PlatformInputDeviceType {
+        match src {
+            InputDeviceType::Button => PlatformInputDeviceType::ConsumerControls,
+            InputDeviceType::Keyboard => PlatformInputDeviceType::Keyboard,
+            InputDeviceType::LightSensor => PlatformInputDeviceType::LightSensor,
+            InputDeviceType::Mouse => PlatformInputDeviceType::Mouse,
+            InputDeviceType::Touchscreen => PlatformInputDeviceType::Touch,
+        }
+    }
+}
+// LINT.ThenChange(/src/ui/lib/input-device-constants/src/lib.rs)
