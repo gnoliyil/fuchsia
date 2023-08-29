89f740da0c13a580ea23ce09055a086433035ed7
David Fang <fangism@google.com>
2023-08-29 17:45:14 +0000

    [rbe] Update proto fetch-compile script
    Tested: ./build/rbe/proto/refresh.sh && ./build/rbe/test_all.sh
      (runs all Python unit-tests, including ones depending on fetched
      protos)
    
    Bug: 132255
    Change-Id: I6b9975759542ff90bd5d2a6a014bfc9f1d30eb15
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/908876
    Fuchsia-Auto-Submit: David Fang <fangism@google.com>
    Reviewed-by: John Wittrock <wittrock@google.com>
    Commit-Queue: David Fang <fangism@google.com>

--- build/rbe/proto/refresh.sh
+++ build/rbe/proto/refresh.sh
@@ -17,22 +17,32 @@ This script updates the public protos needed for reproxy logs collection.
 
 options:
   --reclient-srcdir DIR : location of re-client source
-     If none is provided, then this will checkout re-client source in a temp dir.
+     If none is provided, then this will checkout the source into a temp dir.
+  --remote-apis-srcdir DIR : location of remote-apis source
+     If none is provided, then this will checkout the source into a temp dir.
+  --googleapis-srcdir DIR : location of googleapis source
+     If none is provided, then this will checkout the source into a temp dir.
 
 This populates the Fuchsia source tree with the following files:
 
-  build/rbe/proto/api/log/log.proto
-  build/rbe/proto/api/log/log_pb2.py
-  build/rbe/proto/api/stats/stats.proto
-  build/rbe/proto/api/stats/stats_pb2.py
-  build/rbe/proto/go/api/command/command.proto
-  build/rbe/proto/go/api/command/command_pb2.py
-  third_party/protobuf/python/timestamp_pb2.py
-  third_party/protobuf/python/descriptor_pb2.py
+  build/rbe/proto/:
+    api/log/log.proto
+    api/log/log_pb2.py
+    api/stats/stats.proto
+    api/stats/stats_pb2.py
+    go/api/command/command.proto
+    go/api/command/command_pb2.py
+    and more...
+  third_party/protobuf/python/:
+    timestamp_pb2.py
+    descriptor_pb2.py
+    and more...
 EOF
 }
 
 RECLIENT_SRCDIR=
+REMOTE_APIS_SRCDIR=
+GOOGLEAPIS_SRCDIR=
 
 prev_opt=
 # Extract script options before --
@@ -56,6 +66,10 @@ do
     --help | -h ) usage; exit ;;
     --reclient-srcdir=*) RECLIENT_SRCDIR="$optarg" ;;
     --reclient-srcdir) prev_opt=RECLIENT_SRCDIR ;;
+    --remote-apis-srcdir=*) REMOTE_APIS_SRCDIR="$optarg" ;;
+    --remote-apis-srcdir) prev_opt=REMOTE_APIS_SRCDIR ;;
+    --googleapis-srcdir=*) GOOGLEAPIS_SRCDIR="$optarg" ;;
+    --googleapis-srcdir) prev_opt=GOOGLEAPIS_SRCDIR ;;
     *) echo "Unknown option: $opt" ; usage ; exit 1 ;;
   esac
   shift
@@ -63,22 +77,53 @@ done
 
 readonly DESTDIR="$script_dir"
 
+# TODO(fangism): choose a deterministic cache dir,
+# and pull instead of re-cloning every time.
+tmpdir="$(mktemp -d -t rbe_proto_refresh.XXXX)"
+
 # If reclient-srcdir is not provided, checkout in a tempdir
 test -n "$RECLIENT_SRCDIR" || {
   echo "Fetching re-client source."
-  tmpdir="$(mktemp -d -t rbe_proto_refresh.XXXX)"
   pushd "$tmpdir"
   git clone sso://team/foundry-x/re-client
   popd
   RECLIENT_SRCDIR="$tmpdir"/re-client
 }
 
-echo "Fetching protos from $RECLIENT_SRCDIR, installing to $DESTDIR"
+echo "Installing protos from $RECLIENT_SRCDIR to $DESTDIR"
 mkdir -p "$DESTDIR"/api/log
 grep -v "bq_table.proto" "$RECLIENT_SRCDIR"/api/log/log.proto | \
   grep -v "option.*gen_bq_schema" > "$DESTDIR"/api/log/log.proto
 mkdir -p "$DESTDIR"/api/stats
 cp "$RECLIENT_SRCDIR"/api/stats/stats.proto "$DESTDIR"/api/stats/
+
+
+test -n "$REMOTE_APIS_SRCDIR" || {
+  echo "Fetching bazelbuild/remote-apis source."
+  pushd "$tmpdir"
+  git clone https://github.com/bazelbuild/remote-apis.git
+  popd
+  REMOTE_APIS_SRCDIR="$tmpdir"/remote-apis
+}
+
+echo "Installing protos from $REMOTE_APIS_SRCDIR to $DESTDIR"
+readonly re_proto_subdir=build/bazel
+mkdir -p "$DESTDIR"/"$re_proto_subdir"
+cp -r "$REMOTE_APIS_SRCDIR"/"$re_proto_subdir"/* "$DESTDIR"/"$re_proto_subdir"/
+
+
+test -n "$GOOGLEAPIS_SRCDIR" || {
+  echo "Fetching googleapis/googleapis source."
+  pushd "$tmpdir"
+  git clone https://github.com/googleapis/googleapis.git
+  popd
+  GOOGLEAPIS_SRCDIR="$tmpdir"/googleapis
+}
+
+echo "Installing protos from $GOOGLEAPIS_SRCDIR to $DESTDIR"
+mkdir -p "$DESTDIR"/google
+cp -r "$GOOGLEAPIS_SRCDIR"/google/{api,longrunning,rpc} "$DESTDIR"/google/
+
 
 echo "Fetching proto from http://github.com/bazelbuild/remote-apis-sdks"
 mkdir -p "$DESTDIR"/go/api/command
@@ -90,43 +135,67 @@ protoc=(fx host-tool protoc)
 # Caveat: if fx build-metrics is already enabled with RBE, this fx build may
 # attempt to process and upload metrics before it is ready, and fail.
 
+# relative to $project_root:
 readonly PROTOBUF_SRC=third_party/protobuf/src
 readonly PROTOBUF_DEST=third_party/protobuf/python
 
-echo "Compiling $DESTDIR protos to Python"
-LOG_PROTOS=(
-  api/log/log.proto
-  api/stats/stats.proto
-  go/api/command/command.proto
-  rbe_metrics.proto
+# Walk the proto imports recursive to find what needs to be compiled.
+function walk_proto_imports() {
+  echo "$1"
+  local this="$1"
+  shift
+  local list
+  list=($(grep "^import" "$this" | cut -d'"' -f2))
+  for f in "${list[@]}"
+  do
+    if test -f "$f"
+    then walk_proto_imports "$f"
+    else echo "$f"  # assume that $f is in a different proto path
+    fi
+  done
+}
+
+pushd build/rbe/proto  # under $project_root
+_proto_compile_list=(
+  # top-level protos:
+  $(walk_proto_imports rbe_metrics.proto)
+  $(walk_proto_imports api/log/log.proto)
 )
-for proto in "${LOG_PROTOS[@]}"
+popd
+
+# Subset of these protos are in third_party/protobuf/src.
+proto_compile_list=(
+  $(echo "${_proto_compile_list[@]}" | tr ' ' '\n' | sort -u)
+)
+
+# NOTE: These generated *_pb2.py are NOT checked-in.
+echo "Compiling $DESTDIR protos to Python"
+for proto in "${proto_compile_list[@]}"
 do
-  "${protoc[@]}" \
-    -I="$DESTDIR" \
-    -I="$PROTOBUF_SRC" \
-    --python_out="$DESTDIR" \
-    "$DESTDIR"/"$proto"
+  case "$proto" in
+    google/protobuf/*)
+      # Generate third_party/protobuf/src -> third_party/protobuf/python.
+      echo "  $proto (to $PROTOBUF_DEST) ..."
+      "${protoc[@]}" \
+        -I="$PROTOBUF_SRC" \
+        -I="$PROTOBUF_DEST" \
+        --python_out="$PROTOBUF_DEST" \
+        "$PROTOBUF_SRC"/"$proto"
+      ;;
+    *)  # Everything else expected to be found in-place
+      echo "  $proto (to $DESTDIR) ..."
+      "${protoc[@]}" \
+        -I="$DESTDIR" \
+        -I="$PROTOBUF_SRC" \
+        --python_out="$DESTDIR" \
+        "$DESTDIR"/"$proto"
+      ;;
+  esac
 done
 
-# Generate third_party/protobuf/src -> third_party/protobuf/python.
-# NOTE: These generated *_pb2.py are NOT checked-in.
 # TODO(fangism): provide prebuilt package with protos already compiled.
 # Placing these in "$DESTDIR" doesn't work, because imports get confused with
 # the path to one of two locations with 'google.protobuf'.
-echo "Compiling $PROTOBUF_SRC protos to Python in $PROTOBUF_DEST"
-PB_PROTOS=(
-  timestamp.proto
-  descriptor.proto
-)
-for proto in "${PB_PROTOS[@]}"
-do
-  "${protoc[@]}" \
-    -I="$PROTOBUF_SRC" \
-    -I="$PROTOBUF_DEST" \
-    --python_out="$PROTOBUF_DEST" \
-    "$PROTOBUF_SRC"/google/protobuf/"$proto"
-done
 
 echo "Done."
 
