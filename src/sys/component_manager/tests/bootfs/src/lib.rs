9ca8caf9fc087af5184929b26dfa7b19c5afc550
Nikita Jindal <nikitajindal@google.com>
2023-08-30 18:46:59 +0000

    [pkg-cache] Add pkg-cache to the core_realm AIB as a package
    Bug: b/297461658
    Change-Id: I4139008d613bd3cf2dc980ff18ab5ef3a51fcf84
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/909413
    Reviewed-by: Gary Bressler <geb@google.com>
    Reviewed-by: Erick Tryzelaar <etryzelaar@google.com>
    Reviewed-by: Aaron Wood <aaronwood@google.com>
    Commit-Queue: Nikita Jindal <nikitajindal@google.com>

--- src/sys/component_manager/tests/bootfs/src/lib.rs
+++ src/sys/component_manager/tests/bootfs/src/lib.rs
@@ -17,8 +17,7 @@ use {
 const SAMPLE_UTF8_READONLY_FILE: &str = "/boot/config/build_info/minimum_utc_stamp";
 const SAMPLE_REQUIRED_DIRECTORY: &str = "/boot/lib";
 const KERNEL_VDSO_DIRECTORY: &str = "/boot/kernel/vdso";
-const BOOTFS_READONLY_FILES: &[&str] =
-    &["/boot/config/component_manager", "/boot/meta/pkg-cache.cm"];
+const BOOTFS_READONLY_FILES: &[&str] = &["/boot/config/component_manager"];
 const BOOTFS_DATA_DIRECTORY: &str = "/boot/data";
 const BOOTFS_EXECUTABLE_LIB_FILES: &[&str] = &["ld.so.1", "libdriver_runtime.so"];
 const BOOTFS_EXECUTABLE_NON_LIB_FILES: &[&str] = &["/boot/bin/component_manager"];
