c3a3d017b587050b8bea21fae51e943804a40b7b
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2023-09-06 19:41:16 +0000

    [component_manager][test] Use RealmQuery/Open instead of constructing NS
    Change-Id: Ib01b4fee2a383d813636a8f9f0f215b355917139
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/912353
    Reviewed-by: Justin Mattson <jmatt@google.com>
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/sys/component_manager/tests/realm/src/namespace_test.rs
+++ src/sys/component_manager/tests/realm/src/namespace_test.rs
@@ -2,13 +2,30 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use fidl::endpoints::ServerEnd;
 use fidl_fidl_examples_routing_echo::EchoMarker;
+use fidl_fuchsia_io as fio;
 use fidl_fuchsia_sys2 as fsys;
 use fuchsia_component::client::*;
 
 #[fuchsia::test]
 pub async fn connect_to_incoming_capabilities_for_component_without_program() {
     let query = connect_to_protocol::<fsys::RealmQueryMarker>().unwrap();
+
+    // We can't open the protocol in the component namespace until the component is resolved.
+    let (_echo, server_end) = fidl::endpoints::create_proxy::<EchoMarker>().unwrap();
+    assert!(query
+        .open(
+            "no_program",
+            fsys::OpenDirType::NamespaceDir,
+            fio::OpenFlags::empty(),
+            fio::ModeType::empty(),
+            "/svc/fidl.examples.routing.echo.Echo",
+            ServerEnd::new(server_end.into_channel()),
+        )
+        .await
+        .unwrap()
+        .is_err());
 
     // Trigger resolution.
     let lifecycle =
@@ -19,18 +36,20 @@ pub async fn connect_to_incoming_capabilities_for_component_without_program() {
         .expect("fidl ok")
         .expect("resolved instance");
 
-    // Create incoming namespace.
-    let namespace = query.construct_namespace("./no_program").await.unwrap().unwrap();
-    let svc_entry = namespace
-        .into_iter()
-        .find(|entry| matches!(&entry.path, Some(path) if path == "/svc"))
-        .expect("found Echo entry");
-    let svc_dir = svc_entry
-        .directory
-        .expect("svc has a directory handle")
-        .into_proxy()
-        .expect("get DirectoryProxy from handle");
-    let echo = connect_to_protocol_at_dir_root::<EchoMarker>(&svc_dir).expect("connect to Echo");
+    // Open protocol in the component namespace.
+    let (echo, server_end) = fidl::endpoints::create_proxy::<EchoMarker>().unwrap();
+    query
+        .open(
+            "no_program",
+            fsys::OpenDirType::NamespaceDir,
+            fio::OpenFlags::empty(),
+            fio::ModeType::empty(),
+            "/svc/fidl.examples.routing.echo.Echo",
+            ServerEnd::new(server_end.into_channel()),
+        )
+        .await
+        .unwrap()
+        .unwrap();
     let reply = echo.echo_string(Some("test")).await.expect("called echo string");
     assert_eq!(reply.unwrap(), "test");
 }
