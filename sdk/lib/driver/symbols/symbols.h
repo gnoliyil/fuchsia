ae45807c65a9ce908489c946a57d9fbcdcffb458
Novin Changizi <novinc@google.com>
2023-09-06 19:33:23 +0000

    [fdf][fidl] Create driver FIDL with fdf transport
    Creates a new "Driver" FIDL protocol in the fuchsia.driver.framework
    FIDL library that will be used as a replacement for the C based
    lifecycle hooks in a DFv2 driver. The mechanism to connect to this is
    through a single initialize C hook to exchange the channel that will be
    used to communicate this new protocol.
    
    Bug: 130579
    Change-Id: I7b33a738485cf70a7524844218a7db2f2103d927
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/884201
    API-Review: Jocelyn Dang <jocelyndang@google.com>
    Reviewed-by: Yifei Teng <yifeit@google.com>
    Commit-Queue: Novin Changizi <novinc@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>

--- sdk/lib/driver/symbols/symbols.h
+++ sdk/lib/driver/symbols/symbols.h
@@ -6,7 +6,61 @@
 #define LIB_DRIVER_SYMBOLS_SYMBOLS_H_
 
 #include <lib/fdf/dispatcher.h>
+#include <lib/fdf/types.h>
 #include <zircon/fidl.h>
+
+// The |DriverRegistration| is the ABI for drivers to expose themselves to the Driver Framework.
+// The driver is loaded in as a shared library (also referred to as a DSO), and the global symbol
+// `__fuchsia_driver_registration__` is used by the Driver Framework to locate this
+// DriverRegistration in the driver library. The framework will use this to initiate a FIDL based
+// connection over the driver transport into the driver.
+struct DriverRegistration {
+  // The version is not expected to change since the evolution of the driver APIs will happen
+  // on the FIDL side. This simply exists in case we need to evolve the initialization signature
+  // for any reason.
+  uint64_t version;
+
+  struct v1 {
+    // Pointer to a function that can initialize the driver server on the |server_handle|.
+    //
+    // Errors to initialize the driver server can be propagated by closing the |server_handle| with
+    // an epitaph, but driver start failures should use the corresponding FIDL protocol.
+    //
+    // This function is executed on the default driver dispatcher. This dispatcher is synchronized
+    // and runs on a shared driver thread within a `driver_host`. The driver can access this
+    // dispatcher using |fdf_dispatcher_get_current_dispatcher()| or in C++ with
+    // |fdf::Dispatcher::GetCurrent()|. This can be stored in an
+    // |fdf::UnownedSynchronizedDispatcher| as the driver host is the owner of the dispatcher.
+    //
+    // |server_handle| is the server end for the |fuchsia.driver.framework/Driver| protocol that
+    // the driver should begin serving on. This should be served on the driver dispatcher
+    // that this function is executed on.
+    //
+    // The return result is a `void*` token that the driver framework will pass into |destroy|.
+    void* (*initialize)(fdf_handle_t server_handle);
+
+    // Pointer to a function that can free the resources that were allocated as part of
+    // |initialize|. Generally this will just be the object allocated to serve the
+    // |fuchsia.driver.framework/Driver| protocol.
+    //
+    // This function is executed after all of the dispatchers belonging to the driver have fully
+    // been shutdown, including the default dispatcher that |initialize| was executed on.
+    //
+    // |token| is the return result of the |initialize| function.
+    void (*destroy)(void* token);
+  } v1;
+};
+
+#define DRIVER_REGISTRATION_VERSION_1 1
+
+#define DRIVER_REGISTRATION_VERSION_MAX DRIVER_REGISTRATION_VERSION_1
+
+#define FUCHSIA_DRIVER_REGISTRATION_V1(initialize, destroy) \
+  { .version = DRIVER_REGISTRATION_VERSION_1, .v1 = {initialize, destroy}, }
+
+#define EXPORT_FUCHSIA_DRIVER_REGISTRATION_V1(initialize, destroy)             \
+  extern "C" const DriverRegistration __fuchsia_driver_registration__ __EXPORT \
+  FUCHSIA_DRIVER_REGISTRATION_V1(initialize, destroy)
 
 // Lightweight reference to an encoded FIDL message.
 //
