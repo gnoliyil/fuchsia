8937e132d1ef7a5dab99cd00f9dbec63a7ffa5e0
David Fang <fangism@google.com>
2023-09-07 21:48:04 +0000

    [rbe] Co-locate reproxy log dir with build output
    Keeping dirs on the same device will make move operations atomic.
    
    Keep a symlink at the old /tmp locations for user convenience.
    
    Fixed: 133165
    Change-Id: I385614e1dd7aa2de66ebe87a26988ffa8822286c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/913132
    Reviewed-by: John Wittrock <wittrock@google.com>
    Commit-Queue: David Fang <fangism@google.com>

--- build/rbe/fuchsia-reproxy-wrap.sh
+++ build/rbe/fuchsia-reproxy-wrap.sh
@@ -97,8 +97,37 @@ reproxy="$reclient_bindir"/reproxy
 # Establish a single log dir per reproxy instance so that statistics are
 # accumulated per build invocation.
 date="$(date +%Y%m%d-%H%M%S)"
-reproxy_logdir="$(mktemp -d -t reproxy."$date".XXXX)"
-server_address=unix://"$reproxy_logdir"/reproxy.sock
+readonly build_dir_file="$project_root_rel/.fx-build-dir"
+build_subdir=out/unknown
+if test -f "$build_dir_file"
+then
+  # Locate the reproxy logs and temporary dirs on the same device as
+  # the build output, so that moves can be done atomically,
+  # and this avoids cross-device linking problems.
+  build_subdir="$(cat "$build_dir_file")"
+fi
+readonly logs_root="$project_root/$build_subdir/.reproxy_logs"
+mkdir -p "$logs_root"
+readonly reproxy_logdir="$(mktemp -d -t "reproxy.$date.XXXX" -p "$logs_root")"
+readonly log_base="$(basename "$reproxy_logdir")"
+
+readonly _tmpdir="$(dirname "$(mktemp -u)")"
+# Symlink to the old location, where users may be accustomed to looking.
+ln -s -f "$reproxy_logdir" "$_tmpdir"/
+
+# The socket file doesn't need to be co-located with logs.
+# Using an absolute path to the socket allows rewrapper to be invoked
+# from different working directories.
+readonly socket_path="$_tmpdir/$log_base.sock"
+test "${#socket_path}" -le 100 || {
+  cat <<EOF
+Socket paths are limited to around 100 characters on some platforms.
+  Got: $socket_path (${#socket_path} characters)
+See https://unix.stackexchange.com/questions/367008/why-is-socket-path-length-limited-to-a-hundred-chars
+EOF
+  exit 1
+}
+readonly server_address="unix://$socket_path"
 
 # deps cache dir should be somewhere persistent between builds,
 # and thus, not random.  /var/cache can be root-owned and not always writeable.
@@ -121,7 +150,7 @@ bootstrap_env=(
 )
 
 # These environment variables take precedence over those found in --cfg.
-rewrapper_log_dir="$reproxy_logdir/rewrapper-logs"
+readonly rewrapper_log_dir="$reproxy_logdir/rewrapper-logs"
 mkdir -p "$rewrapper_log_dir"
 rewrapper_env=(
   env
@@ -207,8 +236,7 @@ EOF
 # the build happens.  The default $TMPDIR is not guaranteed to
 # be on the same physical device.
 # Re-use the randomly generated dir name in a custom tempdir.
-build_dir="${FUCHSIA_BUILD_DIR:-.}"
-reproxy_tmpdir="$build_dir"/reproxy_tmpdirs/"$(basename "$reproxy_logdir")"
+reproxy_tmpdir="$project_root/$build_subdir"/.reproxy_tmpdirs/"$log_base"
 mkdir -p "$reproxy_tmpdir"
 
 function cleanup() {
