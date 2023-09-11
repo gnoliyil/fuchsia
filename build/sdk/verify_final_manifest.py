ebbb4cc35cbd25fa233e79e4d96b9a2f62c26665
Hunter Freyer <hjfreyer@google.com>
2023-09-11 15:51:01 +0000

    Reland "[idk] Golden manifests over IDK."
    This reverts commit d97be7dc4dad2524ebfffeaee9c5196713fb4c84.
    
    Original change's description:
    > [idk] Golden manifests over IDK.
    >
    > Compute the "golden manifest" files for the IDK using the final IDK
    > itself, rather than the individual `sdk_collections` that make it up.
    > The `sdk_collections` are an implementation detail, rather than a
    > part of our contract with our partners - we should be validating
    > changes to the actual thing we ship to our partners, not intermediate
    > results.
    >
    > The next step will be to update petals to use this new mechanism, and
    > then delete the existing `sdk_collection`-based manifest files.
    >
    > I compared the new manifest file with the concatenation of core
    > and e2e_testing, and the only non-cosemetic change I found was that
    > arm64/aemu_internal and arm64/qemu_internal are listed in the old
    > manifests, but don't actually appear in the IDK, which I believe
    > is working as intended.
    >
    > Interestingly, the tools for e2e_testing (fake-omaha-client and
    > trace2json) are also not built for arm64. Not sure if that's WAI or
    > not.
    >
    > Change-Id: Id2c280f6031d986afb5ab9e04301614c456bff9e
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/911952
    > Reviewed-by: David Turner <digit@google.com>
    > Commit-Queue: Hunter Freyer <hjfreyer@google.com>
    
    Change-Id: I4734119d54ca2174c0cf3e610eca45e5a0898484
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/912715
    Commit-Queue: Hunter Freyer <hjfreyer@google.com>
    Reviewed-by: David Turner <digit@google.com>

---
+++ build/sdk/verify_final_manifest.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env fuchsia-vendored-python
+# Copyright 2023 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Compare the contents of an IDK to a golden file.
+
+This script reads the manifest.json file from an exported IDK directory and
+compares its contents to one or more existing golden files. If the contents of
+the IDK don't match the contents of the golden file, this returns an error
+indicating the set of changes that should be acknowledged by updating the golden
+file.
+
+Used by `sdk_final_manifest_golden` GN rules.
+"""
+
+import argparse
+import json
+import os
+import sys
+from typing import Sequence, TypedDict
+
+HOST_TOOL_SCHEMES: Sequence[str] = [
+    "host_tool", "ffx_tool", "companion_host_tool"
+]
+
+IdkPart = TypedDict("IdkPart", {"meta": str, "type": str})
+IdkManifest = TypedDict("IdkManifest", {"parts": Sequence[IdkPart]})
+
+
+def part_to_id(part: IdkPart) -> str:
+    """Get a string ID for a part of the IDK.
+
+    Args:
+    - part: An entry from the `parts` field of the IDK manifest.
+
+    Returns:
+      A string ID for that part, that's a bit more human-friendly than the
+      JSON object.
+    """
+    path = part["meta"]
+    if os.path.basename(path) == "meta.json":
+        path = os.path.dirname(path)
+    elif path.endswith("-meta.json"):
+        path = path[:-len("-meta.json")]
+
+    return f'{part["type"]}://{path}'
+
+
+def part_is_tool_for_other_cpu(part_id: str, host_cpu: str) -> bool:
+    """Returns True if a given part is a host tool for a different cpu."""
+    for scheme in HOST_TOOL_SCHEMES:
+        if part_id.startswith(f"{scheme}://") and not part_id.startswith(
+                f"{scheme}://tools/{host_cpu}/"):
+            return True
+    return False
+
+
+def remove_tools_for_other_cpus(part_ids: set[str], cpu: str) -> set[str]:
+    """Takes a set of IDs and returns a set excluding host tools built
+    for other CPU architectures."""
+    return {id for id in part_ids if not part_is_tool_for_other_cpu(id, cpu)}
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "--manifest", help="Path to the IDK manifest", required=True)
+    parser.add_argument(
+        "--golden", help="Path to the golden file", required=True)
+    parser.add_argument(
+        "--inherit_golden",
+        help=(
+            "Inherit items from this golden file, if specified, and omit "
+            "any items from this file in the updated golden."),
+        required=False,
+    )
+    parser.add_argument(
+        "--only_verify_host_tools_for_cpu",
+        help=
+        "If specified, ignore any host tools that aren't built for the named CPU arch.",
+        required=False,
+    )
+    parser.add_argument(
+        "--updated_golden",
+        help=(
+            "Path where a new version of the golden file will be written. The "
+            "contents of this file will be meaningful iff "
+            "--only_verify_host_tools_for_cpu is not specified"),
+        required=True,
+    )
+
+    args = parser.parse_args()
+
+    with open(args.manifest) as f:
+        manifest: IdkManifest = json.load(f)
+    manifest_ids = {part_to_id(part) for part in manifest["parts"]}
+
+    with open(args.golden) as f:
+        golden_ids = {line.strip() for line in f}
+
+    if args.inherit_golden:
+        with open(args.inherit_golden) as f:
+            inherit_golden_ids = {line.strip() for line in f}
+    else:
+        inherit_golden_ids = set()
+
+    effective_golden_ids = golden_ids | inherit_golden_ids
+
+    added_ids: set[str] = manifest_ids - effective_golden_ids
+    removed_ids: set[str] = effective_golden_ids - manifest_ids
+
+    if args.only_verify_host_tools_for_cpu:
+        added_ids = remove_tools_for_other_cpus(
+            added_ids, args.only_verify_host_tools_for_cpu)
+        removed_ids = remove_tools_for_other_cpus(
+            removed_ids, args.only_verify_host_tools_for_cpu)
+
+        # Write the file even if it's not useful as an updated golden, because
+        # GN expects it to be there.
+        with open(args.updated_golden, "w") as f:
+            print("Golden cannot be automatically updated.", file=f)
+    else:
+        with open(args.updated_golden, "w") as f:
+            for id in sorted(manifest_ids - inherit_golden_ids):
+                print(id, file=f)
+
+    if added_ids:
+        print("Parts added to IDK:", file=sys.stderr)
+        for id in sorted(added_ids):
+            print(" - " + id, file=sys.stderr)
+    if removed_ids:
+        print("Parts removed from IDK:", file=sys.stderr)
+        for id in sorted(removed_ids):
+            print(" - " + id, file=sys.stderr)
+    if removed_ids or added_ids:
+        print("Error: IDK contents have changed!", file=sys.stderr)
+        if args.only_verify_host_tools_for_cpu:
+            print(
+                f"""\
+The manifest cannot be automatically updated when not cross compiling host tools.
+
+Please update the manifest manually - {args.golden}
+
+or run fx set with "--args sdk_cross_compile_host_tools=true"
+""",
+                file=sys.stderr,
+            )
+        else:
+            print(
+                f"""\
+Please acknowledge this change by running:
+
+  cp {os.path.abspath(args.updated_golden)} {os.path.abspath(args.golden)}
+""",
+                file=sys.stderr,
+            )
+        return 1
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
