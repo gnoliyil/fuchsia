a75dd600ec47a46e71246d796601e9d953c5ea6a
Jiaming Li <lijiaming@google.com>
2023-09-12 16:39:25 +0000

    [ffx][pb] Change get-artifacts to also support emu
    We are seeing infra bots failure when we try to use product bundle. The reason is, Product Bundles are too big for `test bots`, and we don't really need all the artifacts exist inside Product bundle directory to fulfill flashing/emulating of the test devices.
    
    The solution we are trying is, in build bots, we invoke `ffx product get-artifacts` and upload only the necessary artifacts to CAS, so when test bots fetch all the artifacts from CAS, there will only be artifacts needed for flashing or emulating. This needs `ffx product get-artifacts`  to also support collecting emu artifacts.
    
    Change-Id: I212d5680e850d8c80688cf372aaf67daf8d9ae29
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/913597
    Commit-Queue: Jiaming Li <lijiaming@google.com>
    Reviewed-by: Aidan Wolter <awolter@google.com>

--- src/developer/ffx/plugins/product/get_artifacts/src/lib.rs
+++ src/developer/ffx/plugins/product/get_artifacts/src/lib.rs
@@ -30,7 +30,8 @@ pub async fn pb_get_artifacts(
     let artifacts = match cmd.artifacts_group {
         Type::Flash => extract_flashing_artifacts(product_bundle, cmd)?,
         Type::Bootloader => extract_bootloaders(product_bundle, cmd)?,
-        _ => ffx_bail!("Only get flash artifacts is supported as of now"),
+        Type::Emu => extract_emu_artifacts(product_bundle, cmd)?,
+        _ => ffx_bail!("OTA artifacts is not supported as of now"),
     };
     let artifact_string = artifacts.join("\n");
     if writer.is_machine() {
@@ -110,7 +111,12 @@ fn extract_flashing_artifacts(
     let mut collect_system_artifacts = |system: &mut Option<Vec<Image>>| -> Result<()> {
         if let Some(system) = system {
             for image in system.iter_mut() {
-                artifacts.push(compute_path(&image.source())?);
+                match image {
+                    Image::ZBI { path: _, signed: _ }
+                    | Image::VBMeta(_)
+                    | Image::FVMFastboot(_) => artifacts.push(compute_path(&image.source())?),
+                    _ => continue,
+                }
             }
         }
         Ok(())
@@ -118,6 +124,50 @@ fn extract_flashing_artifacts(
     collect_system_artifacts(&mut product_bundle.system_a)?;
     collect_system_artifacts(&mut product_bundle.system_b)?;
     collect_system_artifacts(&mut product_bundle.system_r)?;
+    Ok(artifacts.iter().map(|x| x.to_string()).collect::<Vec<_>>())
+}
+
+fn extract_emu_artifacts(
+    product_bundle: ProductBundle,
+    cmd: GetArtifactsCommand,
+) -> Result<Vec<String>> {
+    let mut product_bundle = match product_bundle {
+        ProductBundle::V1(_) => ffx_bail!("Only v2 product bundles are supported"),
+        ProductBundle::V2(pb) => pb,
+    };
+
+    let compute_path = |path: &Utf8Path| -> Result<Utf8PathBuf> {
+        if cmd.relative_path {
+            path_relative_from(path, &cmd.product_bundle)
+        } else {
+            Ok(path.clone().into())
+        }
+    };
+
+    let mut artifacts = vec![Utf8PathBuf::from("product_bundle.json")];
+
+    // Collect the systems artifacts.
+    let mut collect_system_artifacts = |system: &mut Option<Vec<Image>>| -> Result<()> {
+        if let Some(system) = system {
+            for image in system.iter_mut() {
+                match image {
+                    Image::ZBI { path: _, signed: _ } | Image::QemuKernel(_) | Image::FVM(_) => {
+                        artifacts.push(compute_path(&image.source())?)
+                    }
+                    _ => continue,
+                }
+            }
+        }
+        Ok(())
+    };
+
+    collect_system_artifacts(&mut product_bundle.system_a)?;
+    collect_system_artifacts(&mut product_bundle.system_b)?;
+    collect_system_artifacts(&mut product_bundle.system_r)?;
+
+    if let Some(path) = product_bundle.virtual_devices_path {
+        artifacts.push(compute_path(&path)?);
+    }
     Ok(artifacts.iter().map(|x| x.to_string()).collect::<Vec<_>>())
 }
 
@@ -176,6 +226,9 @@ mod tests {
             system_a: Some(vec![
                 Image::ZBI { path: Utf8PathBuf::from("zbi/path"), signed: false },
                 Image::FVM(Utf8PathBuf::from("/tmp/product_bundle/system_a/fvm.blk")),
+                Image::FVMFastboot(Utf8PathBuf::from(
+                    "/tmp/product_bundle/system_a/fvm_fastboot.blk",
+                )),
                 Image::QemuKernel(Utf8PathBuf::from("qemu/path")),
             ]),
             system_b: None,
@@ -194,6 +247,79 @@ mod tests {
             String::from("product_bundle.json"),
             String::from("bootloader/path"),
             String::from("credential/path"),
+            String::from("zbi/path"),
+            String::from("/tmp/product_bundle/system_a/fvm_fastboot.blk"),
+        ];
+        assert_eq!(expected_artifacts, artifacts);
+    }
+
+    #[test]
+    fn test_get_emu_artifacts() {
+        let json = r#"
+            {
+                bootloader_partitions: [
+                    {
+                        type: "tpl",
+                        name: "firmware_tpl",
+                        image: "bootloader/path",
+                    }
+                ],
+                partitions: [
+                    {
+                        type: "ZBI",
+                        name: "zircon_a",
+                        slot: "A",
+                    },
+                    {
+                        type: "VBMeta",
+                        name: "vbmeta_b",
+                        slot: "B",
+                    },
+                    {
+                        type: "FVM",
+                        name: "fvm",
+                    },
+                    {
+                        type: "Fxfs",
+                        name: "fxfs",
+                    },
+                ],
+                hardware_revision: "hw",
+                unlock_credentials: [
+                    "credential/path",
+                ],
+            }
+        "#;
+        let mut cursor = std::io::Cursor::new(json);
+        let config: PartitionsConfig = PartitionsConfig::from_reader(&mut cursor).unwrap();
+
+        let pb = ProductBundle::V2(ProductBundleV2 {
+            product_name: "".to_string(),
+            product_version: "".to_string(),
+            partitions: config,
+            sdk_version: "".to_string(),
+            system_a: Some(vec![
+                Image::ZBI { path: Utf8PathBuf::from("zbi/path"), signed: false },
+                Image::FVM(Utf8PathBuf::from("/tmp/product_bundle/system_a/fvm.blk")),
+                Image::FVMFastboot(Utf8PathBuf::from(
+                    "/tmp/product_bundle/system_a/fvm_fastboot.blk",
+                )),
+                Image::QemuKernel(Utf8PathBuf::from("qemu/path")),
+            ]),
+            system_b: None,
+            system_r: None,
+            repositories: vec![],
+            update_package_hash: None,
+            virtual_devices_path: None,
+        });
+        let cmd = GetArtifactsCommand {
+            product_bundle: Utf8PathBuf::new(),
+            relative_path: false,
+            artifacts_group: Type::Flash,
+        };
+        let artifacts = extract_emu_artifacts(pb.clone(), cmd).unwrap();
+        let expected_artifacts = vec![
+            String::from("product_bundle.json"),
             String::from("zbi/path"),
             String::from("/tmp/product_bundle/system_a/fvm.blk"),
             String::from("qemu/path"),
