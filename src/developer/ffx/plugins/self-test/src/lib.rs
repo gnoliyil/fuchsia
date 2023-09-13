e418238ab76d71ce714b25f9de983c3feb32676f
Jacob Rutherford <jruthe@google.com>
2023-09-13 17:55:37 +0000

    [ffx][self-test] Remove debugger tests
    These tests are redundant with zxdb_e2e_tests and add unnecessary
    maintenance in ffx's tests.
    
    Change-Id: I7ff3463b75fa7e3fce761de1bdcd3dd4d1ab9536
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/915694
    Reviewed-by: Steven Grady <slgrady@google.com>
    Commit-Queue: Jacob Rutherford <jruthe@google.com>

--- src/developer/ffx/plugins/self-test/src/lib.rs
+++ src/developer/ffx/plugins/self-test/src/lib.rs
@@ -12,7 +12,6 @@ use std::time::Duration;
 mod component;
 mod config;
 mod daemon;
-mod debug;
 mod experiment;
 // mod log;
 mod target;
@@ -59,8 +58,6 @@ pub async fn selftest(cmd: SelftestCommand) -> Result<()> {
         // TODO(bbosak): re-enable once proactive-logging is disabled (fxb/125487)
         // log::include_log::test_log_run_normal,
         component::include_target::test_list,
-        debug::include_target::test_debug_run_crasher,
-        debug::include_target::test_debug_limbo,
         target::include_target::test_get_ssh_address_includes_port,
         target::include_target::test_target_show
     ];
