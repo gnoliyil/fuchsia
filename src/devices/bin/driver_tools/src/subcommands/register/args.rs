f7c702719aac3af11bc88b60bab533c65b9ac595
Gabe Schine <thatguy@google.com>
2023-09-14 15:45:45 +0000

    [cmxtinction][driver tools] Remove .cmx refs from help text
    Bug: b/300132803
    Change-Id: Ia0248c12b8754b83410c6f06196bda4649c3d855
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/916098
    Reviewed-by: Miguel Flores <miguelfrde@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Commit-Queue: Gabe Schine <thatguy@google.com>

--- src/devices/bin/driver_tools/src/subcommands/register/args.rs
+++ src/devices/bin/driver_tools/src/subcommands/register/args.rs
@@ -11,7 +11,7 @@ use argh::FromArgs;
     description = "Informs the driver manager that a new driver package is available. The driver manager will cache a copy of the driver",
     example = "To register a driver
 
-    $ driver register 'fuchsia-pkg://fuchsia.com/example_driver#meta/example_driver.cmx'",
+    $ driver register 'fuchsia-pkg://fuchsia.com/example_driver#meta/example_driver.cm'",
     error_code(1, "Failed to connect to the driver registrar service")
 )]
 pub struct RegisterCommand {
