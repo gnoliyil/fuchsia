331364d03969da5d3528fcf15c4891acd60d6cd7
John Wittrock <wittrock@google.com>
2023-09-19 23:49:51 +0000

    [ffx] json support for target repo list
    Adds `--machine json` support for `ffx target repository list`.
    
    Test: added unit tests
    Fixed: b/296916324
    Change-Id: Ica020034ccf61fa986250831b9280aa4f56f5d95
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/918894
    Reviewed-by: Erick Tryzelaar <etryzelaar@google.com>
    Commit-Queue: John Wittrock <wittrock@google.com>

--- src/developer/ffx/plugins/target/repository/list/src/lib.rs
+++ src/developer/ffx/plugins/target/repository/list/src/lib.rs
@@ -5,10 +5,10 @@
 use anyhow::{Context, Result};
 use async_trait::async_trait;
 use ffx_target_repository_list_args::ListCommand;
-use fho::{daemon_protocol, FfxMain, FfxTool, SimpleWriter};
+use fho::{daemon_protocol, FfxMain, FfxTool, MachineWriter, ToolIO};
 use fidl_fuchsia_developer_ffx::{RepositoryRegistryProxy, RepositoryStorageType};
 use prettytable::{cell, row, Table};
-use std::{collections::HashMap, io::Write};
+use std::collections::HashMap;
 
 #[derive(FfxTool)]
 pub struct ListTool {
@@ -18,22 +18,23 @@ pub struct ListTool {
     repos: RepositoryRegistryProxy,
 }
 
-fho::embedded_plugin!(ListTool);
+type RepositoryList = Vec<(String, Vec<String>)>;
+type Writer = MachineWriter<RepositoryList>;
 
+fho::embedded_plugin!(ListTool);
 #[async_trait(?Send)]
 impl FfxMain for ListTool {
-    type Writer = SimpleWriter;
-
+    type Writer = Writer;
     async fn main(self, writer: Self::Writer) -> fho::Result<()> {
         list_impl(self.cmd, self.repos, writer).await?;
         Ok(())
     }
 }
 
-async fn list_impl<W: Write>(
+async fn list_impl(
     _cmd: ListCommand,
     repos: RepositoryRegistryProxy,
-    mut writer: W,
+    mut writer: MachineWriter<RepositoryList>,
 ) -> Result<()> {
     let (client, server) = fidl::endpoints::create_endpoints();
     repos.list_registered_targets(server).context("communicating with daemon")?;
@@ -63,7 +64,12 @@ async fn list_impl<W: Write>(
             items.entry(repo).or_insert_with(Vec::new).push(target_identifier);
         }
     }
+
     if items.is_empty() {
+        if writer.is_machine() {
+            return Ok(writer.machine(&vec![])?);
+        }
+
         return Ok(());
     }
 
@@ -73,6 +79,10 @@ async fn list_impl<W: Write>(
 
     let mut items = items.into_iter().collect::<Vec<_>>();
     items.sort_by(|(x, _), (y, _)| x.cmp(&y));
+
+    if writer.is_machine() {
+        return Ok(writer.machine(&items)?);
+    }
 
     let mut table = Table::new();
     table.set_titles(row!("REPO", "TARGET"));
@@ -89,15 +99,37 @@ async fn list_impl<W: Write>(
 #[cfg(test)]
 mod test {
     use super::*;
+    use ffx_writer::Format;
+    use fho::macro_deps::ffx_writer::TestBuffers;
     use fidl_fuchsia_developer_ffx::{
         RepositoryRegistryRequest, RepositoryTarget, RepositoryTargetsIteratorRequest,
     };
     use fuchsia_async as fasync;
     use futures::StreamExt;
 
-    #[fasync::run_singlethreaded(test)]
-    async fn list() {
-        let repos = fho::testing::fake_proxy(move |req| {
+    async fn empty_repo_proxy() -> RepositoryRegistryProxy {
+        fho::testing::fake_proxy(move |req| {
+            fasync::Task::spawn(async move {
+                match req {
+                    RepositoryRegistryRequest::ListRegisteredTargets { iterator, .. } => {
+                        let mut iterator = iterator.into_stream().unwrap();
+                        while let Some(Ok(req)) = iterator.next().await {
+                            match req {
+                                RepositoryTargetsIteratorRequest::Next { responder } => {
+                                    responder.send(&[]).unwrap()
+                                }
+                            }
+                        }
+                    }
+                    other => panic!("Unexpected request: {:?}", other),
+                }
+            })
+            .detach();
+        })
+    }
+
+    async fn repo_proxy() -> RepositoryRegistryProxy {
+        fho::testing::fake_proxy(move |req| {
             fasync::Task::spawn(async move {
                 let mut sent = false;
                 match req {
@@ -145,9 +177,15 @@ mod test {
                 }
             })
             .detach();
-        });
-        let mut out = Vec::<u8>::new();
-        list_impl(ListCommand {}, repos, &mut out).await.unwrap();
+        })
+    }
+
+    #[fasync::run_singlethreaded(test)]
+    async fn list_table() {
+        let repos = repo_proxy().await;
+        let test_buffers = TestBuffers::default();
+        let writer = Writer::new_test(None, &test_buffers);
+        list_impl(ListCommand {}, repos, writer).await.unwrap();
 
         static EXPECT: &str = "\
              +-------+-------------------------+\n\
@@ -161,6 +199,39 @@ mod test {
              | smith | target2 (EPHEMERAL)     |\n\
              +-------+-------------------------+\n";
 
-        assert_eq!(EXPECT, &String::from_utf8_lossy(&out));
+        assert_eq!(EXPECT, test_buffers.into_stdout_str());
+    }
+
+    #[fasync::run_singlethreaded(test)]
+    async fn list_json() {
+        let repos = repo_proxy().await;
+        let test_buffers = TestBuffers::default();
+        let writer = Writer::new_test(Some(Format::Json), &test_buffers);
+        list_impl(ListCommand {}, repos, writer).await.unwrap();
+
+        static EXPECT: &str = "[[\"bob\",[\"target1\\n  alias: target1_alias1\\n  alias: target1_alias2\",\"target3\"]],[\"smith\",[\"target2 (EPHEMERAL)\"]]]\n";
+        assert_eq!(EXPECT, (test_buffers.into_stdout_str()));
+    }
+
+    #[fasync::run_singlethreaded(test)]
+    async fn list_empty() {
+        let repos = empty_repo_proxy().await;
+        let test_buffers = TestBuffers::default();
+        let writer = Writer::new_test(None, &test_buffers);
+        list_impl(ListCommand {}, repos, writer).await.unwrap();
+
+        static EXPECT: &str = "";
+        assert_eq!(EXPECT, (test_buffers.into_stdout_str()));
+    }
+
+    #[fasync::run_singlethreaded(test)]
+    async fn list_json_empty() {
+        let repos = empty_repo_proxy().await;
+        let test_buffers = TestBuffers::default();
+        let writer = Writer::new_test(Some(Format::Json), &test_buffers);
+        list_impl(ListCommand {}, repos, writer).await.unwrap();
+
+        static EXPECT: &str = "[]\n";
+        assert_eq!(EXPECT, (test_buffers.into_stdout_str()));
     }
 }
