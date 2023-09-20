ce2f8014c4068ca08ad3bf29fff7f0a32d22a0ef
Caroline Liu <carolineliu@google.com>
2023-09-20 22:16:13 +0000

    [element_launcher] create an element_launcher component which launches a configured main_element_url, if one exists
    This functionality should be moved to a standalone component, outside of
    tiles-session where it currently exists. Product sessions can eagerly
    start the first element using element_launcher, element_manager, and a
    window manager.
    
    Bug: 133197
    Test: Manually tested in tandem with tqr/912680 shows that emulator
    boots and loads first element.
    
    Change-Id: I69aa3dd7960bfef42518767f5a6edf6dc722095c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/916314
    Reviewed-by: Yegor Pomortsev <ypomortsev@google.com>
    Reviewed-by: Emircan Uysaler <emircan@google.com>
    Commit-Queue: Caroline Liu <carolineliu@google.com>

---
+++ src/session/bin/element_launcher/src/main.rs
@@ -0,0 +1,40 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {
+    element_launcher_config, fidl_fuchsia_element as element,
+    fuchsia_component::client::connect_to_protocol,
+    tracing::{info, warn},
+};
+
+#[fuchsia::main(logging = true)]
+async fn main() {
+    info!("element_launcher is starting.");
+    let config = element_launcher_config::Config::take_from_startup_handle();
+    if config.main_element_url.is_empty() {
+        warn!("element_launcher was started without a main_element_url; quitting.");
+        return;
+    }
+
+    let element_manager = connect_to_protocol::<element::ManagerMarker>()
+        .expect("failed to connect to fuchsia.element.Manager");
+    element_manager
+        .propose_element(
+            element::Spec {
+                component_url: Some(config.main_element_url),
+                annotations: Some(vec![element::Annotation {
+                    key: element::AnnotationKey {
+                        namespace: "element_launcher".to_string(),
+                        value: "name".to_string(),
+                    },
+                    value: element::AnnotationValue::Text("main".to_string()),
+                }]),
+                ..Default::default()
+            },
+            None,
+        )
+        .await
+        .expect("Failed to call ProposeElement.")
+        .expect("Failed to propose element.");
+}
