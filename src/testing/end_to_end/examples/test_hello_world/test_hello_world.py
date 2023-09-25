f1c0fabe7458ca4648e1f42adc38fe97c0de7611
Kevin Cho <chok@google.com>
2023-09-25 20:00:02 +0000

    [lacewing] Add hello world test in //s/t/e/examples.
    Change-Id: I9c316d291a723287f83215c609ab2d766ab89e2e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/920943
    Reviewed-by: Pavan Kumar Juturu <jpavankumar@google.com>
    Commit-Queue: Kevin Cho <chok@google.com>

---
+++ src/testing/end_to_end/examples/test_hello_world/test_hello_world.py
@@ -0,0 +1,25 @@
+# Copyright 2023 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+
+from fuchsia_base_test import fuchsia_base_test
+from mobly import test_runner
+
+_LOGGER: logging.Logger = logging.getLogger(__name__)
+
+
+class HelloWorldTest(fuchsia_base_test.FuchsiaBaseTest):
+
+    def setup_class(self):
+        """Initialize all DUT(s)"""
+        super().setup_class()
+
+    def test_hello_world(self):
+        for fuchsia_device in self.fuchsia_devices:
+            _LOGGER.info(f'{fuchsia_device.device_name} says hello!')
+
+
+if __name__ == '__main__':
+    test_runner.main()
