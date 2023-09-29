868b556e955a0dda7f62b820c78bbe943bcaf42a
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2023-09-29 17:32:45 +0000

    [diagnostics][hierarchy] Remove fn for soft transition
    Fixed: b/302138500
    Change-Id: I05620aca423dba63a266d624f662e0165be59b4e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/922192
    Reviewed-by: Adam Perry <adamperry@google.com>
    Commit-Queue: Miguel Flores <miguelfrde@google.com>

--- src/lib/diagnostics/hierarchy/rust/src/lib.rs
+++ src/lib/diagnostics/hierarchy/rust/src/lib.rs
@@ -312,17 +312,6 @@ property_type_getters_ref!(
     [StringList, string_list, [String]]
 );
 
-// TODO(fxbug.dev/302138500): remove once soft-migration is done.
-impl<Key> Property<Key> {
-    /// Returns the "uint" value or `None` if the property isn't of that type.
-    pub fn uint_ref(&self) -> Option<&u64> {
-        match self {
-            Property::Uint(_, value) => Some(value),
-            _ => None,
-        }
-    }
-}
-
 struct WorkStackEntry<'a, Key> {
     node: &'a DiagnosticsHierarchy<Key>,
     key: Vec<&'a str>,
