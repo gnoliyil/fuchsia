676bdab357d92d484422cf912b92fd968cbaf040
Sergey Ulanov <sergeyu@google.com>
2023-09-30 02:14:06 +0000

    [Starnix] Cleanup tests for extended_pstate.
    1. Added "ExtendedPstate.Syscall" to verify that FP/SIMD registers
       are preserved when calling  syscalls.
    2. Replaced "Fault.PreserveVectorRegisters" with
       "ExtendedPstate.Signals", which verifies that the registers are
       restored when returning from a signal handler. This functionality
       is not implemented correctly yet, so the test is failing.
    3. Renamed "ExtendedPstateInitialState.Basic" to
       "ExtendedPstate.InitialState".
    
    Bug: b/302340440
    Change-Id: I28f79fa9eb2c783c8f007895c237367b226ba264
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/924337
    Commit-Queue: Sergey Ulanov <sergeyu@google.com>
    Reviewed-by: James Robinson <jamesr@google.com>

---
+++ src/starnix/tests/syscalls/cpp/extended_pstate_test.cc
@@ -0,0 +1,214 @@
+// Copyright 2023 The Fuchsia Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/syscall.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <gtest/gtest.h>
+
+#include "src/lib/files/file.h"
+#include "src/lib/files/path.h"
+#include "src/starnix/tests/syscalls/cpp/test_helper.h"
+
+namespace {
+
+struct RegistersValue {
+  uint64_t x[2];
+
+  bool operator==(const RegistersValue& other) const {
+    return x[0] == other.x[0] && x[1] == other.x[1];
+  }
+};
+
+// Stores a 128-bit value in SIMD registers used for the test.
+void SetTestRegisters(const RegistersValue* value) {
+#if defined(__x86_64__)
+  asm volatile("movups (%0), %%xmm0\n" : : "r"(value));
+#elif defined(__aarch64__)
+  asm volatile("ldr q0, [%0]\n" : : "r"(value));
+#elif defined(__riscv)
+  // V and Q extensions are not enabled on RISC-V yet. For now this tests uses two D registers.
+  asm volatile(
+      "fld f0, 0(%0)\n"
+      "fld f1, 8(%0)\n"
+      :
+      : "r"(value));
+#else
+#error Add support for this architecture
+#endif
+}
+
+// Reads a 128-bit value from the SIMD registers that were set in SetTestRegisters().
+RegistersValue GetTestRegisters() {
+  RegistersValue value;
+
+#if defined(__x86_64__)
+  asm volatile("movups %%xmm0, %0\n" : : "m"(value));
+#elif defined(__aarch64__)
+  asm volatile("str q0, %0\n" : : "m"(value));
+#elif defined(__riscv)
+  asm volatile(
+      "fsd f0, 0 + %0\n"
+      "fsd f1, 8 + %0\n"
+      :
+      : "m"(value));
+#else
+#error Add support for this architecture
+#endif
+
+  return value;
+}
+
+RegistersValue GetTestRegistersFromUcontext(ucontext_t* ucontext) {
+  RegistersValue result;
+
+#if defined(__x86_64__)
+  memcpy(&result, reinterpret_cast<void*>(ucontext->uc_mcontext.fpregs->_xmm), sizeof(result));
+  // Issue store that will generate fault which will be fixed in SIGSEGV handler
+#elif defined(__aarch64__)
+  fpsimd_context* fp_context = reinterpret_cast<fpsimd_context*>(ucontext->uc_mcontext.__reserved);
+  EXPECT_EQ(fp_context->head.magic, static_cast<uint32_t>(FPSIMD_MAGIC));
+  memcpy(&result, fp_context->vregs, sizeof(result));
+#elif defined(__riscv)
+  memcpy(&result, reinterpret_cast<void*>(ucontext->uc_mcontext.__fpregs.__d.__f), sizeof(result));
+#else
+#error Add support for this architecture
+#endif
+
+  return result;
+}
+
+// FP/SIMD registers should be initialized to 0 for new processes.
+TEST(ExtendedPstate, InitialState) {
+  // When running in Starnix the child binary is mounted at this path in the test's namespace.
+  std::string child_path = "data/tests/extended_pstate_initial_state_child";
+  if (!files::IsFile(child_path)) {
+    // When running on host the child binary is next to the test binary.
+    char self_path[PATH_MAX];
+    realpath("/proc/self/exe", self_path);
+
+    child_path =
+        files::JoinPath(files::GetDirectoryName(self_path), "extended_pstate_initial_state_child");
+  }
+  ASSERT_TRUE(files::IsFile(child_path)) << child_path;
+  test_helper::ForkHelper helper;
+  helper.RunInForkedProcess([&child_path] {
+    // Set some registers. execve() should reset them to 0.
+    const auto kTestData = RegistersValue{{0x0102030405060708, 0x090a0b0c0d0e0f10}};
+    SetTestRegisters(&kTestData);
+
+    char* argv[] = {nullptr};
+    char* envp[] = {nullptr};
+    ASSERT_EQ(execve(child_path.c_str(), argv, envp), 0)
+        << "execve error: " << errno << " (" << strerror(errno) << ")";
+  });
+}
+
+// Verify that FP/SIMD registers are preserved by syscalls.
+TEST(ExtendedPstate, Syscall) {
+  const auto kTestRegisters = RegistersValue{{0x0102030405060708, 0x090a0b0c0d0e0f10}};
+
+  SetTestRegisters(&kTestRegisters);
+
+  // Make several syscalls. Kernel uses floating point to generate `/proc/uptime` content, which
+  // may affect the registers being tested.
+  int fd = open("/proc/uptime", O_RDONLY);
+  EXPECT_GT(fd, 0);
+  char c;
+  EXPECT_EQ(read(fd, &c, 1), 1);
+  EXPECT_EQ(close(fd), 0);
+
+  EXPECT_EQ(GetTestRegisters(), kTestRegisters);
+}
+
+struct SignalHandlerData {
+  void* sigsegv_target;
+  bool received_sigsegv = false;
+  RegistersValue sigsegv_regs;
+
+  RegistersValue sigusr1_regs;
+};
+
+SignalHandlerData signal_data;
+
+// FP/SIMD registers are expected to be restored when returning form signal handlers
+TEST(ExtendedPstate, Signals) {
+  test_helper::ForkHelper helper;
+  helper.RunInForkedProcess([] {
+    size_t page_size = SAFE_SYSCALL(sysconf(_SC_PAGE_SIZE));
+    void* target = mmap(nullptr, page_size, PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    ASSERT_NE(target, MAP_FAILED);
+    signal_data.sigsegv_target = target;
+
+    // Register SIGUSR1 handler.
+    struct sigaction sigusr1_action = {};
+    sigusr1_action.sa_sigaction = [](int sig, siginfo_t* info, void* ucontext) {
+      if (sig != SIGUSR1) {
+        _exit(1);
+      }
+      signal_data.sigusr1_regs =
+          GetTestRegistersFromUcontext(reinterpret_cast<ucontext_t*>(ucontext));
+
+      // Reset registers content.
+      RegistersValue zero{{0, 0}};
+      SetTestRegisters(&zero);
+    };
+    sigusr1_action.sa_flags = SA_SIGINFO;
+    SAFE_SYSCALL(sigaction(SIGUSR1, &sigusr1_action, nullptr));
+
+    // Register SIGSEGV handler.
+    struct sigaction sigserv_action = {};
+    signal_data.received_sigsegv = false;
+    sigserv_action.sa_sigaction = [](int sig, siginfo_t* info, void* ucontext) {
+      if (sig != SIGSEGV || info->si_addr != signal_data.sigsegv_target) {
+        _exit(1);
+      }
+      signal_data.received_sigsegv = true;
+      signal_data.sigsegv_regs =
+          GetTestRegistersFromUcontext(reinterpret_cast<ucontext_t*>(ucontext));
+
+      // Set registers to a value different from what it was outside of the signal handler.
+      const auto kNestedRegs = RegistersValue{{0x090a0b0c0d0e0f10, 0x0102030405060708}};
+      SetTestRegisters(&kNestedRegs);
+
+      // Raise another signal.
+      raise(SIGUSR1);
+
+      // Nested signal handled should preserve all registers.
+      EXPECT_EQ(GetTestRegisters(), kNestedRegs);
+
+      // Nested signal handler should receive values at the time it was invoked.
+      EXPECT_EQ(signal_data.sigusr1_regs, kNestedRegs);
+
+      // TODO: mprotect is not listed in signal-safety(7), should issue raw syscall
+      mprotect(info->si_addr, 4096, PROT_READ | PROT_WRITE);
+    };
+    sigserv_action.sa_flags = SA_SIGINFO;
+    SAFE_SYSCALL(sigaction(SIGSEGV, &sigserv_action, nullptr));
+
+    const auto kTestRegsValue = RegistersValue{{0x0102030405060708, 0x090a0b0c0d0e0f10}};
+    SetTestRegisters(&kTestRegsValue);
+
+    // Issue a store that will generate fault which will be fixed in SIGSEGV handler.
+    asm volatile("" ::: "memory");
+    *static_cast<char*>(target) = 1;
+    asm volatile("" ::: "memory");
+    ASSERT_TRUE(signal_data.received_sigsegv);
+
+    // Check that the SIMD registers were preserved.
+    EXPECT_EQ(GetTestRegisters(), kTestRegsValue);
+
+    // Validate the registers value passed to the SIGSEGV handler in ucontext.
+    EXPECT_EQ(signal_data.sigsegv_regs, kTestRegsValue);
+  });
+
+  ASSERT_TRUE(helper.WaitForChildren());
+}
+
+}  // namespace
