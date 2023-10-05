01e21d6ccdc2d0a7ef83675bd23b492f1ba65607
Casey Dahlin <sadmac@google.com>
2023-10-05 22:59:51 +0000

    [overnet] Delete examples, hoist, and host tools
    All unused stuff, except perhaps for the examples, which used to be
    exercised as part of our testing strategy. They work the handle types
    specifically, but our fidl tests do that, and the connections end to
    end, but ffx owns the production version of that and exercises a more
    worthwhile version of it.
    
    Bug: b/287694609
    Change-Id: Iadd8aca0f77735689d0192931f2f46b32d467715
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/927133
    Reviewed-by: Ian McKellar <ianloic@google.com>
    API-Review: Ian McKellar <ianloic@google.com>
    Fuchsia-Auto-Submit: Casey Dahlin <sadmac@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Steven Grady <slgrady@google.com>

--- src/connectivity/overnet/tools/onet/src/main.rs
+++
@@ -1,8 +0,0 @@
-// Copyright 2020 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#[fuchsia::main]
-async fn main() -> Result<(), anyhow::Error> {
-    onet_tool::run_onet(&hoist::Hoist::new(None)?, argh::from_env()).await
-}
