a330a7aed966ec7eb4c14b59e66c62c4f04d090a
Colin Nelson <colnnelson@google.com>
2023-10-09 21:19:24 +0000

    [ffx][tests]: integration tests for fastboot
    Adds test for command `target bootloader info`
    
    Adds further tests for `ffx target flash`. Including re-flashing once in
    product mode.
    
    Change-Id: Ic18a853e89a951abe77a5514ee91f15a2e3d911a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/925716
    Reviewed-by: Megan Batty <mgnb@google.com>
    Commit-Queue: Colin Nelson <colnnelson@google.com>

--- src/developer/ffx/tests/target/src/tests.rs
+++ src/developer/ffx/tests/target/src/tests.rs
@@ -2,8 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use std::time::Duration;
-
+use ffx_isolate::Isolate;
 use ffx_testing::{emulator_fixture, Emu, TestContext};
 use fixture::fixture;
 use fuchsia_async as _;
@@ -11,6 +10,7 @@ use futures::{
     io::{AsyncBufReadExt, AsyncReadExt, BufReader},
     AsyncRead, AsyncWrite, AsyncWriteExt, Stream, StreamExt,
 };
+use std::time::Duration;
 
 /// Test `ffx target flash` by bringing up an emulator in fastboot.
 ///
@@ -58,23 +58,82 @@ async fn test_target_flash_gigaboot(ctx: TestContext) {
     emu.check_is_running().expect("Emulator exited unexpectedly");
 
     // Retry ffx target show as it may take the device up to 30 seconds to initialize SSH.
-    let mut times = 2;
-    while times >= 0 {
-        let out = isolate
-            .ffx(&["--target", emu.nodename(), "target", "show"])
-            .await
-            .expect("target show");
+    wait_for_target!(isolate, emu);
+    emu.check_is_running().expect("Emulator exited unexpectedly");
+}
 
-        if times > 0 && !out.status.success() {
-            times -= 1;
-            continue;
-        }
+/// Test `ffx target flash` by bringing up an emulator in fastboot.
+///
+/// The fastboot implementation used by this emulator is Gigaboot (//src/firmware/gigaboot).
+///
+/// This also re-flashes the image once it is in product mode
+#[fixture(emulator_fixture)]
+#[fuchsia::test]
+// TODO(fxbug.dev/130252): test skipped when kernel won't fit for x86 UEFI
+#[cfg_attr(feature = "big_zircon_kernel", ignore)]
+async fn test_target_flash_from_product(ctx: TestContext) {
+    let isolate = ctx.isolate();
+    isolate.start_daemon().await.unwrap();
 
-        assert!(out.status.success(), "status is unexpected: {:?}", out);
-        assert!(out.stdout.contains("Product:"), "stdout is unexpected: {:?}", out);
-        assert!(out.stderr.lines().count() == 0, "stderr is unexpected: {:?}", out);
-        break;
+    let mut emu = Emu::start(&ctx);
+
+    {
+        let serial = emu.serial().await;
+        let (mut serial_lines, mut serial_writer) = serial_lines(serial).await;
+
+        // On initial boot, press `f` to enter fastboot.
+        enter_fastboot(&mut serial_lines, &mut serial_writer).await;
     }
+
+    std::thread::sleep(Duration::from_secs(5));
+
+    emu.check_is_running().expect("Emulator exited unexpectedly");
+
+    let product_bundle_json = Emu::product_bundle_dir();
+
+    // Flash in fastboot, then verify that target boots to product.
+    let output = isolate
+        .ffx(&[
+            "--target",
+            emu.nodename(),
+            "target",
+            "flash",
+            "-b",
+            product_bundle_json.to_str().unwrap(),
+        ])
+        .await
+        .expect("flash target");
+    assert!(output.status.success(), "Failed to run command: {}\n{}", output.stdout, output.stderr);
+
+    std::thread::sleep(Duration::from_secs(4));
+
+    emu.check_is_running().expect("Emulator exited unexpectedly");
+
+    // Retry ffx target show as it may take the device up to 30 seconds to initialize SSH.
+    wait_for_target!(isolate, emu);
+    emu.check_is_running().expect("Emulator exited unexpectedly");
+
+    // At this point the target is in Product mode... flash again and verify
+
+    let output = isolate
+        .ffx(&[
+            "--target",
+            emu.nodename(),
+            "target",
+            "flash",
+            "-b",
+            product_bundle_json.to_str().unwrap(),
+        ])
+        .await
+        .expect("flash target");
+    assert!(output.status.success(), "Failed to run command: {}\n{}", output.stdout, output.stderr);
+
+    std::thread::sleep(Duration::from_secs(4));
+
+    emu.check_is_running().expect("Emulator exited unexpectedly");
+
+    // Retry ffx target show as it may take the device up to 30 seconds to initialize SSH.
+    wait_for_target!(isolate, emu);
     emu.check_is_running().expect("Emulator exited unexpectedly");
 }
 
@@ -120,6 +179,92 @@ async fn test_target_reboot_to_bootloader_gigaboot(ctx: TestContext) {
     emu.check_is_running().expect("Emulator exited unexpectedly");
 }
 
+#[fixture(emulator_fixture)]
+#[fuchsia::test]
+// TODO(fxbug.dev/130252): test skipped when kernel won't fit for x86 UEFI
+#[cfg_attr(feature = "big_zircon_kernel", ignore)]
+async fn test_target_bootloader_info(ctx: TestContext) {
+    let isolate = ctx.isolate();
+    isolate.start_daemon().await.unwrap();
+
+    let mut emu = Emu::start(&ctx);
+
+    {
+        let serial = emu.serial().await;
+        let (mut serial_lines, mut serial_writer) = serial_lines(serial).await;
+
+        // On initial boot, press `f` to enter fastboot.
+        enter_fastboot(&mut serial_lines, &mut serial_writer).await;
+    }
+
+    std::thread::sleep(Duration::from_secs(5));
+
+    let out = isolate
+        .ffx(&["--target", emu.nodename(), "target", "bootloader", "info"])
+        .await
+        .expect("get bootloader info");
+
+    assert!(out.status.success(), "status is unexpected: {:?}", out.status);
+    assert!(out.stdout.contains("\nversion: 0.4\n"), "stdout is unexpected: {:?}", out.stdout);
+    assert!(out.stderr.lines().count() == 0, "stderr is unexpected: {:?}", out.stdout);
+    emu.check_is_running().expect("Emulator exited unexpectedly");
+}
+
+#[fixture(emulator_fixture)]
+#[fuchsia::test]
+// TODO(fxbug.dev/130252): test skipped when kernel won't fit for x86 UEFI
+#[cfg_attr(feature = "big_zircon_kernel", ignore)]
+async fn test_target_bootloader_info_from_product(ctx: TestContext) {
+    let isolate = ctx.isolate();
+    isolate.start_daemon().await.unwrap();
+
+    let mut emu = Emu::start(&ctx);
+
+    {
+        let serial = emu.serial().await;
+        let (mut serial_lines, mut serial_writer) = serial_lines(serial).await;
+
+        // On initial boot, press `f` to enter fastboot.
+        enter_fastboot(&mut serial_lines, &mut serial_writer).await;
+    }
+
+    // Give emulator time to get to fastboot
+    std::thread::sleep(Duration::from_secs(4));
+
+    emu.check_is_running().expect("Emulator exited unexpectedly");
+
+    let product_bundle_json = Emu::product_bundle_dir();
+
+    // Flash in fastboot, then verify that target boots to product.
+    let output = isolate
+        .ffx(&[
+            "--target",
+            emu.nodename(),
+            "target",
+            "flash",
+            "-b",
+            product_bundle_json.to_str().unwrap(),
+        ])
+        .await
+        .expect("flash target");
+    assert!(output.status.success(), "Failed to run command: {}\n{}", output.stdout, output.stderr);
+
+    std::thread::sleep(Duration::from_secs(4));
+
+    emu.check_is_running().expect("Emulator exited unexpectedly");
+
+    // ffx waits for the product to re-enter fastboot before returning.
+    let out = isolate
+        .ffx(&["--target", emu.nodename(), "target", "bootloader", "info"])
+        .await
+        .expect("target bootloader info");
+
+    assert!(out.status.success(), "status is unexpected: {:?}", out.status);
+    assert!(out.stdout.contains("\nversion: 0.4"), "stdout is unexpected: {:?}", out.stdout);
+    assert!(out.stderr.lines().count() == 0, "stderr is unexpected: {:?}", out.stderr);
+    emu.check_is_running().expect("Emulator exited unexpectedly");
+}
+
 async fn serial_lines(
     serial: impl AsyncRead + AsyncWrite,
 ) -> (impl Stream<Item = std::io::Result<String>>, impl AsyncWrite) {
@@ -140,4 +285,45 @@ async fn enter_fastboot(
             break;
         }
     }
+}
+
+async fn wait_for_target(isolate: &Isolate, emu: &Emu, retries: i32) -> anyhow::Result<()> {
+    let mut times = retries;
+    while times >= 0 {
+        let out = isolate
+            .ffx(&["--target", emu.nodename(), "target", "show"])
+            .await
+            .expect("target show");
+
+        if times > 0 && !out.status.success() {
+            times -= 1;
+            continue;
+        }
+
+        if !out.status.success() {
+            anyhow::bail!("status is unexpected: {:?}", out)
+        }
+        if !out.stdout.contains("Product:") {
+            anyhow::bail!("stdout is unexpected: {:?}", out)
+        }
+        if !out.stderr.lines().count() == 0 {
+            anyhow::bail!("stderr is unexpected: {:?}", out)
+        }
+
+        return Ok(());
+    }
+    anyhow::bail!("Could not find target")
+}
+
+#[macro_export]
+/// Waits for an emulator to show up in the isolate's daemon by invoking
+/// ffx target show and looking for a successful command.
+macro_rules! wait_for_target {
+    ($isolate: ident, $emu: ident, $retries: expr) => {
+        wait_for_target(&$isolate, &$emu, $retries).await.unwrap();
+    };
+    ($isolate: ident, $emu: ident) => {
+        // Assume a reasonable default of 2 retries
+        wait_for_target!($isolate, $emu, 2)
+    };
 }
