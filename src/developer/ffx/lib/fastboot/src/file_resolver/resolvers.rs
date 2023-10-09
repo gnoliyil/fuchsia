8b6d29ca058ec194cc1960ce12b345b29c15cecf
Colin Nelson <colnnelson@google.com>
2023-10-09 22:27:41 +0000

    [ffx][fastboot]: Add ZipArchiveResolver
    Adds FileResolver, ZipArchiveResolver which resolves files from a zip
    archive. Note that this does not have any dependencies on Flash
    Manifests.
    
    Change-Id: I24b45039e774fda5948a9a02deba759e9ac0bb47
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/928034
    Fuchsia-Auto-Submit: Colin Nelson <colnnelson@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Megan Batty <mgnb@google.com>

--- src/developer/ffx/lib/fastboot/src/file_resolver/resolvers.rs
+++ src/developer/ffx/lib/fastboot/src/file_resolver/resolvers.rs
@@ -89,6 +89,59 @@ impl FileResolver for Resolver {
     }
 }
 
+#[derive(Debug)]
+pub struct ZipArchiveResolver {
+    temp_dir: TempDir,
+    archive: ZipArchive<File>,
+}
+
+impl ZipArchiveResolver {
+    pub fn new<W: Write>(_writer: &mut W, path: PathBuf) -> Result<Self> {
+        let temp_dir = tempdir()?;
+        let file = File::open(path.clone())
+            .map_err(|e| ffx_error!("Could not open archive file at {}. {}", path.display(), e))?;
+        let archive =
+            ZipArchive::new(file).map_err(|e| ffx_error!("Could not read archive: {}", e))?;
+
+        Ok(Self { temp_dir, archive })
+    }
+}
+
+#[async_trait(?Send)]
+impl FileResolver for ZipArchiveResolver {
+    async fn get_file<W: Write>(&mut self, writer: &mut W, file: &str) -> Result<String> {
+        let mut file = self
+            .archive
+            .by_name(file)
+            .map_err(|_| anyhow!("File not found in archive: {}", file))?;
+
+        let mut outpath = PathBuf::new();
+        outpath.push(self.temp_dir.path());
+        outpath.push(file.sanitized_name());
+        if let Some(p) = outpath.parent() {
+            if !p.exists() {
+                create_dir_all(&p)?;
+            }
+        }
+        let time = Utc::now();
+        write!(
+            writer,
+            "Extracting {} to {}... ",
+            file.sanitized_name().file_name().expect("has a file name").to_string_lossy(),
+            self.temp_dir.path().display()
+        )?;
+        if file.size() > (1 << 24) {
+            write!(writer, "large file, please wait... ")?;
+        }
+        writer.flush()?;
+        let mut outfile = File::create(&outpath)?;
+        copy(&mut file, &mut outfile)?;
+        let duration = Utc::now().signed_duration_since(time);
+        done_time(writer, duration)?;
+        Ok(outpath.to_str().ok_or(anyhow!("invalid temp file name"))?.to_owned())
+    }
+}
+
 pub struct ArchiveResolver {
     temp_dir: TempDir,
     manifest_path: PathBuf,
@@ -256,5 +309,86 @@ impl FileResolver for TarResolver {
             // Should not get here, the parent of the manifest file SHOULD AT LEAST be the temp dir.
             bail!("Could not get file to upload");
         }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// tests
+
+#[cfg(test)]
+mod test {
+    // use tempfile::NamedTempFile;
+
+    use super::*;
+    use std::{io::Read, str::FromStr};
+    use zip::write::{FileOptions, ZipWriter};
+    use zip::CompressionMethod;
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // ZipArchiveResolver
+
+    #[test]
+    fn zip_archive_resolver_new_errors() -> Result<()> {
+        let non_existant_path = PathBuf::from_str("./not-exists.zip")?;
+        let mut w = vec![];
+        assert!(ZipArchiveResolver::new(&mut w, non_existant_path).is_err());
+        Ok(())
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn zip_archive_resolver_get_file() -> Result<()> {
+        // Make a temporary zip file
+        let tmpdir = tempdir()?;
+
+        let mut pbuff = PathBuf::new();
+        pbuff.push(tmpdir.path());
+        pbuff.push("test_zip.zip");
+
+        let file = File::create(pbuff.as_path())?;
+
+        // We use a buffer here, though you'd normally use a `File`
+        let mut zip = ZipWriter::new(file);
+        let options = FileOptions::default().compression_method(CompressionMethod::Stored);
+
+        zip.start_file("hello_world.txt", options)?;
+        let _ = zip.write(b"Hello, World!")?;
+        zip.start_file("foo/hello_world.txt", options)?;
+        let _ = zip.write(b"Hello, nested World!")?;
+
+        zip.flush()?;
+        zip.finish()?;
+
+        let mut w = vec![];
+        let mut resolver = ZipArchiveResolver::new(&mut w, pbuff)?;
+
+        // Test standard file
+        {
+            let file_path = resolver.get_file(&mut w, "hello_world.txt").await?;
+            let mut hello_file = File::open(file_path)?;
+            let mut hello_buf = vec![];
+            hello_file.read_to_end(&mut hello_buf)?;
+            assert_eq!(hello_buf, b"Hello, World!");
+        }
+
+        // Test nested file
+        {
+            let file_path = resolver.get_file(&mut w, "foo/hello_world.txt").await?;
+            let mut hello_file = File::open(file_path)?;
+            let mut hello_buf = vec![];
+            hello_file.read_to_end(&mut hello_buf)?;
+            assert_eq!(hello_buf, b"Hello, nested World!");
+        }
+
+        // Test standard file with leading slash
+        {
+            assert!(resolver.get_file(&mut w, "/hello_world.txt").await.is_err());
+        }
+
+        // Test non-existent file
+        {
+            assert!(resolver.get_file(&mut w, "this-shouldnt-exist.txt").await.is_err());
+        }
+
+        Ok(())
     }
 }
