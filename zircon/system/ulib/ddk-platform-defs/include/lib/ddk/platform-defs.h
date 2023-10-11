7eb2996d19f53ad905ccd5884d68428227d5ec3e
Puneetha Ramachandra <puneetha@google.com>
2023-10-11 22:40:43 +0000

    [board][violet] Add platform ID
    Change-Id: I2fe83d059a4d187226159630f58e3adaadb0eecc
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/930300
    Reviewed-by: Andres Oportus <andresoportus@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Puneetha Ramachandra <puneetha@google.com>

--- zircon/system/ulib/ddk-platform-defs/include/lib/ddk/platform-defs.h
+++ zircon/system/ulib/ddk-platform-defs/include/lib/ddk/platform-defs.h
@@ -86,6 +86,7 @@
 #define PDEV_PID_AV400              0x0F
 #define PDEV_PID_PINECREST          0x10
 #define PDEV_PID_CLOVER             0x11
+#define PDEV_PID_VIOLET             0x12
 
 #define PDEV_DID_GAUSS_AUDIO_IN     0x01
 #define PDEV_DID_GAUSS_AUDIO_OUT    0x02
