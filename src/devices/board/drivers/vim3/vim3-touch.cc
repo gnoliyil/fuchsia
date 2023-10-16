ae216835b025e418422babd1f040ec96da1d171d
Suraj Malhotra <surajmalhotra@google.com>
2023-10-16 23:51:12 +0000

    [vim3][touch] Add metadata via pdev node
    We don't support adding metadata via composite node specs in DFv2. An
    upcoming change will remove that field altogether.
    
    Change-Id: Ife0f75a79e4cad90a507aeb5c621d1683abe0034
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/931423
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Suraj Malhotra <surajmalhotra@google.com>
    Reviewed-by: Nick Eaton <nickeaton@google.com>

--- src/devices/board/drivers/vim3/vim3-touch.cc
+++ src/devices/board/drivers/vim3/vim3-touch.cc
@@ -12,6 +12,8 @@
 #include <lib/ddk/driver.h>
 #include <lib/ddk/metadata.h>
 #include <lib/ddk/platform-defs.h>
+#include <lib/driver/component/cpp/composite_node_spec.h>
+#include <lib/driver/component/cpp/node_add_args.h>
 #include <lib/focaltech/focaltech.h>
 
 #include <vector>
@@ -28,61 +30,87 @@
 #include "src/devices/board/drivers/vim3/vim3-gpios.h"
 #include "src/devices/board/drivers/vim3/vim3.h"
 
+namespace fdf {
+using namespace fuchsia_driver_framework;
+}  // namespace fdf
+   //
 namespace vim3 {
 
+namespace fpbus = fuchsia_hardware_platform_bus;
+
 zx_status_t Vim3::TouchInit() {
-  const ddk::BindRule kI2cRules[] = {
-      ddk::MakeAcceptBindRule(bind_fuchsia::FIDL_PROTOCOL,
+  const std::vector<fdf::BindRule> kI2cRules{
+      fdf::MakeAcceptBindRule(bind_fuchsia::FIDL_PROTOCOL,
                               bind_fuchsia_i2c::BIND_FIDL_PROTOCOL_DEVICE),
-      ddk::MakeAcceptBindRule(bind_fuchsia::I2C_BUS_ID, bind_fuchsia_i2c::BIND_I2C_BUS_ID_I2C_3),
-      ddk::MakeAcceptBindRule(bind_fuchsia::I2C_ADDRESS,
+      fdf::MakeAcceptBindRule(bind_fuchsia::I2C_BUS_ID, bind_fuchsia_i2c::BIND_I2C_BUS_ID_I2C_3),
+      fdf::MakeAcceptBindRule(bind_fuchsia::I2C_ADDRESS,
                               bind_fuchsia_focaltech_platform::BIND_I2C_ADDRESS_TOUCH),
   };
 
-  const device_bind_prop_t kI2cProperties[] = {
-      ddk::MakeProperty(bind_fuchsia::FIDL_PROTOCOL, bind_fuchsia_i2c::BIND_FIDL_PROTOCOL_DEVICE),
-      ddk::MakeProperty(bind_fuchsia::I2C_ADDRESS,
+  const std::vector<fdf::NodeProperty> kI2cProperties{
+      fdf::MakeProperty(bind_fuchsia::FIDL_PROTOCOL, bind_fuchsia_i2c::BIND_FIDL_PROTOCOL_DEVICE),
+      fdf::MakeProperty(bind_fuchsia::I2C_ADDRESS,
                         bind_fuchsia_focaltech_platform::BIND_I2C_ADDRESS_TOUCH),
   };
 
-  const ddk::BindRule kInterruptRules[] = {
-      ddk::MakeAcceptBindRule(bind_fuchsia::PROTOCOL, bind_fuchsia_gpio::BIND_PROTOCOL_DEVICE),
-      ddk::MakeAcceptBindRule(bind_fuchsia::GPIO_PIN,
+  const std::vector<fdf::BindRule> kInterruptRules{
+      fdf::MakeAcceptBindRule(bind_fuchsia::PROTOCOL, bind_fuchsia_gpio::BIND_PROTOCOL_DEVICE),
+      fdf::MakeAcceptBindRule(bind_fuchsia::GPIO_PIN,
                               static_cast<uint32_t>(VIM3_TOUCH_PANEL_INTERRUPT)),
   };
 
-  const device_bind_prop_t kInterruptProperties[] = {
-      ddk::MakeProperty(bind_fuchsia::PROTOCOL, bind_fuchsia_gpio::BIND_PROTOCOL_DEVICE),
-      ddk::MakeProperty(bind_fuchsia_gpio::FUNCTION, bind_fuchsia_gpio::FUNCTION_TOUCH_INTERRUPT)};
+  const std::vector<fdf::NodeProperty> kInterruptProperties{
+      fdf::MakeProperty(bind_fuchsia::PROTOCOL, bind_fuchsia_gpio::BIND_PROTOCOL_DEVICE),
+      fdf::MakeProperty(bind_fuchsia_gpio::FUNCTION, bind_fuchsia_gpio::FUNCTION_TOUCH_INTERRUPT)};
 
-  const ddk::BindRule kResetRules[] = {
-      ddk::MakeAcceptBindRule(bind_fuchsia::PROTOCOL, bind_fuchsia_gpio::BIND_PROTOCOL_DEVICE),
-      ddk::MakeAcceptBindRule(bind_fuchsia::GPIO_PIN,
+  const std::vector<fdf::BindRule> kResetRules{
+      fdf::MakeAcceptBindRule(bind_fuchsia::PROTOCOL, bind_fuchsia_gpio::BIND_PROTOCOL_DEVICE),
+      fdf::MakeAcceptBindRule(bind_fuchsia::GPIO_PIN,
                               static_cast<uint32_t>(VIM3_TOUCH_PANEL_RESET)),
   };
 
-  const device_bind_prop_t kResetProperties[] = {
-      ddk::MakeProperty(bind_fuchsia::PROTOCOL, bind_fuchsia_gpio::BIND_PROTOCOL_DEVICE),
-      ddk::MakeProperty(bind_fuchsia_gpio::FUNCTION, bind_fuchsia_gpio::FUNCTION_TOUCH_RESET),
+  const std::vector<fdf::NodeProperty> kResetProperties{
+      fdf::MakeProperty(bind_fuchsia::PROTOCOL, bind_fuchsia_gpio::BIND_PROTOCOL_DEVICE),
+      fdf::MakeProperty(bind_fuchsia_gpio::FUNCTION, bind_fuchsia_gpio::FUNCTION_TOUCH_RESET),
   };
 
-  static const FocaltechMetadata device_info = {
+  static const FocaltechMetadata device_info{
       .device_id = FOCALTECH_DEVICE_FT5336,
       .needs_firmware = false,
   };
 
-  static const device_metadata_t ft5336_khadas_ts050_touch_metadata[] = {
-      {.type = DEVICE_METADATA_PRIVATE, .data = &device_info, .length = sizeof(device_info)},
+  fpbus::Node dev;
+  dev.name() = "ft5336_touch";
+  dev.vid() = PDEV_VID_GENERIC;
+  dev.pid() = PDEV_VID_GENERIC;
+  dev.did() = PDEV_DID_FOCALTOUCH;
+  dev.metadata() = std::vector<fpbus::Metadata>{
+      {{
+          .type = DEVICE_METADATA_PRIVATE,
+          .data = std::vector<uint8_t>(
+              reinterpret_cast<const uint8_t*>(&device_info),
+              reinterpret_cast<const uint8_t*>(&device_info) + sizeof(device_info)),
+      }},
   };
+  auto parents = std::vector{
+      fdf::ParentSpec{{kI2cRules, kI2cProperties}},
+      fdf::ParentSpec{{kInterruptRules, kInterruptProperties}},
+      fdf::ParentSpec{{kResetRules, kResetProperties}},
+  };
+  auto composite_node_spec = fdf::CompositeNodeSpec{{.name = "ft5336_touch", .parents = parents}};
 
-  auto status = DdkAddCompositeNodeSpec("ft5336_touch",
-                                        ddk::CompositeNodeSpec(kI2cRules, kI2cProperties)
-                                            .AddParentSpec(kInterruptRules, kInterruptProperties)
-                                            .AddParentSpec(kResetRules, kResetProperties)
-                                            .set_metadata(ft5336_khadas_ts050_touch_metadata));
-  if (status != ZX_OK) {
-    zxlogf(ERROR, "ft5336: DdkAddCompositeNodeSpec failed: %s", zx_status_get_string(status));
-    return status;
+  fidl::Arena<> fidl_arena;
+  fdf::Arena arena('FOCL');
+  fdf::WireUnownedResult result = pbus_.buffer(arena)->AddCompositeNodeSpec(
+      fidl::ToWire(fidl_arena, dev), fidl::ToWire(fidl_arena, composite_node_spec));
+  if (!result.ok()) {
+    zxlogf(ERROR, "Failed to send AddCompositeNodeSpec request: %s", result.status_string());
+    return result.status();
+  }
+  if (result->is_error()) {
+    zxlogf(ERROR, "Failed to add composite node spec: %s",
+           zx_status_get_string(result->error_value()));
+    return result->error_value();
   }
   return ZX_OK;
 }
