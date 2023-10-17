f92f97b5b56e5be2414c01382eb120f3ad1eb6bd
Sergey Ulanov <sergeyu@google.com>
2023-10-17 16:47:21 +0000

    [Starnix] Use only exit code to interpret LTP test result
    Previously LTP test runner was trying to parse test output and also use
    exit code to interpret test result. As results the tests were not
    handled consistently with Android in some cases. Specifically, partially
    skipped tests should be considered passing, but they were treated as
    skipped.
    This change simplifies result handling logic to only look at the exit
    code.
    
    Bug: b/287119813, b/287114167
    Change-Id: I26ba70abf22e4f66ad8e71fc393f7d4e6a0c6b6c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/931528
    Reviewed-by: Kevin Lindkvist <lindkvist@google.com>
    Commit-Queue: Sergey Ulanov <sergeyu@google.com>
    Reviewed-by: Vickie Cheng <vickiecheng@google.com>
    Reviewed-by: Henry-Michel Cantave <hmcantave@google.com>

--- src/sys/test_runners/starnix/src/ltp.rs
+++ src/sys/test_runners/starnix/src/ltp.rs
@@ -9,100 +9,8 @@ use fidl_fuchsia_component_runner as frunner;
 use fidl_fuchsia_data as fdata;
 use fidl_fuchsia_io as fio;
 use fidl_fuchsia_test as ftest;
-use fuchsia_async as fasync;
 use fuchsia_zircon as zx;
-use futures::StreamExt;
-use socket_parsing::NewlineChunker;
 use std::collections::HashMap;
-
-/// `Results` represent the results of an LTP test run as reported by the test
-/// binary's `stderr`.
-#[derive(Default)]
-struct Results {
-    passed: Option<usize>,
-    failed: Option<usize>,
-    broken: Option<usize>,
-    skipped: Option<usize>,
-    warnings: Option<usize>,
-}
-
-impl Results {
-    /// Returns whether or not these results should be reported as a "pass" to the test
-    /// framework.
-    ///
-    /// Returns `None` if the `Results` do not contain enough information to determine whether or
-    /// not the test passed.
-    fn passed(&self, expected_result: &str) -> Option<bool> {
-        match (expected_result, self) {
-            (
-                "PASSED",
-                Results {
-                    passed: Some(_),
-                    failed: Some(0),
-                    broken: Some(0),
-                    warnings: Some(0),
-                    skipped: Some(0),
-                },
-            )
-            | (
-                "IGNORED",
-                Results {
-                    passed: Some(_),
-                    failed: Some(0),
-                    broken: Some(0),
-                    skipped: Some(_),
-                    warnings: Some(0),
-                },
-            ) => {
-                // Test case results were parsed successfully, and the results matched
-                // expectations.
-                Some(true)
-            }
-            (
-                _,
-                Results { passed: None, failed: None, broken: None, skipped: None, warnings: None },
-            ) => {
-                // Results were not parsed successfully, so can't determine whether or not the test
-                // passed.
-                None
-            }
-            // Results were parsed successfully, but don't match expectations.
-            _ => Some(false),
-        }
-    }
-
-    /// Parses results from `input` and adds the to the provided `Results` instance.
-    fn count_results(&mut self, input: &[u8]) {
-        match input {
-            input if input.starts_with(b"passed") => {
-                self.passed = Results::parse_number(&input);
-            }
-            input if input.starts_with(b"failed") => {
-                self.failed = Results::parse_number(&input);
-            }
-            input if input.starts_with(b"broken") => {
-                self.broken = Results::parse_number(&input);
-            }
-            input if input.starts_with(b"skipped") => {
-                self.skipped = Results::parse_number(&input);
-            }
-            input if input.starts_with(b"warnings") => {
-                self.warnings = Results::parse_number(&input);
-            }
-            _ => {}
-        };
-    }
-
-    fn parse_number(input: &[u8]) -> Option<usize> {
-        let input = String::from_utf8_lossy(input);
-        input
-            .split_whitespace()
-            .flat_map(|s| s.parse::<usize>())
-            .collect::<Vec<usize>>()
-            .first()
-            .copied()
-    }
-}
 
 struct LtpTestDefinition {
     name: String,
@@ -162,14 +70,9 @@ pub async fn run_ltp_cases(
         test_commands.insert(t.name, t.command);
     });
 
-    // TODO(b/287506763): Parse the expected test results, if available. This is a temporary
-    // measure, eventually the LTP test package will always contain this file.
-    let expected_test_results: Option<HashMap<String, String>> = {
-        match read_file_from_component_ns(&mut start_info, "data/test_results.json").await {
-            Ok(results_string) => serde_json::from_str(&results_string).ok(),
-            _ => None,
-        }
-    };
+    let file_content =
+        read_file_from_component_ns(&mut start_info, "data/test_results.json").await?;
+    let expected_test_results: HashMap<String, String> = serde_json::from_str(&file_content)?;
 
     for test in tests {
         let test_name = test.name.as_ref().expect("No test name");
@@ -178,58 +81,15 @@ pub async fn run_ltp_cases(
             .ok_or_else(|| anyhow!("Invalid test name: {}", test_name))?;
         let command = vec!["/bin/sh", "-c", test_command];
 
-        let (component_controller, component_std_handles) =
+        let (component_controller, std_handles) =
             start_command(&mut start_info, component_runner, &base_path, &command)?;
 
-        // Create the proxy socket endpoints to hand off to the run listener.
-        let (sender_stderr, listener_stderr) = zx::Socket::create_stream();
-        let parsed_std_handles = ftest::StdHandles {
-            out: component_std_handles.out,
-            err: Some(listener_stderr),
-            ..Default::default()
-        };
         let (case_listener_proxy, case_listener) = create_proxy::<ftest::CaseListenerMarker>()?;
-        run_listener_proxy.on_test_case_started(&test, parsed_std_handles, case_listener)?;
+        run_listener_proxy.on_test_case_started(&test, std_handles, case_listener)?;
 
-        /// Max size for message when draining input stream socket. This number is
-        /// slightly smaller than size allowed by Archivist (LogSink service implementation).
-        const MAX_MESSAGE_SIZE: usize = 30720;
-
-        // Read stderr from the test component and:
-        //   - forward the logs to the run listener
-        //   - parse the logs for test results
-        let socket = fasync::Socket::from_socket(component_std_handles.err.unwrap())
-            .expect("failed to create socket for stderr");
-        let mut stderr_lines = NewlineChunker::new_with_newlines(socket, MAX_MESSAGE_SIZE);
-
-        let mut parsed_results = Results::default();
-        while let Some(Ok(line)) = stderr_lines.next().await {
-            let _ = sender_stderr.write(&line);
-            parsed_results.count_results(&line);
-        }
-
-        let exit_code_result = read_ltp_test_result(component_controller.take_event_stream()).await;
-
-        let result = if let Some(expected_test_results) = &expected_test_results {
-            // If a result file was present, we check the parsed results to determine whether or not
-            // the test passed. If the result file is not present, always default to just the exit
-            // code.
-            //
-            // Eventually the exit code result will only be used if the test result parsing failed,
-            // since the test expectations file will always be present.
-            if let Some(success) = parsed_results.passed(&expected_test_results[test_name]) {
-                if success {
-                    ftest::Result_ { status: Some(ftest::Status::Passed), ..Default::default() }
-                } else {
-                    ftest::Result_ { status: Some(ftest::Status::Failed), ..Default::default() }
-                }
-            } else {
-                exit_code_result
-            }
-        } else {
-            exit_code_result
-        };
-
+        let allow_skipped = expected_test_results[test_name] == "IGNORED";
+        let result =
+            read_ltp_test_result(component_controller.take_event_stream(), allow_skipped).await;
         case_listener_proxy.finished(&result)?;
     }
 
@@ -321,6 +181,7 @@ fn start_command(
 
 async fn read_ltp_test_result(
     event_stream: frunner::ComponentControllerEventStream,
+    allow_skipped: bool,
 ) -> ftest::Result_ {
     // Base value used by the ComponentRunner implementation in Starnix to
     // return non-zero error codes (see src/starnix/kernel/execution/component_runner.rs ).
@@ -337,7 +198,7 @@ async fn read_ltp_test_result(
         zx::Status::OK => {
             ftest::Result_ { status: Some(ftest::Status::Passed), ..Default::default() }
         }
-        status if status.into_raw() == LTP_RESULT_TCONF => {
+        status if status.into_raw() == LTP_RESULT_TCONF && allow_skipped => {
             ftest::Result_ { status: Some(ftest::Status::Skipped), ..Default::default() }
         }
         _ => ftest::Result_ { status: Some(ftest::Status::Failed), ..Default::default() },
