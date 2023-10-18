c121003639777e3c4fae4ff0e26702069e8c8d92
Nick Maniscalco <maniscalco@google.com>
2023-10-18 23:55:32 +0000

    [kernel][riscv64] Use mp_sync_exec for arch_sync_cache_range
    Use mp_sync_task for arch_sync_cache_range instead of an SBI remote
    call.  This change fixes a potential race where an
    arch_sync_cache_range concurrent with offlining a CPU can result in a
    lockup in SBI.  Unlike SBI remote calls, mp_sync_task is designed to
    handle this race.  See fxbug.dev/135398 for details.
    
    Bug: 135398
    Change-Id: I9936b09ba58c6597934e512e9d6d850f39641d5a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/933940
    Reviewed-by: Adrian Danis <adanis@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Nick Maniscalco <maniscalco@google.com>

--- zircon/kernel/arch/riscv64/cache.cc
+++ zircon/kernel/arch/riscv64/cache.cc
@@ -7,9 +7,7 @@
 #include <lib/arch/cache.h>
 #include <trace.h>
 
-#include <arch/interrupt.h>
-#include <arch/ops.h>
-#include <arch/riscv64/sbi.h>
+#include <kernel/mp.h>
 
 #define LOCAL_TRACE 0
 
@@ -21,23 +19,14 @@ void arch_clean_invalidate_cache_range(vaddr_t start, size_t len) {}
 void arch_invalidate_cache_range(vaddr_t start, size_t len) {}
 
 // Synchronize the instruction and data cache on all cpus.
-// Note: this is very expensive and requires a cross cpu interrupt via SBI.
+// Note: this is very expensive and requires cross cpu IPIs.
 void arch_sync_cache_range(vaddr_t start, size_t len) {
   LTRACEF("start %#lx, len %zu\n", start, len);
 
-  // To keep the cpu from changing between the local shootdown and the remote,
-  // disable interrupts around this operation.
-  InterruptDisableGuard irqd;
-
-  // Shootdown on the local core
-  __asm__("fence.i");
-
-  // Send the shootdown to the rest of the cores.
-  const cpu_mask_t cpu_mask = mask_all_but_one(arch_curr_cpu_num());
-  // TODO(maniscalco): Use an mp_sync_task instead of an SBI remote call to ensure that we properly
-  // handle the case where one or more CPUs in the mask has transitioned to offline.  See also
-  // fxbug.dev/135398.
-  sbi_remote_fencei(cpu_mask);
+  // Shootdown on all cores.  Using mp_sync_exec instead of an SBI remote fence to handle race
+  // conditions with cores going offline.
+  auto fencei = [](void*) { __asm__("fence.i"); };
+  mp_sync_exec(MP_IPI_TARGET_ALL, /* cpu_mask */ 0, fencei, nullptr);
 }
 
 }  // extern C
