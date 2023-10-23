4a84c62bc049e3cea660b673189d1bc81583b7ba
Ben Keller <galbanum@google.com>
2023-10-23 20:59:58 +0000

    [pkg-cache] Protect cache packages from GC unconditionally
    Previously, cache packages were activated in the dynamic index on
    startup and were protected from GC as long as they remained active in
    the dynamic index, i.e. as long as another package with the same name
    (as indicated by the meta/package file) was not resolved.
    
    Now, cache packages are still activated in the dynamic index on
    startup, but they will be protected from GC regardless of if they
    remain active.
    
    This change was foretold by, and is in accordance with, the Open
    Package Tracking RFC.
    
    Fixed: b/293332528
    Change-Id: I9f8a24299056add0266d35d7675fa5360e5a0e2b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/935191
    Reviewed-by: Erick Tryzelaar <etryzelaar@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Ben Keller <galbanum@google.com>
    Reviewed-by: Aidan Wolter <awolter@google.com>

--- src/lib/assembly/platform_configuration/src/subsystems/storage.rs
+++ src/lib/assembly/platform_configuration/src/subsystems/storage.rs
@@ -144,7 +144,7 @@ impl DefineSubsystemConfiguration<StorageConfig> for StorageSubsystemConfig {
             builder
                 .package("pkg-cache")
                 .component("meta/pkg-cache.cm")?
-                .field("protect_cache_packages", false)?
+                .field("protect_cache_packages", true)?
                 .field("use_fxblob", fxfs_blob)?
                 .field("use_system_image", true)?;
 
