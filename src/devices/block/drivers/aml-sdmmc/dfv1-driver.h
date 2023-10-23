2235b088eee116700ee89252f6d5d7ff3ac284a8
HanBin Yoon <hanbinyoon@google.com>
2023-10-23 16:15:06 +0000

    [aml-sdmmc] Introduce own copy of io-buffer.h/c.
    ddk::IoBuffer cannot be used with DFv2. This change makes a local
    copy, which is used in both the DFv1 and DFv2 modes of the driver.
    
    (The namespace of aml_sdmmc::IoBuffer is explicitly specified for
    highlighting, since ddk::IoBuffer is a well-known and widely-used
    class.)
    
    Bug: b/301003087
    Test: fx test sdmmc-test
    Change-Id: Ibb03fe942ab0b096064ee28393a3cd5caa71e55a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/934199
    Commit-Queue: HanBin Yoon <hanbinyoon@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>

--- src/devices/block/drivers/aml-sdmmc/dfv1-driver.h
+++ src/devices/block/drivers/aml-sdmmc/dfv1-driver.h
@@ -6,7 +6,6 @@
 #define SRC_DEVICES_BLOCK_DRIVERS_AML_SDMMC_DFV1_DRIVER_H_
 
 #include <fidl/fuchsia.hardware.gpio/cpp/wire.h>
-#include <lib/ddk/io-buffer.h>
 #include <lib/driver/outgoing/cpp/outgoing_directory.h>
 #include <lib/mmio/mmio.h>
 #include <lib/zx/interrupt.h>
@@ -14,6 +13,7 @@
 #include <ddktl/device.h>
 
 #include "aml-sdmmc.h"
+#include "io-buffer.h"
 
 namespace aml_sdmmc {
 
@@ -26,7 +26,7 @@ class Dfv1Driver : public Dfv1DriverType, public AmlSdmmc {
 
   Dfv1Driver(zx_device_t* parent, zx::bti bti, fdf::MmioBuffer mmio, aml_sdmmc_config_t config,
              zx::interrupt irq, fidl::ClientEnd<fuchsia_hardware_gpio::Gpio> reset_gpio,
-             ddk::IoBuffer descs_buffer)
+             aml_sdmmc::IoBuffer descs_buffer)
       : Dfv1DriverType(parent),
         AmlSdmmc(std::move(bti), std::move(mmio), config, std::move(irq), std::move(reset_gpio),
                  std::move(descs_buffer)),
