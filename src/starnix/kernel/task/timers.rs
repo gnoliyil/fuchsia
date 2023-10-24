dfa35323ab6ac80fdf489451f42c679cb3c4abb0
Adam Barth <abarth@google.com>
2023-10-24 19:18:38 +0000

    [starnix] Timers should use starnix_lock
    This code was bypassing our tracing mutex machinery. This CL normalizes
    this code to use the same Mutex we use elsewhere in starnix_kernel.
    
    Change-Id: I984d038008c6c47d83714ae03a9a9d51e536413f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/936639
    Reviewed-by: Marina Ciocea <marinaciocea@google.com>
    Reviewed-by: Guocheng Wei <guochengwei@google.com>
    Fuchsia-Auto-Submit: Adam Barth <abarth@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/starnix/kernel/task/timers.rs
+++ src/starnix/kernel/task/timers.rs
@@ -2,10 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use starnix_lock::Mutex;
 use std::collections::HashMap;
-
-// TODO: Why doesn't this use crate::lock::Mutex?
-use fuchsia_sync::Mutex;
 
 use crate::{
     signals::{SignalEvent, SignalEventNotify, SignalEventValue},
