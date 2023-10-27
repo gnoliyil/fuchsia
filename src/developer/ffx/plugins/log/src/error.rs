a9a2b5c146b8d8f5d592b9c2cdba42792f1a6e5e
Megan Batty <mgnb@google.com>
2023-10-27 01:29:38 +0000

    [ffx][log] Clean up error translation to fho error
    - Rather than embedding the library log error in the ffx
      log error, the library log error is translated into the ffx log error to
      make sure that they stay consistent.
    - All branches of the error types are explicitly covered in the
      translation from LogError to fho::Error so that an explicit decision
      is made about whether it's a "user error" or an "unexpected error" and
      new additions to either struct will need a decision about which they
      are.
    - anyhow errors are directly `Into`d to fho errors so that if they are
      user errors (came from `ffx_error!` or similar) they will be properly
      translated into an fho::UserError and be presented without the
      alarming BUG message.
    
    Fixed: b/308015590
    Change-Id: I5b60f6c7c3ee0719ed457bac3f28bfc48edda565
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/938373
    Fuchsia-Auto-Submit: Megan Batty <mgnb@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Miguel Flores <miguelfrde@google.com>

--- src/developer/ffx/plugins/log/src/error.rs
+++ src/developer/ffx/plugins/log/src/error.rs
@@ -31,18 +31,45 @@ pub enum LogError {
     DumpWithSinceNow,
     #[error("No symbolizer configuration provided")]
     NoSymbolizerConfig,
-    #[error(transparent)]
-    LogCommandError(#[from] log_command::LogError),
     #[error("failed to connect to RealmQuery: {:?}", error)]
     RealmQueryConnectionFailed { error: i32 },
 }
 
+impl From<log_command::LogError> for LogError {
+    fn from(value: log_command::LogError) -> Self {
+        use log_command::LogError::*;
+        match value {
+            UnknownError(err) => Self::UnknownError(err),
+            NoBootTimestamp => Self::NoBootTimestamp,
+            IOError(err) => Self::IOError(err),
+            DumpWithSinceNow => Self::DumpWithSinceNow,
+            NoSymbolizerConfig => Self::NoSymbolizerConfig,
+            FfxError(err) => Self::UnknownError(err.into()),
+            Utf8Error(err) => Self::UnknownError(err.into()),
+            FidlError(err) => Self::UnknownError(err.into()),
+        }
+    }
+}
+
 impl From<LogError> for fho::Error {
     fn from(value: LogError) -> Self {
+        use LogError::*;
         match value {
-            LogError::DumpWithSinceNow => fho::Error::User(value.into()),
-            LogError::LogCommandError(log_command::LogError::FfxError(err)) => err.into(),
-            err => fho::Error::Unexpected(err.into()),
+            // anyhow errors may carry ffx user errors, so let the normal translation deal with that.
+            UnknownError(err) => err.into(),
+            // these errors have useful, actionable errors for users
+            DumpWithSinceNow
+            | NoBootTimestamp
+            | NoSymbolizerConfig
+            | RealmQueryConnectionFailed { .. }
+            | OpenTargetError { .. }
+            | TargetConnectionError { .. }
+            | IdentifyHostError { .. }
+            | ConnectCapabilityError { .. } => fho::Error::User(value.into()),
+            // these errors are probably an unexpected problem with no actionable error output.
+            FidlError(err) => fho::Error::Unexpected(err.into()),
+            IOError(err) => fho::Error::Unexpected(err.into()),
+            ConfigError(err) => fho::Error::Unexpected(err.into()),
         }
     }
 }
