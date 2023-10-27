e59e333f752b436ce8e25b8fc7a3c650de70771d
Yilong Li <liyl@google.com>
2023-10-27 22:28:32 +0000

    [display][edid] Ignore non-DMT standard timings.
    The "standard timing descriptors" in EDID / E-EDID uses
    three fields (width, aspect ratio, frame rate) to determine
    the display timing parameters. If a display timing parameter
    cannot be found in the VESA display monitor timing (DMT)
    standard, it will be calculated using formulas in the VESA
    Generalized Timing Formula (GTF) or Coordinated Video Timing
    (CVT) standards.
    
    Most modern displays don't solely rely on the standard timing
    descriptors. Instead, they may use CTA-861-I formats (for
    "standard"-sized TVs or monitors) or detailed display descriptors
    (for "non-standard" displays). None of the displays to be
    officially supported by Fuchsia has non-DMT standard timings.
    
    Currently, the GTF / CVT calculation code is not used by any
    Fuchsia product and is untested. Thus, we decided to delete the
    code, so that all non-DMT standard timings will be ignored.
    We may bring it back in the future if there's a display device
    we need to support which relies on such a calculated timing.
    
    Test: display-tool on Surenoo SUR480480Y021A (uses DTD)
          display-tool on MIMO 10" display (uses CTA-861-I)
    Fixed: 135772
    Change-Id: I4ebfd29a42188dc1d3c44a5810b331e37d77db3c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/938697
    Fuchsia-Auto-Submit: Victor Costan <costan@google.com>
    Reviewed-by: Victor Costan <costan@google.com>
    Fuchsia-Auto-Submit: Yilong Li <liyl@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/graphics/display/lib/edid/edid.cc
+++ src/graphics/display/lib/edid/edid.cc
@@ -4,6 +4,8 @@
 
 #include "src/graphics/display/lib/edid/edid.h"
 
+#include <lib/ddk/debug.h>
+
 #include <cmath>
 #include <cstddef>
 #include <cstdio>
@@ -35,8 +37,6 @@ bool base_validate(const T* block) {
   }
   return sum == 0;
 }
-
-uint32_t round_div(double num, double div) { return (uint32_t)((num / div) + .5); }
 
 }  // namespace
 
@@ -290,7 +290,6 @@ void convert_std_to_timing(const BaseEdid& edid, const StandardTimingDescriptor&
                            timing_params* params) {
   // Pick the largest resolution advertised by the display and then use the
   // generalized timing formula to compute the timing parameters.
-  // TODO(fxbug.dev/31310): Handle secondary GTF and CVT
   // TODO(stevensd): Support interlaced modes and margins
   uint32_t width = std.horizontal_resolution();
   uint32_t height = std.vertical_resolution(edid.edid_version, edid.edid_revision);
@@ -309,48 +308,15 @@ void convert_std_to_timing(const BaseEdid& edid, const StandardTimingDescriptor&
     }
   }
 
-  // Default values for GFT variables
-  static constexpr uint32_t kCellGran = 8;
-  static constexpr uint32_t kMinPorch = 1;
-  static constexpr uint32_t kVsyncRequired = 3;
-  static constexpr uint32_t kHsyncPercent = 8;
-  static constexpr uint32_t kMinVsyncPlusBpUs = 550;
-  static constexpr uint32_t kM = 600;
-  static constexpr uint32_t kC = 40;
-  static constexpr uint32_t kK = 128;
-  static constexpr uint32_t kJ = 20;
-  static constexpr uint32_t kCPrime = ((kC - kJ) * kK / 256) + kJ;
-  static constexpr uint32_t kMPrime = (kK * kM) / 256;
-
-  uint32_t h_pixels_rnd = round_div(width, kCellGran) * kCellGran;
-  double h_period_est = (1000000.0 - kMinVsyncPlusBpUs * v_rate) / (v_rate * (height + kMinPorch));
-  uint32_t vsync_bp = round_div(kMinVsyncPlusBpUs, h_period_est);
-  uint32_t v_total_lines = height + vsync_bp + kMinPorch;
-  double v_field_rate_est = 1000000.0 / (h_period_est * v_total_lines);
-  double h_period = (1.0 * h_period_est * v_field_rate_est) / v_rate;
-  double v_field_rate = 1000000.0 / h_period / v_total_lines;
-  double ideal_duty_cycle = kCPrime - (kMPrime * h_period_est / 1000);
-  uint32_t h_blank_pixels =
-      2 * kCellGran *
-      round_div(h_pixels_rnd * ideal_duty_cycle, (100 - ideal_duty_cycle) * (2 * kCellGran));
-  uint32_t total_pixels = h_pixels_rnd + h_blank_pixels;
-  double pixel_freq = total_pixels / h_period;
-
-  params->pixel_freq_khz = static_cast<uint32_t>(pixel_freq * 10 + 5);
-  params->horizontal_addressable = h_pixels_rnd;
-  params->horizontal_sync_pulse =
-      round_div(kHsyncPercent * total_pixels, 100 * kCellGran) * kCellGran;
-  params->horizontal_front_porch = h_blank_pixels / 2 - params->horizontal_sync_pulse;
-  params->horizontal_blanking = h_blank_pixels;
-  params->vertical_addressable = height;
-  params->vertical_front_porch = kMinPorch;
-  params->vertical_sync_pulse = kVsyncRequired;
-  params->vertical_blanking = vsync_bp + kMinPorch;
-
-  // TODO(fxbug.dev/31310): Set these depending on if we use default/secondary GTF
-  params->flags = timing_params::kPositiveVsync;
-
-  params->vertical_refresh_e2 = static_cast<uint32_t>(v_field_rate * 100 + .5);
+  zxlogf(WARNING,
+         "This EDID contains a non-DMT standard timing (%" PRIu32 "x%" PRIu32 " @%" PRIu32
+         "Hz). The timing is not supported and will be ignored. See fxbug.dev/135772 for "
+         "details.",
+         width, height, v_rate);
+  // TODO(fxbug.dev/135820): timing_iterator uses vertical_addressable == 0 or
+  // horizontal_addressable == 0 to determine whether a timing is valid.
+  // Instead we should use a std::optional<> to represent invalid timings.
+  *params = {};
 }
 
 timing_iterator& timing_iterator::operator++() {
