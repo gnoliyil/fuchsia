dad276979416659114b97a4a346dedc534517649
Hunter Freyer <hjfreyer@google.com>
2023-11-03 13:56:44 +0000

    [rfc_appscript] A few changes.
    - Don't open the API up to everyone.
    - Don't hardcode the table ID. Instead, the Table will pass it
      when calling the script. This makes it easier to avoid using the
      wrong table when testing changes.
    - Split the updates into two stages: one that finds CLs the Table
      doesn't know about, and another that updates the data from gerrit
      for an individual row. This will make it easier to update the
      metadata for active RFCs hourly.
    - Remove the YAML stuff. It only updated the deprecated Area field,
      and I don't think it's quite useful enough to implement with the
      new column.
    
    Change-Id: I4d03083c3f15a43d1fe46aec000b4aff342cbbf3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/940774
    Reviewed-by: Ben Lawson <benlawson@google.com>
    Commit-Queue: Hunter Freyer <hjfreyer@google.com>

--- tools/rfc_appscript/src/fetch_rfcs.js
+++ tools/rfc_appscript/src/fetch_rfcs.js
@@ -5,108 +5,73 @@
 // This file contains javascript which gets deployed to scripts.google.com, in
 // order to power custom logic for the internal RFC tracker.
 
-const TABLE_ID = 'bbfvcDiuyZ08S5ZJ7vN4ZI';
-const TABLE_NAME = 'tables/' + TABLE_ID;
 const TITLE_COL = 'Title';
 const CL_COL = 'CL';
 const CHANGE_ID_COL = 'Change Id';
 const AUTHORS_COL = 'Author(s)';
 const SUBMITTED_COL = 'Submitted';
-const AREA_COL = 'Area (Deprecated)';
 const STATUS_COL = 'Status';
+const NEEDS_SYNC_COL = 'Needs Sync';
+
 const WIP_STATUS = 'Socialization';
 const DEFAULT_STATUS = 'Draft';
 
 const BASE_URL = 'https://fuchsia-review.googlesource.com';
 const RFCS_DIR = 'docs/contribute/governance/rfcs';
-const RFCS_YAML_PATH = 'docs/contribute/governance/rfcs/_rfcs.yaml';
 const SUBJECT_TAG = '[rfc]';
 
-// TODO: Migrate to 'Areas' column with multiple areas once Lookup column type is supported by the Apps Script API.
-const AREA_ALLOWLIST = ['bringup', 'camera', 'cast', 'chromium', 'cobalt', 'component framework',
-  'connectivity', 'dart', 'developer', 'devices', 'diagnostics', 'factory',
-  'fidl', 'firmware', 'flutter', 'fonts', 'general', 'governance', 'graphics',
-  'hwinfo', 'identity', 'intl', 'lib', 'media', 'modular', 'power', 'recovery',
-  'security', 'session', 'speech', 'storage', 'sys', 'testing', 'ui', 'virtualization',
-  'zircon', 'swd', 'hci'];
+// Gets the list of open RFC CLs from gerrit and compares that to the set of
+// RFCs in the tracker. For any CLs missing from the tracker, creates stub rows
+// with just the change_id filled in.
+function fetchRfc(tableId) {
+  const gerritCls = _fetchOpenRfcsCls();
+  const trackerCls = _getExistingRfcs(tableId);
 
-function fetchRfcs() {
-  // See https://gerrit-review.googlesource.com/Documentation/rest-api-changes.html for documentation.
-  let url = BASE_URL + '/changes/?q='
-    + 'dir:' + encodeURIComponent(RFCS_DIR)
-    + '+is:open'
-    + '&o=LABELS'
-    + '&o=DETAILED_ACCOUNTS' //  include _account_id, email and username fields when referencing accounts.
-    + '&n=100';
-  Logger.log(url);
-  let response = UrlFetchApp.fetch(url, { 'muteHttpExceptions': false });
-  let data = JSON.parse(removeXssHeader(response));
-
-  let old_rfcs = getExistingRfcs();
-
-  // Accumulate create/patch requests so that requests can be batched.
-  let create_rows = [];
-  let patch_rows = [];
-  for (let cl of data) {
-    if (!cl.subject.toLowerCase().startsWith(SUBJECT_TAG)) {
-      continue;
+  // Accumulate create requests so that requests can be batched.
+  let createRows = [];
+  for (const changeId of gerritCls) {
+    if (!(changeId in trackerCls)) {
+      createRows.push({
+        row: {
+          values: {
+            [CHANGE_ID_COL]: changeId,
+          }
+        }
+      });
     }
-
-    // Remove subject prefix tags (e.g. "[rfc][docs]")
-    cl.subject = cl.subject.replace(/^(\S+]\s)/i, '');
-
-    // See https://gerrit-review.googlesource.com/Documentation/rest-api-changes.html#get-content for documentation.
-    let yaml = getYamlContent(cl);
-
-    // Update old RFC
-    if (cl.change_id in old_rfcs) {
-      let row_name = old_rfcs[cl.change_id].name;
-      let row = {
-        [TITLE_COL]: cl.subject,
-        [CL_COL]: `fxrev.dev/${cl._number}`,
-        [AUTHORS_COL]: cl.owner.email
-      };
-      // Don't overwrite manually entered area with null.
-      if (yaml.area) {
-        row[AREA_COL] = yaml.area;
-      }
-      patch_rows.push({ name: row_name, values: row });
-      continue;
-    }
-
-    const status = cl.work_in_progress ? WIP_STATUS : DEFAULT_STATUS;
-
-    // Add new RFC
-    create_rows.push({
-      values: {
-        [TITLE_COL]: cl.subject,
-        [STATUS_COL]: status,
-        [CL_COL]: `fxrev.dev/${cl._number}`,
-        [CHANGE_ID_COL]: cl.change_id,
-        [AUTHORS_COL]: cl.owner.email,
-        [SUBMITTED_COL]: cl.created,
-        [AREA_COL]: yaml.area
-      }
-    });
   }
 
-  if (patch_rows.length != 0) {
-    Area120Tables.Tables.Rows.batchUpdate({ requests: patch_rows.map(r => ({ row: r })) }, TABLE_NAME);
-  }
-
-  if (create_rows.length != 0) {
-    Area120Tables.Tables.Rows.batchCreate({ requests: create_rows.map(r => ({ row: r })) }, TABLE_NAME);
+  if (createRows.length != 0) {
+    Area120Tables.Tables.Rows.batchCreate({ requests: createRows }, `tables/${tableId}`);
   }
 }
 
+// Returns an Array of change_ids.
+function _fetchOpenRfcsCls() {
+  const data = _doGerritRequest(BASE_URL + '/changes/?q='
+    + 'dir:' + encodeURIComponent(RFCS_DIR)
+    + '+is:open'
+    + '&n=100');
+
+  const changeIds = [];
+
+  for (const cl of data) {
+    if (cl.subject.toLowerCase().startsWith(SUBJECT_TAG)) {
+      changeIds.push(cl.change_id);
+    }
+  }
+
+  return changeIds;
+}
+
 // Returns {[change_id]: {name: "row_name"}, ...}
-function getExistingRfcs() {
+function _getExistingRfcs(tableId) {
   // In order to limit total response sizes,
   // https://developers.google.com/apps-script/advanced/tables?hl=en#read_rows_of_a_table has clients
   // read through the rows in units of pages, which we do here so that this script won't fail when
   // there are more than N RFCs
   const PAGE_SIZE = 1000;
-  let response = Area120Tables.Tables.Rows.list(TABLE_NAME, { page_size: PAGE_SIZE });
+  let response = Area120Tables.Tables.Rows.list(`tables/${tableId}`, { page_size: PAGE_SIZE });
   let rfcs = {};
   while (response && 'rows' in response) {
     let rows = response.rows;
@@ -123,58 +88,59 @@ function getExistingRfcs() {
     if (!pageToken) {
       response = undefined;
     } else {
-      response = Area120Tables.Tables.Rows.list(TABLE_NAME, { page_size: PAGE_SIZE, page_token: pageToken });
+      response = Area120Tables.Tables.Rows.list(`tables/${tableId}`, { page_size: PAGE_SIZE, page_token: pageToken });
     }
   }
   return rfcs;
 }
 
-function removeXssHeader(rawJson) {
-  return rawJson.getContentText().substring(5);
+// Takes the URL for a gerrit REST API request, does the RPC, and returns a
+// parsed JSON object with the result.
+//
+// See https://gerrit-review.googlesource.com/Documentation/rest-api-changes.html for documentation.
+function _doGerritRequest(url) {
+  console.log("Gerrit RPC: ", url);
+  const response = UrlFetchApp.fetch(url);
+  const rawJson = response.getContentText().substring(5);
+
+  console.log("Gerrit Response: ", rawJson);
+
+  return JSON.parse(rawJson);
 }
 
-// Returns {area: "area", status: "status"}, defaulting to null values
-// if unable to parse the diff of `docs/contribute/governance/rfcs/_rfcs.yaml`.
-function getYamlContent(cl) {
-  let yaml_diff_url = BASE_URL
-    + '/changes/'
-    + cl.change_id
-    + '/revisions/current/files/'
-    + encodeURIComponent(RFCS_YAML_PATH)
-    + '/diff'; // Get the diff only so we can parse out the relevant fields.
+// Apply any programmatic updates we might want to make to the given row.
+// Specifically, fetch the latest state from gerrit and update the row to
+// match.
+function syncRow(tableId, rowId) {
+  const rowName = `tables/${tableId}/rows/${rowId}`;
+  const row = Area120Tables.Tables.Rows.get(rowName);
 
-  let yaml_diff_res = UrlFetchApp.fetch(yaml_diff_url, { 'muteHttpExceptions': false });
-  let yaml_diff_text = removeXssHeader(yaml_diff_res);
-  // `yaml_diff_res_json` is the https://gerrit-review.googlesource.com/Documentation/rest-api-changes.html#diff-info
-  // object, where the added text we want to pull out is nested in the array of DiffContent's `b` fields.
-  let yaml_diff_res_json = JSON.parse(yaml_diff_text);
-  let yaml_diff_content = yaml_diff_res_json["content"].filter(diffInfo => diffInfo.b != null);
+  console.log("Row before: ", row);
 
-  if (yaml_diff_content.length == 0) {
-    Logger.log("No YAML change detected for RFC " + cl.subject);
-    return { area: null, status: null };
-  }
+  const changeId = row.values[CHANGE_ID_COL];
 
-  let [rfc_area, rfc_status] = [null, null];
-  // Loop over all the blocks of added lines, in case the author modified content elsewhere.
-  for (let yaml_diff_lines of yaml_diff_content) {
-    for (let line of yaml_diff_lines.b) {
-      let maybe_rfc_area = line.split("area: [");
-      let maybe_rfc_status = line.split("status: ");
-      if (maybe_rfc_area.length == 2) {
-        rfc_area = maybe_rfc_area[1].split(']')[0].replace(/\'/g, '');
-      }
-      // Mark provided, but 0-length status strings the same as missing status strings.
-      if (maybe_rfc_status.length == 2 && maybe_rfc_status[1] != "''") {
-        rfc_status = maybe_rfc_status[1].replace(/\'/g, '');
-      }
+  const cl = _doGerritRequest(
+    BASE_URL + `/changes/${changeId}?o=DETAILED_ACCOUNTS` //  include _account_id, email and username fields when referencing accounts.
+  );
+
+  // Remove subject prefix tags (e.g. "[rfc][docs]")
+  const title = cl.subject.replace(/^(\S+]\s)/i, '');
+
+  const patchedValues = {
+    [TITLE_COL]: title,
+    [CL_COL]: `fxrev.dev/${cl._number}`,
+    [AUTHORS_COL]: cl.owner.email,
+    [SUBMITTED_COL]: cl.created,
+    [NEEDS_SYNC_COL]: false,
+  };
+
+  if (cl.work_in_progress) {
+    patchedValues[STATUS_COL] = WIP_STATUS
+  } else {
+    if (!row.values[STATUS_COL] || row.values[STATUS_COL] === WIP_STATUS) {
+      patchedValues[STATUS_COL] = DEFAULT_STATUS
     }
   }
-  Logger.log('RFC YAML: ' + cl.subject + "\narea: " + rfc_area + "\nstatus: " + rfc_status);
 
-  if (rfc_area && !AREA_ALLOWLIST.includes(rfc_area.toLowerCase())) {
-    rfc_area = null;
-  }
-
-  return { area: rfc_area, status: rfc_status };
+  Area120Tables.Tables.Rows.patch({ values: patchedValues }, rowName);
 }
