a156dfae38b6a22d048623e822d83b03ea827661
David Fang <fangism@google.com>
2023-11-09 18:53:25 +0000

    [starnix] Eliminate output-dir-leak
    Reference environment variable include inputs using
    paths relative to build output dir, where rustc is invoked.
    
    Bug: 94355
    Change-Id: Ie5cf3217a01f8db5847b88646baa788b5297225d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/944201
    Fuchsia-Auto-Submit: David Fang <fangism@google.com>
    Commit-Queue: David Fang <fangism@google.com>
    Reviewed-by: Benjamin Lerman <qsr@google.com>

--- src/starnix/tools/convert_tarball_to_starnix_container/src/default_init.rs
+++ src/starnix/tools/convert_tarball_to_starnix_container/src/default_init.rs
@@ -2,5 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-pub static DEFAULT_INIT_ARM64: &[u8] = include_bytes!(env!("DEFAULT_INIT_ARM64_PATH"));
-pub static DEFAULT_INIT_X64: &[u8] = include_bytes!(env!("DEFAULT_INIT_X64_PATH"));
+use include_bytes_from_working_dir::include_bytes_from_working_dir_env;
+
+pub static DEFAULT_INIT_ARM64: &[u8] =
+    include_bytes_from_working_dir_env!("DEFAULT_INIT_ARM64_PATH");
+pub static DEFAULT_INIT_X64: &[u8] = include_bytes_from_working_dir_env!("DEFAULT_INIT_X64_PATH");
