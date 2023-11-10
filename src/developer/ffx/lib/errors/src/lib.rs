ee068ef6e85d06fdb06385810bd0d082f62b2d8e
Casey Dahlin <sadmac@google.com>
2023-11-10 05:55:20 +0000

    [ffx] Don't use fuchsia.developer.ffx in errors lib
    Bug: b/291987067
    Change-Id: I6eed384e27c872556cac3ab0f8fc20b112fb2efa
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/941181
    Reviewed-by: Steven Grady <slgrady@google.com>
    Commit-Queue: Casey Dahlin <sadmac@google.com>

--- src/developer/ffx/lib/errors/src/lib.rs
+++ src/developer/ffx/lib/errors/src/lib.rs
@@ -4,6 +4,7 @@
 
 use std::process::ExitStatus;
 
+#[cfg(not(target_os = "fuchsia"))]
 use fidl_fuchsia_developer_ffx::{
     DaemonError, OpenTargetError, TargetConnectionError, TunnelError,
 };
@@ -25,6 +26,7 @@ pub enum FfxError {
     #[error("{}", .0)]
     Error(#[source] anyhow::Error, i32 /* Error status code */),
 
+    #[cfg(not(target_os = "fuchsia"))]
     #[error("{}", match .err {
         DaemonError::TargetCacheError => format!("Target {} could not be looked up in the cache due to an unspecified error. Retry your request, and if that fails run `ffx doctor`.", target_string(.target, .is_default_target)),
         DaemonError::TargetStateError => format!("Target {} is not in a state capable of the requested operation. Inspect `ffx target list` to determine if it is in the expected state.", target_string(.target, .is_default_target)),
@@ -42,12 +44,14 @@ pub enum FfxError {
     })]
     DaemonError { err: DaemonError, target: Option<String>, is_default_target: bool },
 
+    #[cfg(not(target_os = "fuchsia"))]
     #[error("{}", match .err {
         OpenTargetError::QueryAmbiguous => format!("Target specification {} matched multiple targets. Use `ffx target list` to list known targets, and use a more specific matcher.", target_string(.target, .is_default_target)),
         OpenTargetError::TargetNotFound => format!("Target specification {} was not found. Use `ffx target list` to list known targets, and use a different matcher.", target_string(.target, .is_default_target))
     })]
     OpenTargetError { err: OpenTargetError, target: Option<String>, is_default_target: bool },
 
+    #[cfg(not(target_os = "fuchsia"))]
     #[error("{}", match .err {
         TunnelError::CouldNotListen => "Could not establish a host-side TCP listen socket".to_string(),
         TunnelError::TargetConnectFailed => "Couldn not connect to target to establish a tunnel".to_string(),
@@ -57,6 +61,7 @@ pub enum FfxError {
     #[error("{}", format!("No target with matcher {} was found.\n\n* Use `ffx target list` to verify the state of connected devices.\n* Use the SERIAL matcher with the --target (-t) parameter to explicity match a device.", target_string(.target, .is_default_target)))]
     FastbootError { target: Option<String>, is_default_target: bool },
 
+    #[cfg(not(target_os = "fuchsia"))]
     #[error("{}", match .err {
         TargetConnectionError::PermissionDenied => format!("Could not establish SSH connection to the target {}: Permission denied.", target_string(.target, .is_default_target)),
         TargetConnectionError::ConnectionRefused => format!("Could not establish SSH connection to the target {}: Connection refused.", target_string(.target, .is_default_target)),
