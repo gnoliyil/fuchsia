0f2de2a30e996c164d3b34e51a539db523829ca7
Anirudh Mathukumilli <rudymathu@google.com>
2023-11-13 22:28:34 +0000

    [zircon][restricted-mode-shared] Refactor basic test
    Moves any assertions/expectations that can be made from the main test
    thread to that thread. This sets up for a future CL will add a test that
    verifies that a thread in restricted mode cannot access the shared
    region.
    
    Bug: 132980
    Change-Id: Idc645df689d0f35bf2e81a68417bacba12c06e41
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/943341
    Fuchsia-Auto-Submit: Anirudh Mathukumilli <rudymathu@google.com>
    Reviewed-by: Nick Maniscalco <maniscalco@google.com>
    Commit-Queue: Anirudh Mathukumilli <rudymathu@google.com>

--- src/zircon/tests/restricted-mode-shared/basic/restricted-mode-shared.cc
+++ src/zircon/tests/restricted-mode-shared/basic/restricted-mode-shared.cc
@@ -26,27 +26,7 @@ constexpr bool kUseUnifiedAspace = true;
 constexpr bool kUseUnifiedAspace = false;
 #endif  // defined(__x86_64__)
 
-void RunRestrictedMode(zx_handle_t restricted_vmar_handle, zx_status_t* result) {
-  zx::vmar rvmar(restricted_vmar_handle);
-
-  // Set up the restricted mode stack.
-  zx::vmo stack;
-  const uint32_t stack_size = zx_system_get_page_size();
-  zx::result<zx_vaddr_t> res = SetupStack(restricted_vmar_handle, stack_size, &stack);
-  if (res.is_error()) {
-    *result = res.status_value();
-    return;
-  }
-  zx_vaddr_t stack_addr = res.value();
-
-  // Set up the restricted mode code segment.
-  res = SetupCodeSegment(restricted_vmar_handle, write_to_stack_blob());
-  if (res.is_error()) {
-    *result = res.status_value();
-    return;
-  }
-  zx_vaddr_t cs_addr = res.value();
-
+void RunRestrictedMode(zx_vaddr_t cs_addr, zx_vaddr_t stack_addr, zx_status_t* result) {
   // Set up the restricted state.
   zx::vmo restricted;
   *result = zx_restricted_bind_state(0, restricted.reset_and_get_address());
@@ -77,33 +57,19 @@ void RunRestrictedMode(zx_handle_t restricted_vmar_handle, zx_status_t* result) 
     return;
   }
 
-  // Validate that the restricted mode routine wrote the right value to the stack.
-  uint8_t buffer[8];
-  *result = stack.read(buffer, stack_size - 8, 8);
-  if (*result != ZX_OK) {
-    return;
-  }
-  uint8_t expected[8] = {0xdd, 0xdd, 0xcc, 0xcc, 0xbb, 0xbb, 0xaa, 0xaa};
-  for (size_t i = 0; i < sizeof(buffer); i++) {
-    if (expected[i] != buffer[i]) {
-      *result = ZX_ERR_INTERNAL;
-      return;
-    }
-  }
-
   // Validate that the restricted mode routine wrote the right value to the stack without going
   // through zx_vmo_read. This only works when unified address spaces are enabled, which is
   // currently only on x86.
   if constexpr (kUseUnifiedAspace) {
-    memcpy(buffer, (void*)(stack_addr - 8), 8);
-    for (size_t i = 0; i < sizeof(buffer); i++) {
-      if (expected[i] != buffer[i]) {
+    uint8_t expected[8] = {0xdd, 0xdd, 0xcc, 0xcc, 0xbb, 0xbb, 0xaa, 0xaa};
+    uint8_t* stack_values = reinterpret_cast<uint8_t*>(stack_addr - 8);
+    for (size_t i = 0; i < sizeof(expected); i++) {
+      if (expected[i] != stack_values[i]) {
         *result = ZX_ERR_INTERNAL;
         return;
       }
     }
   }
-  *result = ZX_OK;
 }
 
 }  // namespace
@@ -117,6 +83,18 @@ TEST(RestrictedModeUnified, Basic) {
                                      restricted_proc.reset_and_get_address(),
                                      restricted_vmar.reset_and_get_address()));
 
+  // Set up the restricted mode stack.
+  zx::vmo stack;
+  const uint32_t stack_size = zx_system_get_page_size();
+  zx::result<zx_vaddr_t> res = SetupStack(restricted_vmar.get(), stack_size, &stack);
+  ASSERT_OK(res.status_value());
+  zx_vaddr_t stack_addr = res.value();
+
+  // Set up the restricted mode code segment.
+  res = SetupCodeSegment(restricted_vmar.get(), write_to_stack_blob());
+  ASSERT_OK(res.status_value());
+  zx_vaddr_t cs_addr = res.value();
+
   // Spawn a thread inside the shared process that executes `RunRestrictedMode`. We want this
   // thread to have all of the functionality provided by the C runtime (safe stack, libraries,
   // etc.) but we don't want to have to do this ourselves. We accomplish this by setting the
@@ -124,10 +102,19 @@ TEST(RestrictedModeUnified, Basic) {
   // using C++ primitives.
   auto prev_proc = thrd_set_zx_process(restricted_proc.get());
   zx_status_t result = ZX_ERR_INTERNAL;
-  std::thread restricted_thread(RunRestrictedMode, restricted_vmar.get(), &result);
+  std::thread restricted_thread(RunRestrictedMode, cs_addr, stack_addr, &result);
   thrd_set_zx_process(prev_proc);
 
   // Wait for the restricted thread to complete.
   restricted_thread.join();
   ASSERT_OK(result);
+
+  // Validate that the restricted mode routine wrote the right value to the stack using a
+  // zx_vmo_read.
+  uint8_t buffer[8];
+  ASSERT_OK(stack.read(buffer, stack_size - 8, 8));
+  uint8_t expected[8] = {0xdd, 0xdd, 0xcc, 0xcc, 0xbb, 0xbb, 0xaa, 0xaa};
+  for (size_t i = 0; i < sizeof(buffer); i++) {
+    EXPECT_EQ(expected[i], buffer[i]);
+  }
 }
