7d3711938883cf00fe014ebf2501f4a45b4bcf53
Caroline Liu <carolineliu@google.com>
2023-11-16 23:28:11 +0000

    [display][virtio_gpu] enable virtio-abi-test in the v2 virtio_gpu test suite
    Bug: 134883
    Test: fx test virtio-gpu-test
    Change-Id: I1471c766f131d3909de80b97bca6947e8513b8d3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/947872
    Reviewed-by: Victor Costan <costan@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Victor Costan <costan@google.com>

--- src/graphics/display/drivers/virtio-guest/v2/virtio-abi-test.cc
+++ src/graphics/display/drivers/virtio-guest/v2/virtio-abi-test.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "src/graphics/display/drivers/virtio-guest/virtio-abi.h"
+#include "src/graphics/display/drivers/virtio-guest/v2/virtio-abi.h"
 
 #include <type_traits>
 
