d321bc616da09275c49fe02402dc1b74e7de4e3d
Aidan Wolter <awolter@google.com>
2023-11-17 00:02:41 +0000

    [assembly] Forensics configs are added via assembly
    Read the product config and include the proper forensics configs. The
    old GN groups are left around because vendor repos still use them.
    
    Bug: b/295032829
    Change-Id: I897735745f3c0db8cb00b0465909f566ffbc4033
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/941850
    Reviewed-by: Alex Pankhurst <pankhurst@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Aaron Wood <aaronwood@google.com>
    Fuchsia-Auto-Submit: Aidan Wolter <awolter@google.com>

--- src/lib/assembly/platform_configuration/src/subsystems/forensics.rs
+++ src/lib/assembly/platform_configuration/src/subsystems/forensics.rs
@@ -3,18 +3,42 @@
 // found in the LICENSE file.
 
 use crate::subsystems::prelude::*;
+use crate::util;
 use assembly_config_schema::platform_config::forensics_config::ForensicsConfig;
 
 pub(crate) struct ForensicsSubsystem;
 impl DefineSubsystemConfiguration<ForensicsConfig> for ForensicsSubsystem {
     fn define_configuration(
-        _context: &ConfigurationContext<'_>,
+        context: &ConfigurationContext<'_>,
         config: &ForensicsConfig,
         builder: &mut dyn ConfigurationBuilder,
     ) -> anyhow::Result<()> {
         if config.feedback.low_memory {
             builder.platform_bundle("feedback_low_memory_product_config");
         }
+        if config.feedback.large_disk {
+            builder.platform_bundle("feedback_large_disk");
+        }
+        if config.feedback.remote_device_id_provider {
+            builder.platform_bundle("feedback_remote_device_id_provider");
+        }
+
+        match context.build_type {
+            // The userdebug/user configs are platform bundles that add an override config.
+            BuildType::User => builder.platform_bundle("feedback_user_config"),
+            BuildType::UserDebug => builder.platform_bundle("feedback_userdebug_config"),
+            // The eng config is actually an absent override config.
+            BuildType::Eng => {}
+        }
+
+        // Cobalt may be added to anything utility and higher.
+        if matches!(
+            context.feature_set_level,
+            FeatureSupportLevel::Minimal | FeatureSupportLevel::Utility
+        ) {
+            util::add_build_type_config_data("cobalt", context, builder)?;
+        }
+
         Ok(())
     }
 }
