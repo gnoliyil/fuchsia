665733160bc0b623c02bc55c13718b3bc651c232
Benjamin Lerman <qsr@chromium.org>
2023-11-21 13:23:24 +0000

    [starnix] Generalize FileFinalizer
    This allows to use the same mechanism for other object that requires
    delayed finalization. This will be used for FsNode the requires calling
    FsNodeOps::forget
    
    Change-Id: I5126f3637629b2a6fe94cde4fe56bf27715c8dce
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/949433
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Benjamin Lerman <qsr@google.com>
    Reviewed-by: Kevin Lindkvist <lindkvist@google.com>

--- src/starnix/kernel/delayed_releaser.rs
+++ src/starnix/kernel/delayed_releaser.rs
@@ -9,6 +9,7 @@ use crate::{
 use starnix_uapi::ownership::{Releasable, ReleaseGuard};
 use std::{
     cell::RefCell,
+    marker::PhantomData,
     mem::MaybeUninit,
     ops::{Deref, DerefMut},
     sync::Arc,
@@ -70,55 +71,68 @@ impl DelayedReleaser {
     }
 }
 
+pub trait ReleaserAction<T> {
+    fn release(t: ReleaseGuard<T>);
+}
+
 /// Wrapper around `FileObject` that ensures that a unused `FileObject` is added to the current
 /// delayed releasers to be released at the next release point.
-pub struct FileReleaser(MaybeUninit<ReleaseGuard<FileObject>>);
+pub struct ObjectReleaser<T, F: ReleaserAction<T>>(MaybeUninit<ReleaseGuard<T>>, PhantomData<F>);
 
-impl From<FileObject> for FileReleaser {
-    fn from(file_object: FileObject) -> Self {
-        Self(MaybeUninit::new(file_object.into()))
+impl<T, F: ReleaserAction<T>> From<T> for ObjectReleaser<T, F> {
+    fn from(object: T) -> Self {
+        Self(MaybeUninit::new(object.into()), Default::default())
     }
 }
 
-impl Drop for FileReleaser {
+impl<T, F: ReleaserAction<T>> Drop for ObjectReleaser<T, F> {
     fn drop(&mut self) {
         let content = std::mem::replace(&mut self.0, MaybeUninit::uninit());
         // SAFETY
         // The `MaybeUninit` is initialize with a value and only ever extracted in this `drop` method, so
         // it is guaranteed that it is initialized at this point.
-        let file_object = unsafe { content.assume_init() };
-        RELEASERS.with(|cell| {
-            cell.borrow_mut().closed_files.push(file_object);
-        });
+        let object = unsafe { content.assume_init() };
+        F::release(object);
     }
 }
 
-impl std::fmt::Debug for FileReleaser {
+impl<T: std::fmt::Debug, F: ReleaserAction<T>> std::fmt::Debug for ObjectReleaser<T, F> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         self.deref().fmt(f)
     }
 }
 
-impl std::ops::Deref for FileReleaser {
-    type Target = FileObject;
+impl<T, F: ReleaserAction<T>> std::ops::Deref for ObjectReleaser<T, F> {
+    type Target = T;
 
     fn deref(&self) -> &Self::Target {
         // SAFETY
-        // The FileObject is initialized by the factory method and only every uninitialized in
+        // The value is initialized by the factory method and only every uninitialized in
         // Drop. The content is initialized for all the usual object lifecycle.
         let guard = unsafe { self.0.assume_init_ref() };
         guard.deref()
     }
 }
 
-impl std::borrow::Borrow<FileObject> for FileReleaser {
-    fn borrow(&self) -> &FileObject {
+impl<T, F: ReleaserAction<T>> std::borrow::Borrow<T> for ObjectReleaser<T, F> {
+    fn borrow(&self) -> &T {
         self.deref()
     }
 }
 
-impl std::convert::AsRef<FileObject> for FileReleaser {
-    fn as_ref(&self) -> &FileObject {
+impl<T, F: ReleaserAction<T>> std::convert::AsRef<T> for ObjectReleaser<T, F> {
+    fn as_ref(&self) -> &T {
         self.deref()
     }
 }
+
+pub enum FileObjectReleaserAction {}
+impl ReleaserAction<FileObject> for FileObjectReleaserAction {
+    fn release(file_object: ReleaseGuard<FileObject>) {
+        RELEASERS.with(|cell| {
+            cell.borrow_mut().closed_files.push(file_object);
+        });
+    }
+}
+
+pub type FileReleaser = ObjectReleaser<FileObject, FileObjectReleaserAction>;
