86c0466ca47bd88f917af232678a66afe7274505
Casey Dahlin <sadmac@google.com>
2023-11-21 03:31:21 +0000

    [ffx] Move daemon to new RCS port forwarding API
    Bug: b/291987165
    Change-Id: Ia6c5d8d80099a588d663a6010433c354531330c2
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/948035
    Reviewed-by: Steven Grady <slgrady@google.com>
    Fuchsia-Auto-Submit: Casey Dahlin <sadmac@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/developer/ffx/daemon/protocols/forward/src/lib.rs
+++ src/developer/ffx/daemon/protocols/forward/src/lib.rs
@@ -2,13 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use ::rcs as rcs_lib;
 use anyhow::{Context as _, Result};
 use async_net::TcpListener;
 use async_trait::async_trait;
 use ffx_config::ConfigLevel;
-use fidl;
 use fidl_fuchsia_developer_ffx as ffx;
-use fidl_fuchsia_developer_remotecontrol as rcs;
 use fidl_fuchsia_net::SocketAddress;
 use fidl_fuchsia_net_ext::SocketAddress as SocketAddressExt;
 use futures::{future::join, AsyncReadExt as _, AsyncWriteExt as _, StreamExt as _};
@@ -16,6 +15,8 @@ use protocols::prelude::*;
 use serde::{Deserialize, Serialize};
 use serde_json::{self, Value};
 use std::sync::Arc;
+
+const REVERSE_BACKLOG: u16 = 128;
 
 #[ffx_protocol]
 #[derive(Default)]
@@ -36,6 +37,7 @@ struct ForwardConfig {
 }
 
 const TUNNEL_CFG: &'static str = "tunnels";
+const FORWARD_SETUP_TIMEOUT: std::time::Duration = std::time::Duration::from_secs(5);
 
 impl Forward {
     async fn port_forward_task(
@@ -63,19 +65,21 @@ impl Forward {
                 }
             };
 
-            let (socket, remote) = fidl::Socket::create_stream();
-
-            match target.forward_tcp(&target_address, remote).await {
-                Ok(Ok(())) => (),
-                Ok(Err(e)) => {
-                    tracing::error!("Error forwarding port: {:?}", e);
-                    continue;
-                }
+            let socket = match rcs_lib::port_forward::forward_port(
+                &target,
+                SocketAddressExt::from(target_address).0,
+                FORWARD_SETUP_TIMEOUT,
+            )
+            .await
+            {
+                Ok(socket) => socket,
                 Err(e) => {
                     tracing::error!("Error requesting port forward from RCS: {:?}", e);
                     continue;
                 }
-            }
+            };
+
+            let (socket, keep_alive) = socket.split();
 
             let socket = match fuchsia_async::Socket::from_socket(socket) {
                 Ok(socket) => socket,
@@ -85,7 +89,7 @@ impl Forward {
                 }
             };
 
-            Self::establish_tcp_forward(conn, socket, Arc::clone(&tasks));
+            Self::establish_tcp_forward(conn, socket, keep_alive, Arc::clone(&tasks));
         }
     }
 
@@ -98,6 +102,7 @@ impl Forward {
     fn establish_tcp_forward(
         conn: async_net::TcpStream,
         socket: fuchsia_async::Socket,
+        keep_alive: rcs::port_forward::SocketKeepAliveToken,
         tasks: Arc<tasks::TaskManager>,
     ) {
         let (mut socket_read, mut socket_write) = socket.split();
@@ -128,6 +133,7 @@ impl Forward {
         };
         let forward = join(read_write, write_read);
         tasks.spawn(async move {
+            let _keep_alive = keep_alive;
             match forward.await {
                 (Err(a), Err(b)) => {
                     tracing::warn!("Port forward closed with errors:\n  {:?}\n  {:?}", a, b)
@@ -199,71 +205,58 @@ impl FidlProtocol for Forward {
                     }
                 };
 
-                let (client, mut event_stream) =
-                    fidl::endpoints::create_request_stream::<rcs::ForwardCallbackMarker>()?;
-                match target.reverse_tcp(&target_address, client).await? {
-                    Ok(()) => (),
+                let stream = match rcs_lib::port_forward::reverse_port(
+                    &target,
+                    SocketAddressExt::from(target_address).0,
+                    std::time::Duration::from_secs(5),
+                    REVERSE_BACKLOG,
+                )
+                .await
+                {
+                    Ok(s) => s,
                     Err(e) => {
-                        tracing::error!("Could not establish reverse TCP forward: {:?}", e);
+                        tracing::error!("Could establish reverse forward: {:?}", e);
                         return responder
                             .send(Err(ffx::TunnelError::CouldNotListen))
                             .context("error sending response");
                     }
-                }
+                };
 
                 let host_address: SocketAddressExt = host_address.into();
                 let host_address = host_address.0;
                 let tasks = Arc::clone(&self.0);
                 self.0.spawn(async move {
-                    while let Some(result) = event_stream.next().await {
-                        let request = match result {
-                            Ok(request) => request,
+                    let mut stream = std::pin::pin!(stream);
+                    while let Some((addr, socket)) = stream.next().await {
+                        tracing::info!(
+                            "Connection from {:?} forwarding to {:?}",
+                            addr,
+                            host_address
+                        );
+                        let tcp_stream = match async_net::TcpStream::connect(&host_address).await {
+                            Ok(stream) => stream,
                             Err(e) => {
-                                tracing::warn!("Error reported to forwarding callback: {:?}", e);
+                                tracing::error!("Could not connect to {:?}: {:?}", host_address, e);
                                 continue;
                             }
                         };
 
-                        match request {
-                            rcs::ForwardCallbackRequest::Forward {
-                                socket,
-                                addr,
-                                control_handle: _,
-                            } => {
-                                let addr: SocketAddressExt = addr.into();
-                                let addr = addr.0;
-                                tracing::info!(
-                                    "Connection from {:?} forwarding to {:?}",
-                                    addr,
-                                    host_address
-                                );
-                                let tcp_stream =
-                                    match async_net::TcpStream::connect(&host_address).await {
-                                        Ok(stream) => stream,
-                                        Err(e) => {
-                                            tracing::error!(
-                                                "Could not connect to {:?}: {:?}",
-                                                host_address,
-                                                e
-                                            );
-                                            continue;
-                                        }
-                                    };
+                        let (socket, keep_alive) = socket.split();
 
-                                let socket = match fuchsia_async::Socket::from_socket(socket) {
-                                    Ok(socket) => socket,
-                                    Err(e) => {
-                                        tracing::error!(
-                                            "Error converting socket to async: {:?}",
-                                            e
-                                        );
-                                        continue;
-                                    }
-                                };
-
-                                Self::establish_tcp_forward(tcp_stream, socket, Arc::clone(&tasks))
+                        let socket = match fuchsia_async::Socket::from_socket(socket) {
+                            Ok(socket) => socket,
+                            Err(e) => {
+                                tracing::error!("Error converting socket to async: {:?}", e);
+                                continue;
                             }
-                        }
+                        };
+
+                        Self::establish_tcp_forward(
+                            tcp_stream,
+                            socket,
+                            keep_alive,
+                            Arc::clone(&tasks),
+                        )
                     }
                 });
 
@@ -318,7 +311,74 @@ impl FidlProtocol for Forward {
 mod tests {
     use super::*;
     use ffx::DaemonError;
-    use protocols::testing::FakeDaemonBuilder;
+    use fidl_fuchsia_developer_remotecontrol as rcs;
+    use fidl_fuchsia_posix_socket as fsock;
+    use fidl_fuchsia_sys2 as sys2;
+
+    static HOST_ADDRESS: &str = "127.0.0.1:1234";
+    static TARGET_ADDRESS: &str = "127.0.0.1:5678";
+
+    fn host_address() -> SocketAddress {
+        SocketAddressExt(HOST_ADDRESS.parse().unwrap()).into()
+    }
+
+    fn target_address() -> SocketAddress {
+        SocketAddressExt(TARGET_ADDRESS.parse().unwrap()).into()
+    }
+
+    async fn test_stream_socket(mut stream: fsock::StreamSocketRequestStream) {
+        let mut bound = false;
+        let mut listening = false;
+        let mut describe_endpoint = None;
+        while let Some(Ok(request)) = stream.next().await {
+            match request {
+                fsock::StreamSocketRequest::Bind { addr, responder } => {
+                    assert_eq!(target_address(), addr);
+                    assert!(!bound, "bound socket twice");
+                    bound = true;
+                    responder.send(Ok(())).unwrap();
+                }
+                fsock::StreamSocketRequest::Describe { responder } => {
+                    assert!(describe_endpoint.is_none());
+                    let (socket, endpoint) = fidl::Socket::create_stream();
+                    describe_endpoint = Some(endpoint);
+                    responder
+                        .send(fsock::StreamSocketDescribeResponse {
+                            socket: Some(socket),
+                            ..Default::default()
+                        })
+                        .unwrap()
+                }
+                fsock::StreamSocketRequest::Listen { backlog: _, responder } => {
+                    assert!(bound, "listened to unbound socket");
+                    assert!(!listening, "listened to socket twice");
+                    listening = true;
+                    responder.send(Ok(())).unwrap();
+                }
+                other => panic!("Unexpected request: {other:?}"),
+            }
+        }
+    }
+    async fn test_socket_provider(channel: fidl::Channel) {
+        println!("Spawning test provider");
+        let channel = fidl::endpoints::ServerEnd::<fsock::ProviderMarker>::from(channel);
+        let mut stream = channel.into_stream().unwrap();
+
+        while let Some(Ok(request)) = stream.next().await {
+            match request {
+                fsock::ProviderRequest::StreamSocket { domain, proto, responder } => {
+                    assert_eq!(fsock::Domain::Ipv4, domain);
+                    assert_eq!(fsock::StreamSocketProtocol::Tcp, proto);
+                    let (client, stream) =
+                        fidl::endpoints::create_request_stream::<fsock::StreamSocketMarker>()
+                            .unwrap();
+                    fuchsia_async::Task::spawn(test_stream_socket(stream)).detach();
+                    responder.send(Ok(client)).unwrap();
+                }
+                other => panic!("Unexpected request: {other:?}"),
+            }
+        }
+    }
 
     #[derive(Default, Clone)]
     struct TestDaemon;
@@ -336,11 +396,18 @@ mod tests {
                 let mut server = server.into_stream().unwrap();
                 while let Some(request) = server.next().await {
                     match request.unwrap() {
-                        rcs::RemoteControlRequest::ReverseTcp { addr, client: _, responder } => {
-                            assert_eq!(
-                                SocketAddressExt("127.0.0.1:5678".parse().unwrap()),
-                                SocketAddressExt::from(addr)
-                            );
+                        rcs::RemoteControlRequest::OpenCapability {
+                            moniker: _,
+                            capability_set,
+                            capability_name,
+                            server_channel,
+                            flags: _,
+                            responder,
+                        } => {
+                            assert_eq!(sys2::OpenDirType::NamespaceDir, capability_set);
+                            assert_eq!("svc/fuchsia.posix.socket.Provider", capability_name);
+                            fuchsia_async::Task::spawn(test_socket_provider(server_channel))
+                                .detach();
                             responder.send(Ok(())).unwrap();
                         }
                         other => panic!("Unexpected request: {:?}", other),
@@ -384,8 +451,26 @@ mod tests {
 
     #[fuchsia_async::run_singlethreaded(test)]
     async fn test_forward() {
-        let daemon = FakeDaemonBuilder::new().register_fidl_protocol::<Forward>().build();
-        let _proxy = daemon.open_proxy::<ffx::TunnelMarker>().await;
+        let forward = Forward::default();
+        let context = Context::new(TestDaemon);
+        let (client, server) = fidl::endpoints::create_endpoints::<ffx::TunnelMarker>();
+
+        fuchsia_async::Task::local(async move {
+            let mut server = server.into_stream().unwrap();
+            while let Some(request) = server.next().await {
+                let request = request.unwrap();
+                forward.handle(&context, request).await.unwrap();
+            }
+        })
+        .detach();
+
+        client
+            .into_proxy()
+            .unwrap()
+            .forward_port("dummy_target", &host_address(), &target_address())
+            .await
+            .unwrap()
+            .unwrap();
     }
 
     #[fuchsia_async::run_singlethreaded(test)]
@@ -406,11 +491,7 @@ mod tests {
         client
             .into_proxy()
             .unwrap()
-            .reverse_port(
-                "dummy_target",
-                &SocketAddressExt("127.0.0.1:1234".parse().unwrap()).into(),
-                &SocketAddressExt("127.0.0.1:5678".parse().unwrap()).into(),
-            )
+            .reverse_port("dummy_target", &host_address(), &target_address())
             .await
             .unwrap()
             .unwrap();
