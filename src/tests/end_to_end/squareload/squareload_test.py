c84d02f7bcec78a6c50eb71e336a69c4fa1ed6b8
Kevin Cho <chok@google.com>
2023-11-22 21:58:55 +0000

    [power][perf] Add CPU performance trace to squareload test.
    * Initialize trace from host
    * Compute CPU aggregate metrics
    * Publish CPU metrics
    
    Bug: b/311705990
    
    Change-Id: Ia3a927eaa60c5b927bf865023c289fa49299bba5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/950217
    Reviewed-by: Gwen Mittertreiner <gmtr@google.com>
    Reviewed-by: Fadi Meawad <fmeawad@google.com>
    Reviewed-by: Vinicius Felizardo <felizardo@google.com>
    Commit-Queue: Kevin Cho <chok@google.com>

--- src/tests/end_to_end/squareload/squareload_test.py
+++ src/tests/end_to_end/squareload/squareload_test.py
@@ -2,14 +2,16 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import os
 import logging
+import os
 
 from mobly import test_runner
 
 from fuchsia_power_base_test_lib import fuchsia_power_base_test
 from perf_publish import publish
 from power_test_utils import power_test_utils
+from trace_processing import trace_importing, trace_metrics, trace_model
+from trace_processing.metrics import cpu
 
 _LOGGER: logging.Logger = logging.getLogger(__name__)
 
@@ -26,8 +28,40 @@ class SquareloadTest(fuchsia_power_base_test.FuchsiaPowerBaseTest):
 
         Compute and publish power metrics.
         """
+        # Initialize host-side tracing and execute target workload.
+        self.device.tracing.initialize(
+            categories=[
+                "kernel:sched",
+                "kernel:meta",
+                "starnix:atrace",
+                "system_metrics_logger",
+                "system_metrics",
+                "memory_monitor",
+            ],
+            buffer_size=36,
+        )
+        self.device.tracing.start()
         super().test_launch_hermetic_test()
+        self.device.tracing.stop()
 
+        # Process trace-based CPU metrics.
+        fxt_trace_path: str = self.device.tracing.terminate_and_download(
+            directory=self.log_path, trace_file=f"{self.metric_name}.fxt"
+        )
+        trace_json_path: os.PathLike = (
+            trace_importing.convert_trace_file_to_json(
+                trace_path=str(fxt_trace_path)
+            )
+        )
+
+        model: trace_model.Model = trace_importing.create_model_from_file_path(
+            trace_json_path
+        )
+        cpu_results: list[trace_metrics.TestCaseResult] = cpu.metrics_processor(
+            model, {"aggregateMetricsOnly": True}
+        )
+
+        # Process non-trace-based power metrics.
         metrics_processor = power_test_utils.PowerMetricsProcessor(
             power_samples_path=self.power_trace_path
         )
@@ -35,7 +69,7 @@ class SquareloadTest(fuchsia_power_base_test.FuchsiaPowerBaseTest):
         fuchsiaperf_json_path = metrics_processor.write_fuchsiaperf_json(
             output_dir=self.log_path,
             metric_name=self.metric_name,
-            trace_results=[],
+            trace_results=cpu_results,
         )
         publish.publish_fuchsiaperf(
             [fuchsiaperf_json_path],
