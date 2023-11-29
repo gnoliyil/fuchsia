1b347a66cc6fcb0e1186d5845ad63885177c1e5b
Suraj Malhotra <surajmalhotra@google.com>
2023-11-29 01:50:41 +0000

    [driver][compat] Add metadata retrieval APIs
    These APIs simplify metadata retrieval.
    
    Change-Id: Ie22f0d962d37cd65b8d626f0f1b84d8bb668ed86
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/952822
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Novin Changizi <novinc@google.com>
    Fuchsia-Auto-Submit: Suraj Malhotra <surajmalhotra@google.com>
    Reviewed-by: Sarah Chan <spqchan@google.com>

---
+++ sdk/lib/driver/compat/cpp/metadata.h
@@ -0,0 +1,96 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef LIB_DRIVER_COMPAT_CPP_METADATA_H_
+#define LIB_DRIVER_COMPAT_CPP_METADATA_H_
+
+#include <fidl/fuchsia.driver.compat/cpp/wire.h>
+#include <lib/driver/incoming/cpp/namespace.h>
+
+#include <memory>
+#include <string_view>
+
+namespace compat {
+
+namespace internal {
+template <typename FidlType, typename Unpersist>
+zx::result<FidlType> GetMetadata(const std::shared_ptr<fdf::Namespace>& incoming, uint32_t type,
+                                 std::string_view instance, Unpersist unpersist) {
+  zx::result compat = incoming->Connect<fuchsia_driver_compat::Service::Device>(instance);
+  if (compat.is_error()) {
+    return compat.take_error();
+  }
+
+  fidl::WireResult metadata_result = fidl::WireCall(*compat)->GetMetadata();
+  if (!metadata_result.ok()) {
+    return zx::error(metadata_result.status());
+  }
+  if (metadata_result->is_error()) {
+    return metadata_result->take_error();
+  }
+
+  auto& metadata_vector = metadata_result.value()->metadata;
+  for (const auto& metadata : metadata_vector) {
+    if (metadata.type != type) {
+      continue;
+    }
+    size_t size;
+    zx_status_t status = metadata.data.get_prop_content_size(&size);
+    if (status != ZX_OK) {
+      return zx::error(status);
+    }
+
+    return unpersist(metadata.data, size);
+  }
+  return zx::error(ZX_ERR_NOT_FOUND);
+}
+
+}  // namespace internal
+
+// Attempts to talk to a parent to acquire metadata with |type| and then decodes it into |FidlType|.
+template <typename FidlType>
+zx::result<FidlType> GetMetadata(const std::shared_ptr<fdf::Namespace>& incoming, uint32_t type,
+                                 std::string_view instance = "default") {
+  return internal::GetMetadata<FidlType>(
+      incoming, type, instance, [](const zx::vmo& vmo, size_t size) -> zx::result<FidlType> {
+        auto buffer = std::make_unique<uint8_t[]>(size);
+        zx_status_t status = vmo.read(buffer.get(), 0, size);
+        if (status != ZX_OK) {
+          return zx::error(status);
+        }
+
+        fit::result result = fidl::Unpersist<FidlType>(cpp20::span(buffer.get(), size));
+        if (result.is_error()) {
+          return zx::error(result.error_value().status());
+        }
+        return zx::ok(*result);
+      });
+}
+
+// Attempts to talk to a parent to acquire metadata with |type| and then decodes it into |FidlType|.
+// The result is only valid as long as |arena| remains alive.
+template <typename FidlType>
+zx::result<fidl::ObjectView<FidlType>> GetMetadata(const std::shared_ptr<fdf::Namespace>& incoming,
+                                                   fidl::AnyArena& arena, uint32_t type,
+                                                   std::string_view instance = "default") {
+  return internal::GetMetadata<fidl::ObjectView<FidlType>>(
+      incoming, type, instance,
+      [&arena](const zx::vmo& vmo, size_t size) -> zx::result<fidl::ObjectView<FidlType>> {
+        auto buffer = arena.AllocateVector<uint8_t>(size);
+        zx_status_t status = vmo.read(buffer, 0, size);
+        if (status != ZX_OK) {
+          return zx::error(status);
+        }
+
+        fit::result result = fidl::InplaceUnpersist<FidlType>(cpp20::span(buffer, size));
+        if (result.is_error()) {
+          return zx::error(result.error_value().status());
+        }
+        return zx::ok(*result);
+      });
+}
+
+}  // namespace compat
+
+#endif  // LIB_DRIVER_COMPAT_CPP_METADATA_H_
