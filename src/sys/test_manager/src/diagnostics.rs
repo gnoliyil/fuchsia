9d0798a8d4b09339ddd153c5b80787ce4f155b03
Miguel Flores <miguelfrde@google.com>
2023-11-29 19:58:46 +0000

    Revert "[test_manager] Remove obsolete hack waiting for Archivist to respond to requests"
    This reverts commit 0bfa14a48be844e78262c52d48ced85f4b6aaaed.
    
    Reason for revert: seeing if this fixes flake, it may be the case that this is still relevant.
    
    Original change's description:
    > [test_manager] Remove obsolete hack waiting for Archivist to respond to requests
    >
    > I believe this hack is no longer necessary given that we no longer use
    > events v1.
    >
    > Fixed: 105308
    > Change-Id: Ic5b8f16192db8d06b914b329218db727cee542a1
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/911915
    > Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    > Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>
    > Reviewed-by: Christopher Johnson <crjohns@google.com>
    
    Multiply: test_manager_test
    Bug: 136461
    Change-Id: I5ea13e208e1cb006fd2e9b7b5f91b935a47c9168
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/953214
    Reviewed-by: Ankur Mittal <anmittal@google.com>
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>
    Commit-Queue: Miguel Flores <miguelfrde@google.com>

--- src/sys/test_manager/src/diagnostics.rs
+++ src/sys/test_manager/src/diagnostics.rs
@@ -19,6 +19,12 @@ pub(crate) struct ServeSyslogOutcome {
     /// Task serving any protocols needed to proxy logs. For example, this is populated
     /// when logs are served over overnet using DiagnosticsBridge.
     pub logs_iterator_task: Option<fasync::Task<Result<(), Error>>>,
+    /// A task which resolves when Archivist responds to a request. This task
+    /// should resolve before tearing down the realm. This is a workaround that
+    /// ensures that Archivist isn't torn down before it receives all ArchiveAccessor
+    /// requests.
+    // TODO(fxbug.dev/105308): Remove this hack once component events are ordered.
+    pub archivist_responding_task: fasync::Task<()>,
 }
 
 /// Connect to archivist and starting serving syslog.
@@ -42,7 +48,26 @@ pub(crate) fn serve_syslog(
         }
         _ => None,
     };
-    Ok(ServeSyslogOutcome { logs_iterator_task })
+    let archivist_responding_task = fasync::Task::spawn(async move {
+        let (proxy, iterator) =
+            fidl::endpoints::create_proxy().expect("cannot create batch iterator");
+        if let Err(e) = IsolatedLogsProvider::new(&accessor).start_streaming(
+            iterator,
+            StreamMode::Snapshot,
+            DataType::Inspect,
+            Some(0),
+        ) {
+            warn!("Failed to start streaming logs: {:?}", e);
+            return;
+        }
+        // This should always return something immediately, even if there are no logs
+        // due to Snapshot.
+        match proxy.get_next().await {
+            Ok(Ok(_)) => (),
+            other => warn!("Error retrieving logs from archivist: {:?}", other),
+        }
+    });
+    Ok(ServeSyslogOutcome { logs_iterator_task, archivist_responding_task })
 }
 
 fn run_iterator_socket(
@@ -76,16 +101,26 @@ impl<'a> IsolatedLogsProvider<'a> {
         &self,
         iterator: ServerEnd<BatchIteratorMarker>,
     ) -> Result<(), anyhow::Error> {
+        self.start_streaming(iterator, StreamMode::SnapshotThenSubscribe, DataType::Logs, None)
+    }
+
+    fn start_streaming(
+        &self,
+        iterator: ServerEnd<BatchIteratorMarker>,
+        stream_mode: StreamMode,
+        data_type: DataType,
+        batch_timeout: Option<i64>,
+    ) -> Result<(), anyhow::Error> {
         let stream_parameters = StreamParameters {
-            stream_mode: Some(StreamMode::SnapshotThenSubscribe),
-            data_type: Some(DataType::Logs),
+            stream_mode: Some(stream_mode),
+            data_type: Some(data_type),
             format: Some(Format::Json),
             client_selector_configuration: Some(ClientSelectorConfiguration::SelectAll(true)),
-            batch_retrieval_timeout_seconds: None,
+            batch_retrieval_timeout_seconds: batch_timeout,
             ..Default::default()
         };
         self.accessor.stream_diagnostics(&stream_parameters, iterator).map_err(|err| {
-            warn!(%err, "Failed to subscribe to isolated logs");
+            warn!(%err, ?data_type, "Failed to subscribe to isolated diagnostics data");
             err
         })?;
         Ok(())
