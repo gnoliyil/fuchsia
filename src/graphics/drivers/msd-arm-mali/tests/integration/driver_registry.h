739433aba24b542a3ba388b19b3213fcdb43bc0e
John Bauman <jbauman@google.com>
2023-12-18 19:57:50 +0000

    [msd-arm-mali] Switch to DFv2 version
    Switch to always using the DFv2 version of the MSD and remove the DFv1
    version.
    
    Change-Id: Ieedc75c235865dd2044fc8de5dcf51449e4cf043
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/959752
    Reviewed-by: Gurjant Kalsi <gkalsi@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Size-Review: Aidan Wolter <awolter@google.com>
    Reviewed-by: Aaron Wood <aaronwood@google.com>
    Fuchsia-Auto-Submit: John Bauman <jbauman@google.com>
    Reviewed-by: Craig Stout <cstout@google.com>

--- src/graphics/drivers/msd-arm-mali/tests/integration/driver_registry.h
+++ src/graphics/drivers/msd-arm-mali/tests/integration/driver_registry.h
@@ -6,6 +6,7 @@
 
 #include <fidl/fuchsia.driver.development/cpp/fidl.h>
 #include <fidl/fuchsia.driver.registrar/cpp/wire.h>
+#include <glob.h>
 #include <lib/component/incoming/cpp/protocol.h>
 
 #include <gtest/gtest.h>
@@ -13,38 +14,6 @@
 class RegisteredTestDriver {
  public:
   void Init() {
-    // Check whether DFv2 driver is loaded to determine whether to load a DFv2 test driver.
-    {
-      auto driver_development = component::Connect<fuchsia_driver_development::DriverDevelopment>();
-      ASSERT_TRUE(driver_development.is_ok());
-      auto development_client = fidl::SyncClient(std::move(*driver_development));
-
-      auto endpoints = fidl::CreateEndpoints<fuchsia_driver_development::DriverInfoIterator>();
-      ASSERT_TRUE(endpoints.is_ok());
-
-      const char* kDFv2DriverManifestUrl =
-          "fuchsia-pkg://fuchsia.com/msd-arm-mali-dfv2#meta/msd_arm_dfv2.cm";
-      auto driver_info = development_client->GetDriverInfo(
-          {{kDFv2DriverManifestUrl, "fuchsia-pkg://fuchsia.com/msd-arm-mali#meta/msd_arm.cm"},
-           std::move(endpoints->server)});
-
-      ASSERT_TRUE(driver_info.is_ok()) << driver_info.error_value();
-
-      auto driver_info_client = fidl::SyncClient(std::move(endpoints->client));
-
-      auto first_driver_info = driver_info_client->GetNext();
-      ASSERT_TRUE(first_driver_info.is_ok());
-      // Exactly one driver should be available, since the test doesn't work with 0 drivers, and
-      // having both will clash because they have identical bind rules.
-      ASSERT_EQ(first_driver_info->drivers().size(), 1u);
-      fuchsia_driver_framework::DriverInfo& driver = first_driver_info->drivers()[0];
-      ASSERT_TRUE(driver.url().has_value());
-
-      is_dfv2_ = driver.url() == kDFv2DriverManifestUrl;
-
-      printf("Driver %s is DFv2: %d\n", driver.url()->c_str(), is_dfv2_);
-    }
-
     auto registrar = component::Connect<fuchsia_driver_registrar::DriverRegistrar>();
 
     ASSERT_TRUE(registrar.is_ok());
@@ -71,23 +40,26 @@ class RegisteredTestDriver {
   }
 
   // TODO(fxbug.dev/124976): Unify rebind and production drivers.
-  const char* GetRebindDriverSuffix() {
-    return is_dfv2_ ? "msd_arm_rebind_dfv2.cm" : "msd_arm_rebind.cm";
-  }
-  const char* GetTestDriverSuffix() {
-    return is_dfv2_ ? "msd_arm_test_dfv2.cm" : "msd_arm_test.cm";
-  }
+  const char* GetRebindDriverSuffix() { return "msd_arm_rebind_dfv2.cm"; }
+  const char* GetTestDriverSuffix() { return "msd_arm_test_dfv2.cm"; }
 
-  const char* GetParentTopologicalPath() {
+  std::optional<std::string> GetParentTopologicalPath() {
     // TODO(fxbug.dev/127515): Avoid hardcoding this path.
-    return "/dev/sys/platform/05:06:17/aml-gpu-composite/aml-gpu/mali-composite";
+    // Find any aml-gpu-composite device. 05 is PDEV_VID_AMLOGIC, and 17 is
+    // PDEV_DID_AMLOGIC_MALI_INIT. The PID depends on the board.
+    glob_t glob_val;
+    int glob_result = glob("/dev/sys/platform/05:*:17/aml-gpu-composite/aml-gpu/mali-composite", 0,
+                           nullptr, &glob_val);
+    if (glob_result != 0) {
+      EXPECT_EQ(0, glob_result) << "Attempting to find GPU device failed";
+      return {};
+    }
+    std::string first_path{glob_val.gl_pathv[0]};
+    globfree(&glob_val);
+    return first_path;
   }
-
-  bool is_dfv2() const { return is_dfv2_; }
 
  private:
-  // True if the MSD uses the DFv2 APIs.
-  bool is_dfv2_ = false;
   fidl::WireSyncClient<fuchsia_driver_registrar::DriverRegistrar> registrar_client_;
 };
 
