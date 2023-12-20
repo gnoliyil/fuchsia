904f3e8beef437ee5e378bb2cbe8b809de83c5ec
James Sullivan <jfsulliv@google.com>
2023-12-20 22:37:35 +0000

    [driver_test_realm] Wait for /dev/sys/test
    The driver test realm binds a /dev/sys/test device during setup, but
    this can be racy with the later teardown of the test realm.  If the test
    completes quickly and the driver_manager starts to suspend, then the
    later call to add the device will fail and emit an error log (which
    fails unit tests).
    
    Fixed: 316579125
    Multiply: devfs-test.cm
    Change-Id: I0de06101a3d10a7e2d919052379723c34477c724
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/963955
    Reviewed-by: Jocelyn Dang <jocelyndang@google.com>
    Commit-Queue: James Sullivan <jfsulliv@google.com>

--- sdk/lib/driver_test_realm/simple/simple.cc
+++ sdk/lib/driver_test_realm/simple/simple.cc
@@ -2,10 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <fcntl.h>
 #include <fidl/fuchsia.driver.test/cpp/wire.h>
 #include <lib/component/incoming/cpp/protocol.h>
+#include <lib/device-watcher/cpp/device-watcher.h>
 #include <lib/syslog/cpp/log_settings.h>
 #include <lib/syslog/cpp/macros.h>
+
+#include <fbl/unique_fd.h>
 
 int main() {
   fuchsia_logging::SetTags({"simple_driver_test_realm"});
@@ -28,5 +32,19 @@ int main() {
     return 1;
   }
 
+  // Wait until /dev/sys/test has been populated so we know the test realm has been set up.
+  // Otherwise, tearing down the realm at the end of a test can race with asynchronous operations
+  // that occur during setup (see b/316579125).
+  fbl::unique_fd dev(open("/dev", O_RDONLY));
+  if (!dev) {
+    FX_SLOG(ERROR, "Failed to open /dev");
+    return 1;
+  }
+  if (zx_status_t status =
+          device_watcher::RecursiveWaitForFile(dev.get(), "sys/test").status_value();
+      status != ZX_OK) {
+    FX_SLOG(ERROR, "Failed to open /dev/sys/test");
+    return 1;
+  }
   return 0;
 }
