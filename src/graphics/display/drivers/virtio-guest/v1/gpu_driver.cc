2c4febc07ed6490c1f7e60e70c7861fdaa05d4f5
Victor Costan <pwnall@chromium.org>
2023-12-22 16:51:02 +0000

    [virtio-guest] Inline virtio::CreateAndBind() in v1 driver.
    This brings the driver creation code closer to GpuDevice::Create() in
    the v2 driver.
    
    Bug: b/42182631
    Test: Booted core.qemu-x64 using ffx emu start --engine femu --net user
    Change-Id: Ib01313aa5e6f4a9a5a2357c376b295a618cd8041
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/965177
    Reviewed-by: Yilong Li <liyl@google.com>
    Fuchsia-Auto-Submit: Victor Costan <costan@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/graphics/display/drivers/virtio-guest/v1/gpu_driver.cc
+++ src/graphics/display/drivers/virtio-guest/v1/gpu_driver.cc
@@ -3,8 +3,18 @@
 // found in the LICENSE file.
 
 #include <lib/ddk/binding_driver.h>
+#include <lib/ddk/debug.h>
 #include <lib/ddk/driver.h>
+#include <lib/virtio/backends/backend.h>
 #include <lib/virtio/driver_utils.h>
+#include <lib/zx/bti.h>
+#include <lib/zx/result.h>
+#include <zircon/status.h>
+
+#include <memory>
+#include <utility>
+
+#include <fbl/alloc_checker.h>
 
 #include "src/graphics/display/drivers/virtio-guest/v1/gpu.h"
 
@@ -12,16 +22,41 @@ namespace virtio_display {
 
 namespace {
 
-zx_status_t VirtioDisplayBind(void* ctx, zx_device_t* bus_device) {
+zx_status_t VirtioDisplayBind(void* ctx, zx_device_t* parent) {
   char flag[32];
   zx_status_t status =
-      device_get_variable(bus_device, "driver.virtio-gpu.disable", flag, sizeof(flag), nullptr);
+      device_get_variable(parent, "driver.virtio-gpu.disable", flag, sizeof(flag), nullptr);
   // If gpu disabled:
   if (status == ZX_OK && (!strcmp(flag, "1") || !strcmp(flag, "true") || !strcmp(flag, "on"))) {
     zxlogf(INFO, "driver.virtio-gpu.disabled=1, not binding to the GPU");
     return ZX_ERR_NOT_FOUND;
   }
-  return virtio::CreateAndBind<virtio_display::GpuDevice>(ctx, bus_device);
+
+  zx::result<std::pair<zx::bti, std::unique_ptr<virtio::Backend>>> bti_and_backend_result =
+      virtio::GetBtiAndBackend(parent);
+  if (!bti_and_backend_result.is_ok()) {
+    zxlogf(ERROR, "GetBtiAndBackend failed: %s", bti_and_backend_result.status_string());
+    return bti_and_backend_result.error_value();
+  }
+  auto& [bti, backend] = bti_and_backend_result.value();
+
+  fbl::AllocChecker alloc_checker;
+  auto device = fbl::make_unique_checked<GpuDevice>(&alloc_checker, parent, std::move(bti),
+                                                    std::move(backend));
+  if (!alloc_checker.check()) {
+    zxlogf(ERROR, "Failed to allocate memory for GpuDevice");
+    return ZX_ERR_NO_MEMORY;
+  }
+
+  status = device->Init();
+  if (status != ZX_OK) {
+    zxlogf(ERROR, "Failed to initialize device: %s", zx_status_get_string(status));
+    return status;
+  }
+
+  // devmgr now owns the memory for `device`.
+  [[maybe_unused]] auto device_ptr = device.release();
+  return ZX_OK;
 }
 
 constexpr zx_driver_ops_t kDriverOps = {
