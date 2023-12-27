30b5c5e27530c5e70e243f4e7cbb99ec71f8bb74
Aidan Wolter <awolter@google.com>
2023-12-27 22:41:46 +0000

    [rust-3p] Add strum and strum_macros
    Additionally, updates 'heck' and adds stub methods for rustversion.
    
    Bug: b/317781165, b/302150846
    Change-Id: I8b22b1bd29e4450313b48f558f99976d19ef4861
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/965119
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Aidan Wolter <awolter@google.com>
    Reviewed-by: Tyler Mandry <tmandry@google.com>

--- third_party/rust_crates/empty/rustversion/src/lib.rs
+++ third_party/rust_crates/empty/rustversion/src/lib.rs
@@ -1,3 +1,21 @@
 // Copyright 2023 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
+//! Stubs for rustversion in order to allow dependent crates to compile.
+//! We can assume that we are always on the latest version of Rust, as we will
+//! almost always be on a later version than the third_party crates.
+
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+
+#[proc_macro_attribute]
+pub fn before(_args: TokenStream, input: TokenStream) -> TokenStream {
+    TokenStream::new()
+}
+
+#[proc_macro_attribute]
+pub fn since(_args: TokenStream, input: TokenStream) -> TokenStream {
+    input
+}
