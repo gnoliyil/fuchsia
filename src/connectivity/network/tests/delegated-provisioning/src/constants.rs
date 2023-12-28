02fa53027b7167f61fca3867ee0cef831218af9f
Antonio Linhart <antoniolinhart@google.com>
2023-12-28 16:26:05 +0000

    [network/tests] Rename test client interface...
    to something less obtuse. This new name will still permit delegated
    provisioning to occur.
    
    Bug: 287297922
    Change-Id: Ieb7b6a6cd62df224160c0623e245c4811c2095fa
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/962392
    Reviewed-by: Jeff Martin <martinjeffrey@google.com>
    Commit-Queue: Antonio Linhart <antoniolinhart@google.com>

--- src/connectivity/network/tests/delegated-provisioning/src/constants.rs
+++ src/connectivity/network/tests/delegated-provisioning/src/constants.rs
@@ -10,8 +10,5 @@ use net_declare::fidl_ip_v4;
 pub const SERVER_STATIC_IP: fnet::Ipv4Address = fidl_ip_v4!("192.0.168.1");
 pub const DHCP_DYNAMIC_IP: fnet::Ipv4Address = fidl_ip_v4!("192.0.168.2");
 
-// TODO(https://issues.fuchsia.dev/287297922): This is a special name that
-// allows delegated provisioning in certain contexts. Rename it to something
-// less obtuse.
-pub const CLIENT_IFACE_NAME: &'static str = "nothing";
+pub const CLIENT_IFACE_NAME: &'static str = "starnixethx1";
 pub const SERVER_IFACE_NAME: &'static str = "server-ep";
