0762dbc102fc4c1a61f03a9fd5f4996cf43c039c
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2024-01-11 04:46:49 +0000

    [netstack] Migrate benchmarks with tracing away from Dart
    Fixed: 305080566
    Cq-Include-Trybots: luci.fuchsia.try:terminal.vim3-release
    Change-Id: I9bbfe42a16bb5776c47e0da28c1d65587a5561f1
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/970794
    Reviewed-by: Peter Johnston <peterjohnston@google.com>
    Reviewed-by: Bruno Dal Bo <brunodalbo@google.com>
    Reviewed-by: Mark Seaborn <mseaborn@google.com>
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

---
+++ src/connectivity/network/tests/benchmarks/with-tracing/test.py
@@ -0,0 +1,88 @@
+# Copyright 2024 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import pathlib
+
+from fuchsia_base_test import fuchsia_base_test
+from mobly import asserts, test_runner
+
+
+# Runs a test where the purpose is not to produce performance metrics as
+# output, but just to produce a trace for manual inspection.
+#
+# When the test is run on Infra, the trace is made available for download via
+# the "task outputs" link. Currently this host-side wrapper code is needed for
+# making the trace available that way, but in the future that could be done
+# by using "ffx test" to run the test on Infra (see https://fxbug.dev/125224).
+class NetstackBenchmarksWithTracingTest(fuchsia_base_test.FuchsiaBaseTest):
+    def setup_test(self) -> None:
+        super().setup_test()
+        self.device: fuchsia_device.FuchsiaDevice = self.fuchsia_devices[0]
+        self.skip_netstack2 = self.user_params["skip_netstack2"]
+
+    def test_loopback_socket_benchmarks_with_tracing(self):
+        asserts.skip_if(
+            self.skip_netstack2, "Skipping netstack2 tests per user params"
+        )
+        self._run_test(
+            "loopback-socket-benchmarks-with-tracing-pkg-netstack2",
+            "loopback-socket-benchmarks-with-tracing",
+        )
+
+    def test_loopback_socket_benchmarks_with_fast_udp_tracing(self):
+        asserts.skip_if(
+            self.skip_netstack2, "Skipping netstack2 tests per user params"
+        )
+        self._run_test(
+            "loopback-socket-benchmarks-with-tracing-pkg-netstack2",
+            "loopback-socket-benchmarks-with-fast-udp-tracing",
+        )
+
+    def test_loopback_socket_benchmarks_with_netstack3_tracing(self):
+        self._run_test(
+            "loopback-socket-benchmarks-with-tracing-pkg-netstack3",
+            "loopback-socket-benchmarks-with-netstack3-tracing",
+        )
+
+    def test_tun_socket_benchmarks_with_tracing(self):
+        asserts.skip_if(
+            self.skip_netstack2, "Skipping netstack2 tests per user params"
+        )
+        self._run_test(
+            "tun-socket-benchmarks-tests-netstack2",
+            "tun-socket-benchmarks-netstack2-with-tracing",
+        )
+
+    def test_tun_socket_benchmarks_with_netstack3_tracing(self):
+        self._run_test(
+            "tun-socket-benchmarks-tests-netstack3",
+            "tun-socket-benchmarks-netstack3-with-tracing",
+        )
+
+    def _run_test(self, package_name: str, component_name: str):
+        self.device.ffx.run_test_component(
+            f"fuchsia-pkg://fuchsia.com/{package_name}#meta/{component_name}.cm",
+            ffx_test_args=[
+                "--realm",
+                "/core/testing:system-tests",
+                "--output-directory",
+                self.test_case_path,
+            ],
+            timeout=None,
+            capture_output=False,
+        )
+        test_result_files = list(
+            pathlib.Path(self.test_case_path).rglob("trace.fxt")
+        )
+        asserts.assert_equal(len(test_result_files), 1)
+        dest_file = os.path.join(
+            self.log_path,
+            f"{package_name}-{component_name}-trace.fxt",
+        )
+        os.rename(test_result_files[0], dest_file)
+
+
+if __name__ == "__main__":
+    test_runner.main()
