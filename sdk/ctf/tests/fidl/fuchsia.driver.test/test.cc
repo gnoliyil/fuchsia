550d3d0225e5ce0835e1c8e58d560e8bdeb04f5c
Novin Changizi <novinc@google.com>
2024-01-26 19:28:49 +0000

    [ctf][driver.test] Add back driver.test ctf suite
    Adds back the fuchsia.driver.test ctf test. Updates it to not use devfs
    but instead query for the node through the driver.development protocol.
    
    Bug: 290645124
    Change-Id: Ic723885c42392242e249002200462e990a0b50ad
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/976548
    Reviewed-by: Kendal Harland <kjharland@google.com>
    Commit-Queue: Novin Changizi <novinc@google.com>

--- sdk/ctf/tests/fidl/fuchsia.driver.test/test.cc
+++ sdk/ctf/tests/fidl/fuchsia.driver.test/test.cc
@@ -1,53 +1,63 @@
-// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Copyright 2024 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <fuchsia/driver/test/cpp/fidl.h>
-#include <fuchsia/io/cpp/fidl.h>
-#include <lib/device-watcher/cpp/device-watcher.h>
+#include <fidl/fuchsia.driver.development/cpp/fidl.h>
+#include <fidl/fuchsia.driver.test/cpp/fidl.h>
+#include <lib/component/incoming/cpp/protocol.h>
 #include <lib/fdio/directory.h>
-#include <lib/sys/cpp/component_context.h>
-#include <lib/syslog/cpp/log_settings.h>
-#include <lib/syslog/cpp/macros.h>
 
 #include <zxtest/zxtest.h>
 
-void StartDriverTestRealm() {
-  // Connect to DriverTestRealm.
-  fuchsia::driver::test::RealmSyncPtr client;
-  auto context = sys::ComponentContext::Create();
-  context->svc()->Connect(client.NewRequest());
-
-  // Start the DriverTestRealm with correct arguments.
-  fuchsia::driver::test::RealmArgs args;
-
-  auto interface = fidl::InterfaceHandle<fuchsia::io::Directory>();
-  zx_status_t status = fdio_open("/pkg",
-                                 static_cast<uint32_t>(fuchsia::io::OpenFlags::DIRECTORY |
-                                                       fuchsia::io::OpenFlags::RIGHT_READABLE |
-                                                       fuchsia::io::OpenFlags::RIGHT_EXECUTABLE),
-                                 interface.NewRequest().TakeChannel().release());
-  if (status != ZX_OK) {
-    return;
-  }
-  args.set_boot(std::move(interface));
-  args.set_root_driver("#meta/test-parent-sys.cm");
-
-  fuchsia::driver::test::Realm_Start_Result result;
-  auto call_result = client->Start(std::move(args), &result);
-  if (call_result != ZX_OK) {
-    FX_SLOG(ERROR, "Failed to call to Realm:Start", FX_KV("call_result", call_result));
-    return;
-  }
-  if (result.is_err()) {
-    FX_SLOG(ERROR, "Realm:Start failed", FX_KV("error", result.err()));
-    return;
-  }
-}
+namespace {
 
 TEST(DriverTestRealmCts, DriverWasLoaded) {
-  fuchsia_logging::SetTags({"driver_test_realm_test"});
-  StartDriverTestRealm();
+  // Connect to DriverTestRealm.
+  auto dtr_client_result = component::Connect<fuchsia_driver_test::Realm>();
+  ASSERT_OK(dtr_client_result);
+  fidl::SyncClient<fuchsia_driver_test::Realm> dtr_client(std::move(dtr_client_result.value()));
 
-  ASSERT_EQ(ZX_OK, device_watcher::RecursiveWaitForFile("/dev/sys/test").status_value());
+  // Start the DriverTestRealm.
+  fuchsia_driver_test::RealmArgs args{{
+      .use_driver_framework_v2 = true,
+  }};
+  fidl::Result<fuchsia_driver_test::Realm::Start> start_result = dtr_client->Start(std::move(args));
+  ASSERT_TRUE(start_result.is_ok());
+
+  // Connect to the driver development manager.
+  auto fdd_client_result = component::Connect<fuchsia_driver_development::Manager>();
+  ASSERT_OK(fdd_client_result);
+  fidl::SyncClient<fuchsia_driver_development::Manager> fdd_client(
+      std::move(fdd_client_result.value()));
+
+  // Keep checking nodes until we see dev.sys.test which is created by the root driver.
+  while (true) {
+    auto node_info_iter_endpoints_result =
+        fidl::CreateEndpoints<fuchsia_driver_development::NodeInfoIterator>();
+    ASSERT_OK(node_info_iter_endpoints_result);
+
+    // Filtering for an exact match of the node moniker.
+    auto get_node_info_result = fdd_client->GetNodeInfo({{
+        .node_filter = {"dev.sys.test"},
+        .iterator = std::move(node_info_iter_endpoints_result.value().server),
+        .exact_match = true,
+    }});
+    ASSERT_TRUE(get_node_info_result.is_ok());
+
+    fidl::SyncClient<fuchsia_driver_development::NodeInfoIterator> node_info_iter_client(
+        std::move(node_info_iter_endpoints_result.value().client));
+
+    auto node_info_result = node_info_iter_client->GetNext();
+    ASSERT_TRUE(node_info_result.is_ok());
+
+    // Since we filtered for an exact match, there should only be 1 when it is found.
+    if (node_info_result.value().nodes().size() == 1) {
+      // Makes sure the filtering was correct.
+      ASSERT_EQ(node_info_result.value().nodes()[0].versioned_info()->v2()->moniker().value(),
+                "dev.sys.test");
+      break;
+    }
+  }
 }
+
+}  // namespace
