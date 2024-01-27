642155668a0ea2584a548f76f5ae7d26758fd08e
Ghanan Gowripalan <ghanan@google.com>
2024-01-27 02:41:43 +0000

    [starnix/tests] Use `fbl::unique_fd` in memfd tests
    Change-Id: Ife0b0d828aca64df97f225bc4b0da6ccc9d49446
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/980646
    Fuchsia-Auto-Submit: Ghanan Gowripalan <ghanan@google.com>
    Reviewed-by: Adam Perry <adamperry@google.com>
    Commit-Queue: Ghanan Gowripalan <ghanan@google.com>

--- src/starnix/tests/syscalls/cpp/memfd_test.cc
+++ src/starnix/tests/syscalls/cpp/memfd_test.cc
@@ -10,6 +10,7 @@
 // TODO(b/307959737): Build these tests without glibc.
 #include <sys/mman.h>
 
+#include <fbl/unique_fd.h>
 #include <gtest/gtest.h>
 
 #if !defined(__NR_memfd_create)
@@ -38,28 +39,30 @@ const int kPageSize = 4096;
 
 class MemfdTest : public ::testing::Test {
   void SetUp() override {
-    fd_ = static_cast<int>(syscall(__NR_memfd_create, "test_memfd", MFD_ALLOW_SEALING));
+    ASSERT_TRUE(fd_ = fbl::unique_fd(
+                    static_cast<int>(syscall(__NR_memfd_create, "test_memfd", MFD_ALLOW_SEALING))))
+        << strerror(errno);
   }
 
   void TearDown() override {
-    close(fd_);
     if (addr_ != MAP_FAILED) {
       ASSERT_EQ(munmap(addr_, kPageSize), 0);
     }
+    fd_.reset();
   }
 
  protected:
-  int fd_ = -1;
+  fbl::unique_fd fd_;
   void* addr_ = MAP_FAILED;
 };
 
 // These tests currently test only `F_SEAL_FUTURE_WRITE`. `F_SEAL_WRITE` is covered
 // by GVisor tests.
 TEST_F(MemfdTest, SealFutureWriteThenMmap) {
-  ASSERT_EQ(fcntl(fd_, F_ADD_SEALS, F_SEAL_FUTURE_WRITE), 0);
+  ASSERT_EQ(fcntl(fd_.get(), F_ADD_SEALS, F_SEAL_FUTURE_WRITE), 0);
 
   // Readable mapping succeeds even when created from writable FD.
-  addr_ = mmap(0, kPageSize, PROT_READ, MAP_SHARED, fd_, 0);
+  addr_ = mmap(0, kPageSize, PROT_READ, MAP_SHARED, fd_.get(), 0);
   ASSERT_NE(addr_, MAP_FAILED);
 
   // `mprotect()` should fail since the file was sealed for write.
@@ -67,10 +70,10 @@ TEST_F(MemfdTest, SealFutureWriteThenMmap) {
 }
 
 TEST_F(MemfdTest, MmapThenSealFutureWrite) {
-  addr_ = mmap(0, kPageSize, PROT_READ, MAP_SHARED, fd_, 0);
+  addr_ = mmap(0, kPageSize, PROT_READ, MAP_SHARED, fd_.get(), 0);
   ASSERT_NE(addr_, MAP_FAILED);
 
-  ASSERT_EQ(fcntl(fd_, F_ADD_SEALS, F_SEAL_FUTURE_WRITE), 0);
+  ASSERT_EQ(fcntl(fd_.get(), F_ADD_SEALS, F_SEAL_FUTURE_WRITE), 0);
 
   // `mprotect()` still succeed since the mapping was created before the seal.
   ASSERT_EQ(mprotect(addr_, kPageSize, PROT_READ | PROT_WRITE), 0);
@@ -79,11 +82,11 @@ TEST_F(MemfdTest, MmapThenSealFutureWrite) {
 // These tests currently test `F_SEAL_FUTURE_WRITE`. `F_SEAL_WRITE` is covered
 // by GVisor tests.
 TEST_F(MemfdTest, MapWritableThenSealFutureWrite) {
-  addr_ = mmap(0, kPageSize, PROT_READ | PROT_WRITE, MAP_SHARED, fd_, 0);
+  addr_ = mmap(0, kPageSize, PROT_READ | PROT_WRITE, MAP_SHARED, fd_.get(), 0);
   ASSERT_NE(addr_, MAP_FAILED);
 
   // `F_SEAL_FUTURE_WRITE` should still succeed when there are writable mappings.
-  ASSERT_EQ(fcntl(fd_, F_ADD_SEALS, F_SEAL_FUTURE_WRITE), 0);
+  ASSERT_EQ(fcntl(fd_.get(), F_ADD_SEALS, F_SEAL_FUTURE_WRITE), 0);
 }
 
 }  // namespace
